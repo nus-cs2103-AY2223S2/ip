ba7321493f7778eef668eb22592632e937f67a7b
Sean Leong <70213029+seanleong339@users.noreply.github.com>
2022-12-29 13:56:26 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,8 @@
+import java.util.Scanner;
 public class Duke {
+    private static final String greeting = "____________________________________________________________\n" + " Hello! I'm Duke\n" + " What can I do for you?\n" + "____________________________________________________________";
+    private static final String goodbye = "____________________________________________________________\n" + " Bye. Hope to see you again soon!\n" + "____________________________________________________________";
+    private static boolean online = true;
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +10,20 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println(greeting);
+
+        while(online) {
+            String input = sc.nextLine();
+            if(input.equals("bye")) {
+                online = false;
+                System.out.println(goodbye);
+            }
+            else {
+                System.out.println("____________________________________________________________\n" + input + "\n" + "____________________________________________________________");
+            }
+        }
     }
 }
