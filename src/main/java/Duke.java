dada1aeeadf967230100704fa0de9eb74274331c
anchengyang <anchengyang72@gmail.com>
2023-01-11 16:38:57 +0800

    Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,21 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\nL"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("    " + "Hello I'm Monkey D Luffy\n    What can I do for you?");
+
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+
+        while (!input.equals("bye")) {
+            System.out.println("    " + input);
+            input = sc.nextLine();
+        }
+        System.out.println("    " + "Bye. Hope to see you again soon!");
     }
 }
