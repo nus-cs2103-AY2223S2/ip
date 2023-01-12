ee3ff0c30fa3cbd53ffa57f782d7648dabb23c56
yyj-02 <yeohyongjie@outlook.com>
2023-01-12 22:54:52 +0800

    Level-1(RequestHandler): add javadoc

--- src/main/java/RequestHandler.java
+++ src/main/java/RequestHandler.java
@@ -2,12 +2,16 @@ public class RequestHandler {
     String request;
     /**
      * Constructor for the request handler.
-     * @param request the request message
+     * @param request   the request message
      */
     public RequestHandler(String request) {
         this.request = request;
     }
 
+    /**
+     * Provide a sanitised reply
+     * @return  A sanitised response string.
+     */
     public String getReply() {
         return this.request;
     }
