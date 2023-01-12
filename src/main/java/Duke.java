909131dab1b8a6bc048470f339389a4af48ef0e6
gmengqi <1014meng@gmail.com>
2023-01-12 10:52:31 +0800

    greet, echo and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,25 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void greet() {
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+    }
+
+    public static void echo(String command) {
+        if (command.equals("bye"))  {
+            exit();
+        } else {
+            System.out.println(command);
+        }
+    }
+    public static void exit() {
+        System.out.println("Bye. Hope to see you again soon!");
+        sc.close();
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +27,13 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        greet();
+        try{
+            while (sc.hasNext()) {
+                echo(sc.nextLine());
+            }
+        } catch (IllegalStateException e) {
+            System.exit(0);
+        }
     }
 }
