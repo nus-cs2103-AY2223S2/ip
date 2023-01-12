96f371198e904eeb78f8786bfcb0cc6f9fc9bf21
yyj-02 <yeohyongjie@outlook.com>
2023-01-12 11:30:26 +0800

    Level-0: add greet messages

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,28 @@
 public class Duke {
+    public static void returnFormattedResponse(String response) {
+        final int INDENTS = 4;
+
+        String lineAddedResponse = "____________________________________________________________\n"
+                + " " + response.replace("\n", "\n" + " ")
+                + "\n____________________________________________________________\n";
+        String indentedResponse = " ".repeat(INDENTS)
+                + lineAddedResponse.replace("\n", "\n" + " ".repeat(INDENTS));
+
+        System.out.println(indentedResponse);;
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        final String INTRODUCTION_MESSAGE = "Hello! I'm Duke\n"
+                + "What can I do for you?";
+        final String EXITING_MESSAGE = "Bye. Hope to see you again soon!";
+
+        System.out.println(logo);
+        returnFormattedResponse(INTRODUCTION_MESSAGE);
+        returnFormattedResponse(EXITING_MESSAGE);
     }
 }
