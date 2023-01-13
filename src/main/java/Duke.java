9a5adb716e4e5d6759469d38e50a13df1b94336a
c0j0s <c0j0s@outlook.com>
2023-01-13 19:50:36 +0800

    Add Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,26 +14,40 @@ public class Duke {
         String greeting = "What can I do for you?\n";
         System.out.println(greeting);
 
-        List<String> list = new ArrayList<String>();
+        List<Task> list = new ArrayList<Task>();
         Scanner sc = new Scanner(System.in);
 
         convo: while (true) {
             System.out.print("You : ");
-            String input = sc.nextLine().toLowerCase();
-            switch (input) {
+            String[] input = sc.nextLine().toLowerCase().split(" ");
+            int idx = 0;
+            switch (input[0]) {
                 case "bye":
                     System.out.println("Duke: Bye. Hope to see you again soon!");
                     break convo;
                 case "list":
+                    System.out.println("Here are the tasks in your list:");
                     for (int i = 0; i < list.size(); i++) {
                         System.out.println(String.format("%d. %s", i + 1, list.get(i)));
                     }
                     break;
+                case "mark":
+                    System.out.println("Nice! I've marked this task as done:");
+                    idx = Integer.parseInt(input[1]);
+                    list.get(idx - 1).markAsDone();
+                    System.out.println(list.get(idx - 1));
+                    break;
+                case "unmark":
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    idx = Integer.parseInt(input[1]);
+                    list.get(idx - 1).unmarkDone();
+                    System.out.println(list.get(idx - 1));
+                    break;
                 case "":
                     break;
                 default:
-                    System.out.println("Duke: added " + input);
-                    list.add(input);
+                    System.out.println("Duke: added " + input[0]);
+                    list.add(new Task(input[0]));
                     break;
             }
         }
---
+++ src/main/java/Task.java
@@ -0,0 +1,29 @@
+/**
+ * Task
+ */
+public class Task {
+    private String title;
+    private boolean isDone;
+
+    public Task(String title) {
+        this.title = title;
+        this.isDone = false;
+    }
+
+    private String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public void markAsDone() {
+        isDone = true;
+    }
+
+    public void unmarkDone() {
+        isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s] %s", getStatusIcon(), title);
+    }
+}
