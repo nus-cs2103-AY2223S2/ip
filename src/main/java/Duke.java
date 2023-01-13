1a22bb6b4977e479857c51992e65f14e3c3f69dd
YuanXun9834 <lollin9834@gmail.com>
2023-01-13 17:31:11 +0800

    Complete Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +7,27 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        line();
+        indent("Hi, I'm Duke ");
+        indent("What can I do for you :) ?\n");
+        line();
+
+        Scanner scanner = new Scanner(System.in);
+        String echo = scanner.nextLine();
+        while(!echo.equals("bye")){
+            indent(echo + "\n");
+            line();
+            echo = scanner.nextLine();
+        }
+        indent("Bye. Hope to hear from you again!");
+        line();
+    }
+
+    public static void indent(String txt){
+        System.out.println("     " + txt);
+    }
+
+    public static void line(){
+        System.out.println("_________________________________________________________________");
     }
 }
