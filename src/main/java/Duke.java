fe6a536befcdb3e485eb7362fa01eedbd838c49b
chuakid <chuakid@gmail.com>
2023-01-13 16:12:26 +0800

    Duke class: add to list functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,13 +4,20 @@ public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello, Duke here. How can I help you?");
         Scanner sc = new Scanner(System.in);
-        while (true) {
-            String userLine = sc.nextLine();
-            if (userLine.equals("bye")) {
-                System.out.println("Bye, hope to see you again.");
-                break;
+        String[] list = new String[100];
+        int itemCount = 0;
+        String userLine = "";
+        while (!userLine.equals("bye")) {
+            userLine = sc.nextLine();
+            switch (userLine) {
+                case "bye":
+                    System.out.println("Bye, hope to see you again.");
+                    break;
+                default:
+                    list[itemCount] = userLine;
+                    itemCount++;
+                    System.out.println("Added: " + userLine);
             }
-            System.out.println(userLine);
         }
     }
 }
