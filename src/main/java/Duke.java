42efe14c2c058a2009af3f64b2021171ece7ff2a
VietAnh1010 <vietanh101003@gmail.com>
2023-01-14 01:22:49 +0800

    Add the ability to mark tasks as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,12 +7,11 @@ import java.util.stream.IntStream;
 
 public class Duke {
 
-    private static final String LINE =
-            " ".repeat(4) + "____________________________________________________________";
+    private static final String LINE = " ".repeat(4) + "____________________________________________________________";
 
     private static final String INDENTATION = " ".repeat(5);
 
-    private static final List<String> addedTexts = new ArrayList<>();
+    private static final List<Task> addedTasks = new ArrayList<>();
 
     private static void echo(String msg) {
         String displayedMsg = Arrays.stream(msg.split("\n")).map(line -> INDENTATION + line)
@@ -22,17 +21,46 @@ public class Duke {
         System.out.println(LINE);
     }
 
-    private static void addText(String msg) {
-        addedTexts.add(msg);
+    private static void addTask(String msg) {
+        addedTasks.add(new Task(msg));
         String displayedMsg = "added: " + msg;
         echo(displayedMsg);
     }
 
-    private static void listTexts() {
-        String displayedMsg = IntStream.range(0, addedTexts.size())
-                .mapToObj(i -> String.format("%d. %s", i + 1, addedTexts.get(i)))
-                .collect(Collectors.joining("\n"));
+    private static void listTasks() {
+        String displayedMsg = "Here are the tasks in your list:\n" +
+                IntStream.range(0, addedTasks.size()).mapToObj(
+                        i -> String.format("%d.%s", i + 1, addedTasks.get(i)))
+                        .collect(Collectors.joining("\n"));
         echo(displayedMsg);
+    }
+
+    // Note that, indexes start from 1
+    private static void markTaskAsDone(int index) {
+        Task t = addedTasks.get(index - 1);
+        t.markAsDone();
+        echo("Nice! I've marked this task as done:\n  " + t);
+    }
+
+    private static void markTaskAsNotDone(int index) {
+        Task t = addedTasks.get(index - 1);
+        t.markAsNotDone();
+        echo("OK, I've marked this task as not done yet:\n  " + t);
+    }
+
+    /**
+     * Extracts an index from {@code mark} or {@code unmark} command.
+     * 
+     * @param cmd the string represents the command
+     * @return The extracted index
+     */
+    private static int extractIndexFromCommand(String cmd) {
+        int whitespaceIndex = cmd.indexOf(" ");
+        if (whitespaceIndex < 0) {
+            throw new IllegalArgumentException("There is no whitespace in this command");
+        }
+        String intPart = cmd.substring(whitespaceIndex).strip();
+        return Integer.parseInt(intPart);
     }
 
     public static void main(String[] args) {
@@ -44,9 +72,13 @@ public class Duke {
                     echo("Bye. Hope to see you again soon!");
                     break;
                 } else if (in.equals("list")) {
-                    listTexts();
+                    listTasks();
+                } else if (in.startsWith("mark")) {
+                    markTaskAsDone(extractIndexFromCommand(in));
+                } else if (in.startsWith("unmark")) {
+                    markTaskAsNotDone(extractIndexFromCommand(in));
                 } else {
-                    addText(in);
+                    addTask(in);
                 }
             } while (true);
         }
---
+++ src/main/java/Task.java
@@ -0,0 +1,28 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void markAsDone() {
+        if (isDone) {
+            throw new IllegalStateException("Task is already marked as done");
+        }
+        isDone = true;
+    }
+
+    public void markAsNotDone() {
+        if (!isDone) {
+            throw new IllegalStateException("Task is already marked as not done");
+        }
+        isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s] %s", isDone ? "X" : " ", description);
+    }
+}
