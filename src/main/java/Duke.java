b357f6adb4058474296c0b52f1f874d4e456a946
YuanXun9834 <lollin9834@gmail.com>
2023-01-13 18:24:23 +0800

    Complete Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,21 +13,43 @@ public class Duke {
         line();
 
         Scanner scanner = new Scanner(System.in);
-        String echo = scanner.nextLine();
-        while(!echo.equals("bye")){
-            indent(echo + "\n");
-            line();
-            echo = scanner.nextLine();
+        String[] list = new String[100];
+        int index = 0;
+        String input = scanner.nextLine();
+        while(!input.equals("bye")){
+            switch(input) {
+                case "list":
+                    line();
+                    printList(list, index);
+                    line();
+                    break;
+                default:
+                    line();
+                    indent("added: " + input + "\n");
+                    line();
+                    list[index] = input;
+                    index++;
+            }
+            input = scanner.nextLine();
         }
+        line();
         indent("Bye. Hope to hear from you again!");
         line();
     }
 
     public static void indent(String txt){
-        System.out.println("     " + txt);
+        System.out.println("     " + txt );
     }
 
     public static void line(){
         System.out.println("_________________________________________________________________");
+    }
+
+    public static void printList(String[] list, int index){
+        for (int i = 0; i < index ; i++){
+            int num = i + 1;
+            String output = num + ". " + list[i];
+            indent(output);
+        }
     }
 }
