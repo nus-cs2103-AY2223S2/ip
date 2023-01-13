97a28a780f7846ae21570cc7598b22e298fd5ab6
dillongoh <gohjerwee@gmail.com>
2023-01-13 22:22:51 +0800

    modified duke.java to greet, echo and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Hello! I'm TunaBot\n" +
+                " What can I do for you?");
+        boolean toExit = false;
+        while (!toExit) {
+            String command = s.next();
+            System.out.println("--------------------------");
+            switch (command) {
+                case "bye":
+                    toExit = true;
+                    System.out.println("Bye! Blub blub!");
+                    break;
+                default:
+                    System.out.println(command);
+            }
+            System.out.println("--------------------------");
+        }
     }
 }
