94e8d5d4060d297c7737c9dbdd0fbd1357fe5b7b
WillCWX <xuanwilliam@yahoo.com>
2023-01-13 22:15:42 +0800

    Added Duke's Greeting, Echo and Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,61 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        greet();
+        dukeLoop();
+    }
+
+    private static void greet() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("What can I do for you?");
+        System.out.println("---------------------\n");
     }
+
+    /**
+     * Runs the core, repeating functions of Duke
+     */
+    private static void dukeLoop() {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.print(" You: ");
+            String input = sc.nextLine();
+
+            switch (input) {
+                case "bye":
+                    sc.close();
+                    exit();
+                    return;
+                default:
+                    echo(input);
+                    break;
+            }
+        }
+
+    }
+
+    /**
+     * Prints the partitions, ----, and prints the string in-between
+     * 
+     * @param w - The string in between the ---- partitions
+     */
+    private static void printWithPartition(String w) {
+        System.out.println("---------------------");
+        System.out.println(w);
+        System.out.println("---------------------");
+    }
+
+    private static void echo(String w) {
+        printWithPartition("Duke: " + w);
+    }
+
+    private static void exit() {
+        printWithPartition("Duke: " + "Goodbye!");
+    }
+
 }
