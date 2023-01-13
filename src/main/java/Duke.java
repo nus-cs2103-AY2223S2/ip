8844df882bfd0cd7db3bd1df1952d0b35ce28ed0
VietAnh1010 <vietanh101003@gmail.com>
2023-01-13 23:56:27 +0800

    Add the ability to store and display texts

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Duke {
 
@@ -9,12 +12,27 @@ public class Duke {
 
     private static final String INDENTATION = " ".repeat(5);
 
+    private static final List<String> addedTexts = new ArrayList<>();
+
     private static void echo(String msg) {
         String displayedMsg = Arrays.stream(msg.split("\n")).map(line -> INDENTATION + line)
                 .collect(Collectors.joining("\n"));
         System.out.println(LINE);
         System.out.println(displayedMsg);
         System.out.println(LINE);
+    }
+
+    private static void addText(String msg) {
+        addedTexts.add(msg);
+        String displayedMsg = "added: " + msg;
+        echo(displayedMsg);
+    }
+
+    private static void listTexts() {
+        String displayedMsg = IntStream.range(0, addedTexts.size())
+                .mapToObj(i -> String.format("%d. %s", i + 1, addedTexts.get(i)))
+                .collect(Collectors.joining("\n"));
+        echo(displayedMsg);
     }
 
     public static void main(String[] args) {
@@ -25,8 +43,11 @@ public class Duke {
                 if (in.equals("bye")) {
                     echo("Bye. Hope to see you again soon!");
                     break;
+                } else if (in.equals("list")) {
+                    listTexts();
+                } else {
+                    addText(in);
                 }
-                echo(in);
             } while (true);
         }
     }
