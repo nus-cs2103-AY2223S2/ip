0a33e45eaa22cbced7cb461c3f735c264870b85b
dillongoh <gohjerwee@gmail.com>
2023-01-13 22:41:14 +0800

    added Add and List features

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,24 +1,31 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-
+        ArrayList<String> tasks = new ArrayList<String>();
         System.out.println("Hello! I'm TunaBot\n" +
-                " What can I do for you?");
+                "What can I do for you?");
         boolean toExit = false;
         while (!toExit) {
-            String command = s.next();
+            String command = s.nextLine();
             System.out.println("--------------------------");
             switch (command) {
+                case "list":
+                    for (int i = 1; i <= tasks.size(); i++) {
+                        System.out.println(i + ". " + tasks.get(i - 1));
+                    }
+                    break;
                 case "bye":
                     toExit = true;
                     System.out.println("Bye! Blub blub!");
                     break;
                 default:
-                    System.out.println(command);
+                    tasks.add(command);
+                    System.out.println("blub! added: " + command);
             }
             System.out.println("--------------------------");
         }
