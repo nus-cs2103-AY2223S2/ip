b7612c76af670b04c336417926912c51aaf653bd
chuakid <chuakid@gmail.com>
2023-01-13 15:54:17 +0800

    Duke class: add bye and repeating input capability

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,16 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello, Duke here. How can I help you?");
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String userLine = sc.nextLine();
+            if (userLine.equals("bye")) {
+                System.out.println("Bye, hope to see you again.");
+                break;
+            }
+            System.out.println(userLine);
+        }
     }
 }
