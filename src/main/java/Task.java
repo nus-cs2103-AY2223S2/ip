4c806c401a6673616b1856f4a7f71d9a82da48fc
zrei <88964793+zrei@users.noreply.github.com>
2023-01-13 22:09:33 +0800

    Add Task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,11 @@
+public class Task {
+    private boolean isDone = false;
+
+    public void setDone(boolean status) {
+        this.isDone = status;
+    }
+
+    public boolean isDone() {
+        return this.isDone;
+    }
+}
