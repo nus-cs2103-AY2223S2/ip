3b88a7d3525e33cae5b81a91a8e73bb729d9fc50
soowengkit <>
2023-01-13 19:11:32 +0800

    Able to add tasks to list, and list tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,31 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Duke {
     public static Scanner instr = new Scanner(System.in);
+    public static List<Tasks> todoList = new ArrayList<>(100);
     public static void Greet() {
-        System.out.println("Hello! I'm Duke\nWhat can I do for you?\n(To terminate write 'bye')");
+        System.out.println("Hello! I'm Duke\nWhat can I do for you? "+
+                "\n('bye' to terminate)" +
+                "\n('list to access list of tasks)");
     }
     public static void Echo(String str) {
         if(str.equals("bye")){
             Exit();
         }
         else {
-            System.out.println(str);
+            if (str.equals("list")){
+                int n = 1;
+                for(Tasks t: todoList) {
+                    System.out.println(n + ". " + t.getDesc());
+                    n++;
+                }
+            }
+            else {
+                todoList.add(new Tasks(str));
+                System.out.println("added: " + str);
+            }
         }
     }
     public static void Exit() {
---
+++ src/main/java/Tasks.java
@@ -0,0 +1,9 @@
+public class Tasks {
+    final protected String desc;
+    public Tasks(String str) {
+        this.desc = str;
+    }
+    public String getDesc(){
+        return desc;
+    }
+}
