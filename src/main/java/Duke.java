ce7553513d150efb305ede968511e89dcf54a050
leeshaowee <lee_shao_wee@hotmail.com>
2023-01-13 21:20:01 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,34 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static void printRowLine() {
+        println("_________________________________________");
+    }
+
+    public static void println(String s) {
+        System.out.println("\t" + s);
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        printRowLine();
+        println("\tHi There! I'm Shao");
+        println("\tWhat can I do for you?");
+        printRowLine();
+        Scanner scan = new Scanner(System.in);
+        while (true) {
+            String input = scan.next();
+            if (input.equalsIgnoreCase("bye")) {
+                printRowLine();
+                println("Bye! Have a nice day!");
+                printRowLine();
+                println("");
+                break;
+            }
+            printRowLine();
+            println(input);
+            printRowLine();
+            println("");
+        }
+        scan.close();
     }
 }
