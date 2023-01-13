8b6cdfba42a4e3ee62c8ddc69eee6d7e870b939c
zrei <88964793+zrei@users.noreply.github.com>
2023-01-13 21:42:53 +0800

    Implement level-1
    Make duke echo commands and exit when user types "bye".

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,21 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        String greeting = "Hello! I'm Duke\n"
+                    + "What can I do for you?\n";
+        String separator = "____________________________________________________________\n";
+        String goodbye = "Bye. Hope to see you again soon!\n";
+        String endCommand = "bye";
+        System.out.println(greeting + separator);
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String command = sc.next();
+            if (command.equals(endCommand)) {
+                sc.close();
+                System.out.println(goodbye + separator);
+                break;
+            }
+            System.out.println(command + "\n" + separator);
+        }
     }
 }
