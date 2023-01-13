f472592c9882a346cfe2693ae2904253db73ec9d
ChinJunAn <junan1599@gmail.com>
2023-01-13 16:07:05 +0800

    Program can gather user input and echo back. Input stops when user enters "bye".

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,18 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?\n");
+        boolean flag = true;
+        while (flag) {
+            Scanner sc = new Scanner(System.in);
+            String inp = sc.next();
+            if (inp.equals("bye")) {
+                flag = false;
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+            System.out.println(inp+"\n");
+        }
     }
 }
