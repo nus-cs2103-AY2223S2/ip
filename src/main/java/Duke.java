1771fdc376b5b8d25de45807a2d65f8fa2133bb4
Yu Heng Lee <lee2000yh@gmail.com>
2023-01-13 20:08:20 +0800

    Level 2 Done
    added add to list and print list functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,25 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     static String line_break = " \n\t ^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^ \n";
+    static ArrayList<String> items;
     public static void main(String[] args) {
         Scanner usr_in = new Scanner(System.in);
+        items = new ArrayList<>();
         greet();
         // Start user input
-        while (usr_in.hasNext()) {
-            String curr_in = usr_in.next();
+        while (usr_in.hasNextLine()) {
+            String curr_in = usr_in.nextLine();
             if (curr_in.equals("bye")) {
-                end_Program();
+                end();
                 break;
+            } else if (curr_in.equals("list")) {
+                print();
             } else {
-                echo(curr_in);
+                //echo(curr_in);
+                add(curr_in);
             }
         }
     }
 
-    /*
-     * Greets user on start up
-     */
     private static void greet() {
         String logo = "             _____             _____   _    _              _   _ \n" +
                 "     /\\     |_   _|           / ____| | |  | |     /\\     | \\ | |\n" +
@@ -29,24 +34,42 @@ public class Duke {
                 " /_/    \\_\\ |_____|           \\_____| |_|  |_| /_/    \\_\\ |_| \\_|\n" +
                 "                                                                 \n" +
                 "                                                                 ";
-        System.out.println("\t Hello from\n" + logo);
+        System.out.println("\t Hello I'm\n" + logo);
         System.out.println("\t What can I do for you?" + line_break);
     }
 
+//    /*
+//     * Outputs an echo of user input.
+//     * @param in a String from user input
+//     */
+//    static void echo(String in) {
+//        System.out.println(line_break +
+//                        "\t " + in + " \n" +
+//                        line_break);
+//    }
+
     /*
-     * Outputs an echo of user input.
-     * @param in a String from user input
+     * adds item to items list
+     * @param item a string from user input to be added to items list
      */
-    static void echo(String in) {
-        System.out.println(line_break +
-                        "\t " + in + " \n" +
-                        line_break);
+    static void add(String item) {
+        items.add(item);
+        System.out.printf("%s \t added: %s \n %s", line_break, item, line_break);
     }
 
     /*
-     * Outputs simple terminating message
+     * prints out list of items
      */
-    static void end_Program() {
+    static void print() {
+        System.out.println(line_break);
+        for (int i = 0; i < items.size(); i++) {
+            String item = items.get(i);
+            System.out.printf("\t %d. %s\n", i + 1, item);
+        }
+        System.out.println(line_break);
+    }
+
+    static void end() {
         System.out.println(line_break +
                         "\t Bye. See you next time! \n" +
                         line_break);
