b98c6efe350d56d7395698617b584dd30cd82486
Wang Jiefan <wng.jiefan@gmail.com>
2023-01-13 22:00:04 +0800

    Level-1 skeleton

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,22 @@
+import java.util.Scanner;
+
+
 public class Duke {
     public static void main(String[] args) {
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+                + "|____/ \\__,_|_|\\_\\___| ,\n";
+        System.out.println("Hello, I'm\n" + logo + "how may I help?");
+
+        Scanner scanner = new Scanner(System.in);
+
+        while(scanner.hasNextLine()) {
+            String input = scanner.nextLine();
+            if (input.equals("exit")) break;
+            System.out.println(input);
+        }
     }
 }
