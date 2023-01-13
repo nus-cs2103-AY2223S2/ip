f372c042fcf3a2f5825e2dcf8741d0673ddf0db8
ChinJunAn <junan1599@gmail.com>
2023-01-13 16:23:55 +0800

    Added the ability to store user input into an arraylist. If the input is "list", all items in the list will be displayed. Exits when user input is "bye".

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
@@ -10,16 +12,27 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
 
         System.out.println("Hello! I'm Duke\nWhat can I do for you?\n");
+
         boolean flag = true;
+        List<String> ls = new ArrayList<>();
+
         while (flag) {
             Scanner sc = new Scanner(System.in);
-            String inp = sc.next();
-            if (inp.equals("bye")) {
-                flag = false;
-                System.out.println("Bye. Hope to see you again soon!");
-                break;
+            String inp = sc.nextLine();
+            switch (inp) {
+                case "list":
+                    for (int i = 0; i<ls.size();i++) {
+                        System.out.println(i+1 + ". " +ls.get(i));
+                    }
+                    break;
+                case "bye":
+                    System.out.println("Bye. Hope to see you again soon!");
+                    flag = false;
+                    break;
+                default:
+                    ls.add(inp);
+                    System.out.println("added: "+inp);
             }
-            System.out.println(inp+"\n");
         }
     }
 }
