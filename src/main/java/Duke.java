08ca6ef3266099d55b1a3309cc25565f235e56f4
Merrick <merrickneo@gmail.com>
2023-01-14 22:32:31 +0800

    Adjust divider between texts

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        String divider = "    ————————————————————————————————";
+        String logo = "     ____        _        \n"
+                + "    |  _ \\ _   _| | _____ \n"
+                + "    | | | | | | | |/ / _ \\\n"
+                + "    | |_| | |_| |   <  __/\n"
+                + "    |____/ \\__,_|_|\\_\\___|\n";
+        String divider = "    ____________________________________________________________";
         System.out.println(divider);
         System.out.println("    Hello! I'm Duke");
         System.out.println("    What can I do for you?");
