f0f1a3ffe8e64f1ea6caadd55445f0f76da3a943
Gloria Zhang Xinhui <glozxi@gmail.com>
2023-01-14 22:00:00 +0800

    Modify greeting

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,14 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Duke {
+
+    private static void greet() {
+        System.out.println("Hello, I am Duke.\n" +
+                "What can I do for you?");
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greet();
     }
 }
