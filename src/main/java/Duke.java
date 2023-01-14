0e2a37b5248b1c80696f5724919ac23e39d692e8
bokuanT <bokuan2000@gmail.com>
2023-01-14 17:25:55 +0800

    Added JavaDocs for level 0
    Added docs for Duke class, greet(), bidFarewell() and makeSeparation() methods

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,12 @@
+/**
+ * Project Duke is a educational software project designed to take you through
+ * the steps of building a small software incrementally, while applying as many
+ * Java and SE techniques as possible along the way.
+ *
+ * @author Bo Kuan (LG17)
+ * @version CS2103T AY22/23 Semester 2
+ */
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -12,17 +21,26 @@ public class Duke {
         bidFarewell();
     }
 
+    /**
+     * Prints out the greeting message and a line separation
+     */
     public static void greet() {
         System.out.println("Hello! I'm Duke\n" +
                 "What can I do for you?");
         makeSeperation();
     }
 
+    /**
+     * Prints out the goodbye message and a line separation
+     */
     public static void bidFarewell() {
         System.out.println("Bye. Hope to see you again soon!");
         makeSeperation();
     }
 
+    /**
+     * Prints out a line separation
+     */
     public static void makeSeperation() {
         System.out.println("____________________________________________________________");
     }
