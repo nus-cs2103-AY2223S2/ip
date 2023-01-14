1fbb633cb9023c3542ceeb558c705de42704f55c
Irminrics <irminrics@gmail.com>
2023-01-15 01:51:34 +0800

    Mark Level-3

--- src/main/java/Dude.java
+++ src/main/java/Dude.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 
 public class Dude {
-    private static String[] todoList = new String[100];
-    private static int itemCount = 0;
+    private static Task[] todoList = new Task[100];
+    private static int taskCount = 0;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -10,9 +10,11 @@ public class Dude {
 
         while(true) {
             String command = sc.nextLine();
-            getCommands(command);
-            if (command.equals("bye")) {
-                break;
+            if (!command.equals("")) {
+                getCommands(command);
+                if (command.equals("bye")) {
+                    break;
+                }
             }
         }
     }
@@ -32,7 +34,8 @@ public class Dude {
     }
 
     public static void getCommands(String command) {
-        switch(command) {
+        String cmd = command.split(" ")[0];
+        switch(cmd) {
             case "list":
                 System.out.println(" ____________________________________________________________");
                 getList();
@@ -46,24 +49,64 @@ public class Dude {
             case "bye":
                 System.out.println(" ____________________________________________________________");
                 System.out.println("\tCiaos! See you next time.");
+
+                System.out.println(" ____________________________________________________________\n");
+                break;
+            case "mark":
+                System.out.println(" ____________________________________________________________");
+                markTask(Integer.valueOf(command.split(" ")[1]) - 1);
+                System.out.println(" ____________________________________________________________\n");
+                break;
+            case "unmark":
+                System.out.println(" ____________________________________________________________");
+                unmarkTask(Integer.valueOf(command.split(" ")[1]) - 1);
                 System.out.println(" ____________________________________________________________\n");
                 break;
             default:
                 System.out.println(" ____________________________________________________________");
-                addItem(command);
+                addTask(command);
                 System.out.println("\tAdd liao: " + command);
                 System.out.println(" ____________________________________________________________\n");
         }
     }
 
-    public static void addItem(String item) {
-        todoList[itemCount] = item;
-        itemCount++;
+    public static void addTask(String description) {
+        Task task = new Task(description);
+        todoList[taskCount] = task;
+        taskCount++;
     }
 
     public static void getList() {
-        for(int i = 0; i < itemCount; i++) {
-            System.out.println("\t" + (i+1) + ". " + todoList[i]);
+        if (taskCount != 0) {
+            System.out.println("\tHere are the tasks in your list: ");
+            for (int i = 0; i < taskCount; i++) {
+                System.out.println("\t" + (i + 1) + "." + todoList[i].toString());
+            }
+        } else {
+            System.out.println("\tEh... You currently got no task leh.");
         }
     }
+
+    public static void markTask(int task) {
+        if (taskCount >= task - 1) {
+            System.out.println("\tSwee! I've marked this task as done loh:");
+            Task currentTask = todoList[task];
+            currentTask.mark();
+            System.out.println("\t" + currentTask.toString());
+        } else {
+            System.out.println("\tUhh... Where got this task for me to mark?");
+        }
+    }
+
+    public static void unmarkTask(int task) {
+        if (taskCount >= task - 1) {
+            System.out.println("\tOkay liar, I've marked this task as undone liao:");
+            Task currentTask = todoList[task];
+            currentTask.unmark();
+            System.out.println("\t" + currentTask.toString());
+        } else {
+            System.out.println("\tUhh... Where got this task for me to unmark?");
+        }
+    }
+
 }
