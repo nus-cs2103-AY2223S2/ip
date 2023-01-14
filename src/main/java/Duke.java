312ef3f8f33bbe0b84130c15098daa537628e9c1
bokuanT <bokuan2000@gmail.com>
2023-01-14 17:15:22 +0800

    Implemented Level 0. Greet
    Added greet(), bidFarewell() methods for Duke to say hi and bye

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,24 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        greet();
+
+        bidFarewell();
+    }
+
+    public static void greet() {
+        System.out.println("Hello! I'm Duke\n" +
+                "What can I do for you?");
+        makeSeperation();
+    }
+
+    public static void bidFarewell() {
+        System.out.println("Bye. Hope to see you again soon!");
+        makeSeperation();
+    }
+
+    public static void makeSeperation() {
+        System.out.println("____________________________________________________________");
     }
 }
