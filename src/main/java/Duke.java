2e5953a2e05e8198de0ecee56affe08a251d246a
ina <>
2023-01-14 17:26:25 +0800

    updated Duke.java with Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,26 @@
-import java.util.Scanner;
+import java.util.*;
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("---\n hi i'm Duke! what's up? \n---");
 
-        while(sc.hasNext()) {
+        List<String> list = new ArrayList<>();
+        while(sc.hasNextLine()) {
             String command = sc.nextLine();
             if (command.equals("bye")) {
                 System.out.println("---\n bye! see u soon! :-) \n---");
                 System.exit(0);
+            } else if (command.equals("list")) {
+                int index = 1;
+                System.out.println("---");
+                for (String a : list) {
+                    System.out.println(index + ". " + a);
+                    index++;
+                }
+                System.out.println("---");
             } else {
-                System.out.println("---\n" + command + "\n---");
+                list.add(command);
+                System.out.println("---\n added: " + command + "\n---");
             }
         }
     }
