421e213de12bdcb6f215ebd35e39151b3ac12969
TariqDroid <vegita2009@hotmail.com>
2023-01-14 13:54:31 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,19 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello fsrom\n" + logo);
+        System.out.println("Hello! I'm Duke\n" +
+                            "What can I do for you?\n" + logo);
+        Scanner input = new Scanner(System.in);
+        String echo = input.nextLine();
+        while(true){
+            if(echo.equals("bye")) {
+                System.out.println("    -------------------------------------------\n    Bye. Hope to see you again soon!\n    -------------------------------------------");
+                break;
+            } else {
+                System.out.println("    -------------------------------------------\n    " + echo + "\n    -------------------------------------------");
+            }
+            echo = input.nextLine();
+
+        }
     }
 }
