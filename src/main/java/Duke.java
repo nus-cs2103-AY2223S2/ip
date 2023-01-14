e5de6925d517e15d783be7c8bc39aa779c0207a8
ChinJunAn <junan1599@gmail.com>
2023-01-14 12:53:15 +0800

    Improved list function. User can now mark and unmark task

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,15 +14,17 @@ public class Duke {
         System.out.println("Hello! I'm Duke\nWhat can I do for you?\n");
 
         boolean flag = true;
-        List<String> ls = new ArrayList<>();
+        List<Task> ls = new ArrayList<>();
 
         while (flag) {
             Scanner sc = new Scanner(System.in);
             String inp = sc.nextLine();
             switch (inp) {
                 case "list":
+                    System.out.println("Here are the tasks in your list:");
                     for (int i = 0; i<ls.size();i++) {
-                        System.out.println(i+1 + ". " +ls.get(i));
+                        System.out.print(i+1+".");
+                        ls.get(i).printStatus();
                     }
                     break;
                 case "bye":
@@ -30,8 +32,25 @@ public class Duke {
                     flag = false;
                     break;
                 default:
-                    ls.add(inp);
-                    System.out.println("added: "+inp);
+                    //if input match mark/unmark x == we mark/unmark the line
+                    if (inp.matches("mark \\d+")) {
+                        String[] s = inp.split(" ");
+                        int index = Integer.parseInt(s[1]);
+                        Task t = ls.get(index-1);
+                        t.status = true;
+                        System.out.println("Nice! I've marked this task as done:");
+                        t.printStatus();
+                    } else if (inp.matches("unmark \\d+")) {
+                        String[] s = inp.split(" ");
+                        int index = Integer.parseInt(s[1]);
+                        Task t = ls.get(index-1);
+                        t.status = false;
+                        System.out.println("OK, I've marked this task as not done yet:");
+                        t.printStatus();
+                    } else {
+                        ls.add(new Task(false, inp));
+                        System.out.println("added: "+inp);
+                    }
             }
         }
     }
---
+++ src/main/java/Task.java
@@ -0,0 +1,30 @@
+public class Task {
+        boolean status;
+        String des;
+
+        public Task(boolean status, String des) {
+                this.status = status;
+                this.des = des;
+        }
+
+        public void setStatus(boolean status) {
+                this.status = status;
+        }
+
+        public void setDes(String des) {
+                this.des = des;
+        }
+
+        public boolean getStatus() {
+                return status;
+        }
+
+        public String getDes() {
+                return des;
+        }
+
+        public void printStatus() {
+                String s = (status)? "X":" ";
+                System.out.println("[" +s+ "] " + this.des);
+        }
+}
