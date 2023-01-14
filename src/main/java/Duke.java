ee4344deb68fb0a0acfdab1b083877b5150d0eb0
Merrick <merrickneo@gmail.com>
2023-01-14 13:43:30 +0800

    Add greeting, echo and exit feature under Level-1 iP increment

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,22 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("    ————————————————————————————————");
+        System.out.println("    Hello! I'm Duke");
+        System.out.println("    What can I do for you?");
+        System.out.println("    ————————————————————————————————");
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String command = sc.nextLine();
+            System.out.println("    ————————————————————————————————");
+            if (command.equals("bye")) {
+                System.out.println("    Bye. Hope to see you again soon!");
+                System.out.println("    ————————————————————————————————");
+                break;
+            }
+            System.out.println("    " + command);
+            System.out.println("    ————————————————————————————————");
+        }
+        sc.close();
     }
 }
