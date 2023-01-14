8f8413a14bdd8325637fe77702c43f0aa7903817
Merrick <merrickneo@gmail.com>
2023-01-14 14:00:16 +0800

    Add task storing feature for Duke (Level 2)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -7,22 +8,36 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("    ————————————————————————————————");
+        String divider = "    ————————————————————————————————";
+        System.out.println(divider);
         System.out.println("    Hello! I'm Duke");
         System.out.println("    What can I do for you?");
-        System.out.println("    ————————————————————————————————");
+        System.out.println(divider);
         Scanner sc = new Scanner(System.in);
+        ArrayList<Task> tasks = new ArrayList<>();
         while (true) {
             String command = sc.nextLine();
-            System.out.println("    ————————————————————————————————");
-            if (command.equals("bye")) {
+            System.out.println(divider);
+            if (command.equals("list")) {
+                System.out.println(divider);
+                int counter = 1;
+                for (Task t: tasks) {
+                    System.out.println(counter + ". " + t.toString());
+                    counter++;
+                }
+                System.out.println(divider);
+                continue;
+            }
+            else if (command.equals("bye")) {
                 System.out.println("    Bye. Hope to see you again soon!");
-                System.out.println("    ————————————————————————————————");
+                System.out.println(divider);
                 break;
             }
-            System.out.println("    " + command);
-            System.out.println("    ————————————————————————————————");
+            tasks.add(new Task(command));
+            System.out.println("    added: " + command);
+            System.out.println(divider);
         }
+        // close scanner
         sc.close();
     }
 }
