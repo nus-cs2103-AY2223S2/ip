73aced0c3147f631e3376e7c7dadc650af0ba51e
leonpoh <leonpoh@hotmail.com>
2023-01-14 20:22:06 +0800

    Implement Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,21 +1,56 @@
 import java.util.*;
 
 public class Duke {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    Scanner scanner;
+    List<String> list;
 
+    public Duke(Scanner scanner) {
+        this.scanner = scanner;
+        this.list = new ArrayList<>();
+    }
+
+    public void start() {
         System.out.println("Hello! I'm Duke" + "\n" + "What can I do for you?");
 
         while(scanner.hasNext()) {
-            String inputString = scanner.nextLine();
+            String input = scanner.nextLine();
 
-            if(inputString.equals("bye")) {
-                System.out.println("Duke: " + inputString + ". Hope I never see you again!");
+            if(input.equals("bye")) {
+                this.bye();
+                scanner.close();
+                return;
+            }
+
+            else if(input.equals("list")) {
+                this.list();
+
             }
             else {
-                System.out.println("Duke: " + inputString);
+                this.add(input);
             }
         }
-        scanner.close();
+    }
+
+    private void add(String input) {
+        this.list.add(input);
+        System.out.println("added: " + input);
+    }
+
+    private void bye() {
+        System.out.println("Duke: " + "Bye" + ". Hope I never see you again!");
+    }
+
+    private void list() {
+        System.out.println("list: ");
+        for(int i=1; i<list.size() + 1; i++) {
+            System.out.println(i + ". " + list.get(i-1));
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Duke duke = new Duke(scanner);
+        duke.start();
     }
 }
