6bdc71ad92a39519935cd7e58a7eb8aa7ca8e29b
TariqDroid <vegita2009@hotmail.com>
2023-01-14 14:38:57 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,9 +10,11 @@ public class Duke {
         System.out.println("Hello! I'm Duke\n" +
                             "What can I do for you?\n" + logo);
         Scanner input = new Scanner(System.in);
-        String[] list = new String[100];
+        Task[] list = new Task[100];
         String echo = input.nextLine();
         int counter = 0;
+        String echoSplit[] = echo.split(" ");
+
         while(true){
             if(echo.equals("bye")) {
                 System.out.println("    -------------------------------------------\n    Bye. Hope to see you again soon!\n    -------------------------------------------");
@@ -20,16 +22,33 @@ public class Duke {
             } else if(echo.equals("list")) {
                 System.out.println("    -------------------------------------------");
                 for (int i = 0; i < counter; i++) {
-                    System.out.println("    " + String.valueOf(i + 1) + ": " + list[i]);
+                    System.out.println("    " + String.valueOf(i + 1) + ".[" + list[i].getStatusIcon() + "] " + list[i].description);
                 }
                 System.out.println("    -------------------------------------------");
+            } else if(echoSplit[0].equals("mark")) {
+                int index = Integer.valueOf(echoSplit[1]) - 1;
+                list[index].isDone = true;
+
+                System.out.println("    -------------------------------------------");
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("    " + "[" + list[index].getStatusIcon() + "] " + list[index].description);
+                System.out.println("    -------------------------------------------");
+
+            } else if(echoSplit[0].equals("unmark")) {
+                int index = Integer.valueOf(echoSplit[1]) - 1;
+                list[index].isDone = false;
+
+                System.out.println("    -------------------------------------------");
+                System.out.println("Nice! I've marked this task as not done yet:");
+                System.out.println("    " + "[" + list[index].getStatusIcon() + "] " + list[index].description);
+                System.out.println("    -------------------------------------------");
             } else {
-                list[counter] = echo;
+                list[counter] = new Task(echo);
                 System.out.println("    -------------------------------------------\n    " + "added: " + echo +"\n    -------------------------------------------");
                 counter++;
             }
             echo = input.nextLine();
-
+            echoSplit = echo.split(" ");
         }
     }
 }
