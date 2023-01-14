57aa20c6c22ba4269f62e02cfb8db1f4e8b47151
junyi <47441677+immanuelhume@users.noreply.github.com>
2023-01-14 19:50:52 +0800

    Add skeletal code

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,44 @@
+import java.io.*;
+import java.util.*;
+
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        PrintWriter writer = new PrintWriter(System.out);
+        Chungus chungus = new Chungus(writer);
+
+        while (true) {
+            writer.append("chungus> ").flush();
+            String input = sc.nextLine();
+            boolean shouldExit = chungus.handleMessage(input);
+            if (shouldExit)
+                break;
+        }
+
+        sc.close();
+    }
+}
+
+class Chungus {
+    private Writer out;
+
+    public Chungus(Writer _out) throws IOException {
+        out = _out;
+
+        this.info("Hello, I'm Chungus.");
+        this.info("What can I do for you?");
+    }
+
+    public boolean handleMessage(String msg) throws IOException {
+        if (msg.equals("bye")) {
+            this.info("Bye!");
+            return true;
+        }
+        this.info(msg);
+        return false;
+    }
+
+    private void info(String msg) throws IOException {
+        out.append("\u001b[36m").append(msg).append('\n').append("\u001b[0m").flush();
     }
 }
