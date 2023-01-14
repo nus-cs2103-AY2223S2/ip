b0f6d8a984ca14ee76fb42ba7955285249bf825d
Hong Sheng <lee_hong_sheng@hotmail.com>
2023-01-14 19:44:29 +0800

    Completed Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,31 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        boolean active = true;
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello from\n" + logo + "\nWhat can I do for you?");
+        while (active) {
+            String input = s.nextLine();
+            printOutput(input);
+            if (input.equalsIgnoreCase("bye")) {
+                active = false;
+            }
+        }
+    }
+
+    public static void printOutput(String text) {
+        System.out.println("\t____________________________________________________________");
+        if (text.equalsIgnoreCase("bye")) {
+            System.out.println("\t Bye. Hope to see you again soon!");
+        } else {
+            System.out.println("\t " + text);
+        }
+        System.out.println("\t____________________________________________________________");
     }
 }
