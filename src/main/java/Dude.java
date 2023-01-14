5e67f24ce1b61903cc5f8d529a596636f677b7f1
Irminrics <irminrics@gmail.com>
2023-01-15 00:54:17 +0800

    Level-1 - Formatting the dude

--- src/main/java/Dude.java
+++ src/main/java/Dude.java
@@ -3,6 +3,19 @@ import java.util.Scanner;
 public class Dude {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        getIntro();
+
+        while(true) {
+            String command = sc.next();
+            getCommands(command);
+            if (command.equals("bye")) {
+                break;
+            }
+        }
+    }
+
+    public static void getIntro() {
         String logo = "  _____          __     \n" +
                 " |  __ \\ __ __   | |  ___  \n" +
                 " | |  | | | | |/ _` |/ _ \\\n" +
@@ -12,27 +25,24 @@ public class Dude {
         System.out.println(logo);
         System.out.println("Yo! I'm dude");
         System.out.println("What you want me do for you?\n");
-
-        while(true) {
-            String command = sc.next();
-            getCommands(command);
-            if (command.equals("bye")) {
-                break;
-            }
-        }
-
     }
 
     public static void getCommands(String command) {
         switch(command) {
             case "list":
-                System.out.println("list");
+                System.out.println(" ____________________________________________________________");
+                System.out.println("\tlist");
+                System.out.println(" ____________________________________________________________\n");
                 break;
             case "blah":
-                System.out.println("blah");
+                System.out.println(" ____________________________________________________________");
+                System.out.println("\tblah");
+                System.out.println(" ____________________________________________________________\n");
                 break;
             case "bye":
-                System.out.println("Ciaos. See you next time.");
+                System.out.println(" ____________________________________________________________");
+                System.out.println("\tCiaos! See you next time.");
+                System.out.println(" ____________________________________________________________\n");
                 break;
         }
     }
