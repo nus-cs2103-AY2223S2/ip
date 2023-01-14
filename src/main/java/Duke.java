3b301fea668a868823c3634446eaea7409190a77
junyi <47441677+immanuelhume@users.noreply.github.com>
2023-01-14 20:10:40 +0800

    Add task saving and retrieval

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,6 +11,7 @@ public class Duke {
             writer.append("chungus> ").flush();
             String input = sc.nextLine();
             boolean shouldExit = chungus.handleMessage(input);
+            writer.append('\n').flush();
             if (shouldExit)
                 break;
         }
@@ -21,9 +22,11 @@ public class Duke {
 
 class Chungus {
     private Writer out;
+    private ArrayList<String> tasks;
 
     public Chungus(Writer _out) throws IOException {
         out = _out;
+        tasks = new ArrayList<>();
 
         this.info("Hello, I'm Chungus.");
         this.info("What can I do for you?");
@@ -34,11 +37,31 @@ class Chungus {
             this.info("Bye!");
             return true;
         }
-        this.info(msg);
+        if (msg.equals("list")) {
+            for (int i = 1; i <= this.tasks.size(); i++) {
+                this.info().append(String.valueOf(i)).append(". ").append(this.tasks.get(i - 1)).append('\n');
+            }
+            this.flush();
+            return false;
+        }
+
+        this.tasks.add(msg);
+        this.info().append("added: ").append(msg).append('\n');
+        this.flush();
+
         return false;
+    }
+
+    private Writer info() throws IOException {
+        out.append("\u001b[36m");
+        return out;
     }
 
     private void info(String msg) throws IOException {
         out.append("\u001b[36m").append(msg).append('\n').append("\u001b[0m").flush();
+    }
+
+    private void flush() throws IOException {
+        out.append("\u001b[0m").flush();
     }
 }
