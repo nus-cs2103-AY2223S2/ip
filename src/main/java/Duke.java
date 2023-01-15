645c6695e199e000fdf361a4622e5136bf22b9bc
Sean Wong <58366602+ExtraShotLatte@users.noreply.github.com>
2023-01-15 16:07:34 +0800

    Add scanner structure

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
 //        String logo = " ____        _        \n"
@@ -6,9 +8,15 @@ public class Duke {
 //                + "| |_| | |_| |   <  __/\n"
 //                + "|____/ \\__,_|_|\\_\\___|\n";
 //        System.out.println("Hello from\n" + logo);
+
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("    ____________________________________________________________" +
                 "\n     Hello! I'm Duke" +
                 "\n     What can I do for you?" +
                 "\n    ____________________________________________________________");
+
+        sc.close();
+
     }
 }
