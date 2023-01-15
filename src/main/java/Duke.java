de11ca2236683eccea6a863270c0bbf81e3ed469
yanlings <e0774088@u.nus.edu>
2023-01-15 20:28:28 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
@@ -7,12 +9,25 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello I'm Duke\nWhat can I do for you?" + logo);
+        String[] array = new String[100];
         Scanner myObj = new Scanner(System.in);
-        String reply = myObj.next();
+        String reply = myObj.nextLine();
+        int count = 1;
         while (!reply.contains("bye")) {
             System.out.println(reply);
+            if (!reply.contains("list")) {
+                System.out.println("added: " + reply);
+                array[count] = "" + count + "." + reply;
+                count += 1;
+            } else {
+                for (String element: array) {
+                    if (element != null) {
+                        System.out.println(element);
+                    }
+                }
+            }
             myObj = new Scanner(System.in);
-            reply = myObj.next();
+            reply = myObj.nextLine();
         }
         System.out.println("Bye, Hope to see you again soon!");
     }
