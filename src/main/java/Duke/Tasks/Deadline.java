0f9d150d3caf44b25b4d45cd2e1700a39cd0152c
yitong <ssunyitong@gmail.com>
2023-01-15 16:49:00 +0800

    Add some java doc

--- src/main/java/Duke/Tasks/Deadline.java
+++ src/main/java/Duke/Tasks/Deadline.java
@@ -11,7 +11,7 @@ public class Deadline extends Task{
 
     /**
      * Constructor of Deadline
-     * @param desc
+     * @param desc the description
      * @param time
      * @param done
      */
@@ -20,24 +20,41 @@ public class Deadline extends Task{
         this.time = time;
     }
 
+    /**
+     * Marks the Deadline as done
+     * @return Deadline
+     */
     @Override
     public Deadline mark() {
         super.mark();
         return this;
     }
 
+    /**
+     * Unmarks the deadline
+     * @return Deadline
+     */
     @Override
     public Deadline unmark() {
         super.unmark();
         return this;
     }
 
+    /**
+     * Run the deadline and add to table
+     * @param table
+     * @param monitor
+     */
     @Override
     public void run(TaskTable table, Monitor monitor) {
         table.add(this);
         monitor.displayAdd(table, (table.size() - 1));
     }
 
+    /**
+     * Override the String method and convert the deadline to String
+     * @return String
+     */
     @Override
     public String toString() {
         return "[D]" + super.toString() + " (by: " + this.time + ")";
