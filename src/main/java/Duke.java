d41bdc37ff1c76b549affc5521e825cdae68111d
Gloria Zhang Xinhui <glozxi@gmail.com>
2023-01-15 21:50:59 +0800

    Add user input
    Replace buffered reader with scanner. User can exit by inputting "exit".

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,26 +1,54 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Duke {
 
-    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    private static Scanner scanner = new Scanner(System.in);
+    private static final String GREETING = "Hello, I am Duke.\n" +
+            "What can I do for you?";
+    private static final String EXIT_COMMAND = "exit";
+    private static final String BYE = "Goodbye. I hope to see you again.";
+    private static final String PROMPT_SYMBOL = ">";
 
     private static String formatMessage(String message) {
         return "___________________________\n" +
                 message +
-                "\n___________________________\n";
+                "\n___________________________";
     }
 
     private static void printMessage(String message) {
         System.out.println(formatMessage(message));
     }
 
+    private static String getInputFromUser() {
+        return scanner.nextLine();
+    }
+
+    private static void printPrompt() {
+        System.out.print(PROMPT_SYMBOL);
+    }
+
     private static void greet() {
-        printMessage("Hello, I am Duke.\n" +
-                "What can I do for you?");
+        printMessage(GREETING);
+    }
+
+    private static void acceptCommands() {
+        String input;
+        while (true) {
+            printPrompt();
+            input = getInputFromUser();
+            if (input.equals(EXIT_COMMAND)) {
+                return;
+            }
+        }
+    }
+
+    private static void sayGoodbye() {
+        printMessage(BYE);
     }
 
     public static void main(String[] args) {
         greet();
+        acceptCommands();
+        sayGoodbye();
     }
 }
