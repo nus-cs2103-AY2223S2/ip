98500c530b2f4c3293ac17639eef85d37bad018d
Liu Muchen <liumuchen1@gmail.com>
2023-01-15 20:56:16 +0800

    (Update): Ability to mark and unmark tasks
    Changed String to Task for more versatility
    Switched to cases for convenience

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -16,21 +16,60 @@ public class Duke {
     printMsg(introMsg);
 
     Scanner scanner = new Scanner(System.in);
-    String input = scanner.nextLine();
 
-    String exitToken = "bye";
-    String listToken = "list";
-    ArrayList<String> data = new ArrayList<>();
+    ArrayList<Task> tasks = new ArrayList<>();
+    boolean cont = true;
 
-    while (!input.equals(exitToken)) {
-      if (input.equals(listToken)) {
-        printMsg(data);
-      } else {
-        data.add(input);
-        String msg = String.format("Added new entry: %s", input);
-        printMsg(msg);
+    String listOutOfBounds = "You are trying to access"
+        + " a task not in the list.\n"
+        + "Please try again!";
+    String numberFormatException = "You entered an invalid"
+        + " number.\n"
+        + "Please try again!";
+
+    while (cont) {
+      String input = scanner.nextLine();
+      String[] inputSplit = input.split(" ");
+      int index;
+      switch (inputSplit[0]) {
+        case "bye":
+          cont = false;
+          break;
+        case "list":
+          printTasks(tasks);
+          break;
+        case "mark":
+          try {
+            index = Integer.parseInt(inputSplit[1]) - 1;
+            if (index < 0 || index >= tasks.size()) {
+              printMsg(listOutOfBounds);
+            } else {
+              tasks.get(index).markTask();
+              printMarkTask(tasks.get(index));
+            }
+          } catch (NumberFormatException e) {
+            printMsg(numberFormatException);
+          }
+          break;
+        case "unmark":
+          try {
+            index = Integer.parseInt(inputSplit[1]) - 1;
+            if (index < 0 || index >= tasks.size()) {
+              printMsg(listOutOfBounds);
+            } else {
+              tasks.get(index).unmarkTask();
+              printUnmarkTask(tasks.get(index));
+            }
+          } catch (NumberFormatException e) {
+            printMsg(numberFormatException);
+          }
+          break;
+        default:
+          tasks.add(new Task(input));
+          String msg = String.format("Added new entry: %s", input);
+          printMsg(msg);
+          break;
       }
-      input = scanner.nextLine();
     }
 
     String exitMsg = "Thank you for coming!\n"
@@ -48,17 +87,39 @@ public class Duke {
     System.out.println(spacer);
   }
 
-  public static void printMsg(ArrayList<String> msg) {
+  public static void printTasks(ArrayList<Task> tasks) {
     String spacer = "____________________"
         + "______________________";
     String msgHeader = "Current data in the list are:";
 
     System.out.println(spacer);
     System.out.println(msgHeader);
-    for (int i = 0; i < msg.size(); i++) {
-      String output = String.format("%d. %s", i + 1, msg.get(i));
+    for (int i = 0; i < tasks.size(); i++) {
+      String output = String.format("%d. %s", i + 1, tasks.get(i));
       System.out.println(output);
     }
+    System.out.println(spacer);
+  }
+
+  public static void printMarkTask(Task task) {
+    String spacer = "____________________"
+        + "______________________";
+    String msgHeader = "I've marked this task as done:";
+
+    System.out.println(spacer);
+    System.out.println(msgHeader);
+    System.out.println(task);
+    System.out.println(spacer);
+  }
+
+  public static void printUnmarkTask(Task task) {
+    String spacer = "____________________"
+        + "______________________";
+    String msgHeader = "I've unmarked this task as not done:";
+
+    System.out.println(spacer);
+    System.out.println(msgHeader);
+    System.out.println(task);
     System.out.println(spacer);
   }
 }
