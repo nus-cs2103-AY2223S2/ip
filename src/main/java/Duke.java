914b19f8b200e942a22af6c76ce3b8e422251ec9
Hui Xuan <huixuant.stu@gmail.com>
2023-01-15 12:21:54 +0800

    Implemented Level-1 (greet, echo, exit) and personalised chatbot

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,35 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = " ____  ____  ____  ____   __\n"
+                + "(  _ \\(  __)(  _ \\(  _ \\ / _\\\n"
+                + " ) __/ ) _)  ) __/ ) __//    \\\n"
+                + "(__)  (____)(__)  (__)  \\_/\\_/\n";
+        String art = "       _\n"
+                + "  <`--'\\>______\n"
+                + "  /. .  `'     \\\n"
+                + " (`')  ,        @\n"
+                + "  `-._,        /\n"
+                + "     )-)_/--( >  jv\n"
+                + "    ''''  ''''\n";
+        System.out.println("=============================================");
+        System.out.println("Oink! I'm \n" + logo + art);
+        System.out.println("Nice to meet you! How can I assist you today?");
+        System.out.println("=============================================");
+
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String command = sc.nextLine();
+            if (command.equals("bye")) {
+                System.out.println("Oink oink! See you again :)");
+                sc.close();
+                break;
+            } else {
+                System.out.println(command);
+                System.out.println("=============================================");
+            }
+        }
     }
 }
+
