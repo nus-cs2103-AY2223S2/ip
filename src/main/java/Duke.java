e13de98b8d7def642c40bbe5b94caa63cbf5abee
LIN_CHIEH <linchieh@stu.comp.nus.edu.sg>
2023-01-15 09:17:02 +0800

    Completed Level-1, skeletal version of chatbot

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,35 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    private static void displayMessage(String message) {
+        System.out.println(
+                "-----------------------------------------------------------\n" +
+                message + "\n" +
+                "-----------------------------------------------------------\n"
+        );
+    }
+
+    private void init() {
+        displayMessage("Hello! I'm Bob\n" +
+                "What can I do for you?");
+    }
+
+    private void exit() {
+        displayMessage("Bye. Hope to see you again soon!");
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke duke = new Duke();
+        duke.init();
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            String input = sc.next();
+            if (input.equalsIgnoreCase("bye")) {
+                duke.exit();
+                break;
+            }
+            displayMessage(input);
+        }
     }
 }
