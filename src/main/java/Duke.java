2545f4aaffb3d8e1f0b10b6bc24373e16ec30f7e
tyx021 <yxting1109@gmail.com>
2023-01-15 14:24:01 +0800

    Added mark & unmark tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,23 +8,27 @@ public class Duke {
             "\tHello! I'm Duke\n" +
             "\tWhat can I do for you?\n" +
             lines;
-    static List<String> textList;
+    final static String bye = lines +
+            "\tBye. Hope to see you again soon!\n" +
+            lines;
+    static List<Task> taskList;
 
     public static void printList() {
         System.out.print(lines);
-        for (int i = 0; i < textList.size(); i++) {
-            System.out.println("\t" + (i + 1) + ". " + textList.get(i));
+        System.out.println("Here are the tasks in your list:");
+        for (int i = 0; i < taskList.size(); i++) {
+            System.out.println("\t" + (i + 1) + ". " + taskList.get(i).toString());
         }
         System.out.print(lines);
     }
 
-    public static void addList(String text) {
-        textList.add(text);
-        System.out.println(lines + "\tadded: " + text +"\n" + lines);
+    public static void addTask(Task task) {
+        taskList.add(task);
+        System.out.println(lines + "\tadded: " + task +"\n" + lines);
     }
 
     public static void main(String[] args) {
-        textList = new ArrayList<>();
+        taskList = new ArrayList<>();
         System.out.println(greet);
         Scanner sc = new Scanner(System.in);
 
@@ -32,15 +36,24 @@ public class Duke {
         while (!commands.equals("bye")) {
             if (commands.equals("list")) {
                 printList();
+            } else if (commands.contains("mark")) {
+                String[] s = commands.split(" ");
+                Task t = taskList.get(Integer.parseInt(s[1]) - 1);
+                if (s[0].equals("mark")) {
+                    t.markDone();
+                    System.out.println("\tNice! I've marked this task as done:");
+                    System.out.println("\t" + t);
+                } else if (s[0].equals("unmark")) {
+                    t.markNotDone();
+                    System.out.println("\tOK, I've marked this task as not done yet:");
+                    System.out.println("\t" + t);
+                }
             } else {
-                addList(commands);
+                Task t = new Task(commands);
+                addTask(t);
             }
             commands = sc.nextLine();
         }
-        System.out.println(
-                lines +
-                        "\tBye. Hope to see you again soon!\n" +
-                        lines
-        );
+        System.out.println(bye);
     }
 }
