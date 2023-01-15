1e54892cfb8c5ae3d3ad8c80e49f2fe463f49a61
hingen <1627313-hingen@users.noreply.gitlab.com>
2023-01-15 21:40:57 +0800

    Add EchoCommand.java
    The user's input needs to be echoed to the user.
    
    Let's add an EchoCommand class which implements the Command interface.
    
    An EchoCommand class would encapsulate the logic for the echo command
    type and can be utilised through the Command interface.

---
+++ src/main/java/duke/command/EchoCommand.java
@@ -0,0 +1,18 @@
+package duke.command;
+
+/**
+ * Represents an echo command.
+ */
+public class EchoCommand implements Command {
+
+    /**
+     * Returns the user's input.
+     *
+     * @param input {@inheritDoc}
+     * @return User's input.
+     */
+    @Override
+    public String run(String input) {
+        return input;
+    }
+}
