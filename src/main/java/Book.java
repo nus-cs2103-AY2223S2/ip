c846997d9db71bee40ef9e215fa107cb8b2d5905
chia-yh <0.chiayuhong.0@gmail.com>
2023-01-16 01:14:41 +0800

    Add Echo, Exit

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Book {
     /** Logo for Book */
     private static final String LOGO = " ____\n"
@@ -9,8 +11,15 @@ public class Book {
     /** Horizontal line for separation. */
     private static final String LINE =
             "________________________________________________________________________________\n";
+
+    private static final Scanner input = new Scanner(System.in);
+    private static String command = "";
     public static void main(String[] args) {
         System.out.println(LINE + "Good day! This is\n" + LOGO + "What may I help you with?\n" + LINE);
-        System.out.println("Bye. Pick up the book again soon!\n" + LINE);
+        while (!command.equals("bye")) {
+            command = input.nextLine();
+            System.out.println(LINE + command + "\n" + LINE);
+        }
+        System.out.println("Bye. Pick up Book again soon!\n" + LINE);
     }
 }
