c79ab888e33ea774bb1724dc7c082c8e7c444810
LIN_CHIEH <linchieh@stu.comp.nus.edu.sg>
2023-01-15 09:32:22 +0800

    Level-2 completed, add and list items

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,22 +1,38 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+    private final ArrayList<String> userInputs;
+
+    private Duke() {
+        this.userInputs = new ArrayList<>();
+    }
 
     private static void displayMessage(String message) {
         System.out.println(
                 "-----------------------------------------------------------\n" +
-                message + "\n" +
+                message +
                 "-----------------------------------------------------------\n"
         );
     }
 
     private void init() {
-        displayMessage("Hello! I'm Bob\n" +
-                "What can I do for you?");
+        displayMessage("""
+                Hello! I'm Bob
+                What can I do for you?
+                """);
     }
 
     private void exit() {
-        displayMessage("Bye. Hope to see you again soon!");
+        displayMessage("Bye. Hope to see you again soon!\n");
+    }
+
+    private void displayItemList() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= userInputs.size(); i++) {
+            sb.append(i).append(". ").append(userInputs.get(i-1)).append("\n");
+        }
+        displayMessage(sb.toString());
     }
 
     public static void main(String[] args) {
@@ -24,12 +40,21 @@ public class Duke {
         duke.init();
         while (true) {
             Scanner sc = new Scanner(System.in);
-            String input = sc.next();
-            if (input.equalsIgnoreCase("bye")) {
+            String input = sc.nextLine();
+
+            if (input.equals("bye")) {
                 duke.exit();
                 break;
             }
-            displayMessage(input);
+
+            if (input.equals("list")) {
+                duke.displayItemList();
+                continue;
+            }
+
+            duke.userInputs.add(input);
+            displayMessage("added: " + input + "\n");
+
         }
     }
 }
