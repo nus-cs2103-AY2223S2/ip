d9676208adf7be33ed2aced9029ed97ecbbc7e81
Sean Tan <seanfirefox@gmail.com>
2023-01-16 01:25:28 +0800

    Level 2 Add List done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,6 @@
 import java.io.*;
+import java.util.ArrayList;
+
 public class Duke {
     public static void main(String[] args) throws IOException {
         String logo = " ____        _        \n"
@@ -9,9 +11,21 @@ public class Duke {
         System.out.println("Hello from\n" + logo + "\nWhat can I do for you?");
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         String str = bufferedReader.readLine();
+        ArrayList<String> store = new ArrayList<>();
         while (!str.equals("bye")) {
-            System.out.println(str);
-            str = bufferedReader.readLine();
+            if (str.equals("list")) {
+                int number = 1;
+                for(String stored : store) {
+                    System.out.println(number + ". " + stored);
+                    number++;
+                }
+                str = bufferedReader.readLine();
+            } else {
+                store.add(str);
+                System.out.println(str);
+                str = bufferedReader.readLine();
+            }
+
         }
 
         System.out.println("Bye. Hope to see you again soon!");
