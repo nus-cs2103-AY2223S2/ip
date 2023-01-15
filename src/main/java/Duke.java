165411aa216060ca4cbae18b642109f61f0163f4
shazrin <sadmeowkins@outlook.com>
2023-01-15 16:10:16 +0800

    Base skeleton for chatbot

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,53 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static void printText(String text) {
+        System.out.printf("     %s\n", text);
+    }
+
+    private static void printHorizontal() {
+        System.out.println("    ____________________________________________________________");
+    }
+
+    private static void printStartup() {
+        String logo =
+                " /\\_/\\\n" +
+                "( o.o )   ~meow~\n" +
+                " > ^ <";
+        System.out.println(logo);
+        printHorizontal();
+        printText("Hello! I'm Duke");
+        printText("What can I do for you?");
+        printHorizontal();
+    }
+
+    private static void commandEcho(String echoString) {
+        printHorizontal();
+        printText(echoString);
+        printHorizontal();
+    }
+
+    private static void commandExit() {
+        printHorizontal();
+        printText("Bye. Hope to see you again soon!");
+        printHorizontal();
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        printStartup();
+
+        Scanner scanner = new Scanner(System.in);
+        boolean isExit = false;
+        while (!isExit) {
+            String command = scanner.nextLine();
+            switch (command) {
+                case "bye":
+                    commandExit();
+                    isExit = true;
+                    break;
+                default:
+                    commandEcho(command);
+            }
+        }
     }
 }
