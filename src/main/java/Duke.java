c7c5b32c36df03f641913dca5bb875807ca72731
shazrin <sadmeowkins@outlook.com>
2023-01-15 16:19:58 +0800

    Add task and list tasks feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,10 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+    private static final int MAX_TASKS_SIZE = 100;
+    private static ArrayList<String> tasks = new ArrayList<>(MAX_TASKS_SIZE);
+
     private static void printText(String text) {
         System.out.printf("     %s\n", text);
     }
@@ -21,9 +25,22 @@ public class Duke {
         printHorizontal();
     }
 
-    private static void commandEcho(String echoString) {
+    private static void commandAddTask(String task) {
         printHorizontal();
-        printText(echoString);
+        if (tasks.size() == MAX_TASKS_SIZE) {
+            printText("Task list is full!");
+        } else {
+            tasks.add(task);
+            printText(String.format("Added: %s", task));
+        }
+        printHorizontal();
+    }
+
+    private static void commandListTasks() {
+        printHorizontal();
+        for (int index = 0; index < tasks.size(); index++) {
+            printText(String.format("%d. %s", index + 1, tasks.get(index)));
+        }
         printHorizontal();
     }
 
@@ -41,12 +58,15 @@ public class Duke {
         while (!isExit) {
             String command = scanner.nextLine();
             switch (command) {
+                case "list":
+                    commandListTasks();
+                    break;
                 case "bye":
                     commandExit();
                     isExit = true;
                     break;
                 default:
-                    commandEcho(command);
+                    commandAddTask(command);
             }
         }
     }
