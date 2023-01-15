29a0df876dded87a2d9d1a480a77dc5b8f27927e
soowengkit <>
2023-01-15 16:52:13 +0800

    Revert "Able to keep track of different types of tasks, ToDos, Events and Deadlines, with timeframes as strings."
    This reverts commit ac183f3becb658a0cccd9b3e3f4c0e6b6068b850.

--- src/main/java/Tasks.java
+++ src/main/java/Tasks.java
@@ -23,10 +23,4 @@ public class Tasks {
             return "[ ]";
         }
     }
-    public String icon() {
-        return "";
-    }
-    public String mssg() {
-        return "Got it I've added this task: \n";
-    }
 }
