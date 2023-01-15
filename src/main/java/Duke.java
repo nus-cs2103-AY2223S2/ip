f7417fcd5757211dc68ae9b04e640dc1706f4a43
gitsac <isaacchai99@gmail.com>
2023-01-15 18:38:05 +0800

    Add level-1 Increment

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,29 @@
+import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
+
+
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+    public static void main(String[] args) throws IOException{
+        // String logo = " ____        _        \n"
+        //         + "|  _ \\ _   _| | _____ \n"
+        //         + "| | | | | | | |/ / _ \\\n"
+        //         + "| |_| | |_| |   <  __/\n"
+        //         + "|____/ \\__,_|_|\\_\\___|\n";
+        // System.out.println("Hello from\n" + logo);
+        greeting();
+        BufferedReader bReader = new BufferedReader(new InputStreamReader(System.in));
+        String userInput = bReader.readLine();
+        while (!userInput.equals("bye")) {
+            System.out.println(userInput);
+            userInput = bReader.readLine();
+        }
+        System.out.println("Bye! Hope to see you soon again!");
     }
+
+    public static void greeting() {
+        System.out.println("Hello, I am Duke!\nWhat can I do for you?");
+    }
+
+
 }
