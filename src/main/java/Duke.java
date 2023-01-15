3c86dab4a02808243743264d51e6eecc23a1dfb3
Hui Xuan <huixuant.stu@gmail.com>
2023-01-15 12:59:42 +0800

    Implemented Level-2 (add, list)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,16 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+    public static final String divider = "=============================================";
+    public static ArrayList<String> commands = new ArrayList<>();
+    public static void displayList() {
+        for (int i = 0; i < commands.size(); i++) {
+            System.out.println(Integer.toString(i + 1) + ". " + commands.get(i));
+        }
+        System.out.println(divider);
+    }
+
     public static void main(String[] args) {
         String logo = " ____  ____  ____  ____   __\n"
                 + "(  _ \\(  __)(  _ \\(  _ \\ / _\\\n"
@@ -13,10 +23,10 @@ public class Duke {
                 + "  `-._,        /\n"
                 + "     )-)_/--( >  jv\n"
                 + "    ''''  ''''\n";
-        System.out.println("=============================================");
+        System.out.println(divider);
         System.out.println("Oink! I'm \n" + logo + art);
         System.out.println("Nice to meet you! How can I assist you today?");
-        System.out.println("=============================================");
+        System.out.println(divider);
 
         Scanner sc = new Scanner(System.in);
         while (true) {
@@ -25,9 +35,12 @@ public class Duke {
                 System.out.println("Oink oink! See you again :)");
                 sc.close();
                 break;
+            } else if (command.equals("list")) {
+                displayList();
             } else {
-                System.out.println(command);
-                System.out.println("=============================================");
+                commands.add(command);
+                System.out.println("Added: " + command);
+                System.out.println(divider);
             }
         }
     }
