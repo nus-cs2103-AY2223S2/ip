5976f37a217ab0e615f793d6e89c8d2f1814a67a
Yong Xiang Wong <yongxiang2000@gmail.com>
2023-01-15 18:12:24 +0800

    Added Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,14 +5,23 @@ public class Duke {
         System.out.println("Hello I'm Duke");
         System.out.println("What can I do for you?");
         Scanner scanner = new Scanner(System.in);
+        String[] list = new String[100];
+        int counter = 0;
 
         while(true) {
             String message = scanner.nextLine();
-            if(!message.equals("bye")) {
-                System.out.println(message);
-            } else {
+            if(message.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if(message.equals("list")){
+                for(int i = 0; i < counter; i++) {
+                    int label = i + 1;
+                    System.out.println(label + ". " + list[i]);
+                }
+            } else {
+                list[counter] = message;
+                counter++;
+                System.out.println("added: " + message);
             }
         }
     }
