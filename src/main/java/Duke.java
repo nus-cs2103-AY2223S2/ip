3d99b83f95ca86e5d5aad564d20c69e94ec99555
Cheamybunny <cheamjiawei@gmail.com>
2023-01-15 21:35:16 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,45 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static final String LOGO = " ____        _        \n"
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n";
+
+    private static final String LINE = "___________________________________________________\n";
+
+    public void greet() {
+        System.out.println(Duke.LINE
+                + "Hello! I'm Duke\n"
+                + "What can I do for you?\n"
+                + Duke.LINE);
+    }
+
+    public void exit() {
+        System.out.println(Duke.LINE
+                + "Bye. Hope to see you again soon!\n"
+                + Duke.LINE);
+    }
+
+    public void start(Scanner sc) {
+        this.greet();
+
+        while(sc.hasNext()) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                this.exit();
+                break;
+            } else {
+                System.out.println(Duke.LINE + input);
+                System.out.println(Duke.LINE);
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner scanner = new Scanner(System.in);
+        Duke duke = new Duke();
+        duke.start(scanner);
     }
 }
