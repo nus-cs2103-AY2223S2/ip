95bc5b6d409cb801ae8ce817da1e8fe4c863636f
Hafeez <izzhafeez@gmail.com>
2023-01-15 20:28:09 +0800

    Implemented the basic chat bot encapsulated in functions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,46 @@
+import java.util.Scanner;
+
 public class Duke {
+    static String NAME = "Half";
+    static String HORIZONTAL = "+=".repeat(20);
+    static String INDENT = "> ";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String greeting = "Hello! I'm %s.";
+        Duke.greet(greeting);
+
+        String question = "What do you want?";
+        Duke.say(question, false);
+
+        String response = "";
+
+        Scanner scan = new Scanner(System.in);
+
+        while (!response.equals("bye")) {
+            response = scan.nextLine();
+            Duke.say(response, true);
+        }
+
+        Duke.say("Bye. HAND.", true);
+    }
+
+    public static void greet(String greeting) {
+        /**
+         * @param greeting what needs to be said, with placeholder for the name.
+         */
+        System.out.println(String.format(greeting, Duke.NAME));
+    }
+
+    public static void say(String sentence, boolean addLine) {
+        /**
+         * @param sentence what needs to be said.
+         * @param addLine whether to add a horizontal line.
+         * @return void
+         */
+        System.out.println(Duke.INDENT + sentence);
+
+        if (addLine) {
+            System.out.println(Duke.HORIZONTAL);
+        }
     }
 }
