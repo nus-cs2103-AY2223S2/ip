9b5d94e8b3ea7a59d227ce33863cf546fea15dd7
chia-yh <0.chiayuhong.0@gmail.com>
2023-01-16 01:54:51 +0800

    Add Add, List

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -14,12 +14,28 @@ public class Book {
 
     private static final Scanner input = new Scanner(System.in);
     private static String command = "";
+    private static String[] list = new String[100];
+    private static int index = 0;
     public static void main(String[] args) {
-        System.out.println(LINE + "Good day! This is\n" + LOGO + "What may I help you with?\n" + LINE);
+        System.out.print(LINE + "Good day! This is\n" + LOGO + "What may I help you with?\n" + LINE);
+        command = input.nextLine();
         while (!command.equals("bye")) {
+            parse(command);
             command = input.nextLine();
-            System.out.println(LINE + command + "\n" + LINE);
         }
-        System.out.println("Bye. Pick up Book again soon!\n" + LINE);
+        System.out.print(LINE + "Bye. Pick up Book again soon!\n" + LINE);
+    }
+
+    private static void parse(String text) {
+        if (text.equals("list")) {
+            System.out.print(LINE);
+            for (int i = 0; i < index; i++) {
+                System.out.println((i+ 1) + ". " + list[i]);
+            }
+            System.out.print(LINE);
+        } else {
+            list[index++] = text;
+            System.out.print(LINE + "added: " + command + "\n" + LINE);
+        }
     }
 }
