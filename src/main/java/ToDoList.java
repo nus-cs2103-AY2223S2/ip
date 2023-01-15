61bb30cd217799cdba626c19f88d6daef77642d7
anthonynmh <anthonyneomh@gmail.com>
2023-01-16 01:58:25 +0800

    Updated array to store Task objects instead of String

--- src/main/java/ToDoList.java
+++ src/main/java/ToDoList.java
@@ -1,15 +1,26 @@
 public class ToDoList {
-    private static String[] todolist = new String[100];
-    private static int count = 1;
+    private static Task[] todolist = new Task[100];
+    private static int count = 0;
 
     public static void list() {
-        for (int i = 0; i < count - 1; i++) {
-            System.out.println((i + 1) + ". " + todolist[i]);
+        System.out.println("\tHere are the tasks in your list:\n");
+        for (int i = 0; i < count; i++) {
+            System.out.println("\t" + (i + 1) + "." +
+                    todolist[i].printDone() + " " + todolist[i].printTask());
         }
     }
 
     public static void add(String task) {
-        todolist[count - 1] = task;
+        Task t = new Task(count, task);
+        todolist[count] = t;
         count++;
+    }
+
+    public static void markDone (int index) {
+        todolist[index - 1].markDone();
+    }
+
+    public static void unmark(int index) {
+        todolist[index - 1].unmark();
     }
 }
