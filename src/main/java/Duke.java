d249ae88f1217bd5a5890f664ae0e8389c1739d4
Tan Jun Da <e0726893@u.nus.edu>
2023-01-15 11:58:30 +0800

    Added Greet, Echo, Exit features.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,33 @@
+/**
+ * Project name: Duke
+ * Author: Tan Jun Da
+ * Student Number: A0234893U
+ */
+
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = "";
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("____________________________________________________________");
+        while(true) {
+            input = sc.nextLine();
+            System.out.println("____________________________________________________________");
+            if(input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            } else {
+                System.out.println(input);
+                System.out.println("____________________________________________________________");
+            }
+        }
     }
 }
