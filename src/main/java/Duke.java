d665c473b394df328784d9a2d85a552f6e97bbe7
bojie3 <xubojie2013@gmail.com>
2023-01-15 11:36:55 +0800

    Level 1 finished

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,37 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Duke {
+    public static String logo = " ____        _        \n"
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n";
+    public static String intro = "    ____________________________________________________________\n" +
+            "     Hello! I'm Duke\n" +
+            "     What can I do for you?\n" +
+            "    ____________________________________________________________\n";
+    public static String outro = "    ____________________________________________________________\n" +
+            "     Bye. Hope to see you again soon!\n" +
+            "    ____________________________________________________________\n";
+
+    public static String getReply(String str) {
+        return "    ____________________________________________________________\n" +
+                "     " + str + "\n" +
+                "    ____________________________________________________________\n";
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(Duke.intro);
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.print("");
+            String str = sc.nextLine();
+            if (Objects.equals(str, "bye")) {
+                System.out.println(Duke.outro);
+                break;
+            }
+            System.out.println(Duke.getReply(str));
+        }
     }
 }
