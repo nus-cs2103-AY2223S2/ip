84be700f510efbfeb98fbf93e6727dc4400c38f5
rexcyrio <stefancyrio@gmail.com>
2023-01-15 10:53:47 +0800

    Level-1: echo user input

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
+
 public class Duke {
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String welcomeMessage = "============================================================\n"
+                + "Welcome to Duchess\n"
+                + "============================================================\n";
+        String userPrompt = ">> ";
+        System.out.print(welcomeMessage + userPrompt);
+
+        Scanner sc = new Scanner(System.in);
+        String userInput = sc.nextLine();
+
+        while (!userInput.equals("bye")) {
+            System.out.println(userInput);
+            System.out.print(userPrompt);
+            userInput = sc.nextLine();
+        }
+
+        // user input is "bye"
+        System.out.println("Exiting...");
     }
 }
