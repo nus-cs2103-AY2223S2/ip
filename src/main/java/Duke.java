d05a37b719a2df639c1cd634327a1bfb63b5ad17
SPWwj <weiwenjie@u.nus.edu>
2023-01-15 20:20:32 +0800

    Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,11 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-    public static void main(String[] args) {
+    static Scanner sc = new Scanner(System.in);
+    static String dialogSeparator = "____________________________________________________________";
+
+    public static void greeting() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -9,18 +13,32 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
 //        System.out.println("Hello from\n" + logo);
         String dialogSeparator = "____________________________________________________________";
-        String greeting = logo+"\n"+dialogSeparator + "\nHello! I'm Duke\nWhat can I do for you?\n" + dialogSeparator;
-
+        String greeting = logo + "\n" + dialogSeparator + "\nHello! I'm Duke\nWhat can I do for you?\n" + dialogSeparator;
         System.out.println(greeting);
-        Scanner sc = new Scanner(System.in);
+
+    }
+
+    public static void main(String[] args) {
+        greeting();
         String input;
+        ArrayList<String> tasks = new ArrayList<>();
+
         while (true) {
             input = sc.nextLine();
             if (input.equals("bye")) {
                 System.out.println(dialogSeparator + "\n" + "Bye. Hope to see you again soon!" + "\n" + dialogSeparator);
                 break;
+            } else if (input.equals("list")) {
+                int i = 1;
+                System.out.println(dialogSeparator);
+                for (String item : tasks) {
+                    System.out.println(i + ". " + item);
+                }
+                System.out.println(dialogSeparator);
+
             } else {
-                System.out.println(dialogSeparator + "\n" + input + "\n" + dialogSeparator);
+                tasks.add(input);
+                System.out.println(dialogSeparator + "\n" + "added: " + input + "\n" + dialogSeparator);
             }
         }
     }
