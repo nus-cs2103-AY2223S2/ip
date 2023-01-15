27c5f63a68d8d14e0636679e6f034529182fd78c
hingen <1627313-hingen@users.noreply.gitlab.com>
2023-01-15 22:19:33 +0800

    Duke class: change to support user input with echo and bye command
    User's need to be able to input echo and bye commands.
    
    Let's change main(String[]) to have a loop that takes user inputs and
    for each input, run the appropriate command and display the resulting
    message. Exit the loop when the bye command is inputted.
    
    The code is written such that future modifications can be easily made to
    account for other command types.

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,19 +1,54 @@
 package duke;
 
+import duke.command.ByeCommand;
+import duke.command.Command;
+import duke.command.EchoCommand;
+
+import java.util.Scanner;
+
 public class Duke {
+    private final static ByeCommand byeCommand = new ByeCommand();
+    private final static EchoCommand echoCommand = new EchoCommand();
+
     public static void main(String[] args) {
+        printGreeting();
+        runInputLoop();
+    }
+
+    private static void printGreeting() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         printMessage(String.format("Hello from\n%s\nWhat can I do for you?", logo));
-
     }
 
     private static void printMessage(String message) {
         System.out.println("    ____________________________________________________________");
         System.out.printf("     %s\n\n", message.replace("\n", "\n     "));
         System.out.print("    ____________________________________________________________\n\n");
+    }
+
+    private static void runInputLoop() {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String input = scanner.nextLine();
+            Command command = getCommand(input);
+            printMessage(command.run(input));
+
+            if (input.equals("bye")) {
+                break;
+            }
+        }
+    }
+
+    private static Command getCommand(String input) {
+        if (input.equals("bye")) {
+            return byeCommand;
+        } else {
+            return echoCommand;
+        }
     }
 }
