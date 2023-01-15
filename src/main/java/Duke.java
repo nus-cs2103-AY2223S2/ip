94870025bf7557b15ba39751a65395502683abb6
tyx021 <yxting1109@gmail.com>
2023-01-15 12:02:03 +0800

    Added Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,30 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String lines = "\t____________________________________________________________\n";
+        System.out.println(
+                lines +
+                        "\tHello! I'm Duke\n" +
+                        "\tWhat can I do for you?\n" +
+                        lines
+        );
+
+        String commands = "";
+        Scanner sc = new Scanner(System.in);
+        commands = sc.nextLine();
+        while (!commands.equals("bye")) {
+            System.out.println(
+                    lines +
+                    "\t" + commands + "\n" +
+                    lines
+            );
+            commands = sc.nextLine();
+        }
+        System.out.println(
+                lines +
+                "\tBye. Hope to see you again soon!\n" +
+                lines
+        );
     }
 }
