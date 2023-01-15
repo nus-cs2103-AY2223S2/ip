45ac0ad500421e88e845296b9da127b1ab7435c7
anthonynmh <anthonyneomh@gmail.com>
2023-01-16 01:56:39 +0800

    Updated with NumberFormatException handling

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -21,6 +21,18 @@ public class Duke {
                 break;
             } else if (line.equals("list")) {
                 ToDoList.list();
+            } else if (line.matches("mark(.*)")) {
+                try {
+                    ToDoList.markDone(Integer.parseInt(line.substring(line.length() - 1)));
+                } catch (NumberFormatException e) {
+                    System.out.println("Please indicate which task to mark as done.");
+                }
+            } else if (line.matches("unmark(.*)")) {
+                try {
+                    ToDoList.unmark(Integer.parseInt(line.substring(line.length() - 1)));
+                } catch (NumberFormatException e) {
+                    System.out.println("Please indicate which task to unmark.");
+                }
             } else {
                 System.out.println("\t" + line);
                 ToDoList.add(line);
