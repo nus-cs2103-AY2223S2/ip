9846d737b6474fea59484859cbc9cb787a074ea1
yanlings <e0774088@u.nus.edu>
2023-01-15 21:12:57 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,20 +9,33 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello I'm Duke\nWhat can I do for you?" + logo);
-        String[] array = new String[100];
+        Task[] array = new Task[100];
         Scanner myObj = new Scanner(System.in);
         String reply = myObj.nextLine();
-        int count = 1;
+        int count = 0;
         while (!reply.contains("bye")) {
             System.out.println(reply);
-            if (!reply.contains("list")) {
+            Task action = new Task(reply);
+            if (!reply.contains("list") && !reply.contains("mark")) {
                 System.out.println("added: " + reply);
-                array[count] = "" + count + "." + reply;
+                array[count] = action;
                 count += 1;
-            } else {
-                for (String element: array) {
+
+            } else if (reply.contains("unmark")) {
+                int value = Integer.parseInt(reply.replaceAll("[^0-9]", "")) - 1;
+                array[value].unmark();
+                System.out.println(" OK, I've marked this task as not done yet:\n ["+array[value].getStatusIcon()+"]" + array[value].description);
+            } else if (reply.contains("mark")) {
+            int value = Integer.parseInt(reply.replaceAll("[^0-9]", "")) - 1;
+            array[value].mark();
+            System.out.println("Nice! I've marked this task as done:\n ["+array[value].getStatusIcon()+"]" + array[value].description);
+            }
+            else if (reply.contains("list")) {
+                System.out.println("Here are the tasks in your list:\n");
+                int listcount = 1;
+                for (Task element: array) {
                     if (element != null) {
-                        System.out.println(element);
+                        System.out.println("" + listcount + "." + "[" + element.getStatusIcon() + "]" + element.description);
                     }
                 }
             }
@@ -32,3 +45,5 @@ public class Duke {
         System.out.println("Bye, Hope to see you again soon!");
     }
 }
+
+
