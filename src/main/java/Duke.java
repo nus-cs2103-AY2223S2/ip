0c46afb4fea10b447262cca67ca7a473ae6e4962
mayuanxin1234 <yuanxinma@gmail.com>
2023-01-16 23:39:23 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,14 @@
-public class Duke {
+import java.util.Scanner;
+class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        Scanner myObj = new Scanner(System.in);
+        String input = myObj.nextLine();
+        while(!input.equals("bye")) {
+            System.out.println(input);
+            input = myObj.nextLine();
+        }
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
