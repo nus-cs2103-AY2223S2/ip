30cc127ee1b3345c76abc1a31e552230831d6a06
LAPTOP-DOG86KJ2\pinoc <pinochoi@yahoo.com.sg>
2023-01-16 22:08:59 +0800

    added new features "add and list" to Crystal

--- src/main/java/Crystal.java
+++ src/main/java/Crystal.java
@@ -7,12 +7,26 @@ class Crystal {
         System.out.println(" Hi! I am CRYSTAL.\n How may I be of assistance?");
         System.out.println(" ____________________________________________________________");
         Scanner sc = new Scanner(System.in);
-        String input = sc.next();
+        String input = sc.nextLine();
+
+        ArrayList<String> list = new ArrayList<>();
         while (!input.equals("bye")) {
-            System.out.println(" ____________________________________________________________");
-            System.out.println(input);
-            System.out.println(" ____________________________________________________________");
-            input = sc.next();
+            if (input.equals("list")) {
+                System.out.println(" ____________________________________________________________");
+                for (int i = 0; i < list.size(); i++) {
+                    System.out.println(i+1 + ". " + list.get(i));
+                }
+                System.out.println(" ____________________________________________________________");
+            } else {
+                System.out.println(" ____________________________________________________________");
+                list.add(input);
+                System.out.println("added: " + input);
+                System.out.println(" ____________________________________________________________");
+
+            }
+            input = sc.nextLine();
+
+
         }
         System.out.println(" ____________________________________________________________");
         System.out.println(" Thank You. Please come by again~!");
