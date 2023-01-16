56baca829ad57721803b906f97a54e6418cc2507
Joy Ng Jing Ru <e0943052@u.nus.edu>
2023-01-16 13:34:43 +0800

    Add records tracker

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 public class Duke {
+    private static String[] records = new String[100];
+    private static int index = 0;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Echo! I'm Bond.\nWhat is my mission?");
@@ -10,7 +12,15 @@ public class Duke {
                 System.out.println("Roger. Agent Bond signing off ~");
                 break;
             }
-            System.out.println("Bond: " + userinput);
+            else if (userinput.equals("missions")) {
+                for (int i = 0; i < index; i++) {
+                    System.out.println((i+1) + ". " + records[i]);
+                }
+            } else {
+                records[index] = userinput;
+                index++;
+                System.out.println("Registered: " + userinput);
+            }
         }
     }
 }
