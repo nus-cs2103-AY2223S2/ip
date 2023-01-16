8e664a75a7871f2a4a926565fa8b3cc5e9f7fa48
alson001 <alsonang01@gmail.com>
2023-01-16 23:49:03 +0800

    Level 1. Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,28 @@
+import java.util.Scanner;
 public class Duke {
+    public static final String HORIZONTAL_LINE =
+            "____________________________________________________________";
+    public static final String GREETING_MESSAGE =
+            HORIZONTAL_LINE + "\n" + "Hello! I'm Duke" +
+                    "\n" + "What can I do for you?" + "\n" + HORIZONTAL_LINE;
+    public static final String BYE_MESSAGE =
+            HORIZONTAL_LINE + "\n" + "Bye. Hope to see you again soon!" +
+                    "\n" + HORIZONTAL_LINE;
+
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(GREETING_MESSAGE);
+        Scanner scanner = new Scanner(System.in);
+        boolean isOver = false;
+        while (!isOver) {
+            String userCommands = scanner.nextLine();
+            if (userCommands.equalsIgnoreCase("bye")) {
+                isOver = true;
+                System.out.println(BYE_MESSAGE);
+            } else {
+                System.out.println(HORIZONTAL_LINE + "\n" + userCommands + "\n" + HORIZONTAL_LINE);
+            }
+        }
+
     }
 }
