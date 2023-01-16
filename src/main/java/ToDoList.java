22c8a8c6ec41fc7c0076767b60fa730948b0d494
pzhengze <zhengze@u.nus.edu>
2023-01-16 22:27:50 +0800

    Updated ToDoList class to work with Task class

--- src/main/java/ToDoList.java
+++ src/main/java/ToDoList.java
@@ -1,20 +1,29 @@
 import java.util.ArrayList;
 
 public class ToDoList {
-    ArrayList<String> list = new ArrayList<>();
+    ArrayList<Task> list = new ArrayList<>();
 
     public ToDoList() {}
 
     public String add(String s) {
-        list.add(s);
+        list.add(new Task(s));
         return String.format("\t added: %s", s);
+    }
+
+    public String mark(int num) {
+        return list.get(num).mark();
+    }
+
+    public String unMark(int num) {
+        return list.get(num).unMark();
     }
 
     public String list() {
         StringBuilder output = new StringBuilder();
+        output.append("\t Here are the tasks in your list:\n");
         int index = 1;
-        for (String task : list) {
-            output.append(String.format("\t%d. %s\n", index, task));
+        for (Task task : list) {
+            output.append(String.format("\t %d.%s\n", index, task.toString()));
             index++;
         }
 
