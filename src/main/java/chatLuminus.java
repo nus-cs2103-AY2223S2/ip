40760782f0f7d540d12e1d09a40357a81fa8cdaa
Lu Chenyu <adam07018@163.com>
2023-01-16 13:45:00 +0800

    no message

---
+++ src/main/java/chatLuminus.java
@@ -0,0 +1,17 @@
+public class chatLuminus {
+
+    chatLuminus() {
+
+    }
+
+    public static void main(String[] args) {
+        chatLuminus a = new chatLuminus();
+        System.out.println(a.greeting());
+    }
+
+
+    private String greeting() {
+        return "Hello! I'm Duke\nWhat can I do for you?";
+    }
+
+}
