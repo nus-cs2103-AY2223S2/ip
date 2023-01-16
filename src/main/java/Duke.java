af53a20d567dd70b1fed0ac7ab0e9a3a6456f387
liujiayue314 <e0773130@u.nus.edu>
2023-01-16 17:31:16 +0800

    added greet, echo, exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,41 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    private static final String indentation = "    ";
+    private static final String horizontalLines = "____________________________________________________________";
+    private static final String newLine = indentation + horizontalLines;
+
+    public static void echo(String commands) {
+        System.out.println(newLine);
+        System.out.println(indentation + commands);
+        System.out.println(newLine);
+    }
+
+    public static void exit() {
+        System.out.println(newLine);
+        System.out.println(indentation + "Bye. Hope to see you again soon!");
+        System.out.println(newLine);
+    }
+
+    public static void greet() {
+        System.out.println(newLine);
+        System.out.println(indentation + "Hello! I'm Duke");
+        System.out.println(indentation + "What can I do for you?");
+        System.out.println(newLine);
+    }
+
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greet();
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String command = sc.nextLine();
+            if (command.equals("bye")) {
+                break;
+            }
+            echo(command);
+        }
+        exit();
     }
 }
