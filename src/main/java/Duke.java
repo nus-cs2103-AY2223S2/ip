6d292e6fad167c394a8de6691a8002247d4fe61c
gremmyz <92777033+gremmyz@users.noreply.github.com>
2023-01-16 17:40:54 +0800

    add TextUiTesting

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -59,7 +59,7 @@ public class Duke {
                         if (taskToMark == taskCount) {
                             t.markAsDone();
                             System.out.println("Nice! I've marked this task as done:");
-                            System.out.println(t);
+                            System.out.println("    " + t);
                             break;
                         } else {
                             taskCount++;
@@ -82,7 +82,7 @@ public class Duke {
                         if (taskToMark == taskCount) {
                             t.markAsUndone();
                             System.out.println("OK, I've marked this task as not done yet:");
-                            System.out.println(t);
+                            System.out.println("    " + t);
                             break;
                         } else {
                             taskCount++;
