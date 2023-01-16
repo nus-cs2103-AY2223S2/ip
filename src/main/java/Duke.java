5ecb8b99b01c840d8d22364603fdcc2d306dffed
pzhengze <zhengze@u.nus.edu>
2023-01-17 01:01:02 +0800

    Finished Level 5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Duke {
-    private static void printText(String s) {
+    public static void printText(String s) {
         System.out.println("\t____________________________________________________________");
         System.out.println(s);
         System.out.println("\t____________________________________________________________\n");
@@ -15,29 +15,35 @@ public class Duke {
         String input = scanner.next();
 
         while (!input.equals("bye")) {
-            switch (input) {
-                case "list":
-                    printText(list.list());
-                    break;
-                case "mark": {
-                    int num = Integer.parseInt(scanner.next());
-                    printText(list.mark(num - 1));
+            try {
+                switch (input) {
+                    case "list":
+                        printText(list.list());
+                        break;
+                    case "mark": {
+                        int num = Integer.parseInt(scanner.next());
+                        printText(list.mark(num - 1));
                     }
                     break;
-                case "unmark": {
-                    int num = Integer.parseInt(scanner.next());
-                    printText(list.unMark(num - 1));
+                    case "unmark": {
+                        int num = Integer.parseInt(scanner.next());
+                        printText(list.unMark(num - 1));
                     }
                     break;
-                case "todo":
-                    printText(list.add(TaskType.ToDos, scanner.nextLine().strip()));
-                    break;
-                case "deadline":
-                    printText(list.add(TaskType.Deadlines, scanner.nextLine().strip()));
-                    break;
-                case "event":
-                    printText(list.add(TaskType.Events, scanner.nextLine().strip()));
-                    break;
+                    case "todo":
+                        printText(list.add(TaskType.ToDos, scanner.nextLine().strip()));
+                        break;
+                    case "deadline":
+                        printText(list.add(TaskType.Deadlines, scanner.nextLine()));
+                        break;
+                    case "event":
+                        printText(list.add(TaskType.Events, scanner.nextLine()));
+                        break;
+                    default:
+                        throw new DukeException("\t ☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+                }
+            } catch (DukeException dukeException){
+                printText(dukeException.getMessage());
             }
 
             input = scanner.next();
--- src/main/java/ToDoList.java
+++ src/main/java/ToDoList.java
@@ -6,31 +6,69 @@ public class ToDoList {
     public ToDoList() {}
 
     public String add(TaskType type, String s) {
-        String output = "\t Got it. I've added this task:\n";
-        switch (type) {
-            case ToDos:
-                ToDos todo = new ToDos(s);
-                list.add(todo);
-                output += "\t   " + todo;
-                break;
-            case Deadlines:
-                int index = s.indexOf(" /by ");
-                Deadlines deadline = new Deadlines(s.substring(0, index),
-                        s.substring(index + 5));
-                list.add(deadline);
-                output += "\t   " + deadline;
-                break;
-            case Events:
-                int from = s.indexOf(" /from ");
-                int to = s.indexOf(" /to ");
-                Events event = new Events(s.substring(0, from),
-                        s.substring(from + 7, to),
-                        s.substring(to + 5));
-                list.add(event);
-                output += "\t   " + event;
-                break;
+
+        try {
+
+            String output = "\t Got it. I've added this task:\n";
+            switch (type) {
+                case ToDos:
+                    if (s.isBlank()) {
+                        throw new DukeException("\t ☹ OOPS!!! The description of a todo cannot be empty.");
+                    }
+
+                    ToDos todo = new ToDos(s);
+
+                    list.add(todo);
+                    output += "\t   " + todo;
+                    break;
+
+                case Deadlines:
+                    int index = s.indexOf(" /by ");
+                    String time = s.substring(index + 5).strip();
+
+                    if (index == 0) {
+                        throw new DukeException("\t ☹ OOPS!!! The description of a deadline cannot be empty.");
+                    }
+                    if (index == -1 || time.isEmpty()) {
+                        throw new DukeException("\t ☹ OOPS!!! The date/time of a deadline cannot be empty.");
+                    }
+
+                    Deadlines deadline = new Deadlines(s.substring(0, index)
+                                .strip(),
+                            time);
+                    list.add(deadline);
+                    output += "\t   " + deadline;
+                    break;
+
+                case Events:
+                    int from = s.indexOf(" /from ");
+                    int to = s.indexOf(" /to ");
+                    String toTime = s.substring(to + 5).strip();
+
+                    if (from == 0 || to == 0) {
+                        throw new DukeException("\t ☹ OOPS!!! The description of an event cannot be empty.");
+                    }
+                    if (from == -1 ||
+                            to == -1 ||
+                            to < from + 7 ||
+                            s.substring(from + 7, to).isBlank() ||
+                            toTime.isEmpty()) {
+                        throw new DukeException("\t ☹ OOPS!!! The start/end date of an event cannot be empty.");
+                    }
+
+                    Events event = new Events(s.substring(0, from)
+                                .strip(),
+                            s.substring(from + 7, to),
+                            toTime);
+                    list.add(event);
+                    output += "\t   " + event;
+                    break;
+            }
+            return String.format("%s\n\t Now you have %d tasks in the list.", output, list.size());
+        } catch (DukeException dukeException) {
+            return dukeException.getMessage();
         }
-        return String.format("%s\n\t Now you have %d tasks in the list.", output, list.size());
+
     }
 
     public String mark(int num) {
