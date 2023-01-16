b5057df6ac153eb172f07eaf9447114b7bcbe3fc
Sean Wong <58366602+ExtraShotLatte@users.noreply.github.com>
2023-01-16 13:52:28 +0800

    Create basic task implementation

---
+++ src/main/java/Task.java
@@ -0,0 +1,24 @@
+public class Task {
+    private boolean isDone = false;
+    private String name;
+
+    public Task(String name) {
+        this.name = name;
+    }
+
+    public void markDone() {
+        isDone = true;
+    }
+
+    public void markUndone() {
+        isDone = false;
+    }
+
+    public String toString() {
+        if (isDone) {
+            return "[x] " + name;
+        } else {
+            return "[ ] " + name;
+        }
+    }
+}
