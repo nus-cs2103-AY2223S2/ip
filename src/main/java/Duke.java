5a78b68740b056c8d4ee142d30e592a1f2e8bcb9
Joy Ng Jing Ru <e0943052@u.nus.edu>
2023-01-16 13:25:58 +0800

    Edit Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,16 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Echo! I'm Bond.\nWhat is my mission?");
+
+        while (true) {
+            String userinput = sc.nextLine();
+            if (userinput.equals("bye")) {
+                System.out.println("Roger. Agent Bond signing off ~");
+                break;
+            }
+            System.out.println("Bond: " + userinput);
+        }
     }
 }
