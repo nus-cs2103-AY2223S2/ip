b3e643a6066d48e10e6c5872122b75dccc1a5881
anchengyang <anchengyang72@gmail.com>
2023-01-16 16:47:47 +0800

    Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,11 +3,12 @@ import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\nL"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\nL"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println(logo);
         System.out.println("    " + "Hello I'm Monkey D Luffy\n    What can I do for you?");
 
         ArrayList<Task> Tasks = new ArrayList<Task>();
