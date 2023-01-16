d0a2d878acb284bf4f658541a26ca6f703820a77
Darren Sim <e0725166@u.nus.edu>
2023-01-16 21:43:19 +0800

    Completed level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,7 @@
+import java.util.Scanner;
+
 public class Duke {
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +9,25 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        Scanner in = new Scanner(System.in);
+
+        boolean isChatting = true;
+        while (isChatting) {
+            String chat = in.nextLine();
+            if (chat.equals("bye")) {
+                isChatting = false;
+                break;
+            }
+            respond(chat);
+        }
+        System.out.println("----------------------------------");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("----------------------------------");
+    }
+
+    private static void respond(String chat) {
+        System.out.println("----------------------------------");
+        System.out.println(chat);
+        System.out.println("----------------------------------");
     }
 }
