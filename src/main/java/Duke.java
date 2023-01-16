f39724f4690870466d35d1732b1a0a1339387659
FredericChow00 <ruihui00.chow@gmail.com>
2023-01-17 00:35:14 +0800

    Level-2 Completed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,16 +4,23 @@ public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner (System.in);
         String command = "";
+        String[] taskList = new String[100];
+        int taskCount = 0;
         while (!command.equals("bye")) {
             command = sc.nextLine();
             if (command.equals("list")) {
-                System.out.println(command);
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println(String.format("%d. %s", i + 1, taskList[i]));
+                }
             } else if (command.equals("blah")) {
                 System.out.println(command);
-            } else if (command.equals("bye")) {
+            } else if (command.equals("bye")){
                 System.out.println("Bye. Hope to see you again soon!");
+            } else {
+                taskList[taskCount] = command;
+                taskCount++;
             }
         }
     }
