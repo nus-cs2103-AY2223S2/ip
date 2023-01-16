6f2ce1f393e12b3f0cf7c0762054d896c3c91d9b
Germaine <germainelee02@gmail.com>
2023-01-16 23:36:32 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,21 +1,40 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
 
+    private static ArrayList<String> listOfThings = new ArrayList<>();
+
     public static void printWithLines(String text) {
-        System.out.println("    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println("    ____________________________");
         System.out.println("    " + text);
-        System.out.println("    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println("    ____________________________");
 
     }
+
+    public static void addItem(String text) {
+        listOfThings.add(text);
+        printWithLines("added: " + text);
+    }
+
+    public static void printList() {
+        String totalString = "";
+        for (int i = 0; i < listOfThings.size(); i++) {
+            totalString += i + ". " + listOfThings.get(i) + "\n" + "    ";
+        }
+        printWithLines(totalString);
+    }
     public static void main(String[] args) {
-        printWithLines("Hello! I'm Echoey!\n    I'll echo you until you say \"bye\".");
+        printWithLines("Hello! I'm Duke!\n    What can I do for you today?");
         while (true) {
             Scanner input = new Scanner(System.in);
             String line = input.nextLine();
             if (line.equalsIgnoreCase("bye")) break;
-
-            printWithLines(line);
+            if (line.equalsIgnoreCase(("list"))) {
+                printList();
+                continue;
+            }
+            addItem(line);
         }
         printWithLines("Bye! Hope to see you again soon!");
     }
