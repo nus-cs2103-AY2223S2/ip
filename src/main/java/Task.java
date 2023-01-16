dbde18cf216576bc0c3a1a51e09bab839b131a7f
engenhui1999 <79076045+engenhui1999@users.noreply.github.com>
2023-01-17 00:30:55 +0800

    Adding Task files as I have accidentally omitted them earlier.

---
+++ src/main/java/Task.java
@@ -0,0 +1,31 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+    protected static String doneTaskString = "Nice! I've marked this task as done:";
+    protected static String undoneTaskString = "Nice! I've marked this task as not done yet:";
+    protected static String addTaskString = "Got it. I've added this task:";
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public void setDone() {
+        this.isDone = true;
+        System.out.println(doneTaskString + "\n" + this);
+    }
+
+    public void setUndone() {
+        this.isDone = false;
+        System.out.println(undoneTaskString + "\n" + this);
+    }
+
+    @Override
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + this.description;
+    }
+}
