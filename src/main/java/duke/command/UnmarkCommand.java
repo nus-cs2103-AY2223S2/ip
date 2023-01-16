a9120bcafc916bac89fb4b9905b6c881596bea6b
hingen <1627313-hingen@users.noreply.gitlab.com>
2023-01-16 21:20:29 +0800

    UnmarkCommand class: add handling of input errors
    User input for unmark command might be missing an index or the index is
    not an integer or the index is out of bounds.
    
    Let's throw a DukeException with the error described in the exception
    message when such errors occur.

--- src/main/java/duke/command/UnmarkCommand.java
+++ src/main/java/duke/command/UnmarkCommand.java
@@ -1,5 +1,6 @@
 package duke.command;
 
+import duke.exception.DukeException;
 import duke.task.Task;
 
 import java.util.List;
@@ -16,11 +17,26 @@ public class UnmarkCommand implements Command {
      * @param tasks {@inheritDoc}
      * @return CommandResponse object containing an acknowledgement message and an updated task list with the specified
      * task's done status set to false.
+     * @throws DukeException Indicates missing index or non-integer index or out of bound index in input
      */
     @Override
-    public CommandResponse run(String input, List<Task> tasks) {
+    public CommandResponse run(String input, List<Task> tasks) throws DukeException {
         String arg = input.replaceFirst("unmark ", "");
-        int index = Integer.parseInt(arg) - 1;
+
+        if (arg.isEmpty()) {
+            throw new DukeException("The index of an unmark cannot be empty.");
+        }
+
+        int index;
+        try {
+            index = Integer.parseInt(arg) - 1;
+        } catch (NumberFormatException e) {
+            throw new DukeException("The index of an unmark must be an integer.");
+        }
+
+        if (index >= tasks.size() || index < 0) {
+            throw new DukeException("The index of an unmark must be between 1 and the number of task");
+        }
 
         Task task = tasks.get(index);
         task = task.setDone(false);
