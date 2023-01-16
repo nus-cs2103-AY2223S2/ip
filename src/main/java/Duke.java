5c8a1d6d80782813febaefe72a7a019a30d041d4
Huang Zhizhou <e0725208@u.nus.edu>
2023-01-16 13:39:39 +0800

    Added greet, echo, exit features (Level 1)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,20 @@
+import java.util.*;
 public class Duke {
     public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! Im Zhizhou's Chatbot");
+        System.out.println("What can I do for you?");
+        String output = "";
+        while (!(output = userInput.next()).equals("bye")) {
+
+            System.out.println("   " + output);
+        }
+        System.out.println("   Bye. Hope to see you again soon!");
     }
 }
