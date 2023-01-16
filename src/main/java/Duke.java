72616faf33fddb02dacf111c1809e5e43890b30f
Tempura-Person <92144641+Tempura-Person@users.noreply.github.com>
2023-01-16 14:35:06 +0800

    Added Duke echoes and exit function.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -7,8 +9,29 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
+        // Default welcome message and message border
         String border = "_______________________________\n";
         System.out.println(border + "Sup, Duke here.\nWhat do you want from me?\n" + border);
 
+        // while LoopEnd = true loop to accept user input
+        boolean LoopEnd = false;
+        while (LoopEnd == false) {
+            Scanner UserScan = new Scanner(System.in);
+            String UserInput = UserScan.nextLine();
+
+            // switch case for future commands
+            switch (UserInput) {
+
+                // loop breaks, ending program if input is "bye"
+                case ("bye"):
+                    System.out.println(border + "Goodbye, then!\n" + border);
+                    LoopEnd = true;
+                    break;
+
+                // Duke repeats input by default
+                default:
+                    System.out.println(border + UserInput + "\n" + border);
+            }
+        }
     }
 }
