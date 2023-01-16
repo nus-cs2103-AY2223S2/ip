673c31203d55b6288426b55b7c639802408b7de2
hingen <1627313-hingen@users.noreply.gitlab.com>
2023-01-16 21:32:11 +0800

    MarkCommand class: add handling of input errors
    User input for mark command might be missing an index or the index is
    not an integer or the index is out of bounds.
    
    Let's throw a DukeException with the error described in the exception
    message when such errors occur.

--- src/main/java/duke/command/MarkCommand.java
+++ src/main/java/duke/command/MarkCommand.java
@@ -1,5 +1,6 @@
 package duke.command;
 
+import duke.exception.DukeException;
 import duke.task.Task;
 
 import java.util.List;
@@ -16,11 +17,26 @@ public class MarkCommand implements Command {
      * @param tasks {@inheritDoc}
      * @return CommandResponse object containing an acknowledgement message and an updated task list with the specified
      * task's done status set to true.
+     * @throws DukeException Indicates missing index or non-integer index or out of bound index in input
      */
     @Override
-    public CommandResponse run(String input, List<Task> tasks) {
+    public CommandResponse run(String input, List<Task> tasks) throws DukeException {
         String arg = input.replaceFirst("mark ", "");
-        int index = Integer.parseInt(arg) - 1;
+
+        if (arg.isEmpty()) {
+            throw new DukeException("The index of an mark cannot be empty.");
+        }
+
+        int index;
+        try {
+            index = Integer.parseInt(arg) - 1;
+        } catch (NumberFormatException e) {
+            throw new DukeException("The index of an mark must be an integer.");
+        }
+
+        if (index >= tasks.size() || index < 0) {
+            throw new DukeException("The index of an mark must be between 1 and the number of task");
+        }
 
         Task task = tasks.get(index);
         task = task.setDone(true);
