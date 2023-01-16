00b421059a86b811f2f7414f52b886d604b25617
WindyCall <E0902032@U.NUS.EDU>
2023-01-17 00:36:01 +0800

    Complete Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,14 @@ import java.util.Scanner;
 
 public class Duke {
 
+    private String[] lists;
+    private int cnt;
+
+    public Duke() {
+        this.lists = new String[100];
+        this.cnt = 0;
+    }
+
     private static void space() {
         System.out.print("     ");
     }
@@ -34,16 +42,35 @@ public class Duke {
         line();
     }
 
+    private void addCommand(String message) {
+        line();
+        space();
+        System.out.println("added: " + message);
+        this.lists[cnt] = message;
+        this.cnt++;
+        line();
+    }
+
+    private void displayLists() {
+        line();
+        for (int i = 0; i < this.cnt; i++) {
+            space();
+            System.out.println((i + 1) + ". " + this.lists[i]);
+        }
+        line();
+    }
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         greeting();
+        Duke chatBox = new Duke();
         while(true) {
             String userCommand = scan.nextLine();
             if (userCommand.equals("bye")) {
                 byeWords();
                 break;
             }
-            echoCommand(userCommand);
+            if (userCommand.equals("list")) chatBox.displayLists();
+            else chatBox.addCommand(userCommand);
         }
         return ;
     }
