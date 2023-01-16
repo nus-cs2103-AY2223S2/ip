771767ee8a7125f8b5da86dbc082dd62901ea2f6
seadragon2000341 <limhaileongshawn@gmail.com>
2023-01-16 21:01:47 +0800

    Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,8 @@
 import java.util.Scanner;
 public class Duke {
+    private static int index;
+    private static String[] list = new String[100];
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -8,15 +11,26 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+
         while(true){
             Scanner reader = new Scanner(System.in);  // Reading from System.in
             System.out.println("Enter a Command!: ");
-            String s = reader.next();
+            String s = reader.nextLine();
             if(s.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             }
-            System.out.println(s);
+            if(s.equals("list")) {
+                for (int i = 0; i < index; i++) {
+                    System.out.println(i+1 + "." + list[i]);
+                }
+
+            }
+            else {
+                list[index] = s;
+                index++;
+                System.out.println("added: " + s);
+            }
         }
     }
 }
