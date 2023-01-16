e592521b26185be145753fd3b6543e8dd906d82e
bojie3 <xubojie2013@gmail.com>
2023-01-16 15:41:32 +0800

    Changed to level 2 to add tag

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -15,32 +15,19 @@ public class Duke {
             "     Bye. Hope to see you again soon!\n" +
             "    ____________________________________________________________\n";
 
-    private static String editStatus(String s, TaskManager tm) {
-        if (s.length() >= 6 && Objects.equals(s.substring(0, 5), "mark ") && s.substring(5, s.length()).matches("[0-9]+")) {
-            return tm.mark(Integer.parseInt(s.substring(5, s.length())));
-        } else if (s.length() >= 8 && Objects.equals(s.substring(0, 7), "unmark ") && s.substring(7, s.length()).matches("[0-9]+")) {
-            return tm.unmark(Integer.parseInt(s.substring(7, s.length())));
-        } else {
-            return "";
-        }
-    }
-
     public static void main(String[] args) {
         System.out.println(Duke.intro);
         Scanner sc = new Scanner(System.in);
         TaskManager tm = new TaskManager();
         while (true) {
             String str = sc.nextLine();
-            String editStatus = Duke.editStatus(str, tm);
             if (Objects.equals(str, "bye")) {
                 System.out.println(Duke.outro);
                 break;
             } else if (Objects.equals(str, "list")) {
                 System.out.println(tm.getAllTaskStr());
-            } else if (Objects.equals(editStatus, "")){
-                System.out.println(tm.addTask(str));
             } else {
-                System.out.println(editStatus);
+                System.out.println(tm.addTask(str));
             }
         }
     }
--- src/main/java/TaskManager.java
+++ src/main/java/TaskManager.java
@@ -3,44 +3,25 @@ import java.lang.StringBuilder;
 public class TaskManager {
     private static int size = 100;
     private int counter = 0;
-    private Task[] tasks = new Task[TaskManager.size];
-    private static final String starting = "    ____________________________________________________________\n";
-    private static final String ending = "    ____________________________________________________________\n";
-    private static final String spacing = "     ";
+    private String[] data = new String[TaskManager.size];
+    private static String starting = "    ____________________________________________________________\n";
+    private static String ending = "    ____________________________________________________________\n";
+    private static String spacing = "     ";
 
     public TaskManager() {}
 
     public String addTask(String task) {
-        //adds task and return add string for adding task
-        Task t = new Task(task);
-        this.tasks[this.counter] = t;
+        this.data[this.counter] = task;
         this.counter++;
-        return TaskManager.starting + TaskManager.spacing + "added: " + task + "\n" + TaskManager.ending;
-    }
-
-    public String mark(int index) {
-        //marks the task and return the string
-        this.tasks[index - 1].setStatus(true);
-        String s = TaskManager.starting + TaskManager.spacing + "Nice! I've marked this task as done:\n";
-        s += TaskManager.spacing + "  " + this.tasks[index-1].toString() + TaskManager.ending;
-        return s;
-    }
-
-    public String unmark(int index) {
-        //unmarks the task and returns the string
-        this.tasks[index - 1].setStatus(false);
-        String s = TaskManager.starting + TaskManager.spacing + "OK, I've marked this task as not done yet:\n";
-        s += TaskManager.spacing + "  " + this.tasks[index-1].toString() + TaskManager.ending;
-        return s;
+        String s = TaskManager.starting + TaskManager.spacing + "added: " + task + "\n" + TaskManager.ending;
+        return s.toString();
     }
 
     public String getAllTaskStr() {
-        //returns string for all the tasks
         StringBuilder s = new StringBuilder();
         s.append(TaskManager.starting);
-        s.append(TaskManager.spacing).append("Here are the tasks in your list\n");
         for (int i = 0; i < this.counter; i++) {
-            s.append(String.format("%s%d. %s", TaskManager.spacing, i + 1, this.tasks[i].toString()));
+            s.append(TaskManager.spacing).append(String.format("%d. ", i+1)).append(this.data[i]).append("\n");
         }
         s.append(TaskManager.ending);
         return s.toString();
