a01abb67bcf7d2239bbb18e4e2359f4c50e7f61d
WindyCall <E0902032@U.NUS.EDU>
2023-01-17 00:27:07 +0800

    Complete Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,50 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    private static void space() {
+        System.out.print("     ");
+    }
+
+    private static void line() {
+        space();
+        System.out.println("--------------------------------------------------");
+    }
+
+    private static void greeting() {
+        line();
+        space();
+        System.out.println("Hello! I'm WindyCall");
+        space();
+        System.out.println("How can I help you?");
+        line();
+    }
+
+    private static void byeWords() {
+        line();
+        space();
+        System.out.println("Bye. Always willing to provide my help for you!!!");
+        line();
+    }
+
+    private static void echoCommand(String message) {
+        line();
+        space();
+        System.out.println(message);
+        line();
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner scan = new Scanner(System.in);
+        greeting();
+        while(true) {
+            String userCommand = scan.nextLine();
+            if (userCommand.equals("bye")) {
+                byeWords();
+                break;
+            }
+            echoCommand(userCommand);
+        }
+        return ;
     }
 }
