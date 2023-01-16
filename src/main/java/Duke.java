558991405690dfc8deec56ca312d17b01cb9ad96
mayuanxin1234 <yuanxinma@gmail.com>
2023-01-17 00:03:50 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,9 +5,22 @@ class Duke {
         System.out.println("What can I do for you?");
         Scanner myObj = new Scanner(System.in);
         String input = myObj.nextLine();
+        String[] list = new String[100];
+        int currentIndex = 0;
         while(!input.equals("bye")) {
-            System.out.println(input);
-            input = myObj.nextLine();
+            if (!input.equals("list")) {
+                list[currentIndex] = input;
+                System.out.println("added: " + input);
+                currentIndex++;
+                input = myObj.nextLine();
+                continue;
+            } else {
+                for (int i = 0; i < currentIndex; i++) {
+                        System.out.println((i + 1) + ". " + list[i]);
+                }
+                input = myObj.nextLine();
+            }
+
         }
         System.out.println("Bye. Hope to see you again soon!");
     }
