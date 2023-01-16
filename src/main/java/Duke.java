ccf57e01d5d14be50e3508559feb8e996c4187fc
cjyothika <jyothikasivasai@gmail.com>
2023-01-16 21:06:52 +0800

    Duke.java: added mark and unmark functionalities for tasks Task.java: created task class to store if a task a marked and its description

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
+
 public class Duke {
-    static String[] tasks = new String[100];
+    static Task[] tasks = new Task[100];
     static int numOfTasks = 0;
     public static void main(String[] args) {
         /*String logo = " ____        _        \n"
@@ -16,12 +17,18 @@ public class Duke {
 
         while(true){
             String newInput = userInput.nextLine();
+            String[] parsedInput = newInput.split(" ");
+            String command = parsedInput[0];
 
-            if(newInput.equalsIgnoreCase("bye")){
+            if(command.equalsIgnoreCase("bye")){
                 break;
-            } else if(newInput.equalsIgnoreCase("list")){
+            } else if(command.equalsIgnoreCase("list")){
                 showList();
-            }else {
+            } else if(command.equalsIgnoreCase("mark")){
+                markTask(Integer.parseInt(parsedInput[1]) - 1);
+            } else if(command.equalsIgnoreCase("unmark")){
+                unmarkTask(Integer.parseInt(parsedInput[1]) - 1);
+            } else {
                 addTask(newInput);
             }
 
@@ -35,18 +42,32 @@ public class Duke {
         return "____________________________________________________________\n" + in +"\n" + "____________________________________________________________";
     }
 
-    public static void addTask(String task){
-        System.out.println(makeOutput("added: " + task));
-        tasks[numOfTasks] = task;
+    public static void addTask(String description){
+        System.out.println(makeOutput("added: " + description));
+        tasks[numOfTasks] = new Task(description);
         numOfTasks ++;
     }
 
     public static void showList(){
-        String result = "";
+        String result = "Here are the tasks in your list:\n";
         for(int i = 0; i < numOfTasks - 1; i ++){
-            result += Integer.toString(i + 1) + ". " + tasks[i] + "\n";
+            result += Integer.toString(i + 1) + "." + tasks[i].getStatusIcon() + " " + tasks[i] + "\n";
         }
-        result += Integer.toString(numOfTasks) + ". " + tasks[numOfTasks - 1];
+        result += Integer.toString(numOfTasks) + "." + tasks[numOfTasks - 1].getStatusIcon() + " " + tasks[numOfTasks - 1];
+        System.out.println(makeOutput(result));
+    }
+
+    public static void markTask(int taskNo){
+        tasks[taskNo].mark();
+        String result = "Nice! I've marked this task as done:\n";
+        result += tasks[taskNo].getStatusIcon() + " " + tasks[taskNo];
+        System.out.println(makeOutput(result));
+    }
+
+    public static void unmarkTask(int taskNo){
+        tasks[taskNo].unmark();
+        String result = "OK, I've marked this task as not done yet:\n";
+        result += tasks[taskNo].getStatusIcon() + " " + tasks[taskNo];
         System.out.println(makeOutput(result));
     }
 }
