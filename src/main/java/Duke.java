d931af63fc80713812c317177f41bd8521a2638c
Randall <randallnhr@gmail.com>
2023-01-16 18:50:45 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("Hello! I'm Duke\n" +
+                "What can I do for you?");
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String word = br.readLine();
+        ArrayList<String> lst = new ArrayList<>();
+        lst.add(word);
+
+        while (!word.equals("bye")) {
+            System.out.println("Duke: " + word);
+            word = br.readLine();
+        }
+        System.out.println("Duke: Goodbye");
     }
 }
