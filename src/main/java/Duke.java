0703fcd29ad05acc7b63043dd4e4d74aa5180da3
slackernoob <maxang12@gmail.com>
2023-01-17 05:02:12 +0800

    added Level-1: Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,39 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        String spacingIndent = "    ";
+        String welcomeMsg = spacingIndent + " "
+                + "Hello! I'm Duke\n"
+                + spacingIndent + " "
+                + "How can I help you?\n";
+        String exitMsg = spacingIndent + " "
+                + "Farewell! See you soon!\n";
+        String dashes = spacingIndent
+                + "______________________________"
+                + "______________________________\n";
+        String curInput = "";
+        boolean status = true;
+        System.out.println(dashes
+                + welcomeMsg
+                + dashes);
+
+        while (status) {
+            curInput = sc.nextLine();
+
+            if (curInput.equals("bye")){
+                status = false;
+            } else {
+                System.out.println(dashes
+                        + spacingIndent + " " + curInput
+                        + "\n" + dashes);
+            }
+        }
+
+        System.out.println(dashes
+                + exitMsg
+                + dashes);
+
     }
 }
