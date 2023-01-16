4e12dadb97c868b328b8a9c74eda856241cdf880
randallnhr <randallnhr@gmail.com>
2023-01-16 19:25:01 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Duke {
     public static void main(String[] args) throws IOException {
@@ -13,11 +14,21 @@ public class Duke {
 
         String word = br.readLine();
         ArrayList<String> lst = new ArrayList<>();
-        lst.add(word);
 
         while (!word.equals("bye")) {
-            System.out.println("Duke: " + word);
-            word = br.readLine();
+            if (word.equals("list")) {
+                int curr = 1;
+                Iterator<String> iter = lst.iterator();
+                while (iter.hasNext()) {
+                    System.out.println(curr + ". " + iter.next());
+                    curr++;
+                }
+                word = br.readLine();
+            } else {
+                System.out.println("Added: " + word);
+                lst.add(word);
+                word = br.readLine();
+            }
         }
         System.out.println("Duke: Goodbye");
     }
