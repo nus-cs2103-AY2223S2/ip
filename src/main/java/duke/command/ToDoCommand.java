28fecb8e900640db9b716e0f2c5237029c4c4c40
hingen <1627313-hingen@users.noreply.gitlab.com>
2023-01-16 22:47:52 +0800

    ToDoCommand class: add handling of input errors
    User input for todo command might have an empty description.
    
    Let's throw a DukeException with the error described in the exception
    message when such an error occurs.

--- src/main/java/duke/command/ToDoCommand.java
+++ src/main/java/duke/command/ToDoCommand.java
@@ -1,15 +1,43 @@
 package duke.command;
 
+import duke.exception.DukeException;
 import duke.task.Task;
 import duke.task.ToDo;
+
+import java.util.List;
 
 /**
  * Represents an add To-Do task command.
  */
 public class ToDoCommand extends AddCommand {
+    /**
+     * {@inheritDoc}
+     *
+     * @param input {@inheritDoc}
+     * @param tasks {@inheritDoc}
+     * @return {@inheritDoc}
+     * @throws DukeException Indicates empty description in input.
+     */
     @Override
-    protected Task createTask(String input) {
-        String description = input.replaceFirst("todo ", "");
+    public CommandResponse run(String input, List<Task> tasks) throws DukeException {
+        return super.run(input, tasks);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @param input {@inheritDoc}
+     * @return {@inheritDoc}
+     * @throws DukeException Indicates empty description in input.
+     */
+    @Override
+    protected Task createTask(String input) throws DukeException {
+        String description = input.replaceFirst("todo ", "").trim();
+
+        if (description.isEmpty()) {
+            throw new DukeException("The description of a todo cannot be empty.");
+        }
+
         return new ToDo(false, description);
     }
 }
