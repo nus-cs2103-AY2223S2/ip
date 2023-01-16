192f5a55b8a21644b01f288b033bb86521b4f591
bokung <e0774428@u.nus.edu>
2023-01-16 21:27:35 +0800

    Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,10 @@
 import java.util.Scanner;
 
 public class Duke {
-
     private static void printWithDecorations(String input) {
-        System.out.println("OwO OwO OwO OwO OwO OwO");
-        System.out.println('~' + input + '~');
-        System.out.println("OwO OwO OwO OwO OwO OwO");
+        System.out.println("\nOwO OwO OwO OwO OwO OwO");
+        System.out.println(input);
+        System.out.println("OwO OwO OwO OwO OwO OwO\n");
     }
 
     private static void welcome() {
@@ -25,13 +24,34 @@ public class Duke {
         Duke.printWithDecorations(bye);
     }
 
+    private static void displayTasks(String[] tasks, int numTasks) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < numTasks; i++) {
+            sb.append(String.format("%d. %s\n", i+1, tasks[i]));
+        }
+        Duke.printWithDecorations(sb.toString());
+    }
+
     public static void main(String[] args) {
         Duke.welcome();
-        
+
+        String[] tasks = new String[100];
+        int numTasks = 0;
+
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
         while (!input.equals("bye")) {
-            Duke.printWithDecorations(input);
+
+            if (input.equals("list")) {
+                Duke.displayTasks(tasks, numTasks);
+            } else if (numTasks < 100) {
+                Duke.printWithDecorations("added: " + input);
+                tasks[numTasks] = input;
+                numTasks++;
+            } else {
+                Duke.printWithDecorations("Not enough slots!");
+            }
+
             input = sc.nextLine();
         }
 
