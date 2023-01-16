6e286ebb5234cd7efce3debbf1bc6206c9cab14d
Bernice <bernicetoh2003@gmail.com>
2023-01-16 12:50:37 +0800

    add level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,20 +4,22 @@ import java.util.Scanner;
 
 public class Duke {
 
-    public static List li = new ArrayList<String>();
+    public static Task[] li = new Task[100];
 
     public static void addTask(String task) {
-        li.add(task);
+        int i = Task.getCount();
+        li[i] = new Task(task);
         System.out.println("new task added: " + task);
-        System.out.println("What else can I do for you?");
     }
 
     public static void listCommand() {
-        if (li.size() == 0) {
-            System.out.println("You have not added any tasks.");
+        if (Task.getCount() == 0) {
+            System.out.println("You have no tasks in your list.");
         } else {
-            for (int i = 0; i < li.size(); i++) {
-                System.out.println(i+1 + ". " + li.get(i));
+            System.out.println("Here are the tasks in your list:");
+            for (int i = 0; i < Task.getCount(); i++) {
+                System.out.println(i+1+ "." + li[i]);
+
             }
         }
         System.out.println("What else can I do for you?");
@@ -31,6 +33,51 @@ public class Duke {
     public static void byeCommand() {
         System.out.println("Bye. Hope to see you again soon!");
     }
+
+    public static boolean isNumber(String str) {
+        try {
+            Double.parseDouble(str);
+            return true;
+        } catch(NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public static void checkMarkOrAdd(String str) {
+        if (str.equals("mark") || str.equals("unmark") || str.equals("mark ") || str.equals("unmark ")) {
+            System.out.println("You need to specify the task number " +
+                    "that you would like me to " + str + " after the command.");
+            System.out.println("e.g " + str + " 2");
+        } else {
+            String arr[] = str.split("\\s+");
+            if (arr.length != 2 ||
+                    (arr.length == 2 && !arr[0].equals("mark")) && !isNumber(arr[1]) ||
+                    (arr.length == 2 && !arr[0].equals("unmark")) && !isNumber(arr[1])) {
+                addTask(str);
+            } else {
+                if (arr.length == 2 && (arr[0].equals("mark")
+                        && Integer.parseInt(arr[1]) <= Task.getCount() && Integer.parseInt(arr[1]) != 0)) {
+                    // mark task
+                    Task t = li[Integer.parseInt(arr[1]) - 1];
+                    t.mark();
+                } else if (arr.length == 2 && (arr[0].equals("unmark")
+                        && Integer.parseInt(arr[1]) <= Task.getCount() && Integer.parseInt(arr[1]) != 0)) {
+                    // unmark task
+                    Task t = li[Integer.parseInt(arr[1])-1];
+                    t.unMark();
+
+                } else if (arr.length == 2 && arr[0].equals("unmark")
+                        && Integer.parseInt(arr[1]) > Task.getCount() || Integer.parseInt(arr[1]) == 0) {
+                    System.out.println("Huh.. the task does not exist.");
+
+                } else if (arr.length == 2 && arr[0].equals("mark")
+                        && Integer.parseInt(arr[1]) > Task.getCount() || Integer.parseInt(arr[1]) == 0) {
+                    System.out.println("Huh.. the task does not exist.");
+                }
+            }
+        }
+    }
+
 
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -52,7 +99,8 @@ public class Duke {
                 blahCommand();
                 command = sc.nextLine();
             } else {
-                addTask(command);
+                checkMarkOrAdd(command);
+                System.out.println("What else can I do for you?");
                 command = sc.nextLine();
             }
         }
---
+++ src/main/java/Task.java
@@ -0,0 +1,47 @@
+public class Task {
+    public String desc;
+    public boolean isMarked;
+    public static int count = 0;
+
+    Task(String desc) {
+        this.desc = desc;
+        this.isMarked = false;
+        Task.count ++;
+    }
+
+    public void mark() {
+        if (isMarked) {
+            System.out.println("This task has already been marked as done.");
+        } else {
+            isMarked = true;
+            System.out.println("Great job at completing this task! I've marked it as done:");
+            System.out.println(this);
+        }
+    }
+
+    public void unMark() {
+        if (!isMarked) {
+            System.out.println("Oops! This task has not been marked as done before.");
+        } else {
+            isMarked = false;
+            System.out.println("Ok, I've marked this task as not done yet:");
+            System.out.println(this);
+        }
+    }
+
+    public static int getCount() {
+        return Task.count;
+    }
+
+    @Override
+    public String toString() {
+        if (isMarked) {
+            return "[X] " + desc;
+        } else {
+            return "[ ] " + desc;
+        }
+    }
+
+
+
+}
