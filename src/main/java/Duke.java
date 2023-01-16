45c882ef614375ce0d59263b2c221653a826d382
yyj-02 <yeohyongjie@outlook.com>
2023-01-16 16:42:59 +0800

    Level-7(TaskList): add method to populate tasks from file

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,11 @@
 import command.Command;
 import dukeexeption.DukeException;
+import dukeexeption.InvalidArgumentException;
 import parser.Request;
 import storage.TaskList;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Duke {
@@ -10,7 +13,7 @@ public class Duke {
      * Prints the formatted response in the console.
      * @param response  the string to be printed
      */
-    public static void returnFormattedResponse(String response) {
+    private static void returnFormattedResponse(String response) {
         final int INDENTS = 4;
 
         String lineAddedResponse = "____________________________________________________________\n"
@@ -20,6 +23,26 @@ public class Duke {
                 + lineAddedResponse.replace("\n", "\n" + " ".repeat(INDENTS));
 
         System.out.println(indentedResponse);;
+    }
+
+    private static File linkLocalDriveOnStartup(String filepath) {
+        if (filepath.trim().equals("")) {
+            throw new InvalidArgumentException("Filepath cannot be empty.");
+        }
+        File dataFile =  new File(filepath);
+        dataFile.getParentFile().mkdirs();
+
+        if (!dataFile.exists()) {
+            try {
+                dataFile.createNewFile();
+            } catch (IOException error) {
+                error.printStackTrace();
+            }
+        } else if (dataFile.exists() && dataFile.isDirectory()) {
+            throw new InvalidArgumentException("Directory path provided, change filepath to a path to a file.");
+        }
+
+        return dataFile;
     }
 
     public static void main(String[] args) {
@@ -32,11 +55,13 @@ public class Duke {
                 + "What can I do for you?";
         final String EXITING_MESSAGE = "Bye. Hope to see you again soon!";
 
+        File tasksFile = linkLocalDriveOnStartup("./data/tasks.txt");
+
         System.out.println(logo);
         returnFormattedResponse(INTRODUCTION_MESSAGE);
 
         Scanner scanner = new Scanner(System.in);
-        TaskList taskList = new TaskList();
+        TaskList taskList = new TaskList(tasksFile);
         while(true) {
             String request = scanner.nextLine();
             if (request.equals("bye")) {
--- src/main/java/storage/TaskList.java
+++ src/main/java/storage/TaskList.java
@@ -1,20 +1,37 @@
 package storage;
 
+import command.*;
 import task.Task;
 import task.Todo;
 import task.Deadline;
 import task.Event;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class TaskList {
     private final ArrayList<Task> tasks;
+    private final File tasksFile;
 
     /**
      * Constructor for the task list.
      */
     public TaskList() {
         this.tasks = new ArrayList<>();
+        this.tasksFile = null;
+    }
+
+    /**
+     * Constructor for task list from a file.
+     * @param tasksFile     a file containing the stored tasks
+     */
+    public TaskList(File tasksFile) {
+        this.tasks = new ArrayList<>();
+        this.tasksFile = tasksFile;
+        loadTasksFromFile();
     }
 
     /**
@@ -100,5 +117,63 @@ public class TaskList {
      */
     public int countTask() {
         return this.tasks.size();
+    }
+
+    private void loadTasksFromFile() {
+        try {
+            FileReader fr = new FileReader(this.tasksFile);
+            BufferedReader reader = new BufferedReader(fr);
+            String s;
+            int currIndex = 0;
+            while ((s = reader.readLine()) != null) {
+                String[] data = s.split(" \\| ");
+                try {
+                    String taskType = data[0];
+                    String taskIsCompleted = data[1];
+                    String task = data[2];
+
+                    switch (taskType) {
+                        case "T":
+                            new TodoCommand(task).run(this);
+                            break;
+                        case "D":
+                            String deadline = data[3];
+                            new DeadlineCommand(task, deadline).run(this);
+                            break;
+                        case "E":
+                            String eventStartTime = data[3].split("-", 2)[0];
+                            String eventEndTime = data[3].split("-", 2)[1];
+                            new EventCommand(task, eventStartTime, eventEndTime).run(this);
+                            break;
+                        default:
+                            System.out.println("Error occurs at: " + s);
+                            throw new RuntimeException("Datafile provided is corrupted," +
+                                    "create a new file or follow the format.");
+                    }
+
+                    switch (taskIsCompleted) {
+                        case "0":
+                            new UnmarkCommand(currIndex).run(this);
+                            break;
+                        case "1":
+                            new MarkCommand(currIndex).run(this);
+                            break;
+                        default:
+                            System.out.println("Error occurs at: " + s);
+                            throw new RuntimeException("Datafile provided is corrupted," +
+                                    "create a new file or follow the format.");
+                    }
+
+                    currIndex++;
+                } catch (IndexOutOfBoundsException error) {
+                    throw new RuntimeException("Datafile provided is corrupted," +
+                            "create a new file or follow the format.");
+                }
+            }
+            reader.close();
+            fr.close();
+        } catch (IOException error) {
+            error.printStackTrace();
+        }
     }
 }
