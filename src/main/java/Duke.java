7ceead2d9b335e400bfad86bcd28139d50ee5493
0x787af25e <davidgongchenjun@gmail.com>
2023-01-16 20:25:07 +0800

    started switch statement

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,16 +3,24 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Duke {
-    public static ArrayList<Task> taskStore = new ArrayList<>();
-
-    private static ArrayList<String> splitBySpace(String input) {
-        return new ArrayList<String>(Arrays.asList(input.split(" ")));
+    public enum ParseFunctions {
+        SPLIT_ALL, MARK, TODO, DEADLINE, EVENT
     }
+    public static ArrayList<Task> taskStore = new ArrayList<>();
 
     private static Task getTaskForMarking(ArrayList<String> parsed) {
         int completedIndex = Integer.parseInt(parsed.get(1)) - 1; // index of the task completed
         Task completedTask = taskStore.get(completedIndex); // actual task
         return completedTask;
+    }
+
+    public static ArrayList<String> parser(String input, ParseFunctions parse_type) {
+        switch (parse_type) {
+            case SPLIT_ALL:
+                return new ArrayList<String>(Arrays.asList(input.split(" ")));
+
+        }
+        return null;
     }
 
     public static void main(String[] args) {
@@ -22,23 +30,23 @@ public class Duke {
             Scanner myScanner = new Scanner(System.in);
             String command = myScanner.nextLine();
 
-            ArrayList<String> parsed = splitBySpace(command);
+            ArrayList<String> toFindFirstWord = parser(command, ParseFunctions.SPLIT_ALL);
 
-            String first = parsed.get(0);
+            String first = toFindFirstWord.get(0);
 
             if (first.equals("bye")) {
                 System.out.println("  Bye. Hope to see you soon again!");
                 break;
             }
             else if (first.equals("mark")) {
-                Task completedTask = getTaskForMarking(parsed);
+                Task completedTask = getTaskForMarking(toFindFirstWord);
                 completedTask.changeCompletion();
 
                 System.out.println("  You are done with: ");
                 System.out.println("    " + completedTask.toString());
             }
             else if (first.equals("unmark")) {
-                Task completedTask = getTaskForMarking(parsed);
+                Task completedTask = getTaskForMarking(toFindFirstWord);
                 completedTask.changeCompletion();
 
                 System.out.println("  OK, continue working on: ");
