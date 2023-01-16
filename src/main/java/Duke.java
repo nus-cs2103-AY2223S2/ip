91bb51cbd1737989b99f2b2eae3228a9b07214fc
FredericChow00 <ruihui00.chow@gmail.com>
2023-01-17 00:31:58 +0800

    Level-1 Completed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,20 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        Scanner sc = new Scanner(System.in);
+        String command = "";
+        while (!command.equals("bye")) {
+            command = sc.nextLine();
+            if (command.equals("list")) {
+                System.out.println(command);
+            } else if (command.equals("blah")) {
+                System.out.println(command);
+            } else if (command.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+            }
+        }
     }
 }
