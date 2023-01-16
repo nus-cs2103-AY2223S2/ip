f0ca844cc45adf12664da0dd9932b73ae90471a0
Dangabit <ericgohky@gmail.com>
2023-01-16 12:51:07 +0800

    Use Enums

--- src/main/java/KiraBot.java
+++ src/main/java/KiraBot.java
@@ -1,5 +1,9 @@
 import java.util.Scanner;
 
+enum Command {
+    BYE, LIST, MARK, UNMARK, TODO, DEADLINE, EVENT, DELETE
+}
+
 public class KiraBot {
 
     private static void printFormatString(String raw) {
@@ -25,52 +29,54 @@ public class KiraBot {
         while (isActive) {
             try {
                 String[] input = sc.nextLine().split(" ", 2);
-                String command = input[0];
-                switch (command) {
-                case "bye":
-                    isActive = false;
-                    break;
-                case "list":
-                    String dataList = database.list();
-                    printFormatString(dataList);
-                    break;
-                case "mark":
-                    int index = Integer.valueOf(input[1]);
-                    String output = database.mark(index);
-                    printFormatString(output);
-                    break;
-                case "unmark":
-                    index = Integer.valueOf(input[1]);
-                    output = database.unmark(index);
-                    printFormatString(output);
-                    break;
-                case "todo":
-                    ToDo todo = new ToDo(input[1]);
-                    database.store(todo);
-                    printTaskFormatString(todo, database);
-                    break;
-                case "deadline":
-                    String[] format = input[1].split(" /by ", 2);
-                    Deadline deadline = new Deadline(format[0], format[1]);
-                    database.store(deadline);
-                    printTaskFormatString(deadline, database);
-                    break;
-                case "event":
-                    format = input[1].split(" /");
-                    Event event = new Event(format[0],
-                                        format[1].substring(5), 
-                                        format[2].substring(3));
-                    database.store(event);
-                    printTaskFormatString(event, database);
-                    break;
-                case "delete":
-                    index = Integer.valueOf(input[1]);
-                    output = database.delete(index);
-                    printFormatString(output);
-                    break;
-                default:
-                    printFormatString("Sorry, I don't know this command :C\n");
-                    break;
+                Command command;
+                try {
+                    command = Command.valueOf(input[0].toUpperCase());
+                    switch (command) {
+                    case BYE:
+                        isActive = false;
+                        break;
+                    case LIST:
+                        String dataList = database.list();
+                        printFormatString(dataList);
+                        break;
+                    case MARK:
+                        int index = Integer.valueOf(input[1]);
+                        String output = database.mark(index);
+                        printFormatString(output);
+                        break;
+                    case UNMARK:
+                        index = Integer.valueOf(input[1]);
+                        output = database.unmark(index);
+                        printFormatString(output);
+                        break;
+                    case TODO:
+                        ToDo todo = new ToDo(input[1]);
+                        database.store(todo);
+                        printTaskFormatString(todo, database);
+                        break;
+                    case DEADLINE:
+                        String[] format = input[1].split(" /by ", 2);
+                        Deadline deadline = new Deadline(format[0], format[1]);
+                        database.store(deadline);
+                        printTaskFormatString(deadline, database);
+                        break;
+                    case EVENT:
+                        format = input[1].split(" /");
+                        Event event = new Event(format[0],
+                                            format[1].substring(5), 
+                                            format[2].substring(3));
+                        database.store(event);
+                        printTaskFormatString(event, database);
+                        break;
+                    case DELETE:
+                        index = Integer.valueOf(input[1]);
+                        output = database.delete(index);
+                        printFormatString(output);
+                        break;
+                    }
+                } catch (IllegalArgumentException e) {
+                    throw new KiraException("Sorry, I don't know this command :C\n");
                 }
             } catch (KiraException e) {
                 printFormatString("There seems to be an error...\n" + e.getMessage());
