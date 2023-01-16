7096223932080d01b0b030abf1bc6d7d7b799502
Lim Diat Bbin <l_db_@hotmail.com>
2023-01-16 23:39:31 +0800

    Completed Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,18 +2,26 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        String[] inputs = new String[100];
+        int curr = 0;
         System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
+        System.out.println("What can I do for you?\n");
 
         Scanner scanner = new Scanner(System.in);
-        String s = scanner.nextLine();
+        String str = scanner.nextLine();
 
-        while(!s.equals("bye")) {
-            System.out.println(s);
-            s = scanner.nextLine();
+        while(!str.equals("bye")) {
+            if(str.equals("list")) {
+                for(int i = 0; i < curr; i++) {
+                    System.out.println((i + 1) + ". " + inputs[i]);
+                }
+            } else {
+                System.out.println("added: " + str + "\n");
+                inputs[curr++] = str;
+            }
+            str = scanner.nextLine();
         }
 
         System.out.println("Bye. Hope to see you again soon!");
-
     }
 }
