2084f34b596676c4ef244bdf5ccca5066b354e73
swxk19 <s.wxk19@gmail.com>
2023-01-16 19:04:09 +0800

    Greet user, echo user commands & exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,26 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+                
+        Scanner sc = new Scanner(System.in);
+        String line = "init";
+        // System.out.println("Hello from\n" + logo);
+        System.out.println("Hello I'm Duke! \n What can I do for you?");
+        while (!line.equals("bye")) {
+            if (!line.equals("init")) {
+                displayMsg(line);
+            }
+            line = sc.nextLine();
+        }
+        displayMsg("Bye. Hope to see you again soon!");
+    }
+
+    public static void displayMsg(String msg) {
+        System.out.println(wrapMessageBorder(msg));
+    }
+
+    public static String wrapMessageBorder(String msg) {
+        String border = "____________________________________________________________";
+        return border + "\n" + msg + "\n" + border;
     }
 }
