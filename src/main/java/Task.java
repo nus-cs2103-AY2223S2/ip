9c9321203c01465df8c5bd27a65208da092cf898
Tempura-Person <92144641+Tempura-Person@users.noreply.github.com>
2023-01-16 14:42:00 +0800

    Added Task class.

---
+++ src/main/java/Task.java
@@ -0,0 +1,11 @@
+public class Task {
+    String name;
+
+    public Task(String TaskName) {
+        name = TaskName;
+    }
+    // gets name of task
+    public String getName() {
+        return this.name;
+    }
+}
