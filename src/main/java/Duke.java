965f8cdd002a75cbef3de814feb12fae1589c225
Lim Diat Bbin <l_db_@hotmail.com>
2023-01-16 22:29:27 +0800

    Completed Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+
+        while(!s.equals("bye")) {
+            System.out.println(s);
+            s = scanner.nextLine();
+        }
+
+        System.out.println("Bye. Hope to see you again soon!");
+
     }
 }
