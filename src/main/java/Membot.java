aa73559bd85d317aa73242c372c5a19e2d299eea
jiawei <b33b33o11@gmail.com>
2023-01-16 17:22:23 +0800

    feat: added done/undone status for tasks

--- src/main/java/Membot.java
+++ src/main/java/Membot.java
@@ -1,4 +1,5 @@
 import model.Task;
+import utils.InputValidator;
 import utils.Printer;
 
 import java.util.Scanner;
@@ -12,6 +13,8 @@ public class Membot {
             + "|_| |_| |_| \\___||_| |_| |_||_.__/  \\___/  \\__|\n";
     private static final String TERMINATE_KEY = "bye";
     private static final String LIST_KEY = "list";
+    private static final String CHECK_KEY = "done";
+    private static final String UNCHECK_KEY = "undone";
 
     public static void main(String[] args) {
         Printer.println("Welcome to\n" + LOGO);
@@ -20,7 +23,7 @@ public class Membot {
 
         loop: while (scanner.hasNext()) {
             String input = scanner.nextLine();
-            switch (input) {
+            switch (input.trim()) {
                 case TERMINATE_KEY:
                     Printer.printlnIndent("Have a good day! Good bye!");
                     break loop;
@@ -28,9 +31,40 @@ public class Membot {
                     Printer.printIndent("Here are all your tasks:");
                     Printer.listPrint(Task.listAll());
                     break;
+                case CHECK_KEY:
+                    Printer.printIndent("Which task would you like to mark as completed?");
+                    Printer.listPrint(Task.listAll());
+                    break;
                 default:
-                    Task task = Task.create(input);
-                    Printer.printlnAdded(task.toString());
+                    if (input.startsWith(CHECK_KEY) && InputValidator.isCheckInputValid(input)) {
+                        int taskId = Integer.parseInt(input.split(" ")[1]);
+                        try {
+                            Task.setStatusCompleted(taskId);
+                            Printer.printIndent("Well done! The task is marked as completed:");
+                            Printer.printIndent(Task.listOne(taskId));
+                        } catch (IndexOutOfBoundsException e) {
+                            Printer.printlnIndent("Invalid Task ID!");
+                            break;
+                        }
+                    } else if (input.startsWith(UNCHECK_KEY)
+                            && InputValidator.isCheckInputValid(input)) {
+                        int taskId = Integer.parseInt(input.split(" ")[1]);
+                        try {
+                            Task.setStatusNew(taskId);
+                            Printer.printIndent("The task is now marked as not done yet:");
+                            Printer.printIndent(Task.listOne(taskId));
+                        } catch (IndexOutOfBoundsException e) {
+                            Printer.printlnIndent("Invalid Task ID!");
+                            break;
+                        }
+                    } else {
+                        Task task = Task.create(input);
+                        Printer.printIndent("[new] " + task);
+                    }
+
+                    Printer.printIndent("");
+                    Printer.printIndent("Here are your updated tasks:");
+                    Printer.listPrint(Task.listAll());
                     break;
             }
         }
--- src/main/java/model/Task.java
+++ src/main/java/model/Task.java
@@ -7,6 +7,7 @@ public class Task {
     private String title;
     private static final Task[] tasks = new Task[100];
     private static int count = 0;
+    private TaskStatus status = TaskStatus.NEW;
 
     private Task(String title) {
         this.title = title;
@@ -19,16 +20,64 @@ public class Task {
         return task;
     }
 
+    public static boolean isIdValid(int id) {
+        return id >= 1 && id <= Task.count;
+    }
+
+    public static void setStatusCompleted(int id) throws IndexOutOfBoundsException {
+        if (!Task.isIdValid(id)) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        Task.tasks[id - 1].setStatus(TaskStatus.COMPLETED);
+    }
+
+    public static void setStatusNew(int id) throws IndexOutOfBoundsException {
+        if (!Task.isIdValid(id)) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        Task.tasks[id - 1].setStatus(TaskStatus.NEW);
+    }
+
+    private void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+
     public static String[] listAll() {
         ArrayList<String> tasks = new ArrayList<>();
         for (int i = 0; i < Task.count; ++i) {
-            tasks.add(Task.tasks[i].toString());
+            tasks.add(String.format("%s", Task.tasks[i].toString()));
         }
         return tasks.toArray(new String[Task.count]);
     }
 
+    public static String listOne(int id) throws IndexOutOfBoundsException {
+        if (!Task.isIdValid(id)) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        return Task.tasks[id - 1].toString();
+    }
+
+    private String printStatus() {
+        switch (this.status) {
+            case NEW:
+                return " ";
+            case COMPLETED:
+                return "X";
+            default:
+                return "?";
+        }
+    }
+
     @Override
     public String toString() {
-        return String.format("%s", this.title);
+        return String.format("[%s] %s", this.printStatus(), this.title);
     }
+}
+
+ enum TaskStatus {
+    NEW,
+    COMPLETED
 }
