122e0c7359114ad66c1080ea5b80c34e173855d2
cjyothika <jyothikasivasai@gmail.com>
2023-01-17 00:10:50 +0800

    Duke.java: Added functions to handle checked exceptions Expected.txt: updated intended test results of new tests input.txt: added new tests for exception handling DukeException.java: added new Exception class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -20,27 +20,26 @@ public class Duke {
             String[] parsedInput = newInput.split(" ");
             String command = parsedInput[0];
 
-            if(command.equalsIgnoreCase("bye")){
-                break;
-            } else if(command.equalsIgnoreCase("list")){
-                showList();
-            } else if(command.equalsIgnoreCase("mark")){
-                markTask(Integer.parseInt(parsedInput[1]) - 1);
-            } else if(command.equalsIgnoreCase("unmark")){
-                unmarkTask(Integer.parseInt(parsedInput[1]) - 1);
-            } else if(command.equalsIgnoreCase("todo")){
-                addTodo(newInput.split("todo", 2)[1]);
-            } else if(command.equalsIgnoreCase("deadline")){
-                String raw = newInput.split("deadline", 2)[1];
-                String[] parsed = raw.split("/by", 2);
-                addDeadline(parsed[0], parsed[1]);
-            } else if(command.equalsIgnoreCase("event")){
-                String raw = newInput.split("event", 2)[1];
-                String[] parsed1 = raw.split("/from", 2);
-                String[] parsed2 = parsed1[1].split("/to", 2);
-                addEvent(parsed1[0], parsed2[0], parsed2[1]);
-            } else {
-                addTask(newInput);
+            try {
+                if (command.equalsIgnoreCase("bye")) {
+                    break;
+                } else if (command.equalsIgnoreCase("list")) {
+                    showList();
+                } else if (command.equalsIgnoreCase("mark")) {
+                    markTask(Integer.parseInt(parsedInput[1]) - 1);
+                } else if (command.equalsIgnoreCase("unmark")) {
+                    unmarkTask(Integer.parseInt(parsedInput[1]) - 1);
+                } else if (command.equalsIgnoreCase("todo")) {
+                    addTodo(newInput.split("todo", 2)[1]);
+                } else if (command.equalsIgnoreCase("deadline")) {
+                    processDeadline(newInput);
+                } else if (command.equalsIgnoreCase("event")) {
+                    processEvent(newInput);
+                } else {
+                    throw new DukeException("I'm sorry, but I don't know what that means :-(");
+                }
+            } catch (DukeException d) {
+                System.out.println(makeOutput("☹ OOPS!!! " +d.getMessage()));
             }
         }
 
@@ -52,19 +51,49 @@ public class Duke {
         return "____________________________________________________________\n" + in +"\n" + "____________________________________________________________";
     }
 
-    public static void addTodo(String description){
+    public static void addTodo(String description) throws DukeException{
+        if(description.equals("")){
+            throw new DukeException("The description of a todo cannot be empty.");
+        }
         tasks[numOfTasks] = new Todo(description);
         System.out.println(makeOutput(String.format("Got it. I've added this task:\n %s\n Now you have %d tasks in the list."
                 ,tasks[numOfTasks], ++numOfTasks)));
     }
+    public static void processDeadline(String input) throws DukeException{
+        String raw = input.split("deadline", 2)[1];
+        if(raw.equals("")){
+            throw new DukeException("The description of a deadline cannot be empty.");
+        }
+        String[] parsed = raw.split("/by", 2);
+        if(parsed.length < 2){
+            throw new DukeException("When the deadline should be completed by should be specified using /by.");
+        }
+        addDeadline(parsed[0], parsed[1]);
+    }
 
-    public static void addDeadline(String description, String by) {
+    public static void processEvent(String input) throws DukeException{
+        String raw = input.split("event", 2)[1];
+        if(raw.equals("")){
+            throw new DukeException("The description of a event cannot be empty.");
+        }
+        String[] parsed1 = raw.split("/from", 2);
+        if(parsed1.length < 2){
+            throw new DukeException("The event's timeline should be specified using /from and /to.");
+        }
+        String[] parsed2 = parsed1[1].split("/to", 2);
+        if(parsed2.length < 2){
+            throw new DukeException("The event's timeline should be specified using /from and /to.");
+        }
+        addEvent(parsed1[0], parsed2[0], parsed2[1]);
+    }
+
+    public static void addDeadline(String description, String by) throws DukeException{
         tasks[numOfTasks] = new Deadline(description, by);
         System.out.println(makeOutput(String.format("Got it. I've added this task:\n %s\n Now you have %d tasks in the list."
                 ,tasks[numOfTasks], ++numOfTasks)));
     }
 
-    public static void addEvent(String description, String from, String to) {
+    public static void addEvent(String description, String from, String to) throws DukeException{
         tasks[numOfTasks] = new Event(description, from , to);
         System.out.println(makeOutput(String.format("Got it. I've added this task:\n %s\n Now you have %d tasks in the list."
                 ,tasks[numOfTasks], ++numOfTasks)));
