303bf8624b56c08e45814088b02c743965cf86b4
Tempura-Person <92144641+Tempura-Person@users.noreply.github.com>
2023-01-16 15:04:49 +0800

    Updated Task class to show 'done' status.

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,11 +1,21 @@
 public class Task {
-    String name;
+    protected String name;
+    protected boolean isDone;
 
     public Task(String TaskName) {
-        name = TaskName;
+        this.name = TaskName;
+        this.isDone = false;
     }
     // gets name of task
     public String getName() {
         return this.name;
+    }
+    // gets status of task, mark done with 'X'
+    public String getStatus() {
+        return (isDone ? "X" : "");
+    }
+    // marks task as done
+    public void MarkDone()  {
+        this.isDone = true;
     }
 }
