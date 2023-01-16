9fe8e719d6c1e29da00d286ba6492df96470371d
linustws <linustws00@gmail.com>
2023-01-16 19:38:24 +0800

    Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = "  /\\_/\\\n"
+                + " /◞   ◟\\ \n"
+                + "( ◕   ◕ )\n"
+                + " \\     /\n"
+                + "  \\   /\n"
+                + "   \\ /\n"
+                + "    ●\n";
+        System.out.println(logo + "BorzAI\n");
+        System.out.println("When all I do is for you, Kermie ♥\n"
+                + "What can I do for you?\n");
+        Scanner scan = new Scanner(System.in);
+        while (true) {
+            String input = scan.nextLine();
+            if (input.equals("bye")) {
+                break;
+            }
+            System.out.println(input + "\n");
+        }
+        System.out.println("Woof (╯ᆺ╰๑)");
     }
 }
