2160e3f1b4f13f9c0fede5ffdd79aeb93c362614
engenhui1999 <79076045+engenhui1999@users.noreply.github.com>
2023-01-16 23:21:16 +0800

    Added Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,18 +2,15 @@ import java.io.*;
 import java.util.*;
 
 public class Duke {
-    private static String outlines = "____________________________________________________________";
-    private static String introduction = "Hello! I'm Duke\nWhat can I do for you?";
-    private static String farewell = "Bye. Hope to see you again soon!";
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    private static ArrayList<String> db = new ArrayList<String>(100);
+    private static ArrayList<Task> db = new ArrayList<Task>(100);
+    
+    protected static String logo = " ____        _        \n" + "|  _ \\ _   _| | _____ \n" + "| | | | | | | |/ / _ \\\n" + "| |_| | |_| |   <  __/\n" + "|____/ \\__,_|_|\\_\\___|\n";
+    protected static String outlines = "____________________________________________________________";
+    protected static String introduction = "Hello! I'm Duke\nWhat can I do for you?";
+    protected static String farewell = "Bye. Hope to see you again soon!";    
 
     public static void main(String[] args) throws IOException {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println(outlines + "\n" + introduction + "\n" + outlines);
 
         String input = br.readLine();
@@ -34,25 +31,31 @@ public class Duke {
     }
 
     private static void handleMessage(String message) {
-        switch (message) {
-            case "list":
-                read();
-                break;
-            default:
-                update(message);
-
-        }
+        if (message.equals("list")) { read(); }
+        else if (message.startsWith("mark")) { markTask(message); }
+        else if (message.startsWith("unmark", 0)) { unmarkTask(message); }
+        else { update(message); }
     }
     private static void read() {
         System.out.println(outlines);
         for (int i = 1; i <= db.size(); i++) {
-            System.out.println(i + ". " + db.get(i-1));
+            System.out.println(i + "." + db.get(i-1));
         }
         System.out.println(outlines);
     }
 
     private static void update(String message) {
-        db.add(message);
+        db.add(new Task(message));
         System.out.println(outlines + "\n" + "added: "+ message + "\n" + outlines);
+    }
+
+    private static void markTask(String message) {
+        int ind = Integer.parseInt(message.split(" ")[1]) - 1;
+        db.get(ind).setDone();
+    }
+
+    private static void unmarkTask(String message) {
+        int ind = Integer.parseInt(message.split(" ")[1]) - 1;
+        db.get(ind).setUndone();
     }
 }
