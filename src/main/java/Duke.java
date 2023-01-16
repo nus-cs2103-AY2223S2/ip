47378d2858ca49f0869c0eda8ba9549060a38f41
daitenshionyan <snivy.oshowott@gmail.com>
2023-01-16 20:41:27 +0800

    Create level 1 skeleton

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,81 @@
+import java.util.Scanner;
+
+import commands.Command;
+import commands.CommandInput;
+
 public class Duke {
+    private static String SEPARATOR = "____________________________________________________________";
+
+    private boolean isExit = false;
+
+
     public static void main(String[] args) {
+        new Duke().start();
+    }
+
+
+    private void start() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        System.out.println(formatMessage(Command.GREET.execute(CommandInput.parse(""))));
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (!isExit) {
+                System.out.println(formatMessage(execute(scanner.nextLine())));
+            }
+        }
+    }
+
+
+    private String execute(String rawInput) {
+        String msg = rawInput;
+        try (Scanner scanner = new Scanner(rawInput)) {
+            if (scanner.hasNext()) {
+                Command command;
+                try {
+                    command = Command.valueOf(scanner.next().toUpperCase());
+                } catch (IllegalArgumentException illArgEx) {
+                    return msg;
+                }
+
+                if (command.equals(Command.BYE)) {
+                    isExit = true;
+                }
+
+                CommandInput input = CommandInput.parse("");
+                if (scanner.hasNext()) {
+                    try {
+                        input = CommandInput.parse(msg);
+                    } catch (IllegalArgumentException illArgEx) {
+                        return msg;
+                    }
+                }
+
+                try {
+                    msg = command.execute(input);
+                } catch (IllegalArgumentException illArgEx) {
+                    return msg;
+                }
+            }
+        }
+        return msg;
+    }
+
+
+    private String formatMessage(String msg) {
+        StringBuilder builder = new StringBuilder();
+        try (Scanner scanner = new Scanner(msg)) {
+            while (scanner.hasNextLine()) {
+                builder.append("\t" + scanner.nextLine());
+            }
+        }
+        return String.format("\t%s\n%s\n\t%s\n",
+            SEPARATOR, builder.toString(), SEPARATOR
+        );
     }
 }
