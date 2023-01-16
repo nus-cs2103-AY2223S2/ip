2ef1cbc7217e57e88c27683452c16d8fe7ae93b4
JrmCkh <Hhelloogkart@gmail.com>
2023-01-16 21:11:08 +0800

    Added echoing of commands, and exits when user types bye

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+import java.lang.String;
+
 /**
  * The Duke class implements a personal assistant chatbot
  * that helps a person keep track of various things.
@@ -7,23 +10,45 @@
 
 public class Duke {
 
+    private static final String INDENT = "    ";
+    private static final String HORIZONTAL_LINE = "______________________________\n";
     private static final String LOGO
-            = " ____        _        \n"
-            + "|  _ \\ _   _| | _____ \n"
-            + "| | | | | | | |/ / _ \\\n"
-            + "| |_| | |_| |   <  __/\n"
-            + "|____/ \\__,_|_|\\_\\___|\n";
+            = INDENT + " ____        _        \n"
+            + INDENT + "|  _ \\ _   _| | _____ \n"
+            + INDENT + "| | | | | | | |/ / _ \\\n"
+            + INDENT + "| |_| | |_| |   <  __/\n"
+            + INDENT + "|____/ \\__,_|_|\\_\\___|\n";
 
-    private static void greet() {
-        System.out.println("Hello I'm\n" + LOGO + "What can I do for you?\n");
+    private void greet() {
+        System.out.println(INDENT + HORIZONTAL_LINE
+                + INDENT + "Hello I'm\n"
+                + LOGO
+                + INDENT + "What can I do for you?\n"
+                + INDENT + HORIZONTAL_LINE);
     }
 
-    private static void exit() {
-        System.out.println("Bye. Hope to see you again soon!");
+    private void exit() {
+        System.out.println(INDENT + HORIZONTAL_LINE
+                + INDENT + "Bye. Hope to see you again soon!\n"
+                + INDENT + HORIZONTAL_LINE);
+    }
+
+    private void echo(String cmd) {
+        System.out.println(INDENT + HORIZONTAL_LINE
+                + INDENT + cmd + "\n"
+                + INDENT + HORIZONTAL_LINE);
     }
 
     public static void main(String[] args) {
-        greet();
-        exit();
+        Duke duke = new Duke();
+        duke.greet();
+        Scanner input = new Scanner(System.in);
+        String cmd = input.nextLine();
+        while (!cmd.equals("bye")) {
+            duke.echo(cmd);
+            cmd = input.nextLine();
+        }
+        duke.exit();
+        input.close();
     }
 }
