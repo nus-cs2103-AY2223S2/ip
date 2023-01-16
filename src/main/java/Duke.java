a54d2e23c8551ec4c2646905284af66585839748
gitsac <isaacchai99@gmail.com>
2023-01-16 17:03:51 +0800

    Add level-3 Increment

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,7 +5,7 @@ import java.util.*;
 
 public class Duke {
 
-    static List<String> storedText = new ArrayList<String>();
+    static List<Task> storedText = new ArrayList<Task>();
     public static void main(String[] args) throws IOException{
         // String logo = " ____        _        \n"
         //         + "|  _ \\ _   _| | _____ \n"
@@ -25,25 +25,70 @@ public class Duke {
 
     public static void processUserInput(BufferedReader brToUse) throws IOException{
         String userInput = brToUse.readLine();
+
         while (!userInput.equals("bye")) {
-            // System.out.println(userInput);
             if (userInput.equals("list")) {
+                System.out.println("Here are the tasks in your list:\n");
                 for (int i = 0; i < storedText.size(); i++) {
                     Integer currIndex = i + 1;
-                    String toUse = currIndex.toString() + ". " + storedText.get(i);
+                    Task currTask = storedText.get(i);
+                    String toUse = "";
+                    if (currTask.done) {
+                        toUse = currIndex.toString() + ".[X] " + currTask.name;
+                    } else {
+                        toUse = currIndex.toString() + ".[ ] " + currTask.name;                        
+                    }
                     System.out.println(toUse);
                 }
                 userInput = brToUse.readLine();
                 continue;
             }
-            storedText.add(userInput);
-            String toOutput = "added: " + userInput;
+
+            if (userInput.startsWith("mark")) {
+                int indexToUse = Integer.parseInt(userInput.split(" ")[1]) - 1;
+                Task currTask = storedText.get(indexToUse);
+                currTask.setDone();
+                String toOutput = "Nice! I've marked this task as done:\n" + "[X] " + currTask.name;
+                System.out.println(toOutput);
+                userInput = brToUse.readLine();
+                continue;
+            }
+
+            if (userInput.startsWith("unmark")) {
+                int indexToUse = Integer.parseInt(userInput.split(" ")[1]) - 1;
+                Task currTask = storedText.get(indexToUse);
+                currTask.setUndone();
+                String toOutput = "Ok, I've marked this task as not done yet:\n" + "[ ] " + currTask.name;
+                System.out.println(toOutput);
+                userInput = brToUse.readLine();
+                continue;
+            }
+
+            Task toAdd = new Task(userInput);
+            storedText.add(toAdd);
+            String toOutput = "added: " + toAdd.name;
             System.out.println(toOutput);
             userInput = brToUse.readLine();
         }
     }
 
+}
 
+class Task {
+    String name;
+    boolean done;
 
+    Task(String taskName) {
+        this.name = taskName;
+        done = false;
+    }
+
+    public void setDone() {
+        done = true;
+    }
+
+    public void setUndone() {
+        done = false;
+    }
 
 }
