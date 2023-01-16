04ce77aa6aa7a3380aff593b10eb3d9fc34403da
WillCWX <xuanwilliam@yahoo.com>
2023-01-16 19:59:22 +0800

    Closes #3, refactored interpret command

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,9 +5,56 @@ public class Duke {
     private static int end = 0;
 
     public static void main(String[] args) {
-        greet();
         dukeLoop();
     }
+
+    /**
+     * Runs the core, repeating functions of Duke
+     */
+    private static void dukeLoop() {
+        greet();
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String input = sc.nextLine();
+            String[] command = input.split(" ", 8);
+            switch (command[0]) {
+                case "list":
+                    printDukeList();
+                    break;
+                case "echo":
+                    echo(input.substring(5));
+                    break;
+                case "mark":
+                    markTask(input.substring(5));
+                    break;
+                case "unmark":
+                    unmarkTask(input.substring(7));
+                    break;
+                case "bye":
+                    sc.close();
+                    exit();
+                    return;
+                default:
+                    addToList(input);
+                    break;
+            }
+        }
+    }
+
+    /**
+     * Prints the partitions, ----,
+     * then prints the string in-between with a tab spacing
+     * 
+     * @param w - The string in between the ---- partitions
+     */
+    private static void printWithPartition(String w) {
+        System.out.println("---------------------");
+        System.out.print(w);
+        System.out.println("---------------------");
+    }
+
+    // region DUKE FUNCTIONS
+    // ---------------------------------------------------------------------------------------
 
     /**
      * Prints Duke's greetings
@@ -24,61 +71,48 @@ public class Duke {
     }
 
     /**
-     * Runs the core, repeating functions of Duke
+     * Prints the Tasks in Duke's list, including their done status
      */
-    private static void dukeLoop() {
-        Scanner sc = new Scanner(System.in);
-        while (true) {
-            String input = sc.nextLine();
-
-            switch (input) {
-                case "list":
-                    printDukeList();
-                    break;
-                case "bye":
-                    sc.close();
-                    exit();
-                    return;
-                default:
-                    interpretCommand(input);
-                    ;
-                    break;
-            }
+    private static void printDukeList() {
+        String ls = "\tHere are the tasks in your list:\n";
+        for (int i = 0; i < end; i++) {
+            ls = ls + "\t" + Integer.toString(i + 1) + "." +
+                    dukeList[i].toString() + "\n";
         }
-    }
-
-    private static void interpretCommand(String w) {
-        if (w.startsWith("echo ", 0)) {
-            echo(w.substring(5));
-        } else if (w.startsWith("mark ")) {
-            int index = Integer.parseInt(w.substring(5)) - 1;
-            dukeList[index].markDone();
-            printWithPartition("\tNice! I've marked this task as done:\n\t  " +
-                    dukeList[index].toString() + "\n");
-        } else if (w.startsWith("unmark ")) {
-            int index = Integer.parseInt(w.substring(7)) - 1;
-            dukeList[index].unmarkDone();
-            printWithPartition("\tOK, I've marked this task as not done yet:" +
-                    "\n\t  " + dukeList[index].toString() + "\n");
-        } else {
-            addToList(w);
-        }
+        printWithPartition(ls);
     }
 
     /**
-     * Prints the partitions, ----,
-     * then prints the string in-between with a tab spacing
+     * Duke echoes the user's input
      * 
-     * @param w - The string in between the ---- partitions
+     * @param w - The string to echo
      */
-    private static void printWithPartition(String w) {
-        System.out.println("---------------------");
-        System.out.print(w);
-        System.out.println("---------------------");
-    }
-
     private static void echo(String w) {
         printWithPartition("\tDuke: " + w + "\n");
+    }
+
+    /**
+     * Duke marks the task as done and notifies the user
+     * 
+     * @param w - The string representation of the index of the task
+     */
+    private static void markTask(String w) {
+        int index = Integer.parseInt(w) - 1;
+        dukeList[index].markDone();
+        printWithPartition("\tNice! I've marked this task as done:\n\t  " +
+                dukeList[index].toString() + "\n");
+    }
+
+    /**
+     * Duke unmarks the task, making it not done and notifies the user
+     * 
+     * @param w - The string representation of the index of the task
+     */
+    private static void unmarkTask(String w) {
+        int index = Integer.parseInt(w) - 1;
+        dukeList[index].unmarkDone();
+        printWithPartition("\tOK, I've marked this task as not done yet:" +
+                "\n\t  " + dukeList[index].toString() + "\n");
     }
 
     /**
@@ -96,17 +130,14 @@ public class Duke {
         }
     }
 
-    private static void printDukeList() {
-        String ls = "\tHere are the tasks in your list:\n";
-        for (int i = 0; i < end; i++) {
-            ls = ls + "\t" + Integer.toString(i + 1) + "." +
-                    dukeList[i].toString() + "\n";
-        }
-        printWithPartition(ls);
-    }
-
+    /**
+     * Duke says goodbye and is shutdowned
+     */
     private static void exit() {
         printWithPartition("\tGoodbye!\n");
     }
+
+    // ---------------------------------------------------------------------------------------
+    // endregion
 
 }
