9bb4eac9f496d40d7ab5ca91a41c8f9bf085afe8
TamKengHong <tamkenghong@gmail.com>
2023-01-16 15:18:37 +0800

    Added seperator(), Task class, taskList and related functions.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,27 +1,43 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-  // static vars not good practice, in future may use sandwich() function.
-  static String seperator = "____________________________________________________________\n";
+  static ArrayList<Task> taskList = new ArrayList<>();
 
-  public static String greeting() {
-    return seperator + "Nyahello! I'm Nyako!\n" + "What can I do for you nya?\n" + seperator;
+  public static String seperator(String out) {
+    String line = "____________________________________________________________\n";
+    return line + " " + out + "\n" + line;
   }
 
-  public static String echo(String command) {
-    return seperator + command + "\n" + seperator;
+  public static String greeting() {
+    return seperator("Nyahello! I'm Nyako!\n What can I do for you nya?");
+  }
+
+  public static void addTask(String description) {
+    taskList.add(new Task(description));
+  }
+
+  public static void listTasks() {
+    String line = "____________________________________________________________\n";
+    System.out.println(line);
+    for (Task t : taskList) {
+      System.out.println(t);
+    }
+    System.out.println(line);
   }
 
   public static String bye() {
-    return seperator + "Bye bye nya!\n" + seperator;
+    return seperator("Bye bye nya!");
   }
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     System.out.println(greeting());
-    String command = sc.next();
+    String command = sc.nextLine();
     while (!command.equals("bye")) {
-      System.out.println(echo(command));
+      if (command.equals("add")) {
+        addTask(description);
+      }
       command = sc.next();
     }
     System.out.println(bye());
---
+++ src/main/java/Task.java
@@ -0,0 +1,16 @@
+public class Task {
+  String description;
+  int id;
+  static int runningId = 1;
+
+  public Task(String description) {
+    this.id = runningId;
+    runningId++;
+    this.description = description;
+  }
+
+  @Override
+  public String toString() {
+    return String.format(" %d. %s\n", this.id, this.description);
+  }
+}
