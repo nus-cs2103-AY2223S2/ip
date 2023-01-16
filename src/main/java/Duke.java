4289a56a2da92eab58f2677ed4919c002b87a7b0
swxk19 <s.wxk19@gmail.com>
2023-01-16 23:18:47 +0800

    Add support for 3 types of tasks: ToDo, Event, Deadline

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -32,9 +32,21 @@ public class Duke {
                     }
                     displayMsg(output + "\n" + indentString(targetTask.toString(), 1));
                 } else {
-                    dukeList.add(new Task(line));
-                    displayMsg("added: " + line);
-                }   
+                    Task newTask;
+                    if (line.startsWith("event")) {
+                        newTask = Event.create(line);
+                    } else if (line.startsWith("deadline")) {
+                        newTask = Deadline.create(line);
+                    } else {
+                        newTask = ToDo.create(line);
+                    }
+                    dukeList.add(newTask);
+                    StringBuilder output = new StringBuilder();
+                    output.append("Got it. I've added this task:\n" + indentString(newTask.toString(), 1) + "\n" + "Now you have " + dukeList.size());
+                    output.append(" task" + (dukeList.size() == 1 ? "" : "s") + " in the list.");
+                    displayMsg(output.toString());
+                }
+                System.out.println("");   
             }
             line = sc.nextLine();
         }
@@ -71,12 +83,14 @@ public class Duke {
     }
 }
 
-class Task {
+abstract class Task {
+    String type;
     String taskName;
     boolean completed = false;
 
-    public Task(String taskName) {
+    public Task(String taskName, String type) {
         this.taskName = taskName;
+        this.type = type;
     }
 
     public void markDone() {
@@ -86,8 +100,73 @@ class Task {
         this.completed = false;
     }
 
+    public abstract String stringFields(); 
+
     @Override
     public String toString() {
-        return "[" + (this.completed ? "x" : " ") + "] " + this.taskName;
+        return "[" + this.type + "]" + "[" + (this.completed ? "x" : " ") + "] " + this.taskName + this.stringFields();
+    }
+}
+
+class ToDo extends Task {
+
+    // Factory method
+    public static ToDo create(String command) {
+        return command.indexOf("todo") == -1 ? new ToDo(command) : new ToDo(command.substring(5));
+    }
+    
+    public ToDo(String taskName) {
+        super(taskName, "T");
+    }
+
+    @Override
+    public String stringFields() {
+        return "";
+    }
+}
+
+class Deadline extends Task {
+    String dueDate;
+
+    // Factory method
+    public static Deadline create(String command) {
+        String[] commandObjects = command.substring(9).split(" /by ");
+        return new Deadline(commandObjects[0], commandObjects[1]);
+    }
+    
+    public Deadline(String taskName, String dueDate) {
+        super(taskName, "D");
+        this.dueDate = dueDate;
+    }
+
+    @Override
+    public String stringFields() {
+        return " (by: " + dueDate + ")"; 
+    }
+}
+
+class Event extends Task {
+    String fromDate;
+    String toDate;
+
+    public static Event create(String command) {
+        command = command.substring(6);
+        int indexOfFrom = command.indexOf(" /from ");
+        int indexOfTo = command.indexOf(" /to ");
+        String taskName = command.substring(0, indexOfFrom);
+        String fromDate = command.substring(indexOfFrom + 7, indexOfTo);
+        String toDate = command.substring(indexOfTo + 5, command.length());
+        return new Event(taskName, fromDate, toDate);
+    }
+
+    public Event(String taskName, String fromDate, String toDate) {
+        super(taskName, "E");
+        this.fromDate = fromDate;
+        this.toDate = toDate;
+    }
+
+    @Override
+    public String stringFields() {
+        return " (from: " + this.fromDate + " to: " + toDate + ")";
     }
 }
