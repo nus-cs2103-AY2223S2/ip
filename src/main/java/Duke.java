09e0ed1134ab158e39830d0fa57524a48affd146
EvitanRelta <shauntanzongzhi@gmail.com>
2023-01-16 18:51:38 +0800

    Abstract out printing text to console

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,13 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke.say("Hello from\n" + logo);
+    }
+
+    private static void say(String whatToSay) {
+        String horizontalLine = "_".repeat(60);
+        System.out.println(horizontalLine);
+        System.out.println(whatToSay);
+        System.out.println(horizontalLine + '\n');
     }
 }
