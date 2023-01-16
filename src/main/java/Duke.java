6630dc764099762a3d44322d8353244b2b8f2cc5
Sean Wong <58366602+ExtraShotLatte@users.noreply.github.com>
2023-01-16 09:07:19 +0800

    Add switch + loop to print tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,34 +3,48 @@ import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<String> tasks = new ArrayList<String>();
+        //String[] tasks = new String[100];
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Welcome to: \n" + logo);
-
-        Scanner sc = new Scanner(System.in);
-
         System.out.println("    ____________________________________________________________" +
                 "\n     Hello! I'm Duke" +
                 "\n     What can I do for you?" +
                 "\n    ____________________________________________________________");
 
-        
-
         String input = sc.nextLine();
 
         while (!input.equalsIgnoreCase("bye")) {
-            System.out.println("    ____________________________________________________________");
-            System.out.print("     ");
-            System.out.print(input);
-            System.out.println("\n    ____________________________________________________________");
+            switch (input) {
+                case "list":
+                    System.out.println("    ____________________________________________________________");
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println("     " + i + ". " + tasks.get(i));
+                    }
+                    System.out.println("    ____________________________________________________________");
+                    break;
+                default:
+                    tasks.add(input);
+                    String output = "    ____________________________________________________________"
+                            + "\n      added: "
+                            + input
+                            + "\n    ____________________________________________________________";
+                    System.out.println(output);
+                    break;
+            }
             input = sc.nextLine();
         }
+
         System.out.println("    ____________________________________________________________\n" +
                 "     Bye. Hope to see you again soon!\n" +
                 "    ____________________________________________________________");
+
         sc.close();
 
     }
