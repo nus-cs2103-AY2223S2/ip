dc1b617e1bcbd47f932d7f4e354088701e542e25
chia-yh <0.chiayuhong.0@gmail.com>
2023-01-16 12:02:37 +0800

    Add Task.java

---
+++ src/main/java/Task.java
@@ -0,0 +1,23 @@
+public class Task {
+    private String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void mark() {
+        this.isDone = true;
+    }
+
+    public void unmark() {
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        String done = isDone ? "X" : " ";
+        return "[" + done + "] " + description;
+    }
+}
