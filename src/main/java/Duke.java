18e245a8f36825640d2b13ae5173e32cf993a514
NappySprout <sleepylog20@gmail.com>
2023-01-16 22:06:11 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -8,11 +9,24 @@ public class Duke {
         System.out.println(" (*_*)");        
         System.out.println("|(   )|");        
         System.out.println("  |-|");        
+
         Scanner scan = new Scanner(System.in);
+        ArrayList<String> tasks = new ArrayList<String>();
+
+        
+        System.out.println("_____");        
         String input = scan.nextLine();
         while(true) {
-            if(input.equals("bye")) {break;}
-            System.out.println(input);
+            if (input.equals("bye")) {break;}
+            else if (input.equals("list")) {
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.printf("%d. %s%n",i+1,tasks.get(i));
+                }
+            } else {
+                System.out.printf("added: %s%n", input);
+                tasks.add(input);
+            }
+            System.out.println("_____");        
             input = scan.nextLine();
         }
         System.out.println("Bye! Hope to see you again <3!");        
