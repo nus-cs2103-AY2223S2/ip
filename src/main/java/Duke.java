6bddab79199fc1c7f96e9f393f0ca6b7f347da73
StanleyNeoh <95394480+StanleyNeoh@users.noreply.github.com>
2023-01-16 11:43:16 +0800

    Implemented Level-1: Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,32 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static final String LOGO = "\n ____        _        \n"
+        + "|  _ \\ _   _| | _____ \n"
+        + "| | | | | | | |/ / _ \\\n"
+        + "| |_| | |_| |   <  __/\n"
+        + "|____/ \\__,_|_|\\_\\___|\n\n";
+
+    public static void introduce() {
+        System.out.println("Hello I am" + LOGO + "What Can I do for you?");
+    }
+
+    public static String input(Scanner sc) {
+        System.out.print("> ");
+        return sc.nextLine();
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        introduce();
+        while(true) {
+            String cmd = input(sc);
+            if (cmd.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+            System.out.println(cmd);
+        }
+        sc.close();
     }
 }
