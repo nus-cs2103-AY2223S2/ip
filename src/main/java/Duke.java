84fc4b108b5fe076a5e13deb4853585b8e8b394a
TamKengHong <tamkenghong@gmail.com>
2023-01-16 14:29:13 +0800

    Added Level 1 Feature: Greet, Echo, Exit. Added helper functions for each message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,30 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+  // static vars not good practice, in future may use sandwich() function.
+  static String seperator = "____________________________________________________________\n";
+
+  public static String greeting() {
+    return seperator + "Nyahello! I'm Nyako!\n" + "What can I do for you nya?\n" + seperator;
+  }
+
+  public static String echo(String command) {
+    return seperator + command + "\n" + seperator;
+  }
+
+  public static String bye() {
+    return seperator + "Bye bye nya!\n" + seperator;
+  }
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println(greeting());
+    String command = sc.next();
+    while (!command.equals("bye")) {
+      System.out.println(echo(command));
+      command = sc.next();
     }
+    System.out.println(bye());
+    sc.close();
+  }
 }
