186a3cb941d28ced52c5e8afa9147fa3448c1624
anchengyang <anchengyang72@gmail.com>
2023-01-16 16:40:27 +0800

    Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -9,11 +10,24 @@ public class Duke {
 //                + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("    " + "Hello I'm Monkey D Luffy\n    What can I do for you?");
 
+        ArrayList<Task> Tasks = new ArrayList<Task>();
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
 
-        while (!input.equals("bye")) {
-            System.out.println("    " + input);
+        while (true) {
+            if (input.equals("bye")){
+                break;
+            }
+            if (input.equals("list")){
+                for(int i = 0; i < Tasks.size(); i++) {
+                    int num = i + 1;
+                    System.out.print("    " + num + ". " + Tasks.get(i) + "\n");
+                }
+            } else {
+                Task task = new Task(input);
+                Tasks.add(task);
+                System.out.println("    added: " + task);
+            }
             input = sc.nextLine();
         }
         System.out.println("    " + "Bye. Hope to see you again soon!");
---
+++ src/main/java/Task.java
@@ -0,0 +1,11 @@
+public class Task {
+    private String input;
+
+    public Task(String input) {
+        this.input = input;
+    }
+    @Override
+    public String toString() {
+        return this.input;
+    }
+}
