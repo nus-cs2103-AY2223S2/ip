21715e95d05a457bd825d850d8317964b39761ae
Mr-Teal <ultramanruebe@outlook.com>
2023-01-16 11:02:05 +0800

    Duke.java: Complete Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String line="____________________________________________________________";
+        System.out.println(line);
+        System.out.println("Hello! I'm T-Rex. Roarrrrrrrrrrrrrr!");
+        System.out.println("What do you need from me?");
+        System.out.println(line);
+        System.out.println();
+
+        Scanner sc=new Scanner(System.in);
+        String cmd=sc.nextLine();
+        while(!cmd.equals("bye")){
+            System.out.println(line);
+            System.out.println(cmd);
+            System.out.println(line);
+            System.out.println();
+            cmd=sc.nextLine();
+        }
+
+        System.out.println(line);
+        System.out.println("See you! Roarrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr!");
+        System.out.println(line);
+        System.out.println();
     }
 }
