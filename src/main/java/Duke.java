4d8bd3f870f882c5deff7a4b5332f19eab858072
igezt <igez1537@gmail.com>
2023-01-16 18:35:51 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,30 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+
+        System.out.println("    ____________________________________________________________\n" +
+                "     Hello! I'm Duke\n" +
+                "     What can I do for you?\n" +
+                "    ____________________________________________________________");
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        while(true) {
+            String line = scanner.nextLine();
+            if (Objects.equals(line, "bye")) {
+                System.out.println("    ____________________________________________________________\n" +
+                        "     Bye. Hope to see you again soon!\n" +
+                        "    ____________________________________________________________");
+                break;
+            }
+            System.out.println("    ____________________________________________________________\n" +
+                    "     " + line + "\n" +
+                    "    ____________________________________________________________");
+        }
+
+
+
     }
 }
