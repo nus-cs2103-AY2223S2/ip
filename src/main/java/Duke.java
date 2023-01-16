f1bb78cfa0279024cc0304806aa51c3e371572a8
ajjajjajjajj <angjingjie0512@gmail.com>
2023-01-17 02:06:20 +0800

    added skeletal version

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,43 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String LOGO = "       _             _    _    _                _                \n" +
+                "  ___ | |__    __ _ | |_ | |_ (_) _ __    __ _ | |_  ___   _ __  \n" +
+                " / __|| '_ \\  / _` || __|| __|| || '_ \\  / _` || __|/ _ \\ | '_ \\ \n" +
+                "| (__ | | | || (_| || |_ | |_ | || | | || (_| || |_| (_) || | | |\n" +
+                " \\___||_| |_| \\__,_| \\__| \\__||_||_| |_| \\__, | \\__|\\___/ |_| |_|\n" +
+                "                                         |___/                   ";
+
+        System.out.println("Hello from\n" + LOGO);
+
+        reply("This is Chattington the Chatter, I'll chat with you.\n" +
+                "What can I do for you?\n");
+
+        while(true) {
+            Scanner sc = new Scanner(System.in);
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                reply("Alright, goodbye to you too!");
+                break;
+            } else {
+                reply(input);
+            }
+        }
+    }
+
+    private static void reply(String s) {
+        if (s.charAt(s.length() - 1) != '\n') {
+            s += '\n';
+        }
+        String LINE_SEPARATOR = "----------------------------------------\n";
+        String output = LINE_SEPARATOR + s + LINE_SEPARATOR;
+        for (String line : output.split("\n")) {
+            System.out.println(indent(line));
+        }
+    }
+
+    private static String indent(String s) {
+        return "\t" + s;
     }
 }
