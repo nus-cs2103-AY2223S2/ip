8a9a8d9199995703cfa6d6b334066f4e102e4c75
Liu Muchen <liumuchen1@gmail.com>
2023-01-16 23:40:19 +0800

    (Update): Added error prompts for commands

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Duke {
   public static void main(String[] args) {
@@ -20,88 +21,55 @@ public class Duke {
     ArrayList<Task> tasks = new ArrayList<>();
     boolean cont = true;
 
-    String listOutOfBounds = "You are trying to access"
-        + " a task not in the list.\n"
-        + "Please try again!";
-    String numberFormatException = "You entered an invalid"
-        + " number.\n"
-        + "Please try again!";
-    String inputFormatException = "You entered an invalid"
-        + " command template.\n"
-        + "Please try again!";
-
     while (cont) {
       String input = scanner.nextLine();
       String[] inputSplit = input.split(" ", 2);
-      int index;
-      switch (inputSplit[0]) {
-        case "bye":
-          cont = false;
-          break;
-        case "list":
-          printTasks(tasks);
-          break;
-        case "mark":
-          try {
-            index = Integer.parseInt(inputSplit[1]) - 1;
-            if (index < 0 || index >= tasks.size()) {
-              printMsg(listOutOfBounds);
-            } else {
-              tasks.get(index).markTask();
-              printMarkTask(tasks.get(index));
+      try {
+        switch (inputSplit[0]) {
+          case "bye":
+            cont = false;
+            break;
+          case "list":
+            printTasks(tasks);
+            break;
+          case "mark":
+            if (inputSplit.length < 2) {
+              throw new DukeException("Mark command missing list numbering.");
             }
-          } catch (NumberFormatException e) {
-            printMsg(numberFormatException);
-          }
-          break;
-        case "unmark":
-          try {
-            index = Integer.parseInt(inputSplit[1]) - 1;
-            if (index < 0 || index >= tasks.size()) {
-              printMsg(listOutOfBounds);
-            } else {
-              tasks.get(index).unmarkTask();
-              printUnmarkTask(tasks.get(index));
+            execMarkTask(inputSplit[1], tasks);
+            break;
+          case "unmark":
+            if (inputSplit.length < 2) {
+              throw new DukeException("Unmark command missing list numbering.");
             }
-          } catch (NumberFormatException e) {
-            printMsg(numberFormatException);
-          }
-          break;
-        case "todo":
-          Task todo = new Todo(inputSplit[1]);
-          tasks.add(todo);
-          printTask(todo, tasks.size());
-          break;
-        case "deadline":
-          String[] deadlineData = inputSplit[1].split(" /by ", 2);
-          if (deadlineData.length > 1) {
-            Task deadline = new Deadline(deadlineData[0], deadlineData[1]);
-            tasks.add(deadline);
-            printTask(deadline, tasks.size());
-          } else {
-            printMsg(inputFormatException);
-          }
-          break;
-        case "event":
-          String[] eventData1 = inputSplit[1].split(" /from ", 2);
-          if (eventData1.length > 1) {
-            String[] eventData2 = eventData1[1].split(" /to ", 2);
-            if (eventData2.length > 1) {
-              Task event = new Event(eventData1[0], eventData2[0], eventData2[1]);
-              tasks.add(event);
-              printTask(event, tasks.size());
-            } else {
-              printMsg(inputFormatException);
+            execUnmarkTask(inputSplit[1], tasks);
+            break;
+          case "todo":
+            if (inputSplit.length < 2) {
+              throw new DukeException("Todo command missing description.");
             }
-          } else {
-            printMsg(inputFormatException);
-          }
-          break;
-        default:
-          tasks.add(new Task(input));
-          String msg = String.format("Added new entry: %s", input);
-          printMsg(msg);
-          break;
+            execTodo(inputSplit[1], tasks);
+            break;
+          case "deadline":
+            if (inputSplit.length < 2) {
+              throw new DukeException("Deadline command missing description.");
+            }
+            execDeadline(inputSplit[1], tasks);
+            break;
+          case "event":
+            if (inputSplit.length < 2) {
+              throw new DukeException("Event command missing description.");
+            }
+            execEvent(inputSplit[1], tasks);
+            break;
+          default:
+            throw new DukeException("Sorry but I don't understand what this means.");
+        }
+      } catch (DukeException e) {
+        printError(e);
+      } catch (Exception e) {
+        printMsg("Unknown command/error not caught!\n" +
+            "Please try again!");
       }
     }
 
@@ -111,18 +79,23 @@ public class Duke {
     printMsg(exitMsg);
   }
 
-  public static void printMsg(String msg) {
-    String spacer = "____________________"
-        + "______________________";
+  static String spacer = "____________________"
+      + "______________________";
 
+  public static void printMsg(String msg) {
     System.out.println(spacer);
     System.out.println(msg);
     System.out.println(spacer);
   }
 
+  public static void printError(DukeException e) {
+    System.out.println(spacer);
+    System.out.println(e.getMessage());
+    System.out.println("Please try again!");
+    System.out.println(spacer);
+  }
+
   public static void printTasks(ArrayList<Task> tasks) {
-    String spacer = "____________________"
-        + "______________________";
     String msgHeader = "Current data in the list are:";
 
     System.out.println(spacer);
@@ -134,31 +107,52 @@ public class Duke {
     System.out.println(spacer);
   }
 
-  public static void printMarkTask(Task task) {
-    String spacer = "____________________"
-        + "______________________";
+  public static int isValidIndex(String indexStr, ArrayList<Task> tasks)
+      throws DukeException {
+    Pattern p = Pattern.compile("^[0-9]+$");
+    boolean isNumber = p.matcher(indexStr).matches();
+
+    if (!isNumber) {
+      throw new DukeException("Index not an integer");
+    }
+
+    int index = Integer.parseInt(indexStr) - 1;
+    if (index < 0 || index >= tasks.size()) {
+      throw new DukeException("Index out of bounds of tasks list");
+    }
+
+    return index;
+  }
+
+  public static void execMarkTask(String indexStr, ArrayList<Task> tasks)
+      throws DukeException {
+    int index = isValidIndex(indexStr, tasks);
+
+    tasks.get(index).markTask();
+
     String msgHeader = "I've marked this task as done:";
 
     System.out.println(spacer);
     System.out.println(msgHeader);
-    System.out.println(task);
+    System.out.println(tasks.get(index));
     System.out.println(spacer);
   }
 
-  public static void printUnmarkTask(Task task) {
-    String spacer = "____________________"
-        + "______________________";
+  public static void execUnmarkTask(String indexStr, ArrayList<Task> tasks)
+      throws DukeException {
+    int index = isValidIndex(indexStr, tasks);
+
+    tasks.get(index).unmarkTask();
+
     String msgHeader = "I've unmarked this task as not done:";
 
     System.out.println(spacer);
     System.out.println(msgHeader);
-    System.out.println(task);
+    System.out.println(tasks.get(index));
     System.out.println(spacer);
   }
 
   public static void printTask(Task task, int size) {
-    String spacer = "____________________"
-        + "______________________";
     String msgHeader = "I've added this task into the list:";
     String msgFooter = String.format("Now you have a total of %s tasks in the list", size);
 
@@ -167,5 +161,41 @@ public class Duke {
     System.out.println(task);
     System.out.println(msgFooter);
     System.out.println(spacer);
+  }
+
+  public static void execTodo(String description, ArrayList<Task> tasks)
+      throws DukeException {
+    Task todo = new Todo(description);
+    tasks.add(todo);
+    printTask(todo, tasks.size());
+  }
+
+  public static void execDeadline(String data, ArrayList<Task> tasks)
+      throws DukeException {
+    String[] splitData = data.split(" /by ", 2);
+    if (splitData.length < 2) {
+      throw new DukeException("Deadline command format error. Missing /by");
+    }
+
+    Task deadline = new Deadline(splitData[0], splitData[1]);
+    tasks.add(deadline);
+    printTask(deadline, tasks.size());
+  }
+
+  public static void execEvent(String data, ArrayList<Task> tasks)
+      throws DukeException {
+    String[] splitData1 = data.split(" /from ", 2);
+    if (splitData1.length < 2) {
+      throw new DukeException("Event command format error. Missing /from");
+    }
+
+    String[] splitData2 = splitData1[1].split(" /to ", 2);
+    if (splitData2.length < 2) {
+      throw new DukeException("Event command format error. Missing /to");
+    }
+
+    Task event = new Event(splitData1[0], splitData2[0], splitData2[1]);
+    tasks.add(event);
+    printTask(event, tasks.size());
   }
 }
