f8da0db764f3d1f40bc5ca83682f4462b27ed071
linustws <linustws00@gmail.com>
2023-01-16 20:13:50 +0800

    Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -9,17 +10,31 @@ public class Duke {
                 + "  \\   /\n"
                 + "   \\ /\n"
                 + "    ●\n";
-        System.out.println(logo + "BorzAI\n");
-        System.out.println("When all I do is for you, Kermie ♥\n"
-                + "What can I do for you?\n");
-        Scanner scan = new Scanner(System.in);
+        System.out.println(logo + "BorzAI\n"); // Intro
+        System.out.println("\tWhen all I do is for you, Kermie ♥\n"
+                + "\tWhat can I do for you?\n"); // Greeting
+        Scanner scan = new Scanner(System.in); // Allow user input
+        ArrayList<String> taskList = new ArrayList<String>(); // Create list
         while (true) {
             String input = scan.nextLine();
-            if (input.equals("bye")) {
+            if (input.equals("bye")) { // Exit
                 break;
             }
-            System.out.println(input + "\n");
+            if (input.equals("list")) { // Display list
+                for (int i=0; i<taskList.size(); i++) {
+                    String curr = taskList.get(i);
+                    int index = i+1;
+                    if (i == taskList.size()-1) { // Last item
+                        System.out.println("\t" + index + ". " + curr + "\n");
+                        break;
+                    }
+                    System.out.println("\t" + index + ". " + curr);
+                }
+                continue;
+            }
+            taskList.add(input);
+            System.out.println("\t" + input + "\n"); // Echo
         }
-        System.out.println("Woof (╯ᆺ╰๑)");
+        System.out.println("\tWoof (╯ᆺ╰๑)"); // Outro
     }
 }
