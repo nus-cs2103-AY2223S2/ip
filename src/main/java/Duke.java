c8f368333a9a021bf964b7dfd41377bd1f043fcd
jiawei <b33b33o11@gmail.com>
2023-01-16 10:07:35 +0800

    feat: added greet, echo, exit functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,31 @@
+import utils.Printer;
+
+import java.util.Scanner;
+
 public class Duke {
+    private static final String LOGO =
+              "                             _             _   \n"
+            + " _ __ ___    ___  _ __ ___  | |__    ___  | |_ \n"
+            + "| '_ ` _ \\  / _ \\| '_ ` _ \\ | '_ \\  / _ \\ | __|\n"
+            + "| | | | | ||  __/| | | | | || |_) || (_) || |_ \n"
+            + "|_| |_| |_| \\___||_| |_| |_||_.__/  \\___/  \\__|\n";
+    private static final String TERMINATE_KEY = "bye";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Printer.println("Welcome to\n" + LOGO);
+        Printer.println("How may I assist you today?");
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNext()) {
+            String input = scanner.nextLine();
+            switch (input) {
+                case TERMINATE_KEY:
+                    Printer.printlnIndent("Have a good day! Good bye!");
+                    break;
+                default:
+                    Printer.printlnIndent(input);
+                    break;
+            }
+        }
     }
 }
