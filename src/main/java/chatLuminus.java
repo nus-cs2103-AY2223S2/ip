a6d22e514f19b84cd9eb8d020269cbcd31dcf1e7
Lu Chenyu <adam07018@163.com>
2023-01-16 14:21:39 +0800

    lv2

--- src/main/java/chatLuminus.java
+++ src/main/java/chatLuminus.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 public class chatLuminus {
-
+    private ArrayList<String> list;
     chatLuminus() {
-
+        list = new ArrayList<>(100);
     }
 
     public static void main(String[] args) {
@@ -18,12 +19,20 @@ public class chatLuminus {
     private void echo() {
         Scanner sc = new Scanner(System.in);
         while(true) {
-            String input = sc.next();
-            if(input.equals("bye")) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 return;
             }
-            System.out.println(input);
+            if (input.equals("list")) {
+                for (int i = 1; i <= this.list.size(); i++) {
+                    System.out.println(i + ". " + list.get(i - 1));
+                }
+            }
+            else {
+                list.add(input);
+                System.out.println("added: " + input);
+            }
         }
     }
 }
