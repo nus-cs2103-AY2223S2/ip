f73bef5e4ee2097c6fb09c66d35fe6501b80cfcd
JrmCkh <Hhelloogkart@gmail.com>
2023-01-16 20:38:56 +0800

    Add Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,29 @@
+/**
+ * The Duke class implements a personal assistant chatbot
+ * that helps a person keep track of various things.
+ *
+ * @author Chia Jeremy
+ */
+
 public class Duke {
+
+    private static final String LOGO
+            = " ____        _        \n"
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n";
+
+    private static void greet() {
+        System.out.println("Hello I'm\n" + LOGO + "What can I do for you?\n");
+    }
+
+    private static void exit() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greet();
+        exit();
     }
 }
