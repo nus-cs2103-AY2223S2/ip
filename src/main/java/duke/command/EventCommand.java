e0f5e4102a492919cc19eaeedb86b77826149202
hingen <1627313-hingen@users.noreply.gitlab.com>
2023-01-16 22:03:43 +0800

    EventCommand class: add handling of input errors
    User input for event command might be missing a ' /from ' and/or
    ' /to '.
    
    Let's throw a DukeException with the error described in the exception
    message when such an error occurs.

--- src/main/java/duke/command/EventCommand.java
+++ src/main/java/duke/command/EventCommand.java
@@ -1,20 +1,51 @@
 package duke.command;
 
+import duke.exception.DukeException;
 import duke.task.Event;
 import duke.task.Task;
+
+import java.util.List;
 
 /**
  * Represents an add event task command.
  */
 public class EventCommand extends AddCommand {
+    /**
+     * {@inheritDoc}
+     *
+     * @param input {@inheritDoc}
+     * @param tasks {@inheritDoc}
+     * @return {@inheritDoc}
+     * @throws DukeException Indicates missing ' /from ' and/or ' /to ' in input.
+     */
     @Override
-    protected Task createTask(String input) {
+    public CommandResponse run(String input, List<Task> tasks) throws DukeException {
+        return super.run(input, tasks);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @param input {@inheritDoc}
+     * @return {@inheritDoc}
+     * @throws DukeException Indicates missing ' /from ' and/or ' /to ' in input.
+     */
+    @Override
+    protected Task createTask(String input) throws DukeException {
         input = input.replace("event ", "");
-        String[] args = input.split(" /from ");
+
+        String[] args = input.split(" /from ", 2);
+        if (args.length != 2) {
+            throw new DukeException("The input of an event must include a ' /from '");
+        }
 
         String description = args[0];
 
-        String[] startAndEnd = args[1].split(" /to ");
+        String[] startAndEnd = args[1].split(" /to ", 2);
+        if (startAndEnd.length != 2) {
+            throw new DukeException("The input of an event must include a ' /to '");
+        }
+
         String start = startAndEnd[0];
         String end = startAndEnd[1];
 
