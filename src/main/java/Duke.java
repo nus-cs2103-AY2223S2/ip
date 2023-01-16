19528b4ff746c9c4aa063d49dff2b670f76d59f1
jnjy <jefferyng2000@gmail.com>
2023-01-17 00:07:00 +0800

    Level-01: Added Greet, Echo and Exit method

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,45 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greet();
+        echo();
+        exit();
+    }
+
+    public static void greet() {
+        final String logo = "██████   █████  ██████   █████  \n"
+                    + "██   ██ ██   ██ ██   ██ ██   ██ \n"
+                    + "██   ██ ██   ██ ██████  ███████ \n"
+                    + "██   ██ ██   ██ ██   ██ ██   ██ \n"
+                    + "██████   █████  ██   ██ ██   ██ \n";
+        final String intro = "Hola! Soy \n";
+        final String icebreaker = "What can I do for you? \n";
+        System.out.println(intro + logo);
+
+        System.out.println("____________________________________________________________");
+        System.out.println();
+    }
+
+    public static void echo() {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+
+        while(!input.equalsIgnoreCase("bye")) {
+            System.out.println("================================================================");
+            System.out.println(input);
+            System.out.println("================================================================");
+            System.out.println();
+            input = sc.nextLine();
+        }
+        sc.close();
+    }
+
+    public static void exit() {
+        String outro = "bella ciao";
+
+        System.out.println("================================================================");
+        System.out.println(outro);
+        System.out.println("================================================================");
     }
 }
