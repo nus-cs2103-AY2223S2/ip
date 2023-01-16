a4face2cfcc423f04aaecffba672d3bf08001cae
shazrin <sadmeowkins@outlook.com>
2023-01-16 17:29:16 +0800

    Clean up command handling

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,15 @@
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Optional;
 import java.util.Scanner;
-import java.util.regex.MatchResult;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class Duke {
     private static final int MAX_TASKS_SIZE = 100;
     private static ArrayList<Task> tasks = new ArrayList<>(MAX_TASKS_SIZE);
+    private static Pattern todoCommandPattern = Pattern.compile("^todo (.*)$");
+    private static Pattern deadlineCommandPattern = Pattern.compile("^deadline (.*) \\/by (.*)$");
+    private static Pattern eventCommandPattern = Pattern.compile("^event (.*) \\/from (.*) \\/to (.*)$");
+    private static Pattern markUnmarkCommandPattern = Pattern.compile("^(mark|unmark) ([0-9]+)$");
 
     private static void printText(String text) {
         System.out.printf("     %s\n", text);
@@ -32,16 +31,33 @@ public class Duke {
         printHorizontal();
     }
 
-    private static void commandTodo(String description) {
-        addTask(new ToDo(description));
+    private static void commandTodo(String input) {
+        Matcher todoCommandMatcher = todoCommandPattern.matcher(input);
+        if (todoCommandMatcher.find()) {
+            String description = todoCommandMatcher.group(1);
+            addTask(new ToDo(description));
+        }
     }
 
-    private static void commandDeadline(String description, String deadline) {
-        addTask(new Deadline(description, deadline));
+    private static void commandDeadline(String input) {
+        Matcher deadlineCommandMatcher = deadlineCommandPattern.matcher(input);
+        if (deadlineCommandMatcher.find()) {
+            String description = deadlineCommandMatcher.group(1);
+            String by = deadlineCommandMatcher.group(2);
+
+            addTask(new Deadline(description, by));
+        }
     }
 
-    private static void commandEvent(String description, String from, String to) {
-        addTask(new Event(description, from, to));
+    private static void commandEvent(String input) {
+        Matcher eventCommandMatcher = eventCommandPattern.matcher(input);
+        if (eventCommandMatcher.find()) {
+            String description = eventCommandMatcher.group(1);
+            String from = eventCommandMatcher.group(2);
+            String to = eventCommandMatcher.group(3);
+
+            addTask(new Event(description, from, to));
+        }
     }
 
     private static void addTask(Task task) {
@@ -66,35 +82,45 @@ public class Duke {
         printHorizontal();
     }
 
-    private static void commandMark(int taskNo) {
-        if (taskNo > 0 && taskNo <= tasks.size()) {
-            Task task = tasks.get(taskNo - 1);
-            task.setIsDone(true);
+    private static void commandMark(String input) {
+        Matcher markCommandMatcher = markUnmarkCommandPattern.matcher(input);
+        if (markCommandMatcher.find()) {
+            int taskNo = Integer.parseInt(markCommandMatcher.group(2));
 
-            printHorizontal();
-            printText("Nice! I've marked this task as done:");
-            printText(task.toString());
-            printHorizontal();
-        } else {
-            printHorizontal();
-            printText("Invalid task number!");
-            printHorizontal();
+            if (taskNo > 0 && taskNo <= tasks.size()) {
+                Task task = tasks.get(taskNo - 1);
+                task.setIsDone(true);
+
+                printHorizontal();
+                printText("Nice! I've marked this task as done:");
+                printText(task.toString());
+                printHorizontal();
+            } else {
+                printHorizontal();
+                printText("Invalid task number!");
+                printHorizontal();
+            }
         }
     }
 
-    private static void commandUnmark(int taskNo) {
-        if (taskNo > 0 && taskNo <= tasks.size()) {
-            Task task = tasks.get(taskNo - 1);
-            task.setIsDone(false);
+    private static void commandUnmark(String input) {
+        Matcher markCommandMatcher = markUnmarkCommandPattern.matcher(input);
+        if (markCommandMatcher.find()) {
+            int taskNo = Integer.parseInt(markCommandMatcher.group(2));
 
-            printHorizontal();
-            printText("OK, I've marked this task as not done yet:");
-            printText(task.toString());
-            printHorizontal();
-        } else {
-            printHorizontal();
-            printText("Invalid task number!");
-            printHorizontal();
+            if (taskNo > 0 && taskNo <= tasks.size()) {
+                Task task = tasks.get(taskNo - 1);
+                task.setIsDone(false);
+
+                printHorizontal();
+                printText("OK, I've marked this task as not done yet:");
+                printText(task.toString());
+                printHorizontal();
+            } else {
+                printHorizontal();
+                printText("Invalid task number!");
+                printHorizontal();
+            }
         }
     }
 
@@ -107,63 +133,25 @@ public class Duke {
     public static void main(String[] args) {
         printStartup();
 
-        Pattern commandPattern = Pattern.compile("^[a-zA-Z]+");
-        Pattern todoCommandPattern = Pattern.compile("^todo (.*)$");
-        Pattern deadlineCommandPattern = Pattern.compile("^deadline (.*) \\/by (.*)$");
-        Pattern eventCommandPattern = Pattern.compile("^event (.*) \\/from (.*) \\/to (.*)$");
-        Pattern markUnmarkCommandPattern = Pattern.compile("^(mark|unmark) ([0-9]+)$");
-
         Scanner scanner = new Scanner(System.in);
         boolean isExit = false;
         while (!isExit) {
             String input = scanner.nextLine();
 
-            Matcher commandMatcher = commandPattern.matcher(input);
-            if (!commandMatcher.find()) {
-                continue;
-            }
-            String command = commandMatcher.group(0);
+            String command = input.split(" ")[0];
 
             if (command.equals("todo")) {
-                Matcher todoCommandMatcher = todoCommandPattern.matcher(input);
-                if (todoCommandMatcher.find()) {
-                    String description = todoCommandMatcher.group(1);
-
-                    commandTodo(description);
-                }
+                commandTodo(input);
             } else if (command.equals("deadline")) {
-                Matcher deadlineCommandMatcher = deadlineCommandPattern.matcher(input);
-                if (deadlineCommandMatcher.find()) {
-                    String description = deadlineCommandMatcher.group(1);
-                    String by = deadlineCommandMatcher.group(2);
-
-                    commandDeadline(description, by);
-                }
+                commandDeadline(input);
             } else if (command.equals("event")) {
-                Matcher eventCommandMatcher = eventCommandPattern.matcher(input);
-                if (eventCommandMatcher.find()) {
-                    String description = eventCommandMatcher.group(1);
-                    String from = eventCommandMatcher.group(2);
-                    String to = eventCommandMatcher.group(3);
-
-                    commandEvent(description, from, to);
-                }
+                commandEvent(input);
             } else if (command.equals("list")) {
                 commandListTasks();
             } else if (command.equals("mark")) {
-                Matcher markCommandMatcher = markUnmarkCommandPattern.matcher(input);
-                if (markCommandMatcher.find()) {
-                    int taskNumber = Integer.parseInt(markCommandMatcher.group(2));
-
-                    commandMark(taskNumber);
-                }
+                commandMark(input);
             } else if (command.equals("unmark")) {
-                Matcher unmarkCommandMatcher = markUnmarkCommandPattern.matcher(input);
-                if (unmarkCommandMatcher.find()) {
-                    int taskNumber = Integer.parseInt(unmarkCommandMatcher.group(2));
-
-                    commandUnmark(taskNumber);
-                }
+                commandUnmark(input);
             } else if (command.equals("bye")) {
                 commandExit();
                 isExit = true;
