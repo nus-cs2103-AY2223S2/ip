8a80e6b1399e940a1ac2d4bb462be82fb7d69afd
Lim Diat Bbin <l_db_@hotmail.com>
2023-01-17 12:22:06 +0800

    Completed Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,26 +2,39 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-        String[] inputs = new String[100];
+        Task[] inputs = new Task[100];
         int curr = 0;
+
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?\n");
 
         Scanner scanner = new Scanner(System.in);
-        String str = scanner.nextLine();
+        String currInput = scanner.nextLine();
+        String[] splitStr = currInput.split(" ");
 
-        while(!str.equals("bye")) {
-            if(str.equals("list")) {
+        while(!currInput.equals("bye")) {
+            if(currInput.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for(int i = 0; i < curr; i++) {
                     System.out.println((i + 1) + ". " + inputs[i]);
                 }
+            } else if(splitStr[0].equals("mark")) {
+                Task taskToMark = inputs[Integer.parseInt(splitStr[1]) - 1];
+                taskToMark.markDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(taskToMark + "\n");
+            } else if(splitStr[0].equals("unmark")) {
+                Task taskToUnmark = inputs[Integer.parseInt(splitStr[1]) - 1];
+                taskToUnmark.unmarkDone();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println(taskToUnmark + "\n");
             } else {
-                System.out.println("added: " + str + "\n");
-                inputs[curr++] = str;
+                System.out.println("added: " + currInput + "\n");
+                inputs[curr++] = new Task(currInput);
             }
-            str = scanner.nextLine();
+            currInput = scanner.nextLine();
+            splitStr = currInput.split(" ");
         }
-
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
