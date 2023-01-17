7cc241a5641719c7bd1cd5ae263eb9f3de337197
Kang Yue Ran <kangyueran09@gmail.com>
2023-01-17 17:43:13 +0800

    Added Level 1 features, Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
+import java.util.*;
 public class Duke {
+    public static final String dashes = "--------------------------------------";
+    public String format(String input) {
+        return dashes + "\n" + input + "\n" + dashes + "\n";
+    }
+    public void echo() {
+        System.out.println(this.format("Hello! I'm Gerty\nWhat can I do for you?"));
+        while (true) {
+            Scanner input = new Scanner(System.in);
+            String line = input.nextLine();
+            if ("bye".equalsIgnoreCase(line)) {
+                System.out.println(this.format("Bye. Hope to see you again soon!"));
+                break;
+            }
+            System.out.println(this.format(line));
+        }
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke gerty = new Duke();
+        gerty.echo();
     }
 }
