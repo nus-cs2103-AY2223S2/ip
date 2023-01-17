e75f52cc147d03b24937e862599d6bdb3643d6d5
XuanZheng <lim_xuan_zheng@hotmail.com>
2023-01-17 18:57:41 +0800

    Level-1: Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,28 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = "░██████╗░██╗░██████╗░░█████╗░░█████╗░██╗░░██╗░█████╗░██████╗░\n"
+                +     "██╔════╝░██║██╔════╝░██╔══██╗██╔══██╗██║░░██║██╔══██╗██╔══██╗\n"
+                +     "██║░░██╗░██║██║░░██╗░███████║██║░░╚═╝███████║███████║██║░░██║\n"
+                +     "██║░░╚██╗██║██║░░╚██╗██╔══██║██║░░██╗██╔══██║██╔══██║██║░░██║\n"
+                +     "╚██████╔╝██║╚██████╔╝██║░░██║╚█████╔╝██║░░██║██║░░██║██████╔╝\n"
+                +     "░╚═════╝░╚═╝░╚═════╝░╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░\n";
+
+
+
+        System.out.println("Greetings from\n" + logo);
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        while(!input.equals("bye")) {
+            System.out.println(input);
+            input = scanner.nextLine();
+
+        }
+
+        System.out.println("Goodbye. Hope to see you again soon!");
+
     }
 }
