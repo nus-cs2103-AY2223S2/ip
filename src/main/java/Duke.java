23ca140a920fb36f7b95dcfb10be64f2e1469af6
NoyaReoT <rayonteo@gmail.com>
2023-01-17 12:01:11 +0800

    Implemented Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,28 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        // Init
+        displayMessage("Hello! I'm Duke\nWhat can i do for you?");
+        Scanner sc = new Scanner(System.in);
+
+        // Application main loop
+        while (true) {
+            String input = sc.nextLine();
+
+            if (input.equalsIgnoreCase("bye")) break;
+
+            displayMessage(input);
+        }
+
+        displayMessage("Bye! Hope to see you again soon!");
+    }
+
+
+    public static void displayMessage(String msg) {
+
+        String wrapTop = "__________________________\n";
+        String wrapBottom = "\n__________________________";
+        System.out.println(wrapTop + msg + wrapBottom);
     }
 }
