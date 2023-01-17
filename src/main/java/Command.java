16b8583e007f2c5b2a9d3b9810662c342b6f7c18
EvitanRelta <shauntanzongzhi@gmail.com>
2023-01-17 20:59:13 +0800

    Rename private variables in 'Command' class

--- src/main/java/Command.java
+++ src/main/java/Command.java
@@ -1,12 +1,12 @@
 import java.util.Arrays;
 
 public class Command {
-    private final String commandString;
+    private final String fullCommand;
     public final String baseCommand;
     public final String[] parameters;
 
     public Command(String commandString) {
-        this.commandString = commandString;
+        this.fullCommand = commandString;
         String[] splitCommand = commandString.split(" ");
         this.baseCommand = splitCommand[0];
         this.parameters = Arrays.copyOfRange(splitCommand, 1, splitCommand.length);
@@ -14,6 +14,6 @@ public class Command {
 
     @Override
     public String toString() {
-        return commandString;
+        return fullCommand;
     }
 }
