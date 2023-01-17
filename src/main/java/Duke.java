2a5adb802bdbc58558617762e8909a19f1c969c5
LimJiaYan <jiayan.yanyan@gmail.com>
2023-01-17 18:55:34 +0800

    Lvl1: Implement Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,36 @@
+import com.sun.source.tree.WhileLoopTree;
+
+import java.util.Scanner;
+
 public class Duke {
+    private static void greet() {
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\tHello! I'm Duke\n" + "\tWhat can I do for you?");
+        System.out.println("\t____________________________________________________________");
+    }
+    private static void echo(String s) {
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\t" + s);
+        System.out.println("\t____________________________________________________________");
+    }
+
+    private static void exit() {
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\tBye. Hope to see you again soon!");
+        System.out.println("\t____________________________________________________________");
+
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greet();
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        while (!input.equals("bye")) {
+            echo(input);
+            input = scanner.nextLine();
+        }
+
+        exit();
     }
 }
