d7e5dc079745efe8b6064796980c17876b8bed83
Yu Heng Lee <lee2000yh@gmail.com>
2023-01-17 17:09:33 +0800

    Level-8 done
    Redid parsing of commands to account for dates. Added understanding of dates and times for events and deadlines. As a consequence, users must now input in a certain format.

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,13 +1,23 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Deadline extends Task{
     String deadline;
-    public Deadline(String title, String deadline) {
+    LocalDateTime dateline;
+    public Deadline(String title, String deadline) throws DateTimeParseException {
         super(title);
         this.deadline = deadline;
         this.type = "[D]";
+        try {
+            this.dateline = LocalDateTime.parse(deadline, DateTimeFormatter.ofPattern("dd/MM/yyyy HHmm"));
+        } catch (DateTimeParseException e) {
+            throw e;
+        }
     }
 
     @Override
     public String toString() {
-        return super.toString() + " (by: " + deadline + ")";
+        return super.toString() + " (by: " + this.dateline.format(DateTimeFormatter.ofPattern("HHmm MMM d yyyy")) + ")";
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,14 +1,27 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Event extends Task{
     String from;
     String to;
+    LocalDateTime fromDate;
+    LocalDateTime toDate;
     public Event(String title, String from, String to) {
         super(title);
         this.from = from;
         this.to = to;
         this.type = "[E]";
+        try {
+            this.fromDate = LocalDateTime.parse(from, DateTimeFormatter.ofPattern("dd/MM/yyyy HHmm"));
+            this.toDate = LocalDateTime.parse(to, DateTimeFormatter.ofPattern("dd/MM/yyyy HHmm"));
+        } catch (DateTimeParseException e) {
+            throw e;
+        }
     }
     @Override
     public String toString() {
-        return super.toString() + " (from: " + from + " to: " + to + ")";
+        return super.toString() + " (from: " + this.fromDate.format(DateTimeFormatter.ofPattern("HHmm MMM d yyyy")) +
+                                " to: " + this.toDate.format(DateTimeFormatter.ofPattern("HHmm MMM d yyyy")) + ")";
     }
 }
