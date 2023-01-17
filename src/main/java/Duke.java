a82d6c47fdfff38843b09f425a0385c62fc4313d
cydtseng <tsengchenyu@gmail.com>
2023-01-17 15:24:44 +0800

    implement level 2, add, list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        String[] userInputs = new String[100];
+        int numUserInputs = 0;
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -24,8 +26,19 @@ public class Duke {
                         + "Bye. Hope to see you again soon!\n"
                         + "______________________________________\n";
                 continueRunning = false;
-            } else {
+            } else if(userInput.equals("list")){
+                StringBuilder listOfInputs = new StringBuilder();
+                for(int i = 0; i < numUserInputs; i++){
+                    listOfInputs.append(i + 1).append(". ").append(userInputs[i]).append("\n");
+                }
                 resultString = "______________________________________\n"
+                        + listOfInputs
+                        + "______________________________________\n";
+            } else {
+                userInputs[numUserInputs] = userInput;
+                numUserInputs++;
+                resultString = "______________________________________\n"
+                        + "added: "
                         + userInput + "\n"
                         + "______________________________________\n";
             }
