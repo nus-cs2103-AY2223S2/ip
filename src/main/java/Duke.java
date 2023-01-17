a8f097f5e79eec176d806dd99a5319e056be4593
Sean Tan <seanfirefox@gmail.com>
2023-01-17 22:58:00 +0800

    Change from if else to switch case

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,85 +13,110 @@ public class Duke {
         String str = bufferedReader.readLine();
         ArrayList<Task> store = new ArrayList<>();
         while (!str.equals("bye")) {
-            if (str.equals("list")) {
-                int number = 1;
-                for(Task stored : store) {
-                    if (stored instanceof Todo) {
-                        String storedString = stored.getStr();
-                        boolean checked = stored.isChecked();
-                        if (checked) {
-                            System.out.println(number + ". [T][X] " + storedString);
-                        } else {
-                            System.out.println(number + ". [T][ ] " + storedString);
+            String[] arr = str.split(" ", 2);
+            try {
+                switch (arr[0]) {
+                    case "list":
+                        int number = 1;
+                        for (Task stored : store) {
+                            if (stored instanceof Todo) {
+                                String storedString = stored.getStr();
+                                boolean checked = stored.isChecked();
+                                if (checked) {
+                                    System.out.println(number + ". [T][X] " + storedString);
+                                } else {
+                                    System.out.println(number + ". [T][ ] " + storedString);
+                                }
+                            } else if (stored instanceof Deadline) {
+                                String storedString = stored.getStr();
+                                boolean checked = stored.isChecked();
+                                String storedDateTime = ((Deadline) stored).getDateTime();
+                                if (checked) {
+                                    System.out.println(number + ". [D][X] " + storedString + " (by: " + storedDateTime + ")");
+                                } else {
+                                    System.out.println(number + ". [D][ ] " + storedString + " (by: " + storedDateTime + ")");
+                                }
+                            } else if (stored instanceof Event) {
+                                String storedString = stored.getStr();
+                                boolean checked = stored.isChecked();
+                                String storedStart = ((Event) stored).getStart();
+                                String storedEnd = ((Event) stored).getEnd();
+                                if (checked) {
+                                    System.out.println(number + ". [E][X] " + storedString + " (from: " + storedStart +
+                                            " to: " + storedEnd + ")");
+                                } else {
+                                    System.out.println(number + ". [E][ ] " + storedString + " (from: " + storedStart +
+                                            " to: " + storedEnd + ")");
+                                }
+                            }
+                            number++;
                         }
-                    } else if (stored instanceof Deadline) {
-                        String storedString = stored.getStr();
-                        boolean checked = stored.isChecked();
-                        String storedDateTime = ((Deadline) stored).getDateTime();
-                        if (checked) {
-                            System.out.println(number + ". [D][X] " + storedString + " (by: " + storedDateTime + ")");
-                        } else {
-                            System.out.println(number + ". [D][ ] " + storedString+ " (by: " + storedDateTime + ")");
+                        break;
+                    case "mark":
+                        if (arr.length < 2) {
+                            throw new DukeException("Invalid format, please give numbers");
                         }
-                    } else if (stored instanceof Event) {
-                        String storedString = stored.getStr();
-                        boolean checked = stored.isChecked();
-                        String storedStart = ((Event) stored).getStart();
-                        String storedEnd = ((Event) stored).getEnd();
-                        if (checked) {
-                            System.out.println(number + ". [E][X] " + storedString + " (from: " + storedStart +
-                                    " to: " + storedEnd + ")");
-                        } else {
-                            System.out.println(number + ". [E][ ] " + storedString + " (from: " + storedStart +
-                                    " to: " + storedEnd + ")");
+                        marking(true, store, arr);
+                        break;
+                    case "unmark":
+                        if (arr.length < 2) {
+                            throw new DukeException("Invalid format, please give numbers");
                         }
-                    }
+                        marking(false, store, arr);
+                        break;
+                    case "todo":
+                        if (arr.length < 2) {
+                            throw new DukeException("Missing description");
+                        }
+                        Todo todo = new Todo(arr[1], false);
+                        store.add(todo);
 
+                        System.out.println("Got it. I've added this task:\n  [T][ ] " +
+                                arr[1] + "\n Now you have " + store.size() + " tasks in the list");
+                        break;
+                    case "deadline":
+                        if (arr.length < 2) {
+                            throw new DukeException("Missing description");
+                        }
+                        String[] toPrintSplit = arr[1].split(" /by ", 2);
+                        if (toPrintSplit.length < 2) {
+                            throw new DukeException("Missing deadline");
+                        }
+                        Deadline deadline = new Deadline(toPrintSplit[0], false);
+                        deadline.setDateTime(toPrintSplit[1]);
+                        store.add(deadline);
 
-                    number++;
+                        System.out.println("Got it. I've added this task:\n  [D][ ] " +
+                                toPrintSplit[0] + " (by: " + toPrintSplit[1] + ")\n Now you have " +
+                                store.size() + " tasks in the list");
+                        break;
+                    case "event":
+                        if (arr.length < 2) {
+                            throw new DukeException("Missing description");
+                        }
+                        String[] startEndTime = arr[1].split(" /from ");
+                        if (startEndTime.length < 2) {
+                            throw new DukeException("Missing Start Time");
+                        }
+                        String[] dateTime = startEndTime[1].split(" /to ");
+                        if (dateTime.length < 2) {
+                            throw new DukeException("Missing End Time");
+                        }
+                        Event event = new Event(startEndTime[0], false);
+                        event.setStartEnd(dateTime[0], dateTime[1]);
+                        store.add(event);
+
+                        System.out.println("Got it. I've added this task:\n  [E][ ] " +
+                                startEndTime[0] + " (from: " + dateTime[0] + " to: " + dateTime[1] +
+                                ")\n Now you have " + store.size() + " tasks in the list");
+                        break;
+                    default:
+                        throw new DukeException("Unknown format");
                 }
-            } else if (str.startsWith("mark ") && isNumber(str.replace("mark ", ""))) {
-                int index = Integer.parseInt(str.replace("mark ", ""));
-                Task task = store.get(index - 1);
-                task.setChecked(true);
-                System.out.println("Nice! I've marked this task as done: \n" + "[x] " + task.getStr());
-            } else if (str.startsWith("unmark ") && isNumber(str.replace("unmark ", ""))) {
-                int index = Integer.parseInt(str.replace("unmark ", ""));
-                Task task = store.get(index - 1);
-                task.setChecked(false);
-                System.out.println("OK, I've marked this task as not done yet: \n" + "[ ] " + task.getStr());
-            } else if(str.startsWith("todo ")) {
-                String toPrint = str.replace("todo ", "");
-                Todo todo = new Todo(toPrint, false);
-                store.add(todo);
-
-                System.out.println("Got it. I've added this task:\n  [T][ ] " +
-                        toPrint + "\n Now you have " + store.size() + " tasks in the list");
-            } else if(str.startsWith("deadline ")) {
-                String toPrint = str.replace("deadline ", "");
-                String[] toPrintSplit = toPrint.split(" /by ");
-                Deadline deadline = new Deadline(toPrintSplit[0], false);
-                deadline.setDateTime(toPrintSplit[1]);
-                store.add(deadline);
-
-                System.out.println("Got it. I've added this task:\n  [D][ ] " +
-                        toPrintSplit[0] + " (by: " + toPrintSplit[1] + ")\n Now you have " +
-                        store.size() + " tasks in the list");
-            } else if(str.startsWith("event ")) {
-                String toPrint = str.replace("event ", "");
-                String[] toPrintSplit = toPrint.split(" /from ");
-                String[] dateTime = toPrintSplit[1].split(" /to ");
-                Event event = new Event(toPrintSplit[0], false);
-                event.setStartEnd(dateTime[0], dateTime[1]);
-                store.add(event);
-
-                System.out.println("Got it. I've added this task:\n  [E][ ] " +
-                        toPrintSplit[0] + " (from: " + dateTime[0] + " to: " + dateTime[1] +
-                        ")\n Now you have " + store.size() + " tasks in the list");
-            } else {
-                Task task = new Task(str, false);
-                store.add(task);
-                System.out.println(str);
+            } catch (DukeException e) {
+                System.out.println(e.getMessage());
+            } catch (Exception e) {
+                System.out.println("Unknown error");
             }
             str = bufferedReader.readLine();
 
@@ -100,12 +125,16 @@ public class Duke {
         System.out.println("Bye. Hope to see you again soon!");
     }
 
-    public static boolean isNumber(String s) {
-        for (int i = 0; i < s.length(); i++)
-            if (!Character.isDigit(s.charAt(i)))
-                return false;
+    public static void marking(boolean b, ArrayList<? extends Task> store, String[] arr) {
+        int index = Integer.parseInt(arr[1]);
+        Task task = store.get(index - 1);
+        task.setChecked(b);
+        if (b) {
+            System.out.println("Nice! I've marked this task as done: \n" + "[x] " + task.getStr());
+        } else {
+            System.out.println("OK, I've marked this task as not done yet: \n" + "[ ] " + task.getStr());
+        }
 
-        return true;
     }
 
 }
