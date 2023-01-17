6b73f1694f304eb9b7c64e0a0dba130c61c921a1
clevon-w <e0726468@u.nus.edu>
2023-01-18 00:34:54 +0800

    added end time to Event

--- src/main/java/storage/Event.java
+++ src/main/java/storage/Event.java
@@ -3,19 +3,21 @@ package storage;
 public class Event extends Task {
 
     protected String from;
+    protected String to;
 
     /**
      * Creates a new Event with the specified description
      *
      * @param description The specified description
      */
-    public Event(String description, String from) {
+    public Event(String description, String from, String to) {
         super(description);
         this.from = from;
+        this.to = to;
     }
 
     @Override
     public String toString() {
-        return String.format("[E]%s (from: %s)", super.toString(), from);
+        return String.format("[E]%s (from: %s to: %s)", super.toString(), from, to);
     }
 }
