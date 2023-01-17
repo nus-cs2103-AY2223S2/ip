c738592072e0c45c4df31dc3aecc3ba7461fb7a0
joellow88 <joellow88@gmail.com>
2023-01-17 20:44:07 +0800

    Completed Level-2 Included functionality to add objects to list and print list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,9 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Duke {
+    private static boolean isRunning = true;
+    private static List<String> tasks = new ArrayList<String>();
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -19,20 +22,31 @@ public class Duke {
         System.out.println("Hello! I'm DUKE!\nWhat can I do for you?");
         System.out.println(separator);
 
-        boolean isRunning = true;
         while (isRunning) {
             System.out.println(you);
             String command = sc.nextLine();
             System.out.println(separator);
             System.out.println(duke);
-            System.out.println(command);
-            System.out.println(separator);
-            if (command.equals("bye")) {
-                isRunning = false;
+            switch (command) {
+                case "bye":
+                    isRunning = false;
+                    System.out.println("Bye! Hope to see you again soon!");
+                    break;
+                case "list":
+                    if (!tasks.isEmpty()) {
+                        for (int i = 1; i <= tasks.size(); i++) {
+                            System.out.println(String.format("%d. %s", i, tasks.get(i - 1)));
+                        }
+                    } else {
+                        System.out.println("You currently have no items in your to-do list!");
+                    }
+                    break;
+                default:
+                    tasks.add(command);
+                    System.out.println(String.format("Added: %s", command));
+                    break;
             }
+            System.out.println(separator);
         }
-
-
-
     }
 }
