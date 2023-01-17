a9d63da6bb6901fdcbd65528faca6643a9866365
LAPTOP-DOG86KJ2\pinoc <pinochoi@yahoo.com.sg>
2023-01-17 14:47:25 +0800

    added mark and unmark features to Crystal

--- src/main/java/Crystal.java
+++ src/main/java/Crystal.java
@@ -1,5 +1,7 @@
-import java.sql.SQLOutput;
+
 import java.util.*;
+import java.util.regex.*;
+
 
 class Crystal {
     public static void main(String[] args) {
@@ -9,21 +11,46 @@ class Crystal {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
 
-        ArrayList<String> list = new ArrayList<>();
+
+
+
+        //ArrayList<String> list = new ArrayList<>();
+        ArrayList<Task> list = new ArrayList<>();
         while (!input.equals("bye")) {
             if (input.equals("list")) {
                 System.out.println(" ____________________________________________________________");
                 for (int i = 0; i < list.size(); i++) {
-                    System.out.println(i+1 + ". " + list.get(i));
+                    System.out.println(i + 1 + ". " + list.get(i));
                 }
                 System.out.println(" ____________________________________________________________");
-            } else {
+            } else if (input.contains("unmark")) {
+                String getnum = input.replaceAll("[^0-9]", "");
+                int num = Integer.parseInt(getnum);
+                Task t = list.get(num - 1);
                 System.out.println(" ____________________________________________________________");
-                list.add(input);
+                System.out.println("Alright, I've marked this task as done: ");
+                t.isDone = false;
+                System.out.println(t.toString());
+                System.out.println(" ____________________________________________________________");
+            } else if (input.contains("mark")) {
+                String getnum = input.replaceAll("[^0-9]", "");
+                int num = Integer.parseInt(getnum);
+                Task t = list.get(num - 1);
+                System.out.println(" ____________________________________________________________");
+                System.out.println("Alright, I've marked the task as not done: ");
+                t.isDone = true;
+                System.out.println(t.toString());
+                System.out.println(" ____________________________________________________________");
+            }  else {
+                System.out.println(" ____________________________________________________________");
+                //list.add(input);
+                Task t = new Task(input);
+                list.add(t);
                 System.out.println("added: " + input);
                 System.out.println(" ____________________________________________________________");
 
             }
+
             input = sc.nextLine();
 
 
---
+++ src/main/java/Task.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public String toString() {
+        return "[" + this.getStatusIcon() + "] " + this.description;
+    }
+}
