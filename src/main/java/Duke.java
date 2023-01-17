330aa384d555a28a75102916fb9506f6f578b880
Zong Xun <zxun2000@gmail.com>
2023-01-17 18:16:47 +0800

    Add level 2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,26 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Duke {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Hello! I'm Tik. What can i do for you?");
+        ArrayList<String> taskList = new ArrayList<>();
+        System.out.println("Hello! I'm Duke. What can i do for you?");
         while (in.hasNext()) {
             String s = in.nextLine();
             String cmd = s.toLowerCase();
             if (cmd.equals("bye")) {
                 break;
+            } else if (cmd.equals("list")) {
+                for (int i=0; i<taskList.size(); i++) {
+                    System.out.println(i + 1 + ". " + taskList.get(i));
+                }
+            } else {
+                taskList.add(cmd);
+                System.out.println("added: " + cmd);
+
             }
-            System.out.println(s);
+
         }
     }
 }
