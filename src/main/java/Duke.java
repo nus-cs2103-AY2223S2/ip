0f7476be778a30431aee162a1b399b997244c162
jnjy <jefferyng2000@gmail.com>
2023-01-17 19:46:33 +0800

    Level-01: Added javadocs

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,11 +8,19 @@ public class Duke {
         exit();
     }
 
+    /**
+     * Prints response
+     * @param res Duke's response(String) to print
+     */
     public static void printRes(String res) {
         System.out.println("================================================================");
         System.out.println(res);
         System.out.println("================================================================\n");
     }
+
+    /**
+     * Greet the user
+     */
     public static void greet() {
         final String logo = "██████   █████  ██████   █████  \n"
                     + "██   ██ ██   ██ ██   ██ ██   ██ \n"
@@ -26,6 +34,9 @@ public class Duke {
         System.out.println();
     }
 
+    /**
+     * Echo the user's response
+     */
     public static void echo() {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
@@ -37,6 +48,9 @@ public class Duke {
         sc.close();
     }
 
+    /**
+     * Exit the program with an outro
+     */
     public static void exit() {
         String outro = "bella ciao";
         printRes(outro);
