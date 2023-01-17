73e06eb4c6dded0ceb6574a6fe77269a1cc1344a
dillongoh <gohjerwee@gmail.com>
2023-01-17 23:36:41 +0800

    added support for error handling

---
+++ src/main/java/InputException.java
@@ -0,0 +1,5 @@
+public class InputException extends Exception{
+    public InputException(String error) {
+        super(error);
+    }
+}
--- src/main/java/TunaBot.java
+++ src/main/java/TunaBot.java
@@ -1,79 +1,115 @@
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class TunaBot {
+    static String LINE = "------------------------------";
+    static Scanner s = new Scanner(System.in);
+    static ArrayList<Task> tasks = new ArrayList<>();
+    static boolean toExit = false;
     public static void main(String[] args) {
-        String LINE = "------------------------------";
-        Scanner s = new Scanner(System.in);
-        ArrayList<Task> tasks = new ArrayList<>();
+
         System.out.println(LINE);
         System.out.println("    Hello! I'm TunaBot\n" +
                 "    What can I do for you?");
         System.out.println(LINE);
-        boolean toExit = false;
         while (!toExit) {
-            String command = s.next();
+            String input = s.nextLine();
             System.out.println(LINE);
-            int index;
-            String name;
-            String start;
-            String end;
-            int count = tasks.size();
-            switch (command) {
-                case "list":
-                    System.out.println("BLUB! There are " + count + " task(s)!");
-                    for (int i = 1; i <= tasks.size(); i++) {
-                        System.out.println("    " + i + ". " + tasks.get(i - 1));
-                    }
-                    break;
-                case "bye":
-                    toExit = true;
-                    System.out.println("    Bye! Blub blub!");
-                    break;
-                case "mark":
-                    index = s.nextInt();
+            try {
+                parse(input);
+            } catch (InputException e) {
+                System.out.println(e.getMessage());
+            }
+            System.out.println(LINE);
+        }
+    }
+    public static void parse(String input) throws InputException {
+        String[] command = input.split(" ", 2);
+        switch (command[0]) {
+            case "list":
+                System.out.println("BLUB! There are " + tasks.size() + " task(s)!");
+                for (int i = 1; i <= tasks.size(); i++) {
+                    System.out.println("    " + i + ". " + tasks.get(i - 1));
+                }
+                break;
+            case "bye":
+                toExit = true;
+                System.out.println("    Bye! Blub blub!");
+                break;
+            case "mark":
+                if (command.length < 2) {
+                    throw new InputException("BLUB! No task chosen!");
+                }
+                try {
+                    int index = Integer.parseInt(command[1]);
                     tasks.get(index - 1).markDone();
                     System.out.println("    Blub! i have marked this as done!");
                     System.out.println(tasks.get(index - 1));
-                    break;
-                case "unmark":
-                    index = s.nextInt();
+                } catch (IllegalArgumentException e) {
+                    throw new InputException("BLUB! Index chosen isn't a number!");
+                } catch (IndexOutOfBoundsException e) {
+                    throw new InputException("BLUB! Task chosen isn't on the list!");
+                }
+                break;
+            case "unmark":
+                if (command.length < 2) {
+                    throw new InputException("BLUB! No task chosen!");
+                }
+                try {
+                    int index = Integer.parseInt(command[1]);
                     tasks.get(index - 1).unmarkDone();
                     System.out.println("    Blub! i have marked this as not done!");
                     System.out.println(tasks.get(index - 1));
-                    break;
-                case "todo":
-                    name = s.nextLine();
-                    Task newTask = new Task(name);
+                } catch (IllegalArgumentException e) {
+                    throw new InputException("BLUB! Index chosen isn't a number!");
+                } catch (IndexOutOfBoundsException e) {
+                    throw new InputException("BLUB! Task chosen isn't on the list!");
+                }
+                break;
+            case "todo":
+                if (command.length < 2) {
+                    throw new InputException("BLUB! Task needs a name!");
+                } else {
+                    Task newTask = new Task(command[1]);
                     tasks.add(newTask);
-                    count++;
                     System.out.println("    Blub! added: \n" + newTask);
-                    System.out.println("    Blub! You have " + count + " tasks now!");
-                    break;
-                case "event":
-                    name = s.nextLine();
-                    String fromSplit[] = name.split("/from ");
-                    String toSplit[] = fromSplit[1].split("/to ");
-                    Event newEvent = new Event(fromSplit[0], toSplit[0], toSplit[1]);
+                    System.out.println("    Blub! You have " + tasks.size() + " tasks now!");
+                }
+                break;
+            case "event":
+                if(command.length < 2) {
+                    throw new InputException("BLUB! Event needs a name, " +
+                            "a start time and end time!");
+                } else {
+                    String[] details = command[1].split("/from |/to ", 3);
+                    if (details.length < 3) {
+                        throw new InputException("BLUB! Event is missing info!" +
+                                " Please check input. BLUB!");
+                    }
+                    Event newEvent = new Event(details[0], details[1], details[2]);
                     tasks.add(newEvent);
-                    count++;
                     System.out.println("    Blub! added: \n" + newEvent);
-                    System.out.println("    Blub! You have " + count + " tasks now!");
-                    break;
-                case "deadline":
-                    name = s.nextLine();
-                    String split[] = name.split("/by ");
-                    Deadline newDeadline = new Deadline(split[0], split[1]);
+                    System.out.println("    Blub! You have " + tasks.size() + " tasks now!");
+                }
+                break;
+            case "deadline":
+                if(command.length < 2) {
+                    throw new InputException("BLUB! Deadline needs a name and due date!");
+                } else {
+                    String details[] = command[1].split("/by ", 2);
+                    if(details.length < 2) {
+                        throw new InputException("BLUB! Deadline is missing info!" +
+                                " PLease check input. BLUB!");
+                    }
+                    Deadline newDeadline = new Deadline(details[0], details[1]);
                     tasks.add(newDeadline);
-                    count++;
                     System.out.println("    Blub! added: \n" + newDeadline);
-                    System.out.println("    Blub! You have " + count + " tasks now!");
-                    break;
-                default:
-                    System.out.println("UH OH INVALID COMMAND");
-
-            }
-            System.out.println(LINE);
+                    System.out.println("    Blub! You have " + tasks.size() + " tasks now!");
+                }
+                break;
+            default:
+                System.out.println("BLUB OH! INVALID COMMAND!");
         }
     }
 }
