13901b588ccab8815b6fa2fa34f31148b19a0e96
slackernoob <maxang12@gmail.com>
2023-01-18 04:52:09 +0800

    Level-2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,39 +1,52 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
+        ArrayList<String> storage = new ArrayList<String>();
         Scanner sc = new Scanner(System.in);
         String spacingIndent = "    ";
         String welcomeMsg = spacingIndent + " "
                 + "Hello! I'm Duke\n"
                 + spacingIndent + " "
-                + "How can I help you?\n";
+                + "How can I help you?";
         String exitMsg = spacingIndent + " "
-                + "Farewell! See you soon!\n";
+                + "Farewell! See you soon!";
         String dashes = spacingIndent
                 + "______________________________"
-                + "______________________________\n";
+                + "______________________________";
         String curInput = "";
         boolean status = true;
-        System.out.println(dashes
-                + welcomeMsg
-                + dashes);
+        System.out.println(dashes);
+        System.out.println(welcomeMsg);
+        System.out.println(dashes);
 
         while (status) {
             curInput = sc.nextLine();
 
             if (curInput.equals("bye")){
                 status = false;
-            } else {
-                System.out.println(dashes
-                        + spacingIndent + " " + curInput
-                        + "\n" + dashes);
+            } else if (curInput.equals("list")) {
+                System.out.println(dashes);
+                for (int i = 0; i < storage.size(); i++) {
+                    int number = i + 1;
+                    System.out.println(spacingIndent + " "
+                            + number + ". " + storage.get(i));
+                }
+                System.out.println(dashes);
+            }
+            else {
+                storage.add(curInput);
+                System.out.println(dashes);
+                System.out.println(spacingIndent
+                        + " " + "added: " + curInput);
+                System.out.println(dashes);
             }
         }
 
-        System.out.println(dashes
-                + exitMsg
-                + dashes);
+        System.out.println(dashes);
+        System.out.println(exitMsg);
+        System.out.println(dashes);
 
     }
 }
