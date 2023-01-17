5cdda9260d2393be1922f02cf5bf2c662e4ad7b8
laihuiqi <huiiqiii22@gmail.com>
2023-01-17 21:19:12 +0800

    add Deadline.java, Event.java, Todo.java all inherit from Task class

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -2,10 +2,14 @@ public class Task {
 
     protected String description;
     protected boolean isDone;
+    protected static int count = 0;
+
+    public Task() {}
 
     public Task(String description) {
         this.description = description;
         this.isDone = false;
+        count++;
     }
 
     public String getStatusIcon() {
@@ -14,15 +18,20 @@ public class Task {
 
     public void markAsDone() {
         this.isDone = true;
-        Chattime.replyUser(String.format("Congrats! You've done this job:\n       %s", this.checkItem()));
+        Chattime.replyUser(String.format("Congrats! You've done this job:\n       %s", toString()));
     }
 
     public void unmarkDone() {
         this.isDone = false;
-        Chattime.replyUser(String.format("Arghh! This job is not done yet:\n       %s", this.checkItem()));
+        Chattime.replyUser(String.format("Arghh! This job is not done yet:\n       %s", toString()));
     }
 
-    public String checkItem() {
+    public static String totalTask() {
+        return "Now you have " + count + " tasks in the list.";
+    }
+
+    @Override
+    public String toString() {
         return String.format("[%s] %s", this.getStatusIcon(), this.description);
     }
 
