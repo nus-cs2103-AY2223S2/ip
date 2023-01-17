016359bd69ac0a69b516b1832242165be142ee59
cydtseng <tsengchenyu@gmail.com>
2023-01-17 16:09:31 +0800

    implement Level 3, Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,8 +2,7 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-        String[] userInputs = new String[100];
-        int numUserInputs = 0;
+        Task[] userTasks = new Task[100];
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -18,6 +17,7 @@ public class Duke {
                 + "______________________________________\n";
         System.out.print(greeting);
         Scanner scanner = new Scanner(System.in);
+
         while(continueRunning){
             String userInput = scanner.nextLine();
             String resultString = "";
@@ -28,15 +28,38 @@ public class Duke {
                 continueRunning = false;
             } else if(userInput.equals("list")){
                 StringBuilder listOfInputs = new StringBuilder();
-                for(int i = 0; i < numUserInputs; i++){
-                    listOfInputs.append(i + 1).append(". ").append(userInputs[i]).append("\n");
+                for(int i = 0; i < Task.getNumTasks(); i++){
+                    listOfInputs.append(i + 1)
+                            .append(".")
+                            .append("[")
+                            .append(userTasks[i].getStatusIcon())
+                            .append("] ")
+                            .append(userTasks[i]).append("\n");
                 }
                 resultString = "______________________________________\n"
+                        + "Here are the tasks in your list:\n"
                         + listOfInputs
                         + "______________________________________\n";
-            } else {
-                userInputs[numUserInputs] = userInput;
-                numUserInputs++;
+            } else if((userInput.split(" ")[0]).equals("mark")){
+                int numToMark = Integer.parseInt(userInput.split(" ")[1]);
+                userTasks[numToMark-1].setIsDone(true);
+                resultString = "______________________________________\n"
+                        + "Nice! I've marked this task as done:\n"
+                        + " [X] " + userTasks[numToMark-1] + "\n"
+                        + "______________________________________\n";
+
+            } else if((userInput.split(" ")[0]).equals("unmark")){
+                int numToUnmark = Integer.parseInt(userInput.split(" ")[1]);
+                userTasks[numToUnmark-1].setIsDone(false);
+                resultString = "______________________________________\n"
+                        + "OK, I've marked this task as not done yet:\n"
+                        + " [ ] " + userTasks[numToUnmark-1] + "\n"
+                        + "______________________________________\n";
+            }
+            else {
+                Task newTask = new Task(userInput);
+                userTasks[Task.getNumTasks()] = newTask;
+                Task.addTask();
                 resultString = "______________________________________\n"
                         + "added: "
                         + userInput + "\n"
