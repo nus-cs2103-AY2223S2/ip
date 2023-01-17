2fe7718d619965e1f2a7c30ab085852f91139b9c
jianminglok <jianminglok@hotmail.com>
2023-01-17 20:48:09 +0800

    Changed name for chatbot and added greetings

---
+++ src/main/java/TwoFive.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class TwoFive {
+    public static void main(String[] args) {
+        String logo = "  _______            ______ _\n"
+                    + " |__   __|          |  ____(_)\n"
+                    + "    | |_      _____ | |__   ___   _____\n"
+                    + "    | \\ \\ /\\ / / _ \\|  __| | \\ \\ / / _ \\\n"
+                    + "    | |\\ V  V / (_) | |    | |\\ V /  __/\n"
+                    + "    |_| \\_/\\_/ \\___/|_|    |_| \\_/ \\___|\n";
+
+                System.out.println("Hello from\n" + logo);
+
+        String divider = "____________________________________________________________";
+
+        //Greets users
+        System.out.println(divider);
+        System.out.println("In case you're still not clear, I'm TwoFive!");
+        System.out.println("I'm your personal assistant chatbot");
+        System.out.println("What can I do for you?");
+        System.out.println(divider);
+    }
+}
