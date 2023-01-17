8d1cb87cd42a85a64a72aa8233bf43f58aa68edf
laihuiqi <huiiqiii22@gmail.com>
2023-01-18 00:49:35 +0800

    add Week 2 Level-5

--- src/main/java/Chattime.java
+++ src/main/java/Chattime.java
@@ -5,9 +5,20 @@ import java.util.regex.Pattern;
 public class Chattime {
 
     private static final String greet = "Hey! I'm your friend, Chattime!\n" + "     How can I help you *^*";
-    private static final String line = "--------------------------------------------------******************CHATTIME";
+    private static final String line = "---------------------------------------------------------------------" +
+            "******************CHATTIME";
     private static final String goodBye = "Bye bye >^<! Visit me again when you need me ~";
+    private static final String WAITING_TASK = "Task(s) waiting to be completed:";
+    private static final String ADD_TASK = "Got it! I've added this task:\n       %s\n     %s";
+    private static final String UNRECOGNISED_COMMENT = "Sorry... but I don't understand what you said >,<";
+    private static final String LIST_EXP = "OOPS!!! list does not take any description.";
+    private static final String NO_DESCRIPTION = "OOPS!!! The description of %s cannot be empty.";
+    private static final String NO_INDEX = "OOPS!!! The index to %1$s cannot be empty.";
+    private static final String NEED_INT = "OOPS!!! The index to %1$s must be positive integer.";
+    private static final String IDX_OUT_OF_BOUND = "OOPS!!! The index is too large! We currently have %d task(s).";
+    private static final String MISSED_PARAM = "OOPS!!! %s should be in form of %s.";
     private static final ArrayList<Task> storeList = new ArrayList<>();
+
 
     public static void main(String[] args) {
         String logo = "      ___\n"
@@ -28,7 +39,11 @@ public class Chattime {
 
         Scanner sc = new Scanner(System.in);
 
-        chat(sc);
+        try {
+            chat(sc);
+        } catch (ChattimeException e) {
+            replyUser(e.getMessage());
+        }
 
     }
 
@@ -38,36 +53,56 @@ public class Chattime {
         System.out.println("    " + line);
     }
 
-    public static void chat(Scanner sc) {
+    public static void chat(Scanner sc) throws ChattimeException {
         String userInput = sc.nextLine();
         String[] splitCmd = userInput.split(" ", 2);
         String command = splitCmd[0];
 
         while (!command.equals("bye")) {
-
-            if (command.equals("list") && splitCmd.length == 1) {
-                displayList();
-
-            } else if (command.equals("mark")) {
-                mark(splitCmd[1]);
-
-            } else if (command.equals("unmark")) {
-                unmark(splitCmd[1]);
-
-            } else if (command.equals("todo")) {
-                todo(splitCmd[1]);
-
-            } else if (command.equals("deadline")) {
-                deadline(splitCmd[1]);
-
-            } else if (command.equals("event")) {
-                event(splitCmd[1]);
-
-            } else {
-                replyUser("I don't understand what you said >,<");
-
+            try {
+                switch (command) {
+                    case "list":
+                        if (splitCmd.length == 1) {
+                            displayList();
+                        } else {
+                            throw new ChattimeException(LIST_EXP);
+                        }
+                        break;
+                    case "todo":
+                    case "deadline":
+                    case "event":
+                        if (splitCmd.length < 2) {
+                            throw new ChattimeException(String.format(NO_DESCRIPTION, command));
+                        } else {
+                            switch (command) {
+                                case "todo":
+                                    todo(splitCmd[1]);
+                                    break;
+                                case "deadline":
+                                    deadline(splitCmd[1]);
+                                    break;
+                                case "event":
+                                    event(splitCmd[1]);
+                                    break;
+                            }
+                        }
+                        break;
+                    case "mark":
+                    case "unmark":
+                        if (splitCmd.length < 2) {
+                            throw new ChattimeException(String.format(NO_INDEX, command));
+                        } else if (command.equals("mark")) {
+                            mark(splitCmd[1]);
+                        } else {
+                            unmark(splitCmd[1]);
+                        }
+                        break;
+                    default:
+                        throw new ChattimeException(UNRECOGNISED_COMMENT);
+                }
+            } catch(ChattimeException e) {
+                replyUser(e.getMessage());
             }
-
             userInput = sc.nextLine();
             splitCmd = userInput.split(" ", 2);
             command = splitCmd[0];
@@ -80,7 +115,7 @@ public class Chattime {
 
     public static void displayList() {
         int i = 1;
-        String message = "Task(s) waiting to be completed:";
+        String message = WAITING_TASK;
 
         for (Task task : storeList) {
             message = message.concat(String.format("\n     %d. %s", i, task.toString()));
@@ -93,27 +128,42 @@ public class Chattime {
     public static void addTask(Task newTask) {
         storeList.add(newTask);
         String taskData = newTask.toString();
-        String message = "Got it! I've added this task:\n       " + taskData + "\n     " + Task.totalTask();
+        String message = String.format(ADD_TASK, taskData, Task.totalTask());
 
         replyUser(message);
     }
 
-    public static int checkInt(String content) {
+    public static int checkInt(String content, String command) throws ChattimeException {
         if (Pattern.matches("^[0-9]*$", content)) {
-            return Integer.parseInt(content);
+            int index = Integer.parseInt(content);
+            if (index > Task.count) {
+                throw new ChattimeException(String.format(IDX_OUT_OF_BOUND, Task.count));
+            } else {
+                return index;
+            }
         } else {
-            return -1;
+            throw new ChattimeException(String.format(NEED_INT, command));
         }
     }
 
     public static void mark(String context) {
-        int index = checkInt(context);
-        storeList.get(index - 1).markAsDone();
+        int index = 0;
+        try {
+            index = checkInt(context, "mark");
+            storeList.get(index - 1).markAsDone();
+        } catch (ChattimeException e) {
+            replyUser(e.getMessage());
+        }
     }
 
     public static void unmark(String context) {
-        int index = checkInt(context);
-        storeList.get(index - 1).unmarkDone();
+        int index = 0;
+        try {
+            index = checkInt(context, "unmark");
+            storeList.get(index - 1).unmarkDone();
+        } catch (ChattimeException e) {
+            replyUser(e.getMessage());
+        }
     }
 
     public static void todo(String task) {
@@ -121,8 +171,11 @@ public class Chattime {
         addTask(todoTask);
     }
 
-    public static void deadline(String content) {
+    public static void deadline(String content) throws ChattimeException {
         String[] splitBy = content.split(" /by ", 2);
+        if (splitBy.length < 2 || splitBy[1].equals("")) {
+            throw new ChattimeException(String.format(MISSED_PARAM, "deadline", "deadline (task) /by (describe)"));
+        }
         String task = splitBy[0];
         String by = splitBy[1];
 
@@ -130,11 +183,21 @@ public class Chattime {
         addTask(deadlineTask);
     }
 
-    public static void event(String content) {
+    public static void event(String content) throws ChattimeException {
         String[] splitTask = content.split(" /from ", 2);
         String task = splitTask[0];
+
+        if (splitTask.length < 2 || splitTask[1].equals("")) {
+            throw new ChattimeException(
+                    String.format(MISSED_PARAM, "event", "event (task) /from (describe 1) /to (describe 2)"));
+        }
         String[] splitFrom = splitTask[1].split(" /to ", 2);
         String from = splitFrom[0];
+
+        if (splitFrom.length < 2 || splitFrom[1].equals("")) {
+            throw new ChattimeException(
+                    String.format(MISSED_PARAM, "event", "event (task) /from (describe 1) /to (describe 2)"));
+        }
         String to = splitFrom[1];
 
         Event eventTask = new Event(task, from, to);
