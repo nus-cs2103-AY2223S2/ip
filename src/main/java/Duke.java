e0bce91c2ed7e99817c43bf26e91d4c5842c2a93
FireRadical22 <97332558+FireRadical22@users.noreply.github.com>
2023-01-17 15:54:52 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +9,32 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        String intro = "_____________________________________\n"
+                + "Hello! I'm Duke\n"
+                + "What can I do for you?\n"
+                + "_____________________________________\n";
+        System.out.println(intro);
+
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+
+        while(true){
+            if (input.equalsIgnoreCase("bye")){
+                System.out.println(
+                        "_____________________________________\n"
+                        + "Bye. Hope to see you again soon!\n"
+                        + "_____________________________________\n"
+                );
+                System.exit(0);
+            } else {
+                System.out.println(
+                        "_____________________________________\n"
+                        + input + "\n"
+                        + "_____________________________________\n"
+                );
+                input = sc.next();
+            }
+        }
     }
 }
