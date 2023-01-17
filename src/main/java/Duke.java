f2b1ab2e30fe7868433a25ab5171c4078733beb8
ruiyigan <85063215+ruiyigan@users.noreply.github.com>
2023-01-17 11:31:41 +0800

    added logic for level 1 Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,15 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello I'm Duke\n" + "What can I do for you?");
+        Scanner myObj = new Scanner(System.in);
+        String userInput = myObj.nextLine();
+        while (!userInput.equals("bye")) {
+            System.out.println(userInput);
+            myObj = new Scanner(System.in);
+            userInput = myObj.nextLine();
+        }
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
