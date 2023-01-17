2b416450f5dd18893f5745c193a3fe312f369f80
UnwilledTangent <tinsean.work@gmail.com>
2023-01-17 22:03:24 +0800

    Complete Level 5. Handle Errors

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws DukeException {
         Scanner scanner = new Scanner(System.in);
 
         String logo = " ____        _        \n"
@@ -17,65 +17,77 @@ public class Duke {
         ArrayList<Task> tasksList = new ArrayList<Task>();
 
         while (!input.equals("bye")) {
-            input = scanner.nextLine();
-            String command = input.split(" ")[0];
+            try {
+                input = scanner.nextLine();
+                String command = input.split(" ")[0];
 
-            if (command.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
-                break;
-            } else if (command.equals("list")) {
-                int taskNumber = 1;
-                System.out.println("Here are the tasks in your list:");
-                for (Task t : tasksList) {
-                    System.out.println(Integer.toString(taskNumber) + ". " + t);
-                    taskNumber++;
+                if (command.equals("bye")) {
+                    System.out.println("Bye. Hope to see you again soon!");
+                    break;
+                } else if (command.equals("list")) {
+                    int taskNumber = 1;
+                    System.out.println("Here are the tasks in your list:");
+                    for (Task t : tasksList) {
+                        System.out.println(Integer.toString(taskNumber) + ". " + t);
+                        taskNumber++;
+                    }
+                } else if (command.equals("mark")) {
+                    int taskNumber = Integer.parseInt(input.split(" ")[1]) - 1;
+                    tasksList.get(taskNumber).markAsDone();
+                } else if (command.equals("unmark")) {
+                    int taskNumber = Integer.parseInt(input.split(" ")[1]) - 1;
+                    tasksList.get(taskNumber).markAsNotDone();
+                } else if (command.equals("todo")) {
+                    try {
+                        String inputWithoutCommand = input.substring(5);
+
+                        ToDo toDo = new ToDo(inputWithoutCommand);
+                        tasksList.add(toDo);
+
+                        System.out.println("Got it. I've added this task:");
+                        System.out.println(toDo);
+                        System.out.println("Now you have " + tasksList.size() + " tasks in the list.");
+                    } catch (StringIndexOutOfBoundsException e) {
+                        e.printStackTrace();
+                        System.out.println("☹ OOPS!!! The description of a todo cannot be empty.");
+                    }
+                } else if (command.equals("deadline")) {
+                    int indexOfByString = input.indexOf("/by ");
+                    int indexOfDate = indexOfByString + 4;
+
+                    String inputWithoutCommand = input.substring(9, indexOfByString - 1);
+
+                    String date = input.substring(indexOfDate, input.length() - 1);
+
+                    Deadline deadline = new Deadline(input.substring(9, indexOfByString), input.substring(indexOfDate));
+                    tasksList.add(deadline);
+
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(deadline);
+                    System.out.println("Now you have " + tasksList.size() + " tasks in the list.");
+                } else if (command.equals("event")) {
+                    // TODO
+                    String inputWithoutCommand = input.substring(6);
+
+                    int indexOfFromSubstring = inputWithoutCommand.indexOf("/from ");
+                    int indexOfToSubstring = inputWithoutCommand.indexOf("/to ");
+
+                    String description = inputWithoutCommand.substring(0, indexOfFromSubstring);
+                    String startTime = inputWithoutCommand.substring(indexOfFromSubstring + 6, indexOfToSubstring);
+                    String endTime = inputWithoutCommand.substring(indexOfToSubstring + 4);
+
+                    Event event = new Event(description, startTime, endTime);
+                    tasksList.add(event);
+
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(event);
+                    System.out.println("Now you have " + tasksList.size() + " tasks in the list.");
+                } else {
+                    throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
                 }
-            } else if (command.equals("mark")) {
-                int taskNumber = Integer.parseInt(input.split(" ")[1]) - 1;
-                tasksList.get(taskNumber).markAsDone();
-            } else if (command.equals("unmark")) {
-                int taskNumber = Integer.parseInt(input.split(" ")[1]) - 1;
-                tasksList.get(taskNumber).markAsNotDone();
-            } else if (command.equals("todo")) {
-                String inputWithoutCommand = input.substring(5);
-
-                ToDo toDo = new ToDo(inputWithoutCommand);
-                tasksList.add(toDo);
-
-                System.out.println("Got it. I've added this task:");
-                System.out.println(toDo);
-                System.out.println("Now you have " + tasksList.size() + " tasks in the list.");
-            } else if (command.equals("deadline")) {
-                int indexOfByString = input.indexOf("/by ");
-                int indexOfDate = indexOfByString + 4;
-
-                String inputWithoutCommand = input.substring(9, indexOfByString - 1);
-
-                String date = input.substring(indexOfDate, input.length() - 1);
-
-                Deadline deadline = new Deadline(input.substring(9, indexOfByString), input.substring(indexOfDate));
-                tasksList.add(deadline);
-
-                System.out.println("Got it. I've added this task:");
-                System.out.println(deadline);
-                System.out.println("Now you have " + tasksList.size() + " tasks in the list.");
-            } else if (command.equals("event")) {
-                // TODO
-                String inputWithoutCommand = input.substring(6);
-
-                int indexOfFromSubstring = inputWithoutCommand.indexOf("/from ");
-                int indexOfToSubstring = inputWithoutCommand.indexOf("/to ");
-
-                String description = inputWithoutCommand.substring(0, indexOfFromSubstring);
-                String startTime = inputWithoutCommand.substring(indexOfFromSubstring + 6, indexOfToSubstring);
-                String endTime = inputWithoutCommand.substring(indexOfToSubstring + 4);
-
-                Event event = new Event(description, startTime, endTime);
-                tasksList.add(event);
-
-                System.out.println("Got it. I've added this task:");
-                System.out.println(event);
-                System.out.println("Now you have " + tasksList.size() + " tasks in the list.");
+            } catch (DukeException e) {
+                // if you try and catch the exception, you can still continue to run the program! WOW!
+                e.printStackTrace();
             }
         }
     }
@@ -153,6 +165,24 @@ public class Duke {
         @Override
         public String toString() {
             return taskType + super.toString() + " (from: " + this.startTime + " to: " + this.endTime + ")";
+        }
+    }
+
+    public static class DukeException extends Exception {
+         public DukeException() {
+             super();
+         }
+
+         public DukeException(String message, Throwable cause) {
+             super(message, cause);
+         }
+
+        public DukeException(String message) {
+            super(message);
+        }
+
+        public DukeException(Throwable cause) {
+            super(cause);
         }
     }
 }
