3dd6b29758a83abbc62d525eb2819c50a2e1c0ac
Goh Jun Yi <54541329+Junyi00@users.noreply.github.com>
2023-01-17 15:44:16 +0800

    feat: Bot reads and echos back command
    - Bot's name is now "Arii"
    - Bot's behaviour is updated to greet, echo and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,40 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    public static String LOGO = "______     ______     __     __    \n" +
+                                "/\\  __ \\   /\\  == \\   /\\ \\   /\\ \\   \n" +
+                                "\\ \\  __ \\  \\ \\  __<   \\ \\ \\  \\ \\ \\  \n" +
+                                " \\ \\_\\ \\_\\  \\ \\_\\ \\_\\  \\ \\_\\  \\ \\_\\ \n" +
+                                "  \\/_/\\/_/   \\/_/ /_/   \\/_/   \\/_/ \n";
+
+    /**
+     *
+     * @param cmd user command
+     * @return true if program should exit
+     */
+    public boolean handleCommand(String cmd) {
+        if (cmd.equals("bye")) {
+            System.out.println("Till next time...");
+            return true;
+        } else {
+            System.out.println(cmd);
+            return false;
+        }
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner scanner = new Scanner(System.in);
+        Duke duke = new Duke();
+
+        System.out.println("Hello, I am \n" + Duke.LOGO);
+        System.out.println("How shall I assist you today?");
+
+        boolean toExit = false;
+        while (!toExit) {
+            System.out.print("\n:> ");
+            String cmd = scanner.nextLine();
+            toExit = duke.handleCommand(cmd);
+        }
     }
 }
