8f8faadc351e72a32533d69016544ed7e3c86f02
9fc70c892 <89006592+9fc70c892@users.noreply.github.com>
2023-01-17 20:41:35 +0800

    Level-1 Rename splash to Waffles. Added Greeting, Echo input, Exit with bye

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,52 @@
+/**
+ * DUKE
+ * CS2103 project
+ * @author EDWIN LIM
+ * @version 0.01
+ */
+
+import java.util.Scanner;
 public class Duke {
+
+    /**
+     * Main method for the program
+     * @param args UNUSED
+     */
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greet();
+        Scanner sc = new Scanner(System.in);
+        Boolean QUIT = false;
+        while (!QUIT) {
+            String userIn = sc.nextLine();
+            switch (userIn) {
+                case "bye" :
+//                case "exit" :
+                    QUIT = true;
+                    System.out.println("Bye. Hope to see you again soon!\n");
+                    break;
+                default:
+                    System.out.println(userIn);
+                    break;
+            }
+        }
     }
+
+    /**
+     * Prints standard welcome message.
+     */
+    public static void greet() {
+        String logo = "                __  __ _           \n"
+                + "               / _|/ _| |          \n"
+                + "__      ____ _| |_| |_| | ___  ___ \n"
+                + "\\ \\ /\\ / / _` |  _|  _| |/ _ \\/ __|\n"
+                + " \\ V  V / (_| | | | | | |  __/\\__ \\n"
+                + "  \\_/\\_/ \\__,_|_| |_| |_|\\___||___/";
+        System.out.println("Hello from\n" + logo);
+        System.out.println("");
+        System.out.println("Hello! I'm " + "Waffles");
+        System.out.println("What can I do for you?");
+
+    }
+
 }
+
