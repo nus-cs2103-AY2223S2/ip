60d90302a0ffec397afee3548683a98cfdceb8ff
Li Bo Feng <libofeng37@gmail.com>
2023-01-17 15:17:43 +0800

    Implement level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,33 @@
+import java.util.Scanner;
 public class Duke {
+    protected static String divider = "     ____________________________________________________________";
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        printWelcomeMessage();
+
+        while (true) {
+            String command = sc.nextLine();
+            if (command.equals("bye")) {
+                System.out.println(formatMessage("Bye. Hope to see you again soon!"));
+                break;
+            }
+            System.out.println(formatMessage(command));
+        }
+    }
+
+    public static void printWelcomeMessage() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(divider);
+        System.out.println("     Hello! I'm Duke");
+        System.out.println("     What can I do for you?");
+        System.out.println(divider);
+    }
+
+    public static String formatMessage(String message) {
+        return divider + "\n     " + message + "\n" + divider;
     }
 }
