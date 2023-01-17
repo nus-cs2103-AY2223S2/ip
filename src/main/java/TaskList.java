d5f6cbede2c2f014dc393d893f2d89e3f2d9f389
EvitanRelta <shauntanzongzhi@gmail.com>
2023-01-17 20:52:16 +0800

    Add get status method to tasklist

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,6 +1,17 @@
 import java.util.ArrayList;
 
 public class TaskList extends ArrayList<Task> {
+    public String getStatus() {
+        switch (this.size()) {
+            case 0:
+                return "Now you have no tasks in the list.";
+            case 1:
+                return "Now you have 1 task in the list.";
+            default:
+                return String.format("Now you have %d task in the list.", this.size());
+        }
+    }
+
     @Override
     public String toString() {
         if (this.isEmpty()) {
