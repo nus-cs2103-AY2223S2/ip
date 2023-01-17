1488abee3922e1972695775d128c4f9287e8cbbb
Jun-How <e0725343@u.nus.edu>
2023-01-17 23:32:00 +0800

    Implemented Level-1 increment.Renamed bot.

---
+++ src/main/java/JeoBot.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class JeoBot {
+    public static void greeting() {
+        String name = "JeoBot";
+        String divider = "-----------------------";
+        System.out.println(divider + "\n" + "Greetings from " + name + "!\n" + "How may I help you?\n" + divider);
+    }
+
+    public static void echo() {
+        String divider = "________________________________________________________________";
+        boolean hasInput = true;
+        while (hasInput) {
+            Scanner in = new Scanner(System.in);
+            String s = in.nextLine();
+            System.out.println(divider);
+            if (s.equals("bye")) {
+                hasInput = false;
+                System.out.println("Thank you for using my service. Hope to see you again soon!");
+            } else {
+                System.out.println(s);
+            }
+            System.out.println(divider);
+        }
+    }
+
+    public static void main(String[] args) {
+        greeting();
+        echo();
+    }
+}
