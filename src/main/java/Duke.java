731965b51ce83f4f36964fac95188798be5beeb0
liankokhai <unfazing@gmail.com>
2023-01-17 11:45:56 +0800

    implemented vanilla Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,16 +2,39 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-        String text = "Hello! I'm Duke \n What can I do for you";
+        String text;
         String horiLine = "___________________________";
         String terminate = "bye";
         Scanner s = new Scanner(System.in);
-        while (!text.equals(terminate)) {
-            System.out.println(horiLine);
-            System.out.println(text);
-            System.out.println(horiLine);
+        String[] myList = new String[100];
+        int counter = 0;
+        boolean flag = true;
+
+        System.out.println(horiLine);
+        System.out.println("Hello! I'm Duke \nWhat can I do for you");
+        System.out.println(horiLine);
+
+        while (flag) {
             text = s.nextLine();
+            if (text.equals(terminate)) {
+                System.out.println(horiLine);
+                System.out.println("Bye! Hope to see you again soon!");
+                System.out.println(horiLine);
+                flag = false;
+            } else if (text.equals("list")) {
+                System.out.println(horiLine);
+                for (int i=1; i<counter + 1; i++) {
+                    System.out.format("%d. %s \n", i, myList[i - 1]);
+                }
+                System.out.println(horiLine);
+            } else {
+                System.out.println(horiLine);
+                System.out.println("added: " + text);
+                System.out.println(horiLine);
+                myList[counter] = text;
+                counter++;
+            }
         }
-        System.out.println("Bye! Hope to see you again soon!");
+
     }
 }
