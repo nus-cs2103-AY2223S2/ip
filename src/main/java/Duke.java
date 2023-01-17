be3708a7b4a3f3e7fee86d2a801a3a8e88e61f53
FireRadical22 <97332558+FireRadical22@users.noreply.github.com>
2023-01-17 16:19:11 +0800

    Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,12 @@
 import java.util.Scanner;
 
 public class Duke {
+    private static String[] storage = new String[100];
+    private static int pointer = 0;
     public static void main(String[] args) {
+
+
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -17,24 +22,44 @@ public class Duke {
         System.out.println(intro);
 
         Scanner sc = new Scanner(System.in);
-        String input = sc.next();
+        String input = sc.nextLine();
 
         while(true){
-            if (input.equalsIgnoreCase("bye")){
-                System.out.println(
-                        "_____________________________________\n"
-                        + "Bye. Hope to see you again soon!\n"
-                        + "_____________________________________\n"
-                );
-                System.exit(0);
+            if (input.equalsIgnoreCase("bye")) {
+                bye();
+            } else if (input.equalsIgnoreCase("list")){
+                list();
+                input = sc.nextLine();
             } else {
                 System.out.println(
                         "_____________________________________\n"
-                        + input + "\n"
+                        + "added: " + input + "\n"
                         + "_____________________________________\n"
                 );
-                input = sc.next();
+                storage[pointer] = input;
+                pointer++;
+                input = sc.nextLine();
             }
         }
+    }
+
+    private static void bye(){
+        System.out.println(
+                "_____________________________________\n"
+                        + "Bye. Hope to see you again soon!\n"
+                        + "_____________________________________\n"
+        );
+        System.exit(0);
+    }
+
+    private static void list(){
+        System.out.println("_____________________________________\n");
+        for (int i = 0; i < pointer; i++){
+            int index = i + 1;
+            System.out.println(
+                    index + ". " + storage[i] + "\n"
+            );
+        }
+        System.out.println("_____________________________________\n");
     }
 }
