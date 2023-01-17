f707004f269d02d731cea6de9339667435556820
zhoyx <yuxin@Zhous-MacBook-Air-6.local>
2023-01-17 15:20:34 +0800

    Completed Level 1. Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static void reply(String s) {
+        String linebreak = "_________________________________________________________\n";
+        System.out.println(linebreak);
+        System.out.println(s);
+        System.out.println(linebreak);
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String greeting = "Hello! I'm Duke\n What can I do for you?";
+        String bye = "Bye. Hope to see you again!";
+        Scanner sc = new Scanner(System.in);
+        String currentInput = "";
+
+        //Introduction
+        reply(greeting);
+
+        currentInput = sc.nextLine();
+        while (!currentInput.equalsIgnoreCase("bye")) {
+            reply(currentInput);
+            currentInput = sc.nextLine();
+        }
+        reply(bye);
     }
 }
