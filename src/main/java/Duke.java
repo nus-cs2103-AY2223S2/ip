d93347bd2cca963b114b7b283d33cf3458d81ba1
DESKTOP-4FAGDBQ\Lee Jia Xin <jxleejiaxin@gmail.com>
2023-01-17 20:46:59 +0800

    Implemented Level 1: Greet,Echo,Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,17 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        String greeting = "Hello! I'm Duke! \n What can I do for you? \n";
+        System.out.println(greeting);
+
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.next();
+        while (!userInput.equals("bye")) {
+            System.out.println("\n" + userInput + "\n");
+            userInput = scanner.next();
+        }
+        System.out.println("\nBye. Hope to see you again soon!\n");
     }
 }
