af667c9d6a039f859180c5e0e619b197e6582cb6
salty-flower <95509524+salty-flower@users.noreply.github.com>
2023-01-17 15:33:43 +0800

    :truck:Duke -> Tach

---
+++ src/main/java/Tach.java
@@ -0,0 +1,11 @@
+public class Tach {
+    public static void main(String[] args) {
+        String logo = "___________             .__     \n"
+                + "\\__    ___/____    ____ |  |__  \n"
+                + "  |    |  \\__  \\ _/ ___\\|  |  \\ \n"
+                + "  |    |   / __ \\\\  \\___|   Y  \\\n"
+                + "  |____|  (____  /\\___  >___|  /\n"
+                + "               \\/     \\/     \\/ \n";
+        System.out.println("Yoooo from\n" + logo);
+    }
+}
