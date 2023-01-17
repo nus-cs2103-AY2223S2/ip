0d310e334d3f9b648c17bf3b2b3f8a9165ea49d5
mayuanxin1234 <yuanxinma@gmail.com>
2023-01-17 09:00:56 +0800

    Level-5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,122 +4,136 @@ class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
-        Scanner myObj = new Scanner(System.in);
-        String input = myObj.nextLine();
-        String[] s = input.split(" ");
-        Task[] list = new Task[100];
-        int currentIndex = 0;
-        while(!input.equals("bye")) {
-           if (input.equals("list")) {
-                System.out.println("Here are the tasks in your list");
-                for (int i = 0; i < currentIndex; i++) {
-                    System.out.print(i + 1 + ".");
-                    System.out.println(list[i].toString());
-                }
-                input = myObj.nextLine();
-                s = input.split(" ");
-            } else if (s[0].equals("unmark")) {
+
+            Scanner myObj = new Scanner(System.in);
+            String input = myObj.nextLine();
+            String[] s = input.split(" ");
+            Task[] list = new Task[100];
+            int currentIndex = 0;
+            while (!input.equals("bye")) {
+                try {
+                if (input.equals("list")) {
+                    System.out.println("Here are the tasks in your list");
+                    for (int i = 0; i < currentIndex; i++) {
+                        System.out.print(i + 1 + ".");
+                        System.out.println(list[i].toString());
+                    }
+                    input = myObj.nextLine();
+                    s = input.split(" ");
+                } else if (s[0].equals("unmark")) {
                     Task taskName = list[Integer.parseInt(s[1]) - 1];
                     taskName.unMark();
                     System.out.println("OK, I've marked this task as not done yet");
-                        System.out.println(taskName.toString());
+                    System.out.println(taskName.toString());
                     input = myObj.nextLine();
                     s = input.split(" ");
+                } else if (s[0].equals("mark")) {
+                    Task taskName = list[Integer.parseInt(s[1]) - 1];
+                    taskName.markAsDone();
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println(taskName.toString());
+                    input = myObj.nextLine();
+                    s = input.split(" ");
+                } else if (s[0].equals("todo")) {
+                    String taskDescription = "";
+                    if (s.length < 2) {
+                        throw new DukeException("☹ OOPS!!! The description of a todo cannot be empty.");
+                    }
+                    for (int j = 1; j < s.length; j++) {
+                        taskDescription += s[j];
+                        taskDescription += " ";
+                    }
+                    ToDos taskName = new ToDos(taskDescription);
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(taskName.toString());
+                    list[currentIndex] = taskName;
+                    currentIndex++;
+                    System.out.println("Now you have " + currentIndex + " tasks in the list.");
+                    input = myObj.nextLine();
+                    s = input.split(" ");
+                } else if (s[0].equals("deadline")) {
+                    String taskDescription = "";
+                    boolean isTime = false;
+                    String time = "";
+                    if (s.length < 2) {
+                        throw new DukeException("☹ OOPS!!! The description of a deadline cannot be empty.");
+                    }
+                    for (int j = 1; j < s.length; j++) {
+                        if (s[j].equals("/by")) {
+                            isTime = true;
+                        } else if (isTime) {
+                            if (j + 1 == s.length) {
+                                time += s[j];
+                            } else {
+                                time += s[j];
+                                time += " ";
+                            }
+                        } else {
+                            taskDescription += s[j];
+                            taskDescription += " ";
+                        }
+                    }
+                    Deadlines taskName = new Deadlines(taskDescription, time);
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(taskName.toString());
+                    list[currentIndex] = taskName;
+                    currentIndex++;
+                    System.out.println("Now you have " + currentIndex + " tasks in the list.");
+                    input = myObj.nextLine();
+                    s = input.split(" ");
+                } else if (s[0].equals("event")) {
+                    String taskDescription = "";
+                    boolean isStartTime = false;
+                    boolean isEndTime = false;
+                    String startTime = "";
+                    String endTime = "";
+                    if (s.length < 2) {
+                        throw new DukeException("☹ OOPS!!! The description of an event cannot be empty.");
+                    }
+                    for (int j = 1; j < s.length; j++) {
+                        if (s[j].equals("/from")) {
+                            isStartTime = true;
+                        } else if (isStartTime && !s[j].equals("/to") && !isEndTime) {
+                            startTime += s[j];
+                            startTime += " ";
+                        } else if (s[j].equals("/to")) {
+                            isEndTime = true;
+                        } else if (isEndTime) {
+                            if (j + 1 == s.length) {
+                                endTime += s[j];
+                            } else {
+                                endTime += s[j];
+                                endTime += " ";
+                            }
+                        } else {
+                            taskDescription += s[j];
+                            taskDescription += " ";
+                        }
+                    }
+                    Events taskName = new Events(taskDescription, startTime, endTime);
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(taskName.toString());
+                    list[currentIndex] = taskName;
+                    currentIndex++;
+                    System.out.println("Now you have " + currentIndex + " tasks in the list.");
+                    input = myObj.nextLine();
+                    s = input.split(" ");
+                } else {
+                    throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
                 }
-            else if (s[0].equals("mark")) {
-                Task taskName = list[Integer.parseInt(s[1]) - 1];
-                taskName.markAsDone();
-               System.out.println("Nice! I've marked this task as done:");
-               System.out.println(taskName.toString());
-                input = myObj.nextLine();
-                s = input.split(" ");
-            } else if (s[0].equals("todo")){
-                String taskDescription = "";
-                for (int j = 1; j < s.length; j++) {
-                    taskDescription += s[j];
-<<<<<<< HEAD
-                    taskDescription += " ";
-
-=======
->>>>>>> 4693ab64d0745c8fd864c0b7aa542938a869ea0c
-                }
-                ToDos taskName = new ToDos(taskDescription);
-               System.out.println("Got it. I've added this task:");
-               System.out.println(taskName.toString());
-               list[currentIndex] = taskName;
-               currentIndex++;
-               System.out.println("Now you have " + currentIndex + " tasks in the list.");
-               input = myObj.nextLine();
-               s = input.split(" ");
-           }
-           else if (s[0].equals("deadline")){
-               String taskDescription = "";
-               boolean isTime = false;
-               String time = "";
-               for (int j = 1; j < s.length; j++) {
-                   if(s[j].equals("/by")) {
-                       isTime = true;
-                   } else if(isTime) {
-                       if (j + 1 == s.length) {
-                           time += s[j];
-                       } else {
-                           time += s[j];
-                           time += " ";
-                       }
-                   } else {
-                       taskDescription += s[j];
-                       taskDescription += " ";
-                   }
-               }
-               Deadlines taskName = new Deadlines(taskDescription, time);
-               System.out.println("Got it. I've added this task:");
-               System.out.println(taskName.toString());
-               list[currentIndex] = taskName;
-               currentIndex++;
-               System.out.println("Now you have " + currentIndex + " tasks in the list.");
-               input = myObj.nextLine();
-               s = input.split(" ");
-           }
-           else if (s[0].equals("event")){
-               String taskDescription = "";
-               boolean isStartTime = false;
-               boolean isEndTime = false;
-               String startTime = "";
-               String endTime = "";
-               for (int j = 1; j < s.length; j++) {
-                   if(s[j].equals("/from")) {
-                       isStartTime = true;
-                   } else if(isStartTime && !s[j].equals("/to") && !isEndTime) {
-                       startTime += s[j];
-                       startTime += " ";
-                   } else if (s[j].equals("/to")) {
-                       isEndTime = true;
-                   } else if (isEndTime) {
-                       if (j + 1 == s.length) {
-                           endTime += s[j];
-                       } else {
-                           endTime += s[j];
-                           endTime += " ";
-                       }
-                   }
-                    else {
-                       taskDescription += s[j];
-                       taskDescription += " ";
-                   }
-               }
-               Events taskName = new Events(taskDescription, startTime, endTime);
-               System.out.println("Got it. I've added this task:");
-               System.out.println(taskName.toString());
-               list[currentIndex] = taskName;
-               currentIndex++;
-               System.out.println("Now you have " + currentIndex + " tasks in the list.");
-               input = myObj.nextLine();
-               s = input.split(" ");
-           }
 
             }
+                catch (Exception e) {
+                    System.out.println(e);
+                    input = myObj.nextLine();
+                    s = input.split(" ");
+
+                }
 
 
+
+        }
         System.out.println("Bye. Hope to see you again soon!");
-    }
+        }
+
 }
