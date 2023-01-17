e72de1c14ce86289a58781251f9fd62077df7fa7
clevon-w <e0726468@u.nus.edu>
2023-01-17 21:28:07 +0800

    added normalisation of whitespace function to user inputs

--- src/main/java/parser/inputParser.java
+++ src/main/java/parser/inputParser.java
@@ -46,28 +46,59 @@ public class InputParser {
         }
     }
 
+    public String normaliseAllWhitespaces(String s) {
+        int l = s.length();
+        StringBuilder sb = new StringBuilder(l);
+
+        boolean lastWasWhite = false;
+        boolean modified = false;
+
+        for (int i = 0; i < l; i++) {
+            int c = s.codePointAt(i);
+            if (Character.isWhitespace(c)) {
+                if (lastWasWhite || (c == ' ' && i == 0)) {
+                    modified = true;
+                    lastWasWhite = true;
+                    continue;
+                }
+
+                if (c != ' ') {
+                    modified = true;
+                }
+
+                sb.append(' ');
+                lastWasWhite = true;
+            } else {
+                sb.appendCodePoint(c);
+                lastWasWhite = false;
+            }
+        }
+        return modified ? sb.toString() : s;
+    }
+
     public void categorisation() {
-        String[] arrStr = input.split(" ", 2);
+        String normalisedInput = normaliseAllWhitespaces(input);
+        String[] arrStr = normalisedInput.split(" ", 2);
 
-        if (arrStr.length > 1) {
-            if (arrStr[0].equals("")) {
-                throw new MissingArgumentException("Remember to tell me what you want me to do!");
+        if (arrStr.length == 2) { // user input is more than one word
+            if (arrStr[0].equals("mark") || arrStr[0].equals("unmark")) { // user wants to mark/unmark something
+                try {
+                    Integer.parseInt(arrStr[1]);
+                } catch (NumberFormatException nfe) {
+                    throw new InvalidArgumentException("Enter a number after mark/unmark!");
+                }
+
+                this.inputType = arrStr[0].toLowerCase();
+                this.inputContent = arrStr[1];
+            } else { // treat it as a regular create new task response
+                this.inputType = normalisedInput;
+                this.inputContent = normalisedInput;
             }
-
-            try {
-                Integer.parseInt(arrStr[1]);
-            } catch (NumberFormatException nfe) {
-                throw new InvalidArgumentException("Enter a number after mark/unmark!");
-            }
-
-            this.inputType = arrStr[0].toLowerCase();
-            this.inputContent = arrStr[1];
-        } else if (arrStr.length == 1 && !arrStr[0].equals("")) {
+        } else if (arrStr.length == 1) { // user input is only one word (either list or create)
             this.inputType = arrStr[0].toLowerCase();
             this.inputContent = arrStr[0];
-        } else if (arrStr.length == 0) {
+        } else if (arrStr.length == 0) { // for the edge cases where user input " " or ""
             throw new MissingArgumentException("Please enter something for me to parse!");
         }
-
     }
 }
