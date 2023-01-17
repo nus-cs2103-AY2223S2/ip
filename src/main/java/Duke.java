a6fb2ff7bec5e31f471efa8f7946ce7031ca046b
NoyaReoT <rayonteo@gmail.com>
2023-01-17 12:47:50 +0800

    Implemented Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,11 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+    public static ArrayList<String> list = new ArrayList<String>();
+    public static Scanner sc = new Scanner(System.in);
+    public static boolean exitApp = false;
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -9,27 +14,47 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
-        // Init
-        displayMessage("Hello! I'm Duke\nWhat can i do for you?");
-        Scanner sc = new Scanner(System.in);
+        init();
 
-        // Application main loop
-        while (true) {
-            String input = sc.nextLine();
-
-            if (input.equalsIgnoreCase("bye")) break;
-
-            displayMessage(input);
+        // App loop
+        while (!exitApp) {
+            update();
         }
 
+        // Exit message
         displayMessage("Bye! Hope to see you again soon!");
     }
 
+    public static void init() {
+        displayMessage("Hello! I'm Duke\nWhat can i do for you?");
+    }
+
+    public static void update() {
+        String input = sc.nextLine();
+
+        if (input.equalsIgnoreCase("bye")) {
+            exitApp = true;
+            return;
+        }
+
+        if (input.equalsIgnoreCase("list")) {
+            String output = "";
+            for (int i = 0; i != list.size();++i) {
+               String item = list.get(i);
+               output += (i + 1) + ". " + item + "\n";
+            }
+            displayMessage(output);
+            return;
+        }
+
+        list.add(input);
+        displayMessage("Added " + input);
+    }
 
     public static void displayMessage(String msg) {
 
         String wrapTop = "__________________________\n";
         String wrapBottom = "\n__________________________";
-        System.out.println(wrapTop + msg + wrapBottom);
+        System.out.println(wrapTop + msg.toString() + wrapBottom);
     }
 }
