841dc6a57b6e5d12e00e9bb4b1e2df17bdafa685
Li Bo Feng <libofeng37@gmail.com>
2023-01-17 15:34:51 +0800

    Implement level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 public class Duke {
-    protected static String divider = "     ____________________________________________________________";
+    protected static String indent = "     ";
+    protected static String divider = indent + "____________________________________________________________";
+    protected static ArrayList<String> tasks =  new ArrayList<>(100);
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         printWelcomeMessage();
@@ -10,8 +14,12 @@ public class Duke {
             if (command.equals("bye")) {
                 System.out.println(formatMessage("Bye. Hope to see you again soon!"));
                 break;
+            } else if (command.equals("list")) {
+                System.out.println(formatMessage(listTasks()));
+            } else {
+                tasks.add(command);
+                System.out.println(formatMessage("added: " + command));
             }
-            System.out.println(formatMessage(command));
         }
     }
 
@@ -22,12 +30,26 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println(divider);
-        System.out.println("     Hello! I'm Duke");
-        System.out.println("     What can I do for you?");
+        System.out.println(indent + "Hello! I'm Duke");
+        System.out.println(indent + "What can I do for you?");
         System.out.println(divider);
     }
 
     public static String formatMessage(String message) {
-        return divider + "\n     " + message + "\n" + divider;
+        return divider + "\n" + indent + message + "\n" + divider;
+    }
+
+    public static String listTasks() {
+        String output = "";
+        for (int i = 0; i < tasks.size(); i++) {
+            if (i != 0) {
+                output += indent;
+            }
+            output += (i + 1) + ". " + tasks.get(i);
+            if (i < tasks.size() - 1) {
+                output += "\n";
+            }
+        }
+        return output;
     }
 }
