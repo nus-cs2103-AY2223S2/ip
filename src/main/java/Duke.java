7a999fe194e416b6b21d10942f96f4905827dfa8
gohyongjing <gohyongjing@gmail.com>
2023-01-17 17:07:23 +0800

    Add skeletal program for Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,45 @@
+import java.util.Scanner;
 public class Duke {
+    /** Scanner used by each duke */
+    private Scanner scanner;
+    /** Whether the duke is still running or has been commanded to end */
+    private boolean isRunning;
+
+    /**
+     * Runs the duke.
+     */
+    public void run() {
+        this.init();
+        while (this.isRunning) {
+            this.execute(this.readCommand());
+        }
+        this.exit();
+    }
+
+    private void init() {
+        this.scanner = new Scanner(System.in);
+        this.isRunning = true;
+        System.out.println("Hello!");
+    }
+
+    private void exit() {
+        this.scanner.close();
+        System.out.println("GoodBye!");
+    }
+
+    private String readCommand() {
+        return scanner.next();
+    }
+
+    private void execute(String command) {
+        if (command.equals("bye")) {
+            this.running = false;
+        } else {
+            System.out.println(command);
+        }
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke duke = new Duke();
+        duke.run();
     }
 }
