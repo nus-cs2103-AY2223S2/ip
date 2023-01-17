58155c356b407f24945d006666199b287745ea56
Oliver Loo <oliverloo09@gmail.com>
2023-01-17 16:52:00 +0800

    level 2, list function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Duke {
 
     static String line = "      -----------------------------------------------------------------";
-
+    static ArrayList<String> list = new ArrayList<String>(100);
     public static void main(String[] args) {
         greet();
         System.out.println(line);
@@ -16,12 +16,18 @@ public class Duke {
             if (input.equalsIgnoreCase("bye")) {
                 System.out.println("        byebye! Have an exquisite day, cutiepatootie");
                 break;
+            } else if (input.equalsIgnoreCase("list")) {
+                System.out.println(line);
+                for(int i = 0; i < list.size(); i++) {
+                    System.out.println("        " + (i + 1) + ". " + list.get(i));
+                }
+                System.out.println(line);
+            } else {
+                //echoes input
+                System.out.println("\n" + line);
+                echo(input);
+                System.out.println(line);
             }
-
-            //echoes input
-            System.out.println("\n" + line);
-            echo(input);
-            System.out.println(line);
         }
         sc.close();
         System.out.println(line);
@@ -29,7 +35,15 @@ public class Duke {
 
     public static void echo(String name) {
         System.out.println();
-        System.out.println("        " + name);
+        System.out.println("        added: " + name);
+        list.add(name);
+
+    }
+
+    public static void addlist(String name) {
+        System.out.println();
+        System.out.println(name);
+
     }
 
     static void greet() {
