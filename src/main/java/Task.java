edf4f932fb4943a58a7dae054ec59ea7d2f9dd8e
jianminglok <jianminglok@hotmail.com>
2023-01-17 21:26:28 +0800

    Rephrased task name as description and added task status

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,14 +1,33 @@
 public class Task {
-    private String taskName;
+    private String taskDescription;
     private int taskId;
+    private boolean isDone;
     private static int numberOfTasks = 0;
+
     public Task(String taskName) {
-        this.taskName = taskName;
+        this.taskDescription = taskName;
         this.taskId = numberOfTasks + 1;
+        this.isDone = false;
         numberOfTasks++;
     }
+
+    public String getTaskStatus() {
+        //Done tasks are marked with X
+        return (isDone ? "[X]" : "[ ]");
+    }
+
+    //Mark current task as done
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    //Mark current task as undone
+    public void markAsUndone() {
+        this.isDone = false;
+    }
+
     @Override
     public String toString() {
-        return this.taskId + ". " + this.taskName;
+        return this.taskId + ". " + this.getTaskStatus() + " " + this.taskDescription;
     }
 }
