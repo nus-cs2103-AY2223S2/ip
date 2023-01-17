52203eeb2bdb8fab8bc9eb736464945755a019ce
francisyzy <francisyzy@u.nus.edu>
2023-01-17 11:12:56 +0800

    Add echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,9 @@
+import java.io.*;
+import java.util.*;
+
 public class Duke {
+    private static final String line = "____________________________________________________________";
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +11,16 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNext()) {
+            String input = sc.nextLine();
+            System.out.println("    " + line);
+            System.out.println("      " + input);
+            System.out.println("    " + line);
+            System.out.println();
+        }
+
+        sc.close();
     }
 }
