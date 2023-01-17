48c84511224664c3a339f8491458d87292070beb
evanpy <evanleewj@gmail.com>
2023-01-17 17:23:38 +0800

    Added echo functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,49 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    /** 
+     * Outputs given string with formatting.
+     * 
+     * @param string String to be outputted.
+     */
+    private static void output(String string) {
+        System.out.println("____________________________________________________________\n" +
+                            string + "\n" +
+                            "____________________________________________________________\n");
+    }
+
+    // Outputs welcome message.
+    private static void welcomeMsg() {
+        output("Hello! I'm Duke\nWhat can I do for you?");
+    }
+    
+    // Outputs exit message.
+    private static void exitMsg() {
+        output("Bye. Hope to see you again soon!");
+    }
+
+    /** 
+     * Echoes the input given by the user.
+     * 
+     * @param input User input.
+     */
+    private static void echo(String input) {
+        output(input);
+    }
+
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        welcomeMsg();
+
+        while(true) {
+            String input = sc.nextLine();
+            if (input.equals("bye"))
+                break;
+            echo(input);
+        }
+
+        exitMsg();
     }
 }
