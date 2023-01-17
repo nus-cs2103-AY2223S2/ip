d31eb73a50dad8f9c93db8d3b2ca9b0368df4ac2
joellow88 <joellow88@gmail.com>
2023-01-17 20:56:31 +0800

    Added Task.java Refactoring of Duke.java to use Task.java is not complete

---
+++ src/main/java/Task.java
@@ -0,0 +1,18 @@
+public class Task {
+    private String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public boolean isDone() {
+        return isDone;
+    }
+    public void setDone(boolean done) {
+        isDone = done;
+    }
+}
