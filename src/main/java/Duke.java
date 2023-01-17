25fb41c6ab88fb4732dad119fff72e77c70e3203
OliviaJHL <305914126@qq.com>
2023-01-17 19:48:26 +0800

    level-1 Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,28 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        final String Indentation = " ";
+        System.out.println(Indentation + "____________________________________________________________");
+        System.out.println(Indentation + "Hello! I'm Duke");
+        System.out.println(Indentation + "What can I do for you?");
+        System.out.println(Indentation + "____________________________________________________________");
+
+        Scanner str = new Scanner(System.in);
+        String command;
+
+        do {
+            command = str.nextLine();
+            if (!command.equals("bye")) {
+                System.out.println(Indentation + "____________________________________________________________");
+                System.out.println(Indentation + command);
+                System.out.println(Indentation + "____________________________________________________________");
+            }
+
+        } while (!command.equals("bye"));
+
+        System.out.println(Indentation + "____________________________________________________________");
+        System.out.println(Indentation + "Bye. Hope to see you again soon!");
+        System.out.println(Indentation + "____________________________________________________________");
     }
 }
