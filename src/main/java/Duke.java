a7e1dc7167ba1f7901727334b454d70f38c86d90
Zong Xun <zxun2000@gmail.com>
2023-01-17 18:01:16 +0800

    Add level 1: Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,15 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner in = new Scanner(System.in);
+        System.out.println("Hello! I'm Tik. What can i do for you?");
+        while (in.hasNext()) {
+            String s = in.nextLine();
+            String cmd = s.toLowerCase();
+            if (cmd.equals("bye")) {
+                break;
+            }
+            System.out.println(s);
+        }
     }
 }
