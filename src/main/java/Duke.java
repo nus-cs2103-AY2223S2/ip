52b5c50d3ba11e8b55f5be72ed2d484f66ac1b40
Guo-KeCheng <guo.kecheng99@gmail.com>
2023-01-18 06:40:43 +0800

    Implement Level 3: Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,7 +7,6 @@ public class Duke {
     private static int counter;
     private static HashMap<Integer, Task> toDoList;
 
-
     public static void main(String[] args)  {
         boolean terminate = false;
         counter = 0;
@@ -18,7 +17,7 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Welcome to\n" + logo);
 
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
@@ -26,22 +25,44 @@ public class Duke {
         try {
             while (!terminate) {
                 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-                String input = br.readLine();
+                String command = br.readLine();
+                String[] inputs = command.split(" ");
+
+                if (command.equals("")) {
+                    // Do nothing
 
                 // Once detect "bye", print and terminate
-                if (input.equals("bye")) {
+                } else if (command.equals("bye")) {
                     terminate = true;
                     System.out.println("Bye. Hope to see you again soon!");
 
-                // If input is "list", prints the toDoList
-                } else if (input.equals("list")) {
+                // If command is "list", prints the toDoList
+                } else if (command.equals("list")) {
                     print_list();
+
+                // if the first token is "mark" and second token is within limit
+                } else if (inputs[0].equals("mark") && inputs.length == 2) {
+                    Integer task_no = Integer.parseInt(inputs[1]);
+                    if (task_no <= inputs.length) {
+                        mark(task_no);
+                    } else {
+                        System.out.println("Invalid task number!");
+                    }
+
+                // if the first token is "unmark" and second token is within limit
+                } else if (inputs[0].equals("unmark") && inputs.length == 2) {
+                    Integer task_no = Integer.parseInt(inputs[1]);
+                    if (task_no <= inputs.length) {
+                        unmark(task_no);
+                    } else {
+                        System.out.println("Invalid task number!");
+                    }
 
                 // Else handles the commands
                 } else {
-                    toDoList.put(++counter, new Task(input));
+                    toDoList.put(++counter, new Task(command));
                     System.out.print("added: ");
-                    System.out.println(input);
+                    System.out.println(command);
 
                 }
 
@@ -52,9 +73,41 @@ public class Duke {
         }
     }
 
+    /*
+     * print_list iterates through the toDoList and prints
+     * out the status of each individual tasks
+     */
     public static void print_list() {
+        System.out.println("Here are the tasks in your list: ");
+
         for (Integer key: toDoList.keySet()) {
-            System.out.println(key + ": " + toDoList.get(key));
+            Task toDo = toDoList.get(key);
+
+            System.out.println(key + ". " + toDo);
+
         }
+    }
+
+    /*
+     * mark takes in an Integer key
+     * and marks the corresponding task as completed
+     */
+    public static void mark(Integer key) {
+        toDoList.get(key).markCompleted();
+
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println("   " + toDoList.get(key));
+
+    }
+
+    /*
+     * mark takes in an Integer key
+     * and marks the corresponding task as not completed
+     */
+    public static void unmark(Integer key) {
+        toDoList.get(key).markUncompleted();
+
+        System.out.println("OK, I've marked this task as not done yet:");
+        System.out.println("   " + toDoList.get(key));
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,11 +1,23 @@
 public class Task {
+    private boolean isCompleted = false; //by default the task should not be completed
     private String task;
     public Task(String task) {
         this.task = task;
     }
 
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void markUncompleted() {
+        isCompleted = false;
+    }
+    public void markCompleted() {
+        isCompleted = true;
+    }
+
     @Override
     public String toString() {
-        return task;
+        return (isCompleted ? "[X] " + task : "[ ] " + task);
     }
 }
