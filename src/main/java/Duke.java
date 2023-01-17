83410001b33ed7fdbd57af5ccbbc747d637f6553
LimJiaYan <jiayan.yanyan@gmail.com>
2023-01-17 20:16:39 +0800

    Lvl3: Implement mark, unmark

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,38 +1,58 @@
-import com.sun.source.tree.WhileLoopTree;
-
 import java.util.Scanner;
 
 public class Duke {
-    private static String[] task = new String[100];
-    private static int numOfTask = 0;
-    private static void greet() {
+    private static Task[] task = new Task[100];
+
+    public static void greet() {
         System.out.println("\t____________________________________________________________");
         System.out.println("\tHello! I'm Duke\n" + "\tWhat can I do for you?");
         System.out.println("\t____________________________________________________________");
     }
-    private static void add(String s) {
-        task[numOfTask] = s;
-        numOfTask++;
+    public static void add(String s) {
+        int numOfTask = Task.getNumOfTask();
+        task[numOfTask] = new Task(s);
 
         System.out.println("\t____________________________________________________________");
         System.out.println("\tadded: " + s);
         System.out.println("\t____________________________________________________________");
     }
 
-    private static void list() {
+    public static void list() {
         System.out.println("\t____________________________________________________________");
+        System.out.println("\tHere are the tasks in your list:");
+        int numOfTask = Task.getNumOfTask();
         for(int i = 0; i < numOfTask; i++) {
             int tmp = i + 1;
-            String msg = tmp + ". " + task[i];
+            Task currentTask = task[i];
+            String msg = tmp + ".[" + currentTask.getStatusIcon() + "] " + currentTask.getDescription();
             System.out.println("\t" + msg);
         }
         System.out.println("\t____________________________________________________________");
     }
-    private static void exit() {
+    public static void exit() {
         System.out.println("\t____________________________________________________________");
         System.out.println("\tBye. Hope to see you again soon!");
         System.out.println("\t____________________________________________________________");
+    }
 
+    public static void mark(int i) {
+        task[i].setDone(true);
+
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\tNice! I've marked this task as done: ");
+        String msg = "  [" + task[i].getStatusIcon() + "] " + task[i].getDescription();
+        System.out.println("\t" + msg);
+        System.out.println("\t____________________________________________________________");
+    }
+
+    public static void unmark(int i) {
+        task[i].setDone(false);
+
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\tOK, I've marked this task as not done yet:");
+        String msg = "  [" + task[i].getStatusIcon() + "] " + task[i].getDescription();
+        System.out.println("\t" + msg);
+        System.out.println("\t____________________________________________________________");
     }
 
     public static void main(String[] args) {
@@ -41,8 +61,16 @@ public class Duke {
         String input = scanner.nextLine();
 
         while (!input.equals("bye")) {
-            if(input.equals("list")) {
+            if (input.equals("list")) {
                 list();
+            } else if (input.contains("mark")) {
+                String[] split = input.split(" ", 2);
+                int index = Integer.parseInt(split[1]) - 1;
+                if (input.contains("unmark")) {
+                    unmark(index);
+                } else {
+                    mark(index);
+                }
             } else {
                 add(input);
             }
---
+++ src/main/java/Task.java
@@ -0,0 +1,30 @@
+
+public class Task {
+    protected String description;
+    protected boolean isDone;
+    private static int numOfTask = 0;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+        numOfTask++;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public static int getNumOfTask() {
+        return numOfTask;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDone(Boolean b) {
+        isDone = b;
+    }
+    //...
+}
+
