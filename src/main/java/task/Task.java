f6784673edc885800924a5691b0dcd7658faa1f0
daitenshionyan <snivy.oshowott@gmail.com>
2023-01-17 20:21:07 +0800

    Task class: add mark functionalities

--- src/main/java/task/Task.java
+++ src/main/java/task/Task.java
@@ -4,15 +4,41 @@ package task;
 /** A task that the user has to do. */
 public class Task {
     private final String name;
+    private final boolean isDone;
 
 
     public Task(String name) {
+        this(name, false);
+    }
+
+    public Task(String name, boolean isDone) {
         this.name = name;
+        this.isDone = isDone;
+    }
+
+
+    public Task mark(boolean isDone) {
+        return new Task(this.name, isDone);
+    }
+
+
+    public String getName() {
+        return name;
     }
 
 
     @Override
     public String toString() {
-        return name;
+        return String.format("%s %s",
+            getStatusString(),
+            getName()
+        );
+    }
+
+    private String getStatusString() {
+        if (isDone) {
+            return "[X]";
+        }
+        return "[ ]";
     }
 }
