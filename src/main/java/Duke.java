7346867f0266ffb96e2da46112ae2221d4a67193
Guo-KeCheng <guo.kecheng99@gmail.com>
2023-01-17 22:11:41 +0800

    Add Level 1-1 features

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Duke {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
+        boolean terminate = false;
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+
+        try {
+            while (!terminate) {
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                String input = br.readLine();
+
+                if (input.equals("bye")) {
+                    terminate = true;
+                    System.out.println("Bye. Hope to see you again soon!");
+                } else {
+                    System.out.println(input);
+                }
+
+            }
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
