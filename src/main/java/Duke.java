467a90038a17bf2cdc6f6fef80ab868bfd899bd1
KevinEyo1 <eyokaiwenkevin@gmail.com>
2023-01-17 15:31:21 +0800

    Level-1 done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
+    public static String pad(String val) {
+        return "     ---------------------------\n     " + val + "\n     ---------------------------";
+    }
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(logo + "Hello! I'm Duke. How may I be of assistance?\n");
+        String str = sc.nextLine();
+        while (!str.equals("bye")){
+            System.out.println(pad(str));
+            str = sc.nextLine();
+        }
+        System.out.println(pad("Thank you for your patronage. Goodbye!"));
     }
 }
