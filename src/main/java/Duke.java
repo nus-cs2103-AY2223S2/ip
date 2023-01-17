6b51fecec0b40ffd7924cce57d8519e5bc7f0033
liankokhai <unfazing@gmail.com>
2023-01-17 11:17:55 +0800

    implement vanilla Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,17 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String text = "Hello! I'm Duke \n What can I do for you";
+        String horiLine = "___________________________";
+        String terminate = "bye";
+        Scanner s = new Scanner(System.in);
+        while (!text.equals(terminate)) {
+            System.out.println(horiLine);
+            System.out.println(text);
+            System.out.println(horiLine);
+            text = s.nextLine();
+        }
+        System.out.println("Bye! Hope to see you again soon!");
     }
 }
