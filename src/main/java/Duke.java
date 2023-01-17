b74f259369b8899749e10d124609971eb9db3ccc
Jiatong Han <jiatong.han@u.nus.edu>
2023-01-18 00:43:51 +0800

    Init Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,30 +1,37 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+    public static ArrayList<String> todos = new ArrayList<>();
+
     public static void print(String s) {
         System.out.println(s);
     }
 
     public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
-//        System.out.println("Hello from\n" + logo);
-
         String greeting = "Hello! I'm Duke\n" +
                 "     What can I do for you?";
         print(greeting);
-
         Scanner sc = new Scanner(System.in);
         while (sc.hasNextLine()) {
             String input = sc.nextLine();
             if (input.equals("bye")) {
                 print("Bye. Hope to see you again soon!");
                 System.exit(0);
+            } else if (input.equals("list")) {
+                if (todos.isEmpty()) {
+                    print("None yet.");
+                } else {
+                    int i = 1;
+                    for (String s : todos) {
+                        print(i + ". " + s);
+                        i++;
+                    }
+                }
+            } else {
+                todos.add(input);
+                print("added:" + input);
             }
-            print(input);
         }
     }
 }
