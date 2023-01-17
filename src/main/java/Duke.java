5d63943a22225dcc9ffa910ea8bfe5e5fe82472e
ruiyigan <85063215+ruiyigan@users.noreply.github.com>
2023-01-17 12:11:26 +0800

    added comments on Duke.java for better documentation

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,14 +2,28 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        // store: storing text entered by user
+        String[] store;
+        store = new String[100];
+
+        // keeping count the next avaliable index of store
+        int counter = 0;
+
+        // greetings
         System.out.println("Hello I'm Duke\n" + "What can I do for you?");
+
+        // obtaining first input by user
         Scanner myObj = new Scanner(System.in);
         String userInput = myObj.nextLine();
+
+        // exit loop when user input is bye
         while (!userInput.equals("bye")) {
             System.out.println(userInput);
             myObj = new Scanner(System.in);
             userInput = myObj.nextLine();
         }
+
+        // prints exist statement
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
