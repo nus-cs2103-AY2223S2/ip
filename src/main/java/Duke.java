830ae5073ef8901ce4d2b90de8525c36b437f8bd
Kang Yue Ran <kangyueran09@gmail.com>
2023-01-17 19:21:45 +0800

    Added Level 3 features, Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -16,36 +16,65 @@ public class Duke {
             System.out.println(this.formatString(line));
         }
     }
-    public String formatList(List userText) {
-        int index = 1;
+    public String formatList(List userTasks) {
         String formattedList = "";
-        while (!userText.isEmpty()) {
-            formattedList += index + ". " + userText.remove(0) + "\n";
-            index++;
+        for (Object t : userTasks) {
+            int pos = userTasks.indexOf(t) + 1;
+            formattedList += pos + ". " + t + "\n";
         }
         return formattedList.trim();
     }
-    public void addAndList() {
-        List<String> userText = new ArrayList<>();
+    public void addMarkList() {
+        List<Task> userTasks = new ArrayList<>();
 
         System.out.println(this.formatString("Hello! I'm Gerty\nWhat can I do for you?"));
-        while (true) {
+
+        boolean doLoop = true;
+
+        while (doLoop) {
             Scanner input = new Scanner(System.in);
             String line = input.nextLine();
-            if ("bye".equalsIgnoreCase(line)) {
-                System.out.println(this.formatString("Bye. Hope to see you again soon!"));
-                break;
-            } else if ("list".equalsIgnoreCase(line)) {
-                System.out.println(this.formatString(this.formatList(userText)));
-            } else {
-                userText.add(line);
-                System.out.println(this.formatString("added: " + line));
+
+            String arr[] = line.split(" ", 2);
+            String keyword = arr[0];
+
+            switch (keyword.toLowerCase()) {
+                case ("bye"): {
+                    doLoop = false;
+                    System.out.println(this.formatString("Bye. Hope to see you again soon!"));
+                    break;
+                }
+                case ("list"): {
+                    System.out.println(this.formatString("Here are the tasks in your list: \n" + this.formatList(userTasks)));
+                    break;
+                }
+                case ("mark"): {
+                    String i = arr[1];
+                    int index = Integer.parseInt(i) - 1;
+                    Task t = userTasks.get(index);
+                    t.markAsDone();
+                    System.out.println(this.formatString("Nice! I've marked this task as done: \n" + t));
+                    break;
+                }
+                case ("unmark"): {
+                    String i = arr[1];
+                    int index = Integer.parseInt(i) - 1;
+                    Task t = userTasks.get(index);
+                    t.unmarkAsDone();
+                    System.out.println(this.formatString("OK, I've marked this task as not done yet: \n" + t));
+                    break;
+                }
+                default: {
+                    Task t = new Task(line);
+                    userTasks.add(t);
+                    System.out.println(this.formatString(line));
+                    break;
+                }
             }
         }
-
     }
     public static void main(String[] args) {
         Duke gerty = new Duke();
-        gerty.addAndList();
+        gerty.addMarkList();
     }
 }
