dc888189a0d55423fe53f2159ca3db0149e2ee6c
cydtseng <tsengchenyu@gmail.com>
2023-01-17 15:05:07 +0800

    implement duke level 1, greet, echo and exit.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,29 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+
         System.out.println("Hello from\n" + logo);
+        boolean continueRunning = true;
+        String greeting = "______________________________________\n"
+                + "Hey there buddy! I'm Duke\n"
+                + "What can I do for you today?\n"
+                + "______________________________________\n";
+        System.out.print(greeting);
+        Scanner scanner = new Scanner(System.in);
+        while(continueRunning){
+            String userInput = scanner.nextLine();
+            String resultString = "";
+            if (userInput.equals("bye")){
+                resultString = "______________________________________\n"
+                        + "Bye. Hope to see you again soon!\n"
+                        + "______________________________________\n";
+                continueRunning = false;
+            } else {
+                resultString = "______________________________________\n"
+                        + userInput + "\n"
+                        + "______________________________________\n";
+            }
+            System.out.print(resultString);
+        }
     }
 }
