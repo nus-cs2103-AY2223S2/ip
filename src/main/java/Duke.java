ca593633974e57fd30dc4b5b0d48a40aee2280f7
evanpy <evanleewj@gmail.com>
2023-01-17 22:05:54 +0800

    Added task adding and listing functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 
 public class Duke {
+    private static int numOfTasks = 0;
+    private static String[] tasks = new String[100];
 
     /** 
      * Outputs given string with formatting.
@@ -9,18 +11,18 @@ public class Duke {
      */
     private static void output(String string) {
         System.out.println("____________________________________________________________\n" +
-                            string + "\n" +
+                            string + 
                             "____________________________________________________________\n");
     }
 
     // Outputs welcome message.
     private static void welcomeMsg() {
-        output("Hello! I'm Duke\nWhat can I do for you?");
+        output("Hello! I'm Duke\nWhat can I do for you?\n");
     }
     
     // Outputs exit message.
     private static void exitMsg() {
-        output("Bye. Hope to see you again soon!");
+        output("Bye. Hope to see you again soon!\n");
     }
 
     /** 
@@ -32,6 +34,25 @@ public class Duke {
         output(input);
     }
 
+    /** 
+     * Adds new task to task list and outputs success message.
+     * 
+     * @param task Task to be added to task list.
+     */
+    private static void addTask(String task) {
+        tasks[numOfTasks] = task;
+        numOfTasks++;
+        output("added: " + task + "\n");
+    }
+
+    // Outputs all the tasks stored in task list.
+    private static void listTasks() {
+        String listOfTasks = "";
+        for(int idx = 0; idx < numOfTasks; idx++)
+            listOfTasks = listOfTasks + (idx + 1) + ". " + tasks[idx] + "\n";
+        output(listOfTasks);
+    }
+
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -39,11 +60,16 @@ public class Duke {
 
         while(true) {
             String input = sc.nextLine();
-            if (input.equals("bye"))
-                break;
-            echo(input);
+            switch(input) {
+                case "list":
+                    listTasks();
+                    break;
+                case "bye":
+                    exitMsg();
+                    return;
+                default:
+                    addTask(input);
+            }
         }
-
-        exitMsg();
     }
 }
