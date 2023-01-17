f58dd0063d2c46e58423c54017c66b73563e1de8
XylusChen <chenxinyang00@gmail.com>
2023-01-17 19:57:18 +0800

    changed greeting message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,16 @@
 public class Duke {
     public static void main(String[] args) {
+        /*
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+
         System.out.println("Hello from\n" + logo);
+        */
+
+        String greeting = "What's up! Duke here!\nHow can I be of assistance?";
+        System.out.println(greeting + "\n");
     }
 }
