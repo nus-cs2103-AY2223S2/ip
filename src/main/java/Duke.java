4cfb0243eaec3ecccc58aee8fda3a9886b142a47
Branda <brandaang@gmail.com>
2023-01-17 17:07:29 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +7,20 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("Hiii! I'm panpan\nWhat's up?");
+        echo();
+    }
+    public static void echo() {
+        Scanner input = new Scanner(System.in);
+        String cmd;
+        while (true) {
+            cmd = input.nextLine();
+            if (cmd.equals("bye")) {
+                System.out.println("Buhbyeee, hope to see you again soon <3");
+                return;
+            } else {
+                System.out.println(cmd);
+            }
+        }
     }
 }
