06aecd2ce31dba94403eeaf14fd2116e5d2de797
LimJiaYan <jiayan.yanyan@gmail.com>
2023-01-17 19:12:07 +0800

    Lvl2: Implement add, list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,17 +3,31 @@ import com.sun.source.tree.WhileLoopTree;
 import java.util.Scanner;
 
 public class Duke {
+    private static String[] task = new String[100];
+    private static int numOfTask = 0;
     private static void greet() {
         System.out.println("\t____________________________________________________________");
         System.out.println("\tHello! I'm Duke\n" + "\tWhat can I do for you?");
         System.out.println("\t____________________________________________________________");
     }
-    private static void echo(String s) {
+    private static void add(String s) {
+        task[numOfTask] = s;
+        numOfTask++;
+
         System.out.println("\t____________________________________________________________");
-        System.out.println("\t" + s);
+        System.out.println("\tadded: " + s);
         System.out.println("\t____________________________________________________________");
     }
 
+    private static void list() {
+        System.out.println("\t____________________________________________________________");
+        for(int i = 0; i < numOfTask; i++) {
+            int tmp = i + 1;
+            String msg = tmp + ". " + task[i];
+            System.out.println("\t" + msg);
+        }
+        System.out.println("\t____________________________________________________________");
+    }
     private static void exit() {
         System.out.println("\t____________________________________________________________");
         System.out.println("\tBye. Hope to see you again soon!");
@@ -27,7 +41,11 @@ public class Duke {
         String input = scanner.nextLine();
 
         while (!input.equals("bye")) {
-            echo(input);
+            if(input.equals("list")) {
+                list();
+            } else {
+                add(input);
+            }
             input = scanner.nextLine();
         }
 
