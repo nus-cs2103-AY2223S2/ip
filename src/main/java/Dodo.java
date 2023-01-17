f8c72106011312efbea967c8df99c6303460c7a2
Huang Hao <huang_hao12@hotmail.com>
2023-01-17 16:52:18 +0800

    Update UI text and divider

--- src/main/java/Dodo.java
+++ src/main/java/Dodo.java
@@ -1,26 +1,27 @@
 import java.util.Scanner;
 public class Dodo {
+    private static final String DIVIDER = "________________________________\n";
+    private static final String LOGO =
+              " ____  _   _ ____  _   _ \n"
+            + "|  _ \\| | | |  _ \\| | | |\n"
+            + "| | | | | | | | | | | | |\n"
+            + "| |_| | |_| | |_| | |_| |\n"
+            + "|____/ \\___/|____/ \\___/\n";
+    private static final String GREETING = DIVIDER + "Hello from\n" + LOGO + "What can I do for you?\n" + DIVIDER;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String divider = "________________________";
-        String logo =
-                  " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("What can I do for you?");
-        System.out.println(divider);
+        System.out.println(GREETING);
         while (scanner.hasNext()) {
             String input = scanner.next();
             if (input.equals("bye")) {
+                System.out.println(DIVIDER);
                 System.out.println("Bye. Hope to see you again soon!");
-                System.out.println(divider);
+                System.out.println(DIVIDER);
                 break;
             }
+            System.out.println(DIVIDER);
             System.out.println(input);
-            System.out.println(divider);
+            System.out.println(DIVIDER);
         }
     }
 
