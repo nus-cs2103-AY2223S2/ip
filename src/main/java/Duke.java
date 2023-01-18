da2b99b935b5c8aa325e6d97f78649f0e7cbfda9
bryansendeavour <bryanojj@hotmail.com>
2023-01-18 12:00:24 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,25 @@
+import java.io.*;
 public class Duke {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String divider = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String welcome = "Welcome hooman!\n" +
+                "Wat u want today?\n";
+        System.out.println(divider + "Hello from\n" + logo + welcome + divider);
+        String in = br.readLine();
+        String bye = "bye";
+        while(!in.equalsIgnoreCase(bye)){
+            bw.write(divider + in + "\n" + divider + "\n");
+            bw.flush();
+            in = br.readLine();
+        }
+        bw.write(divider + "Ah..... okkkk lo nehmind. GO. BYE. :)\n" + "\n");
+        bw.flush();
     }
 }
