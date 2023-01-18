5c1c069b7f43d2830623ec20ddd72b4934a70dc8
Lee Jia Sheng <thegreyjs@gmail.com>
2023-01-18 23:54:06 +0800

    Implement mark task as done feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,18 +5,40 @@ public class Duke {
     private final static String bye = "bye";
     private final static String goodbyeMessage = "Bye. Hope to see you again soon!";
     private final static String list = "list";
-    private static ArrayList<String> tasks = new ArrayList<>();
+    private final static String mark = "mark";
+    private final static String unmark = "unmark";
+    private static ArrayList<Task> tasks = new ArrayList<>();
 
     private static void indentedPrintln(String message) {
-        String indentedMessage = "   " + message;
+        String indentedMessage = "     " + message;
         System.out.println(indentedMessage);
     }
 
     private static void list() {
+        indentedPrintln("Here are the tasks in your list:");
         int numOfTasks = tasks.size();
         for (int i = 1; i <= numOfTasks; i++) {
-            indentedPrintln(i + ". " + tasks.get(i - 1));
+            indentedPrintln(i + ".[" + tasks.get(i - 1).getStatusIcon() + "] "
+                    + tasks.get(i - 1));
         }
+    }
+
+    private static void mark(int taskNumber) {
+        Task task = tasks.get(taskNumber - 1);
+        task.markAsDone();
+        String message = "Nice! I've marked this task as done:";
+        indentedPrintln(message);
+        indentedPrintln("  [" + task.getStatusIcon() + "] "
+                + task);
+    }
+
+    private static void unmark(int taskNumber) {
+        Task task = tasks.get(taskNumber - 1);
+        task.markAsNotDone();
+        String message = "OK, I've marked this task as not done yet:";
+        indentedPrintln(message);
+        indentedPrintln("  [" + task.getStatusIcon() + "] "
+                + task);
     }
 
     public static void main(String[] args) {
@@ -30,13 +52,16 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
         while (!str.equals(bye)) {
-            if (!str.equals(list)) {
-                tasks.add(str);
-                indentedPrintln("added: " + str);
-            } else {
+            if (str.equals(list)) {
                 list();
+            } else if (str.substring(0, 4).equals(mark)) {
+                mark(Character.getNumericValue(str.charAt(5)));
+            } else if (str.substring(0, 6).equals(unmark)) {
+                unmark(Character.getNumericValue(str.charAt(7)));
+            } else {
+                tasks.add(new Task(str));
+                indentedPrintln("added: " + str);
             }
-
             System.out.println();
             str = sc.nextLine();
         }
