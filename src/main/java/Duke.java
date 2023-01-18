0d57d0b0c2f12607548b1498189cc07105126e51
Aarnav Srinivasan <aarnav.srinivasan@gmail.com>
2023-01-18 23:15:20 +0800

    Add Listing and Adding features

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,8 +4,31 @@ public class Duke {
 
     private static final String LINE = "------------------------------------------------------------";
 
+    private static final String[] TASKS = new String[100];
+    private static int taskIndex = 0;
+
+    private static void listTasks() {
+        if (taskIndex == 0) {
+            System.out.println("You do not have anything added to the list.");
+        } else {
+            System.out.println("Listing all...");
+            for (int i = 0; i < taskIndex; i++) {
+                System.out.println((i + 1) + ") " + TASKS[i]);
+            }
+        }
+    }
+
     private static void processCommand(String cmd) {
-        System.out.println("\nDuke:\n" + cmd);
+
+        if (cmd.equals("list")) {
+            listTasks();
+        } else {
+            TASKS[taskIndex] = cmd;
+            taskIndex++;
+
+            System.out.println("I have added '" + cmd + "' to the list.");
+        }
+
         System.out.println("\n" + LINE + "\n");
     }
 
@@ -17,8 +40,10 @@ public class Duke {
             System.out.print("You:\n");
             String cmd = sc.nextLine().toLowerCase();
 
+            System.out.println("\nDuke:");
+
             if (cmd.equals("bye")) {
-                System.out.println("\nDuke:\nBye. Have a nice day!\n");
+                System.out.println("Bye. Have a nice day!\n");
                 System.out.println(LINE);
                 break;
 
