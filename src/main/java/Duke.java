05ee900e9222e84a87a70e5db265c751b27ab8fb
Lim Diat Bbin <l_db_@hotmail.com>
2023-01-19 00:09:07 +0800

    Completed Level-5: Handle Errors

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,18 @@
 import java.util.Scanner;
 
 public class Duke {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws DukeException {
         Task[] inputs = new Task[100];
         int curr = 0;
 
+        System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?\n");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________\n");
 
         Scanner scanner = new Scanner(System.in);
         String currInput = scanner.nextLine();
+        System.out.println("____________________________________________________________");
 
         String[] splitStr = currInput.split(" ", 2);
         String type = splitStr[0];
@@ -18,29 +21,39 @@ public class Duke {
             boolean isMarkTask = type.equals("mark") || type.equals("unmark");
             boolean isSuppTask = type.equals("todo") || type.equals("deadline") || type.equals("event");
 
+            try {
+                checkIfValid(type);
+            } catch (DukeException e) {
+                System.out.println(e);
+            }
+
             if(currInput.equals("list")) {
                 System.out.println("Here are the tasks in your list:");
                 for(int i = 0; i < curr; i++) {
                     System.out.println((i + 1) + "." + inputs[i]);
                 }
             } else if(isMarkTask) {
-                Task taskToMark = inputs[Integer.parseInt(splitStr[1]) - 1];
-                mark(type, taskToMark);
-                System.out.println(taskToMark + "\n");
+                    Task taskToMark = inputs[Integer.parseInt(splitStr[1]) - 1];
+                    mark(type, taskToMark);
+                    System.out.println(taskToMark);
             } else if(isSuppTask) {
-                addSuppTask(type, inputs, curr, splitStr[1]);
-                System.out.println("Got it. I've added this task:");
-                System.out.println(inputs[curr++]);
-                System.out.println("Now you have " + Task.getCount() + " tasks in the list.\n");
-            } else {
-                System.out.println("added: " + currInput + "\n");
-                inputs[curr++] = new Task(currInput);
+                try {
+                    addSuppTask(type, inputs, curr, splitStr);
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(inputs[curr++]);
+                    System.out.println("Now you have " + Task.getCount() + " tasks in the list.");
+                } catch(DukeException e) {
+                    System.out.println(e);
+                }
             }
+            System.out.println("____________________________________________________________\n");
             currInput = scanner.nextLine();
             splitStr = currInput.split(" ", 2);
             type = splitStr[0];
+            System.out.println("____________________________________________________________");
         }
         System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________\n");
     }
 
     public static void mark(String type, Task taskToMark) {
@@ -53,7 +66,12 @@ public class Duke {
         }
     }
 
-    public static void addSuppTask(String type, Task[] inputs, int curr, String task) {
+    public static void addSuppTask(String type, Task[] inputs, int curr, String[] splitstr) throws DukeException {
+        if(splitstr.length == 1) {
+            throw new DukeException(String.format("The description of a %s cannot be empty.", splitstr[0]));
+        }
+
+        String task = splitstr[1];
         if(type.equals("todo")) {
             inputs[curr] = new Todo(task);
         } else if(type.equals("deadline")) {
@@ -67,6 +85,21 @@ public class Duke {
             String from = arr[1].substring(5).trim();
             String to = arr[2].substring(3);
             inputs[curr] = new Event(desc, from, to);
+        }
+    }
+
+    public static void checkIfValid(String currType) throws DukeException {
+        String[] validTypes = new String[]{"list", "mark", "unmark", "todo", "deadline", "event"};
+        boolean valid = false;
+
+        for(String type : validTypes) {
+            if(currType.equals(type)) {
+                valid = true;
+            }
+        }
+
+        if(!valid) {
+            throw new DukeException("I'm sorry, but I don't know what that means :-(");
         }
     }
 }
