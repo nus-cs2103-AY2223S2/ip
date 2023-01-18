50de2b81f842cb706a37f4cef09a4e83209dff08
Vaidyanaath Suresh <svaidyanaath@gmail.com>
2023-01-18 23:31:16 +0800

    Add print task-list and add task features

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,17 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 public class Duke {
-    private static final String logo = " ____        _        \n"
+    private static final String LOGO = " ____        _        \n"
             + "|  _ \\ _   _| | _____ \n"
             + "| | | | | | | |/ / _ \\\n"
             + "| |_| | |_| |   <  __/\n"
             + "|____/ \\__,_|_|\\_\\___|\n";
+
+    private static final List<String> TASK_LIST =  new ArrayList<>();
+    private static int nTasks = 0;
 
     private static void greet() {
         String greeting = "____________________________________________________________\n"
                 + "Hello! I'm Duke\n"
                 + "What can I do for you?\n"
                 + "____________________________________________________________\n";
-        System.out.println(logo);
+        System.out.println(LOGO);
         System.out.println(greeting);
     }
 
@@ -22,8 +27,26 @@ public class Duke {
         System.out.println(exitMessage);
     }
 
+    private static void addTask(String task) {
+        TASK_LIST.add(task);
+        nTasks++;
+        System.out.println("New task added: " + task);
+    }
+
+    private static void printTaskList() {
+        for (int i = 0; i < nTasks; i++) {
+            System.out.println((i + 1) + ". " + TASK_LIST.get(i));
+        }
+    }
+
     private static void processCommand(String command) {
-        System.out.println(command + "\n");
+        if (command.equals("list")) {
+            printTaskList();
+        } else {
+            addTask(command);
+        }
+
+        System.out.print("\n");
     }
 
     public static void start() {
@@ -31,6 +54,7 @@ public class Duke {
 
         Scanner scanner = new Scanner(System.in);
         while (true) {
+            System.out.print("$ ");
             String input = scanner.nextLine();
             if (input.equals("bye")) {
                 Duke.exit();
