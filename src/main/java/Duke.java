3de2262fe0fe8838710488a6d48af489e5c26ab9
PawanPatil19 <pawankp1903@gmail.com>
2023-01-19 02:00:33 +0800

    Added level-1 increment

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
 public class Duke {
+    private Scanner sc = new Scanner(System.in);
+
+    private void inputResponse(String userInput){
+        if(userInput.equals("bye")) {
+            System.out.println("\tBye. Hope to see you again soon!");
+            return;
+        } else {
+            System.out.println("\t" + userInput);
+            String input = sc.nextLine();
+            inputResponse(input);
+        }
+    }
+
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke dukeObj = new Duke();
+
+        System.out.println("Welcome! I'm Duke.");
+        System.out.println("What can I do for you?");
+
+        String input = dukeObj.sc.nextLine();
+        dukeObj.inputResponse(input);
     }
 }
