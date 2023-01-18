e0388a9d8eea5ac522bc95bc2cf6f5517183e70f
alextang809 <alextang809@gmail.com>
2023-01-19 01:06:50 +0800

    add method to print TaskList

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -10,4 +10,14 @@ public class Task {
     public static void printHorizontalLine() {
         System.out.println(HORIZONTAL_LINE);
     }
+
+    public static void printTaskList(Task[] taskList) {
+        Task.printHorizontalLine();
+        System.out.println("     Here are the tasks in your list:");
+        for (int i = 0; i < Task.getTotalNumberOfTask(); i++) {
+            System.out.println("     " + (i+1) + " " + taskList[i]);
+        }
+        Task.printHorizontalLine();
+    }
+
 }
