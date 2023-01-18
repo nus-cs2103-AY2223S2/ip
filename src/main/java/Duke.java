0cfee538c4b3316303f9356da0ff91f641056d29
ScorpiusSigma <justinleng_@hotmail.com>
2023-01-18 15:41:11 +0800

    Level-1: Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,36 @@
+import java.util.Scanner;
+
 public class Duke {
+    static Scanner in = new Scanner(System.in);
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greetings();
+
+        while (true) {
+            String input = getUserInput();
+            if (input.equals("bye"))
+                break;
+
+            echo(input);
+        }
+
+        bye();
+    }
+
+    public static void greetings() {
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+    }
+
+    public static void bye() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    public static void echo(String input) {
+        System.out.println(input);
+    }
+
+    public static String getUserInput() {
+            String userInput = in.nextLine();
+            return userInput.toLowerCase();
     }
 }
