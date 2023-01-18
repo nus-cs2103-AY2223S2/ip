1f1dc4939b45447161ef7872e9a04c4566198b87
TabrizPlv <tabrizpahlavi@gmail.com>
2023-01-19 03:43:59 +0800

    Added ability to store input

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,22 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
+        ArrayList<String> tasks = new ArrayList<>();
         Scanner myObj = new Scanner(System.in);
         System.out.println("Hey! This is Duke at your service!");
         while(myObj.hasNext()) {
             String input = myObj.nextLine();
-            if(input.equals("bye")) {
+            if (input.equals("list")) {
+                tasks.forEach(x -> System.out.println(x));
+                continue;
+            }
+            tasks.add(input);
+            if (input.equals("bye")) {
                 System.out.println("See you again, thanks for visiting!");
                 break;
             }
             System.out.println(input);
         }
-
     }
 }
