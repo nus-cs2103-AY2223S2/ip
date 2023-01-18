b90c391d85e995ceb60f805aa6bf28052d728f38
RubyNguyen07 <rubynguyen2003@gmail.com>
2023-01-18 09:55:14 +0700

    Change Duke file so that Duke can repeat and exit when needed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Duke {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        String str;
+        while ((str = bf.readLine()) != null) {
+            if (str.toLowerCase().equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+            System.out.println(str);
+        }
+
+        bf.close();
     }
 }
