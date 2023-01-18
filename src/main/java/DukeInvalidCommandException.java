474375472da3c8fcf2faed18757d65dc9a0f0119
SeeuSim <seeusimong@gmail.com>
2023-01-18 08:30:24 +0800

    A-Exceptions

---
+++ src/main/java/DukeInvalidCommandException.java
@@ -0,0 +1,5 @@
+public class DukeInvalidCommandException extends DukeException{
+    public DukeInvalidCommandException() {
+        super("I don't know that command yet. Give me another!");
+    }
+}
