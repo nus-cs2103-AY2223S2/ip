dc82a99e2194b0b2cc5654e547de7df1ef96dbe2
chunzkok <kokcz@comp.nus.edu.sg>
2023-01-18 19:36:54 +0800

    Fix newly added Exceptions

--- src/main/java/ClippyMissingDeadlineException.java
+++ src/main/java/ClippyMissingDeadlineException.java
@@ -1,6 +1,6 @@
-public class ClippyMissingDeadlineException extends ClippyException{
+public class ClippyMissingDeadlineException extends ClippyException {
     public ClippyMissingDeadlineException() {
-        super("deadline cannot be recognised - please specify the deadline as such: \n"
+        super("deadline cannot be recognised - please specify the deadline as such: \n" +
                 "deadline <description> /by <deadline>");
     }
 }
