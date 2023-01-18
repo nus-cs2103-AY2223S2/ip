c2d8d9b2836bca672dbe64af089921f5af952145
kennethk-1201 <kiangjiaming@gmail.com>
2023-01-19 00:31:34 +0800

    Finished Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -22,8 +22,26 @@ public class Duke {
                     System.out.println(tl.toString());
                     break;
                 default:
-                    tl.addTask(input);
-                    System.out.println("added: " + input);
+                    if (input.matches("mark \\d+")) {
+                        Integer idx = Integer.valueOf(input.split(" ")[1]) - 1;
+                        if (idx < 0 || idx >= tl.numberOfTasks()) {
+                            System.out.println("Task number is invalid!");
+                            continue;
+                        }
+                        tl.markTask(idx);
+                        System.out.println("Nice! I've marked this task as done:\n" + tl.getTask(idx));
+                    } else if (input.matches("unmark \\d+")) {
+                        Integer idx = Integer.valueOf(input.split(" ")[1]) - 1;
+                        if (idx < 0 || idx >= tl.numberOfTasks()) {
+                            System.out.println("Task number is invalid!");
+                            continue;
+                        }
+                        tl.unmarkTask(idx);
+                        System.out.println("OK, I've marked this task as not done yet:\n" + tl.getTask(idx));
+                    } else {
+                        tl.addTask(input);
+                        System.out.println("added: " + input);
+                    }
             }
         }
     }
--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -2,14 +2,54 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class TaskList {
-    private List<String> tasks;
+    private class Task {
+        private String content;
+        private boolean done;
+
+        Task(String content) {
+            this.content = content;
+            this.done = false;
+        }
+
+        public void mark() {
+            this.done = true;
+        }
+
+        public void unmark() {
+            this.done = false;
+        }
+
+        @Override
+        public String toString() {
+            String checkBox = this.done ? "[X] " : "[ ] ";
+            return checkBox +  this.content;
+        }
+    }
+
+    private List<Task> tasks;
 
     TaskList() {
         this.tasks = new LinkedList<>();
     }
 
     public void addTask(String task) {
-        this.tasks.add(task);
+        this.tasks.add(new Task(task));
+    }
+
+    public void markTask(int i) {
+        tasks.get(i).mark();
+    }
+
+    public void unmarkTask(int i) {
+        tasks.get(i).unmark();
+    }
+
+    public Task getTask(int i) {
+        return tasks.get(i);
+    }
+
+    public int numberOfTasks() {
+        return tasks.size();
     }
 
     @Override
@@ -17,8 +57,8 @@ public class TaskList {
         String result = "";
         Integer curr = 1;
 
-        for (String s: tasks) {
-            result += curr.toString() + ". " + s + '\n';
+        for (Task t: tasks) {
+            result += curr.toString() + ". " + t + '\n';
             curr++;
         }
 
