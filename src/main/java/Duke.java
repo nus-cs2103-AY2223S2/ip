44548af806f0b28515f568a7723474e7b1e1ec70
ZHTang29 <e0725226@u.nus.edu>
2023-01-18 10:57:51 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,31 @@
+import java.util.Scanner;
+
 public class Duke {
+    public String greetUser() {
+        return "Hello! I'm Duke" + "\n" + "What can I do for you?";
+    }
+
+    public String farewellUser() {
+        return "Bye. Hope to see you again soon!";
+    }
+
+    public void echoUser(String userInput) {
+        System.out.println(userInput);
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke duke = new Duke();
+        String greeting = duke.greetUser();
+        System.out.println(greeting);
+        Scanner sc = new Scanner(System.in);
+        String userInput = sc.next();
+
+        while(!userInput.equals("bye")) {
+            duke.echoUser(userInput);
+            userInput = sc.next();
+        }
+
+        String farewell = duke.farewellUser();
+        System.out.println(farewell);
     }
 }
