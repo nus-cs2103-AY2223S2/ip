cac0614213282878d7ab139e565dc74bb210267c
kayyenl <yenyenlee9@gmail.com>
2023-01-18 16:10:12 +0900

    Level 1 done.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,27 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String greeting = formatStr("Hello! I'm Muse\n"
+                + "What can I do for you?");
+        System.out.println(greeting);
+        Scanner sc = new Scanner(System.in);
+
+
+        String input = sc.nextLine();
+        while (!input.equals("bye")) {
+            System.out.println(formatStr(input));
+            input = sc.nextLine();
+        }
+        String goodbyeMessage = formatStr("Bye. Come back again!");
+        System.out.println(goodbyeMessage);
+    }
+
+    public static String formatStr(String str) {
+        String returnstr =  "############################\n"
+                            + str + "\n"
+                            + "############################";
+        return returnstr;
     }
 }
+
