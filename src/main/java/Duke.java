26f93254b5de872f36b970dab681a4c05fdf6fd5
Isabel Chong <IsabelChong.CYJI@gmail.com>
2023-01-18 15:26:32 +0800

    Duke.java: added in skeleton for DukeException class.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,15 @@
 import java.util.*;
 import java.io.*;
 
+/**
+ * DukeException handles exceptions specific to Duke
+ */
+class DukeException extends Exception {
+    public DukeException(String errorMessage) {
+        super(errorMessage);
+    }
+}
+
 public class Duke {
     public static String TAB = "    ";
     public static String HOR_BAR = "✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦";
@@ -8,12 +17,71 @@ public class Duke {
     /**
      * This function is called at the beginning to greet the user.
      */
-    public static void greeting() {
+    public static void greetingDialogue() {
         System.out.println(TAB + HOR_BAR);
         String logo = TAB + "૮ ˶ᵔ ᵕ ᵔ˶ ა";
         System.out.println(TAB + "Hey there! I'm Berry the Bunny~\n" + logo + "\n"
                 + "    What are you looking to plan today?");
         System.out.println(TAB + HOR_BAR);
+    }
+
+    /**
+     * This function is called at the end to see the user off.
+     */
+    public static void byeDialogue() {
+        System.out.println(TAB + HOR_BAR);
+        System.out.println(TAB + "Bye! Please come back again ૮꒰˶• ༝ •˶꒱ა");
+        System.out.println(TAB + HOR_BAR);
+    }
+
+    public static String cutDescription(int taskType, String input) throws DukeException {
+        try {
+            if (taskType == 1) return input.substring(5);
+            else if (taskType == 2) return input.substring(9);
+            else return input.substring(6);
+        } catch (StringIndexOutOfBoundsException e) {
+            throw new DukeException(unreadableMessage());
+        }
+    }
+
+    /**
+     * This function prints a out of range message.
+     */
+    public static String outOfRangeMessage() {
+        String message =
+                Duke.TAB + Duke.HOR_BAR + "\n" +
+                        Duke.TAB + "Oh no! I cannot find a task with that task number.\n" +
+                        Duke.TAB + "You can check them again by asking me to 'list'\n"+
+                        Duke.TAB + Duke.HOR_BAR;
+        return message;
+    }
+
+    /**
+     * This function prints a out of range message.
+     */
+    public static String unreadableMessage() {
+        String message = Duke.TAB + Duke.HOR_BAR + "\n" +
+                Duke.TAB + "Aww... ''૮₍  ˶•⤙•˶ ₎ა  I haven't learnt enough to do this.\n" +
+                Duke.TAB + "Try these commands instead:\n" +
+
+                Duke.TAB + "ADDING TASKS\n" +
+                Duke.TAB + Duke.TAB + "todo yourTaskName " +
+                                    "- Adds a todo task into your task list.\n"+
+                Duke.TAB + Duke.TAB + "deadline yourTaskName /by yourDeadline " +
+                                    "- Adds a deadline task into your task list with the given deadline.\n"+
+                Duke.TAB + Duke.TAB + "event yourTaskName /from eventStartTime /to eventEndTime " +
+                                    "- Adds an event into your task list with given start and end time.\n"+
+
+                Duke.TAB + "MARKING TASKS\n" +
+                Duke.TAB + Duke.TAB + "mark taskNumber " +
+                "- Marks the task given my task number from the list as done.\n"+
+                Duke.TAB + Duke.TAB + "unmark taskNumber " +
+                "- Marks the task given my task number from the list as not done.\n"+
+
+                Duke.TAB + "LISTING TASKS\n" +
+                Duke.TAB + Duke.TAB + "list - lists out all your tasks\n"+
+                Duke.TAB + Duke.HOR_BAR + "\n";
+        return message;
     }
 
     public static void main(String[] args) throws IOException {
@@ -24,64 +92,64 @@ public class Duke {
         /* Initialise endFlag */
         boolean endFlag = false;
 
+        greetingDialogue();
+
         try {
-           greeting();
-           /* While user has not exited application */
-           while (!endFlag) {
-               String input = br.readLine();
-               String[] splitInput = input.split(" ");
-               String command = splitInput[0];
-               int index;
+            while (!endFlag) {
+                try {
+                    /* While user has not exited application */
+                    String input = br.readLine();
+                    String[] splitInput = input.split(" ");
+                    String command = splitInput[0];
+                    int index;
 
-               /* Handle commands */
-               switch (command) {
-                   case "bye":
-                       System.out.println(TAB + HOR_BAR);
-                       System.out.println(TAB + "Bye! Please come back again ૮꒰˶• ༝ •˶꒱ა");
-                       System.out.println(TAB + HOR_BAR);
-                       endFlag = true;
-                       break;
-                   case "list":
-                       tb.listTasks();
-                       break;
-                   case "mark":
-                       index = Integer.parseInt(splitInput[1]);
-                       if (tb.indexWithinRange(index)) {
-                           tb.markDone(index);
-                       } else {
-                           tb.printOutOfRangeDialogue();
-                       }
-                       break;
-                   case "unmark":
-                       index = Integer.parseInt(splitInput[1]);
-                       if (tb.indexWithinRange(index)) {
-                           tb.markNotDone(index);
-                       } else {
-                           tb.printOutOfRangeDialogue();
-                       }
-                       break;
-                   default: // types of tasks
-                       int taskType;
-                       String description;
+                    /* Handle commands */
+                    switch (command) {
+                        case "bye":
+                            byeDialogue();
+                            endFlag = true;
+                            break;
+                        case "list":
+                            tb.listTasks();
+                            break;
+                        case "mark":
+                            index = Integer.parseInt(splitInput[1]);
+                            if (tb.indexWithinRange(index)) {
+                                tb.markDone(index);
+                            } else {
+                                throw new DukeException(outOfRangeMessage());
+                            }
+                            break;
+                        case "unmark":
+                            index = Integer.parseInt(splitInput[1]);
+                            if (tb.indexWithinRange(index)) {
+                                tb.markNotDone(index);
+                            } else {
+                                throw new DukeException(outOfRangeMessage());
+                            }
+                            break;
+                        default: // types of tasks
+                            int taskType = 0;
+                            String description = " ";
 
-                       if (command.equals("todo")) {
-                           description = input.substring(5);
-                           taskType = 1; // todo
-                       } else if (command.equals("deadline")) {
-                           description = input.substring(9);
-                           taskType = 2; // deadline
-                       } else if (command.equals("event")) {
-                           description = input.substring(6);
-                           taskType = 3; // event
-                       } else {
-                           description = input;
-                           taskType = 0;
-                       }
+                            if (command.equals("todo")) {
+                                taskType = 1; // todo
+                            } else if (command.equals("deadline")) {
+                                taskType = 2; // deadline
+                            } else if (command.equals("event")) {
+                                taskType = 3; // event
+                            } else {
+                                throw new DukeException(unreadableMessage());
+                            }
 
-                       tb.addTask(taskType, description);
-                       break;
-               }
-           }
+                            description = cutDescription(taskType, input);
+                            tb.addTask(taskType, description);
+                            break;
+                    }
+                } catch (DukeException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
         } finally {
             /* Close the BufferedReader */
             br.close();
@@ -114,20 +182,25 @@ class TaskBook {
      * @param taskType
      * @param description
      */
-    public void addTask(int taskType, String description) {
+    public void addTask(int taskType, String description) throws DukeException {
         numOfTasks++;
         Task t;
-        if (taskType == 1)  //todo
-            t = new Todo(description);
-        else if (taskType == 2) // deadline
-            t = new Deadline(description.split("/")[0],
-                    description.split("/by")[1]);
-        else if (taskType == 3)// event
-            t = new Event(description.split("/")[0],
-                    description.split("/from")[1].split("/to")[0],
-                    description.split("/to")[1]);
-        else
-            t = new Task(description);
+
+        try {
+            if (taskType == 1)  //todo
+                t = new Todo(description);
+            else if (taskType == 2) // deadline
+                t = new Deadline(description.split("/")[0],
+                        description.split("/by")[1]);
+            else if (taskType == 3)// event
+                t = new Event(description.split("/")[0],
+                        description.split("/from")[1].split("/to")[0],
+                        description.split("/to")[1]);
+            else
+                t = new Task(description);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new DukeException(Duke.unreadableMessage());
+        }
 
         listOfTasks.add(t);
 
@@ -173,15 +246,6 @@ class TaskBook {
 
     public void printNumberOfTasks() {
         System.out.println(Duke.TAB + "You now have " + numOfTasks + " tasks in the list ૮꒰ˊᗜˋ* ꒱ა");
-    }
-
-    /** This function prints a out of range dialogue.
-     */
-    public void printOutOfRangeDialogue() {
-        System.out.println(Duke.TAB + Duke.HOR_BAR);
-        System.out.println(Duke.TAB + "Oh no! I cannot find a task with that task number.");
-        System.out.println(Duke.TAB + "You can check them again by asking me to 'list'");
-        System.out.println(Duke.TAB + Duke.HOR_BAR);
     }
 }
 
@@ -230,6 +294,9 @@ class Task {
     }
 }
 
+/**
+ * This is a general to-do task.
+ */
 class Todo extends Task {
     public Todo(String description) {
         super(description);
@@ -241,6 +308,9 @@ class Todo extends Task {
     }
 }
 
+/**
+ * This is a task enclosing a deadline.
+ */
 class Deadline extends Task {
     protected String by;
 
@@ -255,6 +325,9 @@ class Deadline extends Task {
     }
 }
 
+/**
+ * This is a task enclosing an event with a from and end time or date.
+ */
 class Event extends Task {
     protected String from;
     protected String to;
@@ -270,4 +343,3 @@ class Event extends Task {
         return "[E]" + super.toString() + " (from:" + from + " to:" + to +")";
     }
 }
-
