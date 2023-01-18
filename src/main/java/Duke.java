7af547581f02973ebb3e83e8e89ea9e9cb3bae2d
homuntan02 <homuntan02@gmail.com>
2023-01-18 23:04:03 +0800

    IP Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,31 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String intro = "  ________________________________\n"
+                + "  Hello! I'm Mark\n"
+                + "  What can I do for you?\n"
+                + "  ________________________________\n";
+        System.out.println(intro);
+
+        Scanner sc= new Scanner(System.in);
+        while(true){
+            String str= sc.nextLine();
+
+            if(str.equals("bye")){
+                break;
+            }
+
+            String reply = "  ________________________________\n"
+                    + "  " + str + "\n"
+                    + "  ________________________________\n";
+            System.out.print(reply);
+        }
+
+        String bye = "  ________________________________\n"
+                + "  Bye! have a great day\n"
+                + "  ________________________________\n";
+        System.out.println(bye);
+
     }
 }
