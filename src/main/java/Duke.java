f27c543b06d294fe70a3291cb4182557ebf835af
dunliang0513 <dunliang014@gmail.com>
2023-01-18 19:02:23 +0800

    Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,19 +2,39 @@ import java.util.Scanner;
 
 public class Duke {
     static String[] inputList = new String[100];
+    static Boolean[] doneList = new Boolean[100];
     static int index = 0;
 
     public static void add(String input) {
         inputList[index] = input;
+        doneList[index] = false;
         index++;
+    }
+    public static void mark(int input) {
+        doneList[input - 1] = true;
+        System.out.println("\t--------------------------");
+        System.out.println("\tNice! I've marked this task as done:");
+        System.out.println("\t[X] " + inputList[input - 1]);
+        System.out.println("\t--------------------------");
+    }
+
+    public static void unmark(int input) {
+        doneList[input - 1] = false;
+        System.out.println("\t--------------------------");
+        System.out.println("\tOK, I've marked this task as not done yet:");
+        System.out.println("\t[ ] " + inputList[input - 1]);
+        System.out.println("\t--------------------------");
     }
 
     public static void list() {
         System.out.println("\t--------------------------");
+        System.out.println("\tHere are the tasks in your list:");
         for (int i = 0; i < index; i++) {
-
-            System.out.println("\t" + (i+1) + ". " + inputList[i]);
-
+            if (doneList[i]) {
+                System.out.println("\t" + (i+1) + ". [X] " + inputList[i]);
+            } else {
+                System.out.println("\t" + (i+1) + ". [ ] " + inputList[i]);
+            }
         }
         System.out.println("\t--------------------------");
     }
@@ -35,20 +55,32 @@ public class Duke {
         String input;
         Scanner scan = new Scanner(System.in);
         input = scan.nextLine();
+        String[] splitInput = input.split(" ");
         while (true) {
-            if (input.equals("bye")) {
+            if (splitInput[0].equals("bye")) {
                 Duke.bye();
                 break;
-            } else if (input.equals("list")) {
+            } else if (splitInput[0].equals("list")) {
                 Duke.list();
                 input = scan.nextLine();
+                splitInput = input.split(" ");
+            } else if (splitInput[0].equals("mark")) {
+                Duke.mark(Integer.parseInt(splitInput[1]));
+                input = scan.nextLine();
+                splitInput = input.split(" ");
+            } else if (splitInput[0].equals("unmark")) {
+                Duke.unmark(Integer.parseInt(splitInput[1]));
+                input = scan.nextLine();
+                splitInput = input.split(" ");
             } else {
                 Duke.add(input);
                 System.out.println("\t--------------------------");
                 System.out.println("\tadded: " + input);
                 System.out.println("\t--------------------------");
                 input = scan.nextLine();
+                splitInput = input.split(" ");
             }
         }
+
     }
 }
