511d6504e925f5949084025d7bc207a188356316
Chu Yi Ting <ytyt3902@outlook.com>
2023-01-19 02:44:51 +0800

    added Deadline, Event, ToDo as subclasses of Task converted Task to abstract class

---
+++ src/main/java/Event.java
@@ -0,0 +1,15 @@
+public class Event extends Task {
+    String start;
+    String end;
+
+    Event(String description, String start, String end) {
+        super(description);
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[E]%s (from: %s to: %s)", super.toString(), start, end);
+    }
+}
--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -7,9 +7,27 @@ public class TaskList {
         taskList = new ArrayList<Task>();
     }
 
-    String addTask(String description) {
-        taskList.add(new Task(description));
-        return String.format("\tadded: %s\n", description);
+    String addTask(String command) {
+        String taskType = command.split(" ")[0];
+        String description; Task task;
+        if (taskType.equals("todo")) {
+            description = command.substring(5);
+            task = new ToDo(description);
+            taskList.add(task);
+        } else if (taskType.equals("deadline")) {
+            int doneByIndex = command.indexOf("/by");
+            description = command.substring(9, doneByIndex - 1);
+            String doneBy = command.substring(doneByIndex + 4);
+            task = new Deadline(description, doneBy);
+            taskList.add(task);
+        } else {
+            int startIndex = command.indexOf("/from"), endIndex = command.indexOf("/to");
+            description = command.substring(6, startIndex - 1);
+            String start = command.substring(startIndex + 6, endIndex - 1), end = command.substring(endIndex + 4);
+            task = new Event(description, start, end);
+            taskList.add(task);
+        }
+        return addTaskText(task);
     }
 
     String markTask(int taskNum) {
@@ -22,6 +40,14 @@ public class TaskList {
         Task task = taskList.get(taskNum - 1);
         task.undoTask();
         return String.format("\tOK, I've unmarked this task as not done yet:\n\t\t%s\n", task);
+    }
+
+    String addTaskText(Task task) {
+        return String.format("\tGot it. I've added this task:\n\t\t%s\n\tNow you have %s task%s in the list.\n",
+                task.toString(), getNumOfTasks(), getNumOfTasks() == 1 ? "" : "s");
+    }
+    int getNumOfTasks() {
+        return taskList.size();
     }
 
     @Override
