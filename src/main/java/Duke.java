cf77bb7f67f4651370a274e32081aad0a4c7346e
HJ-PRK <e0902027@u.nus.edu>
2023-01-18 19:59:24 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,15 @@
 import java.util.*;
 public class Duke {
+    public static class task{
+        int id = 0;
+        String todo;
+        String isDone = "";
+        public task(int id, String todo, String isDone) {
+            this.id = id;
+            this.todo = todo;
+            this.isDone = isDone;
+        }
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String logo = " ____        _        \n"
@@ -16,19 +26,37 @@ public class Duke {
 
         String comm = "";
 
-        List<String> list = new ArrayList<>();
+        List<task> tasklist = new ArrayList<>();
 
         while(!comm.equals("bye")) {
             comm = sc.nextLine();
             if(comm.equals("list")) {
                 System.out.println(line);
-                for(int i=0; i < list.size(); i++) {
-                    int num = i+1;
-                    System.out.println(num + ". " + list.get(i) );
+                System.out.println("Here are the tasks in your list:");
+                for(int i=0; i < tasklist.size(); i++) {
+                    System.out.println(tasklist.get(i).id + ".[" + tasklist.get(i).isDone + "] "  + tasklist.get(i).todo );
                 }
                 System.out.println(line);
+            } else if(comm.startsWith("mark")) {
+                String str = comm.substring(comm.length() -1);
+                int marking = Integer.parseInt(str);
+                task current = tasklist.get(marking-1);
+                current.isDone = "X";
+                System.out.println(line);
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("[" +current.isDone+ "] " + current.todo);
+                System.out.println(line);
+            } else if(comm.startsWith("unmark")) {
+                String str = comm.substring(comm.length() -1);
+                int marking = Integer.parseInt(str);
+                task current = tasklist.get(marking-1);
+                current.isDone = " ";
+                System.out.println(line);
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("[" +current.isDone+ "] " + current.todo);
+                System.out.println(line);
             } else {
-                list.add(comm);
+                tasklist.add(new task(tasklist.size()+1, comm, " "));
                 System.out.println(line);
                 System.out.println("added: " + comm);
                 System.out.println(line);
