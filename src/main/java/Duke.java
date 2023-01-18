397b6156b5f56b02649189de983e483cdc8fc354
chongweiguan <75872136+chongweiguan@users.noreply.github.com>
2023-01-18 22:12:58 +0800

    this is another test

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,6 +6,6 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);*/
-        System.out.println("This is a test");
+        System.out.println("This is a test, another test");
     }
 }
