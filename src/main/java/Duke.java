2dac9dcc8bc36f36750136460d15b7a69cfd8b55
slackernoob <maxang12@gmail.com>
2023-01-18 19:51:37 +0800

    added Level-4: ToDo, Event, Deadline

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -28,7 +28,8 @@ public class Duke {
                 status = false;
             } else if (curInput.equals("list")) {
                 System.out.println(dashes);
-                System.out.println("Here are the tasks in your list:");
+                System.out.println(spacingIndent
+                        + "Here are the tasks in your list:");
                 for (int i = 0; i < storage.size(); i++) {
                     int number = i + 1;
                     System.out.println(spacingIndent + " "
@@ -36,26 +37,68 @@ public class Duke {
                 }
                 System.out.println(dashes);
             } else if (curInput.startsWith("mark")) {
-                int targetIndex = Integer.parseInt(curInput.substring(5)) - 1;
-                storage.set(targetIndex, storage.get(targetIndex).markAsDone());
+                int targetIndex = Integer.parseInt(
+                        curInput.substring(5)) - 1;
+                storage.set(targetIndex,
+                        storage.get(targetIndex).markAsDone());
                 System.out.println(dashes);
-                System.out.println(spacingIndent + "Nice! I've marked this task as done:");
-                System.out.println(spacingIndent + "   " + storage.get(targetIndex));
+                System.out.println(spacingIndent
+                        + "Nice! I've marked this task as done:");
+                System.out.println(spacingIndent
+                        + "   " + storage.get(targetIndex));
                 System.out.println(dashes);
             } else if (curInput.startsWith("unmark")) {
-                int targetIndex = Integer.parseInt(curInput.substring(7)) - 1;
-                storage.set(targetIndex, storage.get(targetIndex).markAsDone());
+                int targetIndex = Integer.parseInt(
+                        curInput.substring(7)) - 1;
+                storage.set(targetIndex,
+                        storage.get(targetIndex).markAsDone());
                 System.out.println(dashes);
-                System.out.println(spacingIndent + "OK, I've marked this task as not done yet:");
-                System.out.println(spacingIndent + "   " + storage.get(targetIndex));
+                System.out.println(spacingIndent
+                        + "OK, I've marked this task as not done yet:");
+                System.out.println(spacingIndent
+                        + "   " + storage.get(targetIndex));
+                System.out.println(dashes);
+            } else if (curInput.startsWith("todo")) {
+                Todo taskTodo = new Todo(curInput.substring(5));
+                storage.add(taskTodo);
+                System.out.println(dashes);
+                System.out.println(spacingIndent + " New task added:");
+                System.out.println(spacingIndent + "  " + taskTodo);
+                System.out.println(spacingIndent
+                        + " Now list has " + storage.size() + " tasks.");
+                System.out.println(dashes);
+            } else if (curInput.startsWith("deadline")) {
+                String desc = curInput.substring(9);
+                String[] descSplit = desc.split("/");
+                Deadline taskDeadline = new Deadline(
+                        descSplit[0].substring(0,descSplit[0].length()-1),
+                        descSplit[1].substring(3));
+                storage.add(taskDeadline);
+                System.out.println(dashes);
+                System.out.println(spacingIndent + " New task added:");
+                System.out.println(spacingIndent + "  " + taskDeadline);
+                System.out.println(spacingIndent
+                        + " Now list has " + storage.size() + " tasks.");
+                System.out.println(dashes);
+            } else if (curInput.startsWith("event")) {
+                String desc = curInput.substring(6);
+                String[] descSplit = desc.split("/");
+                String description = descSplit[0].substring(
+                        0,descSplit[0].length()-1);
+                String start = descSplit[1].substring(
+                        5,descSplit[1].length()-1);
+                String end = descSplit[2].substring(3);
+                Event taskEvent = new Event(description, start, end);
+                storage.add(taskEvent);
+                System.out.println(dashes);
+                System.out.println(spacingIndent + " New task added:");
+                System.out.println(spacingIndent + "  " + taskEvent);
+                System.out.println(spacingIndent
+                        + " Now list has " + storage.size() + " tasks.");
                 System.out.println(dashes);
             }
             else {
-                storage.add(new Task(curInput));
-                System.out.println(dashes);
-                System.out.println(spacingIndent
-                        + " " + "added: " + curInput);
-                System.out.println(dashes);
+                System.out.println("invalid input");
             }
         }
 
