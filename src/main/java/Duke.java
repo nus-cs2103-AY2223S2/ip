542c91a7b6c933d09d0fb2013b579d2f1342582d
chen-jerry-junior <jrchenjiarui@163.com>
2023-01-18 22:06:14 +0800

    level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,8 @@
+import java.util.Scanner;
 public class Duke {
+    /**
+     * A level 1 chat bot Duke.
+     */
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +10,14 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        Scanner ai = new Scanner(System.in);
+        String greetings = "Hello! I'm Duke" + "\nWhat can I do for you?";
+        System.out.println(greetings);
+        String input = ai.nextLine();
+        while (!input.equals("bye")) {
+            System.out.println(input);
+            input = ai.nextLine();
+        }
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
