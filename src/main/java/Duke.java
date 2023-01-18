6df5fabdf88ecf71592cfb728ae76b7a00ebc920
shittake <ericleowyuquan@gmail.com>
2023-01-18 14:54:59 +0800

    Add functionality to echo user input until user decides to end program

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -7,6 +9,16 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         introMessage();
+
+        Scanner scan = new Scanner(System.in);
+        String userInput = scan.nextLine();
+
+        while (!userInput.equals("bye")) {
+            System.out.println(userInput);
+            userInput = scan.nextLine();
+        }
+
+        System.out.println("Bye. Hope to see you again!");
     }
 
     public static void introMessage() {
