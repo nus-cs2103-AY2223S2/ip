d7a36883461569d6a48fb3290f9a2f151d4d146d
HJ-PRK <e0902027@u.nus.edu>
2023-01-18 21:05:42 +0800

    Level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,18 @@
 import java.util.*;
+import java.lang.*;
 public class Duke {
     public static class task{
+        char type;
         int id = 0;
         String todo;
         String isDone = "";
-        public task(int id, String todo, String isDone) {
+        String info;
+        public task(char type, int id, String todo, String isDone, String info) {
+            this.type = type;
             this.id = id;
             this.todo = todo;
             this.isDone = isDone;
+            this.info = info;
         }
     }
     public static void main(String[] args) {
@@ -34,7 +39,8 @@ public class Duke {
                 System.out.println(line);
                 System.out.println("Here are the tasks in your list:");
                 for(int i=0; i < tasklist.size(); i++) {
-                    System.out.println(tasklist.get(i).id + ".[" + tasklist.get(i).isDone + "] "  + tasklist.get(i).todo );
+                    task current = tasklist.get(i);
+                    System.out.println(current.id + ".[" +current.type+ "][" + current.isDone + "] "  + current.todo );
                 }
                 System.out.println(line);
             } else if(comm.startsWith("mark")) {
@@ -55,8 +61,33 @@ public class Duke {
                 System.out.println("OK, I've marked this task as not done yet:");
                 System.out.println("[" +current.isDone+ "] " + current.todo);
                 System.out.println(line);
-            } else {
-                tasklist.add(new task(tasklist.size()+1, comm, " "));
+            } else if(comm.startsWith("todo")) {
+                String doit = comm.substring(5, comm.length());
+                tasklist.add(new task('T', tasklist.size()+1, doit, " ", ""));
+                System.out.println(line);
+                System.out.println("Got it. I've added this task:");
+                System.out.println("  [T][ ] " + doit);
+                System.out.println("Now you have " +tasklist.size()+ " tasks in the list");
+                System.out.println(line);
+            } else if(comm.startsWith("deadline")) {
+                String doit = comm.substring(9, comm.length());
+                String[] parts = doit.split("/by");
+                tasklist.add(new task('D', tasklist.size()+1, parts[0], " ", parts[1]));
+                System.out.println(line);
+                System.out.println("Got it. I've added this task:");
+                System.out.println("   [D][ ] " + parts[0] + "(by:" +parts[1]+")");
+                System.out.println("Now you have " +tasklist.size()+ " tasks in the list");
+                System.out.println(line);
+            } else if(comm.startsWith("event")) {
+                String doit = comm.substring(6, comm.length());
+                String[] froms = doit.split("/from");
+                String[] tos = doit.split("/to");
+                tasklist.add(new task('E', tasklist.size()+1, froms[0], " ", froms[1]));
+                System.out.println(line);
+                System.out.println("Got it. I've added this task:");
+                System.out.println("   [E][ ] " +froms[0]+ " (from:" +froms[1]+ "to:" +tos[1]);
+            }else {
+                //tasklist.add(new task('T', tasklist.size()+1, parts[0], " "));
                 System.out.println(line);
                 System.out.println("added: " + comm);
                 System.out.println(line);
