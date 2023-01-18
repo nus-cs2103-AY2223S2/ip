287cd534f0c551446ed2abacacc31a46e17b8e11
kennycjy <kennychon00@gmail.com>
2023-01-18 15:40:21 +0800

    Implemented Skeletal Structure for Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,28 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(logo);
+
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+
+        boolean program_running_status = true;
+
+        String echo;
+        Scanner scan = new Scanner(System.in);
+
+        while (program_running_status) {
+            echo = scan.nextLine();
+
+            if (echo.equals("bye")) {
+                break;
+            }
+            System.out.println("    Duke says:");
+            System.out.println("    " + echo);
+            
+        }
+        scan.close();
+        System.out.println("    Duke says:");
+        System.out.println("    Bye. Hope you run this program again!");
     }
 }
