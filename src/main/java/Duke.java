568c6dd65621ed10de6c35400c3197e14b711318
Nicholas Cheong <nicholascheon@gmail.com>
2023-01-18 22:59:48 +0800

    Level 4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,7 +8,8 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
         System.out.println("Hi, I'm Nero and I am an automated chat bot" + "\n" + "What would you like to do?");
         while (true) {
-            String[] input = sc.nextLine().split(" ");
+            String originalString = sc.nextLine();
+            String[] input = originalString.split(" ");
             if (input[0].equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!" + "\n");
                 break;
@@ -17,41 +18,60 @@ public class Duke {
                 for (int i = 0; i < counter; i++) {
                     int index = i + 1;
                     Task currTask = allTasks[i];
-                    System.out.println(index + ". " + currTask.getStatusIcon() + " " + currTask.getDescription());
+                    System.out.println(index + ". " + currTask.currToPrint());
                 }
             } else if (input[0].equals("mark")) {
                 int taskToMark = Integer.parseInt(input[1]) - 1;
                 Task currTask = allTasks[taskToMark];
                 currTask.markAsDone();
-                System.out.println("Great job on completing this task! " + "\n" + currTask.getStatusIcon() + " " + currTask.getDescription());
+                System.out.println("Great job on completing this task! " + "\n" + currTask.currToPrint());
             } else if (input[0].equals("unmark")) {
                 int taskToUnmark = Integer.parseInt(input[1]) - 1;
                 Task currTask = allTasks[taskToUnmark];
                 currTask.markAsUndone();
-                System.out.println("Remember to complete this task!! " + "\n" + currTask.getStatusIcon() + " " + currTask.getDescription());
-            } else {
+                System.out.println("Remember to complete this task!! " + "\n" + currTask.currToPrint());
+            } else if (input[0].equals("todo")) {
                 String toAdd = "";
-                for (int i = 0; i < input.length; i++) {
+                for (int i = 1; i < input.length; i++) {
                     toAdd += input[i] + " ";
                 }
-                Task newTask = new Task(toAdd);
+                Task newTask = new ToDo(toAdd);
                 allTasks[counter] = newTask;
                 counter++;
-                System.out.println(newTask.getDescription() + "\n");
+                System.out.println("Got it! I've added this task to the list!" + "\n" + newTask.currToPrint() + "\n" + "Now you have " + counter +
+                        " tasks in the list!" + "\n");
+            } else if (input[0].equals("deadline")) {
+                String[] splitString = originalString.split("/");
+                String description = splitString[0].replace("deadline", "");
+                String duration = splitString[1].replace("by", "by:");
+                Task newTask = new Deadline(description, duration);
+                allTasks[counter] = newTask;
+                counter++;
+                System.out.println("Got it! I've added this task to the list!" + "\n" + newTask.currToPrint() + "\n" + "Now you have " + counter +
+                        " tasks in the list!" + "\n");
+            } else if (input[0].equals("event")) {
+                String[] splitString = originalString.split("/");
+                String description = splitString[0].replace("event", "");
+                String duration = splitString[1].replace("from", "from:") + splitString[2].replace("to", "to:");
+                Task newTask = new Event(description, duration);
+                allTasks[counter] = newTask;
+                counter++;
+                System.out.println("Got it! I've added this task to the list!" + "\n" + newTask.currToPrint() + "\n" + "Now you have " + counter +
+                        " tasks in the list!" + "\n");
             }
         }
     }
 }
 
-class Task {
+abstract class Task {
     protected String description;
     protected boolean isDone;
-
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
 
+    public abstract String currToPrint();
     public String getDescription() {
         return description;
     }
@@ -59,10 +79,10 @@ class Task {
     public boolean getStatus() {
         return isDone;
     }
+
     public String getStatusIcon() {
         return (isDone ? "[X]" : "[ ]");
     }
-
 
     public void markAsDone() {
         isDone = true;
@@ -70,6 +90,65 @@ class Task {
 
     public void markAsUndone(){
         isDone = false;
+    }
+
+
+}
+
+class ToDo extends Task {
+    public ToDo(String description) {
+        super(description);
+    }
+
+    public String getTaskIcon() {
+        return "[T]";
+    }
+
+    public String currToPrint() {
+        return this.getTaskIcon() + this.getStatusIcon() + " " + this.getDescription();
+    }
+}
+
+class Deadline extends Task {
+
+    public String duration;
+    public Deadline(String description, String duration) {
+        super(description);
+        this.duration = duration;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+    public String getTaskIcon() {
+        return "[D]";
+    }
+
+    public String currToPrint() {
+        return this.getTaskIcon() + this.getStatusIcon() + " " + this.getDescription() +
+                " (" + this.getDuration() + ")";
+    }
+
+}
+
+class Event extends Task {
+
+    public String duration;
+    public Event(String description, String duration) {
+        super(description);
+        this.duration = duration;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+    public String getTaskIcon() {
+        return "[E]";
+    }
+
+    public String currToPrint() {
+        return this.getTaskIcon() + this.getStatusIcon() + " " + this.getDescription() +
+                " (" + this.getDuration() + ")";
     }
 
 }
