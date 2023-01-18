c403d78390e35a4ac56788e2f52c2ffa6a72daf3
shuimeihe <shuimeihe@gmail.com>
2023-01-18 22:14:40 +0800

    no message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,39 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Duke {
+    public static ArrayList<Task> tasks = new ArrayList<>();
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        printASCII();
+
+        try (Scanner sc = new Scanner(System.in)) {
+            String input = "";
+
+            do {
+                input = sc.nextLine();
+
+                System.out.println(input);
+
+            } while (!input.equals("bye"));
+
+        } catch (Exception e) {
+            System.out.println("Encountered exception: " + e + "\n Exiting program");
+        } finally {
+            System.out.println("Goodbye!");
+        }
+    }
+
+    static void printASCII(){
+        String line = " ---------------------------------------------------------";
+        String logo = "\t\t\t\t            | |       \n" +
+                "\t\t\t\t _ __  _   _| | _____ \n" +
+                "\t\t\t\t| '_ \\| | | | |/ / _ \\\n" +
+                "\t\t\t\t| |_) | |_| |   <  __/\n" +
+                "\t\t\t\t| .__/ \\__,_|_|\\_\\___|\n" +
+                "\t\t\t\t| |                   \n" +
+                "\t\t\t\t|_|                  ";
+        System.out.println(line + "\n" + logo + "\n" + line);
+        System.out.println("Welcome to PUKE, the worst chat bot in existence");
+        System.out.println("Input a command");
     }
 }
