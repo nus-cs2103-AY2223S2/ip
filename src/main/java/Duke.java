594ff42708f4f05dd002bc0837a23ce676df043a
Valerie Tan <valerietanhx@gmail.com>
2023-01-19 00:42:09 +0800

    Add Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        System.out.println("<コ:彡");
+        System.out.println("Hello! I'm Duke, your favourite pink octopus.");
+        System.out.println("What can I do for you today?");
+
+        Scanner sc = new Scanner(System.in);
+
+        String input = sc.nextLine();
+
+        while (!input.equals("bye")) {
+            System.out.println("(\\ (\\ \n" +
+                    "(„• ֊ •„) ♡\n" +
+                    "━O━O━━━━━━━━━━━━━━━━━━━━━━━━━");
+            System.out.println(input);
+            input = sc.nextLine();
+        }
+
+        System.out.println("Bye bye :( Hope to see you again soon!");
+        sc.close();
+
     }
 }
