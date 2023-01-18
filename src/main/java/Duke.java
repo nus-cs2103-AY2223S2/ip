bd4a953afb6e01cfbb59574ecd4bd0292a5c0d92
Bransonlj <Bransonlj@gmail.com>
2023-01-19 00:20:20 +0800

    Level-2: added list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,26 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
         String line = "____________________________________________________________";
         System.out.println(line + "\nHello! I'm Duke\nWhat can I do for you?\n" + line);
         String input = scanner.nextLine();
         while (!input.equals("bye")) {
-            System.out.println(line + "\n" + input + "\n" + line);
+            if (input.equals("list")) {
+                System.out.println(line);
+                for (int i = 0; i < list.size(); i++) {
+                    System.out.println((i+1) + ". " + list.get(i));
+                }
+
+                System.out.println(line);
+            } else {
+                list.add(input);
+                System.out.println(line + "\nadded: " + input + "\n" + line);
+            }
+
             input = scanner.nextLine();
         }
         System.out.println(line + "\nBye. Hope to see you again soon!\n" + line);
