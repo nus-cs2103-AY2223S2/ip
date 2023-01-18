080cbf2d5156ea70df908539058997ffd8d92c1d
“ShawnYip-Au” <shawnyipau@gmail.com>
2023-01-18 14:00:28 +0800

    Implement Duke asking for inputs

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;  // Import the Scanner class
+
 public class Duke {
+
+    private static Scanner getInput = new Scanner(System.in); // Create a static Scanner object
+
     public static void main(String[] args) {
         System.out.println(intro());
+        askForInput();
     }
 
     private static String logo() {
@@ -13,5 +19,10 @@ public class Duke {
 
     private static String intro() {
         return "Hello! I'm\n" + logo() + "\nWhat can I do for you?";
+    }
+
+    private static String askForInput() {
+        System.out.print("> ");
+        return getInput.nextLine();
     }
 }
