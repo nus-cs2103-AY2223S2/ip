5ffbbab01760adae54a7a7bab91668d2f6aa903b
Mehvin <melvinokx@gmail.com>
2023-01-18 17:45:06 +0800

    Level 5. Handle Errors

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -17,29 +17,28 @@ public class Duke {
     }
 
     private static void addTask(String description, TaskType taskType) {
-        System.out.println("Got it. I've added this task:");
         switch (taskType) {
             case TODO:
                 Todo todoTask = new Todo(description);
                 list.add(todoTask);
-                System.out.println(todoTask);
+                System.out.println("Got it. I've added this task:\n" + todoTask);
                 break;
             case EVENT:
                 String modifiedDescription = description.split(" /from ")[1];
                 Event eventTask = new Event(description.split(" /from ")[0], modifiedDescription.split(" /to ")[0], modifiedDescription.split(" /to ")[1]);
                 list.add(eventTask);
-                System.out.println(eventTask);
+                System.out.println("Got it. I've added this task:\n" + eventTask);
                 break;
             case DEADLINE:
                 Deadline deadlineTask = new Deadline(description.split(" /by ")[0], description.split(" /by ")[1]);
                 list.add(deadlineTask);
-                System.out.println(deadlineTask);
+                System.out.println("Got it. I've added this task:\n" + deadlineTask);
                 break;
         }
         System.out.println("Now you have " + list.size() + " tasks in the list.");
     }
 
-    private static void mark(int itemNo) {
+    private static void mark(int itemNo) throws DukeException {
         list.get(itemNo).setStatus(true);
         System.out.println("Nice! I've marked this task as done:\n" + list.get(itemNo).toString());
     }
@@ -77,32 +76,42 @@ public class Duke {
             String userInput = sc.nextLine();
             String cmd = userInput.split(" ")[0];
 
-            switch (cmd) {
-                case "bye":
-                    quit = true;
-                    System.out.println("MEL: Bye. Hope to see you again soon!");
-                    break;
-                case "list":
-                    displayList();
-                    break;
-                case "mark":
-                    mark(Integer.parseInt(userInput.split(" ")[1]) - 1);
-                    break;
-                case "unmark":
-                    unmark(Integer.parseInt(userInput.split(" ")[1]) - 1);
-                    break;
-                case "todo":
-                    addTask(userInput.split("todo ")[1], TaskType.TODO);
-                    break;
-                case "event":
-                    addTask(userInput.split("event ")[1], TaskType.EVENT);
-                    break;
-                case "deadline":
-                    addTask(userInput.split("deadline ")[1], TaskType.DEADLINE);
-                    break;
-                default:
-                    System.out.println("OOPS!!! I'm sorry, but I don't know what that means :-(");
-                    break;
+            try {
+                switch (cmd) {
+                    case "bye":
+                        quit = true;
+                        System.out.println("MEL: Bye. Hope to see you again soon!");
+                        break;
+                    case "list":
+                        displayList();
+                        break;
+                    case "mark":
+                        mark(Integer.parseInt(userInput.split(" ")[1]) - 1);
+                        break;
+                    case "unmark":
+                        unmark(Integer.parseInt(userInput.split(" ")[1]) - 1);
+                        break;
+                    case "todo":
+                        addTask(userInput.split("todo ")[1], TaskType.TODO);
+                        break;
+                    case "event":
+                        addTask(userInput.split("event ")[1], TaskType.EVENT);
+                        break;
+                    case "deadline":
+                        addTask(userInput.split("deadline ")[1], TaskType.DEADLINE);
+                        break;
+                    default:
+                        throw new DukeException("I'm sorry, but I don't know what that means");
+                }
+            }
+            catch(DukeException exception) {
+                System.out.println(exception.getMessage());
+            }
+            catch (IndexOutOfBoundsException e) {
+                System.out.println("MEL: Invalid format of input, please try again!");
+            }
+            catch (NumberFormatException e) {
+                System.out.println("MEL: Invalid integer, please try again!");
             }
         }
     }
