bacc588828968829fcbbce744a0e3aa1c489db66
Branda <brandaang@gmail.com>
2023-01-18 19:00:36 +0800

    Level-6

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 public class Duke {
     public static void main(String[] args) throws DukeException {
         String logo = " ____        _        \n"
@@ -7,7 +8,7 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        System.out.println("Hiii! I'm panpan\nWhat's up?");
+        System.out.println("ฅʕ•ᴥ•ʔฅ :: Hiii! I'm duke, what can I help you with?");
         //echo();
         add();
     }
@@ -17,7 +18,7 @@ public class Duke {
         while (true) {
             cmd = input.nextLine();
             if (cmd.equals("bye")) {
-                System.out.println("Buhbyeee, hope to see you again soon <3");
+                System.out.println("ʕっ￫ᴥ￩ʔっ :: Buhbyeee, hope to see you again soon!");
                 return;
             } else {
                 System.out.println(cmd);
@@ -28,53 +29,89 @@ public class Duke {
     public static void add() throws DukeException {
         Scanner input = new Scanner(System.in);
         String cmd;
-        Task[] list = new Task[100];
+        ArrayList<Task> list = new ArrayList<Task>(100);
         Integer count = 0;
         Integer num;
         while (true) {
+            System.out.println("•──────────────────♛─────────────────•");
             try {
                 cmd = input.nextLine();
+                System.out.println("•──────────────────♛─────────────────•");
                 if (cmd.equals("bye")) {
-                    System.out.println("Buhbyeee, hope to see you again soon <3");
+                    System.out.println("ʕっ￫ᴥ￩ʔっ :: Buhbyeee, hope to see you again soon!");
                     return;
                 } else if (cmd.equals("list")) {
-                    System.out.println("Here are the tasks in your list:");
+                    System.out.println("ʕ•ᴥ•ʔっ :: Here are the task(s) in your list:");
                     for (int i = 1; i <= count; i++) {
-                        System.out.println(i + "." + list[i - 1].toString());
+                        System.out.println(i + "." + list.get(i-1).toString());
                     }
                 } else if (cmd.startsWith("mark")) {
                     num = Integer.valueOf(cmd.substring(5));
-                    list[num - 1].setDone();
-                    System.out.println("Nice! I've marked this task as done:\n  " + list[num - 1].toString());
+                    if (count < num) {
+                        throw new DukeException("ʕ ﾟ ● ﾟʔ :: ☹ OOPS!!! The task does not exist!");
+                    }
+                    list.get(num - 1).setDone();
+                    System.out.println("ʕ•̀ω•́ʔ✧ :: Nice! I've marked this task as done:\n  " + list.get(num - 1).toString());
                 } else if (cmd.startsWith("unmark")) {
                     num = Integer.valueOf(cmd.substring(7));
-                    list[num - 1].setUndone();
-                    System.out.println("OK, I've marked this task as not done yet:\n  " + list[num - 1].toString());
+                    if (count < num) {
+                        throw new DukeException("ʕ ﾟ ● ﾟʔ :: ☹ OOPS!!! The task does not exist!");
+                    }
+                    list.get(num - 1).setUndone();
+                    System.out.println("ʕ•̀ω•́ʔ✧ :: OK, I've marked this task as not done yet:\n  " + list.get(num - 1).toString());
                 } else if (cmd.startsWith("todo") || cmd.startsWith("deadline") || cmd.startsWith("event")) {
                     boolean valid = true;
                     if (cmd.startsWith("todo")) {
                         if (cmd.substring(4).equals("")) {
                             valid = false;
-                            throw new DukeException("☹ OOPS!!! The description of a todo cannot be empty.");
+                            throw new DukeException("ʕ ﾟ ● ﾟʔ :: ☹ OOPS!!! The description of a todo cannot be empty!");
                         } else {
-                            list[count] = new ToDo(cmd.substring(5));
+                            list.add(new ToDo(cmd.substring(5)));
                         }
                     } else if (cmd.startsWith("deadline")) {
                         int ind = cmd.indexOf("/by");
-                        list[count] = new Deadline(cmd.substring(9, ind - 1), cmd.substring(ind + 4));
+                        if (ind == -1) {
+                            valid = false;
+                            throw new DukeException("ʕ ﾟ ● ﾟʔ :: ☹ OOPS!!! The deadline is missing!");
+                        }
+                        if (ind == 9) {
+                            valid = false;
+                            throw new DukeException("ʕ ﾟ ● ﾟʔ :: ☹ OOPS!!! The description of a deadline cannot be empty!");
+                        }
+                        list.add(new Deadline(cmd.substring(9, ind - 1), cmd.substring(ind + 4)));
                     } else {
                         int start = cmd.indexOf("/from");
                         int end = cmd.indexOf("/to");
-                        list[count] = new Event(cmd.substring(6, start - 1), cmd.substring(start + 6, end - 1), cmd.substring(end + 4));
+                        if (start == -1 || end == -1) {
+                            valid = false;
+                            throw new DukeException("ʕ ﾟ ● ﾟʔ :: ☹ OOPS!!! The event duration is missing!");
+                        }
+                        if (start == 6) {
+                            valid = false;
+                            throw new DukeException("ʕ ﾟ ● ﾟʔ :: ☹ OOPS!!! The description of an event cannot be empty!");
+                        }
+                        list.add(new Event(cmd.substring(6, start - 1), cmd.substring(start + 6, end - 1), cmd.substring(end + 4)));
                     }
                     if (valid) {
-                        System.out.println("Got it. I've added this task:");
-                        System.out.println("  " + list[count].toString());
+                        System.out.println("ฅʕ'ᴥ'ʔฅ :: Got it. I've added this task:");
+                        System.out.println("  " + list.get(count).toString());
                         count++;
-                        System.out.println("Now you have " + count + " tasks in the list.");
+                        System.out.println("ʕ•ᴥ•ʔっ :: Now you have " + count + " task(s) in the list.");
                     }
+                } else if (cmd.startsWith("delete")) {
+                    if (count == 0) {
+                        throw new DukeException("ʕ ﾟ ● ﾟʔ :: ☹ OOPS!!! The list is empty!");
+                    }
+                    num = Integer.valueOf(cmd.substring(7));
+                    if (count < num) {
+                        throw new DukeException("ʕ ﾟ ● ﾟʔ :: ☹ OOPS!!! The task does not exist!");
+                    }
+                    System.out.println("ฅʕ'ᴥ'ʔฅ :: Noted. I've removed this task :");
+                    System.out.println("  " + list.remove(num - 1).toString());
+                    count --;
+                    System.out.println("ʕ•ᴥ•ʔっ :: Now you have " + count + " task(s) in the list.");
                 } else {
-                    throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+                    throw new DukeException("╮ʕ˚ᴥ˚ʔ╭ :: ☹ OOPS!!! I'm sorry, but I don't know what that means!");
                 }
             } catch (DukeException e) {
                 System.out.println(e);
--- src/main/java/DukeException.java
+++ src/main/java/DukeException.java
@@ -2,4 +2,9 @@ public class DukeException extends Exception {
     public DukeException(String error) {
         super(error);
     }
+
+    @Override
+    public String toString() {
+        return this.getMessage();
+    }
 }
