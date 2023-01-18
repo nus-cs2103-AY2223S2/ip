1902bfe53e3794108b28833e7a3ead6ed8cebb4e
seadragon2000341 <limhaileongshawn@gmail.com>
2023-01-18 10:56:20 +0800

    Level-6

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Duke {
-    private static int index;
-    private static Task[] list = new Task[100];
+    private static ArrayList<Task> list = new ArrayList<Task>();
 
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -23,21 +24,29 @@ public class Duke {
 
             else if(spStg[0].equals("list")) {
                 System.out.println("Here are the tasks in your list:");
-                for (int i = 0; i < index; i++) {
-                    System.out.println(i+1 + ". " + list[i].toString());
+                for (int i = 0; i < list.size(); i++) {
+                    System.out.println(i+1 + ". " + list.get(i));
                 }
+            }
+
+            else if(spStg[0].equals("delete")) {
+                int i =  Integer.parseInt(spStg[1]) - 1;
+                System.out.println("Noted. I've removed this task:");
+                System.out.println(list.get(i));
+                list.remove(i);
+                System.out.println("Now you have " + list.size() + " tasks in the list.");
             }
 
             else if(spStg[0].equals("mark")) {
                 int i =  Integer.parseInt(spStg[1]) - 1;
-                list[i].mark();
-                System.out.println("Nice! I've marked this task as done:\n" + list[i]);
+                list.get(i).mark();
+                System.out.println("Nice! I've marked this task as done:\n" + list.get(i));
             }
 
             else if(spStg[0].equals("unmark")) {
                 int i =  Integer.parseInt(spStg[1]) - 1;
-                list[i].unmark();
-                System.out.println("OK, I've marked this task as not done yet:\n" + list[i]);
+                list.get(i).unmark();
+                System.out.println("OK, I've marked this task as not done yet:\n" + list.get(i));
             }
 
             else {
@@ -47,7 +56,7 @@ public class Duke {
                         System.out.println("☹ OOPS!!! The description of a todo cannot be empty.");
                         continue;
                     }
-                    list[index] = new Todo(spStg[1]);
+                    list.add(new Todo(spStg[1]));
                 }
                 else if (firstWord.equals("deadline")) {
                     if (spStg.length == 1) {
@@ -55,24 +64,24 @@ public class Duke {
                         continue;
                     }
                     String[] sppStg = spStg[1].split("/by");
-                    list[index] = new Deadline(sppStg[0], sppStg[1]);
+                    list.add(new Deadline(sppStg[0], sppStg[1]));
                 }
+
                 else if (firstWord.equals("event")) {
                     if (spStg.length == 1) {
                         System.out.println("☹ OOPS!!! The description of an event cannot be empty.");
                         continue;
                     }
                     String[] sppStg = spStg[1].split("/from|/to");
-                    list[index] = new Event(sppStg[0], sppStg[1], sppStg[2]);
+                    list.add(new Event(sppStg[0], sppStg[1], sppStg[2]));
                 }
                 else {
                     System.out.println("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
                     continue;
                 }
-                System.out.println("Got it. I've added this task:");
-                System.out.println(list[index]);
-                index++;
-                System.out.println("Now you have " + index + " tasks in the list.");
+
+                System.out.println("Got it. I've added this task:\n" + list.get(list.size() - 1));
+                System.out.println("Now you have " + list.size() + " tasks in the list.");
             }
         }
     }
