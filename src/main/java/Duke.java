eb4ed7f8925667b9d195c4c35ecdb60a854f384c
davidni <davidni055@gmail.com>
2023-01-18 12:06:16 +0800

    Update adding of items to list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -7,24 +8,35 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        greetEchoExit();
+        userInputs();
     }
 
-    private static void greetEchoExit() {
+    private static void userInputs() {
         Scanner sc = new Scanner(System.in);
+        ArrayList list = new ArrayList<>(100);
 
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
-        
-        while (true) {
-            String str = sc.nextLine();
-            if (str.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
-                break;
-            } else {
-                System.out.println(str);
-            }
 
+        while (true) {
+            String input = sc.nextLine();
+
+            switch (input) {
+                case "bye":
+                    System.out.println("Bye. Hope to see you again soon!");
+                    break;
+
+                case "list":
+                    for (int i = 0; i < list.size(); i++) {
+                        System.out.println(i + 1 + "." + list.get(i));
+                    }
+                    break;
+
+                default:
+                    list.add(input);
+
+            }
         }
     }
 }
+
