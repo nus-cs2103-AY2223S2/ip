4b42a44135380c2e12ddcae7cbb7aca22cdd6276
gwynethguo <e0905362@u.nus.edu>
2023-01-18 10:37:25 +0800

    Add features: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,13 +3,15 @@ public class Duke {
     String lines = "____________________________________________________________\n";
     boolean exit = false;
     String msg;
+    String[] tasks = new String[100];
+    int num_tasks = 0;
     public static void main(String[] args) {
         Duke duke = new Duke();
         System.out.println(duke.welcome_msg());
         Scanner scanner = new Scanner(System.in);
         while(!duke.exit) {
             String inp = scanner.nextLine();
-            duke.msg = duke.add_lines(inp + "\n");
+            duke.msg = duke.add_lines("added: " + inp + "\n");
             duke.check_msg(inp);
             System.out.println(duke.msg);
         }
@@ -22,6 +24,15 @@ public class Duke {
         if (inp.equals("bye")) {
             this.exit = true;
             this.msg = this.add_lines("Bye. Hope to see you again soon!\n");
+        } else if (inp.equals("list")) {
+            this.msg = "";
+            for(int i = 0; i < num_tasks; i++) {
+                this.msg += Integer.toString(i + 1) + ". " + this.tasks[i] + "\n";
+            }
+            this.msg = this.add_lines(this.msg);
+        } else {
+            tasks[num_tasks] = inp;
+            num_tasks = num_tasks + 1;
         }
     }
 
