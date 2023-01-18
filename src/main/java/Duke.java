f2dead859f5f71f44435baca22b83fea45515bcd
Lee Jia Sheng <thegreyjs@gmail.com>
2023-01-18 22:27:18 +0800

    Add a list to store tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,22 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     private final static String bye = "bye";
     private final static String goodbyeMessage = "Bye. Hope to see you again soon!";
+    private final static String list = "list";
+    private static ArrayList<String> tasks = new ArrayList<>();
 
     private static void indentedPrintln(String message) {
         String indentedMessage = "   " + message;
         System.out.println(indentedMessage);
+    }
+
+    private static void list() {
+        int numOfTasks = tasks.size();
+        for (int i = 1; i <= numOfTasks; i++) {
+            indentedPrintln(i + ". " + tasks.get(i - 1));
+        }
     }
 
     public static void main(String[] args) {
@@ -20,7 +30,14 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
         while (!str.equals(bye)) {
-            indentedPrintln(str);
+            if (!str.equals(list)) {
+                tasks.add(str);
+                indentedPrintln("added: " + str);
+            } else {
+                list();
+            }
+
+            System.out.println();
             str = sc.nextLine();
         }
         indentedPrintln(goodbyeMessage);
