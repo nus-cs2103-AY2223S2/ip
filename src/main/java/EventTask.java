ed3826e9ff02518f0bd9d465d0c790a7af91f0f9
Goh Jun Yi <54541329+Junyi00@users.noreply.github.com>
2023-01-18 15:21:48 +0800

    chore: Add missing space during display for EventTask

--- src/main/java/EventTask.java
+++ src/main/java/EventTask.java
@@ -16,6 +16,6 @@ public class EventTask extends Task{
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + "(from: " + from + " to: " + to + ")";
+        return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
     }
 }
