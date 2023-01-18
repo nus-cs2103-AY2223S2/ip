452bb0b47cc431d55e5a8be77bd7a5d9d76f3cc9
alextang809 <alextang809@gmail.com>
2023-01-18 20:42:03 +0800

    add welcome msg and bye msg

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,24 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+
+
+        printWelcomeMessage();
+        Scanner in = new Scanner(System.in);
+        Task[] tasks = new Task[MAX_TASK];
+
+        public static void printByeMessage() {
+            Task.printHorizontalLine();
+            System.out.println("     Bye. Hope to see you again soon!");
+            Task.printHorizontalLine();
+        }
+
+        public static void printWelcomeMessage() {
+            Task.printHorizontalLine();
+            System.out.println("     Hello! I'm Duke");
+            System.out.println("     What can I do for you?");
+            Task.printHorizontalLine();
+        }
     }
 }
