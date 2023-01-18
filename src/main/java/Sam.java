c2d25aa2691b82f42c4f5fb34244282de5dae808
CloudHill <rh.cloudhill@gmail.com>
2023-01-18 17:46:42 +0800

    greet, echo, exit

--- src/main/java/Sam.java
+++ src/main/java/Sam.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Sam {
 	public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    boolean live = true;
+
 		String logo =
 				"\t  ██████╗ █████╗ ███╗   ███╗\n"
 			+ "\t ██╔════╝██╔══██╗████╗ ████║\n"
@@ -9,6 +14,26 @@ public class Sam {
 			+ "\t ╚═════╝ ╚═╝  ╚═╝╚═╝     ╚═╝";
 
 		System.out.println(logo);
-		System.out.println("Hello, I am Sam!");
+		talk("Hello, I am Sam!");
+
+    while (live) {
+      System.out.println();
+      System.out.print("> ");
+      System.out.println();
+      String input = scanner.nextLine();
+      if (input.equals("bye")) {
+        live = false;
+        talk("Bye!");
+      } else {
+        talk(input);
+      }
+    }
+    scanner.close();
 	}
+
+  private static void talk(String message) {
+    System.out.println("┌───────────────────────────────────────────┐");
+    System.out.println("  " + message);
+    System.out.println("└───────────────────────────────────────────┘");
+  }
 }
