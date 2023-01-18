fac97b480813444f37ad105d8c51fff65bc709cb
gohyongjing <gohyongjing@gmail.com>
2023-01-18 18:04:15 +0800

    Refactor Command to use ArrayList of Strings

--- src/main/java/Command.java
+++ src/main/java/Command.java
@@ -1,13 +1,14 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Command {
 
     /** Arguments of the command */
-    private String[][] arguments;
-    private int numArgs;
+    private ArrayList<ArrayList<String>> arguments;
 
     public Command(String input) {
         String[] terms = input.strip().split(" /");
-        this.arguments = new String[terms.length][];
-        this.numArgs = 0;
+        this.arguments = new ArrayList<ArrayList<String>>();
         for (String term : terms) {
             int firstSpace = term.indexOf(" ");
             String key = (firstSpace == -1
@@ -16,8 +17,8 @@ public class Command {
             String value = (firstSpace == -1
                     ? ""
                     : term.substring(firstSpace + 1));
-            this.arguments[this.numArgs] = new String[] {key, value};
-            this.numArgs += 1;
+            this.arguments.add(new ArrayList<String>(
+                    Arrays.asList(key, value)));
         }
         this.checkArgs();
     }
@@ -27,24 +28,24 @@ public class Command {
      * @return the type of command.
      */
     public String getType() {
-        return this.arguments[0][0];
+        return this.arguments.get(0).get(0);
     }
 
     public String getArg(String key) {
-        for (int i = 0; i < this.arguments.length; i++) {
-            if (this.arguments[i][0].equals(key)) {
-                return this.arguments[i][1];
+        for (int i = 0; i < this.arguments.size(); i++) {
+            if (this.arguments.get(i).get(0).equals(key)) {
+                return this.arguments.get(i).get(1);
             }
         }
-        throw new IllegalArgumentException("Command " + this.arguments[0][0]
+        throw new IllegalArgumentException("Command " + this.arguments.get(0).get(0)
                 + " does not contain keyword argument " + key);
     }
 
     private void checkArgs() {
-        if (this.arguments.length == 0) {
+        if (this.arguments.size() == 0) {
             return;
         }
-        switch (this.arguments[0][0]) {
+        switch (this.arguments.get(0).get(0)) {
         case "bye":
             // Fallthrough
         case "list":
@@ -61,32 +62,31 @@ public class Command {
             checkHasOnly(new String[] {"from", "to"});
             break;
         default:
-            throw new IllegalArgumentException("Command " + this.arguments[0][0]
+            throw new IllegalArgumentException("Command " + this.arguments.get(0).get(0)
                     + " not recognised");
         }
     }
 
     private void checkHasOnly(String[] args) {
-        if (this.arguments.length > 1 + args.length) {
-            throw new IllegalArgumentException("Command " + this.arguments[0][0]
+        if (this.arguments.size() > 1 + args.length) {
+            throw new IllegalArgumentException("Command " + this.arguments.get(0).get(0)
                     + " takes in " + args.length + " argument(s) but "
-                    + this.arguments.length + " were given");
+                    + this.arguments.size() + " were given");
         }
         for (String arg : args) {
             if (!contains(arg)) {
-                throw new IllegalArgumentException("Command " + this.arguments[0][0]
+                throw new IllegalArgumentException("Command " + this.arguments.get(0).get(0)
                         + " requires argument " + arg + " but was not given");
             }
         }
     }
 
     private boolean contains(String arg) {
-        for (int i = 1; i < this.arguments.length; i++) {
-            if (this.arguments[i][0].equals(arg)) {
+        for (int i = 1; i < this.arguments.size(); i++) {
+            if (this.arguments.get(i).get(0).equals(arg)) {
                 return true;
             }
         }
         return false;
     }
-
 }
