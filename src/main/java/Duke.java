19d109b084a7b9d768d04567d06d98f592ace3fa
chen-jerry-junior <jrchenjiarui@163.com>
2023-01-18 22:38:22 +0800

    level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,8 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Duke {
     /**
-     * A level 1 chat bot Duke.
+     * A level 2 chat bot Duke.
      */
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -13,9 +14,17 @@ public class Duke {
         Scanner ai = new Scanner(System.in);
         String greetings = "Hello! I'm Duke" + "\nWhat can I do for you?";
         System.out.println(greetings);
+        ArrayList<String> todo = new ArrayList<>();
         String input = ai.nextLine();
         while (!input.equals("bye")) {
-            System.out.println(input);
+            if (!input.equals("list")) {
+                todo.add(input);
+                System.out.println("added: " + input);
+            } else {
+                for (int i = 0; i < todo.size(); i++) {
+                    System.out.println(i + 1 + ". " + todo.get(i));
+                }
+            }
             input = ai.nextLine();
         }
         System.out.println("Bye. Hope to see you again soon!");
