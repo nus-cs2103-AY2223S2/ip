2aedb4798ae80bdb559c4cbd70bc5a2c7f088a40
Lee Jia Sheng <thegreyjs@gmail.com>
2023-01-18 21:47:51 +0800

    Implement greet, echo and exit function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,14 @@
+import java.util.Scanner;
+
 public class Duke {
+    private final static String bye = "bye";
+    private final static String goodbyeMessage = "Bye. Hope to see you again soon!";
+
+    private static void indentedPrintln(String message) {
+        String indentedMessage = "   " + message;
+        System.out.println(indentedMessage);
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +16,13 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        while (!str.equals(bye)) {
+            indentedPrintln(str);
+            str = sc.nextLine();
+        }
+        indentedPrintln(goodbyeMessage);
     }
 }
