8a50930e70a516752cf36162f61b3c055493f0c5
zxisatree <62459441+zxisatree@users.noreply.github.com>
2023-01-18 19:49:17 +0800

    Implemented main input loop

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,30 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner input = new Scanner(System.in);
+        String reply = "";
+
+        printTextWithLines("Hello! I'm Duke\nWhat can I do for you?");
+
+        while (true) {
+            reply = input.nextLine();
+            if (reply.equals("bye")) {
+                printTextWithLines("Bye. Hope to see you again soon!");
+                break;
+            }
+            printTextWithLines(reply);
+        }
+
+    }
+
+    static void printTextWithLines(String text) {
+        printLineBreak();
+        System.out.println(text);
+        printLineBreak();
+        System.out.println();
+    }
+    static void printLineBreak() {
+        System.out.println("____________________________________________________________");
     }
 }
