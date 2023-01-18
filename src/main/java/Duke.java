e3b53a3578fd68dcde3c75734934164008847042
HJ-PRK <e0902027@u.nus.edu>
2023-01-18 22:12:57 +0800

    Level-6

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,16 +3,19 @@ import java.lang.*;
 public class Duke {
     public static class task{
         char type;
-        int id = 0;
         String todo;
         String isDone = "";
         String info;
-        public task(char type, int id, String todo, String isDone, String info) {
+        public task(char type, String todo, String isDone, String info) {
             this.type = type;
-            this.id = id;
             this.todo = todo;
             this.isDone = isDone;
-            this.info = info;
+            //this.info = "(" +info+ ")";
+            if(info == "") {
+                this.info = info;
+            } else {
+                this.info = "("  +info+ ")";
+            }
         }
     }
     public static void main(String[] args) {
@@ -40,7 +43,7 @@ public class Duke {
                     System.out.println("Here are the tasks in your list:");
                     for (int i = 0; i < tasklist.size(); i++) {
                         task current = tasklist.get(i);
-                        System.out.println(current.id + ".[" + current.type + "][" + current.isDone + "] " + current.todo);
+                        System.out.println(i+1 + ".[" + current.type + "][" + current.isDone + "] " + current.todo);
                     }
                     System.out.println(line);
                 } else if (comm.startsWith("mark")) {
@@ -61,9 +64,19 @@ public class Duke {
                     System.out.println("OK, I've marked this task as not done yet:");
                     System.out.println("[" + current.isDone + "] " + current.todo);
                     System.out.println(line);
-                } else if (comm.startsWith("todo")) {
+                } else if(comm.startsWith("delete")) {
+                    String str = comm.substring(comm.length() -1);
+                    int marking = Integer.parseInt(str);
+                    task current = tasklist.get(marking-1);
+                    tasklist.remove(marking-1);
+                    System.out.println(line);
+                    System.out.println("Noted. I've removed this task:");
+                    System.out.println("[" +current.type+ "][" +current.isDone+ "] " +current.todo+ "" +current.info+"");
+                    System.out.println("Now you have " +tasklist.size()+ " tasks in the list");
+                    System.out.println(line);
+                }else if (comm.startsWith("todo")) {
                     String doit = comm.substring(5, comm.length());
-                    tasklist.add(new task('T', tasklist.size() + 1, doit, " ", ""));
+                    tasklist.add(new task('T', doit, " ", ""));
                     System.out.println(line);
                     System.out.println("Got it. I've added this task:");
                     System.out.println("  [T][ ] " + doit);
@@ -72,7 +85,7 @@ public class Duke {
                 } else if (comm.startsWith("deadline")) {
                     String doit = comm.substring(9, comm.length());
                     String[] parts = doit.split("/by");
-                    tasklist.add(new task('D', tasklist.size() + 1, parts[0], " ", parts[1]));
+                    tasklist.add(new task('D', parts[0], " ", parts[1]));
                     System.out.println(line);
                     System.out.println("Got it. I've added this task:");
                     System.out.println("   [D][ ] " + parts[0] + "(by:" + parts[1] + ")");
@@ -82,11 +95,11 @@ public class Duke {
                     String doit = comm.substring(6, comm.length());
                     String[] froms = doit.split("/from");
                     String[] tos = doit.split("/to");
-                    tasklist.add(new task('E', tasklist.size() + 1, froms[0], " ", froms[1]));
+                    tasklist.add(new task('E', froms[0], " ", froms[1]));
                     System.out.println(line);
                     System.out.println("Got it. I've added this task:");
                     System.out.println("   [E][ ] " + froms[0] + " (from:" + froms[1] + "to:" + tos[1]);
-                } else {
+                } else if (!comm.equals("bye")){
                     System.out.println(line);
                     System.out.println("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
                     System.out.println(line);
