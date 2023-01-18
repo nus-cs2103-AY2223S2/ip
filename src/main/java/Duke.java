a1ad598a50e0f137697c3c2909ab428eb649f269
DESKTOP-401ETA8\USER <adrielkee@gmail.com>
2023-01-18 13:53:13 +0800

    updates to greet echo, exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,10 @@
 import java.util.*;
 public class Duke {
     public static void main(String[] args) {
+        greet();
+        echo();
+    }
+    private static void greet(){
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -8,6 +12,8 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Duke\n What can I do for you?\n");
+    }
+    private static void echo(){
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
         while(input.equals("bye") == false){
@@ -15,11 +21,13 @@ public class Duke {
             System.out.println(input);
             System.out.println("---------------------------------------");
             input = sc.next();
-
         }
+        exit();
+    }
+    private static void exit(){
         System.out.println("---------------------------------------");
         System.out.println("Bye. Hope to see you again soon!\n");
-
     }
+
 
 }
