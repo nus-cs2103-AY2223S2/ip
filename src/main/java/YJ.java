4219e00f964871984aac690671b0c75173b5f99f
Kim Young Jin <kim@locofy.ai>
2023-01-19 00:10:49 +0800

    Do Level 3

--- src/main/java/YJ.java
+++ src/main/java/YJ.java
@@ -8,7 +8,7 @@ public class YJ {
         System.out.println("Hello! I'm YJ. What can I do for you?");
 
         // ArrayList of String
-        List<String> tasks = new ArrayList<>();
+        List<Task> tasks = new ArrayList<>();
 
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
@@ -16,12 +16,30 @@ public class YJ {
         while (!input.equals("bye")) {
             if(input.equals("list")) {
                 for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println((i + 1) + ". " + tasks.get(i));
+                    Task task = tasks.get(i);
+                    System.out.println((i + 1) + "." + task.getStatusIcon() + " " + task.description);
+                }
+            } else if(input.startsWith("mark")) {
+                Integer taskNumber = Integer.parseInt(input.split(" ")[1]);
+                if(taskNumber != null && tasks.get(taskNumber - 1) != null){
+                    Task task = tasks.get(taskNumber - 1);
+                    task.markAsDone();
+                    System.out.println("Niceoooo you're done wit this: ");
+                    System.out.println(task.getStatusIcon() + " " + task.description);
+                }
+            } else if(input.startsWith("unmark")){
+                Integer taskNumber = Integer.parseInt(input.split(" ")[1]);
+                if(taskNumber != null && tasks.get(taskNumber - 1) != null){
+                    Task task = tasks.get(taskNumber - 1);
+                    task.markAsNotDone();
+                    System.out.println("Ok lah you haven't finish this yet");
+                    System.out.println(task.getStatusIcon() + " " + task.description);
                 }
             } else {
-                tasks.add(input);
+                tasks.add(new Task(input));
                 System.out.println("Added: " + input);
             }
+
             input = sc.nextLine();
         }
 
@@ -29,3 +47,4 @@ public class YJ {
         sc.close();
     }
 }
+
