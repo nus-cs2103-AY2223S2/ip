45b933a97b7224bc90f526cbf409ddae402dddbb
Nicholas Cheong <nicholascheon@gmail.com>
2023-01-18 11:28:45 +0800

    Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello, I'm Nero and I am an automated chat bot" + "\n" + "What would you like to do?");
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!" + "\n");
+                break;
+            } else {
+                System.out.println(input + "\n");
+            }
+        }
+
     }
+
+
 }
