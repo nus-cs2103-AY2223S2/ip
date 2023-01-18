1509ecf61ee0dac1e57d7517915008d074883fcb
Saraan <saraankannusami@gmail.com>
2023-01-18 17:01:05 +0800

    Level 3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,17 @@
 import java.util.ArrayList;
-import java.util.Objects;
 import java.util.Scanner;
 
 public class Duke {
     private static final String GREETING = "Hello! I'm Duke\nWhat can I do for you?";
     private static final String GOODBYE = "Bye. Hope to see you again soon!";
-    private static final ArrayList<String> LIST = new ArrayList<>();
+    private static final ArrayList<Task> TASKS = new ArrayList<>();
+
+    /**
+     * Available commands.
+     */
+    public enum Command {
+        LIST, MARK, UNMARK, BYE
+    }
 
     /**
      * @param txt text to indent.
@@ -16,9 +22,9 @@ public class Duke {
     }
 
     /**
-     * prints aesthetic line.
+     * prints border line.
      */
-    private static void aestheticLine() {
+    private static void borderLine() {
         System.out.print("    ____________________________________________________________\n");
     }
 
@@ -27,42 +33,63 @@ public class Duke {
      * @param inp input string.
      */
     private static void prettifyOut(String inp) {
-        aestheticLine();
+        borderLine();
         System.out.println(autoIndent(inp));
-        aestheticLine();
+        borderLine();
     }
 
     /**
-     * Checks if string is terminating string.
      * @param inp input string.
-     * @return True if terminating string, false otherwise.
+     * @return False if final command.
      */
-    private static boolean checkEnd(String inp) {
-        return Objects.equals(inp.toLowerCase(), "bye");
-    }
-
-    /**
-     * Checks if string is command for list.
-     * @param inp input string.
-     * @return True if command for list, false otherwise.
-     */
-    private static boolean checkList(String inp) {
-        return Objects.equals(inp.toLowerCase(), "list");
-    }
-
-    /**
-     * Prints list of tasks.
-     */
-    private static void printList() {
-        if (LIST.isEmpty()) prettifyOut("Your list is empty!");
-        else {
-            int count = 1;
-            for (String item : LIST) {
-                System.out.println(autoIndent(count + ". " + item));
-                count++;
-            }
-
+    private static boolean executeInput(String inp) {
+        Duke.Command cmd;
+        try {
+            cmd = Duke.Command.valueOf(inp.split(" ")[0].toUpperCase());
+        } catch(IllegalArgumentException e) {
+            TASKS.add(new Task(inp));
+            borderLine();
+            System.out.println(autoIndent("added: " + inp));
+            borderLine();
+            return true;
         }
+        switch(cmd) {
+            case LIST:
+                if (TASKS.isEmpty()) prettifyOut("Your list is empty!");
+                else {
+                    int count = 1;
+                    borderLine();
+                    System.out.println(autoIndent("Here are the tasks in your list:"));
+                    for (Task item : TASKS) {
+                        System.out.println(autoIndent(count + "." + item));
+                        count++;
+                    }
+                    borderLine();
+                }
+                break;
+            case MARK:
+                int taskToMark = Integer.parseInt(inp.split(" ")[1]) - 1;
+                if (taskToMark >= TASKS.size()) {
+                    prettifyOut("Task does not exist!");
+                } else {
+                    TASKS.get(taskToMark).markAsDone();
+                    prettifyOut("Nice! I've marked this task as done:\n" + TASKS.get(taskToMark));
+                }
+                break;
+            case UNMARK:
+                int taskToUnmark = Integer.parseInt(inp.split(" ")[1]) - 1;
+                if (taskToUnmark >= TASKS.size()) {
+                    prettifyOut("Task does not exist!");
+                } else {
+                    TASKS.get(taskToUnmark).markAsUndone();
+                    prettifyOut("OK, I've marked this task as not done yet:\n" + TASKS.get(taskToUnmark));
+                }
+                break;
+            case BYE:
+                prettifyOut(GOODBYE);
+                return false;
+        }
+        return true;
     }
 
     /**
@@ -70,27 +97,18 @@ public class Duke {
      * @param args command line arguments.
      */
     public static void main(String[] args) {
+        boolean cont = true;
         prettifyOut(GREETING);
 
         Scanner input = new Scanner(System.in);
-        while (true) {
+        while (cont) {
             String userInput = input.nextLine();
             if (userInput.isBlank()) {
-                aestheticLine();
+                borderLine();
                 continue;
             }
-            if (checkEnd(userInput)) break;
 
-            aestheticLine();
-            if (checkList(userInput)) {
-                printList();
-            } else {
-                LIST.add(userInput);
-                System.out.println(autoIndent("added: " + userInput));
-            }
-            aestheticLine();
+            cont = executeInput(userInput);
         }
-
-        prettifyOut(GOODBYE);
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,36 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    /**
+     * @return the status icon.
+     */
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    /**
+     * Marks the task as done.
+     */
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    /**
+     * Marks the task as undone.
+     */
+    public void markAsUndone() {
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + this.description;
+    }
+    //...
+}
