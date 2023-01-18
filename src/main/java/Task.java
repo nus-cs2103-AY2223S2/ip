0fe7424e23ac0c3104480814afd422c0dc694d2f
alextang809 <alextang809@gmail.com>
2023-01-19 00:49:19 +0800

    add static member totalNumberOfTask add static method getTotalNumberOfTask create TaskList class to store List of Task

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,6 +1,11 @@
 public class Task {
     public static final String HORIZONTAL_LINE = "    ____________________________________________________________";
     public String description;
+    public static int totalNumberOfTask = 0;
+
+    public static int getTotalNumberOfTask() {
+        return totalNumberOfTask;
+    }
 
     public static void printHorizontalLine() {
         System.out.println(HORIZONTAL_LINE);
--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,5 +1,9 @@
 import java.util.ArrayList;
 
-public class TaskList extends Task{
+public class TaskList extends Task {
+    public static void printTaskList(TaskList taskList) {
+
+    }
+    ArrayList<Task> arrayList = new ArrayList<>();
 
 }
