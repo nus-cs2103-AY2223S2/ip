64076b41dbd7cd9f5dba148d015d10a23353f9f6
Ethan <ethanchen2009@hotmail.com>
2023-01-18 15:09:09 +0800

    Implement Duke Level-1 : Greet users, echo their input and exits upon typing bye

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,40 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    private static final String BAR =
+            "    ____________________________________________________________";
+
+    private static final String INDENTATION = "     ";
+    private static final String NEW_LINE = "\n";
+
+    private static void greet() {
+        System.out.println(BAR);
+        System.out.println(INDENTATION + "Hello! I'm Duke");
+        System.out.println(INDENTATION + "What can I do for you?");
+        System.out.println(BAR);
+        System.out.print(NEW_LINE);
+    }
+
+    private static void echo(String textToPrint) {
+        System.out.println(BAR);
+        System.out.println(INDENTATION + textToPrint);
+        System.out.println(BAR);
+        System.out.print(NEW_LINE);
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greet();
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                echo("Bye. Hope to see you again soon!");
+                scanner.close();
+                break;
+            } else {
+                echo(input);
+            }
+        }
     }
 }
