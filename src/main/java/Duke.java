da6b5fea0d43ec1b34706f7cdba75526523e0f6d
KevinEyo1 <eyokaiwenkevin@gmail.com>
2023-01-19 01:18:24 +0800

    Level-5 done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -17,13 +17,12 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println(logo + "Hello! I'm Duke. How may I be of assistance?\n");
+        System.out.println(logo + "Hello! I'm Duke. How may I be of assistance?");
         String str = sc.nextLine();
         while (true){
             String[] cmd_details = str.split(" ", 2);
             String cmd = cmd_details[0];
             String details = "";
-            boolean valid = false;
             if (str.equals("bye")) {
                 // exit
                 break;
@@ -43,52 +42,64 @@ public class Duke {
                 str = sc.nextLine();
                 continue;
             }
-            if (cmd_details.length > 1) {
+            int len = cmd_details.length;
+            if (len > 1) {
                 details = cmd_details[1];
             }
-            if (cmd.equals("mark") && pointer != -1) {
-                // mark task as done
-                Task curr = storage[Integer.valueOf(details) - 1];
-                System.out.println(pad(curr.outputMarked() + curr.toString()));
-                valid = true;
-            } else if (cmd.equals("unmark") && pointer != -1) {
-                // mark task as undone
-                Task curr = storage[Integer.valueOf(details) - 1];
-                System.out.println(pad(curr.outputUnmarked() + curr.toString()));
-                valid = true;
-            } else if (cmd.equals("todo")) {
-                pointer++;
-                Todo addition = new Todo(details);
-                storage[pointer] = addition;
-                System.out.println(pad("Understood. I have added the task:\n" + addition.toString()
-                    + "\nThere are now " + Integer.toString(pointer + 1) + " task(s) in the list."));
-                valid = true;
-            } else if (cmd.equals("deadline")) {
-                String[] temp = details.split(" /by ");
-                if (temp.length == 2) {
+            try {
+                if (cmd.equals("mark")) {
+                    if (pointer == -1 || len == 1) {
+                        throw new DukeException("Unable to mark.");
+                    }
+                    // mark task as done
+                    Task curr = storage[Integer.valueOf(details) - 1];
+                    System.out.println(pad(curr.outputMarked() + curr.toString()));
+                } else if (cmd.equals("unmark")) {
+                    if (pointer == -1 || len == 1) {
+                        throw new DukeException("Unable to unmark.");
+                    }
+                    // mark task as undone
+                    Task curr = storage[Integer.valueOf(details) - 1];
+                    System.out.println(pad(curr.outputUnmarked() + curr.toString()));
+                } else if (cmd.equals("todo")) {
+                    if (len == 1) {
+                        throw new DukeException("Description of todo cannot be empty.");
+                    }
+                    pointer++;
+                    Todo addition = new Todo(details);
+                    storage[pointer] = addition;
+                    System.out.println(pad("Understood. I have added the task:\n" + addition.toString()
+                            + "\nThere are now " + Integer.toString(pointer + 1) + " task(s) in the list."));
+                } else if (cmd.equals("deadline")) {
+                    String[] temp = details.split(" /by ");
+                    if (len == 1 || temp.length != 2) {
+                        throw new DukeException("Description of deadline cannot be incomplete.");
+                    }
                     pointer++;
                     Deadline addition = new Deadline(temp[0], temp[1]);
                     storage[pointer] = addition;
                     System.out.println(pad("Understood. I have added the task:\n" + addition.toString()
                             + "\nThere are now " + Integer.toString(pointer + 1) + " task(s) in the list."));
-                    valid = true;
-                }
-            } else if (cmd.equals("event")) {
-                String[] temp = details.split(" /from ");
-                if (temp.length == 2) {
-                    String[] temp2 = temp[1].split(" /to ");
-                    if (temp2.length == 2) {
-                        pointer++;
-                        Event addition = new Event(temp[0], temp2[0], temp2[1]);
-                        storage[pointer] = addition;
-                        System.out.println(pad("Understood. I have added the task:\n" + addition.toString()
-                                + "\nThere are now " + Integer.toString(pointer + 1) + " task(s) in the list."));
-                        valid = true;
+
+                } else if (cmd.equals("event")) {
+                    String[] temp = details.split(" /from ");
+                    if (len == 1 || temp.length != 2) {
+                        throw new DukeException("Description of event cannot be incomplete.");
                     }
+                    String[] temp2 = temp[1].split(" /to ");
+                    if (temp2.length != 2) {
+                        throw new DukeException("Description of event cannot be incomplete.");
+                    }
+                    pointer++;
+                    Event addition = new Event(temp[0], temp2[0], temp2[1]);
+                    storage[pointer] = addition;
+                    System.out.println(pad("Understood. I have added the task:\n" + addition.toString()
+                            + "\nThere are now " + Integer.toString(pointer + 1) + " task(s) in the list."));
+                } else {
+                    throw new DukeException("I do not quite understand that.");
                 }
-            }
-            if (!valid) {
-                System.out.println(pad("Invalid input."));
+            } catch (DukeException err) {
+                System.out.println(pad(err.getMessage()));
             }
             str = sc.nextLine();
         }
