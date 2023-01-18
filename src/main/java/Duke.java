9ba0632978cc8173c252a0a98d7b3b2b60968ec3
wz2k <87819279+wz2k@users.noreply.github.com>
2023-01-18 09:47:42 +0800

    feat: chatbot greeting and echoing

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,25 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello. This is Duke.");
+        Scanner scanner = new Scanner(System.in);
+        String endMessage = "Chat with Duke has ended";
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (Objects.equals(input, "bye")) {
+                reply(endMessage);
+                break;
+            }
+
+            reply(input);
+        }
+    }
+
+    public static void reply(String message) {
+        System.out.println(message);
     }
 }
