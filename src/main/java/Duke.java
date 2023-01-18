d083943e112f357ad57a5cb4891f063c60bdb77d
ZhangYQ <yuquan40@gmail.com>
2023-01-19 00:24:49 +0800

    Level 3 - added new feature of list and mark/unmark tasks in the list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,43 +1,63 @@
 import java.util.Scanner;
 public class Duke {
-    public static Integer taskCount;
+    public static Task[] taskList = new Task[100];
+    public static Integer taskCount = 0;
     public static void main(String[] args) {
-        /*
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        */
-
         //Greets User
         String lineBreak = "-----------------";
         System.out.println( lineBreak + '\n' +
-                            "Hello! I'm Panda" + '\n' +
-                            "What can I do for you?" + '\n' +
-                            lineBreak);
+                "Hello! I'm Panda" + '\n' +
+                "What can I do for you?" + '\n' +
+                lineBreak);
 
         Scanner sc = new Scanner(System.in);
         String user_input = sc.nextLine();
-
-        String[] tasks = new String[100];
         // #Edit code from here onwards#
 
-        taskCount = 0;
-        //Echo action unless user says bye
         while(!user_input.equals("bye")) {
-            if(user_input.equals("list")) {
-                System.out.println(lineBreak);
-                for(int i=0; i<taskCount; i++) {
-                    System.out.println("" + (i+1) + ". " + tasks[i]);
+            String[] inputArray = user_input.split(" ");
+            switch (inputArray[0]) {
+                case "list":
+                    if (taskList[0] == null) {
+                        System.out.println(lineBreak + '\n' +
+                                "There is currently no task in your list, trying adding some!" + '\n' +
+                                lineBreak);
+                    } else {
+                        System.out.println(lineBreak + '\n' +
+                                "Here are the tasks in your list: ");
+                        for (int i = 0; i < taskCount; i++) {
+                            System.out.println("" + (i + 1) + ". " + taskList[i]);
+                        }
+                        System.out.println(lineBreak);
+                    }
+                    break;
+                case "mark": {
+                    int taskNum = Integer.parseInt(inputArray[1]);
+                    Task currTask = taskList[taskNum - 1];
+                    currTask.check();
+                    System.out.println(lineBreak + '\n' +
+                            "Nice! I've marked this task as done:" + '\n' +
+                            currTask + '\n' +
+                            lineBreak);
+                    break;
                 }
-                System.out.println(lineBreak);
-            } else {
-                tasks[taskCount] = user_input;
-                System.out.println(lineBreak + '\n' +
-                        "added: " + user_input + '\n' +
-                        lineBreak);
-                taskCount++;
+                case "unmark": {
+                    int taskNum = Integer.parseInt(inputArray[1]);
+                    Task currTask = taskList[taskNum - 1];
+                    currTask.uncheck();
+                    System.out.println(lineBreak + '\n' +
+                            "OK, I've marked this task as not done yet:" + '\n' +
+                            currTask + '\n' +
+                            lineBreak);
+                    break;
+                }
+                default:
+                    taskList[taskCount] = new Task(user_input);
+                    System.out.println(lineBreak + '\n' +
+                            "added: " + user_input + '\n' +
+                            lineBreak);
+                    taskCount++;
+                    break;
             }
             user_input = sc.nextLine();
         }
