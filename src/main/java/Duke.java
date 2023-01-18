9002733960d063a8da159173cafa15da6a442b96
potty10 <christophertan8664@gmail.com>
2023-01-19 01:55:31 +0800

    Implement list and add feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,28 +1,42 @@
+import java.util.List;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
+
+        // Initialise variables
+        List<String> userHistory =  new ArrayList<String>();
+        Scanner scanner = new Scanner(System.in);
+        String command = "";
+        String delimiter = "\t---";
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+
         System.out.println("Hello from\n" + logo);
 
-        // Initialise variables
-        Scanner scanner = new Scanner(System.in);
-        String command = "";
-        String delimiter = "---";
         while (true) {
             command = scanner.nextLine();
             if (command.equals("bye")) {
                 System.out.println(delimiter);
-                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("\tBye. Hope to see you again soon!");
                 System.out.println(delimiter);
                 break;
             }
             System.out.println(delimiter);
-            System.out.println(command);
+            if (command.equals("list")) {
+                int i = 1;
+                for (String s: userHistory) {
+                    System.out.printf("\t%d. %s\n", i, s);
+                    i++;
+                }
+            } else {
+                System.out.printf("\tadded: %s\n", command);
+                userHistory.add(command);
+            }
             System.out.println(delimiter);
         }
     }
