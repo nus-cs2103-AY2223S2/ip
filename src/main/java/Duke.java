5f7cc70951691074a52b5a84e035e116af6c458b
axmszr <e0727266@u.nus.edu>
2023-01-19 04:14:29 +0800

    Added Level 1 functionality Changed name to Cbot (should I change the .java file name too?)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,37 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
+        String dukeLogo = " ____        _\n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+		
+		String cbotLogo = "  _____  _            _\n"
+				+ " /  ___]| |   ^-^   _| |_\n"
+				+ "|  / :D | |___  ___[_ + _]\n"
+				+ "|  \\___ | / . \\/ . \\ | |\n"
+				+ " \\_____]|_,_*_/\\_*_/ |_/\n";
+		
+		String indent = "      ~ ";
+		String prompt = " v v\n";
+		String endString = "bye";
+		String stall = "\n   o\n   o\n   o\n\n";
+		
+        System.out.println(stall + indent + "Hey! D:< I'm not\n" + dukeLogo);
+		System.out.println(indent + "I'm\n" + cbotLogo);
+		System.out.println(stall + indent
+				+ "How can I help you today?\n" + prompt);
+		
+		Scanner sc = new Scanner(System.in);
+		String userInput = sc.nextLine();
+		
+		while (!userInput.equals(endString)) {
+			System.out.println("\n" + indent + userInput + "\n" + prompt);
+			userInput = sc.nextLine();
+		}
+		
+		System.out.println(stall + indent + "See you again!");
     }
 }
