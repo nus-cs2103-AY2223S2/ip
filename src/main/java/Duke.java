09fbcf64bc4ccb0c83f077d397c1744f115f877f
gwynethguo <e0905362@u.nus.edu>
2023-01-18 09:11:32 +0800

    Add main features: Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,31 @@
+import java.util.Scanner;
 public class Duke {
+    String lines = "____________________________________________________________\n";
+    boolean exit = false;
+    String msg;
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke duke = new Duke();
+        System.out.println(duke.welcome_msg());
+        Scanner scanner = new Scanner(System.in);
+        while(!duke.exit) {
+            String inp = scanner.nextLine();
+            duke.msg = duke.add_lines(inp + "\n");
+            duke.check_msg(inp);
+            System.out.println(duke.msg);
+        }
+    }
+    String welcome_msg() {
+        return this.add_lines("Hello! I'm Duke\nWhat can I do for you?\n");
+    }
+
+    void check_msg(String inp) {
+        if (inp.equals("bye")) {
+            this.exit = true;
+            this.msg = this.add_lines("Bye. Hope to see you again soon!\n");
+        }
+    }
+
+    String add_lines(String msg) {
+        return this.lines + msg + this.lines;
     }
 }
