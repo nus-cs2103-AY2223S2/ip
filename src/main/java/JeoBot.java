18eeb9f8ec180b7ec12b81dc48092918d520a7cc
Jun-How <e0725343@u.nus.edu>
2023-01-18 13:50:56 +0800

    Implemented Level-3

--- src/main/java/JeoBot.java
+++ src/main/java/JeoBot.java
@@ -10,15 +10,62 @@ public class JeoBot {
     public static void echo() {
         String divider = "________________________________________________________________";
         boolean hasInput = true;
+        Storage st = new Storage();
         while (hasInput) {
             Scanner in = new Scanner(System.in);
             String s = in.nextLine();
+            s = s.trim();
+            String command = s.replaceAll("\\s", "").toLowerCase();
+            // Check if first word of command is "mark" or "unmark", and if an integer follows after
+            if (command.contains("unmark") && command.substring(0, 6).equals("unmark")
+                    && command.substring(6).matches("-?\\d+")) {
+                command = "unmark";
+            } else if (command.contains("mark") && command.substring(0, 4).equals("mark")
+                    && command.substring(4).matches("-?\\d+")) {
+                command = "mark";
+            }
             System.out.println(divider);
-            if (s.equals("bye")) {
-                hasInput = false;
-                System.out.println("Thank you for using my service. Hope to see you again soon!");
-            } else {
-                System.out.println(s);
+            switch (command) {
+                case "bye":
+                    hasInput = false;
+                    System.out.println("Thank you for using JeoBot. Hope to see you again soon!");
+                    break;
+                case "list":
+                    st.showTasks();
+                    break;
+                case "mark":
+                    s = s.replaceAll("\\s", "");
+                    // Check if input command given is just "mark" without an integer (no task number)
+                    if (s.substring(4).equals("")) {
+                        System.out.println("Please indicate task number!");
+                        break;
+                    }
+                    int index = Integer.parseInt(s.substring(4)) - 1;
+                    // Check if integer falls within the list of task numbers
+                    if (index >= 0 && index < st.getNumberOfTasks()) {
+                        st.markTask(index);
+                    } else {
+                        System.out.println("Invalid task number given!");
+                    }
+                    break;
+                case "unmark":
+                    s = s.replaceAll("\\s", "");
+                    // Check if input command given is just "unmark" without an integer (no task number)
+                    if (s.substring(6).equals("")) {
+                        System.out.println("Please indicate task number!");
+                        break;
+                    }
+                    index = Integer.parseInt(s.substring(6)) - 1;
+                    // Check if integer falls within the list of task numbers
+                    if (index >= 0 && index < st.getNumberOfTasks()) {
+                        st.unmarkTask(index);
+                    } else {
+                        System.out.println("Invalid task number given!");
+                    }
+                    break;
+                default:
+                    Task task = new Task(s);
+                    st.addTask(task);
             }
             System.out.println(divider);
         }
--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -1,20 +1,51 @@
 import java.util.ArrayList;
 
 public class Storage {
-    protected ArrayList<String> tasks;
+    protected ArrayList<Task> taskList;
 
     public Storage() {
-        this.tasks = new ArrayList<String>();
+        this.taskList = new ArrayList<>();
     }
 
-    public void addTask(String name) {
-        this.tasks.add(name);
-        System.out.println("added: " + name);
+    public int getNumberOfTasks() {
+        return this.taskList.size();
+    }
+
+    public void addTask(Task task) {
+        this.taskList.add(task);
+        System.out.println("Added: " + task);
+    }
+
+    public void markTask(int i) {
+        Task task = this.taskList.get(i);
+        if (task.getStatusIcon().equals("X")) {
+            System.out.println("This task is already marked as done!");
+        } else {
+            task.markAsDone();
+            System.out.println("Good job! I've marked this task as done:");
+        }
+        System.out.println(task);
+    }
+
+    public void unmarkTask(int i) {
+        Task task = this.taskList.get(i);
+        if (task.getStatusIcon().equals(" ")) {
+            System.out.println("This task is already marked as not done!");
+        } else {
+            task.unmarkFromDone();
+            System.out.println("Oh no, I've marked this task as not done yet:");
+        }
+        System.out.println(task);
     }
 
     public void showTasks() {
-        for (int i = 0; i < this.tasks.size(); i++) {
-            System.out.println(Integer.toString(i+1) + ". " + this.tasks.get(i));
+        System.out.println("Here are the tasks in your list:");
+        for (int i = 0; i < this.getNumberOfTasks(); i++) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(i+1);
+            sb.append(".");
+            sb.append(this.taskList.get(i));
+            System.out.println(sb);
         }
     }
 }
