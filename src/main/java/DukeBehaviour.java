2162d577719c44e3e6bd6856304428ec0042d3d1
szejiancheng <sze2001jc@gmail.com>
2023-01-19 04:10:32 +0800

    add Task.java class, add Level-3 functionality

--- src/main/java/DukeBehaviour.java
+++ src/main/java/DukeBehaviour.java
@@ -2,11 +2,11 @@ import java.util.ArrayList;
 
 public class DukeBehaviour {
     Boolean isActive = true;
-    ArrayList<String> taskList = new ArrayList<String>();
+    ArrayList<Task> taskList = new ArrayList<>();
 
 
     public DukeBehaviour() {
-        System.out.println("Duke constructor called...");
+        System.out.println("DukeBehaviour constructor called...");
     }
 
     public void receiveInput(String userIn){
@@ -15,6 +15,10 @@ public class DukeBehaviour {
             isActive = false;
         } else if (userIn.equals("list")){
             displayList();
+        } else if (userIn.split(" ")[0].equals("mark")) {
+            mark(userIn);
+        } else if (userIn.split(" ")[0].equals("unmark")){
+            unmark(userIn);
         }
         else {
             addTask(userIn);
@@ -25,14 +29,29 @@ public class DukeBehaviour {
         System.out.println(userIn);
     }
 
+    private void mark(String userIn) {
+        Task currTask = taskList.get(Integer.parseInt(userIn.split(" ")[1])-1);
+        currTask.markDone();
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println(currTask.getTaskView());
+    }
+
+    private void unmark(String userIn){
+        Task currTask = taskList.get(Integer.parseInt(userIn.split(" ")[1])-1);
+        currTask.unmark();
+        System.out.println("OK, I've marked this task as not done yet:");
+        System.out.println(currTask.getTaskView());
+    }
+
+
     private void addTask(String userIn){
-        taskList.add(userIn);
+        taskList.add(new Task(userIn));
         System.out.println("added: " + userIn);
     }
 
     private void displayList() {
         for (int i = 0; i<taskList.size(); i++){
-            System.out.println((i+1) + ". " + taskList.get(i));
+            System.out.println((i+1) + ". " + taskList.get(i).getTaskView());
         }
     }
 }
