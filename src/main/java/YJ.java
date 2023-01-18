f5125f806c667f1e1aaeb7cbb6befdbaa918ef3a
Kim Young Jin <kim@locofy.ai>
2023-01-18 23:40:47 +0800

    Do Level 2

--- src/main/java/YJ.java
+++ src/main/java/YJ.java
@@ -1,19 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class YJ {
     public static void main(String[] args) {
 
-        // Greet the user, simply echo commands entered by the user, and exit when the user types bye.
         System.out.println("Hello! I'm YJ. What can I do for you?");
+
+        // ArrayList of String
+        List<String> tasks = new ArrayList<>();
 
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
 
         while (!input.equals("bye")) {
-            System.out.println("YJ says: " + input);
+            if(input.equals("list")) {
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println((i + 1) + ". " + tasks.get(i));
+                }
+            } else {
+                tasks.add(input);
+                System.out.println("Added: " + input);
+            }
             input = sc.nextLine();
         }
 
+        System.out.println("Byebye, YJ will miss you :(");
         sc.close();
     }
 }
