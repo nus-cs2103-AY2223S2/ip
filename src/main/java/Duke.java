510ffffa7ef4fc8056fade56f20a3a5d81f4f6ed
randallnhr <randallnhr@gmail.com>
2023-01-18 21:31:42 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,24 +12,51 @@ public class Duke {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String word = br.readLine();
-        ArrayList<String> lst = new ArrayList<>();
+        String[] word = br.readLine().split(" ");
+        ArrayList<Task> lst = new ArrayList<>();
 
-        while (!word.equals("bye")) {
-            if (word.equals("list")) {
+        while (!word[0].equals("bye")) {
+            if (word[0].equals("list")) {
                 int curr = 1;
-                Iterator<String> iter = lst.iterator();
+                Iterator<Task> iter = lst.iterator();
                 while (iter.hasNext()) {
-                    System.out.println(curr + ". " + iter.next());
+                    System.out.println(curr + " " + iter.next());
                     curr++;
                 }
-                word = br.readLine();
+                word = br.readLine().split(" ");
+            } else if (word[0].equals("mark")) {
+                Task t = lst.get(Integer.parseInt(word[1]) - 1);
+                t.isDone = true;
+                System.out.println("Task has been marked as done:\n " + t);
+                word = br.readLine().split(" ");
+            } else if (word[0].equals("unmark")) {
+                Task t = lst.get(Integer.parseInt(word[1]) - 1);
+                t.isDone = false;
+                System.out.println("Task has been marked as not done:\n " + t);
+                word = br.readLine().split(" ");
             } else {
-                System.out.println("Added: " + word);
-                lst.add(word);
-                word = br.readLine();
+                System.out.println("Added task: " + word[0]);
+                lst.add(new Task(word[0]));
+                word = br.readLine().split(" ");
             }
         }
         System.out.println("Duke: Goodbye");
+    }
+}
+
+class Task {
+    protected String name;
+    protected boolean isDone;
+
+    public Task(String name) {
+        this.name = name;
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return isDone
+                ? "[X] " + this.name
+                : "[ ] " + this.name;
     }
 }
