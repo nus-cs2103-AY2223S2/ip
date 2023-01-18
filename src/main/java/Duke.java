75e85cb7894526d6d4b404203c6d5a5322668009
venuslimm <limjiayivenus@gmail.com>
2023-01-18 18:03:58 +0800

    complete Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,49 @@
+import java.util.*;
+
 public class Duke {
+    public static Scanner sc = new Scanner(System.in);
+    public static String horizontalLine = "________________________________\n";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = "  _____     _       _  __  U _____ u      ____     _   _    _  __  U _____ u \n"
+                + " |\" ___|U  /\"\\  u  |\"|/ /  \\| ___\"|/     |  _\"\\ U |\"|u| |  |\"|/ /  \\| ___\"|/ \n"
+                + "U| |_  u \\/ _ \\/   | ' /    |  _|\"      /| | | | \\| |\\| |  | ' /    |  _|\"   \n"
+                + "\\|  _|/  / ___ \\ U/| . \\\\u  | |___      U| |_| |\\ | |_| |U/| . \\\\u  | |___   \n"
+                + " |_|    /_/   \\_\\  |_|\\_\\   |_____|      |____/ u<<\\___/   |_|\\_\\   |_____|  \n"
+                + " )(\\\\,-  \\\\    >>,-,>> \\\\,-.<<   >>       |||_  (__) )(  ,-,>> \\\\,-.<<   >>  \n"
+                + "(__)(_/ (__)  (__)\\.)   (_/(__) (__)     (__)_)     (__)  \\.)   (_/(__) (__) \n";
+        System.out.println(horizontalLine
+                + "Hello!~ I'm the one and only\n"
+                + logo
+                + "What can I do for you?\n"
+                + horizontalLine);
+        printMenu();
+    }
+
+    /**
+     * This method does not return anything.
+     * It only continuously prints the menu until the user enters "Bye".
+     */
+    public static void printMenu() {
+        boolean exitStatus = false;
+        while(true) {
+            String input = sc.nextLine();
+            switch(input) {
+                case "bye":
+                    System.out.println(horizontalLine
+                            + "Hope I have been useful to you.\n"
+                            + "See you again soon. Bye!~\n"
+                            + horizontalLine);
+                    exitStatus = true;
+                    break;
+                default:
+                    System.out.println(horizontalLine
+                            + input + "\n"
+                            + horizontalLine);
+            }
+            if (exitStatus) {
+                break;
+            }
+        }
     }
 }
