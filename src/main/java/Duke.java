38645305df3b625ec4eebac7772c2173acef9daf
DESKTOP-4FAGDBQ\Lee Jia Xin <jxleejiaxin@gmail.com>
2023-01-18 10:50:46 +0800

    Implement Add, List: ability to store text entered by user and display them when requested

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,15 +3,41 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
 
+        //Always greets user when Duke is run
         String greeting = "Hello! I'm Duke! \n What can I do for you? \n";
         System.out.println(greeting);
 
+        //placeholder array for task list
+        Task[] taskArray = new Task[100];
+
+        //Use scanner to read input
         Scanner scanner = new Scanner(System.in);
-        String userInput = scanner.nextLine();
-        while (!userInput.equals("bye")) {
-            System.out.println("\n" + userInput + "\n");
-            userInput = scanner.nextLine();
+
+        int arrayIndex = 0; //keep track of tail end of array
+
+        while (scanner.hasNext()) {
+            String userInput = scanner.nextLine();
+            if (userInput.equals("bye")) {
+                break;
+            } else if (userInput.equals("list")) {
+                int taskCount = 1;
+                for (Task t : taskArray) {
+                    if (t == null) {
+                        break;
+                    } else {
+                        System.out.println(String.valueOf(taskCount) + ". " + t.toString() + "\n");
+                        taskCount++;
+                    }
+                }
+            } else {
+                Task currentTask = new Task(userInput);
+                taskArray[arrayIndex] = currentTask;
+                arrayIndex++;
+                System.out.println("\nadded: " + currentTask.toString() + "\n");
+            }
         }
+
+        //Exit
         System.out.println("\nBye. Hope to see you again soon!\n");
     }
 }
