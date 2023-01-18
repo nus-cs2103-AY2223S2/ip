919c4bd57b99e8a9628b5cec0eacfb0a4b826d57
PawanPatil19 <pawankp1903@gmail.com>
2023-01-19 02:31:12 +0800

    Added Level-2 increment

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,30 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 public class Duke {
     private Scanner sc = new Scanner(System.in);
+    private ArrayList<String> list = new ArrayList<>();
 
     private void inputResponse(String userInput){
+        String input;
         if(userInput.equals("bye")) {
             System.out.println("\tBye. Hope to see you again soon!");
             return;
+        } else if(userInput.equals("list")) {
+            this.displayList();
+            input = this.sc.nextLine();
+            this.inputResponse(input);
         } else {
-            System.out.println("\t" + userInput);
-            String input = sc.nextLine();
-            inputResponse(input);
+            System.out.println("\tadded: " + userInput);
+            this.list.add(userInput);
+            input = this.sc.nextLine();
+            this.inputResponse(input);
+        }
+    }
+
+    private void displayList() {
+        int listSize = this.list.size();
+        for(int i = 1; i <= listSize; i++) {
+            System.out.println("\t" + i + ". " + this.list.get(i - 1));
         }
     }
 
