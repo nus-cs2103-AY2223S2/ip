16272f4db1a494e2cac41f3106d7f9369f81f566
joytqt-1202 <joytanqiaotong@gmail.com>
2023-01-18 12:52:59 +0800

    Level-2. Adding items into list and displaying list.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,26 +3,43 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Duke {
+    private static String[] arr = new String[100];
+    private static int curr = 0;
     public static void main(String[] args) throws IOException {
         String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+                    + "|  _ \\ _   _| | _____ \n"
+                    + "| | | | | | | |/ / _ \\\n"
+                    + "| |_| | |_| |   <  __/\n"
+                    + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("---------------------------------- \n Hello from\n" + logo + "\n What can I do for you? \n ---------------------------------\n");
 
         awaitInput();
     }
 
-    public static void awaitInput() throws IOException {
+    private static void awaitInput() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String user_input = br.readLine();
-        while (!user_input.toUpperCase().equals("BYE")) {   
-            System.out.println("\n----------------------------------\n" + user_input + "\n----------------------------------\n");
-            user_input = br.readLine();
+        String userInput = br.readLine();
+        while (!userInput.equalsIgnoreCase("BYE")) {   
+            handleInput(userInput);
+            userInput = br.readLine();
         }
-        System.out.println("\n----------------------------------\n Bye! Hope to see you again!\n ----------------------------------");
+        System.out.println("\n----------------------------------\n\n Bye! Hope to see you again!\n\n----------------------------------");
+    }
+
+    private static void handleInput(String userInput) {
+        System.out.println("\n----------------------------------\n");
+        if (userInput.equalsIgnoreCase("list")) {
+            System.out.println("Current items in your list are: \n");
+            for (int i = 0; i < curr; i++) {
+                System.out.println(i+1 + ") " + arr[i]);
+            }
+        } else {
+            arr[curr] = userInput;
+            curr++;
+            System.out.println("added: " + userInput);
+        }
+        System.out.println("\n----------------------------------\n");
     }
 
 }
