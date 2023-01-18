193fe156a3e215c2b0ec8504eb8e741de5e0a8da
chunzkok <kokcz@comp.nus.edu.sg>
2023-01-18 19:01:58 +0800

    Implement 'todo' command

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Arrays;
 public class Duke {
-    private static final List<Task> toDoList = new ArrayList<>();
+    private static final List<Task> tasks = new ArrayList<>();
     public static void main(String[] args) {
         final Scanner sc = new Scanner(System.in);
         final String logo = "   _____  _  _                      \n" +
@@ -24,7 +25,7 @@ public class Duke {
 
     /**
      * Takes in a command and attempts to perform it, if valid.
-     * Accepted commands: [add], mark, unmark, list, bye
+     * Accepted commands: [add], todo, mark, unmark, list, bye
      * [add] is invoked whenever any string that does not match other commands is entered.
      * @param   command a string containing the command entered by the user
      * @return          true if programme should continue accepting further commands, else false
@@ -38,22 +39,29 @@ public class Duke {
             case "mark":
                 // todo: check if second argument is a valid number
                 prettyPrint("Great job! I've marked this task as done: ");
-                toDoList.get(Integer.parseInt(args[1]) - 1).complete();
-                prettyPrint(toDoList.get(Integer.parseInt(args[1]) - 1).toString());
+                tasks.get(Integer.parseInt(args[1]) - 1).complete();
+                prettyPrint(tasks.get(Integer.parseInt(args[1]) - 1).toString());
                 return true;
             case "unmark":
                 // todo: check if second argument is a valid number
                 prettyPrint("Aww... I've marked this task as not done yet: ");
-                toDoList.get(Integer.parseInt(args[1]) - 1).uncomplete();
-                prettyPrint(toDoList.get(Integer.parseInt(args[1]) - 1).toString());
+                tasks.get(Integer.parseInt(args[1]) - 1).uncomplete();
+                prettyPrint(tasks.get(Integer.parseInt(args[1]) - 1).toString());
+                return true;
+            case "todo":
+                prettyPrint("Got it! I've added this task:");
+                tasks.add(new ToDo(String.join(" ", Arrays.copyOfRange(args, 1, args.length))));
+                prettyPrint(tasks.get(tasks.size() - 1).toString());
+                prettyPrint(String.format("Now you have %d task%s in the list.",
+                        tasks.size(), tasks.size() > 1 ? "s" : ""));
                 return true;
             case "list":
-                for (int i = 1; i <= toDoList.size(); i++) {
-                    prettyPrint(String.format("%d. %s", i, toDoList.get(i - 1)));
+                for (int i = 1; i <= tasks.size(); i++) {
+                    prettyPrint(String.format("%d. %s", i, tasks.get(i - 1)));
                 }
                 return true;
             default:
-                toDoList.add(new Task(command));
+                tasks.add(new Task(command));
                 prettyPrint("added: " + command);
                 return true;
         }
