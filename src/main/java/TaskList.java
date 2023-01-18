c90157a930169325a49b093bcdac28c0191aae61
alextang809 <alextang809@gmail.com>
2023-01-19 01:01:18 +0800

    commment TaskList class, try to modify Task

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,9 +1,13 @@
-import java.util.ArrayList;
-
-public class TaskList extends Task {
-    public static void printTaskList(TaskList taskList) {
-
-    }
-    ArrayList<Task> arrayList = new ArrayList<>();
-
-}
+//import java.util.ArrayList;
+//
+//public class TaskList extends Task {
+//    public static void printTaskList(TaskList taskList) {
+//
+//    }
+//
+//    public static void addTaskList() {
+//
+//    }
+//    ArrayList<Task> taskList = new ArrayList<>();
+//
+//}
