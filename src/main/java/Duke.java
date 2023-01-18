3e0a5bb2a9f793f75506100110ec52814422f887
securespider <e0725031@u.nus.edu>
2023-01-19 00:59:52 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        introDuke();
+        Scanner scanner = new Scanner(System.in);
+        String word = scanner.nextLine();
+        List<String> listOfWords = new ArrayList<>();
+        while (!word.equals("bye")) {
+            System.out.println("-".repeat(20));
+            if (word.equals("list")) {
+                int count = 1;
+                for (String words: listOfWords) {
+                    System.out.println(count++ + ". " + words);
+                }
+            } else {
+                System.out.println("added " + word);
+                listOfWords.add(word);
+            }
+            System.out.println("-".repeat(20));
+            word = scanner.nextLine();
+        }
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    public static void introDuke() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -9,12 +33,6 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("What can I do for you?");
-        Scanner scanner = new Scanner(System.in);
-        String word = scanner.nextLine();
-        while (!word.equals("bye")) {
-            System.out.println(word);
-            word = scanner.nextLine();
-        }
-        System.out.println("Bye. Hope to see you again soon!");
+
     }
 }
