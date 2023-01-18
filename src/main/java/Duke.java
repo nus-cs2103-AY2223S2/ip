f4b6c887acf5e2f322e012e8ec9e6da09edabaeb
Brian Quek <astraxquek@gmail.com>
2023-01-19 01:25:11 +0800

    feat: greet, echo, exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,39 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner inputScanner = new Scanner(System.in);
+        boolean loop = true;
+
+        String tabSpace = "    ";
+        String bracket =  tabSpace + "_______________________________________________________";
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        String welcomeMsg = ("Hello from\n" + logo + "\nWhat can I do for you?");
+        System.out.println(welcomeMsg);
+
+
+        while (loop) {
+        String input = inputScanner.nextLine();
+
+        System.out.println(bracket);
+        switch(input) {
+            case "bye":
+                System.out.println(tabSpace + "Bye! See you soon!");
+                loop = false;
+                break;
+            default:
+                System.out.println(tabSpace + input);
+        }
+        System.out.println(bracket);
+
+        }
+
+        inputScanner.close();
     }
 }
