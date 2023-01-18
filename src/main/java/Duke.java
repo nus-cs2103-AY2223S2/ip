6c2d870e852e22f3e02a53c9cc92924dfcc20680
afiq <afiqzuhairi@gmail.com>
2023-01-18 17:49:19 +0800

    added add and list functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,14 +10,25 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello I'm Duke\n"
                 + "What can I do for you?");
+        String[] taskArray = new String[100];
+        int arrayIndex = 0;
         while(true) {
             Scanner sc = new Scanner(System.in);
             String input = sc.nextLine();
-            if(input.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
-                return;
-            } else {
-                System.out.println(input);
+            switch(input) {
+                case "bye":
+                    System.out.println("Bye. Hope to see you again soon!");
+                    return;
+                case "list":
+                    for(int i = 0; i < arrayIndex; i++) {
+                        int listNumber = i + 1;
+                        System.out.println(listNumber + ". " + taskArray[i]);
+                    }
+                    break;
+                default:
+                    taskArray[arrayIndex] = input;
+                    System.out.println("added: " + input);
+                    arrayIndex++;
             }
         }
     }
