3c1403f0cdae7a7525a8e6256948f3e83504dea8
LAPTOP-EG15LSJU\hungy <e0726431@u.nus.edu>
2023-01-19 01:17:46 +0800

    Added level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
@@ -8,9 +9,26 @@ public class Duke {
         System.out.println(logo + "Hello! I'm GPT-1!");
         System.out.println("What can I do for you?" + logo);
 
+        ArrayList<String> storer = new ArrayList<>();
         while (!echo.equals("bye")) {
-            echo = sc.next();
-            System.out.println(logo + echo + logo);
+            echo = sc.nextLine();
+
+            if (echo.equals("list")) {
+                System.out.println(logo);
+                for (int i = 1; i <= storer.size(); i++) {
+                    int j = i - 1;
+                    System.out.println(i + ". " + storer.get(j));
+                }
+                System.out.println(logo);
+
+            } else {
+                System.out.println(logo);
+                storer.add(echo);
+                System.out.println("added: " + echo);
+                System.out.println(logo);
+            }
+
+
         }
         System.out.println(logo + "Bye. Hope to see you again soon!" + logo);
     }
