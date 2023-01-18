cfc64a02d1ab7e749f3429262474ab33e9502ae1
4ndrelim <andre_lin@u.nus.edu>
2023-01-19 03:26:01 +0800

    Implement marking/unmarking of tasks and formatting tasks' status

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,10 +1,24 @@
 public class Task {
     private String description;
+    private boolean isDone;
 
     public Task(String des) {
         this.description = des;
+        this.isDone = false;
     }
-    
+
+    public void markTask() {
+        this.isDone = true;
+    }
+
+    public void unmarkTask() {
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return this.isDone ? "[X] " + this.getDescription() : "[] " + this.getDescription();
+    }
+
     public String getDescription() {
         return this.description;
     }
