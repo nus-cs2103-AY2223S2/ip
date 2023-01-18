aef88bdf2fb4471f9956e0c06b1a73b945faff32
kayyenl <yenyenlee9@gmail.com>
2023-01-18 20:37:28 +0900

    still working on level 4, trying to format the words properly

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -16,9 +16,22 @@ public class Duke {
                 System.out.println(formatStr(listThings(tasks)));
             } else if (splitArr[0].equals("mark") || splitArr[0].equals("unmark")) {
                 mark(splitArr[0], Integer.parseInt(splitArr[1]) - 1, tasks);
+            } else if (splitArr[0].equals("todo")) {
+                Todo newTodo = new Todo(input);
+                tasks.add(newTodo);
+                System.out.println(formatStr(addReport(newTodo, tasks)));
+            }else if (splitArr[0].equals("deadline")) {
+                Deadline newDead = new Deadline(input);
+                tasks.add(newDead);
+                System.out.println(formatStr(addReport(newDead, tasks)));
+            }else if (splitArr[0].equals("event")) {
+                Event newEvent = new Event(input);
+                tasks.add(newEvent);
+                System.out.println(formatStr(addReport(newEvent, tasks)));
             } else {
+                Task newTask = new Task(input);
                 tasks.add(new Task(input));
-                System.out.println(formatStr(addReport(input)));
+                System.out.println(formatStr(addReport(newTask, tasks)));
             }
             input = sc.nextLine();
         }
@@ -26,11 +39,58 @@ public class Duke {
         System.out.println(goodbyeMessage);
     }
 
+
     public static String formatStr(String str) {
-        String returnstr =  "############################\n"
+        String returnstr =  ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n"
                             + str + "\n"
-                            + "############################";
+                            + "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<";
         return returnstr;
+    }
+
+    public static class Todo extends Task {
+        public Todo(String content) {
+            super(content);
+        }
+
+        public String toString() {
+            String sign = "";
+            if (super.mark == false) {
+                sign = " ";
+            } else {
+                sign = "X";
+            }
+            return ". [T][" + sign + "] " + super.content;
+        }
+    }
+    public static class Deadline extends Task {
+        public Deadline(String content) {
+            super(content);
+        }
+
+        public String toString() {
+            String sign = "";
+            if (super.mark == false) {
+                sign = " ";
+            } else {
+                sign = "X";
+            }
+            return ". [D][" + sign + "] " + super.content;
+        }
+    }
+    public static class Event extends Task {
+        public Event(String content) {
+            super(content);
+        }
+
+        public String toString() {
+            String sign = "";
+            if (super.mark == false) {
+                sign = " ";
+            } else {
+                sign = "X";
+            }
+            return ". [E][" + sign + "] " + super.content;
+        }
     }
 
     public static class Task {
@@ -44,21 +104,24 @@ public class Duke {
 
         public void setMark() {
             this.mark = !this.mark;
+            String outputStr;
             if (mark == true) {
-                System.out.println("NICE! The task is completed.");
+                outputStr = "NICE! You finished this: \n"
+                + "[" + markSign(this.mark) + "] " + this.content;
             } else {
-                System.out.println("Ok, the task is not done.");
+                outputStr = "Ok, you have undone this: \n"
+                + "[" + markSign(this.mark) + "] " + this.content;
             }
+            System.out.println(formatStr(outputStr));
+        }
+
+        public String markSign(boolean markBool) {
+            if(markBool == true) return "X";
+            else return " ";
         }
 
         public String toString() {
-            String sign = "";
-            if (this.mark == false) {
-                sign = " ";
-            } else {
-                sign = "X";
-            }
-            return ". [" + sign + "] " + this.content;
+            return ". [" + markSign(this.mark) + "] " + this.content;
         }
     }
 
@@ -66,9 +129,14 @@ public class Duke {
         arrTasks.get(index).setMark();
     }
 
-    public static String addReport(String str) {
-        String returnStr = "added: " + str;
+    public static String addReport(Task task, List<Task> taskList) {
+        String returnStr = "gotcha.\nyou added: " + task.toString().substring(2) + "\n"
+                + numberOfTasks(taskList);
         return returnStr;
+    }
+
+    public static String numberOfTasks(List<Task> taskList) {
+        return "You have " + taskList.size() + " tasks in this list!";
     }
 
     public static String listThings(List<Task> arrList) {
