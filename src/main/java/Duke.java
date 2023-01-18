bb5eb08ebe05875cf09945b7e67bce374bb7d3f6
chongweiguan <75872136+chongweiguan@users.noreply.github.com>
2023-01-18 23:04:59 +0800

    Level 2 complete

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,14 +9,20 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);*/
         Scanner input = new Scanner(System.in);
+        List<String> list = new ArrayList<>();
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
         while (true) {
             String command = input.nextLine();
             if (command.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (command.equals("list")) {
+                for(int i = 1; i < list.size() + 1; i++) {
+                    System.out.println(i + ". " + list.get(i-1));
+                }
             } else {
-                System.out.println(command);
+                list.add(command);
+                System.out.println("added: " + command);
             }
         }
         input.close();
