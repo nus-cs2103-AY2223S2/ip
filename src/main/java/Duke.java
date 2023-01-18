aa2cea8e9b3e2f4723647a9360b4eca1b5913c21
“ShawnYip-Au” <shawnyipau@gmail.com>
2023-01-18 16:33:55 +0800

    Implement mark and unmark event

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -24,6 +24,12 @@ public class Duke {
             case LIST:
                 System.out.println("\nHere are the tasks in your list:\n" + printList());
                 break;
+            case MARK:
+                markEvent(s);
+                break;
+            case UNMARK:
+                unmarkEvent(s);
+                break;
             }
             
             s = askForInput();
@@ -67,6 +73,24 @@ public class Duke {
         }
 
         return EventType.ADD;
+    }
+
+    private static void markEvent(String s) {
+        String[] arr = s.split(" ");
+        int num = Integer.parseInt(arr[1]);
+
+        Task t = storedInputs.get(num-1);
+        t.markDone();
+        System.out.println("\nNice! I've marked this task as done:\n  " + t + "\n");
+    }
+
+    private static void unmarkEvent(String s) {
+        String[] arr = s.split(" ");
+        int num = Integer.parseInt(arr[1]);
+
+        Task t = storedInputs.get(num-1);
+        t.markUnDone();
+        System.out.println("\nOK, I've marked this task as not done yet:\n  " + t + "\n");
     }
 
     private static String printList() {
