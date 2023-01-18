c11ec9e02cfeb3588b986be825fe638c496f8b79
Chu Yi Ting <ytyt3902@outlook.com>
2023-01-18 13:51:09 +0800

    created simple duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,30 @@
+import java.util.Scanner;
+
 public class Duke {
+    static String greetings() {
+        String greetingsMessage = "\tHello! I'm Duke\n\tWhat can I do for you?\n";
+        return String.format("%s%s%s", horizontalLine(), greetingsMessage, horizontalLine());
+    }
+
+    static String goodbye() {
+        String goodbyeMessage = "\tBye. Hope to see you soon!\n";
+        return String.format("%s%s%s", horizontalLine(), goodbyeMessage, horizontalLine());
+    }
+
+    static String respond(String command) {
+        String response = String.format("\t%s\n", command);
+        return String.format("%s%s%s", horizontalLine(), response, horizontalLine());
+    }
+    static String horizontalLine() {
+        return "____________________________________\n";
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        System.out.println(greetings());
+        String command;
+        while (!(command = sc.nextLine()).equals("bye")) {
+            System.out.println(respond(command));
+        }
+        System.out.println(goodbye());
     }
 }
