ad9da7a34507608143edbddb43f7196526abaacf
LAPTOP-EG15LSJU\hungy <e0726431@u.nus.edu>
2023-01-19 00:58:56 +0800

    Added level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,17 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String echo = "";
+        Scanner sc = new Scanner(System.in);
+        String logo = "\n____________________________________________________________\n";
+
+        System.out.println(logo + "Hello! I'm GPT-1!");
+        System.out.println("What can I do for you?" + logo);
+
+        while (!echo.equals("bye")) {
+            echo = sc.next();
+            System.out.println(logo + echo + logo);
+        }
+        System.out.println(logo + "Bye. Hope to see you again soon!" + logo);
     }
 }
