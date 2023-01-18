6ec6377329a4f5f62c994e404756712ef1622b8d
joytqt-1202 <joytanqiaotong@gmail.com>
2023-01-18 12:30:33 +0800

    Level-1. Skeletal version of Duke (Name of chatbot to be changed)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Duke {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("---------------------------------- \n Hello from\n" + logo + "\n What can I do for you? \n ---------------------------------\n");
+
+        awaitInput();
     }
+
+    public static void awaitInput() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String user_input = br.readLine();
+        while (!user_input.toUpperCase().equals("BYE")) {   
+            System.out.println("\n----------------------------------\n" + user_input + "\n----------------------------------\n");
+            user_input = br.readLine();
+        }
+        System.out.println("\n----------------------------------\n Bye! Hope to see you again!\n ----------------------------------");
+    }
+
 }
