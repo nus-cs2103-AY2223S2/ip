a90278f2e988a6565fd2cb26f2bbff273b3aca63
CloudHill <rh.cloudhill@gmail.com>
2023-01-18 17:44:26 +0800

    renamed Duke to Sam

---
+++ src/main/java/Sam.java
@@ -0,0 +1,14 @@
+public class Sam {
+	public static void main(String[] args) {
+		String logo =
+				"\t  ██████╗ █████╗ ███╗   ███╗\n"
+			+ "\t ██╔════╝██╔══██╗████╗ ████║\n"
+			+ "\t ╚█████╗ ███████║██╔████╔██║\n"
+			+ "\t  ╚═══██╗██╔══██║██║╚██╔╝██║\n"
+			+ "\t ██████╔╝██║  ██║██║ ╚═╝ ██║\n"
+			+ "\t ╚═════╝ ╚═╝  ╚═╝╚═╝     ╚═╝";
+
+		System.out.println(logo);
+		System.out.println("Hello, I am Sam!");
+	}
+}
