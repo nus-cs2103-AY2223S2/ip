f81275be535949466a2e6125cee72eeee9f2f3d1
mandykqh <84360725+mandykqh@users.noreply.github.com>
2023-01-18 22:20:50 +0800

    Complete Level 2 - Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Duke {
     private final static String logo =
@@ -7,6 +8,7 @@ public class Duke {
             + "| |_| | |_| |   <  __/\n"
             + "|____/ \\__,_|_|\\_\\___|";
     private Scanner sc = new Scanner(System.in);
+    private ArrayList<String> inputs = new ArrayList<>(100);
     public static void main(String[] args) {
         Duke duke = new Duke();
         duke.activate();
@@ -19,7 +21,11 @@ public class Duke {
 
         String i = this.getInput();
         while(!this.saysBye(i)) {
-            this.echoText(i);
+            if(this.saysList(i)) {
+                this.printList();
+            } else {
+                this.addTask(i);
+            }
             i = this.getInput();
         }
         this.terminate();
@@ -39,11 +45,31 @@ public class Duke {
         return s.equalsIgnoreCase("bye");
     }
     public void terminate() {
-        this.printLine();
+        //this.printLine();
         System.out.println("Bye. Hope to see you again soon!");
         this.printLine();
+        sc.close();
     }
     public void printLine() {
         System.out.println("_______________________________________________________");
+    }
+    public void storeInput(String i) {
+        this.inputs.add(i);
+    }
+    public boolean saysList(String s) {
+        return s.equalsIgnoreCase("list");
+    }
+    public void printList() {
+        //this.printLine();
+        for(int i = 1; i <= this.inputs.size(); i++) {
+            System.out.println(i + ". " + this.inputs.get(i-1));
+        }
+        this.printLine();
+    }
+    public void addTask(String i) {
+        //this.printLine();
+        System.out.println("Added: " + i);
+        this.storeInput(i);
+        this.printLine();
     }
 }
