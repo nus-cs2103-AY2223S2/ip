f194ae8b8d3252696ed81aaaeb0e3b7918e8b73c
afiq <afiqzuhairi@gmail.com>
2023-01-18 17:26:18 +0800

    added greet, echo, exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,17 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("Hello I'm Duke\n"
+                + "What can I do for you?");
+        while(true) {
+            Scanner sc = new Scanner(System.in);
+            String input = sc.nextLine();
+            if(input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                return;
+            } else {
+                System.out.println(input);
+            }
+        }
     }
 }
