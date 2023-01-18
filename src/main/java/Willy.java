8bb54baf5cd7da61dd0d75952cf583205e2cdb9f
WesleyBLDC <wezzdc@gmail.com>
2023-01-19 02:52:32 +0800

    Level-2

--- src/main/java/Willy.java
+++ src/main/java/Willy.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Willy {
@@ -8,15 +11,31 @@ public class Willy {
 
         Scanner sc = new Scanner(System.in);
 
+        List<String> lst = new ArrayList<String>();
+
         while (true) {
             String command = sc.nextLine();
 
-            if (command.equals("list")) {
-                System.out.println("list\n");
-            }
             if (command.equals("blah")) {
                 System.out.println("blah\n");
             }
+
+            if (command.equals("read book")) {
+                System.out.println("added: read book\n");
+                lst.add("read book");
+            }
+            if (command.equals("return book")) {
+                System.out.println("added: return book\n");
+                lst.add("return book");
+            }
+
+            if (command.equals("list")) {
+                // System.out.println("list\n");
+                for (int index = 0; index < lst.size(); index++) {
+                    System.out.println((index + 1) + " " + lst.get(index));
+                }
+            }
+
             if (command.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 System.exit(0);
