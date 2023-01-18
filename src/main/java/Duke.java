2b2c1bc790e1e1c9eb4b77509db4d82ec3f2e2f5
HJ-PRK <e0902027@u.nus.edu>
2023-01-18 19:34:09 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.*;
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -14,21 +14,30 @@ public class Duke {
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
         System.out.println(line);
 
-        String comm = sc.nextLine();
+        String comm = "";
 
-        if(comm.equals("list")) {
-            System.out.println(line);
-            System.out.println("list");
-            System.out.println(line);
-        } else if(comm.equals("blah")) {
-            System.out.println(line);
-            System.out.println("blah");
-            System.out.println(line);
-        } else if(comm.equals("bye")) {
+        List<String> list = new ArrayList<>();
+
+        while(!comm.equals("bye")) {
+            comm = sc.nextLine();
+            if(comm.equals("list")) {
+                System.out.println(line);
+                for(int i=0; i < list.size(); i++) {
+                    int num = i+1;
+                    System.out.println(num + ". " + list.get(i) );
+                }
+                System.out.println(line);
+            } else {
+                list.add(comm);
+                System.out.println(line);
+                System.out.println("added: " + comm);
+                System.out.println(line);
+            }
+        }
             System.out.println(line);
             System.out.println("Bye. Hope to see you again soon!");
             System.out.println(line);
-        }
+
 
 
     }
