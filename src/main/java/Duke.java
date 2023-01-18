2a3e35af3719dee2c82937ddec12d4f4b41920f4
bryansendeavour <bryanojj@hotmail.com>
2023-01-18 13:05:14 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,25 +1,49 @@
 import java.io.*;
+import java.util.*;
 public class Duke {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String divider = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+    private static final String divider = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+
+    public static void welcome() {
         String welcome = "Welcome hooman!\n" +
                 "Wat u want today?\n";
-        System.out.println(divider + "Hello from\n" + logo + welcome + divider);
+        System.out.println(divider + "Hello from\n" + welcome + divider);
+    }
+
+    public static void bye() {
+        System.out.println(divider + "Ah..... okkkk nehmind. GO. BYE. :)");
+    }
+
+    public static void printList(ArrayList<String> al) {
+        ListIterator<String> li = al.listIterator();
+        if (!li.hasNext()) {
+            System.out.println("List is empty.......\n" + divider);
+        } else {
+            int count = 1;
+            while (li.hasNext()) {
+                System.out.println(count + ". " + li.next());
+                count++;
+            }
+            System.out.print(divider);
+        }
+    }
+    public static void printDefault(String s) {
+        System.out.println("added: " + s + "\n" + divider);
+    }
+    public static void main(String[] args) throws Exception {
+        welcome();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<String> al = new ArrayList<>();
         String in = br.readLine();
         String bye = "bye";
         while(!in.equalsIgnoreCase(bye)){
-            bw.write(divider + in + "\n" + divider + "\n");
-            bw.flush();
+            switch (in.toUpperCase()) {
+                case "LIST" : printList(al);
+                              break;
+                default : printDefault(in);
+                          al.add(in);
+            }
             in = br.readLine();
         }
-        bw.write(divider + "Ah..... okkkk lo nehmind. GO. BYE. :)\n" + "\n");
-        bw.flush();
+        bye();
     }
 }
