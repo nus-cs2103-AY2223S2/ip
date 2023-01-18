f25568e71635eca06b006dd1c52d8b050d34fec5
“ShawnYip-Au” <shawnyipau@gmail.com>
2023-01-18 14:30:42 +0800

    Implement store input capabilities

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,19 @@
+import java.util.List; // Import List class
+import java.util.LinkedList; //Import LinkedList class
 import java.util.Scanner;  // Import the Scanner class
 
 public class Duke {
 
     private static Scanner getInput = new Scanner(System.in); // Create a static Scanner object
 
+    private static List<String> storedInputs = new LinkedList<>(); // List to store inputs
+
     public static void main(String[] args) {
         System.out.println(intro());
         String s = askForInput();
         while(!s.equals("bye")) {
-            System.out.println("\n" + s + "\n");
+            storedInputs.add(s);
+            System.out.println("\nadded: " + s + "\n");
             s = askForInput();
         }
         System.out.println("Good Riddance!");
