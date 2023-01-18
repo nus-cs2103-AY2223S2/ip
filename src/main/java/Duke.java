2894bba7e9c53049ede9f7830b602eb11fe6ea2f
davidni <davidni055@gmail.com>
2023-01-18 13:12:19 +0800

    Update list ability to mark and unmark items from the list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,20 +8,28 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        // Allow users to add, mark and un-mark items in a list
         userInputs();
     }
 
     private static void userInputs() {
         Scanner sc = new Scanner(System.in);
-        ArrayList list = new ArrayList<>(100);
+        ArrayList<Task> list = new ArrayList<Task>(100);
 
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
 
         while (true) {
             String input = sc.nextLine();
+            String[] part = input.split(" ");
+            int index = 0;
 
-            switch (input) {
+            if (part[0].equals("mark") || part[0].equals("unmark")) {
+                index = Integer.parseInt(part[1]) - 1;
+            }
+            
+            switch (part[0]) {
                 case "bye":
                     System.out.println("Bye. Hope to see you again soon!");
                     break;
@@ -32,11 +40,41 @@ public class Duke {
                     }
                     break;
 
-                default:
-                    list.add(input);
+                case "mark":
+                    list.get(index).toBeMarked();
+                    break;
 
+                case "unmark":
+                    list.get(index).toBeUnmarked();
+                    break;
+
+                default:
+                    list.add(new Task (part[0]));
             }
         }
+    }
+}
+
+class Task {
+    private final String name;
+    private boolean checkMark;
+
+    public Task(String name) {
+        this.name = name;
+        this.checkMark = false;
+    }
+
+    public void toBeMarked() {
+        this.checkMark = true;
+    }
+
+    public void toBeUnmarked() {
+        this.checkMark = false;
+    }
+
+    @Override
+    public String toString() {
+        return (checkMark ? "[X] " : "[] ") + name;
     }
 }
 
