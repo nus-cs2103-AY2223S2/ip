e56ef7e09b50c05686811b739709b2e4b31a6980
potty10 <christophertan8664@gmail.com>
2023-01-19 00:51:17 +0800

    Add echo feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,22 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        // Initialise variables
+        Scanner scanner = new Scanner(System.in);
+        String command = "";
+        String delimiter = "---";
+        while (true) {
+            command = scanner.nextLine();
+            if (command.equals("bye")) {
+                System.out.println(delimiter);
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(delimiter);
+                break;
+            }
+            System.out.println(delimiter);
+            System.out.println(command);
+            System.out.println(delimiter);
+        }
     }
 }
