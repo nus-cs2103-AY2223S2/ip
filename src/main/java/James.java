6ed4e892b2938eaee557fda1cb6b794058430c1a
JunHao247 <onlyjunhao247@gmail.com>
2023-01-18 16:16:08 +0800

    Level-1

---
+++ src/main/java/James.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class James {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm James.");
+        System.out.println("How can I help you today?");
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you soon!");
+                break;
+            }
+            System.out.println(input);
+        }
+        scanner.close();
+    }
+}
