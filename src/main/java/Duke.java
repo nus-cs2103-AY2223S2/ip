9bdf32f0665dc762576c0260a11c272a5288414d
Oskar Lew <oskarlew98@gmail.com>
2023-01-18 21:40:12 +0800

    Level-3 Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -15,22 +15,40 @@ public class Duke {
 
     private static void chat() {
         int count = 0;
-        String[] data = new String[100];
+        Task[] data = new Task[100];
         Scanner sc = new Scanner(System.in);
         while (sc.hasNext()) {
             String echo = sc.nextLine();
-            if (echo.equals("bye")) {
+            String[] command = echo.split(" ");
+
+            if (command[0].equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             }
-            if (echo.equals("list")) {
+
+            if (command[0].equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < count; i++) {
-                    System.out.println((i+1) + ". " + data[i]);
+                    System.out.println((i+1) + "." + data[i].getStatusIcon() + " " + data[i].description);
+                }
+                continue;
+            }
+            if (command[0].equals("mark") || command[0].equals("unmark")) {
+                String action = command[0];
+                int index = Integer.parseInt(command[1]) - 1;
+                if (action.equals("mark")) {
+                    data[index].markAsDone(); 
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println(data[index].getStatusIcon() + " " + data[index].description);
+                } else {
+                    data[index].unmarkTask();
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    System.out.println(data[index].getStatusIcon() + " " + data[index].description);
                 }
                 continue;
             }
             System.out.println("added: " + echo);
-            data[count] = echo;
+            data[count] = new Task(echo);
             count++;
         }
         sc.close();
