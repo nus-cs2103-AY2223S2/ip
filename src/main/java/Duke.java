d0d9302d894e4512b2ddb361b5338c69e1c7be22
zxisatree <62459441+zxisatree@users.noreply.github.com>
2023-01-19 00:28:45 +0800

    Implemented mark and unmark commands, added Task class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String reply = "";
         TodoList todoList = new TodoList();
+        String reply = "";
 
         printTextWithLines("Hello! I'm Duke\nWhat can I do for you?");
 
@@ -15,12 +15,19 @@ public class Duke {
                 break;
             } else if (reply.equals("list")) {
                 printTextWithLines(todoList.toString());
+            } else if (reply.startsWith("mark")) {
+                int taskNumber = Integer.parseInt(reply.split(" ")[1]);
+                todoList.setDone(taskNumber, true);
+                printTextWithLines("Nice! I've marked this task as done:\n" + todoList.getTask(taskNumber));
+            } else if (reply.startsWith("unmark")) {
+                int taskNumber = Integer.parseInt(reply.split(" ")[1]);
+                todoList.setDone(taskNumber, false);
+                printTextWithLines("OK, I've marked this task as not done yet:\n" + todoList.getTask(taskNumber));
             } else {
                 todoList.addTask(reply);
                 printTextWithLines("added: " + reply);
             }
         }
-
     }
 
     static void printTextWithLines(String text) {
--- src/main/java/TodoList.java
+++ src/main/java/TodoList.java
@@ -1,16 +1,24 @@
 public class TodoList {
     // taskList is 1 indexed
-    private String[] taskList;
+    private Task[] taskList;
     private int taskCount;
 
     public TodoList() {
-        this.taskList = new String[100];
+        this.taskList = new Task[100];
         this.taskCount = 0;
     }
 
+    public Task getTask(int taskNumber) {
+        return taskList[taskNumber - 1];
+    }
+
     public void addTask(String taskDescription) {
-        this.taskList[taskCount] = taskDescription;
+        this.taskList[taskCount] = new Task(taskDescription);
         this.taskCount++;
+    }
+
+    public void setDone(int taskNumber, boolean done) {
+        taskList[taskNumber - 1].setDone(done);
     }
 
     @Override
