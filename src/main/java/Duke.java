7b9dfd207f4843d1265d79bdcc4549c3b8df4b21
Ang <angyuze6@gmail.com>
2023-01-18 18:10:29 +0800

    Level-2 Changes

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
         /* String logo = " ____        _        \n"
@@ -7,26 +9,33 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo); */
-        System.out.println("Hello! I am Munch hehe :)");
+        System.out.println("Hello! I am Munchmunch hehe :)");
         System.out.println("How may I help you?");
         System.out.println("__________________________");
-        // String word = "random";
+        ArrayList<String> inst = new ArrayList<String>();
         Boolean exit = true;
         while (exit) {
+
             Scanner text = new Scanner(System.in);
             String word = text.nextLine();
 
             if (word.equals("bye")) {
                 System.out.println("See ya champ! Enjoy your day!");
                 exit = false;
+            } else if (word.equals("list")) {
+                for(int i = 0; i < inst.size(); i++) {
+                    System.out.println((i+1) + "." + inst.get(i));
+                }
             } else {
-                System.out.println(word);
+                // adding word into list
+                inst.add(word);
+                System.out.println("Added: " + word);
             }
             System.out.println("__________________________");
         }
 
 
-        
+
 
     }
 }
