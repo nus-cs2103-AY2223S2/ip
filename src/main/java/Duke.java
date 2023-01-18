386b88b08af5dfcbd6c592389bc99eb70dfc5538
cshuwen <chenshuwen0210@gmail.com>
2023-01-18 18:28:00 +0800

    Level2, add and list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
@@ -5,9 +7,20 @@ public class Duke {
         String start_message = "Hello! I'm Duke\n" +  "What can I do for you?";
         System.out.println(start_message);
         Scanner scanner = new Scanner(System.in);
+        List<String> list = new ArrayList<String>();
         String s = scanner.nextLine();
+        int i = 1;
         while (!s.equals("bye")) {
-            System.out.println(s);
+            if (s.equals("list")){
+                i = 1;
+                for (String str: list){
+                    System.out.println(String.valueOf(i) + ". " + str);
+                    i++;
+                }
+            } else {
+                list.add(s);
+                System.out.println("added: " + s);
+            }
             s = scanner.nextLine();
         }
         System.out.println("Bye. Hope to see you again soon!");
