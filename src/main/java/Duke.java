8ad5fc416a6b679085f6ae42557481e77341ff3c
RubyNguyen07 <rubynguyen2003@gmail.com>
2023-01-18 10:38:26 +0700

    Duke can mark item as done or undone

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 
 public class Duke {
     public static void main(String[] args) throws IOException {
@@ -12,27 +14,57 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
 
 
-        String[] arr = new String[100];
-        int curr = 0;
+        ArrayList<Task> arr = new ArrayList<>();
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
         String str;
         while ((str = bf.readLine()) != null) {
-            if (str.toLowerCase().equals("bye")) {
+            str = str.toLowerCase();
+
+            if (str.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             }
 
-            if (str.toLowerCase().equals("list")) {
+            if (str.equals("list")) {
                 int cnt = 1;
-                while (cnt <= curr) {
-                    System.out.println(cnt + ". " + arr[cnt - 1]);
+                while (cnt <= arr.size()) {
+                    Task item = arr.get(cnt - 1);
+                    System.out.println(cnt + ". " + "[" +  item.getStatus() + "] " + item.taskName);
                     cnt++;
                 }
+            } else if (str.length() >= 4 && str.substring(0, 4).equals("mark")) {
+                // Handle exception here
+                int numOfTask = Integer.parseInt(str.substring(5));
+
+                // Handle when the index is out of bound -> create a separate function
+                if (numOfTask <= 0 || numOfTask > arr.size()) {
+                    System.out.println("Index of item is out of bound");
+                    continue;
+                }
+
+                Task itemToMark = arr.get(numOfTask - 1);
+                itemToMark.markDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("[X] " + itemToMark.taskName);
+
+            } else if (str.length() >= 4 && str.substring(0, 6).equals("unmark")) {
+                // Handle exception here
+                int numOfTask = Integer.parseInt(str.substring(7));
+
+                // Handle when the index is out of bound
+                if (numOfTask <= 0 || numOfTask > arr.size()) {
+                    System.out.println("Index of item is out of bound");
+                    continue;
+                }
+
+                Task itemToMark = arr.get(numOfTask - 1);
+                itemToMark.markUndone();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("[] " + itemToMark.taskName);
             } else {
-                arr[curr] = str;
+                arr.add(new Task(str));
                 System.out.println("added: " + str);
-                curr++;
             }
         }
 
---
+++ src/main/java/Task.java
@@ -0,0 +1,21 @@
+public class Task {
+    String taskName;
+    boolean isDone;
+
+    public Task(String name) {
+        this.taskName = name;
+        isDone = false;
+    }
+
+    public void markDone() {
+        isDone = true;
+    }
+
+    public void markUndone() {
+        isDone = false;
+    }
+
+    public String getStatus() {
+        return isDone ? "X" : " ";
+    }
+}
