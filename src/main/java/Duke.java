28c465814d78b002431c5903af847d1c4c287b39
4ndrelim <andre_lin@u.nus.edu>
2023-01-19 01:53:11 +0800

    Add helper method to format user input

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,12 @@
 import java.util.Scanner;
 public class Duke {
+
+    /**
+    Helper method to format response to user input
+     */
+    private static String respond(String input) {
+        return "added: " + input;
+    }
     public static void main(String[] args) {
         String welcomeMsg = "Hello i'm Duke\nWhat can I do for you?";
         String goodbyeMsg = "Bye. Hope to see you again soon!";
@@ -12,7 +19,7 @@ public class Duke {
                 System.out.println(goodbyeMsg);
                 return;
             }
-            System.out.println(echo);
+            System.out.println(respond(echo));
         }
     }
 }
