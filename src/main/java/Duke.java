cdeb4c2e202581395f31a797e4e4857b7f470f23
HJ-PRK <e0902027@u.nus.edu>
2023-01-18 19:21:13 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,35 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        //System.out.println("Hello from\n" + logo);
+
+        String line = "____________________________________________________________";
+        System.out.println(line);
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+        System.out.println(line);
+
+        String comm = sc.nextLine();
+
+        if(comm.equals("list")) {
+            System.out.println(line);
+            System.out.println("list");
+            System.out.println(line);
+        } else if(comm.equals("blah")) {
+            System.out.println(line);
+            System.out.println("blah");
+            System.out.println(line);
+        } else if(comm.equals("bye")) {
+            System.out.println(line);
+            System.out.println("Bye. Hope to see you again soon!");
+            System.out.println(line);
+        }
+
+
     }
 }
