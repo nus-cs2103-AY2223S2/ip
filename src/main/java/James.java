8cad2b2f6f0c1b21c7853dc964e3311072444a0a
JunHao247 <onlyjunhao247@gmail.com>
2023-01-18 16:59:41 +0800

    Level-1

--- src/main/java/James.java
+++ src/main/java/James.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 public class James {
     public static void main(String[] args) {
+        ArrayList<String> inputs = new ArrayList<>();
         System.out.println("Hello! I'm James.");
         System.out.println("How can I help you today?");
         Scanner scanner = new Scanner(System.in);
@@ -10,8 +12,17 @@ public class James {
                 System.out.println("Bye. Hope to see you soon!");
                 break;
             }
-            System.out.println(input);
+            if (input.equals("list")) {
+                System.out.println("List of inputs:");
+                for (int i = 0; i < inputs.size(); i++) {
+                    System.out.println((i + 1) + ". " + inputs.get(i));
+                }
+            } else {
+                inputs.add(input);
+                System.out.println("added: " + input);
+            }
         }
         scanner.close();
     }
+
 }
