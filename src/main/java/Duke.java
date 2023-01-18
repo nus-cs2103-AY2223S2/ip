4e4ad2380596c7a3b1098fc14ba295380468d341
securespider <e0725031@u.nus.edu>
2023-01-19 00:35:21 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,13 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("What can I do for you?");
+        Scanner scanner = new Scanner(System.in);
+        String word = scanner.nextLine();
+        while (!word.equals("bye")) {
+            System.out.println(word);
+            word = scanner.nextLine();
+        }
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
