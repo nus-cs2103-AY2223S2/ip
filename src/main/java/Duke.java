5280498a45a30ac4885e4e8eb215c1831ed6e6f2
DESKTOP-4FAGDBQ\Lee Jia Xin <jxleejiaxin@gmail.com>
2023-01-18 23:24:33 +0800

    Add minimal for Level 5 due to lack of time. Will improve upon in the error handling in the future.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,9 @@
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Duke {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws DukeException {
 
         //Always greets user when Duke is run
         String greeting = "Hello! I'm Duke! \n What can I do for you? \n";
@@ -20,7 +22,7 @@ public class Duke {
         //6 cases: list, mark, unmark, todo, deadline, event
         //while loop checks for user exit input("bye")
         while (!keyWord.equals("bye")) {
-            int taskIndex;
+            int taskIndex = 0;
             Task currentTask;
             StringBuilder taskDescription;
             String tempScanner;
@@ -37,54 +39,86 @@ public class Duke {
                         taskCount++;
                     }
                 }
+                if (taskCount == 1) {
+                    System.out.println("You have no task in the list.");
+                }
                 keyWord = scanner.next();
                 break;
             case "mark":
-                taskIndex = Integer.parseInt(scanner.next()) - 1;
-                currentTask = taskArray[taskIndex];
-                currentTask.markAsDone();
-                System.out.println("\n____________________________________________________________");
-                System.out.println("\nNice! I've marked this task as done:\n");
-                System.out.println(currentTask.toString());
-                System.out.println("\n____________________________________________________________\n");
-                keyWord = scanner.next();
-                break;
-            case "unmark":
-                taskIndex = Integer.parseInt(scanner.next()) - 1;
-                currentTask = taskArray[taskIndex];
-                currentTask.markAsUndone();
-                System.out.println("\n____________________________________________________________");
-                System.out.println("\nOK, I've marked this task as not done yet:\n");
-                System.out.println(currentTask.toString());
-                System.out.println("\n____________________________________________________________\n");
-                keyWord = scanner.next();
-                break;
-            case "todo": // ToDo tasks have no date/time attached
-                taskDescription = new StringBuilder(scanner.nextLine());
-                currentTask = new ToDo(taskDescription.toString());
-                taskArray[arrayIndex] = currentTask;
-                arrayIndex++;
-                addTaskReply(currentTask, arrayIndex);
-                keyWord = scanner.next();
-                break;
-            case "deadline": // Deadline tasks have one date/time
-                taskDescription = new StringBuilder();
-                tempScanner = scanner.next();
-
-                //Using while loop, append scanner input until /by to taskDescription
-                while (!tempScanner.equals("/by")) {
-                    taskDescription.append(" ").append(tempScanner);
-                    tempScanner = scanner.next();
+                try {
+                    taskIndex = scanner.nextInt() - 1;
+                    currentTask = taskArray[taskIndex];
+                    currentTask.markAsDone();
+                    System.out.println("\n____________________________________________________________");
+                    System.out.println("\nNice! I've marked this task as done:\n");
+                    System.out.println(currentTask.toString());
+                    System.out.println("\n____________________________________________________________\n");
+                    keyWord = scanner.next();
+                    break;
+                } catch (NoSuchElementException e) {
+                    System.out.println("Oops! Mark must be followed by an integer.");
                 }
+            case "unmark":
+                try {
+                    taskIndex = scanner.nextInt() - 1;
+                    currentTask = taskArray[taskIndex];
+                    currentTask.markAsUndone();
+                    System.out.println("\n____________________________________________________________");
+                    System.out.println("\nOK, I've marked this task as not done yet:\n");
+                    System.out.println(currentTask.toString());
+                    System.out.println("\n____________________________________________________________\n");
+                    keyWord = scanner.next();
+                    break;
+                } catch (NoSuchElementException e) {
+                    System.out.println("Oops! Unmark must be followed by an integer.");
+                }
+            case "todo": // ToDo tasks have no date/time attached
+                tempScanner = scanner.nextLine();
+                try {
+                    if (tempScanner.isEmpty() || tempScanner.equals(" ")) {
+                        throw new DukeException("The description of a todo cannot be empty.");
+                    } else {
+                        taskDescription = new StringBuilder(tempScanner);
+                        currentTask = new ToDo(taskDescription.toString());
+                        taskArray[arrayIndex] = currentTask;
+                        arrayIndex++;
+                        addTaskReply(currentTask, arrayIndex);
+                        keyWord = scanner.next();
+                        break;
+                    }
+                } catch (DukeException e) {
+                    System.out.println(e.toString());
+                    keyWord = scanner.next();
+                    break;
+                }
+            case "deadline": // Deadline tasks have one date/time
+                tempScanner = scanner.nextLine();
+                try {
+                    if (tempScanner.isEmpty() || tempScanner.equals(" ")) {
+                        throw new DukeException("The description of a deadline cannot be empty.");
+                    }
+                    taskDescription = new StringBuilder(tempScanner);
+                    tempScanner = scanner.next();
 
-                //Everything after /by is the deadline
-                String by = scanner.nextLine();
-                currentTask = new Deadline(taskDescription.toString(),by);
-                taskArray[arrayIndex] = currentTask;
-                arrayIndex++;
-                addTaskReply(currentTask, arrayIndex);
-                keyWord = scanner.next();
-                break;
+                    //Using while loop, append scanner input until /by to taskDescription
+                    while (!tempScanner.equals("/by")) {
+                        taskDescription.append(" ").append(tempScanner);
+                        tempScanner = scanner.next();
+                    }
+
+                    //Everything after /by is the deadline
+                    String by = scanner.nextLine();
+                    currentTask = new Deadline(taskDescription.toString(),by);
+                    taskArray[arrayIndex] = currentTask;
+                    arrayIndex++;
+                    addTaskReply(currentTask, arrayIndex);
+                    keyWord = scanner.next();
+                    break;
+                } catch (DukeException e) {
+                    System.out.println(e.toString());
+                    keyWord = scanner.next();
+                    break;
+                }
             case "event": // Event tasks have two date/times: from and to
                 taskDescription = new StringBuilder();
                 tempScanner = scanner.next();
@@ -115,7 +149,8 @@ public class Duke {
                 break;
             default:
                 //if user input does not match any case at all
-                System.out.println("Input not recognized.");
+                System.out.println("OOPS!!! I'm sorry, but I don't know what that means.");
+                keyWord = scanner.next();
             }
         }
 
@@ -130,4 +165,5 @@ public class Duke {
         System.out.println("\nNow you have " + String.valueOf(arrayIndex) + " tasks in the list.");
         System.out.println("\n____________________________________________________________\n");
     }
+
 }
