5a98ecd6813b0ddb2cfc5b4bc1ea10f11b4dc0dd
alextang809 <alextang809@gmail.com>
2023-01-18 22:45:03 +0800

    add constant HORIZONTAL_LINE

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class Duke {
+    public static final int MAX_TASK = 1000;
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -9,23 +10,41 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
-
-
         printWelcomeMessage();
+
         Scanner in = new Scanner(System.in);
         Task[] tasks = new Task[MAX_TASK];
 
-        public static void printByeMessage() {
-            Task.printHorizontalLine();
-            System.out.println("     Bye. Hope to see you again soon!");
-            Task.printHorizontalLine();
+        untilBye:
+        while (true) {
+            String msgDescription = in.nextLine();
+            switch (msgDescription) {
+                case "bye":
+                    printByeMessage();
+                    break;
+                default:
+                    printTaskMessage(msgDescription);
+                    break ;
+            }
         }
 
-        public static void printWelcomeMessage() {
-            Task.printHorizontalLine();
-            System.out.println("     Hello! I'm Duke");
-            System.out.println("     What can I do for you?");
-            Task.printHorizontalLine();
-        }
+    }
+    public static void printByeMessage() {
+        Task.printHorizontalLine();
+        System.out.println("     Bye. Hope to see you again soon!");
+        Task.printHorizontalLine();
+    }
+
+    public static void printWelcomeMessage() {
+        Task.printHorizontalLine();
+        System.out.println("     Hello! I'm Duke");
+        System.out.println("     What can I do for you?");
+        Task.printHorizontalLine();
+    }
+
+    public static void printTaskMessage(String description) {
+        Task.printHorizontalLine();
+        System.out.println(description);
+        Task.printHorizontalLine();
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,3 +1,8 @@
 public class Task {
-    
+    public static final String HORIZONTAL_LINE = "    ____________________________________________________________";
+    public String description;
+
+    public static void printHorizontalLine() {
+        System.out.println(HORIZONTAL_LINE);
+    }
 }
