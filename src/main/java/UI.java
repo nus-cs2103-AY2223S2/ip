d9def8851cbace4b62798d4e01bcff52bbdd366f
tingyic <e0735450@u.nus.edu>
2023-01-19 21:19:51 +0800

    Added UI.java

---
+++ src/main/java/UI.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+/**
+ * The skeleton version of Duke.
+ * Return appropriate replies or updates user on certain tasks
+ * depending on user input.
+ */
+public class UI {
+    /**
+     * Welcome introduction.
+     * Return the introduction message.
+     */
+    public static String introduction() {
+        return "Good day! My name is Duke. \n" + "How am I of service today?";
+    }
+
+    /**
+     * Return the departing message.
+     */
+    public static String departure() {
+        return "It has been a great pleasure serving you. \n" + "Have a nice day.";
+    }
+
+}
