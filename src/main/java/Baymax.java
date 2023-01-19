289bb77e5d9db555f2485ab5cb110e8771628980
Cyrus-Krispin <cyruskrispin@gmail.com>
2023-01-19 19:00:31 +0800

    Add, List

--- src/main/java/Baymax.java
+++ src/main/java/Baymax.java
@@ -1,24 +1,34 @@
-import java.util.Scanner;
+import java.util.*;
 public class Baymax {
     public static void main(String[] args) {
         System.out.println("Hello, I am Baymax your personal Chatbot Companion. \nWhat can I do for you today?");
-        returnSame();
+        returnValue();
         System.out.println("See you soon");
     }
-    public static void returnSame() {
+
+    public static void returnValue() {
         Scanner input = new Scanner(System.in);
         String exit = "bye";
+        String outputL = "list";
         String currentInput;
+        List<String> myList = new ArrayList<String>();
+        int Counter = 0;
         while (true) {
             currentInput = input.nextLine();
             if (exit.equals(currentInput)) {
                 break;
+            } else if (outputL.equals(currentInput)) {
+                int index = 1;
+                for (String s : myList) {
+                    System.out.println((index++) + ": " + s);
+                }
             } else {
-                System.out.println(currentInput);
+                myList.add(Counter, currentInput);
+                System.out.println("added: " + currentInput);
+                Counter++;
             }
         }
     }
-
 }
 
 
