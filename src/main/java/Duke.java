03e2e5134a31362d24f8cdb853ff21812076eeec
praveenkrishna0512 <praveenkrishna0512@gmail.com>
2023-01-20 04:31:44 +0800

    added functionality for tracking three types of tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,29 +10,34 @@ public class Duke {
         System.out.println("----------------------------------------------------");
     }
 
-    private static void addToList(String title, TaskType type) {
+    private static void addToList(String title, TaskType type, String start, String end) {
         // TODO: Handle all task types
         Task task;
         if (type == TaskType.TODO) {
             task = new ToDo(title);
         } else if (type == TaskType.DEADLINE) {
-            task = new Deadline(title, "test");
+            task = new Deadline(title, end);
         } else if (type == TaskType.EVENT) {
-            task = new Event(title, "test", "test");
+            task = new Event(title, start, end);
         } else {
             System.out.println("Something seems wrong...");
             return;
         }
         taskList.add(task);
-        System.out.println(task.toString());
+        System.out.println("Added this to your task list:");
+        System.out.println("  " + task.toString());
+        System.out.println(String.format("Number of tasks: %d", taskList.size()));
     }
 
     private static void printList() {
+        if (taskList.size() == 0) {
+            System.out.println("You have zero tasks now!");
+            return;
+        }
         System.out.println("Your tasks so far!!");
         for (int i = 0; i < taskList.size(); i++) {
             Task task = taskList.get(i);
-            String completedSubstring = task.getDone() ? "[x]" : "[]";
-            String toPrint = String.format("%d.%s %s", i + 1, completedSubstring, task.getTitle());
+            String toPrint = String.format("%d. %s", i + 1, task.toString());
             System.out.println(toPrint);
         }
     }
@@ -40,13 +45,12 @@ public class Duke {
     private static void changeTaskCompletionStatus(int taskNumber, boolean completionStatus) {
         Task task = taskList.get(taskNumber);
         task.setDone(completionStatus);
-        String completedSubstring = completionStatus ? "[x]" : "[]";
         if (completionStatus) {
             System.out.println("Solid work man! This task is marked done");
         } else {
             System.out.println("Aww what happened? This task is marked as undone");
         }
-        String toPrint = String.format("  %s %s", completedSubstring, task.getTitle());
+        String toPrint = task.toString();
         System.out.println(toPrint);
     }
 
@@ -57,7 +61,7 @@ public class Duke {
         if (commandIndex != -1) {
             // There is no space character in the command
             command = rawCommand.substring(0, commandIndex);
-            arguments = rawCommand.substring(commandIndex);
+            arguments = rawCommand.substring(commandIndex + 1);
         } else {
             command = rawCommand;
             arguments = null;
@@ -71,19 +75,26 @@ public class Duke {
                 printList();
                 break;
             case "mark":
-                changeTaskCompletionStatus(Integer.parseInt(arguments.substring(1)) - 1, true);
+                changeTaskCompletionStatus(Integer.parseInt(arguments) - 1, true);
                 break;
             case "unmark":
-                changeTaskCompletionStatus(Integer.parseInt(arguments.substring(1)) - 1, false);
+                changeTaskCompletionStatus(Integer.parseInt(arguments) - 1, false);
                 break;
             case "todo":
-                addToList(rawCommand, TaskType.TODO);
+                addToList(arguments, TaskType.TODO, null, null);
                 break;
             case "deadline":
-                addToList(rawCommand, TaskType.DEADLINE);
+                int slashIndex = arguments.indexOf('/');
+                String dateBy = arguments.substring(slashIndex + 4);
+                addToList(arguments.substring(0, slashIndex - 1), TaskType.DEADLINE, null, dateBy);
                 break;
             case "event":
-                addToList(rawCommand, TaskType.EVENT);
+                int firstSlashIndex = arguments.indexOf('/');
+                String startAndEnd = arguments.substring(firstSlashIndex + 6);
+                int secondSlashIndex = startAndEnd.indexOf('/');
+                String start = startAndEnd.substring(0, secondSlashIndex - 1);
+                String end = startAndEnd.substring(secondSlashIndex + 4);
+                addToList(arguments.substring(0, firstSlashIndex - 1), TaskType.EVENT, start, end);
                 break;
             default:
                 System.out.println("Unrecognized Command");
