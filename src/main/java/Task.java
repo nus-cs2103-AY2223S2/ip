bf02f4c45de126d1592ee800af2454e9d613708d
DESKTOP-R4TTM5F\oPandaCAT <e0411191@u.nus.edu>
2023-01-19 21:07:04 +0800

    Added class Task

---
+++ src/main/java/Task.java
@@ -0,0 +1,21 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+
+    public void markDone() {
+        this.isDone = true;
+    }
+
+    public void markUndone() {
+        this.isDone = false;
+    }
+}
