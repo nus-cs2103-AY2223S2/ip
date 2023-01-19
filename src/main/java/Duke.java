4f0dba89c8a3ed75cb287f675778f692052e54a9
LAPTOP-UOT6M2SH\manas <manasijsaha9616@gmail.com>
2023-01-19 23:31:59 +0800

    did the level 1 task

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
+import java.util.*;
 public class Duke {
+    public static Scanner sc = new Scanner(System.in);
+    public static void reply() {
+        String str = sc.nextLine();
+        if(str.equals("bye")) {
+            System.out.println("Bye mortal, I will get back to destroying" +
+                    " galaxies");
+        }
+        else {
+            System.out.println(str);
+            reply();
+        }
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String welcome_msg = "Hello my name is Thanos, my hobbies are helping people maintain their shedule and " +
+                             "destroying galaxyies.";
+        System.out.println(welcome_msg);
+        reply();
+
     }
 }
