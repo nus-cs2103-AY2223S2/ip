ca1958de9ac0d0e049219707c2b91e80cccbd4df
EksdeeeX <Bryantanzhongkang@gmail.com>
2023-01-19 17:18:30 +0800

    no message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,24 @@
-public class Duke {
+import java.util.Scanner;
 
+public class Duke {
+    private static final String intro = "Hi! I'm Duke! :)\nHow may I help?";
+    private static final String outro = "Goodbye!";
     public static void main(String[] args) {
-        String intro = 
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        System.out.println(intro);
+        System.out.println();
+        System.out.print("User: ");
+        String input = sc.next();
+        
+        while(!input.equals("bye")) {
+            System.out.print("Duke: ");
+            System.out.println(input);
+            System.out.println();
+            System.out.print("User: ");
+            input = sc.next();
+        }
+
+        System.out.print("Duke: ");
+        System.out.println(outro);
     }
 }
