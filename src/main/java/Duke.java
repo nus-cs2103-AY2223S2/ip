2b3b75b914675bcd042f395a633e7d49652f1472
Warren Yang <e0544083@u.nus.edu>
2023-01-19 14:50:34 +0800

    level 1: greet, echo, exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +7,23 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        //greet
+        System.out.println("Hello I'm Duke\nWhat can I do for you?");
+
+        Scanner takingInput = new Scanner(System.in);
+        String userTyped = takingInput.nextLine();
+
+        while (true) {
+            if (userTyped.equals("bye")) {
+                //exit
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                //echo
+                System.out.println(userTyped);
+                userTyped = takingInput.nextLine();
+            }
+        }
     }
 }
