a391604b39e65bc4091ff19f9d68b2f0fca9ac51
Mmaimer33 <rchousalkar@gmail.com>
2023-01-19 20:00:14 +0800

    Moved final values to new Values class.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,29 +1,15 @@
 import java.util.Scanner;
 
 public class Duke {
-    // Logo art generated on https://patorjk.com/software/taag/#p=display&f=Graffiti&t=PixlBot
-    private static String logo = "__________.__       .__ __________        __   \n" +
-            "\\______   \\__|__  __|  |\\______   \\ _____/  |_ \n" +
-            " |     ___/  \\  \\/  /  | |    |  _//  _ \\   __\\\n" +
-            " |    |   |  |>    <|  |_|    |   (  <_> )  |  \n" +
-            " |____|   |__/__/\\_ \\____/______  /\\____/|__|  \n" +
-            "                   \\/           \\/             ";
-
-    private static String hline = "--------------------";
-
-    // Color support obtained from https://stackoverflow.com/a/45444716
-    private static final String PURPLE = "\033[0;35m";  // PURPLE
-    private static final String RESET = "\033[0m";  // Text Reset
 
     private static void PixlPrint(String text) {
-        System.out.println(PURPLE + "PixlBot: " + RESET + text);
-        System.out.println(hline);
-        System.out.println(hline);
+        System.out.println(Values.PURPLE + "PixlBot: " + Values.RESET + text);
+        System.out.println(Values.HLINE);
     }
 
     public static void main(String[] args) {
         // Print the logo and welcome user.
-        System.out.println("Welcome to\n" + logo);
+        System.out.println("Welcome to\n" + Values.LOGO);
         System.out.println("Enter a command to start.\n");
 
         Scanner scanner = new Scanner(System.in);
@@ -37,5 +23,6 @@ public class Duke {
         }
 
         PixlPrint("Goodbye! See you again :)");
+        System.out.println(Values.HLINE);
     }
 }
---
+++ src/main/java/Values.java
@@ -0,0 +1,17 @@
+// A class to store global variable values.
+public final class Values {
+    // Logo art generated on https://patorjk.com/software/taag/#p=display&f=Graffiti&t=PixlBot
+    public static final String LOGO = "__________.__       .__ __________        __   \n" +
+            "\\______   \\__|__  __|  |\\______   \\ _____/  |_ \n" +
+            " |     ___/  \\  \\/  /  | |    |  _//  _ \\   __\\\n" +
+            " |    |   |  |>    <|  |_|    |   (  <_> )  |  \n" +
+            " |____|   |__/__/\\_ \\____/______  /\\____/|__|  \n" +
+            "                   \\/           \\/             ";
+
+    // Horizontal line for formatting
+    public static final String HLINE = "--------------------";
+
+    // Color support obtained from https://stackoverflow.com/a/45444716
+    public static final String PURPLE = "\033[0;35m";  // PURPLE
+    public static final String RESET = "\033[0m";  // Text Reset
+}
