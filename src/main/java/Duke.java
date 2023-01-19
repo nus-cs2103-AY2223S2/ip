80b65a70f7b8fdc73e74040b2d303bd8ee0c5a7f
winsonheng <winsonh5147@gmail.com>
2023-01-19 14:20:56 +0800

    Completed Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static final String FULL_LINE = "_______________________________________________\n";
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String welcomeString = "Hello I'm Duke! Type anything and I'll echo it.\n";
+        String byeString = "Bye. Hope to see you again soon!\n";
+        System.out.println(FULL_LINE + welcomeString + FULL_LINE + "\n");
+
+        String input = "";
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            input = sc.nextLine();
+            if (input.equals("bye")) {
+                System.out.println(FULL_LINE + byeString + FULL_LINE);
+                break;
+            } else {
+                System.out.println(FULL_LINE + input + "\n" + FULL_LINE);
+            }
+        }
     }
 }
