888ecd45beb0a5747936536a7d71f4b64c1a3bbd
PeanutButters93 <lowjianfeng@hotmail.com>
2023-01-19 13:45:42 +0800

    Changed built-in phrases to give Duke a formal personality. Edited the chatbox to have an indentation and max 60 character limit.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,22 @@
 import java.util.Scanner;
 
 public class Duke {
+    public static int MAXCHAR = 60;
     public static String[] phrases = {
-        "Hi I'm Duke.\nHow may I help?",
+        "I am Duke.\nHow may I be of service?",
         "Goodbye. Shutting down."
     };
 
     public static void chatbox(String text) {
-        System.out.println("____________________________________________________________");
-        System.out.println(text);
-        System.out.println("____________________________________________________________");
+        System.out.println("\t____________________________________________________________");
+        for (String substr : text.split("\n", -1)) {
+            while (substr.length() > MAXCHAR) {
+                System.out.println("\t" + substr.substring(0, MAXCHAR));
+                substr = substr.substring(MAXCHAR, substr.length());
+            }
+            System.out.println("\t" + substr);
+        }
+        System.out.println("\t____________________________________________________________");
     }
 
     public static void main(String[] args) {
