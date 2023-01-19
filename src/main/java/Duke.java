3ab70ac384f5cccd2d818933d15d401de1ca571e
CedricPei <wenqi_pei@u.nus.edu>
2023-01-19 13:03:11 +0800

    Revert "Level 1"
    This reverts commit a2b5ffd02b292764de7bb415d6d82d011aa8f8f5.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,7 @@
-import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.List;
-
 public class Duke {
-    static int exit = 0;
-    static List<String> todo = new ArrayList<>();
-
     public static void main(String[] args) {
+        int exit = 0;
         System.out.println("Hello! I'm Duke\nWhat can I do for you?\n" );
 
         while (exit != 1) {
@@ -15,16 +10,9 @@ public class Duke {
             if (info.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!\n");
                 exit = 1;
-            } else if (info.equals("list")) {
-                showList();
             } else {
-                System.out.println("added: " + info);
-                todo.add(info);
+                System.out.println(info);
             }
         }
-    }
-
-    public static void showList() {
-        todo.forEach( (s) -> System.out.println( (todo.indexOf(s)+1) +". " + s));
     }
 }
