541bb7bc320d3093a5190974eb9e24a0c0747ac0
panavdua <114715893+panavdua@users.noreply.github.com>
2023-01-19 21:51:53 +0800

    Added new subclasses of events and completed Level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,46 +14,107 @@ public class Duke {
         System.out.println("Whatsup bro");
         System.out.println("____________________________________________________________");
 
-        Task arr[] = new Task[100];
-        String word = sc.nextLine();
+        Task[] arr = new Task[100];
+        String command = sc.nextLine();
         int counter = 0;
-        while(word.compareTo("bye") != 0) {
-            Task t = new Task(word);
-            int len = word.length();
-            if (word.compareTo("list") == 0) {
-                System.out.println("____________________________________________________________");
-                System.out.println("Here are the tasks in your list:");
-                for (int i = 0; i < counter; i++) {
-                    System.out.println((i+1) + ". [" + arr[i].getStatusIcon() + "] " + arr[i]);
-                }
-                System.out.println("____________________________________________________________");
-            } else if(word.charAt(0) == 'm' && Character.isDigit(word.charAt(len-1))) {
-                int num = Integer.parseInt(String.valueOf(word.charAt(len-1)));
-                System.out.println("____________________________________________________________");
-                System.out.println("Nice! I've marked this task as done:");
-                Task cur = arr[num-1];
-                cur.markAsDone();
-                System.out.println("[" + cur.getStatusIcon() + "] " + arr[num-1]);
-                System.out.println("____________________________________________________________");
-            } else if(word.charAt(0) == 'u' && Character.isDigit(word.charAt(len-1))) {
-                int num = Integer.parseInt(String.valueOf(word.charAt(len-1)));
-                System.out.println("____________________________________________________________");
-                System.out.println("OK, I've marked this task as not done yet:");
-                Task cur = arr[num-1];
-                cur.markAsNotDone();
-                System.out.println("[" + cur.getStatusIcon() + "] " + arr[num - 1]);
-                System.out.println("____________________________________________________________");
-            } else {
-                System.out.println("____________________________________________________________");
-                System.out.println("added: " + word);
-                System.out.println("____________________________________________________________");
-                arr[counter] = t;
-                counter++;
+        while(true) {
+            String[] temp = command.split(" ");
+            String first = temp[0];
+            //Task t = new Task(command);
+            int len = command.length();
+            switch(first) {
+                case "list":
+                    System.out.println("____________________________________________________________");
+                    System.out.println("Here are the tasks in your list:");
+                    for (int i = 0; i < counter; i++) {
+                        System.out.println((i+1) + ". " + arr[i]);
+                    }
+                    System.out.println("____________________________________________________________");
+                    break;
+                case "todo":
+                    String todomessage = command.substring(5);
+                    Task todo = new ToDo(todomessage);
+                    arr[counter] = todo;
+                    counter++;
+                    System.out.println("____________________________________________________________");
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(todo);
+                    System.out.println("Now you have " + counter + " tasks in the list.");
+                    System.out.println("____________________________________________________________");
+                    break;
+                case "deadline":
+                    String by = command.substring(22);
+                    String deadlinemessage = command.substring(9, 20);
+                    Task deadline = new Deadline(deadlinemessage, by);
+                    arr[counter] = deadline;
+                    counter++;
+                    System.out.println("____________________________________________________________");
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(deadline);
+                    System.out.println("Now you have " + counter + " tasks in the list.");
+                    System.out.println("____________________________________________________________");
+                    break;
+                case "event":
+                    int fromindex = 0, toindex = 0;
+                    String eventmessage = "", from = "", to = "";
+                    for (int j = 0; j < temp.length; j++) {
+                        if (temp[j].compareTo("/from") == 0) {
+                            fromindex = j;
+                        }
+                        if (temp[j].compareTo("/to") == 0) {
+                            toindex = j;
+                        }
+                    }
+                    for (int j = 0; j < temp.length; j++) {
+                        if (j < fromindex && j > 0) {
+                            eventmessage += temp[j] + " ";
+                        }
+                        if (j > fromindex && j < toindex) {
+                            from += temp[j] + " ";
+                        }
+                        if (j > toindex) {
+                            to += temp[j];
+                        }
+                    }
+
+                    Task event = new Event(eventmessage, from, to);
+                    arr[counter] = event;
+                    counter++;
+                    System.out.println("____________________________________________________________");
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(event);
+                    System.out.println("Now you have " + counter + " tasks in the list.");
+                    System.out.println("____________________________________________________________");
+                    break;
+                case "mark":
+
+                case "unmark":
+                    int num = Integer.parseInt(String.valueOf(command.charAt(len-1)));
+                    Task cur = arr[num-1];
+                    System.out.println("____________________________________________________________");
+                    if (first.compareTo("mark") == 0) {
+                        System.out.println("Nice! I've marked this task as done:");
+                        cur.markAsDone();
+                    } else {
+                        System.out.println("OK, I've marked this task as not done yet:");
+                        cur.markAsNotDone();
+                    }
+                    System.out.println(cur);
+                    System.out.println("____________________________________________________________");
+                    break;
+                case "bye":
+                    System.out.println("____________________________________________________________");
+                    System.out.println("Bye. Hope to see you again soon!");
+                    System.out.println("____________________________________________________________");
+                    System.exit(0);
+                default:
+                    System.out.println("Please enter valid keyword");
+
+
             }
-            word = sc.nextLine();
+
+            command = sc.nextLine();
         }
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -19,6 +19,6 @@ public class Task {
 
     @Override
     public String toString() {
-        return this.description;
+        return "[" + this.getStatusIcon() + "] " + this.description;
     }
 }
