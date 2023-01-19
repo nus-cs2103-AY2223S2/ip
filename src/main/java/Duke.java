879f4ef1e318933543b309313416bb2e331b5d01
Shi Jiaao <e0372500@u.nus.edu>
2023-01-19 17:07:48 +0800

    add Level-3 requirements

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,15 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.lang.String;
 public class Duke {
+    private static String printList(ArrayList<Task> toDoList) {
+        String res = "";
+        res += "here are your tasks\n";
+        for (int i = 0; i < toDoList.size(); i++) {
+            res += String.format("%d.%s\n", i + 1, toDoList.get(i));
+        }
+        return res;
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -13,22 +22,36 @@ public class Duke {
         System.out.println("Hello! I'm Duke\n" +
                 "What can I do for you?\n");
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> toDoList = new ArrayList<String>();
+        ArrayList<Task> toDoList = new ArrayList<>();
         while (true) {
             String command = sc.nextLine();
-            if (command.equals("list")) {
-                for (int i = 0; i < toDoList.size(); i++) {
-                    System.out.println(i + 1 + ": " + toDoList.get(i));
-                }
-                continue;
+            switch (command.split(" ")[0]) {
+                case "list":
+                    System.out.println(printList(toDoList));
+                    break;
+                case "mark":
+                    int indexToMark = Character.getNumericValue
+                            (command.charAt(command.length() - 1)) - 1;
+                    toDoList.get(indexToMark).markDone();
+                    System.out.println("Nice! I've marked this task as done:\n" + "    " +
+                            toDoList.get(indexToMark));
+                    break;
+                case "unmark":
+                    indexToMark = Character.getNumericValue
+                            (command.charAt(command.length() - 1)) - 1;
+                    toDoList.get(indexToMark).markUndone();
+                    System.out.println("OK, I've marked this task as not done yet:\n" + "    " +
+                            toDoList.get(indexToMark));
+                    break;
+                case "bye":
+                    System.out.println("Bye. Hope to see you again soon!");
+                    sc.close();
+                    return;
+                default:
+                    Task curTask = new Task(command);
+                    toDoList.add(curTask);
+                    System.out.println("added: " + command);
             }
-            if (command.equals("bye")) {
-                break;
-            }
-            toDoList.add(command);
-            System.out.println("Added: " + command);
         }
-        System.out.println("Bye. Hope to see you again soon!");
-        sc.close();
     }
 }
