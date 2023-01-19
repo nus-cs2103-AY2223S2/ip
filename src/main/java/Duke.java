bccd978234a57ab2a64053cd15c77fa603b2e6b1
HAO ZEYU <823549721@qq.com>
2023-01-19 20:11:26 +0800

    Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,21 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc= new Scanner(System.in);
+        System.out.println("------------------------------------------------------------------------------");
+        System.out.println("Hello! I'm Duke\n" +
+                "What can I do for you?");
+        System.out.println("------------------------------------------------------------------------------");
+        String cur = sc.nextLine();
+        while(!cur.equals("bye")) {
+            System.out.println("------------------------------------------------------------------------------");
+            System.out.println(cur);
+            System.out.println("------------------------------------------------------------------------------");
+            cur = sc.nextLine();
+        }
+        System.out.println("------------------------------------------------------------------------------");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("------------------------------------------------------------------------------");
     }
 }
