2fae1a21a4458d355266dba43b9afe9bd6ffd326
CarrieLi1015 <e0775592@u.nus.edu>
2023-01-19 16:20:53 +0800

    Week 2_level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,33 @@
+import java.util.*;
 public class Duke {
+    private static void printDashes() {
+        System.out.println ("****************************************");
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+
+        Scanner sc = new Scanner(System.in);
+
+        printDashes();
         System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! This is Esther!");
+        System.out.println("How can I help you today? ^_^");
+        printDashes();
+
+        while(true) {
+            String userInput = sc.nextLine();
+            if(!userInput.equals("bye")) {
+                System.out.println("Your input is: " + userInput);
+                printDashes();
+            } else {
+                System.out.println("Bye. Hope to see you again soon! ^_^");
+                printDashes();
+                break;
+            }
+        }
     }
 }
