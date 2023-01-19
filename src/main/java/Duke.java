e6ecec23a4b40aaf71d90fbaa4ddb63653993b75
GlennOngJunJie <glennongjj@gmail.com>
2023-01-19 20:38:27 +0800

    level 4 done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -17,7 +17,7 @@ public class Duke {
               userInput = userInputObj.nextLine();
               if (userInput.equals("list")) {
                   for (int i = 0; i < storage.size(); i++) {
-                      System.out.println((i + 1) + ".[" + storage.get(i).getStatusIcon() + "] " + storage.get(i).description);
+                      System.out.println((i + 1) + "." + storage.get(i).toString());
                   }
               } else if (userInput.contains("mark") && userInput.substring(0,4).equals("mark")) {
                   int position = Integer.valueOf(userInput.substring(5));
@@ -27,6 +27,21 @@ public class Duke {
                   int position = Integer.valueOf(userInput.substring(7));
                   storage.get(position - 1).markAsNotDone();
                   System.out.println("OK, I've marked this task as not done yet:\n" + "[" + storage.get(position - 1).getStatusIcon() + "] " + storage.get(position - 1).description);
+              } else if (userInput.contains("todo") && userInput.substring(0,4).equals("todo")) {
+                  Todo todoTask = new Todo(userInput.substring(5));
+                  storage.add(todoTask);
+                  System.out.println("Got it. I've added this task:\n  " + todoTask.toString() + "\nNow you have " + storage.size() + " tasks in the list");
+              } else if (userInput.contains("deadline") && userInput.substring(0,8).equals("deadline")) {
+                  int position = userInput.indexOf("/by ");
+                  Deadline deadlineTask = new Deadline(userInput.substring(9, position), userInput.substring(position + 4));
+                  storage.add(deadlineTask);
+                  System.out.println("Got it. I've added this task:\n  " + deadlineTask.toString() + "\nNow you have " + storage.size() + " tasks in the list");
+              } else if (userInput.contains("event") && userInput.substring(0,5).equals("event")) {
+                  int position1 = userInput.indexOf("/from ");
+                  int position2 = userInput.indexOf("/to ");
+                  Event deadlineTask = new Event(userInput.substring(6, position1), userInput.substring(position1 + 6, position2), userInput.substring(position2 + 4));
+                  storage.add(deadlineTask);
+                  System.out.println("Got it. I've added this task:\n  " + deadlineTask.toString() + "\nNow you have " + storage.size() + " tasks in the list");
               } else {
                   Task userInputTask = new Task(userInput);
                   storage.add(userInputTask);
