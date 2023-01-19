4b64eb562a77e037105faff2153021bb469f0a05
4ndrelim <andre_lin@u.nus.edu>
2023-01-19 10:37:07 +0800

    Refactor code for farmatting and collapsing print statements

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,6 @@
 import java.util.Scanner;
 public class Duke {
 
-    /**
-    Helper method to format response to user input
-     */
-    private static String respond(String input) {
-        return "added: " + input;
-    }
     public static void main(String[] args) {
         String welcomeMsg = "Hello i'm Duke\nWhat can I do for you?";
         String goodbyeMsg = "Bye. Hope to see you again soon!";
@@ -19,26 +13,24 @@ public class Duke {
                 System.out.println(goodbyeMsg);
                 return;
             } else if (echo.equals("list")) {
-                System.out.println("Here are the tasks in your list:");
-                System.out.println(tasks.listTasks()); // list all the stored tasks
+                System.out.println("Here are the tasks in your list:\n" + tasks.listTasks()); // list all the stored tasks
             } else if (echo.matches("mark \\d+")) {
                 String[] sp = echo.split(" ");
                 if (sp.length == 2 && Integer.valueOf(sp[1]) <= tasks.size()) { // otherwise, invalid mark and ignore for now
-                    System.out.println("Nice! I've marked this task as done:");
                     String marked = tasks.toggleMark(Integer.valueOf(sp[1])-1);
-                    System.out.println(marked);
+                    System.out.println("Nice! I've marked this task as done:\n\t" + marked);
                 }
             } else if (echo.matches("unmark \\d+")) {
                 String[] sp = echo.split(" ");
-                if (sp.length == 2 && Integer.valueOf(sp[1]) <= tasks.size()) { // otherwise, invalid unmark and ignore for now
-                    System.out.println("OK, I've marked this task as not done yet:");
-                    String unmarked= tasks.toggleUnmark(Integer.valueOf(sp[1])-1);
-                    System.out.println(unmarked);
+                int taskNum = Integer.valueOf(sp[1]);
+                if (sp.length == 2 && taskNum <= tasks.size()) { // otherwise, invalid unmark and ignore for now
+                    String unmarked= tasks.toggleUnmark(taskNum-1);
+                    System.out.println("OK, I've marked this task as not done yet:\n\t" + unmarked);
                 }
-            } else {
+            } else { // add task
                 Task task = new Task(echo); // convert into a task
                 tasks.addTask(task);
-                System.out.println(respond(task.getDescription())); // can consider parking this under TaskList
+                System.out.println("Got it. I've added this task:\n\t" + task.getDescription() + "\n" + "Now you have " + tasks.size() + " task(s) in the list.");
             }
         }
     }
