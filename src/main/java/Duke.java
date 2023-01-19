399ecd67e0b5f5c5cf1b96cdd524f93ccb2bd3f5
nerdyboy98 <chewchunkiat@gmail.com>
2023-01-19 14:50:49 +0800

    Level 5. Handle Errors

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -49,29 +49,43 @@ public class Duke {
                 System.out.println("    OK, I've marked this task as not done yet:");
                 System.out.println("     " + toDoList.get(selection).toString());
             } else if (parts[0].equals("todo")){
-                Todo newToDo = new Todo(parts[1]);
-                toDoList.add(newToDo);
-                System.out.println("    Got it. I've added this task:");
-                System.out.println("     " + newToDo.toString());
-                System.out.println("    Now you have " + toDoList.size() + " tasks in the list");
+                try {
+                    Todo newToDo = new Todo(parts[1]);
+                    toDoList.add(newToDo);
+                    System.out.println("    Got it. I've added this task:");
+                    System.out.println("     " + newToDo.toString());
+                    System.out.println("    Now you have " + toDoList.size() + " tasks in the list");
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println("    OOPS!!! The description of todo cannot be empty.");
+                }
             } else if (parts[0].equals("deadline")) {
-                String [] deadlineParts = parts[1].split("/");
-                String [] deadline = deadlineParts[1].split(" ", 2);
-                Deadline newDeadline = new Deadline(deadlineParts[0], deadline[1]);
-                toDoList.add(newDeadline);
-                System.out.println("    Got it. I've added this task:");
-                System.out.println("     " + newDeadline.toString());
-                System.out.println("    Now you have " + toDoList.size() + " tasks in the list");
+                try {
+                    String[] deadlineParts = parts[1].split("/");
+                    String[] deadline = deadlineParts[1].split(" ", 2);
+                    Deadline newDeadline = new Deadline(deadlineParts[0], deadline[1]);
+                    toDoList.add(newDeadline);
+                    System.out.println("    Got it. I've added this task:");
+                    System.out.println("     " + newDeadline.toString());
+                    System.out.println("    Now you have " + toDoList.size() + " tasks in the list");
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println("    OOPS!!! The description of deadline cannot be empty.");
+                }
 
             } else if (parts[0].equals("event")) {
-                String [] eventParts = parts[1].split("/");
-                String [] from = eventParts[1].split(" ", 2);
-                String [] to = eventParts[2].split(" ", 2);
-                Event newEvent = new Event(eventParts[0], from[1], to[1]);
-                toDoList.add(newEvent);
-                System.out.println("    Got it. I've added this task:");
-                System.out.println("     " + newEvent.toString());
-                System.out.println("    Now you have " + toDoList.size() + " tasks in the list");
+                try {
+                    String[] eventParts = parts[1].split("/");
+                    String[] from = eventParts[1].split(" ", 2);
+                    String[] to = eventParts[2].split(" ", 2);
+                    Event newEvent = new Event(eventParts[0], from[1], to[1]);
+                    toDoList.add(newEvent);
+                    System.out.println("    Got it. I've added this task:");
+                    System.out.println("     " + newEvent.toString());
+                    System.out.println("    Now you have " + toDoList.size() + " tasks in the list");
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println("    OOPS!!! The description of event cannot be empty.");
+                }
+            } else {
+                System.out.println("    OOPS!!! I'm sorry, but I don't know what that means :<");
             }
 
         }
