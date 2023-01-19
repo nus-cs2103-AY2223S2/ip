754f6769448e53600add4d0c076050469f36a266
szejiancheng <sze2001jc@gmail.com>
2023-01-19 23:47:16 +0800

    Add DukeBehaviour.java javadoc comments

--- src/main/java/DukeBehaviour.java
+++ src/main/java/DukeBehaviour.java
@@ -1,14 +1,29 @@
 import java.util.ArrayList;
 
+/**
+ * Class to encapsulate behavior of personal assistant Duke!
+ * @author Sze Jian Cheng
+ */
+
 public class DukeBehaviour {
     Boolean isActive = true;
     ArrayList<Task> taskList = new ArrayList<>();
 
-
+    /**
+     * Public constructor for the DukeBehaviour Object.
+     * @param None
+     * @return None
+     */
     public DukeBehaviour() {
         //System.out.println("DukeBehaviour constructor called...");
     }
 
+    /**
+     * Public method for DukeBehaviour to receive user input in the form of Strings.
+     * @param userIn user input of type String
+     * @return None
+     * @exception DukeException on input error
+     */
     public void receiveInput(String userIn){
         try {
             if (userIn.equals("bye")) {
@@ -30,6 +45,12 @@ public class DukeBehaviour {
         }
     }
 
+    /**
+     * Private method for handling delete command.
+     * @param userIn user input of type String
+     * @return None
+     * @throws DukeException on index out-of-range
+     */
     private void delete(String userIn) throws DukeException{
         int index = Integer.parseInt(userIn.split(" ")[1]);
         if (index < 1 || index > taskList.size()){
@@ -45,7 +66,12 @@ public class DukeBehaviour {
     private void echo(String userIn){
         System.out.println(userIn);
     }
-
+    /**
+     * Private method for handling mark command.
+     * @param userIn user input of type String
+     * @return None
+     * @throws DukeException on index out-of-range
+     */
     private void mark(String userIn) throws DukeException{
         int index = Integer.parseInt(userIn.split(" ")[1]);
         if (index < 1 || index > taskList.size()){
@@ -56,18 +82,29 @@ public class DukeBehaviour {
         System.out.println("Nice! I've marked this task as done:");
         System.out.println(currTask);
     }
-
-    private void unmark(String userIn){
-        Task currTask = taskList.get(Integer.parseInt(userIn.split(" ")[1])-1);
+    /**
+     * Private method for handling unmark command.
+     * @param userIn user input of type String
+     * @return None
+     * @throws DukeException on index out-of-range
+     */
+    private void unmark(String userIn) throws DukeException{
+        int index = Integer.parseInt(userIn.split(" ")[1]);
+        if (index < 1 || index > taskList.size()){
+            throw new DukeException("index " + index +" not in range!");
+        }
+        Task currTask = taskList.get(index-1);
         currTask.unmark();
         System.out.println("OK, I've marked this task as not done yet:");
         System.out.println(currTask);
     }
-
+    /**
+     * Private method for handling all task-related commands.
+     * @param userIn user input of type String
+     * @return None
+     */
     private void addTask(String userIn){
-
         String taskType = userIn.split(" ")[0];
-        //System.out.println("tasktype: " + taskType);
         try{
             switch (taskType) {
                 case "todo":
@@ -86,7 +123,11 @@ public class DukeBehaviour {
             System.out.println(e.getMessage());
         }
     }
-
+    /**
+     * Private method for handling commands regarding Events.
+     * @param userIn user input of type String
+     * @return None
+     */
     private void addEvent(String userIn) {
         String descAndTimes = userIn.replace("event", "");
         Event newEvent = new Event(
@@ -99,7 +140,11 @@ public class DukeBehaviour {
         System.out.println(newEvent);
         System.out.println("Now you have " + taskList.size() + " tasks in the list.");
     }
-
+    /**
+     * Private method for handling commands regarding Deadlines.
+     * @param userIn user input of type String
+     * @return None
+     */
     private void addDeadline(String userIn) {
         String descAndBy = userIn.replace("Deadline", "");
         Deadline newDeadline = new Deadline(descAndBy.split("/")[0].trim(), descAndBy.split("/")[1].replace("by", "").trim());
@@ -108,7 +153,11 @@ public class DukeBehaviour {
         System.out.println(newDeadline);
         System.out.println("Now you have " + taskList.size() + " tasks in the list.");
     }
-
+    /**
+     * Private method for handling commands regarding ToDos.
+     * @param userIn user input of type String
+     * @return None
+     */
     private void addToDo(String userIn) throws DukeException{
         if (userIn.split(" ").length == 1) {
             throw new DukeException("todo cannot have no description!");
@@ -119,7 +168,10 @@ public class DukeBehaviour {
         System.out.println(newToDo);
         System.out.println("Now you have " + taskList.size() + " tasks in the list.");
     }
-
+    /**
+     * Private method for printing list of tasks stored.
+     * @return None
+     */
     private void displayList() {
         System.out.println("Here are the tasks in your list: ");
         for (int i = 0; i<taskList.size(); i++) {
