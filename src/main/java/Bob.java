a433933910d9ce29ab2d05c3c4aa240a3758d713
LeonardYam <leonardyam@Leonards-MacBook-Air.local>
2023-01-19 18:52:45 +0800

    Added a task list feature to Bob

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,26 +1,36 @@
 
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.ArrayList;
-public class Bob {
-    private static Scanner scanner = new Scanner(System.in);
 
+public class Bob {
+    private Scanner scanner = new Scanner(System.in);
+
+    private ArrayList<Task> taskList = new ArrayList<>();
     private static Integer spacing = 5;
 
-    private static String wrapper = padLeft("~".repeat(20));
+    private static String wrapper = padLeft("~".repeat(30));
 
-    // Echoes
-    private static void echo() {
+    /**
+     * Main program for Bob, our chat-bot
+     */
+    public void start() {
         String flag = "bye";
-        ArrayList<String> prevInputs = new ArrayList<String>();
+
+        //Introduction message
+        formattedPrint("Hi, my name is Bob :) \n" +
+                "How may I help you?");
+
         String input = scanner.nextLine();
 
         while (!input.equals(flag)) {
-            if (!input.equals("list")) {
-                prevInputs.add(input);
+            if (input.equals("list")) { // Output list
+                formattedPrint(taskList);
+            } else if (isMarkCommand(input)) { // Marking task
+                handleMarkCommand(input);
+            } else {  // Add to list
+                taskList.add(new Task(input));
                 formattedPrint("added: " + input);
-            // Output list
-            } else {
-                formattedPrint(prevInputs);
             }
             input = scanner.nextLine();
         }
@@ -40,14 +50,20 @@ public class Bob {
         System.out.println(wrapper);
     }
 
-    private static void formattedPrint(ArrayList<String> s) {
+    private static void formattedPrint(ArrayList<Task> s) {
         System.out.println(wrapper);
         outputList(s);
         System.out.println(wrapper);
     }
 
-    private static void outputList(ArrayList<String> list) {
-        for (String s : list) {
+    private static String getTaskStatus(Task t) {
+        return String.format("[%s] %s", t.getStatusIcon(), t.description);
+    }
+    private static void outputList(ArrayList<Task> list) {
+        // Iterate through list items sequentially
+        for (int i = 0, n = list.size(); i < n; i++) {
+            Task t = list.get(i);
+            String s = String.format("%d. %s", i + 1, getTaskStatus(t));
             System.out.println(padLeft(s));
         }
     }
@@ -59,14 +75,51 @@ public class Bob {
         }
     }
 
-    /**
-     * Runs a simple chat-bot
-     * @param args
-     */
-    public static void main(String[] args) {
-        // Introduction
-        formattedPrint("Hi! I'm Bob! \n " +
-                        "How can I help you?");
-        echo();
+    // Determine if a string can be used to mark/unmark a task in a list of size max
+    // Commands are "mark" and "unmark"
+    private boolean isMarkCommand(String s) {
+        String[] words = s.split(" ");
+        String[] markCommands = new String[] {"mark", "unmark"};
+        Integer max = taskList.size();
+
+        return words.length == 2
+                && Arrays.stream(markCommands).anyMatch(words[0]::equals) // Check if word[0] matches any command
+                && isInt(words[1]);
+    }
+
+    //Check that the string is a number
+    private static boolean isInt(String s) {
+        if (s == null) {
+            return false;
+        }
+        // Check that every char is a digit
+        for (int i = 0, len = s.length(); i < len; i++) {
+            char c = s.charAt(i);
+            if (c < '0' || c > '9') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+    private void handleMarkCommand(String s) {
+        String[] commands = s.split(" ");
+        Integer index = Integer.valueOf(commands[1]);
+
+        if (index < 1 || index >= taskList.size()) { // Index not in range
+            formattedPrint("Index given is out of range!");
+        } else {
+            Task t = taskList.get(index - 1);
+
+            if (commands[0].equals("mark")) { // mark task
+                t.mark();
+                formattedPrint("I've marked this task as done! \n" +
+                        getTaskStatus(t));
+            } else { // unmark task
+                t.unmark();
+                formattedPrint("I've unmarked this task as not done! \n" +
+                        getTaskStatus(t));
+            }
+        }
     }
 }
---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Bob bob = new Bob();
+        bob.start();
+    }
+}
