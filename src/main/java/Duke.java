d40beecc8d35329a5bbf13ae4d50bd9f12069da7
Arkobrata Chaudhuri <arkobrata.2002@gmail.com>
2023-01-19 18:42:58 +0800

    Added Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        String INDENT_LINE = "____________________________________________________________";
+
+        System.out.println(INDENT_LINE);
+        System.out.println("Hello! I'm Vincent");
+        System.out.println("What can I do for you?");
+        System.out.println(INDENT_LINE);
+
+        while (true) {
+            String command = sc.next();
+            if (!command.equals("bye")) {
+                System.out.println(INDENT_LINE);
+                System.out.println(command);
+                System.out.println(INDENT_LINE);
+            } else {
+                System.out.println(INDENT_LINE);
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(INDENT_LINE);
+                break;
+            }
+        }
     }
 }
