bd51a4427425373223f364608d47884a1bd476b5
Darren Chang <darrenchang2@gmail.com>
2023-01-19 15:15:25 +0800

    Implement Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,19 +4,27 @@ public class Duke {
 
         System.out.println("Hello! I'm Duke What can I do for you?");
         Scanner sc= new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<String>();
         boolean exit = false;
         while(!exit) {
             String str = sc.nextLine();
-            exit = echo(str);
+            exit = echo(str,list);
         }
 
     }
-    public static boolean echo(String str) {
+    public static boolean echo(String str, ArrayList<String> list) {
         if(str.equals("bye")) {
             System.out.println("Bye. Hope to see you again soon!");
             return true;
+        } else if(str.equals("list")) {
+            for (int i = 0; i < list.size(); i++) {
+                int index = i + 1;
+                System.out.println(  index + "." + list.get(i));
+            }
+            return false;
         } else {
-            System.out.println(str);
+            list.add(str);
+            System.out.println( "added:" + str);
             return false;
         }
     }
