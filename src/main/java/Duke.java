ba1a129b7cc25473a11fba4971d863b7c1fe165f
JamesLiuZX <liuzixin@comp.nus.edu.sg>
2023-01-19 17:40:05 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,52 +3,74 @@ import java.util.Scanner;
 
 public class Duke {
 
-    public static void main(String[] args) {
-        greet();
-        run();
+  public static void main(String[] args) {
+    greet();
+    run();
+  }
+
+  public static void run() {
+    DukeList records = new DukeList();
+    Scanner userInput = new Scanner(System.in);
+    while (true) {
+      String input = userInput.nextLine();
+      handleInput(input, records);
+    }
+  }
+
+  public static void handleInput(String input, DukeList d) {
+    String[] split = input.split(" ");
+    String command = split[0];
+    if (split.length > 1) {
+      Integer index = Integer.parseInt(split[1]);
+      handleMultipleInput(command, index, d);
+    }
+    else {
+      handleSingleInput(command, d);
     }
 
-    public static void run() {
-        DukeList records = new DukeList();
-        Scanner userInput = new Scanner(System.in);
-        while (true) {
-            String input = userInput.nextLine();
-            handleInput(input, records);
-        }
-    }
+  }
 
-    public static void handleInput(String input, DukeList d) {
-        switch(input) {
-            case "bye":
-                exit();
-                break;
-            case "list":
-                System.out.println(d.toString());
-                break;
-            default:
-                d.insert(input);
-                System.out.println(format(input));
-        }
+  public static void handleSingleInput(String command, DukeList d) {
+    switch (command) {
+      case "bye":
+        exit();
+        break;
+      case "list":
+        System.out.println(d.toString());
+        break;
+      default:
+        d.insert(command);
     }
+  }
 
-    public static String format(String input) {
-        return "    ____________________________________________________________\n" +
-                "    " +
-                input +
-                "\n    ____________________________________________________________";
+  public static void handleMultipleInput(String command, Integer index, DukeList d) {
+    switch (command) {
+      case "mark":
+        d.mark(index);
+        break;
+      case "unmark":
+        d.unMark(index);
+        break;
     }
+  }
 
-    public static void greet() {
-        String greeting = "    ____________________________________________________________\n" +
-                "     Hello! I'm Duke\n" +
-                "     What can I do for you?\n" +
-                "    ____________________________________________________________";
-        System.out.println(greeting);
-    }
+  public static String format(String input) {
+    return "____________________________________________________________\n" +
+            input +
+            "\n____________________________________________________________";
+  }
 
-    public static void exit() {
-        String exitMsg = format("Bye. Hope to see you again soon!");
-        System.out.println(exitMsg);
-        System.exit(1);
-    }
+  public static void greet() {
+    String greeting = "____________________________________________________________\n" +
+            "Hello! I'm Duke\n" +
+            "What can I do for you?\n" +
+            "____________________________________________________________";
+    System.out.println(greeting);
+  }
+
+  public static void exit() {
+    String exitMsg = format("Bye. Hope to see you again soon!");
+    System.out.println(exitMsg);
+    System.exit(1);
+  }
 }
--- src/main/java/DukeList.java
+++ src/main/java/DukeList.java
@@ -7,9 +7,9 @@
 import java.util.ArrayList;
 
 public class DukeList {
-  //Variables are kept private and accessed only through internal getter and setter methods.
-  private static final int size = 100;
-  private static ArrayList<String> records;
+  //Variables are kept protected and accessed only through internal getter and setter methods.
+  protected static final int size = 100;
+  protected static ArrayList<Task> records;
 
   //Constructor
   public DukeList() {
@@ -17,8 +17,35 @@ public class DukeList {
   }
 
   public static void insert(String record) {
-      records.add(record);
-      System.out.println("added: " + record);
+      Task t = new Task(record);
+      records.add(t);
+      System.out.println(Duke.format("added: " + t));
+  }
+
+  public void mark(int index) {
+    if (index < 0 || index > size) {
+      System.out.println(String.format("Indices have to be positive and less than %d.", size));
+      return;
+    }
+    Task task = records.get(index);
+    task.markDone();
+    System.out.println(Duke.format("Nice! I've marked this task as done:\n" + task.toString()));
+    return;
+  }
+
+  public void unMark(int index) {
+    if (index < 0 || index > size) {
+      System.out.println(String.format("Indices have to be positive and less than %d.", size));
+      return;
+    }
+    Task task = records.get(index);
+    task.markUndone();
+    System.out.println(Duke.format("OK, I've marked this task as not done yet:\n\n" + task.toString()));
+    return;
+  }
+
+  public Task getTask(int index) {
+    return records.get(index);
   }
 
   @Override
