a121d24a20bd50bbcfaa1553cc78ad66afe55252
DESKTOP-R4TTM5F\oPandaCAT <e0411191@u.nus.edu>
2023-01-19 23:48:58 +0800

    Set up necessary methods to be called after parser

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 public class Duke {
     private static String DIV_OPEN = "____________________________________________________________\n";
@@ -7,40 +8,46 @@ public class Duke {
     public static int listNum = 1;
     public static boolean running = true;
 
-    public static void parser(String commandLine) {
-        System.out.println(DIV_OPEN);
-        Scanner sc = new Scanner(System.in);
+    public static void parser(String commandLine) throws DukeException {
 
-        switch(commandLine) {
+        System.out.println(DIV_OPEN);
+
+        String[] command = commandLine.split(" ");
+
+        switch(command[0]) {
 
             case "list":
                 printList();
                 break;
 
             case "mark":
-                int markNum = sc.nextInt();
-                list[markNum].markDone();
-                System.out.println("Nice! I've marked this task as done:\n  " + list[markNum]);
+                if (command.length != 2) {
+                    throw new DukeException("Please check the number of your arguments!");
+                }
+                markTask(command[1]);
+
                 break;
 
             case "unmark":
-                int unmarkNum = sc.nextInt();
-                list[unmarkNum].markUndone();
-                System.out.println("Ok, I've marked this task as not done yet:\n  " + list[unmarkNum]);
+                if (command.length != 2) {
+                    throw new DukeException("Please check the number of your arguments!");
+                }
+                unmarkTask(command[1]);
                 break;
 
             case "todo":
-                String msg = sc.nextLine();
-                String todoName = msg.substring(1);
-                Todo todo = new Todo(todoName);
-                list[listNum] = todo;
-                System.out.println("Got it. I've added this task:\n  " + list[listNum]);
-                System.out.println("Now you have " + listNum + " tasks in the list.");
-                listNum++;
+                if (command.length < 2) {
+                    throw new DukeException("The description of a todo cannot be empty!");
+                }
+                addTodo(Arrays.copyOfRange(command, 1, command.length));
                 break;
 
             case "deadline":
-                msg = sc.nextLine();
+                int byIndex = Arrays.asList(command).indexOf("/by");
+                if (command.length < 4 || byIndex == -1 || byIndex == command.length - 1) {
+                    throw new DukeException("Check the format again!");
+                }
+                addDeadline(Arrays.copyOfRange(command, 1, command.length));
                 int byIndex = msg.indexOf(" /by ");
                 String dlName = msg.substring(1, byIndex);
                 String dlBy = msg.substring(byIndex + 5);
@@ -84,6 +91,54 @@ public class Duke {
         }
     }
 
+    public static void markTask(String arg) throws DukeException {
+        try {
+            int num = Integer.parseInt(arg);
+            if (num >= listNum || num <= 0) {
+                throw new DukeException("Task number is out of bounds!");
+            }
+            list[num].markDone();
+            System.out.println("Nice! I've marked this task as done:\n  " + list[num]);
+        } catch (NumberFormatException ex) {
+            throw new DukeException("Invalid number");
+        }
+    }
+
+    public static void unmarkTask(String arg) throws DukeException {
+        try {
+            int num = Integer.parseInt(arg);
+            if (num >= listNum || num <= 0) {
+                throw new DukeException("Task number is out of bounds!");
+            }
+            list[num].markUndone();
+            System.out.println("Ok, I've marked this task as not done yet:\n  " + list[num]);
+        } catch (NumberFormatException ex) {
+            throw new DukeException("Invalid number");
+        }
+    }
+
+    public static void addTodo(String[] args) {
+        int len = args.length;
+        String taskName = "";
+        for (int i = 0; i < len - 1; i++) {
+            taskName += args[i] + " ";
+        }
+        taskName += args[len];
+        Todo todo = new Todo(taskName);
+        list[listNum] = todo;
+        System.out.println("Got it. I've added this task:\n  " + list[listNum]);
+        System.out.println("Now you have " + listNum + " tasks in the list.");
+        listNum++;
+    }
+
+    public static void addDeadline(String[] args) {
+
+    }
+
+    public static void addEvent(String[] args) {
+        
+    }
+
     public static void main(String[] args) {
         // Initialize
         String logo = " ____        _        \n"
@@ -103,7 +158,7 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
         while(running) {
 
-            String msg = sc.next();
+            String msg = sc.nextLine();
 
             // Terminate Duke
             if (msg.equals("bye")) {
