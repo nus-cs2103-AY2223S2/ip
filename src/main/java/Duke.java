9c9f4fe21ca6174be482ca93c170a5917bbf0af6
lxz333 <e0909010@u.nus.edu>
2023-01-19 23:57:02 +0800

    current level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,30 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        System.out.println(greeting());
+
+        Scanner sc = new Scanner(System.in);
+        String userInput = sc.nextLine();
+
+        while (!userInput.equals("bye")) {
+            String output = echo(userInput);
+            System.out.println(output);
+            userInput = sc.nextLine();
+        }
+
+        System.out.println(ending());
+    }
+
+    public static String echo(String input) {
+        return input;
+    }
+
+    public static String ending() {
+        return "Bye~ Hope to see you again soon:)";
+    }
+
+    public static String greeting() {
+        return "Hi~ I'm Duke>_< \nWhat can I do for you?";
     }
 }
