029e86b9b27bfeb9a3a622d1bdda24c61c7f7ae0
pzaiming <zaiming.pan@gmail.com>
2023-01-19 12:40:34 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,27 +2,38 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+
     public static void main(String[] args) {
-        ArrayList<String> lst = new ArrayList<>();
+        ArrayList<Task> lst = new ArrayList<>();
         System.out.println("---------------------------------------------------------------------");
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
         System.out.println("---------------------------------------------------------------------");
         Scanner input = new Scanner(System.in);
-        String str = input.nextLine();
-        while (!str.equals("bye")) {
+        String cmd = input.nextLine();
+        while (!cmd.equals("bye")) {
             System.out.println("--------------------------------------------------------------------");
-            if (str.equals("list")) {
+            if (cmd.equals("list")) {
                 int counter = 1;
-                for (String temp : lst) {
-                    System.out.println(counter++ + ". " + temp);
+                for (Task tmp : lst) {
+                    System.out.println(counter++ + ". " + tmp.toString());
                 }
+            } else if (cmd.split(" ")[0].equals("mark")) {
+                int id = Integer.parseInt(cmd.split(" ")[1]);
+                Task t = lst.get(id-1);
+                t.mark();
+                System.out.println("Nice! I've marked this task as done:\n" + t);
+            } else if (cmd.split(" ")[0].equals("unmark")) {
+                int id = Integer.parseInt(cmd.split(" ")[1]);
+                Task t = lst.get(id-1);
+                t.unmark();
+                System.out.println("OK, I've marked this task as not done yet:\n" + t);
             } else {
-                System.out.println("added: " + str);
-                lst.add(str);
+                System.out.println("added: " + cmd);
+                Task t = new Task(cmd);
+                lst.add(t);
             }
             System.out.println("--------------------------------------------------------------------");
-            input = new Scanner(System.in);
-            str = input.nextLine();
+            cmd = input.nextLine();
         }
         System.out.println("--------------------------------------------------------------------");
         System.out.println("Bye. Hope to see you again soon!");
