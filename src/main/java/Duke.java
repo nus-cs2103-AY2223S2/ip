013bad83dcc29cc01aaf50d155608e81a6b44f34
ChickenChiang <96032660+ChickenChiang@users.noreply.github.com>
2023-01-19 21:06:10 +0800

    git tag Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,29 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+        String logo = " _____  _               _____   _\n"
+                + "|  ___|| |     _    _  |  ___| | |  _\n"
+                + "| |    | |    | |  | | | |     | |/ /\n"
+                + "| |___ | |___ | |__| | | |___  |    \\\n"
+                + "|_____||_____||______| |_____| |_| \\_\\\n";
+        System.out.println(logo);
+        System.out.println("Howdy! I'm Cluck!\n" +
+                "What can I cluck-a-doodle-do for you?\n");
+
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("Buh-cluck, see ya!");
+                break;
+            }
+            System.out.println(input);
+        }
     }
 }
