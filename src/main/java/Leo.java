dc2120d31c5807c5815d69382f8fc01038ac3148
Vigneshwar Hariharan <vigneshwar.hariharan1@gmail.com>
2023-01-19 20:45:27 +0800

    Add taskList, addTask and processRequest to handle add, list and exit.

--- src/main/java/Leo.java
+++ src/main/java/Leo.java
@@ -1,8 +1,9 @@
-import java.util.Scanner;
-
-import javax.swing.text.html.HTMLDocument.HTMLReader.HiddenAction;
+import java.util.*;
 
 public class Leo {
+
+    private ArrayList<String> taskList = new ArrayList<>();
+
     public static void main(String[] args) {
         // String logo = " ____ _ \n"
         // + "| _ \\ _ _| | _____ \n"
@@ -18,30 +19,36 @@ public class Leo {
         String cmd = sc.nextLine(); // reads in command fed by user
 
         while (!cmd.equals("bye")) {
-            printPaddedText(cmd);
+            processRequest(cmd);
             cmd = sc.nextLine();
         }
-        printGoodbye();
+        processRequest(cmd);
         sc.close();
     }
 
-    private static void printPaddedText(String cmd) {
+    private void addTask(String task) {
+        taskList.add(task);
+    }
+
+    private void processRequest(String cmd) {
         for (int i = 0; i < 25; i++) {
             System.out.print("*");
         }
         System.out.println();
-        System.out.println("\t" + cmd);
+        if (cmd.equals("bye")) {
+            System.out.println("It was nice talking, see you soon!");
+        } else if (cmd.equals("list")){
+            for (int i = 0; i < taskList.size(); i++) {
+                System.out.printf("%d) %s\n", i + 1, taskList.get(i));
+            }
+        } else {
+            addTask(cmd);
+            System.out.println("added: " + cmd);
+        }
         for (int i = 0; i < 25; i++) {
             System.out.print("*");
         }
         System.out.println();
     }
 
-    private static void printGoodbye() {
-        for (int i = 0; i < 25; i++) {
-            System.out.print("*");
-        }
-        System.out.println();
-        System.out.println("It was nice talking, see you soon!");
-    }
 }
