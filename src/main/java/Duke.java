8cc3650ebdb87d15280d7cec9257bb397cf1dd51
markusyeo <62845336+markusyeo@users.noreply.github.com>
2023-01-19 23:00:55 +0800

    A-TextUiTesting

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,8 +11,14 @@ public class Duke {
                 "║╚╝║║╚╝║║║═╣\n" +
                 "╚══╝╚══╝╚══╝\n" +
                 "            \n";
+        logo = "     |             \n" +
+            "     |  _ \\    _ \\ \n" +
+            " \\   | (   |   __/ \n" +
+            "\\___/ \\___/  \\___|\n";
 
-        String greeting = "\tHello! I'm Joe\n" +
+
+
+                String greeting = "\tHello! I'm Joe\n" +
                 "\tWhat Can I do for you?";
 
         System.out.println("Hello from\n" + logo);
