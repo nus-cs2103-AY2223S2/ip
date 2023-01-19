f38bd291736d305bf23f76e2dccabf66818873dc
evanpy <evanleewj@gmail.com>
2023-01-19 21:52:56 +0800

    Added constructor documentation

--- src/main/java/DukeException.java
+++ src/main/java/DukeException.java
@@ -1,4 +1,9 @@
 public class DukeException extends Exception {
+    /** 
+     * A public constructor to initialize DukeException instance.
+     * 
+     * @param message Error message.
+     */
     DukeException(String message) {
         super(message);
     }
