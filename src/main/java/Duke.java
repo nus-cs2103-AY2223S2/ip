c9f4b6641e678bd99f0c2bb9cea22816d6335370
praveenkrishna0512 <praveenkrishna0512@gmail.com>
2023-01-19 16:28:05 +0800

    Completed Level 1 - Greet, Echo and Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,42 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Duke {
+
+    private static Scanner sc = new Scanner(System.in);
+
+    public static void printLine() {
+        System.out.println("----------------------------------------------------");
+    }
+
+    public static boolean echoOrQuit(String command) {
+        if (Objects.equals(command, "bye")) {
+            System.out.println("Sad...Alright bye!");
+            return false;
+        }
+        System.out.println(command);
+        return true;
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        printLine();
+        System.out.println(logo);
+        System.out.println("Hope you are doing great!");
+        System.out.println("What can I do for you?");
+        printLine();
+
+        boolean promptAgain = true;
+        while (promptAgain) {
+            String command = sc.nextLine();
+            printLine();
+            promptAgain = echoOrQuit(command);
+            printLine();
+        }
     }
 }
