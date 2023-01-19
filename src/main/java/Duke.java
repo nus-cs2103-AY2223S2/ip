764f79790da541f58e96c66df74fc8d6095352de
Hmuu Myat Moe <hmuumyatmoe@gmail.com>
2023-01-19 16:15:36 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +7,18 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        System.out.println("Hi! I am Duke. How may I help you today?");
+
+        Scanner sc = new Scanner(System.in);
+
+        while(true) {
+            String cmd = sc.nextLine();
+            if (cmd.equals("bye")) {
+                System.out.println("Bye! Hope to see you again soon!\n");
+                return;
+            }
+            System.out.println(cmd + "\n");
+        }
     }
 }
