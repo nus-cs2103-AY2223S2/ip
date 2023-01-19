fd47eb5e0852ecc4d745233e4418cb5c0e4e3c8d
noahxinjie <noahxinjie@gmail.com>
2023-01-19 17:16:21 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,20 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Chiwa, your personal chatbot (◔◡◔✿)");
+        System.out.println("What can I do for you today?");
+
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("Bye~ Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println(input);
+            }
+        }
+        scanner.close();
     }
 }
