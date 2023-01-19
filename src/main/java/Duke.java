39038694a1d5748a0a2edb3cf4279ea8afaeca07
Statspadders <weihao_toh@rocketmail.com>
2023-01-19 23:16:10 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,21 @@
+import java.util.*;
+import java.io.*;
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
+    public static void main(String[] args) throws IOException {
+        /* String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+                + "|____/ \\__,_|_|\\_\\___|\n"; */
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String text = "";
+        while( (text = br.readLine()) != "bye") {
+            System.out.println(text);
+        }
+        System.out.println("Bye. Hope to see you again soon!");
+
+
     }
 }
