b143836fe17d3ac88db59cc54aec71a10afc7df4
jeraldkiew <jeraldkiew007@gmail.com>
2023-01-19 18:46:53 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,23 +2,26 @@ import java.util.*;
 
 public class Duke {
     public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
-//        System.out.println("Hello from\n" + logo);
-
         System.out.println("Leo: Yoooz it's your boy Leo! What's up?");
         Scanner sc = new Scanner(System.in);
+        String[] strArr = new String[100];
+        int count = 0;
         while(true) {
             System.out.print("You: ");
             String str = sc.nextLine();
             if(str.equals("bye")) {
                 System.out.println("Leo: Good talk man, catch you again some other time!");
                 break;
+            } else if(str.equals("list")) {
+                System.out.println("Leo: Here you go!");
+                for(int i = 0; i < count; i++) {
+                    System.out.println(i + 1 + ". " + strArr[i]);
+                }
+            } else {
+                strArr[count] = str;
+                count++;
+                System.out.println("Leo: added '" + str + "'");
             }
-            System.out.println("Leo: " + str);
         }
     }
 }
