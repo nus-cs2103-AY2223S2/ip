c9d6fd2d50bb03859129cfb9a02b04ee44e34e77
jacques <jacquesliaozeli@hotmail.com>
2023-01-19 15:23:30 +0800

    duke with list, level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,24 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> inputList = new ArrayList<>();
 
         System.out.println("Hello, I am Duke. \nWhat can I do for you?");
 
         String input = scanner.nextLine();
 
-        while (!input.equals("bye")) {
-            System.out.println(input);
+        while (!input.equalsIgnoreCase("bye")) {
+            if (input.equalsIgnoreCase("list")) {
+                for (int i = 0; i < inputList.size(); i++) {
+                    System.out.println(i+1 + ". " + inputList.get(i));
+                }
+            } else {
+                System.out.println("added: " + input);
+                inputList.add(input);
+            }
             input = scanner.nextLine();
         }
 
