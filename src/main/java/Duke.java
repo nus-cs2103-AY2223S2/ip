f202d7bec16d899576c5a05377b9c49158a813f5
KSunil2001 <k.sunil.avinash@gmail.com>
2023-01-19 15:12:44 +0800

    Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static void printReply(String message){
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\t" + message);
+        System.out.println("\t____________________________________________________________");
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner input = new Scanner(System.in);
+        String message = "Hello! I'm C-3PO, Human Cyborg Relations.\n\tWhat can I do for you?";
+        printReply(message);
+        while(true) {
+            String command = input.nextLine();
+            if (command.equalsIgnoreCase("bye")) {
+                message = "Bye. Hope to see you again soon!";
+                printReply(message);
+                break;
+            }
+            printReply(command);
+        }
+        input.close();
     }
 }
