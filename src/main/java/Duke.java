af0ce0c66d1fc1d16843d3986dcf7294567ad644
Nigel Teo <nigelteosw@gmail.com>
2023-01-19 20:38:14 +0800

    Mark as Done
    Add the ability to mark tasks as done. Optionally, add the ability to change the status back to not done.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Duke {
     public static void main(String[] args) {
@@ -10,19 +12,42 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println(logo);
         System.out.println("\nHello! I'm Duke\nWhat can I do for you?\n");
-        ArrayList<String> myList = new ArrayList<String>(100);
+
+        ArrayList<Task> myList = new ArrayList<Task>(100);
+
         Scanner myObj = new Scanner(System.in);
         String userInput = myObj.nextLine();
         while (!userInput.equals("bye")) {
             if (userInput.equals("list")) {
+                System.out.println(" Here are the tasks in your list:");
                 for (int i = 0; i < myList.size(); i++) {
-                    System.out.println(String.valueOf(i + 1) + ". " + myList.get(i));
+                    System.out.println(String.valueOf(i + 1) + ".[" + myList.get(i).getStatusIcon() + "] " +  myList.get(i).getTaskDesc());
+                }
+            }
+            else if (userInput.startsWith("mark")) {
+                Matcher matcher = Pattern.compile("\\d+$").matcher(userInput);
+                if (matcher.find()) {
+                    int lastInteger = Integer.parseInt(matcher.group()) - 1;
+                    myList.get(lastInteger).setDone();
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println("[" + myList.get(lastInteger).getStatusIcon() + "] " + myList.get(lastInteger).getTaskDesc());
+                }
+
+            }
+            else if (userInput.startsWith("unmark")) {
+                Matcher matcher = Pattern.compile("\\d+$").matcher(userInput);
+                if (matcher.find()) {
+                    int lastInteger = Integer.parseInt(matcher.group()) - 1;
+                    myList.get(lastInteger).setUndone();
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    System.out.println("[" + myList.get(lastInteger).getStatusIcon() + "] " + myList.get(lastInteger).getTaskDesc());
+
                 }
             }
             else {
-                myList.add(userInput);
-                System.out.println("added: " + userInput);
-
+                Task task = new Task(userInput);
+                myList.add(task);
+                System.out.println("added: " + task.getTaskDesc());
             }
             userInput = myObj.nextLine();
         }
