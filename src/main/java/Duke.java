397a5db620d41db41e3e264c617e4198bfaea6e6
EksdeeeX <Bryantanzhongkang@gmail.com>
2023-01-19 17:35:41 +0800

    no message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,15 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     private static final String intro = "Hi! I'm Duke! :)\nHow may I help?";
     private static final String outro = "Goodbye!";
+    private static ArrayList<String> list = new ArrayList<>();
+
+    private static void addToList(String str) {
+        list.add(str);
+        System.out.println("added " + str + "!");
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println(intro);
@@ -11,15 +18,29 @@ public class Duke {
         String input = sc.next();
         
         while(!input.equals("bye")) {
-            System.out.print("Duke: ");
-            System.out.println(input);
-            System.out.println();
-            System.out.print("User: ");
+            if (input.equals("list")) {
+                if(list.isEmpty()) {
+                    System.out.print("Duke: ");
+                    System.out.println("List empty!");
+                    System.out.println();
+                    System.out.print("User: ");
+                } else {
+                    for(int i  = 0; i < list.size(); i++) {
+                        System.out.println(i+1 + ". " + list.get(i));
+                    }
+                    System.out.println();
+                    System.out.print("User: ");
+                }
+            } else {
+                System.out.print("Duke: ");
+                addToList(input);
+                System.out.println();
+                System.out.print("User: ");
+            }
             input = sc.next();
         }
-
         System.out.print("Duke: ");
         System.out.println(outro);
     }
-    
+
 }
