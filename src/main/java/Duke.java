8800bd83921fcc1af5314b691d72ee89dff09e37
dohaduong <duonghado03@gmail.com>
2023-01-19 20:53:49 +0700

    Level 2 completed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,14 +8,31 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        String[] listOfAction = new String[100];
         System.out.println("\nHello, I'm Duke");
-        System.out.pgi rintln("What can I do for you?");
+        System.out.println("What can I do for you?");
         Scanner sc = new Scanner(System.in);
         String bye = "bye";
         String s = "";
+        int i = 0;
         while (!bye.equals(s)) {
             s = sc.next();
-            System.out.println(s);
+            if (s.equals("bye")) {
+                break;
+            }
+            else if (!s.equals("list")) {
+                System.out.println(String.format("added: %s", s));
+                listOfAction[i] = String.format("%d. %s", i + 1, s);
+                i++;
+            }
+            else {
+                for (int j = 0; j < listOfAction.length; j ++) {
+                    if (listOfAction[j] == null) {
+                        break;
+                    }
+                    System.out.println(listOfAction[j]);
+                }
+            }
         }
         sc.close();
         System.out.println("Bye. Hope to see you again soon!");
