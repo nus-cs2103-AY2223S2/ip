4b31f2728cad13572aaa8db29e3ea301a3f80ff8
leeyi45 <leeyi45@gmail.com>
2023-01-19 23:33:39 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public final class Duke {
     public final static void main(String[] args) throws IOException {
@@ -12,6 +13,7 @@ public final class Duke {
         System.out.println("Hello from\n" + logo);
 
         final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<String> list = new ArrayList<>();
 
         while (true) {
             String input = reader.readLine();
@@ -19,8 +21,19 @@ public final class Duke {
             if (input.equalsIgnoreCase("bye")) {
                 System.out.println("Ok bye bye!");
                 return;
+            } else if (input.equalsIgnoreCase("list")) {
+                if (list.size() == 0) {
+                    System.out.println("No stored items!");
+                } else {
+                    for (int i = 0; i < list.size(); i++) {
+                        System.out.format("%d. %s\n", i + 1, list.get(i));
+                    }
+                }
+                continue;
             }
-            System.out.println(input);
+
+            list.add(input);
+            System.out.format("added: %s\n", input);
         }
     }
 }
