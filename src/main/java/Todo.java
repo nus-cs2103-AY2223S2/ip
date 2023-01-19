7728cb863328cbb1ef314b1ab1729a5be32a94ca
DESKTOP-R4TTM5F\oPandaCAT <e0411191@u.nus.edu>
2023-01-19 21:48:00 +0800

    Added classes Todo, Deadline and Event

--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -1,2 +1,12 @@
-public class Todo {
+public class Todo extends Task {
+
+    public Todo(String description) {
+        super(description);
+    }
+
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
+
 }
