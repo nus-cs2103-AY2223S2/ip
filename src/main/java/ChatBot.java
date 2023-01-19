1f85f96ae9013f82f91dab9d3cc4f6b9d145f8a7
duong truong minh <gaoranger2002@gmail.com>
2023-01-20 00:02:37 +0800

    Level 3 complete

--- src/main/java/ChatBot.java
+++ src/main/java/ChatBot.java
@@ -9,7 +9,17 @@ public class ChatBot {
         if (command.equals("list")) {
             String shown_list = todoList.toString();
             System.out.println(shown_list);
-        } else {
+        }
+        else if (Character.isDigit(command.charAt(command.length() - 1))) {
+            String[] input = command.split(" ");
+            int digit = Integer.parseInt(input[1]);
+            if (input[0].equals("mark")) {
+                todoList.mark(digit);
+            } else {
+                todoList.unmark(digit);
+            }
+        }
+        else {
             todoList.add(command);
             System.out.println("added: " + command);
         }
--- src/main/java/TodoList.java
+++ src/main/java/TodoList.java
@@ -2,12 +2,36 @@ import java.util.ArrayList;
 
 public class TodoList {
     ArrayList<String> todo_list;
+    ArrayList<String> mark_list;
+
     public TodoList() {
         this.todo_list = new ArrayList<>(100);
+        this.mark_list = new ArrayList<>(100);
     }
 
     public void add(String task) {
         todo_list.add(task);
+        mark_list.add(" ");
+    }
+
+    public void mark(int index) {
+        int todo_list_length = todo_list.size();
+        if (index < 0 || index >= todo_list_length) {
+            return;
+        }
+        mark_list.set(index - 1, "X");
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println(String.format("[X] %s\n", todo_list.get(index-1)));
+    }
+
+    public void unmark(int index) {
+        int todo_list_length = todo_list.size();
+        if (index < 0 || index >= todo_list_length) {
+            return;
+        }
+        mark_list.set(index - 1, " ");
+        System.out.println("OK, I've marked this task as not done yet:");
+        System.out.println(String.format("[ ] %s\n", todo_list.get(index-1)));
     }
 
     @Override
@@ -15,7 +39,8 @@ public class TodoList {
         String shown_list = "";
         int todo_list_length = todo_list.size();
         for (int i = 1; i <= todo_list_length; i++) {
-            shown_list += String.format("%d. %s\n", i, todo_list.get(i-1));
+            String marked = mark_list.get(i - 1);
+            shown_list += String.format("%d.[" + marked +"] %s\n", i, todo_list.get(i-1));
         }
         return shown_list;
     }
