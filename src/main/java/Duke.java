c7e76c2f7750acf1aff98b16398007fe5d7433ac
TimothyLawSongEn <e0725564@u.nus.edu>
2023-01-19 11:06:26 +0800

    Added the ability to store tasks entered by the user and display a task list when requested.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,14 +3,22 @@ public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
 
+        String[] arr = new String[100];
+        int nextIndex = 0;
+
         while (true) {
             Scanner bucky = new Scanner(System. in);
             String str = bucky.nextLine();
             if (str.equals("bye")) {
                 System.out.println ("Bye. Hope to see you again soon!");
                 break;
+            } else if (str.equals("list")){
+                for (int i = 0 ; i < nextIndex ; i++) {
+                    System.out.println((i+1) + ". " + arr[i]);
+                }
             } else {
-                System.out.println(str);
+                System.out.println("added: " + str);
+                arr[nextIndex++] = str;
             }
         }
         
