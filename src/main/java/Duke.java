4334ca696740a05659cd0fac7567f66ccb36b65d
gekhro <jethrosimsy@gmail.com>
2023-01-19 23:07:11 +0800

    level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -10,11 +11,21 @@ public class Duke {
         System.out.println("What can i do for you?");
         Scanner scan = new Scanner(System.in);
         String input = scan.nextLine();
-        
+        ArrayList<String> list = new ArrayList<String>(); 
+        int counter = 0;
+
         while (!input.equals("bye")){
-            System.out.println(input);
+            if (input.equals("list")){
+                for (int i = 0; i < counter; i++) {
+                    System.out.println( i + 1 + ". " + list.get(i));
+                }
+            }
+            list.add(input);
+            counter = counter + 1;
+            System.out.println(" added: " + input);
             input = scan.nextLine();
         }
+
         System.out.println("Bye. Hope to see you again!");
     }
 }
