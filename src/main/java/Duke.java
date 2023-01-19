a93bb863b4972440c5f25aa19e28783de09d594f
Xiuxuan Wang <36021591+wxxedu@users.noreply.github.com>
2023-01-19 12:54:47 +0800

    implemented a basic loop to respond to user inputs

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,18 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        final Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            // sanitize the input
+            input.trim();
+            if (input.equals("bye")) {
+                System.out.println('\t' + "Bye, hope to see you again!");
+                break;
+            } else {
+                System.out.println('\t' + input);
+            }
+        }
+
     }
 }
