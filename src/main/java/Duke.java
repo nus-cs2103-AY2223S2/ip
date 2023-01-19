f7a2e1b7e9534a77c2a7085ce81e5442e6a55960
jeraldkiew <jeraldkiew007@gmail.com>
2023-01-19 18:35:13 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,24 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
+
+        System.out.println("Leo: Yoooz it's your boy Leo! What's up?");
+        Scanner sc = new Scanner(System.in);
+        while(true) {
+            System.out.print("You: ");
+            String str = sc.nextLine();
+            if(str.equals("bye")) {
+                System.out.println("Leo: Good talk man, catch you again some other time!");
+                break;
+            }
+            System.out.println("Leo: " + str);
+        }
     }
 }
