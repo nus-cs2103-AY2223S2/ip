889c432f273869eb62b8ef689fe156dd3478587a
nramapurath <nramapurath@gmail.com>
2023-01-19 17:36:59 +0800

    Add class to represent tasks

---
+++ src/main/java/task/Task.java
@@ -0,0 +1,30 @@
+package task;
+
+/**
+ * Class to store task description along with completion status.
+ */
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task (String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void markAsNotDone() {
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
