00095b4bab8f35419bbf130cb8496de277b6efa5
tricixg <64681919+tricixg@users.noreply.github.com>
2023-01-19 17:15:34 +0800

    Level-3: Mark as Done
    -ability to mark and unmark tasks
    -Task class created

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -20,40 +20,83 @@ public class Duke {
 
         System.out.println("  ───── ･ ｡ﾟ☆: *.☽ .* :☆ﾟ. ───── \n Hello! I'm Broccoli the dinosaur \n           <|°▿▿▿▿°|/ \n      What can I do for you? \n   ──── ･ ｡ﾟ☆: *.☽ .* :☆ﾟ. ────" ); 
         //getInput();
-        ArrayList<String> array=new ArrayList<String>();
+        ArrayList<Task> array=new ArrayList<Task>();
 
         int i = 0;
         while (i<100) {
-            String hello = getInput();
-            if(hello.equals("bye")) {
+            String[] hello = getInput();
+            String combined = String.join(" ", hello);
+            if(combined.equals("bye")) {
                 System.out.println("    ──────── ⋅ ∙ ∘ ☽ ༓ ☾ ∘ ⋅ ⋅ ─────────");
                 System.out.println( "      Bye. Hope to see you again soon!");
                 System.out.println("\n   ‧˚₊꒷꒦︶︶︶︶︶꒷꒦︶︶︶︶︶꒦꒷‧₊˚⊹");
                 break;
 
-            } else if (hello.equals("list")){
+            } else if (combined.equals("list")){
                 System.out.println("    ────────────── ⋆⋅☆⋅⋆ ───────────────");
                 for(int j=0; j<array.size(); j++) {
-                    System.out.println( "       "+ (j+1) + ". "+ array.get(j));
+                    System.out.println("      "+(j+1) +"."+ array.get(j).toString());
+                    //System.out.println( "       "+ (j+1) + ". "+ array.get(j));
                 }
                 System.out.println("    ────────────── ⋆⋅☆⋅⋆ ───────────────");
-            } else {
+            } else if(hello[0].equals("mark")){
+                array.get((Integer.parseInt(hello[1])-1)).markAsDone();
+                System.out.println("    ────────────── ⋆⋅☆⋅⋆ ───────────────");
+                System.out.println("      Nice! I've marked this task as done:");
+                System.out.println("      "+ array.get((Integer.parseInt(hello[1])-1)).toString());
+                System.out.println("    ────────────── ⋆⋅☆⋅⋆ ───────────────");
+            } else if(hello[0].equals("unmark")){
+                array.get((Integer.parseInt(hello[1])-1)).markAsUnDone();
+                System.out.println("    ────────────── ⋆⋅☆⋅⋆ ───────────────");
+                System.out.println("      OK, I've marked this task as not done yet:");
+                System.out.println("      "+ array.get((Integer.parseInt(hello[1])-1)).toString());
+                System.out.println("    ────────────── ⋆⋅☆⋅⋆ ───────────────");
+            }else {
                 System.out.println("    ═══*.·:·.☽✧    ✦    ✧☾.·:·.*═══");
-                System.out.println("     added:" + hello);
+                System.out.println("     added:" + combined);
                 System.out.println("\n    ═══*.·:·.☽✧    ✦    ✧☾.·:·.*═══");
-                array.add(hello);
+                Task t = new Task(combined);
+                array.add(t);
                 i++;
             }
         }
+        
 
     }
 
-    private static String getInput(){
+    private static String[] getInput(){
         Scanner sc = new Scanner(System.in);
         String userinput = sc.nextLine();
-        return userinput;
+        String [] splitted = userinput.split("\\s+");
+
+        return splitted;
     }
+    static class Task {
+        protected String description;
+        protected boolean isDone;
+    
+        public Task(String description) {
+            this.description = description;
+            this.isDone = false;
+        }
+    
+        public String getStatusIcon() {
+            return (isDone ? "X" : " "); // mark done task with X
+        }
+    
+        public void markAsDone() {
+            this.isDone = true;
+        }
 
+        public void markAsUnDone() {
+            this.isDone = false;
+        }
 
+        public String toString() {
+            return "[" + this.getStatusIcon() + "] " + this.description;
+        }
+    }
+    
+    
 
 }
