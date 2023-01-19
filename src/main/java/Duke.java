98b0b8bef5ba20a44fac405f13486cd638bd0bde
duong truong minh <gaoranger2002@gmail.com>
2023-01-19 22:25:07 +0800

    Level 1 complete

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,45 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        begin();
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        while (!input.equals("bye")) {
+            execute(input);
+            input = scanner.nextLine(); //ready for next input
+        }
+
+        bye();
+    }
+
+    private static void begin() {
+        endCommand();
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("What can I do for you?");
+
+        endCommand();
+    }
+
+    private static void endCommand() {
+        System.out.println("____________________________________________________________");
+    }
+
+    private static void execute(String command) {
+        System.out.println(command);
+        endCommand();
+    }
+
+    private static void bye() {
+        System.out.println("Bye. Hope to see you again soon!");
+        endCommand();
     }
 }
