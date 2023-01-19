3351cbbd6e97c8e7942a0f86c3fd6dbcf2006634
Wesley Teo <wesley.tyx@gmail.com>
2023-01-19 14:42:46 +0800

    Added inputs for Duke and made Duke repeat the input.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
+
 public class Duke {
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+                + "|____/ \\__,_|_|\\_\\___|";
+        System.out.println("Hello from\n" + logo + "  managed by Wesley Teo.\n\nWhat can I do for you?\n---------------------------------");
+        String input = sc.next();
+        while (!input.equals("bye")) {
+            System.out.println("---------------------------------\n    " + input + "\n---------------------------------");
+            input = sc.next();
+        }
+        System.out.println("---------------------------------\nBye. Hope to see you again soon!");
     }
 }
