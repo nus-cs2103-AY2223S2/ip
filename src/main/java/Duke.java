22b26df15123167fe72a8ec3fc6cca07a1dca63c
Wang <shaofeng1234567@gmail.com>
2023-01-19 23:50:26 +0800

    Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        String greeting = """
+                Hello! I'm Alpha Beast
+                What can I do for you?
+                """;
+
+        System.out.println(greeting);
+        Scanner sc = new Scanner(System.in);
+        String input;
+        while (true) {
+            input = sc.nextLine();
+            if (input.equals("bye")){
+                System.out.println("Bye. Hope to see you again soon!\n");
+                break;
+            }
+
+            System.out.println(input);
+        }
     }
 }
