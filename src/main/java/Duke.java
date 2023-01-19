5c73f09e69f2805457b9762c91677570888c0e68
rachtan27 <e0775283@u.nus.edu>
2023-01-19 12:57:28 +0800

    Completed Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,10 @@
 import java.util.Scanner;
+import java.util.Arrays;
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String[] storeCommands = new String[100];
+        int numElem = 0;
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -10,10 +13,20 @@ public class Duke {
         System.out.println("Hello from\n" + logo + "How may I be of service to you? \nEnter your command:");
         String commandToEcho = sc.nextLine();
         while (!commandToEcho.equals("bye")) {
-            System.out.println(commandToEcho);
+            if(commandToEcho.equals("list")) {
+                for (int i = 0; i < numElem; i++) {
+                    System.out.println(String.format("%d. %s",i+1,storeCommands[i]));
+                }
+            }
+            else {
+                storeCommands[numElem] = commandToEcho;
+                numElem++;
+                System.out.println("added: " + commandToEcho);
+            }
             commandToEcho = sc.nextLine();
         }
         System.out.println("Bye. Hope to see you again soon!");
+
 
 
 
