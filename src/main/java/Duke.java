87a0bd009edf0ba8b313c5c7bca6f35aa83cadf6
KevinEyo1 <eyokaiwenkevin@gmail.com>
2023-01-19 11:43:06 +0800

    Level-6 done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,13 @@
 import java.io.IOException;
 import java.sql.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
-    public static Task[] storage = new Task[100];
-    public static int pointer = -1;
+    public static ArrayList<Task> storage = new ArrayList<Task>();
+//    public static int pointer = -1;
     public static String pad(String val) {
         return "---------------------------\n" + val + "\n---------------------------";
     }
@@ -31,11 +32,11 @@ public class Duke {
                 // list tasks
 
                 System.out.println("---------------------------");
-                if (pointer == -1) {
+                if (storage.isEmpty()) {
                     System.out.println("No tasks added yet.");
                 } else {
-                    for (int i = 0; i < pointer + 1; i++) {
-                        System.out.println(Integer.toString(i + 1) + ". " + storage[i].toString());
+                    for (int i = 0; i < storage.size(); i++) {
+                        System.out.println(Integer.toString(i + 1) + ". " + storage.get(i).toString());
                     }
                 }
                 System.out.println("---------------------------");
@@ -48,39 +49,36 @@ public class Duke {
             }
             try {
                 if (cmd.equals("mark")) {
-                    if (pointer == -1 || len == 1) {
+                    if (storage.isEmpty() || len == 1) {
                         throw new DukeException("Unable to mark.");
                     }
                     // mark task as done
-                    Task curr = storage[Integer.valueOf(details) - 1];
+                    Task curr = storage.get(Integer.valueOf(details) - 1);
                     System.out.println(pad(curr.outputMarked() + curr.toString()));
                 } else if (cmd.equals("unmark")) {
-                    if (pointer == -1 || len == 1) {
+                    if (storage.isEmpty() || len == 1) {
                         throw new DukeException("Unable to unmark.");
                     }
                     // mark task as undone
-                    Task curr = storage[Integer.valueOf(details) - 1];
+                    Task curr = storage.get(Integer.valueOf(details) - 1);
                     System.out.println(pad(curr.outputUnmarked() + curr.toString()));
                 } else if (cmd.equals("todo")) {
                     if (len == 1) {
                         throw new DukeException("Description of todo cannot be empty.");
                     }
-                    pointer++;
                     Todo addition = new Todo(details);
-                    storage[pointer] = addition;
+                    storage.add(addition);
                     System.out.println(pad("Understood. I have added the task:\n" + addition.toString()
-                            + "\nThere are now " + Integer.toString(pointer + 1) + " task(s) in the list."));
+                            + "\nThere are now " + Integer.toString(storage.size()) + " task(s) in the list."));
                 } else if (cmd.equals("deadline")) {
                     String[] temp = details.split(" /by ");
                     if (len == 1 || temp.length != 2) {
                         throw new DukeException("Description of deadline cannot be incomplete.");
                     }
-                    pointer++;
                     Deadline addition = new Deadline(temp[0], temp[1]);
-                    storage[pointer] = addition;
+                    storage.add(addition);
                     System.out.println(pad("Understood. I have added the task:\n" + addition.toString()
-                            + "\nThere are now " + Integer.toString(pointer + 1) + " task(s) in the list."));
-
+                            + "\nThere are now " + Integer.toString(storage.size()) + " task(s) in the list."));
                 } else if (cmd.equals("event")) {
                     String[] temp = details.split(" /from ");
                     if (len == 1 || temp.length != 2) {
@@ -90,12 +88,20 @@ public class Duke {
                     if (temp2.length != 2) {
                         throw new DukeException("Description of event cannot be incomplete.");
                     }
-                    pointer++;
                     Event addition = new Event(temp[0], temp2[0], temp2[1]);
-                    storage[pointer] = addition;
+                    storage.add(addition);
                     System.out.println(pad("Understood. I have added the task:\n" + addition.toString()
-                            + "\nThere are now " + Integer.toString(pointer + 1) + " task(s) in the list."));
-                } else {
+                            + "\nThere are now " + Integer.toString(storage.size()) + " task(s) in the list."));
+                } else if (cmd.equals("delete")) {
+                    if (storage.isEmpty() || len == 1) {
+                        throw new DukeException("Unable to delete.");
+                    }
+                    Task curr = storage.get(Integer.valueOf(details) - 1);
+                    storage.remove(Integer.valueOf(details) - 1);
+                    System.out.println(pad("Noted. I have removed the task:\n" + curr.toString()
+                            + "\nThere are now " + Integer.toString(storage.size()) + " task(s) in the list."));
+                }
+                else {
                     throw new DukeException("I do not quite understand that.");
                 }
             } catch (DukeException err) {
