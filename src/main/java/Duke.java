6822983046657341ed2fd5651e76f7b4830ac6e3
SeanChinJunKai <e0774037@u.nus.edu>
2023-01-19 11:13:18 +0800

    Completed Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
 public class Duke {
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        /*
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+         */
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello! I'm Duke \nWhat can I do for you?");
+        while(true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                break;
+            } else {
+                System.out.println(input);
+            }
+        }
+        System.out.println("Bye. Hope to see you again soon!");
     }
+
 }
