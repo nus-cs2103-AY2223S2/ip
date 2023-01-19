f5cbbad20cb2f21e301b83d57e06030ca8c67e8a
nramapurath <nramapurath@gmail.com>
2023-01-19 16:52:11 +0800

    Add javadocs in Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,19 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+import task.Task;
 
 public class Duke {
+    /**
+     * Prints a line 4 spaces away from the left edge of the screen to visually
+     * separate Duke's replies from user input.
+     */
     private static void printLine() {
         System.out.printf("%64s%n", "    ____________________________________________________________");
     }
 
+    /**
+     * Prints Duke's greeting message (bounded by lines above and below).
+     */
     private static void greet() {
         printLine();
         System.out.printf("    %s%n", "Hello! I'm Duke");
@@ -13,9 +21,15 @@ public class Duke {
         printLine();
     }
 
+    /**
+     * Replies to user inputs according to requirements.
+     * If user inputs "bye", return to exit Duke.
+     * If user inputs "list", print current tasks.
+     * If user inputs a task, add to current tasks.
+     */
     public static void echo() {
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> tasks = new ArrayList<String>();
+        ArrayList<String> tasks = new ArrayList<>();
         String input = sc.nextLine();
         while (!input.equals("bye")) {
             printLine();
@@ -36,6 +50,7 @@ public class Duke {
         System.out.printf("    %s%n", "Bye. Hope to see you again soon!");
         printLine();
     }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
