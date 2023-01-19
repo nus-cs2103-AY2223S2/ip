cff2c6b8e79c5b32372aa339ddcde63a3db97ea3
Arkobrata Chaudhuri <arkobrata.2002@gmail.com>
2023-01-19 19:26:52 +0800

    Added Mark as Done/Undone

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,25 +11,41 @@ public class Duke {
         System.out.println("What can I do for you?");
         System.out.println(INDENT_LINE);
 
-        ArrayList<String> commandList = new ArrayList<>();
+        ArrayList<Task> taskList = new ArrayList<>();
 
         while (true) {
-            String command = sc.nextLine();
-            if (command.equals("bye")) {
+            String task = sc.nextLine();
+            if (task.equals("bye")) {
                 System.out.println(INDENT_LINE);
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println(INDENT_LINE);
                 break;
-            } else if (command.equals("list")) {
+            } else if (task.equals("list")) {
                 System.out.println(INDENT_LINE);
-                for (int i = 0; i < commandList.size(); i++) {
-                    System.out.println((i+1) + ". " + commandList.get(i));
+                System.out.println("Here are the tasks in your list:");
+                for (int i = 0; i < taskList.size(); i++) {
+                    System.out.println((i+1) + "." + taskList.get(i));
                 }
                 System.out.println(INDENT_LINE);
-            } else {
-                commandList.add(command);
+            } else if (task.substring(0, 4).equals("mark")) {
+                int index = Integer.parseInt(task.substring(5)) - 1;
+                taskList.get(index).markAsDone();
                 System.out.println(INDENT_LINE);
-                System.out.println("added: " + command);
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println((index+1) + "." + taskList.get(index));
+                System.out.println(INDENT_LINE);
+            } else if (task.substring(0, 6).equals("unmark")) {
+                int index = Integer.parseInt(task.substring(7)) - 1;
+                taskList.get(index).markAsUndone();
+                System.out.println(INDENT_LINE);
+                System.out.println("Nice! I've marked this task as not done yet:");
+                System.out.println((index+1) + "." + taskList.get(index));
+                System.out.println(INDENT_LINE);
+            }
+            else {
+                taskList.add(new Task(task));
+                System.out.println(INDENT_LINE);
+                System.out.println("added: " + task);
                 System.out.println(INDENT_LINE);
             }
         }
