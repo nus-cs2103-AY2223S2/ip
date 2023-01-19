5d852e00448e4860e581a432b5731bc045f8cdc3
DESKTOP-R4TTM5F\oPandaCAT <e0411191@u.nus.edu>
2023-01-19 21:11:19 +0800

    Added toString method

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -2,9 +2,14 @@ public class Task {
     protected String description;
     protected boolean isDone;
 
+
     public Task(String description) {
         this.description = description;
         this.isDone = false;
+    }
+
+    public String toString() {
+        return "[" + this.getStatusIcon() + "]" + this.description;
     }
 
     public String getStatusIcon() {
