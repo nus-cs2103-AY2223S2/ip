d115b9cba536a6ec05ce4296c266f87db04074da
Samarth Verma <samarthverma@icloud.com>
2023-01-19 11:42:12 +0800

    Greet user with message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,47 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        printGreeting();
+
+        while (true) {
+            String userInput = getUserInput();
+            if (userInput.equals("bye")) {
+                printBye();
+                break;
+            } else {
+                printEcho(userInput);
+            }
+        }
     }
+
+    private static void printBye() {
+        printLine();
+        System.out.println("Bye. Hope to see you again soon!");
+        printLine();
+    }
+
+    private static void printEcho(String userInput) {
+        System.out.println();
+        System.out.println(userInput);
+        printLine();
+    }
+
+    private static String getUserInput() {
+        Scanner in = new Scanner(System.in);
+        return in.nextLine();
+    }
+
+    private static void printLine() {
+        System.out.println("____________________________________________________________");
+    }
+
+    private static void printGreeting() {
+        printLine();
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        printLine();
+    }
+
+
 }
