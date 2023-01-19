974a2bfd058f33735317bc011b166978de246d2d
kohkaixun <e0726276@u.nus.edu>
2023-01-19 15:59:44 +0800

    Added store display list feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,13 +12,21 @@ public class Duke {
         System.out.println("What can I do for you?");
 
         Scanner scanner = new Scanner(System.in);
+        String[] list = new String[100];
+        int index = 0;
         while (true) {
             String data = scanner.nextLine();
             if (data.equals("bye")) {
                 System.out.println("Goodbye! It's been a pleasure talking to you!");
                 break;
+            } else if (data.equals("list")) {
+                for (int i = 0 ; i < index ; i++) {
+                    System.out.println((i + 1) + ". " + list[i]);
+                }
             } else {
-                System.out.println(data);
+                list[index] = data;
+                index++;
+                System.out.println("Added: " + data);
             }
         }
     }
