8673b0af80dd59c44a955dd000f6a9200b1f4e5a
panavdua <114715893+panavdua@users.noreply.github.com>
2023-01-19 17:36:26 +0800

    Created Level - 1 framework

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,30 @@
+import java.util.*;
 public class Duke {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello hello! I'm Panav");
+        System.out.println("Whatsup bro");
+        System.out.println("____________________________________________________________");
+
+        String word = sc.nextLine();
+        while(word.compareTo("bye") != 0) {
+            System.out.println("____________________________________________________________");
+            System.out.println(word);
+            System.out.println();
+            System.out.println("____________________________________________________________");
+            word = sc.nextLine();
+        }
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println();
+        System.out.println("____________________________________________________________");
     }
 }
