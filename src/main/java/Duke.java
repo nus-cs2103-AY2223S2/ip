c70d3df958bf7045455e8683e65218ce2b980fd6
Statspadders <weihao_toh@rocketmail.com>
2023-01-19 23:34:07 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,11 +9,23 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n"; */
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String text = "";
-        while( (text = br.readLine()) != "bye") {
-            System.out.println(text);
+        ArrayList < String > list = new ArrayList < String >();
+
+        while( !(text = br.readLine()).equals("bye")) {
+            if ( text.equals("list")) {
+                for (int i = 0; i < list.size(); i++) {
+                    int j = i + 1;
+                    System.out.println(j + " " + list.get(i));
+                }
+            } else {
+                list.add(text);
+                System.out.println("Added : " + text);
+            }
         }
+
         System.out.println("Bye. Hope to see you again soon!");
 
 
