97be1b72c06e13f08866a689867728d2126dfbfa
zm-l <97417244+zm-l@users.noreply.github.com>
2023-01-19 20:19:44 +0800

    Level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -28,7 +28,7 @@ public class Duke {
                 System.out.println("Here are the tasks in your list:");
                 for (int i = 1; i <= userTasks.size(); i++)
                 {
-                    System.out.println(i + ".[" + userTasks.get(i - 1).getStatusIcon() + "] " + userTasks.get(i - 1));
+                    System.out.println(i + "." + userTasks.get(i - 1));
                 }
             }
             else if (matchMark.matches())
@@ -36,20 +36,42 @@ public class Duke {
                 System.out.println("Nice! I've marked this task as done:");
                 int idx = Integer.parseInt(userInput.split(" ")[1]);
                 userTasks.get(idx - 1).markTask();
-                System.out.println("    " + idx + ".[" + userTasks.get(idx - 1).getStatusIcon() + "] " + userTasks.get(idx - 1));
+                System.out.println("    " + idx + "." + userTasks.get(idx - 1));
             }
             else if (matchUnmark.matches())
             {
                 System.out.println("OK, I've marked this task as not done yet:");
                 int idx = Integer.parseInt(userInput.split(" ")[1]);
                 userTasks.get(idx - 1).unmarkTask();
-                System.out.println("    " + idx + ".[" + userTasks.get(idx - 1).getStatusIcon() + "] " + userTasks.get(idx - 1));
+                System.out.println("    " + idx + "." + userTasks.get(idx - 1));
             }
             else
             {
-                Task userTask = new Task(userInput);
+                Task userTask = null;
+                String taskType = userInput.split(" ")[0];
+                if (taskType.equals("todo"))
+                {
+                    String taskName = userInput.split(" ", 2)[1];
+                    userTask = new ToDo(taskName);
+                }
+                else if (taskType.equals("deadline"))
+                {
+                    String[] taskNameAndDeadline = userInput.split(" ", 2)[1].split(" /by ");
+                    String taskName = taskNameAndDeadline[0];
+                    String deadline = taskNameAndDeadline[1];
+                    userTask = new Deadline(taskName, deadline);
+                }
+                else if (taskType.equals("event"))
+                {
+                    String[] taskNameAndDate = userInput.split(" ", 2)[1].split(" /from ");
+                    String taskName = taskNameAndDate[0];
+                    String[] toAndFrom = taskNameAndDate[1].split(" /to ");
+                    String from = toAndFrom[0];
+                    String to = toAndFrom[1];
+                    userTask = new Event(taskName, from, to);
+                }
                 userTasks.add(userTask);
-                System.out.println("added: " + userInput);
+                System.out.println("Got it. I've added this task: \n    " + userTask + "\nNow you have " + userTasks.size() + " tasks in the list.");
             }
             userInput = input.nextLine();
         }
