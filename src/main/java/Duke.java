6aff4ed33ca4e4957d7e3f1fb3ae6ae0cd61185b
DESKTOP-R4TTM5F\oPandaCAT <e0411191@u.nus.edu>
2023-01-19 20:15:05 +0800

    add String dividor modified greeting

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,16 @@
 public class Duke {
+
+    private static String dividor = "____________________________________________________________\n";
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+                + "|____/ \\__,_|_|\\_\\___|\n"
+                + "\n"
+                + "Hello! I'm Duke\n"
+                + "What can I do for you?\n";
+
+        System.out.println(dividor + logo + dividor);
     }
 }
