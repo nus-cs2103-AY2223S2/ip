cc934b305f9b8777f1bb8dee9669735c6bb8a8e1
Shirshajit Sen Gupta <shirshajit@gmail.com>
2023-01-19 13:41:18 +0800

    remove UI lines

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,54 +2,14 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-
-    public static void printer(String[] input) {
-        System.out.println("\t____________________________________________________________");
-        for (String i : input) {
-            System.out.println("\t" + i);
-        }
-        System.out.println("\t____________________________________________________________");
-    }
-
-    public static void printer(String input) {
-        System.out.println("\t____________________________________________________________");
-        System.out.println("\t" + input);
-        System.out.println("\t____________________________________________________________");
-    }
-
-    public static void taskPrinter(ArrayList<Task> input) {
-        System.out.println("\t____________________________________________________________");
-        for (int i = 0; i < input.size(); i++) {
-            Task t = input.get(i);
-            System.out.println("\t" + (i + 1) + "." + t.toString());
-        }
-        System.out.println("\t____________________________________________________________");
-    }
-
-    public static void taskMarker(Task input) {
-        System.out.println("\t____________________________________________________________");
-
-        if (input.done) {
-            System.out.println("\tNice! I've marked this task as done:");
-
-        } else {
-            System.out.println("\tNice! I've marked this task as not done:");
-        }
-        System.out.println("\t" + input.toString());
-        System.out.println("\t____________________________________________________________");
-    }
-
     public static void main(String[] args) {
-        // String logo = " ____ _ \n"
-        // + "| _ \\ _ _| | _____ \n"
-        // + "| | | | | | | |/ / _ \\\n"
-        // + "| |_| | |_| | < __/\n"
-        // + "|____/ \\__,_|_|\\_\\___|\n";
-        // System.out.println("Hello from\n" + logo);
-        Scanner sc = new Scanner(System.in);
-        String[] strings = { "Hello I'm Duke", "What can I do for you?" };
-        printer(strings);
 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\tHello! I'm Duke");
+        System.out.println("\tWhat can I do for you?");
+
+        System.out.println("\t____________________________________________________________");
         TaskList taskList = new TaskList();
         Commands commands = new Commands(taskList);
 
@@ -75,6 +35,9 @@ public class Duke {
 
         }
         sc.close();
-        printer("Bye. Hope to see you again soon!");
+        System.out.println("\t____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("\t____________________________________________________________");
+
     }
 }
