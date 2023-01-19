c40c3061de6de8a9b07d4b0f0869d361fee84436
benjamin-wee <benjaminwee20@gmail.com>
2023-01-19 19:04:24 +0800

    Second commit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,10 @@
-import java.util.Scanner;
-import java.util.Objects;
+import java.util.*;
 
 public class Duke {
     public static void main(String[] args) {
+
+        List<String> list = new ArrayList<>();
+
         System.out.println("Hello! I'm Somebody\n" + "What can I do for you?");
         Scanner scn = new Scanner(System.in);
         while (true) {
@@ -13,8 +15,14 @@ public class Duke {
                 System.out.println("Bye. Hope to see you again soon!");
                 scn.close();
                 return;
+            } else if (Objects.equals(input.toLowerCase(), "list")) {
+                for (int i = 1; i <= list.size(); i++) {
+                    System.out.println(i + ". " + list.get(i - 1));
+                }
             } else {
-                System.out.println(input);
+                list.add(input);
+                System.out.println("added: " + input);
+
             }
 
         }
