c82018a5f48186968f1f0188d2efac56c85a9d28
kohkaixun <e0726276@u.nus.edu>
2023-01-19 11:40:15 +0800

    Added echo bye exit feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,18 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String data = scanner.nextLine();
+            if (data.equals("bye")) {
+                System.out.println("Goodbye! It's been a pleasure talking to you!");
+                break;
+            } else {
+                System.out.println(data);
+            }
+        }
     }
 }
