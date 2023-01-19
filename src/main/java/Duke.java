24b37764d01d5f673c526cc0696d1541a19d324a
Darren Chang <darrenchang2@gmail.com>
2023-01-19 14:42:11 +0800

    Implement Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
+import java.util.*;
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        System.out.println("Hello! I'm Duke What can I do for you?");
+        Scanner sc= new Scanner(System.in);
+        boolean exit = false;
+        while(!exit) {
+            String str = sc.nextLine();
+            exit = echo(str);
+        }
+
+    }
+    public static boolean echo(String str) {
+        if(str.equals("bye")) {
+            System.out.println("Bye. Hope to see you again soon!");
+            return true;
+        } else {
+            System.out.println(str);
+            return false;
+        }
     }
 }
