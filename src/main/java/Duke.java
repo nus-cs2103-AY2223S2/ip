0137c459f0d43d80ac44c5256ddcb6da165ee6a9
Bryan Juniano <junianob@gmail.com>
2023-01-19 19:38:45 +0800

    Completed Level-2
    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,14 +9,25 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("How can I help you?");
+        String list[] = new String[100];
+        int index = 0;
         while(true) {
             Scanner myObj = new Scanner(System.in);
             String input = myObj.nextLine();
+            if(input.equals("list")){
+                for(int i = 0; i<index; i++){
+                    System.out.println(i+1+": "+list[i]);
+                }
+            }
             if(input.equals("bye")){
                 System.out.println("bai");
                 break;
             }
-            System.out.println(input);
+            else {
+                System.out.println("added: " +input);
+                list[index] = input;
+                index++;
+            }
         }
     }
 }
