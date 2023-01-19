d919c7843111ea89d76cd0b1a7c51acbdebf1b4d
gyulong1 <48115998+gyulong1@users.noreply.github.com>
2023-01-19 19:08:30 +0800

    Added Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,30 +10,52 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("What can I do for you?\n");
 
-        Scanner sc = new Scanner(System.in);
-        List<String> arr = new ArrayList<>();
+        Scanner sc = new Scanner (System.in);
+        // List<String> arr = new ArrayList<String>();
+        List<Task> arr = new ArrayList<>();
 
-        while (sc.hasNextLine()) {
-            String ip = sc.nextLine();
-            if (ip.equalsIgnoreCase("bye")) {
+        while(sc.hasNextLine()) {            String ip = sc.nextLine();
+            if(ip.equalsIgnoreCase("bye")){
                 break;
-            } else if (ip.equalsIgnoreCase("list")) {
-                for (int i = 0; i < arr.size(); i++) {
-                    System.out.println(i + 1 + ". " + arr.get(i));
+            }
+            else if (ip.equalsIgnoreCase("list")){
+                for(int i = 0; i < arr.size(); i++){
+                    System.out.printf("%d.[%S] %s\n", i+1, arr.get(i).getStatusIcon(), arr.get(i).getDesc());
                 }
                 System.out.println();
             }
             // level 1
             // System.out.println(ip + "\n");
 
-            // level 2
-            else {
+            else if (ip.contains("mark")){
+                String[] parts = ip.split(" ");
+                if (parts.length != 2){
+                    System.out.println("invalid\n");
+                    continue;
+                }
+                int index = Integer.parseInt(parts[1]) - 1;
+                if (parts[0].equalsIgnoreCase("mark")){
+                    System.out.println("Nice! I've marked this task as done:");
+                    arr.get(index).setStatus(parts[0]);
+                }
+                else if (parts[0].equalsIgnoreCase("unmark")){
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    arr.get(index).setStatus(parts[0]);
+                }
+                System.out.printf("[%S] %s\n\n", arr.get(index).getStatusIcon(), arr.get(index).getDesc());
+            }
+
+            else{
+                // level 2
                 System.out.println("added: " + ip + "\n");
-                arr.add(ip);
+                // arr.add(ip);
+                Task t = new Task(ip);
+                arr.add(t);
             }
 
         }
 
         System.out.println("Bye. Hope to see you again soon!");
+
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,25 @@
+public class Task {
+    protected String desc;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.desc = description;
+        this.isDone = false;
+    }
+    public String getDesc() {
+        return this.desc;
+    }
+
+    public void setStatus(String isDone){
+        if (isDone.equalsIgnoreCase("mark")){
+            this.isDone = true;
+        }
+        else if (isDone.equalsIgnoreCase("unmark")){
+            this.isDone = false;
+        }
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+}
