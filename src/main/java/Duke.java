7ba91f1207e107b1abe6126254022558899dcebc
Nicholas Lee <nic.leejunyi@gmail.com>
2023-01-19 22:22:56 +0800

    added storage functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+
 
 public class Duke {
 
@@ -11,17 +13,40 @@ public class Duke {
 
         String input;
 
+        ArrayList<String> taskList = new ArrayList<String>();
+
         while(true) {
+
             System.out.print("You: ");
             input = scanner.nextLine();
+
             if (input.equals("bye")) {
                 System.out.println("UwU_TaskMaster ＵｗＵ: Bye bye... UwU");
                 break;
             }
-            System.out.println("\nUwU_TaskMaster ＵｗＵ: " + input);
-            System.out.println("__________________________________________");
+
+            if (input.equals("list")) {
+                if (taskList.size() == 0) {
+                    System.out.println("UwU_TaskMaster ＵｗＵ: Wooks wike *screams* you do not have any OwO tasks!");
+                    System.out.println("__________________________________________");
+                    continue;
+                }
+
+                System.out.println("\nUwU_TaskMaster ＵｗＵ: Heww are your tasks UwU!");
+                for (int i = 0; i < taskList.size(); i++) {
+                    System.out.println((i + 1) + ". " + taskList.get(i));
+                }
+                System.out.println("__________________________________________");
+            } else {
+
+                if (!input.equals("")) {
+                    taskList.add(input);
+                    System.out.println("\nUwU_TaskMaster ＵｗＵ: Y-Y-Youw t-task has been successfuwwy added!!");
+                    System.out.println("__________________________________________");
+                }
+
+            }
         }
         scanner.close();
-
     }
 }
