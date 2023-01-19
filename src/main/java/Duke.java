4e58fe1f272866904a9ee1c771bf12230467338b
EksdeeeX <Bryantanzhongkang@gmail.com>
2023-01-19 17:04:17 +0800

    no message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,4 @@
-public class Eksdee {
+public class Duke {
 
     public static void main(String[] args) {
         String intro = 
--- src/main/java/Eksdee.java
+++
@@ -1,7 +0,0 @@
-public class Duke {
-
-    public static void main(String[] args) {
-        String intro = 
-        System.out.println("Hello from\n" + logo);
-    }
-}
