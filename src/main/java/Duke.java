95d2074519ce56eebd540def0ed3c49536837a83
kynhan <kynhan123@gmail.com>
2023-01-19 22:11:54 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Duke {
     private static Scanner scanner = new Scanner(System.in);
     private static String horizontalLine = "************************";
+    private static String[] storage = new String[100];
+    private static int count = 0;
 
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -16,22 +18,30 @@ public class Duke {
         System.out.println("HELLO! I'M DUKE!");
         System.out.println("HOW CAN I HELP?");
         System.out.println(horizontalLine);
-        echo();
+        readInput();
     }
 
-    private static void echo() {
+    private static void readInput() {
         String input = scanner.nextLine();
 
-        // If user inputs bye to close program
         if (input.equals("bye")) {
             System.out.println(horizontalLine);
             System.out.println("BYE!");
             System.out.println(horizontalLine);
+        } else if (input.equals("list")) {
+            System.out.println(horizontalLine);
+            for (int i = 0; i < count; i++) {
+                System.out.println("" + (i+1) + ". " + storage[i]);
+            }
+            System.out.println(horizontalLine);
+            readInput();
         } else {
             System.out.println(horizontalLine);
-            System.out.println(input);
+            System.out.println("added: " + input);
+            storage[count] = input;
+            count++;
             System.out.println(horizontalLine);
-            echo();
+            readInput();
         }
     }
 
