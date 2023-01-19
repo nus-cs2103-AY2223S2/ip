61afdc8a1d008ebc6f3b8e439388509b4fbc7ade
KSunil2001 <k.sunil.avinash@gmail.com>
2023-01-19 16:01:06 +0800

    Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,23 +1,48 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
-    public static void printReply(String message){
+    private static List<String> listOfCommands = new ArrayList<String>();
+    public static void printLine(){
         System.out.println("\t____________________________________________________________");
-        System.out.println("\t" + message);
-        System.out.println("\t____________________________________________________________");
+    }
+    public static void greet() {
+        printLine();
+        System.out.println("\tHello! I'm C-3PO, Human Cyborg Relations.\n\tWhat can I do for you?");
+        printLine();
+    }
+    public static void exit() {
+        printLine();
+        System.out.println("\tBye. Hope to see you again soon!");
+        printLine();
+    }
+    public static void saveCommand(String command) {
+        listOfCommands.add(command);
+        printLine();
+        System.out.println("\tadded: " + command);
+        printLine();
+    }
+    public static void listCommands() {
+        printLine();
+        for (int i = 1; i <= listOfCommands.size(); i++) {
+            System.out.println("\t" + i + ". " + listOfCommands.get(i-1));
+        }
+        printLine();
     }
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String message = "Hello! I'm C-3PO, Human Cyborg Relations.\n\tWhat can I do for you?";
-        printReply(message);
-        while(true) {
+        greet();
+        while (true) {
             String command = input.nextLine();
             if (command.equalsIgnoreCase("bye")) {
-                message = "Bye. Hope to see you again soon!";
-                printReply(message);
+                exit();
                 break;
+            } else if (command.equalsIgnoreCase("list")) {
+                listCommands();
+            } else {
+                saveCommand(command);
             }
-            printReply(command);
         }
         input.close();
     }
