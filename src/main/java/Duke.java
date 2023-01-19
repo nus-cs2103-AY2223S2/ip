20ce1ef0121218c829fa39388d91bc26d8c9e631
bryanongjxairetec <bryanong@airetec.sg>
2023-01-19 18:32:52 +0800

    Level 1 Commit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,24 @@
+import java.util.Locale;
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello! I'm Duke\n");
+        System.out.println("What can I do for you?\n");
+
+        while (true) {
+            String a = sc.nextLine().toLowerCase();
+            if (Objects.equals(a, "bye".toLowerCase())) {
+                System.out.println("Bye. Hope to see you again soon!");
+                return;
+            } else {
+                System.out.println(a);
+            }
+
+        }
     }
 }
