e9a7c041f0ecf53d49e9f8679e93d7acae72906d
Nicholas Tan <ntnic13@gmail.com>
2023-01-19 13:12:07 +0800

    Level-1: Implemented echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,28 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("Welcome welcome!");
+        echo();
     }
+
+    public static void echo() {
+        System.out.println("Start by typing something and Dukey will echo!! Type bye to exit!! ");
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                break;
+            }
+            System.out.println("Dukey: " + input);
+            if (input.equals("Bye")) {
+                System.out.println("Dukey: Did you mean 'bye'? Type 'bye' to exit!!");
+            }
+        }
+        System.out.println("\nDukey: Goodbye!! Please return to Dukey again soon!! :) ");
+        scanner.close();
+
+    }
+
+
+
 }
