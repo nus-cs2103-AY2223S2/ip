49ad3df17172ef0ea860d32624ae81869460618e
Arkobrata Chaudhuri <arkobrata.2002@gmail.com>
2023-01-19 18:58:12 +0800

    Added Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -10,17 +11,26 @@ public class Duke {
         System.out.println("What can I do for you?");
         System.out.println(INDENT_LINE);
 
+        ArrayList<String> commandList = new ArrayList<>();
+
         while (true) {
-            String command = sc.next();
-            if (!command.equals("bye")) {
-                System.out.println(INDENT_LINE);
-                System.out.println(command);
-                System.out.println(INDENT_LINE);
-            } else {
+            String command = sc.nextLine();
+            if (command.equals("bye")) {
                 System.out.println(INDENT_LINE);
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println(INDENT_LINE);
                 break;
+            } else if (command.equals("list")) {
+                System.out.println(INDENT_LINE);
+                for (int i = 0; i < commandList.size(); i++) {
+                    System.out.println((i+1) + ". " + commandList.get(i));
+                }
+                System.out.println(INDENT_LINE);
+            } else {
+                commandList.add(command);
+                System.out.println(INDENT_LINE);
+                System.out.println("added: " + command);
+                System.out.println(INDENT_LINE);
             }
         }
     }
