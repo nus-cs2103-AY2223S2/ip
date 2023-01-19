c8a2b0902bb74494035aec4b7c5b67dd5b2ce4da
jacques <jacquesliaozeli@hotmail.com>
2023-01-19 22:27:12 +0800

    level-3 commit, mark and unmark

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 public class Duke {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> inputList = new ArrayList<>();
+        ArrayList<Task> tasks = new ArrayList<>();
 
         System.out.println("Hello, I am Duke. \nWhat can I do for you?");
 
@@ -12,12 +12,27 @@ public class Duke {
 
         while (!input.equalsIgnoreCase("bye")) {
             if (input.equalsIgnoreCase("list")) {
-                for (int i = 0; i < inputList.size(); i++) {
-                    System.out.println(i+1 + ". " + inputList.get(i));
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println(i+1 + ". " + tasks.get(i));
                 }
+            } else if (input.startsWith("mark ")) {
+                String taskName = input.substring(5);
+                int index = Integer.parseInt((taskName)) - 1;
+                Task task = tasks.get(index);
+                task.mark();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(task);
+            } else if (input.startsWith("unmark ")) {
+                String taskName = input.substring(7);
+                int index = Integer.parseInt((taskName)) - 1;
+                Task task = tasks.get(index);
+                task.unmark();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println(task);
             } else {
-                System.out.println("added: " + input);
-                inputList.add(input);
+                Task task = new Task(input);
+                tasks.add(task);
+                System.out.println("added: " + task);
             }
             input = scanner.nextLine();
         }
@@ -25,5 +40,40 @@ public class Duke {
         System.out.println("Bye. Hope to see you again soon!");
 
         scanner.close();
+    }
+}
+
+class Task {
+    private String taskName;
+    private boolean done;
+
+    public Task(String taskName) {
+        this.taskName = taskName;
+        this.done = false;
+    }
+
+    public String getName() {
+        return taskName;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void mark() {
+        this.done = true;
+    }
+
+    public void unmark() {
+        this.done = false;
+    }
+
+    @Override
+    public String toString() {
+        if (this.done) {
+            return "[X] " + this.taskName;
+        } else {
+            return "[ ] " + this.taskName;
+        }
     }
 }
