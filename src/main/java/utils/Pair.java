170b29cef40f2cb25624a3558a530a6e995fd30a
Xiuxuan Wang <36021591+wxxedu@users.noreply.github.com>
2023-01-19 19:15:44 +0800

    implemented the pair class

---
+++ src/main/java/utils/Pair.java
@@ -0,0 +1,44 @@
+package utils;
+
+/**
+ * A Pair of objects.
+ * @param <L> the type of the left object.
+ * @param <R> the type of the right object.
+ */
+public class Pair<L, R> {
+    /**
+     * Creates a new pair.
+     * @param left the object stored as the left.
+     * @param right the object stored as the right.
+     */
+    Pair(L left, R right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    /**
+     * The left object.
+     */
+    private final L left;
+
+    /**
+     * The right object.
+     */
+    private final R right;
+
+    /**
+     * Gets the left object.
+     * @return the object stored as left.
+     */
+    public L getLeft() {
+        return left;
+    }
+
+    /**
+     * Gets the right object.
+     * @return the object stored as right.
+     */
+    public R getRight() {
+        return right;
+    }
+}
