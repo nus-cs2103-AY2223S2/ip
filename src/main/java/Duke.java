418c04a584c3a6b094c3de1b2f83e90c3651c918
GlenLim <glenlimyf@gmail.com>
2023-01-19 17:57:47 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,28 +1,63 @@
 import java.util.*;
 public class Duke {
-    private static ArrayList<String> tasks = new ArrayList<>();
+    private static ArrayList<Task> tasks = new ArrayList<>();
 
     public Boolean awaitInstruction(){
         Scanner sc = new Scanner(System.in);
         String answer = sc.nextLine();
+ok
         System.out.println("___________________");
-        if (answer.equals("bye")){
+        if (answer.startsWith("mark")){
+            String wordArr[] = answer.split(" ");
+            System.out.println(Arrays.toString(wordArr));
+            String index = wordArr[1];
+            markTask(index);
+        }
+        else if (answer.startsWith("unmark")){
+            String wordArr[] = answer.split(" ");
+            String index = wordArr[1];
+            unmarkTask(index);
+        }
+        else if (answer.equals("bye")){
             System.out.println("Goodbye");
             return true;
         }
-        if (answer.equals("list")){
-            for (int i = 0; i < tasks.size(); i++){
-                System.out.print((i+1) + ": " + tasks.get(i) + "\n");
-            }
-        }
-        else{
-            tasks.add(answer);
-            System.out.println("added:" + answer);
+        else if (answer.equals("list")){ //return list of tasks
+            printTaskList();
+        } else { //else new task
+            Task task = new Task(answer);
+            addTask(task);
         }
         System.out.println("___________________");
         return false;
     }
+    private void addTask(Task task){
+        tasks.add(task);
+        System.out.println("added:" + task.getDescription());
+    }
 
+    private void printTaskList(){
+        for (int i = 0; i < tasks.size(); i++){
+            Task task = tasks.get(i);
+            System.out.print((i+1) + "." + "[" + task.getStatusIcon() + "] " + task.getDescription() + "\n");
+        }
+    }
+
+    private void markTask(String index){
+        int i = Integer.parseInt(index);
+        Task task = tasks.get(i - 1);
+        task.updateState();
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.print("[" + task.getStatusIcon() + "] " + task.getDescription() + "\n");
+    }
+
+    private void unmarkTask(String index){
+        int i = Integer.parseInt(index);
+        Task task = tasks.get(i - 1);
+        task.updateState();
+        System.out.println("OK, I've marked this task as not done yet:");
+        System.out.print("[" + task.getStatusIcon() + "] " + task.getDescription() + "\n");
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
