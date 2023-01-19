453a63d202af3dc000b32648e1bd9cd740e3673d
Bryan Juniano <junianob@gmail.com>
2023-01-19 20:02:27 +0800

    Completed Level-3
    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 
+import static java.lang.Integer.parseInt;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -10,21 +12,41 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("How can I help you?");
         String list[] = new String[100];
+        Boolean done[] = new Boolean[100];
         int index = 0;
         while(true) {
             Scanner myObj = new Scanner(System.in);
             String input = myObj.nextLine();
             if(input.equals("list")){
+                System.out.println("Here are your tasks:");
                 for(int i = 0; i<index; i++){
-                    System.out.println(i+1+": "+list[i]);
+                    if(done[i]){
+                        System.out.println(i + 1 + ".[x] " + list[i]);
+                    }
+                    else {
+                        System.out.println(i + 1 + ":[ ] " + list[i]);
+                    }
                 }
             }
-            if(input.equals("bye")){
+            else if(input.split(" ")[0].equals("mark")){
+                int temp = parseInt(input.split(" ")[1]);
+                System.out.println("marking task " + temp);
+                done[temp-1] = true;
+
+            }
+            else if(input.split(" ")[0].equals("unmark")){
+                int temp = parseInt(input.split(" ")[1]);
+                System.out.println("unmarking task " + temp);
+                done[temp-1] = false;
+
+            }
+            else if(input.equals("bye")){
                 System.out.println("bai");
                 break;
             }
             else {
                 System.out.println("added: " +input);
+                done[index] = false;
                 list[index] = input;
                 index++;
             }
