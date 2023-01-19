a650fc6ff6865206ea0a74c4f9a661975b96b91e
jerome-neo <jeromeneo9@gmail.com>
2023-01-19 23:09:25 +0800

    Level 4. ToDos, Events, Deadlines

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,7 +8,7 @@ public class Duke {
         System.out.println("Here are the tasks in your list: ");
         for (int i = 0; i < stored.size(); i++) {
             Task task = stored.get(i);
-            System.out.println((i+1) + ". [" + task.getStatusIcon() + "] " + task.getDescription());
+            System.out.println((i+1) + ". " + task.toString());
         }
     }
     public static void main(String[] args) {
@@ -43,15 +43,43 @@ public class Duke {
                 Task task = myTasks.get(taskNumber);
                 task.taskDone();
                 System.out.println("Nice! I've marked this task as done:");
-                System.out.println(" [" + task.getStatusIcon() + "] " + task.getDescription());
-
+                System.out.println(task.toString());
+                System.out.println(separator);
             } else if (input.startsWith("unmark")) {
                 int taskNumber = Integer.parseInt(input.substring(7));
                 taskNumber--;
                 Task task = myTasks.get(taskNumber);
                 task.taskNotDone();
                 System.out.println("OK, I've marked this task as not done yet:");
-                System.out.println(" [" + task.getStatusIcon() + "] " + task.getDescription());
+                System.out.println(task.toString());
+                System.out.println(separator);
+            } else if (input.startsWith("todo")) {
+                String taskDescription = input.substring(5);
+                Todo task = new Todo(taskDescription);
+                myTasks.add(task);
+                System.out.println("Got it. I've added this task:");
+                System.out.println("Now we have " + myTasks.size() + " tasks in the list.");
+                System.out.println(separator);
+            } else if (input.startsWith("deadline")) {
+                String[] temp = input.substring(9).split(" /by ");
+                String taskDescription = temp[0];
+                String by = temp[1];
+                Deadline task = new Deadline(taskDescription, by);
+                myTasks.add(task);
+                System.out.println("Got it. I've added this task:");
+                System.out.println("Now we have " + myTasks.size() + " tasks in the list.");
+                System.out.println(separator);
+            } else if (input.startsWith("event")) {
+                String[] temp = input.substring(6).split(" /from ");
+                String taskDescription = temp[0];
+                String[] time = temp[1].split(" /to ");
+                String from = time[0];
+                String to = time[1];
+                Event task = new Event(taskDescription, from, to);
+                myTasks.add(task);
+                System.out.println("Got it. I've added this task:");
+                System.out.println("Now we have " + myTasks.size() + " tasks in the list.");
+                System.out.println(separator);
             } else {
                 Task task = new Task(input);
                 myTasks.add(task);
