eb823af491452ed57609b8e72d62d2846946993e
GlennOngJunJie <glennongjj@gmail.com>
2023-01-19 14:19:14 +0800

    Level 3 done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,16 +11,25 @@ public class Duke {
 //        System.out.println("Hello from\n" + logo);
           System.out.println("Hello! I'm Duke\nWhat can I do for you?");
           Scanner userInputObj = new Scanner(System.in);
-          ArrayList<String> storage = new ArrayList<String>();
+          ArrayList<Task> storage = new ArrayList<Task>();
           String userInput = "";
           while (!userInput.equals("bye")) {
               userInput = userInputObj.nextLine();
               if (userInput.equals("list")) {
                   for (int i = 0; i < storage.size(); i++) {
-                      System.out.println((i + 1) + ". " + storage.get(i));
+                      System.out.println((i + 1) + ".[" + storage.get(i).getStatusIcon() + "] " + storage.get(i).description);
                   }
+              } else if (userInput.contains("mark") && userInput.substring(0,4).equals("mark")) {
+                  int position = Integer.valueOf(userInput.substring(5));
+                  storage.get(position - 1).markAsDone();
+                  System.out.println("Nice! I've marked this task as done:\n" + "[" + storage.get(position - 1).getStatusIcon() + "] " + storage.get(position - 1).description);
+              } else if (userInput.contains("unmark") && userInput.substring(0,6).equals("unmark")) {
+                  int position = Integer.valueOf(userInput.substring(7));
+                  storage.get(position - 1).markAsNotDone();
+                  System.out.println("OK, I've marked this task as not done yet:\n" + "[" + storage.get(position - 1).getStatusIcon() + "] " + storage.get(position - 1).description);
               } else {
-                  storage.add(userInput);
+                  Task userInputTask = new Task(userInput);
+                  storage.add(userInputTask);
                   System.out.println("added: " + userInput);
               }
           }
