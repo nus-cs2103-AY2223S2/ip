7fb3c62b82e24090b65ccb0470a9d15b7a605f39
Muhammad Fahim Tajwar <fahim.tajwar@u.nus.edu>
2023-01-19 15:28:52 +0800

    Started with chatbot that imitates user commands, and exits on 'bye'.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
+      String divider = "____________________________________________________________\n";
+      System.out.println(divider + "Hello! I'm Duke");
+      System.out.println("What can I do for you?\n" + divider);
+
+      Scanner sc = new Scanner(System.in);
+      while(sc.hasNext()) {
+        String instr = sc.next();
+        if(instr.equals("bye")) {
+          System.out.println(divider + "Bye. Hope to see you again soon!\n" + divider);
+          break;
+        } else {
+          System.out.println(divider + instr + "\n" + divider);
+        }
+      }
     }
 }
