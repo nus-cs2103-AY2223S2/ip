5fc80a748f0e8c09f8eb270ba4fc1abc5bf4142e
JavonTeo <javonteo@hotmail.com>
2023-01-19 11:58:53 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Duke {
-//    public static void main(String[] args) {
+    //    public static void main(String[] args) {
 //        String logo = " ____        _        \n"
 //                + "|  _ \\ _   _| | _____ \n"
 //                + "| | | | | | | |/ / _ \\\n"
@@ -15,16 +15,30 @@ public class Duke {
 
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
-        String[] ls = new String[100];
+        Task[] ls = new Task[100];
         int lsCount = 0;
         while (!s.equals("bye")) {
             if (s.equals("list")) {
                 for (int i = 0; i < lsCount; i++) {
-                    System.out.println( (i+1) + ". " + ls[i]);
+                    System.out.println((i + 1) + ". " + ls[i]);
                 }
             }
             else {
-                ls[lsCount] = s;
+                String[] words = s.split(" ");
+                if (words.length == 2) {
+                    try {
+                        int i = Integer.parseInt(words[1]);
+                        if (words[0].equals("mark")) {
+                            ls[i-1].markUnmark(true);
+                        } else if (words[0].equals("unmark")) {
+                            ls[i-1].markUnmark(false);
+                        }
+                        s = sc.nextLine();
+                        continue;
+                    } catch (NumberFormatException err) {
+                    }
+                }
+                ls[lsCount] = new Task(s);
                 lsCount += 1;
                 System.out.println("added: " + s);
             }
---
+++ src/main/java/Task.java
@@ -0,0 +1,33 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "[X]" : "[ ]"); // mark done task with X
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    @Override
+    public String toString() {
+        return this.getStatusIcon() + " " + this.getDescription();
+    }
+
+    public void markUnmark(boolean marked) {
+        this.isDone = marked;
+        if (marked) {
+            System.out.println("Nice! I've marked this task as done:");
+        }
+        else {
+            System.out.println("OK, I've marked this task as not done yet:");
+        }
+        System.out.println(this);
+    }
+}
