0e603c417c182afbc50a2bc4a1aa61b131b49043
Wesley Teo <wesley.tyx@gmail.com>
2023-01-19 16:19:50 +0800

    Added mark feature to mark tasks as done or undone

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,7 @@ import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Duke {
-    static LinkedList<String> todo = new LinkedList<>();
+    static LinkedList<Task> todo = new LinkedList<>();
     static String logo = " ____        _        \n"
             + "|  _ \\ _   _| | _____ \n"
             + "| | | | | | | |/ / _ \\\n"
@@ -10,19 +10,57 @@ public class Duke {
             + "|____/ \\__,_|_|\\_\\___|";
     static String line = "---------------------------------";
 
+    static void add(Task cur) {
+        todo.add(cur);
+        System.out.println("Added: " + cur.description);
+    }
+    static void list(LinkedList<Task> list) {
+        for (Task cur : todo) {
+            System.out.println(todo.indexOf(cur) + 1 + ". [" + cur.getStatusIcon() + "] " + cur.description);
+        }
+    }
+
+    static boolean isNumeric(String str) {
+        char[] charArray = str.toCharArray();
+        for (char c : charArray) {
+            if (!Character.isDigit(c)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static void markTask(int index) {
+        Task cur = todo.get(index);
+        cur.markDone();
+        System.out.println("Nice! I've marked this task as done: ");
+        System.out.println("[" + cur.getStatusIcon() + "] " + cur.description);
+
+    }
+    static void unmarkTask(int index) {
+        Task cur = todo.get(index);
+        cur.markUndone();
+        System.out.println("OK, I've marked this task as not done yet: ");
+        System.out.println("[" + cur.getStatusIcon() + "] " + cur.description);
+
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Hello from\n" + logo + "  managed by Wesley Teo.\n\nWhat can I do for you?\n" + line);
         String input = sc.nextLine();
         System.out.println(line);
+
         while (!input.equals("bye")) {
+            String[] inputArr = input.split(" ");
             if (input.equals("list")) {
-                for (String task : todo) {
-                    System.out.println(todo.indexOf(task) + 1 + ". " + task);
-                }
-            } else {
-                todo.add(input);
-                System.out.println("Added: " + input);
+                list(todo);
+            } else if (inputArr.length == 2 && inputArr[0].equals("mark") && isNumeric(inputArr[1]) ) {
+                markTask(Integer.parseInt(inputArr[1]) -1);
+            } else if (inputArr.length == 2 && inputArr[0].equals("unmark") && isNumeric(inputArr[1])){
+                unmarkTask(Integer.parseInt(inputArr[1]) -1);
+            }
+            else {
+                add(new Task(input));
             }
             System.out.println(line);
             input = sc.nextLine();
