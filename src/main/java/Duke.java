6835a149f53758f872b6129bbb4db1c48c16619e
toh-xinyi <tohxinyi10@gmail.com>
2023-01-19 22:58:39 +0800

    Add ability to store text

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,11 @@
 import java.io.*;
+import java.util.*;
 public class Duke {
+    public static void printInListFormat(ArrayList<String> texts) {
+        for (int i = 0; i < texts.size(); i++) {
+            System.out.printf("%d. %s\n", i + 1, texts.get(i));
+        }
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -10,9 +16,16 @@ public class Duke {
         try {
             InputStreamReader isr = new InputStreamReader(System.in);
             BufferedReader br = new BufferedReader(isr);
+            ArrayList<String> text = new ArrayList<>();
             String line = br.readLine();
             while (!line.equals("bye")) {
-                System.out.println(line);
+                if (line.equals("list")) {
+                    printInListFormat(text);
+                }
+                else {
+                    text.add(line);
+                    System.out.printf("added: %s\n", line);
+                }
                 line = br.readLine();
             }
             System.out.println("Bye. Hope to see you again soon!");
