f96b880ca456b7592c68d440241b0325e363721b
venuslimm <limjiayivenus@gmail.com>
2023-01-19 12:29:22 +0800

    Handle invalid input using DukeException

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -60,59 +60,63 @@ public class Duke {
      * @param   input   User input for the program menu
      * @return  Status whether the program should exit or not
      */
-    private static boolean printMenu(String input) throws DukeException, NoSuchElementException, IllegalArgumentException {
+    private static boolean printMenu(String input) throws DukeException, NoSuchElementException {
         boolean exitStatus = false;
 
         String[] splitInput = input.split(" ");
-        Action action = Action.valueOf(splitInput[0]);
 
-        switch (action) {
-            case todo:
-                if (splitInput.length < 2) {
-                    throw new DukeException("The description of a todo cannot be empty.");
-                }
-                todo(input.split(" ", 2)[1]);
-                break;
-            case deadline:
-                if (splitInput.length < 2) {
-                    throw new DukeException("The description of a deadline cannot be empty.");
-                }
-                deadline(input.split(" ", 2)[1]);
-                break;
-            case event:
-                if (splitInput.length < 2) {
-                    throw new DukeException("The description of a event cannot be empty.");
-                }
-                event(input.split(" ", 2)[1]);
-                break;
-            case list:
-                list();
-                break;
-            case mark:
-                if (splitInput.length < 2) {
-                    throw new DukeException("The task index cannot be empty.");
-                }
-                mark(splitInput[1]);
-                break;
-            case unmark:
-                if (splitInput.length < 2) {
-                    throw new DukeException("The task index cannot be empty.");
-                }
-                unmark(splitInput[1]);
-                break;
-            case delete:
-                if (splitInput.length < 2) {
-                    throw new DukeException("The task index cannot be empty.");
-                }
-                delete(splitInput[1]);
-                break;
-            case bye:
-                exit();
-                exitStatus = true;
-                break;
-            default:
-                throw new DukeException("☹ I'm sorry, but Fake Duke doesn't know what that means :-(");
+        try {
+            Action action = Action.valueOf(splitInput[0]);
+
+            switch (action) {
+                case todo:
+                    if (splitInput.length < 2) {
+                        throw new DukeException("The description of a todo cannot be empty.");
+                    }
+                    todo(input.split(" ", 2)[1]);
+                    break;
+                case deadline:
+                    if (splitInput.length < 2) {
+                        throw new DukeException("The description of a deadline cannot be empty.");
+                    }
+                    deadline(input.split(" ", 2)[1]);
+                    break;
+                case event:
+                    if (splitInput.length < 2) {
+                        throw new DukeException("The description of a event cannot be empty.");
+                    }
+                    event(input.split(" ", 2)[1]);
+                    break;
+                case list:
+                    list();
+                    break;
+                case mark:
+                    if (splitInput.length < 2) {
+                        throw new DukeException("The task index cannot be empty.");
+                    }
+                    mark(splitInput[1]);
+                    break;
+                case unmark:
+                    if (splitInput.length < 2) {
+                        throw new DukeException("The task index cannot be empty.");
+                    }
+                    unmark(splitInput[1]);
+                    break;
+                case delete:
+                    if (splitInput.length < 2) {
+                        throw new DukeException("The task index cannot be empty.");
+                    }
+                    delete(splitInput[1]);
+                    break;
+                case bye:
+                    exit();
+                    exitStatus = true;
+                    break;
+            }
+        } catch (IllegalArgumentException e) {
+            throw new DukeException("☹ I'm sorry, but Fake Duke doesn't know what that means :-(");
         }
+
         return exitStatus;
     }
 
