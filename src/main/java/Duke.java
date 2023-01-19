ec29612ddaf624372e5ae666e6cd4b30361f5459
FireRadical22 <97332558+FireRadical22@users.noreply.github.com>
2023-01-19 20:58:32 +0800

    Code cleanup
    - Created mark and unmark method to throw DukeException in the event of user input error
    - Updated delete method to throw DukeException in the event of user input error

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -42,7 +42,7 @@ public class Duke {
                 } else if (lcInput.contains("deadline")) {
                     if (inputs.length <= 1) {
                         throw new DukeException("What is the deadline task????");
-                    } else if (!input.toLowerCase().contains("/by")) {
+                    } else if (!lcInput.contains("/by")) {
                         throw new DukeException("Put in the deadline of your task Please!");
                     } else {
                         deadline(input);
@@ -60,45 +60,19 @@ public class Duke {
                         throw new DukeException("What is the event task????");
                     }
                     events(input);
-                } else {
-                    if (Pattern.compile("\\D+.\\d+").matcher(input).find()) {
-                        int index = Integer.parseInt(inputs[1]);
-                        if (inputs[0].equals("mark")) {
-                            try {
-                                storage.get(index - 1).markAsDone();
-                                System.out.println(
-                                        "_____________________________________\n"
-                                                + "Nice! I've marked this task as done\n"
-                                                + " " + storage.get(index - 1).toString() + "\n"
-                                                + "_____________________________________\n"
-                                );
-                            } catch (IndexOutOfBoundsException err){
-                                throw new DukeException("Invalid index given!");
-                            }
-                        } else if (inputs[0].equals("unmark")) {
-                            try {
-                                storage.get(index - 1).unMark();
-                                System.out.println(
-                                        "_____________________________________\n"
-                                                + "Ok, I've marked this task as not done yet\n"
-                                                + " " + storage.get(index - 1).toString() + "\n"
-                                                + "_____________________________________\n"
-                                );
-                            } catch (IndexOutOfBoundsException err) {
-                                throw new DukeException("Invalid index given!");
-                            }
-                        } else if (inputs[0].equals("delete")) {
-                            try {
-                                delete(index - 1);
-                            } catch (IndexOutOfBoundsException err){
-                                throw new DukeException("Invalid index given!");
-                            }
-                        } else {
-                            throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
-                        }
+                } else if (Pattern.compile("\\D+.\\d+").matcher(input).find()){
+                    int index = Integer.parseInt(inputs[1]);
+                    if (inputs[0].equals("mark")) {
+                        mark(inputs, index);
+                    } else if (inputs[0].equals("unmark")) {
+                        unmark(index);
+                    } else if (inputs[0].equals("delete")) {
+                        delete(index - 1);
                     } else {
                         throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
                     }
+                } else {
+                    throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
                 }
             } catch (DukeException e){
                 System.out.println(
@@ -197,15 +171,50 @@ public class Duke {
         return "Now you have " + newCount + task + " in the list.";
     }
 
-    private static void delete(int index){
-        Task taskRemoved = storage.remove(index);
-        System.out.println(
-                "_____________________________________\n"
-                +  "Noted. I've removed this task:\n"
-                +  " " + taskRemoved.toString() + "\n"
-                + taskCount() + "\n"
-                + "_____________________________________\n"
-        );
+    private static void delete(int index) throws DukeException{
+        try {
+            Task taskRemoved = storage.remove(index);
+            System.out.println(
+                    "_____________________________________\n"
+                            +  "Noted. I've removed this task:\n"
+                            +  " " + taskRemoved.toString() + "\n"
+                            + taskCount() + "\n"
+                            + "_____________________________________\n"
+            );
+        } catch (IndexOutOfBoundsException err){
+            throw new DukeException("Invalid index given!");
+        }
+
+    }
+
+    public static void mark(String[] args, int index) throws DukeException{
+        if (args[0].equals("mark")) {
+            try {
+                storage.get(index - 1).markAsDone();
+                System.out.println(
+                        "_____________________________________\n"
+                                + "Nice! I've marked this task as done\n"
+                                + " " + storage.get(index - 1).toString() + "\n"
+                                + "_____________________________________\n"
+                );
+            } catch (IndexOutOfBoundsException err) {
+                throw new DukeException("Invalid index given!");
+            }
+        }
+    }
+
+    public static void unmark(int index) throws DukeException{
+        try {
+            storage.get(index - 1).unMark();
+            System.out.println(
+                    "_____________________________________\n"
+                            + "Ok, I've marked this task as not done yet\n"
+                            + " " + storage.get(index - 1).toString() + "\n"
+                            + "_____________________________________\n"
+            );
+        } catch (IndexOutOfBoundsException err) {
+            throw new DukeException("Invalid index given!");
+        }
     }
 }
 
