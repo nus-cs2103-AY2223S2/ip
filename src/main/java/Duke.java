0aea3b9ded89e7bd2269cf5a51b2749063b3b00a
Sheemo <shaohongfly@gmail.com>
2023-01-19 18:42:05 +0800

    Add Greet and echo functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,56 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static final String bannerLine = "_".repeat(30);
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String logo = " ,ggg, ,ggggggg,                                                               ,ggg,\n"
+                    + "dP\"\"Y8,8P\"\"\"\"\"Y8b              ,dPYb,                                         dP\"\"8I\n"
+                    + "Yb, `8dP'     `88              IP'`Yb                                        dP   8\n"
+                    + " `\"  88'       88              I8  8I      gg               gg              dP    88\n"
+                    + "     88        88              I8  8bgg,   \"\"               \"\"             ,8'    88\n"
+                    + "     88        88    ,gggg,gg  I8 dP\" \"8   gg    ,gggggg,   gg             d88888888   gg     gg    ,gggg,gg   ,ggg,,ggg,,ggg,    ,ggg,\n"
+                    + "     88        88   dP\"  \"Y8I  I8d8bggP\"   88    dP\"\"\"\"8I   88       __   ,8\"     88   I8     8I   dP\"  \"Y8I  ,8\" \"8P\" \"8P\" \"8,  i8\" \"8i\n"
+                    + "     88        88  i8'    ,8I  I8P' \"Yb,   88   ,8'    8I   88      dP\"  ,8P      Y8   I8,   ,8I  i8'    ,8I  I8   8I   8I   8I  I8, ,8I\n"
+                    + "     88        Y8,,d8,   ,d8b,,d8    `Yb,_,88,_,dP     Y8,_,88,_    Yb,_,dP       `8b,,d8b, ,d8I ,d8,   ,d8b,,dP   8I   8I   Yb, `YbadP'\n"
+                    + "     88        `Y8P\"Y8888P\"`Y888P      Y88P\"\"Y88P      `Y88P\"\"Y8     \"Y8P\"         `Y8P\"\"Y88P\"888P\"Y8888P\"`Y88P'   8I   8I   `Y8888P\"Y888\n"
+                    + "                                                                                            ,d8I'\n"
+                    + "                                                                                          ,dP'8I\n"
+                    + "                                                                                         ,8\"  8I\n"
+                    + "                                                                                         I8   8I\n"
+                    + "                                                                                         `8, ,8I\n"
+                    + "                                                                                          `Y8P\"\n";
         System.out.println("Hello from\n" + logo);
+
+        Duke duke = new Duke();
+        duke.run();
+    }
+
+    public void run() {
+        greet();
+        commandLoop();
+    }
+
+    public void greet() {
+        printInBanner("Greetings humans~\n'tis I! Nakiri Ayame!~\nWhat can I do for you?");
+    }
+
+    public void commandLoop() {
+        Scanner input = new Scanner(System.in);
+        String cmd;
+        while (true) {
+            cmd = input.nextLine();
+            if (cmd.equals("bye")) {
+                break;
+            }
+            printInBanner(cmd);
+        }
+        printInBanner("Otsunakiri~\nByebye!~");
+    }
+
+    private void printInBanner(String message) {
+        System.out.println(bannerLine);
+        System.out.println(message);
+        System.out.println(bannerLine);
     }
 }
