ca7faad5fb06c6b28c3a5841a244467804178f82
toh-xinyi <tohxinyi10@gmail.com>
2023-01-19 23:43:55 +0800

    Ability to mark tasks as done or undone

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,29 +1,35 @@
 import java.io.*;
 import java.util.*;
 public class Duke {
-    public static void printInListFormat(ArrayList<String> texts) {
-        for (int i = 0; i < texts.size(); i++) {
-            System.out.printf("%d. %s\n", i + 1, texts.get(i));
+    public static void printInListFormat(ArrayList<Task> tasks) {
+        for (int i = 0; i < tasks.size(); i++) {
+            Task currTask = tasks.get(i);
+            System.out.printf("%d. [%s] %s\n", i + 1, currTask.getStatusIcon(),
+                    currTask.getDescription());
         }
     }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello! I'm Dupe\nWhat can I do for you?");
         try {
             InputStreamReader isr = new InputStreamReader(System.in);
             BufferedReader br = new BufferedReader(isr);
-            ArrayList<String> text = new ArrayList<>();
+            ArrayList<Task> tasks = new ArrayList<>();
             String line = br.readLine();
             while (!line.equals("bye")) {
                 if (line.equals("list")) {
-                    printInListFormat(text);
-                }
-                else {
-                    text.add(line);
+                    printInListFormat(tasks);
+                } else if (line.split(" ")[0].equals("mark")) {
+                    Task toMarkDone = tasks.get(Integer.parseInt(line.split(" ")[1]) - 1);
+                    toMarkDone.markDone();
+                    System.out.printf("Nice! I've marked this task as done:\n [%s] %s\n", toMarkDone.getStatusIcon(),
+                            toMarkDone.getDescription());
+                } else if (line.split(" ")[0].equals("unmark")) {
+                    Task toMarkUndone = tasks.get(Integer.parseInt(line.split(" ")[1]) - 1);
+                    toMarkUndone.markUndone();
+                    System.out.printf("OK, I've marked this task as not done yet:\n [%s] %s\n", toMarkUndone.getStatusIcon(),
+                            toMarkUndone.getDescription());
+                } else {
+                    tasks.add(new Task(line));
                     System.out.printf("added: %s\n", line);
                 }
                 line = br.readLine();
