7e1c9d449b43c462022fa2eaf666a7340fabd4e0
szejiancheng <sze2001jc@gmail.com>
2023-01-19 23:21:57 +0800

    Add delete method to DukeBehaviour.java, updated testcases

--- src/main/java/DukeBehaviour.java
+++ src/main/java/DukeBehaviour.java
@@ -10,26 +10,48 @@ public class DukeBehaviour {
     }
 
     public void receiveInput(String userIn){
-        if (userIn.equals("bye")){
-            //System.out.println("exit command received, exiting...");
-            isActive = false;
-        } else if (userIn.equals("list")){
-            displayList();
-        } else if (userIn.split(" ")[0].equals("mark")) {
-            mark(userIn);
-        } else if (userIn.split(" ")[0].equals("unmark")){
-            unmark(userIn);
-        } else {
-            addTask(userIn);
+        try {
+            if (userIn.equals("bye")) {
+                //System.out.println("exit command received, exiting...");
+                isActive = false;
+            } else if (userIn.equals("list")) {
+                displayList();
+            } else if (userIn.split(" ")[0].equals("mark")) {
+                mark(userIn);
+            } else if (userIn.split(" ")[0].equals("unmark")) {
+                unmark(userIn);
+            } else if (userIn.split(" ")[0].equals("delete")) {
+                delete(userIn);
+            } else {
+                addTask(userIn);
+            }
+        } catch (DukeException e) {
+            System.out.println(e.getMessage());
         }
+    }
+
+    private void delete(String userIn) throws DukeException{
+        int index = Integer.parseInt(userIn.split(" ")[1]);
+        if (index < 1 || index > taskList.size()){
+            throw new DukeException("index " + index +" not in range!");
+        }
+        Task currTask = taskList.get(index-1);
+        taskList.remove(index-1);
+        System.out.println("Noted. I've removed this task:");
+        System.out.println(currTask);
+        System.out.println("Now you have " + taskList.size() + " tasks in the list.");
     }
 
     private void echo(String userIn){
         System.out.println(userIn);
     }
 
-    private void mark(String userIn) {
-        Task currTask = taskList.get(Integer.parseInt(userIn.split(" ")[1])-1);
+    private void mark(String userIn) throws DukeException{
+        int index = Integer.parseInt(userIn.split(" ")[1]);
+        if (index < 1 || index > taskList.size()){
+            throw new DukeException("index " + index +" not in range!");
+        }
+        Task currTask = taskList.get(index-1);
         currTask.markDone();
         System.out.println("Nice! I've marked this task as done:");
         System.out.println(currTask);
