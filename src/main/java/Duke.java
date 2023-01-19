79f4cb168f3a5a499d2649edeba1b8379cb68585
Jed Ng <jednghk@gmail.com>
2023-01-19 23:24:11 +0800

    Add an echo feature for Duke skeleton

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,40 @@
+import java.util.Scanner;
+
+/*
+Level-1
+idea:
+- design a "reply" method to format the duke reply text
+- print hello statement
+- scanf input
+    - print indented horizontal line
+    - print input
+    - print indented horizontal line
+
+ */
 public class Duke {
+    private static void reply(String s) {
+        System.out.println("\t"
+                + "____________________________________________________________");
+        System.out.println("\t" + s.replace("\n", "\n    "));
+        System.out.println("\t"
+                + "____________________________________________________________");
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(logo);
+        reply("Hello! I'm Duke\nWhat can I do for you?");
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String userInput = sc.nextLine();
+            if (userInput.equals("bye")) {
+                reply("Bye! Hope to see you again soon!");
+                break;
+            }
+            reply(userInput);
+        }
     }
 }
