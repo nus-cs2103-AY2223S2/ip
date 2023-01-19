344486d081f7b3d2909ac5735ead171e3ca5c4cb
filbert <fillberth@gmail.com>
2023-01-19 23:52:36 +0800

    Implement Level 5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -46,75 +46,111 @@ public class Duke {
 
         // level 3 functionality:
         while (true) {
-            // scan for user input
-            rawInput = scanner.nextLine();
-            rawSplit = rawInput.split(" ", 2);
-            command = rawSplit[0];
-            if (rawSplit.length > 1) {
-                arguments = rawSplit[1].split("\\W\\/[a-zA-Z]+\\W");
-            }
+            try {
+                // scan for user input
+                rawInput = scanner.nextLine();
+                rawSplit = rawInput.split(" ", 2);
+                command = rawSplit[0];
+                if (rawSplit.length > 1) {
+                    arguments = rawSplit[1].split("\\W\\/[a-zA-Z]+\\W");
+                } else {
+                    arguments = new String[0];
+                }
 
-            // parse commands with no arguments
-            if (command.equals("bye")) {
-                // case: "bye"
-                Duke.prettyPrint("Bye. Hope to see you again soon!");
-                break;
-            } else if (command.equals("list")) {
-                // case: "list"
-                StringBuilder sb = new StringBuilder(
-                    "Here are the tasks in your list:\n"
-                );
-                for (int i = 0; i < list.size(); i++) {
-                    Task currentTask = list.get(i);
+                // parse commands with no arguments
+                if (command.equals("bye")) {
+                    Duke.prettyPrint("Bye. Hope to see you again soon!");
+                    break;
+                } else if (command.equals("list")) {
+                    if (list.size() == 0) {
+                        Duke.prettyPrint("You have no tasks! Try adding some.");
+                    } else {
+                        StringBuilder sb = new StringBuilder(
+                            "Here are the tasks in your list:\n"
+                        );
+                        for (int i = 0; i < list.size(); i++) {
+                            Task currentTask = list.get(i);
+                            String s = String.format(
+                                "%d: %s\n",
+                                i + 1,
+                                currentTask.toString()
+                            );
+                            sb.append(s);
+                        }
+                        // pprint string
+                        Duke.prettyPrint(sb.toString());
+                    }
+                }
+                // parse commands with arguments
+                else if (command.equals("mark")) {
+                    // possible errors for mark and unmark
+                    // 1. invalid # arguments (no mark target given)
+                    // 2. mark target doesnt exist (out of range)
+                    int index = Integer.parseInt(arguments[0]) - 1;
+                    Task currentTask = list.get(index);
+                    currentTask.markAsDone();
                     String s = String.format(
-                        "%d: %s\n",
-                        i + 1,
+                        "Nice! I've marked this task as done:\n %s",
                         currentTask.toString()
                     );
-                    sb.append(s);
+                    Duke.prettyPrint(s);
+                } else if (command.equals("unmark")) {
+                    int index = Integer.parseInt(arguments[0]) - 1;
+                    Task currentTask = list.get(index);
+                    currentTask.unmarkAsDone();
+                    String s = String.format(
+                        "Ok, I've marked this task as not done yet:\n %s",
+                        currentTask.toString()
+                    );
+                    Duke.prettyPrint(s);
+                } else if (command.equals("todo")) {
+                    // implicitly handles todo commands with empty descriptions
+                    // as those will have 0 arguments
+                    if (arguments.length != 1) {
+                        throw new InvalidArgumentException(
+                            command,
+                            arguments.length,
+                            1
+                        );
+                    }
+                    Task currentTask = new ToDo(arguments[0]);
+                    Duke.addTaskToList(currentTask, list);
+                } else if (command.equals("deadline")) {
+                    // possible errors for deadline & event
+                    // 1. arguments may not have the proper format.
+                    //    code currently reads any string starting with '/' as
+                    //    the start of an argument. not necessarily '/by'
+                    //    or '/from' or '/to', as expected
+                    if (arguments.length != 2) {
+                        throw new InvalidArgumentException(
+                            command,
+                            arguments.length,
+                            2
+                        );
+                    }
+                    Task currentTask = new Deadline(arguments[0], arguments[1]);
+                    Duke.addTaskToList(currentTask, list);
+                } else if (command.equals("event")) {
+                    if (arguments.length != 3) {
+                        throw new InvalidArgumentException(
+                            command,
+                            arguments.length,
+                            3
+                        );
+                    }
+                    Task currentTask = new Event(
+                        arguments[0],
+                        arguments[1],
+                        arguments[2]
+                    );
+                    Duke.addTaskToList(currentTask, list);
                 }
-                // pprint string
-                Duke.prettyPrint(sb.toString());
-            }
-            // parse commands with arguments
-            else if (command.equals("mark")) {
-                // case: "mark'
-                int index = Integer.parseInt(arguments[0]) - 1;
-                Task currentTask = list.get(index);
-                currentTask.markAsDone();
-                String s = String.format(
-                    "Nice! I've marked this task as done:\n %s",
-                    currentTask.toString()
-                );
-                Duke.prettyPrint(s);
-            } else if (command.equals("unmark")) {
-                // case: "unmark'
-                int index = Integer.parseInt(arguments[0]) - 1;
-                Task currentTask = list.get(index);
-                currentTask.unmarkAsDone();
-                String s = String.format(
-                    "Ok, I've marked this task as not done yet:\n %s",
-                    currentTask.toString()
-                );
-                Duke.prettyPrint(s);
-            } else if (command.equals("todo")) {
-                // case: "todo"
-                Task currentTask = new ToDo(arguments[0]);
-                Duke.addTaskToList(currentTask, list);
-            } else if (command.equals("deadline")) {
-                Task currentTask = new Deadline(arguments[0], arguments[1]);
-                Duke.addTaskToList(currentTask, list);
-            } else if (command.equals("event")) {
-                Task currentTask = new Event(
-                    arguments[0],
-                    arguments[1],
-                    arguments[2]
-                );
-                Duke.addTaskToList(currentTask, list);
-            } else {
-                Duke.prettyPrint(
-                    "Sorry, I didn't understand your command. Maybe try again?"
-                );
+                // undefined commands
+                else {
+                    throw new UnknownCommandException(rawInput);
+                }
+            } catch (DukeException e) {
+                Duke.prettyPrint(e.toString());
             }
         }
     }
