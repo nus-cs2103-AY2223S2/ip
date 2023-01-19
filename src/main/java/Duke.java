8dfc57364cb236421dc210561ca192bef4eded9d
Clyde Lhui <62978225+clydelhui@users.noreply.github.com>
2023-01-20 01:40:41 +0800

    Modified Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
@@ -8,12 +10,26 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         Scanner usrInput = new Scanner(System.in);
-        String input = usrInput.nextLine();
-        while(!input.equals("bye")){
-            System.out.println(input);
+        String input = "";
+        List<String> lst = new ArrayList<>();
+        while(true){
             input = usrInput.nextLine();
+            switch (input) {
+                case "bye":
+                    System.out.println("Goodbye!");
+                    usrInput.close();
+                    System.exit(0);
+                case "list":
+                    for (int j = 0; j < lst.size(); j++) {
+                        System.out.println((j + 1) + ":" + lst.get(j));
+                    }
+                    break;
+                default:
+                    System.out.println("added:" + input);
+                    lst.add(input);
+                    
+            }
         }
-        System.out.println("Goodbye!");
     }
 
 }
