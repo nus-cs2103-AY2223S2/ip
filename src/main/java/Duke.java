8e9294bac40b93fdbed619dfbf2cfb4aaaf2adc7
CedricPei <wenqi_pei@u.nus.edu>
2023-01-19 12:20:51 +0800

    Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,18 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        int exit = 0;
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?\n" );
+
+        while (exit != 1) {
+            Scanner sc = new Scanner(System.in);
+            String info = sc.nextLine();
+            if (info.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!\n");
+                exit = 1;
+            } else {
+                System.out.println(info);
+            }
+        }
     }
 }
