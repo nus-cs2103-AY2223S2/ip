7d0f9695921d8f0e4ef4f67fd7a5714c285def8a
thedesalizes <altaria99@gmail.com>
2023-01-19 20:15:40 +0800

    Implemented Level-2 features

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 public class Duke {
     public static void main(String[] args) {
         String line = "____________________________________________________________";
@@ -13,9 +14,18 @@ public class Duke {
         System.out.println(line);
         Scanner scanner = new Scanner(System.in);
         boolean running = true;
+        ArrayList<String> tasks = new ArrayList<String>();
         while (running) {
             String input = scanner.nextLine();
             switch (input) {
+                case "list":
+                    System.out.println(line);
+                    for (int i = 0; i < tasks.size(); i++) {
+                        int current = i + 1;
+                        System.out.println(current + ". " + tasks.get(i));
+                    }
+                    System.out.println(line);
+                    break;
                 case "bye":
                     System.out.println(line);
                     System.out.println("bye ;;w;; see you again soon :3");
@@ -24,7 +34,8 @@ public class Duke {
                     break;
                 default:
                     System.out.println(line);
-                    System.out.println(input);
+                    tasks.add(input);
+                    System.out.println("added: " + input);
                     System.out.println(line);
             }
         }
