bece335bb4792a985231d19d3fc9a0868024f49b
Shi Jiaao <e0372500@u.nus.edu>
2023-01-19 16:44:21 +0800

    add Task.java class

---
+++ src/main/java/Task.java
@@ -0,0 +1,14 @@
+// skeleton code for this class is taken from cs2103 website
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+}
