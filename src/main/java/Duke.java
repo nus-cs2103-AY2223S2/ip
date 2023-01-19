dc3d9e624313c32b6a8542ce49c15758f73e336e
DESKTOP-GGI1K1E\kelvi <e0726844@u.nus.edu>
2023-01-19 13:48:22 +0800

    Level 1: Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,30 @@
+
+import java.util.Scanner;
+
 public class Duke {
+
+    public static void Level1() {
+        System.out.println( "____________________________________________________________\n"
+            + " Hello! I'm Duke\n"
+            + " What can I do for you?\n"
+            + "____________________________________________________________\n");
+
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            String command = sc.nextLine();
+            if (command.equals("bye")) {
+                break;
+            }
+            System.out.println( "____________________________________________________________\n"
+                    + command
+                    + "\n____________________________________________________________\n");
+        }
+
+        System.out.println( "____________________________________________________________\n"
+                + " Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________\n");
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +32,6 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        Level1();
     }
 }
