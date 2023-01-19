dcc623b030681632c3c5e0509797b4ecc994ce74
Karen-Lam <karenycarrot@gmail.com>
2023-01-19 16:38:16 +0800

    add Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,6 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +9,15 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String word = scanner.nextLine();
+            if (word.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+            System.out.println(word);
+        }
     }
 }
