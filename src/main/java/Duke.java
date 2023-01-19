b6048874d1af4ce7d2c4af090651c5bad4d23d58
Wesley Teo <wesley.tyx@gmail.com>
2023-01-19 15:20:42 +0800

    Added list functionality and adding items to list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,19 +1,33 @@
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Duke {
-    static Scanner sc = new Scanner(System.in);
+    static LinkedList<String> todo = new LinkedList<>();
+    static String logo = " ____        _        \n"
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|";
+    static String line = "---------------------------------";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|";
-        System.out.println("Hello from\n" + logo + "  managed by Wesley Teo.\n\nWhat can I do for you?\n---------------------------------");
-        String input = sc.next();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello from\n" + logo + "  managed by Wesley Teo.\n\nWhat can I do for you?\n" + line);
+        String input = sc.nextLine();
+        System.out.println(line);
         while (!input.equals("bye")) {
-            System.out.println("---------------------------------\n    " + input + "\n---------------------------------");
-            input = sc.next();
+            if (input.equals("list")) {
+                for (String task : todo) {
+                    System.out.println(todo.indexOf(task) + 1 + ". " + task);
+                }
+            } else {
+                todo.add(input);
+                System.out.println("Added: " + input);
+            }
+            System.out.println(line);
+            input = sc.nextLine();
+            System.out.println(line);
         }
-        System.out.println("---------------------------------\nBye. Hope to see you again soon!");
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
