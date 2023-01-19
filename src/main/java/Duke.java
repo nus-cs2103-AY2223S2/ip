88c52549db2a5fd86fe5f720732af5dedd4d3c23
roultitude <51879825+roultitude@users.noreply.github.com>
2023-01-19 18:08:31 +0800

    Added functionality for Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,19 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
+
     public static void main(String[] args) {
+        List<String> list = new ArrayList<String>();
         Scanner input = new Scanner(System.in);
         System.out.println("Hello! I'm DonkeyChat!\nWhat can I do for you?");
         boolean isRunning = true;
-        while(isRunning){
+        while (isRunning) {
             String currCommand = input.nextLine();
-            switch(currCommand){
+            switch (currCommand) {
                 case "bye":
                     System.out.println("Adios!");
                     isRunning = false;
                     break;
+                case "list":
+                    for (int i = 0; i < list.size(); i++) {
+                        System.out.println(i + 1 + ". " + list.get(i));
+                    }
+                    break;
                 default:
-                    System.out.println(currCommand);
+                    System.out.println("added: " + currCommand);
+                    list.add(currCommand);
                     break;
             }
         }
