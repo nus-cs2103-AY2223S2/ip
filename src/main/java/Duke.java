8f980a1a053908d552a6192c8df1d19b7cbe44fc
Chandrasekaran Akash <enigmatrix2000@gmail.com>
2023-01-19 20:01:11 +0800

    feat: add, list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,11 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
         var reader = new Scanner(System.in);
         var writer = System.out;
+        var items = new ArrayList<String>();
 
         writer.println("Hello, I'm Kude!");
 
@@ -11,11 +13,17 @@ public class Duke {
             writer.print("> ");
             var cmd = reader.nextLine();
 
-            if (cmd.equals("bye")) {
+            if (cmd.equals("list")) {
+                for (int i = 0; i < items.size(); i++) {
+                    writer.println("< " + (i + 1) + ". " + items.get(i));
+                }
+            }
+            else if (cmd.equals("bye")) {
                 writer.println("< Bye!!");
                 return;
             } else {
-                writer.println("< " + cmd);
+                items.add(cmd);
+                writer.println("< added: " + cmd);
             }
         }
     }
