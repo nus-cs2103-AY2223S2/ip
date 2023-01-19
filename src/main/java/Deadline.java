cb411fc632a1f2c2c6869ecdef11d88ab01465d6
EksdeeeX <Bryantanzhongkang@gmail.com>
2023-01-20 00:49:37 +0800

    no message

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,13 @@
+public class Deadline extends Task{
+    private String deadline;
+
+    public Deadline(String task, String deadline) {
+        super(task);
+        this.deadline = deadline;
+    }
+
+    @Override 
+    public String toString() {
+        return "[D] " + super.toString() + " (by: " + this.deadline + ")";
+    }
+}
--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,22 +1,115 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.lang.StringBuilder;
 
 public class Duke {
     private static final String intro = "Hi! I'm Duke! :)\nHow may I help?";
     private static final String outro = "Goodbye!";
     private static ArrayList<Task> list = new ArrayList<>();
 
-    private static void addToList(Task t) {
-        list.add(t);
-        System.out.println("added " + t.getName() + "!");
-    }
-
-    private static void emptyErr() {
-        System.out.print("Duke: ");
-        System.out.println("List empty!");
+    private static void reset() {
         System.out.println();
         System.out.print("User: ");
     }
+    private static void addToList(Task t) {
+        list.add(t);
+        System.out.print("Duke: ");
+        System.out.println("added " + t.getName() + "!");
+        System.out.println("Now you have " + list.size() + " tasks in the list.");
+    }
+
+    private static void emptyErr() {
+        System.out.println("Duke: Error!! List empty! Try again.");
+        reset();
+    }
+
+    private static void viewList() {
+        System.out.println("Here's your list of tasks:");
+        System.out.println();
+        for(int i  = 0; i < list.size(); i++) {
+        System.out.println(i+1 + ". " + list.get(i));
+        }
+    }
+
+    private static void markTask(int num) {
+        if(num >= list.size()) {
+            System.out.println("Duke: Task no." + (num+1) + " not found. Try again.");
+        } else {
+            list.get(num).mark();
+            System.out.println("Duke: Nice It's marked!");
+            System.out.println(list.get(num));
+        }
+    }
+
+    private static void unmarkTask(int num) {
+        if(num >= list.size()) {
+            System.out.println("Duke: Task no." + (num+1) + " not found. Try again.");
+        } else {
+            list.get(num).unmark();
+            System.out.println("Duke: Ok! It's unmarked!");
+            System.out.println(list.get(num));
+        }
+    }
+
+    private static ToDo makeToDo(String[] task) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1; i < task.length; i++) {
+            sb.append(task[i] + " ");
+        }
+        return new ToDo(sb.toString());
+    }
+
+    private static Event makeEvent(String[] task) {
+        StringBuilder start = new StringBuilder();
+        StringBuilder end = new StringBuilder();
+        StringBuilder desc = new StringBuilder();
+        boolean isDescripton = true;
+        boolean first = false;
+        boolean second = false;
+
+        for(int i = 1; i < task.length; i++) {
+            if(task[i].equals("/from")) {
+                first = true;
+                isDescripton = false;
+                continue;
+            }
+            if(task[i].equals("/to")) {
+                second = true;
+                first = false;
+                continue;
+            }
+            if(first) {
+                start.append(task[i] + " ");
+            }
+            if(second) {
+                end.append(task[i] + " ");
+            }
+            if(isDescripton) {
+                desc.append(task[i] + " ");
+            }
+        }
+        return new Event(desc.toString(), start.toString(), end.toString());
+    }
+
+    private static Deadline makeDeadline(String[] task) {
+        StringBuilder desc = new StringBuilder();
+        StringBuilder by = new StringBuilder();
+        boolean isDesc = true;
+
+        for(int i = 1; i < task.length; i++) {
+            if(task[i].equals("/by")) {
+                isDesc = false;
+                continue;
+            }
+            if(isDesc) {
+                desc.append(task[i] + " ");
+            } else {
+                by.append(task[i] + " ");
+            }
+        }
+        return new Deadline(desc.toString(), by.toString());
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println(intro);
@@ -30,41 +123,40 @@ public class Duke {
                 if(list.isEmpty()) {
                     emptyErr();
                 } else {
-                    System.out.println("Here's your list of tasks!");
-                    System.out.println();
-                    for(int i  = 0; i < list.size(); i++) {
-                        System.out.println(i+1 + ". " + list.get(i));
-                    }
-                    System.out.println();
-                    System.out.print("User: ");
+                    viewList();
+                    reset();
                 }
             } else if (input.equals("mark")) {
                 if(list.isEmpty()) {
                     emptyErr();
                 } else {
                     int num = (Integer.parseInt(raw[1]) - 1);
-                    list.get(num).mark();
-                    System.out.println("Nice! It's marked!");
-                    System.out.println(list.get(num));
-                    System.out.println();
-                    System.out.print("User: ");
+                    markTask(num);
+                    reset();
                 }
             } else if(input.equals("unmark")) {
                 if(list.isEmpty()) {
                     emptyErr();
                 } else {
                     int num = (Integer.parseInt(raw[1]) - 1);
-                    list.get(num).unmark();
-                    System.out.println("Ok! It's unmarked!");
-                    System.out.println(list.get(num));
-                    System.out.println();
-                    System.out.print("User: ");
+                    unmarkTask(num);
+                    reset();
                 }
+            } else if (input.equals("todo")) {
+                ToDo temp = makeToDo(raw);
+                addToList(temp);
+                reset();
+            } else if (input.equals("event")) {
+                Event temp = makeEvent(raw);
+                addToList(temp);
+                reset();
+            } else if (input.equals("deadline")) {
+                Deadline temp = makeDeadline(raw);
+                addToList(temp);
+                reset();
             } else {
-                System.out.print("Duke: ");
-                addToList(new Task(input));
-                System.out.println();
-                System.out.print("User: ");
+                System.out.println("Duke; Sorry I don't recognise that command :( Please try again.");
+                reset();
             }
             raw = sc.nextLine().split(" ");
             input = raw[0];
