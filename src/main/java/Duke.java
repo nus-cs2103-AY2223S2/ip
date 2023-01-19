e9480919d4fb3cd5733948fde26875704f14de24
bryanongjxairetec <bryanong@airetec.sg>
2023-01-19 19:02:36 +0800

    second commit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,22 +1,26 @@
-import java.util.Locale;
-import java.util.Objects;
-import java.util.Scanner;
+import java.util.*;
 
 public class Duke {
     public static void main(String[] args) {
-
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Hello! I'm Duke\n");
         System.out.println("What can I do for you?\n");
 
+        List<String> lst = new ArrayList<>();
+
         while (true) {
             String a = sc.nextLine().toLowerCase();
-            if (Objects.equals(a, "bye".toLowerCase())) {
+            if (Objects.equals(a, "bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 return;
+            } else if (Objects.equals(a, "list")) {
+                for (int i = 1; i <= lst.size(); i ++){
+                    System.out.println(i + ". " + lst.get(i - 1));
+                }
             } else {
-                System.out.println(a);
+                lst.add(a);
+                System.out.println("added: " + a);
             }
 
         }
