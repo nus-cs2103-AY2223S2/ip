c96703d82c694557a8a52818fa1e7fb970b5f2bf
GlennOngJunJie <glennongjj@gmail.com>
2023-01-19 21:17:24 +0800

    Level 5 done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Duke {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws DukeException {
 //        String logo = " ____        _        \n"
 //                + "|  _ \\ _   _| | _____ \n"
 //                + "| | | | | | | |/ / _ \\\n"
@@ -28,21 +28,36 @@ public class Duke {
                   storage.get(position - 1).markAsNotDone();
                   System.out.println("OK, I've marked this task as not done yet:\n" + "[" + storage.get(position - 1).getStatusIcon() + "] " + storage.get(position - 1).description);
               } else if (userInput.contains("todo") && userInput.substring(0,4).equals("todo")) {
-                  Todo todoTask = new Todo(userInput.substring(5));
-                  storage.add(todoTask);
-                  System.out.println("Got it. I've added this task:\n  " + todoTask.toString() + "\nNow you have " + storage.size() + " tasks in the list");
+                  if (userInput.substring(5).equals("")){
+                      throw new DukeException("☹ OOPS!!! The description of a todo cannot be empty.");
+                  } else {
+                      Todo todoTask = new Todo(userInput.substring(5));
+                      storage.add(todoTask);
+                      System.out.println("Got it. I've added this task:\n  " + todoTask.toString() + "\nNow you have " + storage.size() + " tasks in the list");
+                  }
               } else if (userInput.contains("deadline") && userInput.substring(0,8).equals("deadline")) {
                   int position = userInput.indexOf("/by ");
-                  Deadline deadlineTask = new Deadline(userInput.substring(9, position), userInput.substring(position + 4));
-                  storage.add(deadlineTask);
-                  System.out.println("Got it. I've added this task:\n  " + deadlineTask.toString() + "\nNow you have " + storage.size() + " tasks in the list");
+                  if (userInput.substring(9).equals("")){
+                      throw new DukeException("☹ OOPS!!! The description of a deadline cannot be empty.");
+                  } else {
+                      Deadline deadlineTask = new Deadline(userInput.substring(9, position), userInput.substring(position + 4));
+                      storage.add(deadlineTask);
+                      System.out.println("Got it. I've added this task:\n  " + deadlineTask.toString() + "\nNow you have " + storage.size() + " tasks in the list");
+                  }
               } else if (userInput.contains("event") && userInput.substring(0,5).equals("event")) {
                   int position1 = userInput.indexOf("/from ");
                   int position2 = userInput.indexOf("/to ");
-                  Event deadlineTask = new Event(userInput.substring(6, position1), userInput.substring(position1 + 6, position2), userInput.substring(position2 + 4));
-                  storage.add(deadlineTask);
-                  System.out.println("Got it. I've added this task:\n  " + deadlineTask.toString() + "\nNow you have " + storage.size() + " tasks in the list");
-              } else {
+                  if (userInput.substring(6).equals("")){
+                      throw new DukeException("☹ OOPS!!! The description of a event cannot be empty.");
+                  } else {
+                      Event deadlineTask = new Event(userInput.substring(6, position1), userInput.substring(position1 + 6, position2), userInput.substring(position2 + 4));
+                      storage.add(deadlineTask);
+                      System.out.println("Got it. I've added this task:\n  " + deadlineTask.toString() + "\nNow you have " + storage.size() + " tasks in the list");
+                  }
+              } else if (userInput.contains(" ") && userInput.substring(userInput.indexOf(" ") + 1).equals("")){
+                  throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+              }
+              else {
                   Task userInputTask = new Task(userInput);
                   storage.add(userInputTask);
                   System.out.println("added: " + userInput);
