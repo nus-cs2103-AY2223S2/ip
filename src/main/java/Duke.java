45009ec6d28f684851876cb8e031c19125d6e20e
peaol <pearltwe23@gmail.com>
2023-01-19 20:04:17 +0800

    Level-3 implementation

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,20 +8,64 @@ public class Duke {
 
         Scanner myObj = new Scanner(System.in);
         String input;
-        List<String> list = new ArrayList<String>();
+        List<Task> list = new ArrayList<Task>();
         input = myObj.nextLine();
+        char m = 'm';
+        char a = 'a';
+        char r = 'r';
+        char k = 'k';
+        char u = 'u';
+        char n = 'n';
 
         while (!input.equals("bye")) {
+            //print List
             if (input.equals("list")) {
                 int counter = 1;
-                for (String task : list) {
-                    System.out.println(counter + ". " + task);
+                for (Task task : list) {
+                    System.out.println(counter + ". " + "[" + task.getStatusIcon() + "] " + task.description);
                     counter++;
                 }
                 input = myObj.nextLine();
+
+                //mark a Task
+            } else if (input.charAt(0) == m && input.charAt(1) == a && input.charAt(2) == r && input.charAt(3) == k) {
+                //getting the index
+                String indexString = input.substring(5);
+                //converting the index from String to Int
+                int indexInt = Integer.parseInt(indexString) - 1;
+
+                //in case the Task index to mark exceeds current number of Tasks or neg number
+                if (indexInt + 1 > list.size() || indexInt < 0) {
+                    System.out.println("Task does not exist");
+                } else {
+                    Task t = list.get(indexInt);
+                    t.markDone();
+                    System.out.println("Nice! I've marked this task as done:\n" + "[X] " + t.description);
+                }
+                input = myObj.nextLine();
+
+                //unmark a Task
+            } else if (input.charAt(0) == u && input.charAt(1) == n && input.charAt(2) == m && input.charAt(3) == a
+                    && input.charAt(4) == r && input.charAt(5) == k) {
+                String indexString = input.substring(7);
+                //converting the index from String to Int
+                int indexInt = Integer.parseInt(indexString) - 1;
+
+                //in case the Task index to mark exceeds current number of Tasks or neg number
+                if (indexInt + 1 > list.size() || indexInt < 0) {
+                    System.out.println("Task does not exist");
+                } else {
+                    Task t = list.get(indexInt);
+                    t.markUndone();
+                    System.out.println("OK, I've marked this task as not done yet:\n" + "[ ] " + t.description);
+                }
+                input = myObj.nextLine();
+
+                //normal case: create Task and add Task to list
             } else {
-                list.add(input);
-                System.out.println("added: " + input);
+                Task t  = new Task(input);
+                list.add(t);
+                System.out.println("added: " + t.description);
                 input = myObj.nextLine();
             }
         }
