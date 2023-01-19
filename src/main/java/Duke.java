b92037f53dd1ffea4e1ad3be51c6bb6b28caad4e
pkpaing <pkpaing@gmail.com>
2023-01-19 20:28:32 +0800

    Level 5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,52 +14,11 @@ public class Duke {
 
         // loop while user has not entered 'bye' command
         while (!s.equals("bye")) {
-
-            // user enters list command
-            if (s.equals("list")) {
-                if (tasks[0] == null) {
-                    System.out.println("    You have no tasks");
-                } else {
-                    for (int i = 0; i < taskCounter; i++) {
-                        Task task = tasks[i];
-                        System.out.println("    " + (i + 1) + ". " + task.toString());
-                    }
-                }
-
-            // user enters mark or unmark command
-            } else if (s.contains("mark") || s.contains("unmark")) {
-                int taskNumber = Integer.parseInt(s.substring(s.length() - 1)) - 1;
-                tasks[taskNumber].toggleMarked();
-                if (s.contains("unmark")) {
-                    System.out.println("    OK, I've marked this task as not done yet:");
-                } else {
-                    System.out.println("    Nice! I've marked this task as done:");
-                }
-                System.out.println("  " + tasks[taskNumber].toString());
-
-            // user enters a new task
-            }  else if (s.contains("todo")) {
-                Task newTask = new Todo(s);
-                tasks[taskCounter] = newTask;
-                taskCounter++;
-                System.out.println("    added: " + newTask);
-
-            } else if (s.contains("deadline")) {
-                String by = s.substring(s.indexOf("/") + 4);
-                Task newTask = new Deadline(s.substring(9, s.indexOf("/") - 1), by);
-                tasks[taskCounter] = newTask;
-                taskCounter++;
-                System.out.println("    added: " + newTask);
-
-            } else if (s.contains("event")) {
-                String from = s.substring(s.indexOf("/") + 6, s.lastIndexOf("/"));
-                String to = s.substring(s.lastIndexOf("/") + 4);
-                Task newTask = new Event(s.substring(6, s.indexOf("/") - 1), from, to);
-                tasks[taskCounter] = newTask;
-                taskCounter++;
-                System.out.println("    added: " + newTask);
-            } else {
-                System.out.println("OOPS!!! I'm sorry, but I don't know what that means :-(");
+            try{
+                // method handleCommand handles current command, returns updated taskCounter variable
+                taskCounter = handleCommand(s, tasks, taskCounter);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
             }
 
             // take in next command
@@ -69,6 +28,74 @@ public class Duke {
         // exit
         System.out.println("    Bye. Hope to see you soon!");
     }
+
+    // handleCommand takes in command String s, current tasks, and current number of tasks
+    // updates task array and returns taskCounter
+    public static int handleCommand(String s, Task[] tasks, int taskCounter){
+        // user enters list command
+        if (s.contains("list")) {
+            if (tasks[0] == null) {
+                System.out.println("    You have no tasks");
+            } else {
+                for (int i = 0; i < taskCounter; i++) {
+                    Task task = tasks[i];
+                    System.out.println("    " + (i + 1) + ". " + task.toString());
+                }
+            }
+
+            // user enters mark or unmark command
+        } else if (s.contains("mark") || s.contains("unmark")) {
+            int taskNumber = Integer.parseInt(s.substring(s.length() - 1)) - 1;
+            tasks[taskNumber].toggleMarked();
+            if (s.contains("unmark")) {
+                System.out.println("    OK, I've marked this task as not done yet:");
+            } else {
+                System.out.println("    Nice! I've marked this task as done:");
+            }
+            System.out.println("  " + tasks[taskNumber].toString());
+
+            // user enters a new task
+        }  else if (s.contains("todo")) {
+            if (s.substring(4).isBlank()) {
+                System.out.println("    OOPS!!! The description of a todo cannot be empty.");
+            } else {
+                Task newTask = new Todo(s.substring(5));
+                tasks[taskCounter] = newTask;
+                System.out.println("    added: " + newTask);
+                return taskCounter + 1;
+            }
+
+        } else if (s.contains("deadline")) {
+            if (s.substring(8).isBlank()) {
+                System.out.println("    OOPS!!! The description of a deadline cannot be empty.");
+            } else {
+                String by = s.substring(s.indexOf("/") + 4);
+                Task newTask = new Deadline(s.substring(9, s.indexOf("/") - 1), by);
+                tasks[taskCounter] = newTask;
+                System.out.println("    added: " + newTask);
+                return taskCounter + 1;
+            }
+
+        } else if (s.contains("event")) {
+            if (s.substring(5).isBlank()) {
+                System.out.println("    OOPS!!! The description of a event cannot be empty.");
+            } else {
+                String from = s.substring(s.indexOf("/") + 6, s.lastIndexOf("/"));
+                String to = s.substring(s.lastIndexOf("/") + 4);
+                Task newTask = new Event(s.substring(6, s.indexOf("/") - 1), from, to);
+                tasks[taskCounter] = newTask;
+                System.out.println("    added: " + newTask);
+                return taskCounter + 1;
+            }
+
+        } else {
+            //throw new DukeException("OOPS!!! I'm sorry, but I don't know what that means :-(");
+            System.out.println("    OOPS!!! I'm sorry, but I don't know what that means :-(");
+            return taskCounter;
+        }
+        return taskCounter;
+    }
+
 }
 
 // custom Task class to store individual tasks that the user enters
@@ -137,4 +164,11 @@ public class Event extends Task{
         return "[E]" + super.toString() + " (from: " + from + "to: " + to + ")";
     }
 
+}
+
+public class DukeException extends Exception {
+    public DukeException(String message) {
+
+        super(message);
+    }
 }
