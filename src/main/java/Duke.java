f4eab402153563deddcb8c77b54adff67096be0b
Ryan Jansen <rjgiga444@gmail.com>
2023-01-19 17:14:43 +0800

    Implemented Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
+
+    private static ArrayList<String> tasks = new ArrayList<>(100);
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String logo = " ____        _        \n"
@@ -17,12 +20,31 @@ public class Duke {
 
         String input = sc.nextLine();
         while(!input.equals("bye")) {
-            printMsg(input);
+            if (input.equals("list")) {
+                printTasks();
+            } else {
+                addTask(input);
+            }
             input = sc.nextLine();
         }
 
         printMsg("Bye. Hope to see you again soon!");
 
+    }
+
+    private static void addTask(String task) {
+        printMsg("added: " + task);
+        tasks.add((task));
+    }
+
+    private static void printTasks() {
+        int count = 1;
+        System.out.println("    ____________________________________________________________");
+        for (String task : tasks) {
+            System.out.println("     " + count + ". " + task);
+            count++;
+        }
+        System.out.println("    ____________________________________________________________");
     }
 
     private static void printMsg(String msg) {
