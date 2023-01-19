de5b7bb8881e63ab596e9b6d8b16522b2f51a6b8
ChickenChiang <96032660+ChickenChiang@users.noreply.github.com>
2023-01-19 23:15:11 +0800

    Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,17 +13,37 @@ public class Duke {
                 + "| |___ | |___ | |__| | | |___  |    \\\n"
                 + "|_____||_____||______| |_____| |_| \\_\\\n";
         System.out.println(logo);
-        System.out.println("Howdy! I'm Cluck!\n" +
-                "What can I cluck-a-doodle-do for you?\n");
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    Howdy! I'm Cluck!\n" +
+                "    What can I cluck-a-doodle-do for you?\n");
+        System.out.println("    ____________________________________________________________");
 
+        List<String> toDoList = new LinkedList<>();
         Scanner sc = new Scanner(System.in);
         while (true) {
+
             String input = sc.nextLine();
             if (input.equals("bye")) {
-                System.out.println("Buh-cluck, see ya!");
+                System.out.println("    ____________________________________________________________");
+                System.out.println("    Buh-cluck, see ya!");
+                System.out.println("    ____________________________________________________________");
+
                 break;
             }
-            System.out.println(input);
+            else if (input.equals("list")) {
+                System.out.println("    ____________________________________________________________");
+                for (int i = 0; i < toDoList.size(); i++) {
+
+                    System.out.println("    " + (i+1) + ": " + toDoList.get(i));
+                }
+                System.out.println("    ____________________________________________________________");
+            } else {
+                toDoList.add(input);
+                System.out.println("    ____________________________________________________________");
+                System.out.println("    added: " + input);
+                System.out.println("    ____________________________________________________________");
+
+            }
         }
     }
 }
