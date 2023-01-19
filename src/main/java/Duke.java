089557a76c5e3a3db00c6702c1051b6f244c5b39
zm-l <97417244+zm-l@users.noreply.github.com>
2023-01-19 19:29:24 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 public class Duke {
     public static void main(String[] args)
@@ -11,25 +13,47 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo + "\nWhat can I do for you?");
 
-        Scanner myObj = new Scanner(System.in);
-        String userInput = myObj.nextLine();
-        ArrayList<String> userInputs = new ArrayList<String>();
+        Scanner input = new Scanner(System.in);
+        String userInput = input.nextLine();
+        ArrayList<Task> userTasks = new ArrayList<Task>();
         while (!userInput.equals("bye"))
         {
+            Pattern mark = Pattern.compile("mark [0-9]+");
+            Pattern unmark = Pattern.compile("unmark [0-9]+");
+            Matcher matchMark = mark.matcher(userInput);
+            Matcher matchUnmark = unmark.matcher(userInput);
+
             if (userInput.equals("list"))
             {
-                for (int i = 1; i <= userInputs.size(); i++)
+                System.out.println("Here are the tasks in your list:");
+                for (int i = 1; i <= userTasks.size(); i++)
                 {
-                    System.out.println(i + ". " + userInputs.get(i - 1));
+                    System.out.println(i + ".[" + userTasks.get(i - 1).getStatusIcon() + "] " + userTasks.get(i - 1));
                 }
+            }
+            else if (matchMark.matches())
+            {
+                System.out.println("Nice! I've marked this task as done:");
+                int idx = Integer.parseInt(userInput.split(" ")[1]);
+                userTasks.get(idx - 1).markTask();
+                System.out.println("    " + idx + ".[" + userTasks.get(idx - 1).getStatusIcon() + "] " + userTasks.get(idx - 1));
+            }
+            else if (matchUnmark.matches())
+            {
+                System.out.println("OK, I've marked this task as not done yet:");
+                int idx = Integer.parseInt(userInput.split(" ")[1]);
+                userTasks.get(idx - 1).unmarkTask();
+                System.out.println("    " + idx + ".[" + userTasks.get(idx - 1).getStatusIcon() + "] " + userTasks.get(idx - 1));
             }
             else
             {
-                userInputs.add(userInput);
+                Task userTask = new Task(userInput);
+                userTasks.add(userTask);
                 System.out.println("added: " + userInput);
             }
-            userInput = myObj.nextLine();
+            userInput = input.nextLine();
         }
         System.out.println("Bye. Hope to see you again soon!");
+        input.close();
     }
 }
