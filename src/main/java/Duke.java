d13d037d5acb2c573e13c35941b33c6615765027
DESKTOP-GGI1K1E\kelvi <e0726844@u.nus.edu>
2023-01-19 14:40:27 +0800

    Level 3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -15,25 +15,43 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println(LINE + " Hello! I'm Duke\n What can I do for you?\n" + LINE);
 
-        List<String> tasks = new ArrayList();
+        int n = 0;
+        String[] task;
+        List<String[]> tasks = new ArrayList<String[]>();
+
         boolean flag_continue = true;
         while (flag_continue) {
             Scanner sc = new Scanner(System.in);
-            String command = sc.nextLine();
-            switch(command) {
+            String input = sc.next();
+            switch(input) {
                 case "bye":
                     flag_continue = false;
                     break;
                 case "list":
                     String output = "";
                     for(int i = 0; i < tasks.size(); i++) {
-                        output += Integer.toString(i + 1) + ". " + tasks.get(i) + "\n";
+                        output += Integer.toString(i + 1) + ".[" + tasks.get(i)[1] + "] " + tasks.get(i)[0] + "\n";
                     }
                     System.out.println(LINE + output + LINE);
                     break;
+                case "mark":
+                    n = sc.nextInt() - 1;
+                    task = tasks.get(n);
+                    task[1] = "X";
+                    tasks.set(n, task);
+                    System.out.println(LINE + "Nice! I've marked this task as done:\n  [X] " + task[0] + "\n" + LINE);
+                    break;
+                case "unmark":
+                    n = sc.nextInt() - 1;
+                    task = tasks.get(n);
+                    task[1] = " ";
+                    tasks.set(n, task);
+                    System.out.println(LINE + "OK, I've marked this task as not done yet:\n  [ ] " + task[0] + "\n" + LINE);
+                    break;
                 default:
-                    tasks.add(command);
-                    System.out.println(LINE + command + "\n" + LINE);
+                    String[] cmd = {input, " "};
+                    tasks.add(cmd);
+                    System.out.println(LINE + input + "\n" + LINE);
                     break;
             }
         }
