8db722f70bed17ff1a776768b0956df98a0f252a
TimothyLawSongEn <e0725564@u.nus.edu>
2023-01-19 16:18:08 +0800

    Added support for tracking three types of tasks.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -17,19 +17,30 @@ public class Duke {
                 break;
             } else if (str.equals("list")) {
                 for (int i = 0 ; i < nextIndex ; i++) {
-                    System.out.println((i+1) + ". [" + arr[i].getStatusIcon() + "] " + arr[i]);
+                    System.out.println((i+1) + ". " + arr[i]);
                 }
             } else if (firstWord.equals("mark")) {
                 int num = Integer.parseInt(s[1]) - 1;
                 arr[num].markAsDone();
-                System.out.println("Nice! I've marked this task as done:\n[" + arr[num].getStatusIcon() + "] " + arr[num]);
+                System.out.println("Nice! I've marked this task as done:\n" + arr[num]);
             } else if (firstWord.equals("unmark")) {
                 int num = Integer.parseInt(s[1]) - 1;
                 arr[num].unmark();
-                System.out.println("OK, I've marked this task as not done yet:\n[" + arr[num].getStatusIcon() + "] " + arr[num]);
+                System.out.println("OK, I've marked this task as not done yet:\n" + arr[num]);
             } else {
-                System.out.println("added: " + str);
-                arr[nextIndex++] = new Task(str);
+                if (firstWord.equals("todo")) {
+                    arr[nextIndex++] = new ToDo(s[1]);
+                }
+                else if (firstWord.equals("deadline")) {
+                    String st[] = s[1].split(" /by ", 2);
+                    arr[nextIndex++] = new Deadline(st[0], st[1]);
+                } else if (firstWord.equals("event")) {
+                    String st[] = s[1].split(" /from ", 2);
+                    String stt[] = st[1].split(" /to ", 2);
+                    arr[nextIndex++] = new Event(st[0], stt[0], stt[1]);
+                }
+                System.out.println("Got it. I've added this task:\n" + arr[nextIndex-1]
+                    + "\nNow you have " + nextIndex + " tasks in the list.");
             }
         }
         
