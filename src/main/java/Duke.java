27431b619427f4a943fdac466728deb66cba9e18
winsonheng <winsonh5147@gmail.com>
2023-01-19 17:25:07 +0800

    Completed Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,12 @@
+package main.java;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     private static final String FULL_LINE = "_______________________________________________\n";
-    private static ArrayList<String> inputList = new ArrayList<>();
+    private static ArrayList<Task> taskList = new ArrayList<>();
+
     public static void main(String[] args) {
         String welcomeString = "Hello I'm Duke! Type anything and I'll echo it.";
         String byeString = "Bye. Hope to see you again soon!";
@@ -15,20 +18,31 @@ public class Duke {
 
         while (true) {
             input = sc.nextLine();
+            String output = "";
             if (input.equals("bye")) {
                 printFormattedOutput(byeString);
                 break;
             } else if (input.equals("list")) {
-                String output = "";
-                for (int i = 0; i < inputList.size(); ++i) {
-                    output += (i + 1) + ". " + inputList.get(i) + "\n";
+                output = "Here are the tasks in your list:";
+                for (int i = 0; i < taskList.size(); ++i) {
+                    output += String.format("\n%d.%s", i + 1, taskList.get(i).toString());
                 }
-                printFormattedOutput(output);
+            } else if (input.matches("mark \\d+")) {
+                int index = Integer.parseInt(input.replace("mark ", "")) - 1;
+                Task task = taskList.get(index);
+                task.markAsDone();
+                output = "Nice! I've marked this task as done:\n" + task.toString();
+            } else if (input.matches("unmark \\d+")) {
+                int index = Integer.parseInt(input.replace("unmark ", "")) - 1;
+                Task task = taskList.get(index);
+                task.markAsUndone();
+                output = "OK, I've marked this task as not done yet:\n" + task.toString();
             } else {
-                inputList.add(input);
-                String output = "Added: " + input;
-                printFormattedOutput(output);
+                Task task = new Task(input);
+                taskList.add(task);
+                output = "Added: " + input;
             }
+            printFormattedOutput(output);
         }
     }
 
---
+++ src/main/java/Task.java
@@ -0,0 +1,28 @@
+package main.java;
+
+public class Task {
+    private String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void markAsUndone() {
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (this.isDone ? "X" : " ");
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s] %s", this.getStatusIcon(), this.description);
+    }
+}
