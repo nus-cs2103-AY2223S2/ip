b3ce7978bd0d22283b9c0922c97e7cce3f971cff
Shi Jiaao <e0372500@u.nus.edu>
2023-01-19 15:30:44 +0800

    change Duke.java to match Level-1 requirements

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +6,20 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        // System.out.println("Hello from\n" + logo);
+
+        /* project starts here */
+        System.out.println("Hello! I'm Duke\n" +
+                "What can I do for you?\n");
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String command = sc.next();
+            System.out.println(command);
+            if (command.equals("bye")) {
+                break;
+            }
+        }
+        System.out.println("Bye. Hope to see you again soon!");
+        sc.close();
     }
 }
