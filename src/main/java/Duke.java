6b533db5a07129c967bfd3efd2887c432f51b86d
GlenLim <glenlimyf@gmail.com>
2023-01-19 13:56:22 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,29 @@
+import java.util.*;
 public class Duke {
+    private static String[] tasks = new String[100];
+
+    public void awaitInstruction(){
+        System.out.println("Hello I am Duke\nWhat can I do for you?");
+        Scanner sc = new Scanner(System.in);
+        String answer = sc.next();
+        if (answer.equals("bye")){
+            System.out.println("Goodbye");
+        } else{
+            System.out.println(answer);
+        }
+
+    }
+
+    
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        Duke duke = new Duke();
+        duke.awaitInstruction();
     }
+
 }
