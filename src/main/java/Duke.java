da95a38dacd68fab97247bb71be78ddabbc7c466
cshuwen <chenshuwen0210@gmail.com>
2023-01-19 20:41:47 +0800

    Level6 Delete

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -15,19 +15,23 @@ public class Duke {
             Integer len = s.length();
             try {
                 if (s.equals("list")) {
+                    if (list.size() == 0){
+                        String err_msg = "You have not upload any task yet";
+                        throw new DukeException(err_msg);
+                    }
                     System.out.println("Here are the tasks in your list:");
                     i = 1;
                     for (Task task : list) {
                         System.out.println(i + ". " + task.toString());
                         i++;
                     }
-                } else if (len >= 6 && s.substring(0, 4).equals("mark")) {
+                } else if (len >= 8 && s.substring(0, 6).equals("delete")) {
                     try{
-                        Integer num = Integer.parseInt(s.substring(5));
+                        Integer num = Integer.parseInt(s.substring(7));
                         Task curr_task = list.get(num - 1);
-                        System.out.println("Nice! I've marked this task as done");
-                        curr_task.markAsDone();
-                        System.out.println(curr_task.getStatusIcon() + " " + curr_task.getDes());
+                        System.out.println("Noted. I've removed this task: \n  " + curr_task.toString());
+                        list.remove(curr_task);
+                        System.out.println("Now you have " + (list.size()) + " tasks in the list");
                     } catch (IndexOutOfBoundsException err1){
                         String err_msg = "☹ OOPS!!! Please the Task number that you have keyed in is invalid.";
                         System.out.println(err_msg);
@@ -35,7 +39,21 @@ public class Duke {
                         String err_msg = "☹ OOPS!!! Please key in a valid Number.";
                         System.out.println(err_msg);
                     }
-                } else if (len >= 8 && s.substring(0, 6).equals("unmark")) {
+                } else if (len >= 6 && s.substring(0, 4).equals("mark")) {
+                    try {
+                        Integer num = Integer.parseInt(s.substring(5));
+                        Task curr_task = list.get(num - 1);
+                        System.out.println("Nice! I've marked this task as done");
+                        curr_task.markAsDone();
+                        System.out.println(curr_task.getStatusIcon() + " " + curr_task.getDes());
+                    } catch (IndexOutOfBoundsException err1) {
+                        String err_msg = "☹ OOPS!!! Please the Task number that you have keyed in is invalid.";
+                        System.out.println(err_msg);
+                    } catch (NumberFormatException err2) {
+                        String err_msg = "☹ OOPS!!! Please key in a valid Number.";
+                        System.out.println(err_msg);
+                    }
+                }else if (len >= 8 && s.substring(0, 6).equals("unmark")) {
                     try{
                         Integer num = Integer.parseInt(s.substring(7));
                         Task curr_task = list.get(num - 1);
@@ -49,7 +67,7 @@ public class Duke {
                         String err_msg = "☹ OOPS!!! Please key in a valid Number.";
                         System.out.println(err_msg);
                     }
-                } else if (len >= 4 && s.substring(0, 4).equals("todo")) {
+                } else if (len >= 6 && s.substring(0, 4).equals("todo")) {
                     if (len <= 5) {
                         String err_msg = "☹ OOPS!!! The description of a todo cannot be empty";
                         s = scanner.nextLine();
@@ -60,7 +78,7 @@ public class Duke {
                     list.add(todo);
                     System.out.println("added: " + todo);
                     System.out.println("Now you have " + list.size() + " tasks in the list");
-                } else if (len >= 8 && s.substring(0, 8).equals("deadline")) {
+                } else if (len >= 10 && s.substring(0, 8).equals("deadline")) {
                     String[] ddl_str_arr = s.split("/");
                     if (len <= 9 || ddl_str_arr.length <= 1) {
                         String err_msg = "☹ OOPS!!! The description or date of a deadline cannot be empty";
@@ -71,7 +89,7 @@ public class Duke {
                     list.add(deadline);
                     System.out.println("added: " + deadline);
                     System.out.println("Now you have " + list.size() + " tasks in the list");
-                } else if (len >= 5 && s.substring(0, 5).equals("event")) {
+                } else if (len >= 7 && s.substring(0, 5).equals("event")) {
                     String[] event_str_arr = s.split("/");
                     if (len <= 9 || event_str_arr.length <= 2) {
                         String err_msg = "☹ OOPS!!! The description or date of a event cannot be empty";
