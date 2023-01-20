ea07cf51919750689801babccf9a7bba796ac42f
YuanXun9834 <lollin9834@gmail.com>
2023-01-20 16:15:15 +0800

    Update date representation in Deadlines and Events

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,14 +1,18 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task{
-    private String date;
+    private LocalDate date;
 
     public Deadline(String date, String name){
         super(name);
-        this.date = date;
+        this.date = LocalDate.parse(date);
     }
 
     @Override
     public String toString(){
-        String s = "[ D ]" + super.toString() + String.format("(%s)", date);
+        String dateFormatted = date.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+        String s = "[ D ]" + super.toString() + String.format("(%s)", dateFormatted);
         return s;
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,16 +1,21 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task{
-    private String date1;
-    private String date2;
+    private LocalDate date1;
+    private LocalDate date2;
 
     public Event(String date1, String date2, String name){
         super(name);
-        this.date1 = date1;
-        this.date2 = date2;
+        this.date1 = LocalDate.parse(date1);
+        this.date2 = LocalDate.parse(date2);
     }
 
     @Override
     public String toString(){
-        String s = "[ E ]" + super.toString() + String.format("(%s %s)", date1, date2);
+        String d1 = date1.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+        String d2 = date2.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+        String s = "[ E ]" + super.toString() + String.format("(%s %s)", d1, d2);
         return s;
     }
 }
