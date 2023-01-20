c131d8b84f06f8c651eba75cb27b6c65a111819f
Warren Yang <e0544083@u.nus.edu>
2023-01-20 14:46:05 +0800

    edited constructor for Event to take in start time and end time

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,14 +1,16 @@
 //Event is one of 3 types of Task, so it must be a child of Task
 public class Event extends Task {
-    protected String timeRange;
+    protected String startTime;
+    protected String endTime;
 
-    public Event(String name, String timeRange) {
+    public Event(String name, String startTime, String endTime) {
         super(name);
-        this.timeRange = timeRange;
+        this.startTime = startTime;
+        this.endTime = endTime;
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " " + "(at: " + timeRange + ")";
+        return "[E]" + super.toString() + " " + "(from: " + startTime + " to: " + endTime + ")";
     }
 }
