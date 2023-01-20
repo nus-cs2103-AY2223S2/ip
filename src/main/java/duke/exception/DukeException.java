4deafdd57d0e6a4e435d288e0cee9a1869d64d56
zrei <88964793+zrei@users.noreply.github.com>
2023-01-21 00:48:08 +0800

    Add JavaDoc comments

--- src/main/java/duke/exception/DukeException.java
+++ src/main/java/duke/exception/DukeException.java
@@ -1,4 +1,6 @@
 package duke.exception;
+
+/** Exception for problems that arise when running Duke */
 public class DukeException extends Exception {
     public DukeException(String message) {
         super("☹ OOPS!!! " + message);
--- src/main/java/duke/task/Task.java
+++ src/main/java/duke/task/Task.java
@@ -1,9 +1,16 @@
 package duke.task;
 
+/** Class that represents a task */
 public abstract class Task {
     private boolean isDone = false;
     private String name;
 
+    /**
+     * Construct a Task object with the specified
+     * name.
+     *
+     * @param name Name to give task.
+     */
     public Task(String name) {
         this.name = name;
     }
@@ -16,6 +23,12 @@ public abstract class Task {
         return this.isDone;
     }
 
+    /**
+     * Convert the task to a string representation
+     * that can be saved in storage.
+     *
+     * @return String representation to be saved in storage.
+     */
     public abstract String getFileRepresentation();
 
     protected String getName() {
@@ -27,6 +40,13 @@ public abstract class Task {
         return (this.isDone ? "[X] " : "[ ] ") + this.name;
     }
 
+    /**
+     * Convert the string representation of a task from storage
+     * to a Task object.
+     *
+     * @param stringRepresentation String representation of a task from storage.
+     * @return Task object.
+     */
     public static Task createTaskFromStringRepresentation(String stringRepresentation) {
         char typeOfTask = stringRepresentation.charAt(0);
         String[] details = stringRepresentation.split("@");
