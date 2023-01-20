bc136f5ebdc1c1bf20126a8e2004b4c5d50332f8
benjamin-wee <benjaminwee20@gmail.com>
2023-01-20 23:41:48 +0800

    added marking and unmarking status of tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Duke {
     public static void main(String[] args) {
 
-        List<String> list = new ArrayList<>();
+        ArrayList<Task> list = new ArrayList<>();
 
         System.out.println("Hello! I'm Somebody\n" + "What can I do for you?");
         Scanner scn = new Scanner(System.in);
@@ -11,18 +11,36 @@ public class Duke {
 
             String input = scn.nextLine();
 
-            if (Objects.equals(input.toLowerCase(), "bye")) {
+            if (input.startsWith("mark")) {
+
+                String index = input.split(" ", 2)[1];
+                Task task = list.get(Integer.parseInt(index) - 1);
+                task.markAsDone();
+                System.out.println("Nice! I've marked this task as done:\n" + "[" + task.getStatusIcon() + "] "
+                        + task.getDescription());
+
+            } else if (input.startsWith("unmark")) {
+
+                String index = input.split(" ", 2)[1];
+                Task task = list.get(Integer.parseInt(index) - 1);
+                task.unmark();
+                System.out.println("OK, I've marked this task as not done yet:\n" + "[" + task.getStatusIcon() + "] "
+                        + task.getDescription());
+
+            } else if (Objects.equals(input.toLowerCase(), "bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 scn.close();
                 return;
             } else if (Objects.equals(input.toLowerCase(), "list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 1; i <= list.size(); i++) {
-                    System.out.println(i + ". " + list.get(i - 1));
+                    Task task = list.get(i - 1);
+                    System.out.println(i + "." + "[" + task.getStatusIcon() + "] " + task.getDescription());
                 }
             } else {
-                list.add(input);
+                Task task = new Task(input);
+                list.add(task);
                 System.out.println("added: " + input);
-
             }
 
         }
---
+++ src/main/java/Task.java
@@ -0,0 +1,25 @@
+public class Task {
+    protected String description;
+    protected Boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+
+    public void markAsDone() {
+        isDone = true;
+    }
+
+    public void unmark() {
+        isDone = false;
+    }
+}
