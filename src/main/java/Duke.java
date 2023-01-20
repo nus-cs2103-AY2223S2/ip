15dfcc6c7596ed055de2022bcd84b242f20ec41b
cetigerlily <cetigerlily@gmail.com>
2023-01-20 10:43:12 +0800

    update

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,24 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner s = new Scanner(System.in);
+        String separator = "____________________________________________________________";
+
+        String openingMessage = separator + "\nHello! I'm Duke\n" + "What can I do for you?\n" + separator;
+        System.out.println(openingMessage);
+
+        while(s.hasNext()) {
+            String command = s.nextLine();
+            if(command.equals("bye")) {
+                String closingMessage = separator + "\nBye. Hope to see you again soon!\n" + separator;
+                System.out.println(closingMessage);
+                break;
+            } else {
+                String message = separator + "\n" + command + "\n" + separator;
+                System.out.println(message);
+            }
+        }
+        s.close();
     }
 }
