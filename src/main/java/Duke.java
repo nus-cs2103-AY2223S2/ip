981a5c6bf882827312d9fff70840ebff5a8c580b
runoutofit <runoutofit@gmail.com>
2023-01-20 17:19:29 +0800

    level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,8 @@
+import java.util.Scanner;
+
 public class Duke {
+
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +10,16 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        Scanner sc = new Scanner(System.in);
+
+        String echo = sc.nextLine();
+
+        while (!(echo.equals("bye"))) {
+            System.out.print(echo);
+            echo = sc.nextLine();
+        }
+        System.out.print("bye. Hope to see you again soon!");
     }
+
 }
