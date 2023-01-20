be99e8919c7ff4208102ed31e7454a3efe239475
Wang <shaofeng1234567@gmail.com>
2023-01-20 15:19:54 +0800

    Level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,19 +11,29 @@ public class Duke {
         greeting(greeting);
         memory store = new memory();
 
+
         loop:
         while (true) {
             String input = sc.nextLine();
+            String[] tokens = input.split(" ");
             //echo(input);
-
-            switch (input) {
+            switch (tokens[0]) {
                 case "bye":
                     echo(input);
                     break loop;
 
                 case "list":
-                    store.read();
+                    //store.read();
+                    store.readBoth();
                     break;
+                case "mark":
+                    store.mark(Integer.parseInt(tokens[1]) - 1);
+                    break;
+
+                case "unmark":
+                    store.unmark(Integer.parseInt(tokens[1]) - 1);
+                    break;
+
                 default:
                     store.write(input);
             }
@@ -49,22 +59,27 @@ class memory {
 
     static int slots = 0;
     ArrayList<String> store;
+    ArrayList<Boolean> checkBox;
     int size;
 
     memory() {
         store = new ArrayList<>(100);
+        checkBox = new ArrayList<>(100);
         size = 100;
         slots++;
     }
 
     memory(int size) {
         store = new ArrayList<>(size);
+        checkBox = new ArrayList<>(size);
         slots++;
     }
 
     void write(String input) {
         store.add(input);
+        checkBox.add(false);
         System.out.println("added: " + input);
+
     }
 
     void read() {
@@ -72,5 +87,27 @@ class memory {
             System.out.println(x + 1 + ". " + store.get(x));
 
     }
+
+    void readBoth() {
+        for (int x = 0; x < store.size(); x++) {
+            if (checkBox.get(x))
+                System.out.println(x + 1 + ". " + "[X] " + store.get(x));
+            else
+                System.out.println(x + 1 + ". " + "[] " + store.get(x));
+        }
+    }
+
+    void mark(int number) {
+        checkBox.set(number, true);
+        System.out.println("Nice! I've marked this task as done:\n" +
+                "[X] " + store.get(number));
+    }
+
+    void unmark(int number) {
+        checkBox.set(number, false);
+        System.out.println("OK, I've marked this task as not done yet:\n" +
+                "[] " + store.get(number));
+    }
+
 
 }
