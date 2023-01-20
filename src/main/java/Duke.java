a8ae81596db2d72e6e814f28d84ed9a17f8ebac9
Wang <shaofeng1234567@gmail.com>
2023-01-20 14:38:49 +0800

    Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,22 +4,35 @@ import java.util.Scanner;
 public class Duke {
 
     static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
 
         String greeting = "Hello! I'm Alpha Beast What can I do for you?";
         greeting(greeting);
-        /* create a class of array*/
-        ArrayList<String> memory = new ArrayList<>();
+        memory store = new memory();
 
+        loop:
         while (true) {
             String input = sc.nextLine();
-            echo(input);
-            if(input.equals("bye"))
-                break;
+            //echo(input);
+
+            switch (input) {
+                case "bye":
+                    echo(input);
+                    break loop;
+
+                case "list":
+                    store.read();
+                    break;
+                default:
+                    store.write(input);
+            }
+
+
         }
     }
 
-    static void greeting(String message){
+    static void greeting(String message) {
         System.out.println(message);
     }
 
@@ -28,6 +41,36 @@ public class Duke {
             System.out.println("Bye. Hope to see you again soon!\n");
         else
             System.out.println(input);
+    }
+
+}
+
+class memory {
+
+    static int slots = 0;
+    ArrayList<String> store;
+    int size;
+
+    memory() {
+        store = new ArrayList<>(100);
+        size = 100;
+        slots++;
+    }
+
+    memory(int size) {
+        store = new ArrayList<>(size);
+        slots++;
+    }
+
+    void write(String input) {
+        store.add(input);
+        System.out.println("added: " + input);
+    }
+
+    void read() {
+        for (int x = 0; x < store.size(); x++)
+            System.out.println(x + 1 + ". " + store.get(x));
+
     }
 
 }
