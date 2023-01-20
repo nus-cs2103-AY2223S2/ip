5439fe826308b1d98ad81f52409bf341eec737bc
SeanChinJunKai <e0774037@u.nus.edu>
2023-01-20 23:59:23 +0800

    Completed Level-6

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -43,6 +43,11 @@ public class Duke {
                 continue;
             }
 
+            if (tokens[0].equals("delete")) {
+                deleteTask(tokens, taskList);
+                continue;
+            }
+
             switch (tokens[0]) {
                 case "todo": {
                     if (tokens.length < 2) {
@@ -119,6 +124,18 @@ public class Duke {
             Task chosenTask = tasks.get(chosenId - 1);
             chosenTask.unmark();
             System.out.println("OK, I've marked this task as not done yet:\n" + chosenTask);
+        } catch (NumberFormatException | IndexOutOfBoundsException e) {
+            System.out.println("Given task number is invalid");
+        }
+    }
+
+    private static void deleteTask(String[] inputTokens, ArrayList<Task> tasks) {
+        try {
+            int chosenId = Integer.parseInt(inputTokens[1]);
+            Task chosenTask = tasks.get(chosenId - 1);
+            tasks.remove(chosenTask);
+            System.out.println("Noted. I've removed this task:\n" + chosenTask);
+            System.out.printf("Now you have %d tasks in the list.\n", tasks.size());
         } catch (NumberFormatException | IndexOutOfBoundsException e) {
             System.out.println("Given task number is invalid");
         }
