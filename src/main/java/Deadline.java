df5db80567765348c1ba569097835891710af161
zrei <88964793+zrei@users.noreply.github.com>
2023-01-20 18:02:00 +0800

    Add function to convert task to a string representation for storage

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -10,4 +10,9 @@ public class Deadline extends Task {
     public String toString() {
         return "[D] " + super.toString() + " (by: " + by + ")";
     }
+
+    @Override
+    public String getFileRepresentation() {
+        return "D" + "@" + (super.isDone() ? "1" : "0") + "@" + this.getName() + "@" + this.by;
+    }
  }
--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -7,4 +7,9 @@ public class Todo extends Task {
     public String toString() {
         return "[T] " + super.toString();
     }
+
+    @Override
+    public String getFileRepresentation() {
+        return "T" + "@" + (super.isDone() ? "1" : "0") + "@" + this.getName();
+    }
 }
