4361a2aeef0bfd7e99699b6834e0db929bde4548
dawg420 <88711624+dawg420@users.noreply.github.com>
2023-01-20 15:17:11 +0800

    Level 1: Added greet, echo, exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,19 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+
+        boolean loop = true;
+        while (loop) {
+            Scanner echoScanner = new Scanner(System.in);
+            String msg = echoScanner.nextLine();
+            String bye = "bye";
+            if (!bye.equalsIgnoreCase(msg)) {
+                System.out.println(msg);
+            } else {
+                System.out.println("Bye. Hope to see you again soon!");
+                loop = false;
+            }
+        }
     }
 }
