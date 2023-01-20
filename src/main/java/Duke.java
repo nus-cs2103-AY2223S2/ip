cfbaf3932160657fe053a131795e24bd12965b68
tangyuantyy <e0774642@u.nus.edu>
2023-01-20 11:56:31 +0800

    update

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,20 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello I'm tyy\nWhat can I do for you?");
+
+        Scanner scan = new Scanner(System.in);
+        String input = scan.next();
+        while (!input.equals("bye")) {
+            System.out.println(" " + input);
+            input = scan.next();
+        }
+        System.out.println(" " + "Ciao. Hope to see you again soon!");
     }
 }
