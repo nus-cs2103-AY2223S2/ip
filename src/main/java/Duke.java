cbd5c84e5991bda3ac6827fe463d07b6529f6e39
jereldlimjy <jereldlimjy@hotmail.com>
2023-01-20 13:39:13 +0800

    mark as done, task class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
+    // Length of horizontal line
     private static final int HORIZONTAL_LINE_LENGTH = 80;
 
     public static void printHorizontalLine() {
@@ -15,18 +16,18 @@ public class Duke {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        List<String> tasks = new ArrayList<>();
+        List<Task> tasks = new ArrayList<>();
 
+        // Greeting
         printHorizontalLine();
-
         System.out.println(
             "Karen:\n" +
             "Can I speak to your manager?\n" +
             "Just kidding, this is Karen. How can I help you today?"
         );
-
         printHorizontalLine();
 
+        // Commands
         String command = sc.nextLine();
 
         while (!command.equals("bye")) {
@@ -35,11 +36,35 @@ public class Duke {
 
             if (command.equals("list")) {
                 for (int i = 0; i < tasks.size(); i++) {
-                    String currTask = tasks.get(i);
-                    System.out.printf("%d. %s\n", i + 1, currTask);
+                    Task currTask = tasks.get(i);
+                    System.out.printf("%d.[%s] %s\n", i + 1, currTask.getStatusIcon(), currTask.getDescription());
+                }
+            } else if (command.contains("mark") || command.contains("unmark")) {
+                int taskNumber = Integer.parseInt(command.split(" ")[1]);
+                String action = command.split(" ")[0];
+
+                // check if task number is valid
+                if (taskNumber > tasks.size() || taskNumber <= 0) {
+                    System.out.println("Can you please get your eyes checked?");
+                } else {
+                    Task currTask = tasks.get(taskNumber - 1);
+
+                    if (action.equals("mark")) {
+                        currTask.markAsDone();
+                        System.out.printf(
+                            "Congrats, I guess you get a medal?\n" +
+                            "[%s] %s\n", currTask.getStatusIcon(), currTask.getDescription()
+                        );
+                    } else if (action.equals("unmark")) {
+                        currTask.markAsNotDone();
+                        System.out.printf(
+                            "Why are you so lazy?\n" +
+                            "[%s] %s\n", currTask.getStatusIcon(), currTask.getDescription()
+                        );
+                    }
                 }
             } else {
-                tasks.add(command);
+                tasks.add(new Task(command));
                 System.out.printf("added: %s\n", command);
             }
 
@@ -47,6 +72,7 @@ public class Duke {
             command = sc.nextLine();
         }
 
+        // Exit
         printHorizontalLine();
         System.out.println("Karen:\n" + "Bye. This was of great inconvenience.");
         printHorizontalLine();
