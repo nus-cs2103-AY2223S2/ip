3a9059413fb5de9acd0035d5702afdfb22e9e4f8
Niu BoQian <e0774823@u.nus.edu>
2023-01-20 09:22:27 +0800

    Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,26 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        
+        while (sc.hasNext()) {
+            String output = sc.nextLine();
+            String lineBreak = "***---***---***---***---***---***---***";
+            if (output.equals("bye")) {
+                System.out.println(lineBreak + "\n" + "    GoodBye, have a nice day!");
+                break;
+            } else {
+                System.out.println(lineBreak + "\n" + "    " + output);
+            }
+        }
     }
 }
