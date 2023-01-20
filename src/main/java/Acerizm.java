746ae2bff894ced032e1ec4dcfe8307a3e91128c
Haiqel <34855234+Acerizm@users.noreply.github.com>
2023-01-20 10:49:06 +0800

    added list for printing out stuff

--- src/main/java/Acerizm.java
+++ src/main/java/Acerizm.java
@@ -1,4 +1,6 @@
 // import libraries here
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Acerizm {
@@ -16,17 +18,28 @@ public class Acerizm {
 
     // Chat method is for level 1 of chatbot
     public static void chat(){
+        // level 2: added a temporary list to store items
+        List<String> itemList = new ArrayList<>();
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("*-".repeat(100));
         System.out.println("What can I do for you?");
         System.out.println("*-".repeat(100));
         while(scanner.hasNextLine()){
             String input = scanner.nextLine();
-            if(!input.equals("bye")) {
+            if(!input.equals("bye") && !input.equals("list")) {
                 System.out.println("*-".repeat(100));
-                System.out.println(input);
+                System.out.println(String.format("added: %s",input));
+                itemList.add(input);
                 System.out.println("*-".repeat(100));
-            } else {
+            } else if(input.equals("list")){
+                System.out.println("*-".repeat(100));
+                for(int i=0; i < itemList.size();i++){
+                    System.out.println(String.format("%d. %s",i,itemList.get(i)));
+                }
+                System.out.println("*-".repeat(100));
+            }
+            else {
                 System.out.println("*-".repeat(100));
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println("*-".repeat(100));
