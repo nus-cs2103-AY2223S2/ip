085555f23cc03fd97428b0acc8cbf2f24f51925c
cetigerlily <cetigerlily@gmail.com>
2023-01-20 11:30:13 +0800

    update

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,18 +3,27 @@ public class Duke {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         String separator = "____________________________________________________________";
+        ArrayList<String> todoList = new ArrayList<>();
 
         String openingMessage = separator + "\nHello! I'm Duke\n" + "What can I do for you?\n" + separator;
         System.out.println(openingMessage);
 
         while(s.hasNext()) {
-            String command = s.nextLine();
-            if(command.equals("bye")) {
+            String input = s.nextLine();
+            if(input.equals("bye")) {
                 String closingMessage = separator + "\nBye. Hope to see you again soon!\n" + separator;
                 System.out.println(closingMessage);
                 break;
+            } else if(input.equals("list")) {
+                System.out.println(separator);
+                for(int i = 0; i < todoList.size(); i++) {
+                    String number = Integer.toString(i + 1);
+                    System.out.println(number + ". " + todoList.get(i));
+                }
+                System.out.println(separator);
             } else {
-                String message = separator + "\n" + command + "\n" + separator;
+                String message = separator + "\nadded: " + input + "\n" + separator;
+                todoList.add(input);
                 System.out.println(message);
             }
         }
