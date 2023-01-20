c1e1835e5a85c22e1ad4fa104d2689cbbaa691a9
Teo Weide <teoweide1999@gmail.com>
2023-01-20 14:44:30 +0800

    Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+        List ls = new ArrayList<>();
 
         System.out.println("Hello from Bench Monster");
         System.out.println("What can I do for you?");
@@ -12,8 +15,15 @@ public class Duke {
             if (type.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if(type.equals("list")) {
+                for(int i=0; i < ls.size(); i++) {
+                    int index = i + 1;
+                    System.out.println(index + ". " + ls.get(i));
+                }
+                type = s.nextLine();
             } else {
-                System.out.println(type);
+                System.out.println("added: " + type);
+                ls.add(type);
                 type = s.nextLine();
             }
         }
