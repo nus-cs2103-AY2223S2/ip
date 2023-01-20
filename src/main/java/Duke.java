9039f8e11a7a4b3c1da7dcc551b6797e9d726a77
duong truong minh <gaoranger2002@gmail.com>
2023-01-20 14:56:14 +0800

    Level 5 complete

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -24,12 +24,7 @@ public class Duke {
     private static void begin() {
         endCommand();
 
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello from Duke");
         System.out.println("What can I do for you?");
 
         endCommand();
