8296191d86e626ba069e556cb05018a0a06f8772
Teo Weide <teoweide1999@gmail.com>
2023-01-20 14:34:02 +0800

    Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,21 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Hello from Bench Monster");
+        System.out.println("What can I do for you?");
+        String type = s.nextLine();
+
+        while (true) {
+            if (type.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println(type);
+                type = s.nextLine();
+            }
+        }
     }
-}
+}//class
