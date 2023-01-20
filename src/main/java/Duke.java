f336871fdcf150d90dfd668e017ad10c44e98a93
Niu BoQian <e0774823@u.nus.edu>
2023-01-20 09:36:57 +0800

    Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,8 @@ import java.util.*;
 
 public class Duke {
     public static void main(String[] args) {
+        String[] inputs = new String[100];
+
         Scanner sc = new Scanner(System.in);
 
         String logo = " ____        _        \n"
@@ -11,15 +13,27 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
-        
+        int index = -1;
+        String lineBreak = "***---***---***---***---***---***---***" + "\n" + "    ";
+
         while (sc.hasNext()) {
-            String output = sc.nextLine();
-            String lineBreak = "***---***---***---***---***---***---***";
-            if (output.equals("bye")) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
                 System.out.println(lineBreak + "\n" + "    GoodBye, have a nice day!");
                 break;
+            } else if (input.equals("list")) {
+                StringBuilder sb = new StringBuilder();
+                for (int i = 1; i <= 100; i++) {
+                    if (inputs[i - 1] != null) {
+                        sb.append(i + ". " + input + "\n");
+                    }
+                }
+
+                System.out.println(lineBreak + sb.toString());
             } else {
-                System.out.println(lineBreak + "\n" + "    " + output);
+                index++;
+                inputs[index] = input;
+                System.out.println(lineBreak + "added: " + input);
             }
         }
     }
