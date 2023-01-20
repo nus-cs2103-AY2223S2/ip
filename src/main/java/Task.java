7b8fb5e937f405b703cccffac16c11b4214dde79
zrei <88964793+zrei@users.noreply.github.com>
2023-01-20 22:47:19 +0800

    Add method to convert task to a string to be saved in storage

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -24,4 +24,26 @@ public abstract class Task {
     public String toString() {
         return (this.isDone ? "[X] " : "[ ] ") + this.name;
     }
+
+    public static Task createTaskFromStringRepresentation(String stringRepresentation) {
+        char typeOfTask = stringRepresentation.charAt(0);
+        String[] details = stringRepresentation.split("@");
+        Task newTask = null;
+        switch (typeOfTask) {
+        case 'T':
+            newTask = new Todo(details[2]);
+            break;
+        case 'E':
+            newTask = new Event(details[2], details[3], details[4]);
+            break;
+        case 'D':
+            newTask = new Deadline(details[2], details[3]);
+            break;
+        default:
+            break;
+        }
+        newTask.setDone(details[1].equals("1"));
+        return newTask;
+    }
+
 }
