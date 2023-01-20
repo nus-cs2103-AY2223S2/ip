1d78f22c52be1c1e752b9d15cc40d9035a788ab0
Benny <benny.ong348@gmail.com>
2023-01-20 10:04:57 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,21 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    public static void userInput() {
+        System.out.println("Greetings");
+        Scanner user = new Scanner(System.in);
+
+        while (true) {
+            String input = user.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("Bye!");
+                break;
+            }
+            System.out.println(input);
+        }
+        return;
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +23,6 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        userInput();
     }
 }
