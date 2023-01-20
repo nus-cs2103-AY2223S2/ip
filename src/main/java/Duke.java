2c5be5180ac569393064da26a90faeb6ff649c9a
Niu BoQian <e0774823@u.nus.edu>
2023-01-20 14:19:37 +0800

    Completed adding enum. Updated test file

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,60 +14,78 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
 
         int index = 0;
-        String lineBreak = "***---***---***---***---***---***---***" + "\n" + "    ";
+        String lineBreak1 = "(((***---***---***---***---***---***\n";
+        String lineBreak2 = "---***---***---***---***---***---***)))\n\n";
 
         while (sc.hasNext()) {
             try {    
                 String inputLine = sc.nextLine();
                 DukeException.checkInput(inputLine);
                 String words[] = inputLine.split(" ");
-                String firstWord = words[0];
-                if (firstWord.equals("bye")) {
-                    System.out.println(lineBreak + "GoodBye, have a nice day!");
-                    break;
-                } else if (firstWord.equals("list")) {
-                    System.out.println(lineBreak);
-                    for (int i = 1; i <= tasks.size(); i++) {
-                        System.out.println("    " + i + ". " + tasks.get(i - 1));
+                Command command = Command.valueOf(words[0].toUpperCase());
+                switch (command) {
+                    case BYE: 
+                        System.out.println(lineBreak1 + "    GoodBye, have a nice day!\n" + lineBreak2);
+                        break;
+                    case LIST:
+                        System.out.print(lineBreak1);
+                        for (int i = 1; i <= tasks.size(); i++) {
+                            System.out.print("    " + i + ". " + tasks.get(i - 1) + "\n");
+                        }
+                        System.out.print(lineBreak2);
+                        break;
+                    case MARK:
+                        int ind = Integer.parseInt(words[1]);
+                        if (ind > tasks.size()) {
+                            System.out.println(lineBreak1 + "This task does not exist");
+                        } else {
+                            Task updatedTask = tasks.get(ind - 1).mark();
+                            tasks.set(ind - 1, updatedTask);
+                            System.out.println(lineBreak1 + "Congrats on completing the following task:\n    " + updatedTask);
+                            System.out.print(lineBreak2);
+                        }
+                        break;
+                    case UNMARK:
+                        int ind1 = Integer.parseInt(words[1]);
+                        if (ind1 > tasks.size()) {
+                            System.out.println(lineBreak1 + "This task does not exist");
+                            System.out.print(lineBreak2);
+                        } else {
+                            Task updatedTask = tasks.get(ind1 - 1).unmark();
+                            tasks.set(ind1 - 1, updatedTask);
+                            System.out.println(lineBreak1 + "Unchecked the following task:\n    " + updatedTask);
+                            System.out.print(lineBreak2);
+                        }
+                        break;
+                    case DEADLINE:
+                        String[] parts = inputLine.split("/");
+                        Deadline task = new Deadline(parts[0].split(" ")[1], false, parts[1]);
+                        tasks.add(task);
+                        System.out.println(lineBreak1 + "Successfully added the following task:\n    " + task);
+                        System.out.print(lineBreak2);
+                        break;
+                    case EVENT:
+                        String[] parts1 = inputLine.split("/");
+                        Event event = new Event(parts1[0].split(" ")[1], false, parts1[1], parts1[2]);
+                        tasks.add(event);
+                        System.out.println(lineBreak1 + "Successfully added the following task:\n    " + event);
+                        System.out.print(lineBreak2);
+                        break;
+                    case TODO:
+                        
+                        Todo todo = new Todo(inputLine.split(" ", 2)[1], false);
+                        tasks.add(todo);
+                        System.out.println(lineBreak1 + "Successfully added the following task:\n    " + todo);
+                        System.out.print(lineBreak2);
+                        break;
+                    case DELETE:
+                        System.out.println(lineBreak1+ "The following task is removed:\n    " + tasks.remove(Integer.parseInt(words[1]) - 1));
+                        System.out.print(lineBreak2);
+                        break;
                     }
-                } else if (firstWord.equals("mark")) {
-                    int ind = Integer.parseInt(words[1]);
-                    if (ind > tasks.size()) {
-                        System.out.println(lineBreak + "This task does not exist");
-                    } else {
-                        Task updatedTask = tasks.get(ind - 1).mark();
-                        tasks.set(ind - 1, updatedTask);
-                        System.out.println(lineBreak + "Congrats on completing the following task:\n    " + updatedTask);
-                    }
-                } else if (firstWord.equals("unmark")) {
-                    int ind = Integer.parseInt(words[1]);
-                    if (ind > tasks.size()) {
-                        System.out.println(lineBreak + "This task does not exist");
-                    } else {
-                        Task updatedTask = tasks.get(ind - 1).unmark();
-                        tasks.set(ind - 1, updatedTask);
-                        System.out.println(lineBreak + "Unchecked the following task:\n    " + updatedTask);
-                    }
-                } else if (firstWord.equals("deadline")) {
-                    String[] parts = inputLine.split("/");
-                    Deadline task = new Deadline(parts[0].split(" ")[1], false, parts[1]);
-                    tasks.add(task);
-                    System.out.println(lineBreak + "Successfully added the following task:\n    " + task);
-                } else if (firstWord.equals("event")) {
-                    String[] parts = inputLine.split("/");
-                    Event task = new Event(parts[0].split(" ")[1], false, parts[1], parts[2]);
-                    tasks.add(task);
-                    System.out.println(lineBreak + "Successfully added the following task:\n    " + task);
-                } else if (firstWord.equals("todo")) {
-                    String[] parts = inputLine.split(" ", 2);
-                    Todo task = new Todo(parts[1], false);
-                    tasks.add(task);
-                    System.out.println(lineBreak + "Successfully added the following task:\n    " + task);
-                } else if (firstWord.equals("delete")) {
-                    tasks.remove(Integer.parseInt(words[1]) - 1);
-                }
             } catch (DukeException e) {
-                System.out.println(lineBreak + e.getMessage());
+                System.out.println(lineBreak1 + e.getMessage());
+                System.out.print(lineBreak2);
             }
         }
     }
@@ -177,6 +195,10 @@ class DukeException extends Exception {
             throw new DukeException("☹ OOPS!!! Please specify which task to delete.");
         } 
     }
+}
+
+enum Command {
+    BYE, LIST, MARK, UNMARK, DELETE, TODO, DEADLINE, EVENT
 }
 
 
