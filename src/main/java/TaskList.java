7f2aa6e48425915a45d71d355cbc965f08acc003
zrei <88964793+zrei@users.noreply.github.com>
2023-01-20 22:48:19 +0800

    Add TaskList.java

---
+++ src/main/java/TaskList.java
@@ -0,0 +1,87 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class TaskList {
+    private ArrayList<Task> taskStore = new ArrayList<>(100);
+
+    public TaskList() {
+
+    }
+
+    public TaskList(String taskList) {
+        this.processTaskListString(taskList);
+    }
+
+    public void addTask(Task task) {
+        taskStore.add(task);
+    }
+
+    public Task deleteTask(int index) throws DukeException {
+        if (index >= this.getSize()) {
+            throw new DukeException("There aren't that many tasks in the list!");
+        } else if (index < 0) {
+            throw new DukeException("I don't recognise that task number.");
+        }
+        Task task = this.getTaskAtIndex(index);
+        this.taskStore.remove(index);
+        return task;
+    }
+
+    public Task markTaskDone(int index) throws DukeException {
+        if (index >= this.getSize()) {
+            throw new DukeException("There aren't that many tasks in the list!");
+        } else if (index < 0) {
+            throw new DukeException("I don't recognise that task number.");
+        }
+        this.getTaskAtIndex(index).setDone(true);
+        return this.getTaskAtIndex(index);
+    }
+
+    public Task unmarkTask(int index) throws DukeException {
+        if (index >= this.getSize()) {
+            throw new DukeException("There aren't that many tasks in the list!");
+        } else if (index < 0) {
+            throw new DukeException("I don't recognise that task number.");
+        }
+        this.getTaskAtIndex(index).setDone(false);
+        return this.getTaskAtIndex(index);
+    }
+
+    private void processTaskListString(String taskList) {
+        Scanner sc = new Scanner(taskList);
+        while (sc.hasNextLine()) {
+            String taskStringRepresentation = sc.nextLine();
+            this.addTask(Task.createTaskFromStringRepresentation(taskStringRepresentation));
+        }
+        sc.close();
+    }
+
+    public String createTaskListString() {
+        StringBuffer representation = new StringBuffer();
+        for (Task t : taskStore) {
+            representation.append(t.getFileRepresentation() + "\n");
+        }
+        return representation.toString();
+    }
+
+    public int getSize() {
+        return this.taskStore.size();
+    }
+
+    @Override
+    public String toString() {
+        StringBuffer res = new StringBuffer();
+        for (int i = 0; i < this.getSize() - 1; i++) {
+            res.append((i + 1) + ". " + this.getTaskAtIndex(i) + "\n");
+        }
+        if (this.getSize() - 1 >= 0) {
+            res.append(this.getSize() + ". " + this.getTaskAtIndex(this.getSize() - 1));
+        }
+        return res.toString();
+    }
+
+    private Task getTaskAtIndex(int index) {
+        return this.taskStore.get(index);
+    }
+
+}
