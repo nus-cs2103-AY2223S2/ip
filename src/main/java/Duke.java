9741e49805e74df6bd5c63de8bd2777cc09de0b3
dawg420 <88711624+dawg420@users.noreply.github.com>
2023-01-20 16:09:42 +0800

    Level 2: Added add, list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -9,17 +10,23 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
-
+        ArrayList<String> listOfThings = new ArrayList<String>();
         boolean loop = true;
         while (loop) {
             Scanner echoScanner = new Scanner(System.in);
             String msg = echoScanner.nextLine();
             String bye = "bye";
-            if (!bye.equalsIgnoreCase(msg)) {
-                System.out.println(msg);
-            } else {
-                System.out.println("Bye. Hope to see you again soon!");
+            String showList = "list";
+            if (bye.equalsIgnoreCase(msg)) {
+                System.out.println("Bye. Hope to see Byou again soon!");
                 loop = false;
+            } else if (showList.equalsIgnoreCase(msg)) {
+                for (String s : listOfThings) {
+                    System.out.println(s);
+                }
+            } else {
+                listOfThings.add(msg);
+                System.out.println("added " + msg);
             }
         }
     }
