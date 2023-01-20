626ffc6f90708ddfa971ec78d7b9b26f31c6a86a
zuohui48 <chenzuohui11@gmail.com>
2023-01-20 12:14:17 +0800

    Handle errors

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 public class Duke {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws DukeException {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -31,21 +31,49 @@ public class Duke {
                 Task newTask;
                 String[] inputs = input.split(" ");
                 String type = inputs[0];
-                if (type.equals("todo")) {
-                    String name = input.split(" ", 2)[1];
-                    newTask = new ToDo(name);
-                } else if (type.equals("deadline")) {
-                    String[] nameAndDeadline = input.split(" ", 2)[1].split(" /by ");
-                    String name = nameAndDeadline[0];
-                    String deadline = nameAndDeadline[1];
-                    newTask = new Deadline(name, deadline);
-                } else {
-                    String[] nameAndStart = input.split(" ", 2)[1].split(" /from ");
-                    String name = nameAndStart[0];
-                    String[] startAndEnd = nameAndStart[1].split(" /to ");
-                    String start = startAndEnd[0];
-                    String end = startAndEnd[1];
-                    newTask = new Event(name, start, end);
+                switch(type) {
+                    case "todo": {
+                        if (inputs.length < 2) {
+                            throw new DukeException("☹ OOPS!!! The description of a todo cannot be empty.");
+                        }
+                        String name = input.split(" ", 2)[1];
+                        newTask = new ToDo(name);
+                        break;
+                    }
+                    case "deadline": {
+                        if (inputs.length < 2) {
+                            throw new DukeException("☹ OOPS!!! The description of a deadline cannot be empty.");
+                        }
+                        String[] nameAndDeadline = input.split(" ", 2)[1].split(" /by ");
+                        if (nameAndDeadline.length < 2) {
+                            throw new DukeException("☹ OOPS!!! The deadline of a deadline cannot be empty.");
+                        }
+                        String name = nameAndDeadline[0];
+                        String deadline = nameAndDeadline[1];
+                        newTask = new Deadline(name, deadline);
+                        break;
+                    }
+                    case "event": {
+                        if (inputs.length < 2) {
+                            throw new DukeException("☹ OOPS!!! The description of a event cannot be empty.");
+                        }
+                        String[] nameAndStart = input.split(" ", 2)[1].split(" /from ");
+                        if (nameAndStart.length < 2) {
+                            throw new DukeException("☹ OOPS!!! The start of a event cannot be empty.");
+                        }
+                        String name = nameAndStart[0];
+                        String[] startAndEnd = nameAndStart[1].split(" /to ");
+                        if (startAndEnd.length < 2) {
+                            throw new DukeException("☹ OOPS!!! The end of a event cannot be empty.");
+                        }
+                        String start = startAndEnd[0];
+                        String end = startAndEnd[1];
+                        newTask = new Event(name, start, end);
+                        break;
+                    }
+                    default: {
+                        throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+                    }
                 }
                 list.add(newTask);
                 System.out.println("Got it. I've added this task:\n" + newTask + "\nNow you have " + list.size() + " tasks in the list.");
---
+++ src/main/java/DukeException.java
@@ -0,0 +1,6 @@
+import java.lang.Exception;
+public class DukeException extends IllegalArgumentException {
+    public DukeException(String msg) {
+        super(msg);
+    }
+}
