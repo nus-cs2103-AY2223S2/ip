19b52e85d412252967e954525bcdcf0d6c5dbd7f
sarthak181 <0108sarthak@gmail.com>
2023-01-21 01:51:32 +0800

    level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,14 +9,65 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
+        ArrayList<Task> todo = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
         while (true) {
             String str = sc.nextLine();
             if (str.equals("bye")) {
                 break;
             }
-            System.out.println(str);
+            if (str.equals("list")) {
+                recite(todo);
+            }
+            else if (str.length() > 4 && (str.substring(0, 5).equals("mark "))) {
+                int n = 0;
+                Task t = null;
+                if (str.length() == 6) {
+                    String num = String.valueOf(str.charAt(5));
+                    n = Integer.parseInt(num) - 1;
+                    t = todo.get(n);
+                    t.markAsDone();
+                } else {
+                    String num = str.substring(5, 7);
+                    n = Integer.parseInt(num) - 1;
+                    t = todo.get(n);
+                    t.markAsDone();
+                }
+                System.out.println("Nice! I've marked this task as done:\n"
+                                    + "  [" + t.getStatusIcon() + "] " + t.description);
+
+            }
+            else if (str.length() > 6 && (str.substring(0, 7).equals("unmark "))) {
+                int n = 0;
+                Task t = null;
+                if (str.length() == 8) {
+                    String num = String.valueOf(str.charAt(7));
+                    n = Integer.parseInt(num) - 1;
+                    t = todo.get(n);
+                    t.unMark();
+                } else {
+                    String num = str.substring(7, 9);
+                    n = Integer.parseInt(num) - 1;
+                    t = todo.get(n);
+                    t.unMark();
+                }
+                System.out.println("OK, I've marked this task as not done yet:\n"
+                        + "  [" + t.getStatusIcon() + "] " + t.description);
+
+            }
+            else {
+                Task t = new Task(str);
+                todo.add(t);
+                System.out.println("added: " + str);
+            }
         }
         System.out.println("Bye. Hope to see you again soon!");
+    }
+    static void recite(ArrayList<Task> l) {
+        for (int i = 1; i <= l.size(); i++) {
+            Task t = l.get(i-1);
+            System.out.println("" + i + ".[" + t.getStatusIcon() + "] " + t.description);
+        }
+
     }
 }
