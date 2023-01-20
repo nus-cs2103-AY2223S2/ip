a8baf991f8df73bf99ec9027db6497c4e6921768
Wang <shaofeng1234567@gmail.com>
2023-01-20 14:17:08 +0800

    Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,24 +1,33 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
 
-        String greeting = """
-                Hello! I'm Alpha Beast
-                What can I do for you?
-                """;
+        String greeting = "Hello! I'm Alpha Beast What can I do for you?";
+        greeting(greeting);
+        /* create a class of array*/
+        ArrayList<String> memory = new ArrayList<>();
 
-        System.out.println(greeting);
-        Scanner sc = new Scanner(System.in);
-        String input;
         while (true) {
-            input = sc.nextLine();
-            if (input.equals("bye")){
-                System.out.println("Bye. Hope to see you again soon!\n");
+            String input = sc.nextLine();
+            echo(input);
+            if(input.equals("bye"))
                 break;
-            }
-
-            System.out.println(input);
         }
     }
+
+    static void greeting(String message){
+        System.out.println(message);
+    }
+
+    static void echo(String input) {
+        if (input.equals("bye"))
+            System.out.println("Bye. Hope to see you again soon!\n");
+        else
+            System.out.println(input);
+    }
+
 }
