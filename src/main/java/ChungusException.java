a7f09683f9ae284b80354bdd1d69874eaaa07829
junyi <47441677+immanuelhume@users.noreply.github.com>
2023-01-20 14:09:03 +0800

    Use 4 spaces for indentation

--- src/main/java/ChungusException.java
+++ src/main/java/ChungusException.java
@@ -1,21 +1,21 @@
 class ChungusException extends RuntimeException {
-	public ChungusException(String msg) {
-		super(msg);
-	}
+    public ChungusException(String msg) {
+        super(msg);
+    }
 
-	public ChungusException(String msg, Throwable cause) {
-		super(msg, cause);
-	}
+    public ChungusException(String msg, Throwable cause) {
+        super(msg, cause);
+    }
 }
 
 class TaskNotFoundException extends ChungusException {
-	public TaskNotFoundException(int idx) {
-		super(String.format("Task %d does not exist", idx));
-	}
+    public TaskNotFoundException(int idx) {
+        super(String.format("Task %d does not exist", idx));
+    }
 }
 
 class TaskMarshalException extends ChungusException {
-	public TaskMarshalException(String s) {
-		super(String.format("Bad marshal format: \"%s\"", s));
-	}
+    public TaskMarshalException(String s) {
+        super(String.format("Bad marshal format: \"%s\"", s));
+    }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -2,211 +2,211 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 abstract class Task {
-	private String desc;
-	private boolean isDone;
+    private String desc;
+    private boolean isDone;
 
-	protected final static DateTimeFormatter dateTimeFmt = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+    protected final static DateTimeFormatter dateTimeFmt = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
 
-	public Task(String _desc) {
-		desc = _desc;
-		isDone = false;
-	}
+    public Task(String _desc) {
+        desc = _desc;
+        isDone = false;
+    }
 
-	protected String format() {
-		return (isDone() ? "[X] " : "[ ] ") + desc;
-	}
+    protected String format() {
+        return (isDone() ? "[X] " : "[ ] ") + desc;
+    }
 
-	public boolean isDone() {
-		return isDone;
-	}
+    public boolean isDone() {
+        return isDone;
+    }
 
-	public void setDone() {
-		isDone = true;
-	}
+    public void setDone() {
+        isDone = true;
+    }
 
-	public void setNotDone() {
-		isDone = false;
-	}
+    public void setNotDone() {
+        isDone = false;
+    }
 
-	public String desc() {
-		return desc;
-	}
+    public String desc() {
+        return desc;
+    }
 
-	@Override
-	public abstract String toString();
+    @Override
+    public abstract String toString();
 
-	public abstract String marshal();
+    public abstract String marshal();
 
-	public static Task unmarshal(String s) {
-		char typ = s.charAt(0);
-		switch (typ) {
-			case 'T':
-				return Todo.unmarshal(s);
-			case 'D':
-				return Deadline.unmarshal(s);
-			case 'E':
-				return Event.unmarshal(s);
-			default:
-				return null;
-		}
-	}
+    public static Task unmarshal(String s) {
+        char typ = s.charAt(0);
+        switch (typ) {
+            case 'T':
+                return Todo.unmarshal(s);
+            case 'D':
+                return Deadline.unmarshal(s);
+            case 'E':
+                return Event.unmarshal(s);
+            default:
+                return null;
+        }
+    }
 
-	protected static void trueOrThrow(boolean cond, RuntimeException t) {
-		if (!cond) {
-			throw t;
-		}
-	}
+    protected static void trueOrThrow(boolean cond, RuntimeException t) {
+        if (!cond) {
+            throw t;
+        }
+    }
 }
 
 class Todo extends Task {
-	public Todo(String desc) {
-		super(desc);
-	}
+    public Todo(String desc) {
+        super(desc);
+    }
 
-	@Override
-	public String toString() {
-		return "[T]" + format();
-	}
+    @Override
+    public String toString() {
+        return "[T]" + format();
+    }
 
-	@Override
-	public String marshal() {
-		StringBuilder builder = new StringBuilder();
+    @Override
+    public String marshal() {
+        StringBuilder builder = new StringBuilder();
 
-		builder.append('T');
-		builder.append(isDone() ? '1' : '0');
-		builder.append(desc());
+        builder.append('T');
+        builder.append(isDone() ? '1' : '0');
+        builder.append(desc());
 
-		return builder.toString();
-	}
+        return builder.toString();
+    }
 
-	public static Todo unmarshal(String s) {
-		trueOrThrow(s.charAt(0) == 'T', new TaskMarshalException(s));
+    public static Todo unmarshal(String s) {
+        trueOrThrow(s.charAt(0) == 'T', new TaskMarshalException(s));
 
-		boolean isDone = s.charAt(1) == '0' ? false : true;
+        boolean isDone = s.charAt(1) == '0' ? false : true;
 
-		String desc = s.substring(2, s.length());
-		Todo ret = new Todo(desc);
-		if (isDone) {
-			ret.setDone();
-		} else {
-			ret.setNotDone();
-		}
+        String desc = s.substring(2, s.length());
+        Todo ret = new Todo(desc);
+        if (isDone) {
+            ret.setDone();
+        } else {
+            ret.setNotDone();
+        }
 
-		return ret;
-	}
+        return ret;
+    }
 }
 
 class Deadline extends Task {
-	LocalDateTime deadline;
+    LocalDateTime deadline;
 
-	public Deadline(String desc, LocalDateTime _deadline) {
-		super(desc);
-		deadline = _deadline;
-	}
+    public Deadline(String desc, LocalDateTime _deadline) {
+        super(desc);
+        deadline = _deadline;
+    }
 
-	@Override
-	public String toString() {
-		return "[D]" + format() + String.format(" (by: %s)", deadline);
-	}
+    @Override
+    public String toString() {
+        return "[D]" + format() + String.format(" (by: %s)", deadline);
+    }
 
-	@Override
-	public String marshal() {
-		StringBuilder builder = new StringBuilder();
+    @Override
+    public String marshal() {
+        StringBuilder builder = new StringBuilder();
 
-		builder.append('D');
-		builder.append(isDone() ? '1' : '0');
-		builder.append(Chonk.chonkify(desc()));
-		builder.append(Chonk.chonkify(deadline.format(dateTimeFmt)));
+        builder.append('D');
+        builder.append(isDone() ? '1' : '0');
+        builder.append(Chonk.chonkify(desc()));
+        builder.append(Chonk.chonkify(deadline.format(dateTimeFmt)));
 
-		return builder.toString();
-	}
+        return builder.toString();
+    }
 
-	public static Deadline unmarshal(String s) {
-		if (s.charAt(0) != 'D') {
-			throw new TaskMarshalException(s);
-		}
-		boolean isDone = s.charAt(1) == '0' ? false : true;
+    public static Deadline unmarshal(String s) {
+        if (s.charAt(0) != 'D') {
+            throw new TaskMarshalException(s);
+        }
+        boolean isDone = s.charAt(1) == '0' ? false : true;
 
-		int idx = 2;
-		Pair<String, Integer> dechonked;
+        int idx = 2;
+        Pair<String, Integer> dechonked;
 
-		dechonked = Chonk.dechonkify(s, idx);
-		trueOrThrow(dechonked != null, new TaskMarshalException(s));
-		String desc = dechonked.first();
-		idx = dechonked.second();
+        dechonked = Chonk.dechonkify(s, idx);
+        trueOrThrow(dechonked != null, new TaskMarshalException(s));
+        String desc = dechonked.first();
+        idx = dechonked.second();
 
-		dechonked = Chonk.dechonkify(s, idx);
-		trueOrThrow(dechonked != null, new TaskMarshalException(s));
-		String deadline = dechonked.first();
+        dechonked = Chonk.dechonkify(s, idx);
+        trueOrThrow(dechonked != null, new TaskMarshalException(s));
+        String deadline = dechonked.first();
 
-		Deadline ret = new Deadline(desc, LocalDateTime.parse(deadline, dateTimeFmt));
-		if (isDone) {
-			ret.setDone();
-		} else {
-			ret.setNotDone();
-		}
+        Deadline ret = new Deadline(desc, LocalDateTime.parse(deadline, dateTimeFmt));
+        if (isDone) {
+            ret.setDone();
+        } else {
+            ret.setNotDone();
+        }
 
-		return ret;
-	}
+        return ret;
+    }
 }
 
 class Event extends Task {
-	LocalDateTime from, to;
+    LocalDateTime from, to;
 
-	public Event(String desc, LocalDateTime _from, LocalDateTime _to) {
-		super(desc);
-		from = _from;
-		to = _to;
-	}
+    public Event(String desc, LocalDateTime _from, LocalDateTime _to) {
+        super(desc);
+        from = _from;
+        to = _to;
+    }
 
-	@Override
-	public String toString() {
-		return "[E]" + format() + String.format(" (from: %s to: %s)", from, to);
-	}
+    @Override
+    public String toString() {
+        return "[E]" + format() + String.format(" (from: %s to: %s)", from, to);
+    }
 
-	@Override
-	public String marshal() {
-		StringBuilder builder = new StringBuilder();
+    @Override
+    public String marshal() {
+        StringBuilder builder = new StringBuilder();
 
-		builder.append('E');
-		builder.append(isDone() ? '1' : '0');
-		builder.append(Chonk.chonkify(desc()));
-		builder.append(Chonk.chonkify(from.format(dateTimeFmt)));
-		builder.append(Chonk.chonkify(to.format(dateTimeFmt)));
+        builder.append('E');
+        builder.append(isDone() ? '1' : '0');
+        builder.append(Chonk.chonkify(desc()));
+        builder.append(Chonk.chonkify(from.format(dateTimeFmt)));
+        builder.append(Chonk.chonkify(to.format(dateTimeFmt)));
 
-		return builder.toString();
-	}
+        return builder.toString();
+    }
 
-	public static Event unmarshal(String s) {
-		if (s.charAt(0) != 'E') {
-			throw new TaskMarshalException(s);
-		}
-		boolean isDone = s.charAt(1) == '0' ? false : true;
+    public static Event unmarshal(String s) {
+        if (s.charAt(0) != 'E') {
+            throw new TaskMarshalException(s);
+        }
+        boolean isDone = s.charAt(1) == '0' ? false : true;
 
-		int idx = 2;
-		Pair<String, Integer> dechonked;
+        int idx = 2;
+        Pair<String, Integer> dechonked;
 
-		dechonked = Chonk.dechonkify(s, idx);
-		trueOrThrow(dechonked != null, new TaskMarshalException(s));
-		String desc = dechonked.first();
-		idx = dechonked.second();
+        dechonked = Chonk.dechonkify(s, idx);
+        trueOrThrow(dechonked != null, new TaskMarshalException(s));
+        String desc = dechonked.first();
+        idx = dechonked.second();
 
-		dechonked = Chonk.dechonkify(s, idx);
-		trueOrThrow(dechonked != null, new TaskMarshalException(s));
-		String from = dechonked.first();
-		idx = dechonked.second();
+        dechonked = Chonk.dechonkify(s, idx);
+        trueOrThrow(dechonked != null, new TaskMarshalException(s));
+        String from = dechonked.first();
+        idx = dechonked.second();
 
-		dechonked = Chonk.dechonkify(s, idx);
-		trueOrThrow(dechonked != null, new TaskMarshalException(s));
-		String to = dechonked.first();
+        dechonked = Chonk.dechonkify(s, idx);
+        trueOrThrow(dechonked != null, new TaskMarshalException(s));
+        String to = dechonked.first();
 
-		Event ret = new Event(desc, LocalDateTime.parse(from, dateTimeFmt), LocalDateTime.parse(to, dateTimeFmt));
-		if (isDone) {
-			ret.setDone();
-		} else {
-			ret.setNotDone();
-		}
+        Event ret = new Event(desc, LocalDateTime.parse(from, dateTimeFmt), LocalDateTime.parse(to, dateTimeFmt));
+        if (isDone) {
+            ret.setDone();
+        } else {
+            ret.setNotDone();
+        }
 
-		return ret;
-	}
+        return ret;
+    }
 }
