c332196701f210c3b6bd695a638b29c8cdf454f9
StefanosAv <>
2023-01-20 11:24:09 +0800

    Add Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,8 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Duke {
+    public static ArrayList<String> taskList = new ArrayList<>();
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -17,15 +20,33 @@ public class Duke {
         // loop until "bye"
         while (true) {
             // read user input
-            String echo = input.nextLine();
+            String command = input.nextLine();
 
             // check if user inputs "bye"
-            if (echo.equalsIgnoreCase("bye")) {
+            if (command.equalsIgnoreCase("bye")) {
                 System.out.println(byeStr);
                 return;
             }
-            // echo user input
-            System.out.println(echo);
+            // check if user inputs "list" and display items in list
+            else if (command.equalsIgnoreCase("list")) {
+                if (taskList.size() == 0) {
+                    System.out.println("Your list is empty!");
+                } else {
+                    for (int i = 0; i < taskList.size(); i++) {
+                        System.out.println(i + 1 + ". " + taskList.get(i));
+                    }
+                }
+            }
+            else{
+                // add user input to list and echo
+                taskList.add(command);
+                System.out.println(adder(command));
+            }
         }
+    }
+
+    // Method to items to list
+    private static String adder(String userInput) {
+        return "added: " + userInput;
     }
 }
