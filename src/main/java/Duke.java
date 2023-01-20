329b00aea72af9d9089849a4063ceb9d84deed72
StefanosAv <>
2023-01-20 10:42:06 +0800

    Add Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +7,25 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        String helpStr = "What can I help you with?";
+        String byeStr = "Bye. Hope to see you again soon!";
+
+        // create a Scanner object to read user input
+        Scanner input = new Scanner(System.in);
+
+        // loop until "bye"
+        while (true) {
+            // read user input
+            String echo = input.nextLine();
+
+            // check if user inputs "bye"
+            if (echo.equalsIgnoreCase("bye")) {
+                System.out.println(byeStr);
+                return;
+            }
+            // echo user input
+            System.out.println(echo);
+        }
     }
 }
