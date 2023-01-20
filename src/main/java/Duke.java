0ca0d7a8f3e69c41da9c42a302aa92c08a80b3a3
DESKTOP-JCFEE72\Jiaxin Yao <yaojiax88@gmail.com>
2023-01-20 15:18:20 +0800

    implement functionality for greeting, echoing and exiting.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,36 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
+        /*String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+         */
+        String newLine = System.getProperty("line.separator");
+        System.out.println("-------------------------------------------------------" +
+                newLine + "Hello! Jak Sie Masz! I am Duke.\n What I do for you boss?");
+
+        String inData;
+        boolean exit = false;
+        Scanner scan = new Scanner( System.in );
+        inData = scan.nextLine();
+
+        while(!exit) {
+            if (inData.equals("bye")) {
+                exit = true;
+                System.out.println("Bye. Hope to see you again soon!");
+            } else {
+                System.out.println(inData);
+                inData = scan.nextLine();
+            }
+        }
+
+
+
+
+
+
+
     }
 }
