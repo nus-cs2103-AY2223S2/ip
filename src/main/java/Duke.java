a429bc4ea7be591af5ad6b22fd037f7e77f3eab7
jereldlimjy <jereldlimjy@hotmail.com>
2023-01-20 12:09:05 +0800

    add greet, echo, exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,40 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static final int HORIZONTAL_LINE_LENGTH = 80;
+
+    public static void printHorizontalLine() {
+        for (int i = 0; i < HORIZONTAL_LINE_LENGTH; i++) {
+            System.out.print("\u2500");
+        }
+
+        System.out.println();
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+
+        printHorizontalLine();
+
+        System.out.println(
+            "Karen:\n" +
+            "Can I speak to your manager?\n" +
+            "Just kidding, this is Karen. How can I help you today?"
+        );
+
+        printHorizontalLine();
+
+        String command = sc.nextLine();
+
+        while (!command.equals("bye")) {
+            printHorizontalLine();
+            System.out.println("Karen:\n" + command);
+            printHorizontalLine();
+            command = sc.nextLine();
+        }
+
+        printHorizontalLine();
+        System.out.println("Karen:\n" + "Bye. This was of great inconvenience.");
+        printHorizontalLine();
     }
 }
