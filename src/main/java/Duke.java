ff652eca4e25a6ec699091d28f808d09c1120447
runoutofit <runoutofit@gmail.com>
2023-01-21 02:37:41 +0800

    level 3 Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,22 +11,34 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo + "\n");
+        System.out.println("Hello from\n" + logo + "\n" + "How may i help you?\n");
 
         Scanner sc = new Scanner(System.in);
 
         String echo = sc.nextLine();
 
-        List<String> store = new ArrayList<>();
+        List<Task> store = new ArrayList<>();
 
         while (!echo.equals("bye")) {
             if (echo.equals("list")) {
                 for (int i = 0; i < store.size(); i++) {
-                    System.out.print(i+1 + ":" + store.get(i) + "\n");
+                    System.out.print(i + 1 + ":" + store.get(i) + "\n");
                 }
                 echo = sc.nextLine();
+            } else if ((echo.length() > 4) && (echo.substring(0,4).equals("mark"))) {
+                int index = Integer.parseInt(echo.substring(5));
+                index--;
+                store.get(index).markAsDone();
+                System.out.print("Nice! I've marked this task as done:\n" + store.get(index).toString() + "\n");
+                echo = sc.nextLine();
+            } else if ((echo.length() > 6) && (echo.substring(0, 6).equals("unmark"))) {
+                int index = Integer.parseInt(echo.substring(7));
+                index--;
+                store.get(index).unmark();
+                System.out.print("OK, I've marked this task as not done yet:\n" + store.get(index).toString() + "\n");
+                echo = sc.nextLine();
             } else {
-                store.add(echo);
+                store.add(new Task(echo));
                 System.out.print("added:" + echo + "\n");
                 echo = sc.nextLine();
             }
