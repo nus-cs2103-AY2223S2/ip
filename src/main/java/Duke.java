03631f977c925766242800b0d9886bbbb28dd14e
Wei Jie <ongweijie7@gmail.com>
2023-01-21 02:10:27 +0800

    Added greet, echo and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,17 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello from! I'm Duke\n" + "What can I do for you?\n");
+        Scanner reader = new Scanner(System.in);
+
+        while (true) {
+            String input = reader.nextLine();
+            if (input.equals("bye")) {
+                System.out.print("Bye. Hope to see you again soon!");
+                break;
+            }
+            System.out.println(input);
+        }
+
     }
 }
