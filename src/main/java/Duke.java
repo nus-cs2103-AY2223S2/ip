e9a8db311811f3c16753f457e54a1fca90045356
DESKTOP-JCFEE72\Jiaxin Yao <yaojiax88@gmail.com>
2023-01-22 00:41:31 +0800

    implement functionality for marking as done.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,26 @@
 import java.util.*;
+ class Task {
+    public String description;
+     public boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public void changeCompletion() {
+        this.isDone = !this.isDone;
+    }
+    @Override
+     public String toString() {
+        return "[" + getStatusIcon() + "] " + description;
+    }
+
+}
 public class Duke {
     public static void main(String[] args) {
         /* String logo = " ____        _        \n"
@@ -8,7 +30,7 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
          */
         String newLine = System.getProperty("line.separator");
-        ArrayList<String> tostore = new ArrayList<>(100);
+        ArrayList<Task> toStore = new ArrayList<>(100);
         System.out.println("-------------------------------------------------------" +
                 newLine + "Hello! Jak Sie Masz! I am Duke.\n What I do for you boss?");
 
@@ -16,22 +38,49 @@ public class Duke {
         boolean exit = false;
         Scanner scan = new Scanner( System.in );
         inData = scan.nextLine();
+        String[] arrofStr = inData.split(" ");
 
         while(!exit) {
-            if (inData.equals("bye")) {
-                exit = true;
-                System.out.println("Bye. Hope to see you again soon!");
-            } else if (inData.equals("list")){
-                int counter = 0;
-                for(String i: tostore) {
-                    counter++;
-                    System.out.println(counter + ". " + i);
-                }
-                inData = scan.nextLine();
-            } else {
-                System.out.println("added: " + inData);
-                tostore.add(inData);
-                inData = scan.nextLine();
+            switch (arrofStr[0]) {
+                case "bye":
+                    exit = true;
+                    System.out.println("Bye. Hope to see you again soon!");
+                    break;
+                case "list":
+                    System.out.println("Here are the tasks in your list my good sir:");
+                    int counter = 0;
+                    for (Task i : toStore) {
+                        counter++;
+                        System.out.println(counter + ". " + i.toString());
+                    }
+                    inData = scan.nextLine();
+                    arrofStr = inData.split(" ");
+                    break;
+                case "unmark":
+                    int indx = Integer.parseInt(arrofStr[1]) - 1;
+                    Task toMark = toStore.get(indx);
+                    toMark.changeCompletion();
+                    toStore.set(indx , toMark);
+                    System.out.println("OK, I've marked this task as not done yet:\n" + toMark);
+                    inData = scan.nextLine();
+                    arrofStr = inData.split(" ");
+                    break;
+                case "mark":
+                    indx = Integer.parseInt(arrofStr[1]) - 1;
+                    toMark = toStore.get(indx);
+                    toMark.changeCompletion();
+                    toStore.set(indx , toMark);
+                    System.out.println("Nice! I've marked this task as done:\n" + toMark);
+                    inData = scan.nextLine();
+                    arrofStr = inData.split(" ");
+                    break;
+                default:
+                    System.out.println("added: " + inData);
+                    Task toAdd = new Task(inData);
+                    toStore.add(toAdd);
+                    inData = scan.nextLine();
+                    arrofStr = inData.split(" ");
+                    break;
             }
         }
 
