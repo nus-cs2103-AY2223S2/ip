74082535dc9adbb0929df6648bfb935945edf093
dawg420 <88711624+dawg420@users.noreply.github.com>
2023-01-21 19:13:30 +0800

    Handles errors when the task to mark is invalid or outside of the list; when the task is not preceded by the type of the task; and when the todo field is empty

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -71,6 +71,12 @@ public class Duke {
         }
     }
 
+    public static class DukeException extends RuntimeException {
+        public DukeException(String errorMessage, Throwable err) {
+            super(errorMessage, err);
+        }
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -93,21 +99,43 @@ public class Duke {
             String byWhen = "";
             String fromWhen = "";
             String toWhen = "";
-            if (msg.contains("mark") || msg.contains("unmark")) {
-                firstWord = msg.substring(0, msg.indexOf(" "));
+            if (msg.contains("mark")) {
                 try {
+                    firstWord = msg.substring(0, msg.indexOf(" "));
                     secondInt = Integer
                             .parseInt(msg.substring(msg.indexOf(" ") + 1, msg.length()));
+                    Task thisTask = listOfThings.get(secondInt - 1);
+                    thisTask.markDone();
+                    listOfThings.set(secondInt - 1, thisTask);
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println(thisTask);
                 } catch (Exception ex) {
-
+                    System.err.println("Please indicate a valid task!");
+                }
+            } else if (msg.contains("unmark")) {
+                try {
+                    firstWord = msg.substring(0, msg.indexOf(" "));
+                    secondInt = Integer
+                            .parseInt(msg.substring(msg.indexOf(" ") + 1, msg.length()));
+                    Task thisTask = listOfThings.get(secondInt - 1);
+                    thisTask.markUndone();
+                    listOfThings.set(secondInt - 1, thisTask);
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    System.out.println(thisTask);
+                } catch (Exception ex) {
+                    System.err.println("Please indicate a valid task!");
                 }
             } else if (msg.contains("todo")) {
-                firstWord = msg.substring(msg.indexOf(" ") + 1, msg.length());
-                Todo newTodo = new Todo(firstWord);
-                listOfThings.add(newTodo);
-                System.out.println("Got it. I've added this task:");
-                System.out.println(newTodo);
-                System.out.println("Now you have " + listOfThings.size() + " tasks in the list.");
+                try {
+                    firstWord = msg.substring(msg.indexOf(" ") + 1, msg.length());
+                    Todo newTodo = new Todo(firstWord);
+                    listOfThings.add(newTodo);
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(newTodo);
+                    System.out.println("Now you have " + listOfThings.size() + " tasks in the list.");
+                } catch (Exception ex) {
+                    System.err.println("Whoops! The description of a todo cannot be empty!");
+                }
             } else if (msg.contains("deadline")) {
                 firstWord = msg.substring(msg.indexOf(" ") + 1, msg.indexOf("/by") - 1);
                 byWhen = msg.substring(msg.indexOf("/by") + 4, msg.length());
@@ -125,8 +153,7 @@ public class Duke {
                 System.out.println("Got it. I've added this task:");
                 System.out.println(newEvent);
                 System.out.println("Now you have " + listOfThings.size() + " tasks in the list.");
-            }
-            if (bye.equalsIgnoreCase(msg)) {
+            } else if (bye.equalsIgnoreCase(msg)) {
                 System.out.println("Bye. Hope to see you again soon!");
                 echoScanner.close();
                 loop = false;
@@ -134,19 +161,8 @@ public class Duke {
                 for (int i = 0; i < listOfThings.size(); i++) {
                     System.out.println(i + 1 + ". " + listOfThings.get(i));
                 }
-            } else if (mark.equalsIgnoreCase(firstWord) && secondInt != 0) {
-                Task thisTask = listOfThings.get(secondInt - 1);
-                thisTask.markDone();
-                listOfThings.set(secondInt - 1, thisTask);
-                System.out.println("Nice! I've marked this task as done:");
-                System.out.println(thisTask);
-            } else if (unmark.equalsIgnoreCase(firstWord) && secondInt != 0) {
-                Task thisTask = listOfThings.get(secondInt - 1);
-                thisTask.markUndone();
-                listOfThings.set(secondInt - 1, thisTask);
-                System.out.println("OK, I've marked this task as not done yet:");
-                System.out.println(thisTask);
             } else {
+                System.err.println("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
             }
         }
     }
