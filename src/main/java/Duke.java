56f9f7a309ed6647d1f30f90668cbda1f6730e84
John Chia Heng Kit <programmingwithjohn@gmail.com>
2023-01-21 12:23:23 +0800

    Duke.java added from ip folder

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,40 @@
+import java.io.*;
+import java.util.*;
 public class Duke {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+        StringBuilder sb = new StringBuilder();
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String line = "-------------------------------";
+        System.out.println(line);
+        System.out.println("Hiii Im\n" + logo);
+        System.out.println("What can I do for you hmm?");
+        System.out.println(line);
+        String inp;
+        /**
+         * Simply echoes commands entered by the user,
+         * and exits when the user types "bye".
+         */
+        while (true) {
+            inp = br.readLine();
+            System.out.println(line);
+            if (inp.equals("bye")) {
+                System.out.println("Byeee! Hope to see you again! Signing off, Duke.");
+                System.out.println(line);
+                break;
+            }
+            System.out.println(inp);
+            System.out.println(line);
+        }
+
+
+
+
     }
 }
