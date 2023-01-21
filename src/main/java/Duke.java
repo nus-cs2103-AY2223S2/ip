fcd3c3fd0e0d0717d5e2d20b789db9c242ad168b
4ndrelim <andre_lin@u.nus.edu>
2023-01-21 17:27:17 +0800

    Add span class to format responses

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,43 +4,43 @@ public class Duke {
     public static void main(String[] args) {
         String welcomeMsg = "Hello i'm Duke\nWhat can I do for you?";
         String goodbyeMsg = "Bye. Hope to see you again soon!";
-        System.out.println(welcomeMsg);
+        System.out.println(Span.format(welcomeMsg));
         TaskList tasks = new TaskList(); // create a list for all the tasks
         Scanner scanner = new Scanner(System.in); // creates a scanner object
         while (scanner.hasNextLine()) {
             String echo = scanner.nextLine(); // get user input
             try {
                 if (echo.equals("bye")) {
-                    System.out.println(goodbyeMsg);
+                    System.out.println(Span.format(goodbyeMsg));
                     return;
                 } else if (echo.equals("list")) {
-                    System.out.println("Here are the tasks in your list:\n" + tasks.listTasks()); // list all the stored tasks
+                    System.out.println(Span.format("Here are the tasks in your list:\n" + tasks.listTasks())); // list all the stored tasks
                 } else if (echo.matches("mark \\d+")) {
                     String[] sp = echo.split(" ");
                     int taskNum = Integer.valueOf(sp[1]);
                     if (sp.length == 2 && 1 <= taskNum && taskNum <= tasks.size()) { // otherwise, invalid mark and ignore for now
                         String marked = tasks.toggleMark(taskNum-1);
-                        System.out.println("Nice! I've marked this task as done:\n\t" + marked);
+                        System.out.println(Span.format("Nice! I've marked this task as done:\n\t" + marked));
                     }
                 } else if (echo.matches("unmark \\d+")) {
                     String[] sp = echo.split(" ");
                     int taskNum = Integer.valueOf(sp[1]);
                     if (sp.length == 2 && 1 <= taskNum && taskNum <= tasks.size()) { // otherwise, invalid unmark and ignore for now
                         String unmarked = tasks.toggleUnmark(taskNum-1);
-                        System.out.println("OK, I've marked this task as not done yet:\n\t" + unmarked);
+                        System.out.println(Span.format("OK, I've marked this task as not done yet:\n\t" + unmarked));
                     }
                 }  else if (echo.matches("delete \\d+")) {
                     String[] sp = echo.split(" ");
                     int taskNum = Integer.valueOf(sp[1]);
                     if (sp.length == 2 && 1 <= taskNum && taskNum <= tasks.size()) { // otherwise, invalid delete, error handling later
                         Task removed = tasks.delete(taskNum);
-                        System.out.println("Noted. I've removed this task:\n\t" + removed.getStatusIcon() + "\n" + "Now you have " + tasks.size() + " task(s) in the list.");
+                        System.out.println(Span.format("Noted. I've removed this task:\n\t" + removed.getStatusIcon() + "\n" + "Now you have " + tasks.size() + " task(s) in the list."));
                     }
                 } else { // add task
                     Task task = null; // to be converted into the right (sub) task below
                     if (echo.matches("todo\\s*.*")) {
                         if (echo.length() < 6) {
-                            throw new MissingArgumentException("The description of a todo cannot be empty.", "todo");
+                            throw new MissingArgumentException(Span.format("The description of a todo cannot be empty."), "todo");
                         }
                         String info = echo.substring(5);
                         task = new ToDo(info); // pass description of todo
@@ -54,15 +54,15 @@ public class Duke {
                         String[] time = sp[1].split(" /to ");
                         task = new Event(sp[0], time[0], time[1]);
                     } else {
-                        throw new UnknownCommandException("I'm sorry, but I don't know what that means :-(", echo);
+                        throw new UnknownCommandException(Span.format("I'm sorry, but I don't know what that means :-("), echo);
                     }
                     tasks.addTask(task);
-                    System.out.println("Got it. I've added this task:\n\t" + task.getStatusIcon() + "\n" + "Now you have " + tasks.size() + " task(s) in the list.");
+                    System.out.println(Span.format("Got it. I've added this task:\n\t" + task.getStatusIcon() + "\n" + "Now you have " + tasks.size() + " task(s) in the list."));
                 }
             } catch (UnknownCommandException e) {
-                System.out.println(e);
+                System.out.println(Span.format(e.toString()));
             } catch (MissingArgumentException e) {
-                System.out.println(e);
+                System.out.println(Span.format(e.toString()));
             }
         }
     }
---
+++ src/main/java/Span.java
@@ -0,0 +1,9 @@
+public class Span {
+    public static String format(String s) {
+        String[] sp = s.split("\n");
+        s = String.join("\n\t", sp);
+        return String.format("\t___________________________________________________________________________\n" +
+                "\t%s\n" +
+                "\t___________________________________________________________________________", s);
+    }
+}
