fcd123dd0c8b18b35dbab611b53cfe9629f0884f
douglch <douglchwm@gmail.com>
2023-01-21 16:12:53 +0800

    reformatted code, added TaskList class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,8 @@
-import java.util.ArrayList;
 import java.util.Scanner;
-import java.lang.StringBuilder;
-import java.util.Collections;
 
 public class Duke {
-    static int numOfTasks = 0;
     public static void main(String[] args) {
-        ArrayList<Task> taskList = new ArrayList<>();
-
+        TaskList taskList = new TaskList();
         String dog = "⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⠀⣀⣀⣀⣀⢀⣀⣀⣀⣀⣀⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀\n" +
                 "⠀⠀⠀⠀⠀⣀⣰⣿⣿⡻⠟⠋⠉⠉⣻⠟⠉⠉⠉⠛⢯⡛⢿⣿⣷⣤⣀⠀⠀⠀⠀⠀\n" +
                 "⠀⣠⣴⠾⠛⢋⣿⠟⠋⠀⠀⠀⠀⢀⡟⠀⠀⠀⠀⠀⠀⠈⠂⣹⣿⡈⠙⠻⢶⣄⡀⠀\n" +
@@ -35,101 +30,32 @@ public class Duke {
                 break;
             } else if (inputArr[0].equals("list")) {
                 System.out.println("Here are the tasks in your list dawg:");
-                for (int i = 0; i < numOfTasks; i++) System.out.println( (i+1) + "." + taskList.get(i));
+                taskList.printList();
             } else if (inputArr[0].equals("mark")) {
-                taskList.get(Integer.parseInt(inputArr[1]) - 1).markStatus(true);
+                taskList.markStatus(inputArr[1]);
                 System.out.println("The task is marked, dawg");
             } else if (inputArr[0].equals("unmark")){
-                taskList.get(Integer.parseInt(inputArr[1]) - 1).markStatus(false);
+                taskList.unMarkStatus(inputArr[1]);
                 System.out.println("Gotcha dawg, unmarked.");
             } else if (inputArr[0].equals("todo") || inputArr[0].equals("deadline") || inputArr[0].equals("event")) {
                 try {
-                    addTasks(taskList, inputArr, inputArr[0]);
+                    taskList.addTasks(inputArr, inputArr[0]);
                 } catch (EmptyDescException e) {
-                    System.out.println(e);
+                    e.printStackTrace();
                 }
             } else if (inputArr[0].equals("delete")) {
                 int inputIndex = Integer.parseInt(inputArr[1]);
-                Task task = taskList.get(inputIndex-1);
-                taskList.remove(inputIndex-1);
-                numOfTasks--;
-                System.out.println("Removing your task? It's gone now RUFF:");
-                System.out.println("  " + task);
-                System.out.println("Now you have " + numOfTasks + " in the list!");
+                taskList.deleteTasks(inputIndex);
             } else {
                 try {
                     throw new UnknownCommandException("test");
                 } catch (UnknownCommandException e) {
-                    System.out.println(e);
+                    e.printStackTrace();
                 }
             }
         }
 
     }
 
-    public static void addTasks(ArrayList<Task> taskList, String[] inputArr, String taskType) throws EmptyDescException {
-        String desc = "variable not initialised";
-        StringBuilder sb = new StringBuilder();
-        if (inputArr.length == 1) throw new EmptyDescException(taskType, "test");
-        switch (taskType) {
-            case "todo":
-                // task is from index 1
-                for (int i = 1; i < inputArr.length; i++) {
-                    sb.append(inputArr[i]);
-                    if (i != inputArr.length - 1) {
-                        sb.append(" ");
-                    }
-                }
-                Todo todo = new Todo(sb.toString());
-                taskList.add(todo); numOfTasks++;
-                System.out.println("Gotcha, I've added:");
-                System.out.println("  " + todo);
-                System.out.println("Now you have " + numOfTasks + " in the list!");
-                break;
 
-            case "deadline":
-                for (int i = 1; i < inputArr.length; i++) {
-                    if (inputArr[i].equals("/by")) {
-                        desc = sb.toString();
-                        sb.setLength(0);
-                        continue;
-                    }
-                    sb.append(inputArr[i]);
-                    // No need for whitespace at the end
-                    if (i != inputArr.length - 1) {
-                        sb.append(" ");
-                    }
-                }
-                Deadline deadline = new Deadline(desc, sb.toString());
-                taskList.add(deadline); numOfTasks++;
-                System.out.println("Gotcha, I've added:");
-                System.out.println("  " + deadline);
-                System.out.println("Now you have " + numOfTasks + " in the list!");
-                break;
-            case "event":
-                String from = "local variable not initialised";
-                for (int i = 1; i < inputArr.length; i++) {
-                    if (inputArr[i].equals("/from")) {
-                        desc = sb.toString();
-                        sb.setLength(0);
-                        continue;
-                    } else if (inputArr[i].equals("/to")) {
-                        from = sb.toString();
-                        sb.setLength(0);
-                        continue;
-                    }
-                    sb.append(inputArr[i]);
-                    // No need for whitespace at the end
-                    if (i != inputArr.length - 1) {
-                        sb.append(" ");
-                    }
-                }
-                Event event = new Event(desc, from, sb.toString());
-                taskList.add(event); numOfTasks++;
-                System.out.println("Gotcha, I've added:");
-                System.out.println("  " + event);
-                System.out.println("Now you have " + numOfTasks + " in the list!");
-                break;
-        }
-    }
 }
---
+++ src/main/java/TaskList.java
@@ -0,0 +1,97 @@
+import java.util.ArrayList;
+import java.lang.StringBuilder;
+
+public class TaskList {
+    protected ArrayList<Task> taskList;
+    protected int numOfTasks;
+    public TaskList() {
+        this.taskList = new ArrayList<>();
+        this.numOfTasks = 0;
+    }
+    public void printList() {
+        for (int i = 0; i < this.numOfTasks; i++) System.out.println((i+1) + "." + this.taskList.get(i));
+    }
+
+    public void markStatus(String index) {
+        taskList.get(Integer.parseInt(index) - 1).markStatus(true);
+    }
+
+    public void unMarkStatus(String index) {
+        taskList.get(Integer.parseInt(index) - 1).markStatus(false);
+    }
+
+    public void addTasks(String[] inputArr, String taskType) throws EmptyDescException {
+        String desc = "variable not initialised";
+        StringBuilder sb = new StringBuilder();
+        if (inputArr.length == 1) throw new EmptyDescException(taskType, "test");
+        switch (taskType) {
+            case "todo":
+                // task is from index 1
+                for (int i = 1; i < inputArr.length; i++) {
+                    sb.append(inputArr[i]);
+                    if (i != inputArr.length - 1) {
+                        sb.append(" ");
+                    }
+                }
+                Todo todo = new Todo(sb.toString());
+                this.taskList.add(todo); this.numOfTasks++;
+                System.out.println("Gotcha, I've added:");
+                System.out.println("  " + todo);
+                System.out.println("Now you have " + this.numOfTasks + " in the list!");
+                break;
+
+            case "deadline":
+                for (int i = 1; i < inputArr.length; i++) {
+                    if (inputArr[i].equals("/by")) {
+                        desc = sb.toString();
+                        sb.setLength(0);
+                        continue;
+                    }
+                    sb.append(inputArr[i]);
+                    // No need for whitespace at the end
+                    if (i != inputArr.length - 1) {
+                        sb.append(" ");
+                    }
+                }
+                Deadline deadline = new Deadline(desc, sb.toString());
+                this.taskList.add(deadline); this.numOfTasks++;
+                System.out.println("Gotcha, I've added:");
+                System.out.println("  " + deadline);
+                System.out.println("Now you have " + this.numOfTasks + " in the list!");
+                break;
+            case "event":
+                String from = "local variable not initialised";
+                for (int i = 1; i < inputArr.length; i++) {
+                    if (inputArr[i].equals("/from")) {
+                        desc = sb.toString();
+                        sb.setLength(0);
+                        continue;
+                    } else if (inputArr[i].equals("/to")) {
+                        from = sb.toString();
+                        sb.setLength(0);
+                        continue;
+                    }
+                    sb.append(inputArr[i]);
+                    // No need for whitespace at the end
+                    if (i != inputArr.length - 1) {
+                        sb.append(" ");
+                    }
+                }
+                Event event = new Event(desc, from, sb.toString());
+                this.taskList.add(event); this.numOfTasks++;
+                System.out.println("Gotcha, I've added:");
+                System.out.println("  " + event);
+                System.out.println("Now you have " + this.numOfTasks + " in the list!");
+                break;
+        }
+    }
+
+    public void deleteTasks(int inputIndex) {
+        Task task = this.taskList.get(inputIndex-1);
+        this.taskList.remove(inputIndex-1);
+        this.numOfTasks--;
+        System.out.println("Removing your task? It's gone now RUFF:");
+        System.out.println("  " + task);
+        System.out.println("Now you have " + this.numOfTasks + " in the list!");
+    }
+}
