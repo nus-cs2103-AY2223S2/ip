ca436b5d5bbd00570921dc9692c2be9d5791df58
LAPTOP-UOT6M2SH\manas <manasijsaha9616@gmail.com>
2023-01-21 23:08:02 +0800

    did the level 3 task

---
+++ src/main/java/ATask.java
@@ -0,0 +1,9 @@
+public class ATask {
+
+    public String task_name;
+    public boolean status;
+    public ATask(String str, boolean state) {
+        task_name = str;
+        status = state;
+    }
+}
--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,23 +2,50 @@ import java.util.*;
 import java.util.ArrayList;
 public class Duke {
     public static Scanner sc = new Scanner(System.in);
-    public static ArrayList<String> lst = new ArrayList<String>();
+    public static ArrayList<ATask> lst = new ArrayList<ATask>();
     public static void add_to_list(String str) {
-        lst.add(str);
+        lst.add(new ATask(str, false));
         System.out.println("added: " + str);
     }
+
     public static void print_list() {
         for(int i = 0; i < lst.size(); i++) {
-            System.out.println((i+1) + ". " + lst.get(i));
+            if(lst.get(i).status) {
+                System.out.println((i+1) + ". " + "[X]" + lst.get(i).task_name);
+            }
+            else {
+                System.out.println((i+1) + ". " + "[ ]" + lst.get(i).task_name);
+            }
         }
+    }
+
+    public static void mark(ATask tsk) {
+        tsk.status = true;
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println("[X] " + tsk.task_name);
+    }
+
+    public static void unmark(ATask tsk) {
+        tsk.status = false;
+        System.out.println("OK, I've marked this task as not done yet:");
+        System.out.println("[ ] " + tsk.task_name);
     }
     public static void reply() {
         String str = sc.nextLine();
         if(str.equals("bye")) {
             System.out.println("Bye mortal, I will get back to destroying" +
                     " galaxies");
-        } else if(str.equals("list")) {
+        }
+        else if(str.equals("list")) {
             print_list();
+            reply();
+        }
+        else if((str.split(" ", 2)[0]).equals("mark")) {
+            mark(lst.get(Integer.parseInt((str.split(" ", 2)[1])) - 1));
+            reply();
+        }
+        else if((str.split(" ", 2)[0]).equals("unmark")) {
+            unmark(lst.get(Integer.parseInt((str.split(" ", 2)[1])) - 1));
             reply();
         }
         else {
