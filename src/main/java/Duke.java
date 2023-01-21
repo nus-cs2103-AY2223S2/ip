2419203a9c7cef6d3047b2af2dfa44fa8d97aee3
Elvern <elvern18@gmail.com>
2023-01-22 03:25:57 +0800

    completed level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,22 @@
+import java.lang.reflect.Array;
+import java.util.*;
 public class Duke {
+
+    ArrayList<Task> list;
+
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+        Scanner sc = new Scanner(System.in);
+        String command = sc.nextLine();
+        while (!command.equals("bye")) {
+            System.out.println(command);
+            command = sc.nextLine();
+        }
+        System.out.println("Bye. Hope to see you again soon!");
     }
+
+
 }
+
+
