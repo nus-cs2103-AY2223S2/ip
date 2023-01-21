58056fe5c1fcc6219f8fbb699af57285961a05e0
hhchinh2002 <hhchinh2002@gmail.com>
2023-01-21 21:23:10 +0800

    Implement a skeletal version of the bot

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,32 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static final String DIVIDER_LINE = "____________________________________________________\n";
+
+    private static void displayIntro() {
+        System.out.println(reply("Hello! I'm Duke\n What can I do for you?"));
+    }
+
+    private static void displayOutro() {
+        System.out.println(reply("Bye. Hope to see you again soon!"));
+    }
+
+    private static String reply(String command) {
+        return DIVIDER_LINE + command + "\n" + DIVIDER_LINE;
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        displayIntro();
+
+        Scanner input = new Scanner(System.in);
+        String command = input.nextLine();
+
+        while (!command.equals("bye")) {
+            String reply = reply(command);
+            System.out.println(reply);
+            command = input.nextLine();
+        }
+
+        displayOutro();
     }
 }
