3c3620463d0d069c07f5918cd1c5bfd92bcf8b46
anunayajoshi <anunayajoshi@gmail.com>
2023-01-21 13:53:03 +0800

    Level 2: Add, list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,23 +1,63 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+
+    private final ArrayList<String> storage = new ArrayList<>();
+
+    private final Scanner sc = new Scanner(System.in);
+
+    public void greet() {
+        String logo = " ____        _        \n"
+        + "|  _ \\ _   _| | _____ \n"
+        + "| | | | | | | |/ / _ \\\n"
+        + "| |_| | |_| |   <  __/\n"
+        + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println(logo + "Hello! I'm Duke\nWhat can I do for you?");
+        separator();
+    }
+
+    public void exit() {
+        separator();
+        System.out.println("\tBye. Hope to see you again soon!");
+        separator();
+        System.exit(0);
+    }
+
+    public void list() {
+        separator();
+        for (int i=0; i < this.storage.size(); i++) {
+            int currentNumber = i+1;
+            String currentList = storage.get(i);
+            System.out.println("\t" + currentNumber + ". " + currentList);
+
+        }
+        separator();
+    }
+
+    public void add(String str) {
+        storage.add(str);
+        separator();
+        System.out.println("\tadded: " + str );
+        separator();
+    }
+
+    public static void separator() {
+        System.out.println("---------------------------------------------------------------");
+    }
+
+
     public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
-
-        Scanner sc = new Scanner(System.in);
-
-        while (true) {
-            String str = sc.nextLine();
+        Duke addressBook = new Duke();
+        addressBook.greet();
+        while (addressBook.sc.hasNextLine()) {
+            String str = addressBook.sc.nextLine();
             if (str.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
-                System.exit(0);
+                addressBook.exit();
+            } else if (str.equals("list")) {
+                addressBook.list();
             } else {
-                System.out.println(str);
+                addressBook.add(str);
             }
         }
     }
