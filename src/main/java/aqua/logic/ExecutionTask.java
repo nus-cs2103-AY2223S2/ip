5203013e75c9bfb4c05fc081dea55e375dcbfd54
daitenshionyan <snivy.oshowott@gmail.com>
2023-01-21 13:02:40 +0800

    ExecutionTask: create class

---
+++ src/main/java/aqua/logic/ExecutionTask.java
@@ -0,0 +1,29 @@
+package aqua.logic;
+
+import aqua.exception.IllegalSyntaxException;
+import aqua.exception.ProcedureExecutionException;
+import aqua.manager.AppManager;
+
+
+public abstract class ExecutionTask<T> {
+    private final ArgumentMap args;
+    private final AppManager manager;
+
+
+    public ExecutionTask(ArgumentMap args, AppManager manager) {
+        this.args = args;
+        this.manager= manager;
+    }
+
+
+    protected abstract T process(ArgumentMap args, AppManager manager)
+            throws IllegalSyntaxException, ProcedureExecutionException;
+
+    protected abstract String getDataDisplay(T data, AppManager manager);
+
+
+    public String execute() throws IllegalSyntaxException, ProcedureExecutionException {
+        T data = process(args, manager);
+        return getDataDisplay(data, manager);
+    }
+}
