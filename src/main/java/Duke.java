9d072e21d5859b43ddd3c50d8c10964c634bdd56
hhchinh2002 <hhchinh2002@gmail.com>
2023-01-21 23:03:37 +0800

    Add ability to store texts input and display back

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,18 +1,33 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     private static final String DIVIDER_LINE = "____________________________________________________\n";
+    private static ArrayList<String> tasks = new ArrayList<String>();
 
     private static void displayIntro() {
-        System.out.println(reply("Hello! I'm Duke\n What can I do for you?"));
+        System.out.println(reply("Hello! I'm Duke\n What can I do for you?" + "\n"));
     }
 
     private static void displayOutro() {
-        System.out.println(reply("Bye. Hope to see you again soon!"));
+        System.out.println(reply("Bye. Hope to see you again soon!" + "\n"));
+    }
+
+    private static void addTask(String task) {
+        tasks.add(task);
+        System.out.println(reply("added: " + task + "\n"));
+    }
+
+    private static void displayTasks() {
+        String list_of_tasks = "";
+        for (int i = 0; i < tasks.size(); i++) {
+            list_of_tasks += i + 1 + ". " + tasks.get(i) + "\n";
+        }
+        System.out.println(reply(list_of_tasks));
     }
 
     private static String reply(String command) {
-        return DIVIDER_LINE + command + "\n" + DIVIDER_LINE;
+        return DIVIDER_LINE + command + DIVIDER_LINE;
     }
 
     public static void main(String[] args) {
@@ -20,10 +35,15 @@ public class Duke {
 
         Scanner input = new Scanner(System.in);
         String command = input.nextLine();
-
         while (!command.equals("bye")) {
-            String reply = reply(command);
-            System.out.println(reply);
+            if (command.equals("list")) {
+                displayTasks();
+                command = input.nextLine();
+                continue;
+            }
+            if (!tasks.contains(command)) {
+                addTask(command);
+            }
             command = input.nextLine();
         }
 
