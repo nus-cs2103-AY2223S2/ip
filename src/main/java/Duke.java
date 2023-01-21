692ba50d623f4e3e6b22381bee886b6e2e7ab169
minosx31 <n_minghao28@hotmail.com>
2023-01-21 15:39:13 +0800

    Increment Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,12 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Duke {
     public static void main(String[] args) {
+        Pattern mark = Pattern.compile("(mark|unmark)([0-9])?");
+        Pattern index = Pattern.compile("(mark|unmark)?([0-9])");
         ArrayList<Task> tasks = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
 
@@ -16,16 +20,31 @@ public class Duke {
 
         while (true) {
             String input = sc.nextLine();
+            Matcher matchMark = mark.matcher(input);
+            Matcher matchIndex = index.matcher(input);
             if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again!");
                 break;
             } else if (input.equals("list")) {
+                System.out.println("Here are the tasks in your list");
                 if (tasks.size() != 0) {
                     for (int i=0; i < tasks.size(); i++) {
-                        System.out.println((i+1) + ". " + tasks.get(i).getDescription());
+                        System.out.println((i+1) + ".[" + tasks.get(i).getStatusIcon() + "] " + tasks.get(i).getDescription());
                     }
                 }
-            } else {
+            } else if (matchMark.find() && matchIndex.find()) {
+                Task currTask = tasks.get(Integer.parseInt(matchIndex.group())-1);
+                if (matchMark.group().equals("mark")) {
+                    currTask.markAsDone();
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println("[" + currTask.getStatusIcon() + "] " + currTask.getDescription());
+                } else if (matchMark.group().equals("unmark")) {
+                    currTask.markAsUndone();
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    System.out.println("[" + currTask.getStatusIcon() + "] " + currTask.getDescription());
+                }
+            }
+            else {
                 Task newTask = new Task(input);
                 tasks.add(newTask);
                 System.out.println(newTask.toString());
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,12 +1,26 @@
 public class Task {
     private String description;
+    private boolean isDone;
 
     public Task (String description) {
         this.description = description;
+        this.isDone = false;
     }
 
     public String getDescription() {
         return this.description;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void markAsUndone() {
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (this.isDone ? "X" : " ");
     }
 
     @Override
