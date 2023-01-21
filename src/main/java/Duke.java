d0be4ee5d02d84c16dfd481f2dc1a6f864e1607d
dawg420 <88711624+dawg420@users.noreply.github.com>
2023-01-21 17:18:51 +0800

    Level 4: Added Todos, Events, Deadlines

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -30,6 +30,47 @@ public class Duke {
         }
     }
 
+    public static class Todo extends Task {
+        public Todo(String description) {
+            super(description);
+        }
+
+        @Override
+        public String toString() {
+            return "[T]" + super.toString();
+        }
+    }
+
+    public static class Deadline extends Task {
+        protected String by;
+
+        public Deadline(String description, String by) {
+            super(description);
+            this.by = by;
+        }
+
+        @Override
+        public String toString() {
+            return "[D]" + super.toString() + " (by: " + by + ")";
+        }
+    }
+
+    public static class Event extends Task {
+        protected String from;
+        protected String to;
+
+        public Event(String description, String from, String to) {
+            super(description);
+            this.from = from;
+            this.to = to;
+        }
+
+        @Override
+        public String toString() {
+            return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
+        }
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -49,7 +90,13 @@ public class Duke {
             String showList = "list";
             String mark = "mark";
             String unmark = "unmark";
-            if (msg.contains(" ")) {
+            String todo = "todo";
+            String list = "list";
+            String event = "event";
+            String byWhen = "";
+            String fromWhen = "";
+            String toWhen = "";
+            if (msg.contains("mark") || msg.contains("unmark")) {
                 firstWord = msg.substring(0, msg.indexOf(" "));
                 try {
                     secondInt = Integer
@@ -57,6 +104,30 @@ public class Duke {
                 } catch (Exception ex) {
 
                 }
+            } else if (msg.contains("todo")) {
+                firstWord = msg.substring(msg.indexOf(" ") + 1, msg.length());
+                Todo newTodo = new Todo(firstWord);
+                listOfThings.add(newTodo);
+                System.out.println("Got it. I've added this task:");
+                System.out.println(newTodo);
+                System.out.println("Now you have " + listOfThings.size() + " tasks in the list.");
+            } else if (msg.contains("deadline")) {
+                firstWord = msg.substring(msg.indexOf(" ") + 1, msg.indexOf("/by") - 1);
+                byWhen = msg.substring(msg.indexOf("/by") + 4, msg.length());
+                Deadline newDeadline = new Deadline(firstWord, byWhen);
+                listOfThings.add(newDeadline);
+                System.out.println("Got it. I've added this task:");
+                System.out.println(newDeadline);
+                System.out.println("Now you have " + listOfThings.size() + " tasks in the list.");
+            } else if (msg.contains("event")) {
+                firstWord = msg.substring(msg.indexOf(" ") + 1, msg.indexOf("/from") - 1);
+                fromWhen = msg.substring(msg.indexOf("/from") + 6, msg.indexOf("/to") - 1);
+                toWhen = msg.substring(msg.indexOf("/to") + 4, msg.length());
+                Event newEvent = new Event(firstWord, fromWhen, toWhen);
+                listOfThings.add(newEvent);
+                System.out.println("Got it. I've added this task:");
+                System.out.println(newEvent);
+                System.out.println("Now you have " + listOfThings.size() + " tasks in the list.");
             }
             if (bye.equalsIgnoreCase(msg)) {
                 System.out.println("Bye. Hope to see Byou again soon!");
@@ -78,9 +149,6 @@ public class Duke {
                 System.out.println("OK, I've marked this task as not done yet:");
                 System.out.println(thisTask);
             } else {
-                Task newTask = new Task(msg);
-                listOfThings.add(newTask);
-                System.out.println("added " + msg);
             }
         }
     }
