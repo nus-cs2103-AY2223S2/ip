118ca375787b8ebda93f73bf7e27ca197199a904
daitenshionyan <snivy.oshowott@gmail.com>
2023-01-21 13:48:05 +0800

    Delete old commands

--- src/main/java/command/CommandFunction.java
+++
@@ -1,8 +0,0 @@
-package command;
-
-import aqua.exception.IllegalSyntaxException;
-
-@FunctionalInterface
-public interface CommandFunction {
-    public String apply(CommandInput input) throws IllegalSyntaxException;
-}
--- src/main/java/command/TaskCreator.java
+++
@@ -1,9 +0,0 @@
-package command;
-
-import aqua.exception.IllegalSyntaxException;
-import task.Task;
-
-@FunctionalInterface
-public interface TaskCreator {
-    public Task apply(CommandInput input) throws IllegalSyntaxException;
-}
