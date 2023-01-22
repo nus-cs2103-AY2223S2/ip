278b92dbca3e16115f3bc6c9a5fb829d1a280e26
Elijah Ku <69575234+Zeli0@users.noreply.github.com>
2023-01-22 22:46:52 +0800

    Implemented skeleton of Duke.java
    The chatbot planned to be made only prints out a logo.
    
    Let's implement the backbone of the chatbot that will be built upon later.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,41 @@
+import java.util.Scanner;
 public class Duke {
+    //@@author IceFire
+    //Reused from https://stackoverflow.com/questions/36514289
+    // with minor modifications
+    private static String dashedLine()
+    {
+        StringBuilder sb = new StringBuilder(90);
+        for (int i = 0; i < 90; i++) {
+            sb.append("-");
+        }
+        return sb.toString();
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
+        /*String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+                + "|____/ \\__,_|_|\\_\\___|\n";*/
+
+        Scanner userInput = new Scanner(System.in);
+        System.out.println(dashedLine());
+        System.out.println("Hi everyone, Dukethony Codetano here, the internet's busiest program; hope you're doing well. \n"
+                + "How can I help you?");
+        System.out.println(dashedLine());
+        while(true) {
+            String input = userInput.nextLine();
+            if (input.equals("bye")) {
+                System.out.println(dashedLine());
+                System.out.println("Tran \n" +
+                        "sition. Anthony Fantano, Code, Forever.");
+                System.out.println(dashedLine());
+                break;
+            }
+            System.out.println(dashedLine());
+            System.out.println(input);
+            System.out.println(dashedLine());
+        }
     }
 }
