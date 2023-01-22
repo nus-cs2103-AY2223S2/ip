21a64e765d9686ff549e49ade83600b2e83456ea
Ai Bo <bo.ai@u.nus.edu>
2023-01-22 22:38:23 +0800

    abstract out the command parsing routine to the duke class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,15 @@
+import java.io.FileNotFoundException;
 import java.util.Objects;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.io.File;
+import java.io.IOException;
+import java.io.FileWriter;
+import java.util.Scanner;
 
 
 public class Duke {
+    protected final String RECORD_PATH = "./data/duke.txt";
     protected final ArrayList<Task> tasks = new ArrayList<>();
     protected final String name;
 
@@ -11,16 +17,99 @@ public class Duke {
         this.name = "Duke";
     }
 
-    public void print_structured_string(String s) {
-        String longLine = "____________________________________________________________";
-        System.out.println(longLine + "\n" + s + "\n" + longLine);
+    /**
+     * Returns whether the input string is of the specified command type
+     * @param string: the input string from the user
+     * @param command: a candidate command to check against
+     * @return: whether the input string is of the specified command type
+     */
+    public boolean checkCommand(String string, Command command) {
+        boolean isCommand = false;
+        switch (command) {
+        case LIST:
+            isCommand = string.equalsIgnoreCase("list");
+            break;
+        case MARK:
+        case UNMARK:
+        case TODO:
+        case DEADLINE:
+        case EVENT:
+        case DELETE:
+            isCommand = string.toUpperCase().startsWith(command.name());
+            break;
+        }
+        return isCommand;
     }
 
-    public String listTasksMsg() {
-        String s = "Here are the tasks in your list:\n";
+    /**
+     * Gets the content of the command
+     * @param string: the command string
+     * @param command: a command type
+     * @return the content of the command
+     * @throws DukeException when the string is not complete
+     */
+    public String getCommandContent(String string, Command command) throws DukeException {
+        String commandString = command.name().toLowerCase();
+        if ((!commandString.equals("list")) && string.length() <= commandString.length() + 1) {
+            throw new DukeException("The command argument is not complete.");
+        }
+        return string.substring(string.indexOf(commandString) + commandString.length() + " ".length());
+    }
+
+    /**
+     * Handles the input string from the user
+     * @param inMsg: the input message from the user
+     * @throws DukeException when the command is unknown
+     */
+    public void handleCommand(String inMsg) throws DukeException {
+        if (checkCommand(inMsg,Command.LIST)) {
+            print_structured_string(listTasksMsg());
+        } else if (checkCommand(inMsg,Command.MARK)) {
+            int idx = Integer.parseInt(inMsg.substring(5)) - 1;
+            print_structured_string(markTaskDone(idx));
+        } else if (checkCommand(inMsg,Command.UNMARK)) {
+            int idx = Integer.parseInt(inMsg.substring(7)) - 1;
+            print_structured_string(unmarkTaskDone(idx));
+        } else if (checkCommand(inMsg, Command.TODO)){
+            String todoName = getCommandContent(inMsg, Command.TODO);
+            ToDo todo = new ToDo(todoName);
+            print_structured_string(addTask(todo));
+        } else if (checkCommand(inMsg, Command.DEADLINE)) {
+            String deadlineContent = getCommandContent(inMsg, Command.DEADLINE);
+            Deadline ddl = new Deadline(deadlineContent);
+            print_structured_string(addTask(ddl));
+        } else if (checkCommand(inMsg, Command.EVENT)) {
+            String eventContent = getCommandContent(inMsg, Command.EVENT);
+            Event event = new Event(eventContent);
+            print_structured_string(addTask(event));
+        } else if (checkCommand(inMsg, Command.DELETE)) {
+            String indexToDelete = getCommandContent(inMsg, Command.DELETE);
+            print_structured_string(deleteTask(Integer.parseInt(indexToDelete)));
+        } else {
+            throw new DukeException("  OOPS!!! I'm sorry, but I don't know what that means :-(");
+        }
+    }
+
+    /**
+     * Print the string in a pre-specified format
+     * @param string: the string content to print out
+     */
+    public void print_structured_string(String string) {
+        String longLine = "____________________________________________________________";
+        System.out.println(longLine + "\n" + string + "\n" + longLine);
+    }
+
+    /**
+     * Return the string representation of the task list
+     * @param isIndexed: whether to add an index number at the beginning of each task
+     *                 or not
+     * @return the string representation of the task list
+     */
+    public String getTaskListString(boolean isIndexed) {
+        String s = "";
         int count = 1;
         for (Task t: this.tasks) {
-            s += count + ". " + t;
+            s += (isIndexed ? (count + ". ") : "")  + t;
             if (count < tasks.size()) {
                 s += "\n";
             }
@@ -29,25 +118,51 @@ public class Duke {
         return s;
     }
 
+    /**
+     * Returns the string representation to be printed out when the command "list" is invoked
+     * @return the string representation of the message
+     */
+    public String listTasksMsg() {
+        String taskListString = "Here are the tasks in your list:\n" +
+                this.getTaskListString(true);
+        return taskListString;
+    }
+
+    /**
+     * The hello message at start-up
+     * @return the hello message
+     */
     public String greeting() {
         return String.format("Hello! I'm %s \nWhat can I do for you?", this.name);
     }
 
-    public String echo(String s) {
-        return s;
-    }
-
-    public String endMsg() {
+    /**
+     * Returns the end message and do final clean-up
+     * @return the bye-bye message to be printed out
+     */
+    public String endMsg() throws DukeException {
+        this.saveToFile(this.RECORD_PATH);
         return "Bye. Hope to see you again soon!";
     }
 
-    public boolean isEnd(String s) {
-        return Objects.equals(s.toLowerCase(), "bye");
+    /**
+     * Checks if the command marks the end
+     * @param string: User-input string
+     * @return if the string marks the end of the conversation
+     */
+    public boolean isEnd(String string) {
+        return Objects.equals(string.toLowerCase(), "bye");
     }
 
-    public String addTask(Task t) {
-        this.tasks.add(t);
-        return String.format("Got it. I've added this task:\n  %s\nNow you have %d tasks in the list.", t, tasks.size());
+    /**
+     * Add a task to the list
+     * @param task: a task to add
+     * @return the string response after adding a task
+     */
+    public String addTask(Task task) {
+        this.tasks.add(task);
+        return String.format("Got it. I've added this task:\n  %s\nNow you have %d tasks in the list.",
+                task, tasks.size());
     }
 
     public void markTaskDone(String s) {
@@ -88,5 +203,41 @@ public class Duke {
         Task t = tasks.get(idx);
         tasks.remove(idx);
         return String.format("Noted. I've removed this task:\n  %s\nNow you have %d tasks in the list.", t, tasks.size());
+    }
+
+    /**
+     * Saves the list of tasks to a file
+     * @param path: the path of the file to save to
+     */
+    public void saveToFile(String path) throws DukeException {
+        String taskListString = this.getTaskListString(false); // no index for file
+        // https://www.w3schools.com/java/java_files_create.asp
+        try {
+            File file = new File(path);
+            if (!file.createNewFile()) {
+                throw new DukeException("File already exists at " + path);
+            }
+            FileWriter myWriter = new FileWriter(path);
+            myWriter.write(taskListString);
+            myWriter.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+    public void loadUpRecordIfExists(String path) {
+        try {
+            File file = new File("filename.txt");
+            Scanner myReader = new Scanner(file);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                System.out.println(data);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,34 +17,8 @@ public class Main {
             if (duke.isEnd(inMsg)) {
                 break;
             }
-
             try {
-                if (checkCommand(inMsg,Command.LIST)) {
-                    duke.print_structured_string(duke.listTasksMsg());
-                } else if (checkCommand(inMsg,Command.MARK)) {
-                    int idx = Integer.parseInt(inMsg.substring(5)) - 1;
-                    duke.print_structured_string(duke.markTaskDone(idx));
-                } else if (checkCommand(inMsg,Command.UNMARK)) {
-                    int idx = Integer.parseInt(inMsg.substring(7)) - 1;
-                    duke.print_structured_string(duke.unmarkTaskDone(idx));
-                } else if (checkCommand(inMsg, Command.TODO)){
-                    String todoName = getCommandContent(inMsg, Command.TODO);
-                    ToDo todo = new ToDo(todoName);
-                    duke.print_structured_string(duke.addTask(todo));
-                } else if (checkCommand(inMsg, Command.DEADLINE)) {
-                    String deadlineContent = getCommandContent(inMsg, Command.DEADLINE);
-                    Deadline ddl = new Deadline(deadlineContent);
-                    duke.print_structured_string(duke.addTask(ddl));
-                } else if (checkCommand(inMsg, Command.EVENT)) {
-                    String eventContent = getCommandContent(inMsg, Command.EVENT);
-                    Event event = new Event(eventContent);
-                    duke.print_structured_string(duke.addTask(event));
-                } else if (checkCommand(inMsg, Command.DELETE)) {
-                    String indexToDelete = getCommandContent(inMsg, Command.DELETE);
-                    duke.print_structured_string(duke.deleteTask(Integer.parseInt(indexToDelete)));
-                } else {
-                    throw new DukeException("  OOPS!!! I'm sorry, but I don't know what that means :-(");
-                }
+                duke.handleCommand(inMsg);
             } catch (DukeException e) {
                 duke.print_structured_string(e.toString());
             } catch (NumberFormatException e) {
@@ -52,34 +26,11 @@ public class Main {
             }
         }
 
-        // bye-bye message
-        duke.print_structured_string(duke.endMsg());
-    }
-
-    
-    public static boolean checkCommand(String s, Command c) {
-        boolean isCommand = false;
-        switch (c) {
-            case LIST:
-                isCommand = s.equalsIgnoreCase("list");
-                break;
-            case MARK:
-            case UNMARK:
-            case TODO:
-            case DEADLINE:
-            case EVENT:
-            case DELETE:
-                isCommand = s.toUpperCase().startsWith(c.name());
-                break;
+        try {
+            // bye-bye message
+            duke.print_structured_string(duke.endMsg());
+        } catch (DukeException e) {
+            duke.print_structured_string(e.toString());
         }
-        return isCommand;
-    }
-
-    public static String getCommandContent(String s, Command c) throws DukeException {
-        String commandString = c.name().toLowerCase();
-        if ((!commandString.equals("list")) && s.length() <= commandString.length() + 1) {
-            throw new DukeException("The command argument is not complete.");
-        }
-        return s.substring(s.indexOf(commandString) + commandString.length() + " ".length());
     }
 }
