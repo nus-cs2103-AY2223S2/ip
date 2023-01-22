3c77221730f2ec17f3864a1b13089efbe3074055
c0j0s <c0j0s@outlook.com>
2023-01-23 01:45:46 +0800

    Add initial gui for duke

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -3,6 +3,16 @@ package duke;
 import java.io.InputStream;
 import java.io.PrintStream;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
 import duke.command.Command;
 import duke.database.DukeRepo;
 import duke.database.DukeRepoImpl;
@@ -13,7 +23,7 @@ import duke.ui.Ui;
 /**
  * Duke agent that knows how to manage a todo list.
  */
-public class Duke {
+public class Duke extends Application{
 
     private DukeRepo db;
     private Ui ui;
@@ -21,7 +31,7 @@ public class Duke {
     /**
      * Default constructor.
      */
-    Duke() {
+    public Duke() {
         db = new DukeRepoImpl();
         ui = new Ui();
     }
@@ -29,7 +39,7 @@ public class Duke {
     /**
      * Constructor for testing
      */
-    Duke(InputStream in, PrintStream out) {
+    public Duke(InputStream in, PrintStream out) {
         db = new DukeRepoImpl();
         ui = new Ui(in, out);
     }
@@ -55,6 +65,70 @@ public class Duke {
         }
     }
 
+    /**
+     * Entry point for JavaFx
+     */
+    @Override
+    public void start(Stage stage) {
+        //Step 1. Setting up required components
+        ScrollPane scrollPane;
+        VBox dialogContainer;
+        TextField userInput;
+        Button sendButton;
+        Scene scene;
+
+        //The container for the content of the chat to scroll.
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+
+        userInput = new TextField();
+        sendButton = new Button("Send");
+
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+        scene = new Scene(mainLayout);
+
+        stage.setScene(scene);
+        stage.show();
+
+        //Step 2. Formatting the window to look as expected
+        stage.setTitle("Duke");
+        stage.setResizable(false);
+        stage.setMinHeight(600.0);
+        stage.setMinWidth(400.0);
+
+        mainLayout.setPrefSize(400.0, 600.0);
+
+        scrollPane.setPrefSize(385, 535);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
+
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+
+        userInput.setPrefWidth(325.0);
+
+        sendButton.setPrefWidth(55.0);
+
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+
+        AnchorPane.setLeftAnchor(userInput , 1.0);
+        AnchorPane.setBottomAnchor(userInput, 1.0);
+        
+    }
+
+    /**
+     * Main entry
+     * 
+     * @param args
+     */
     public static void main(String[] args) {
         new Duke().run();
     }
