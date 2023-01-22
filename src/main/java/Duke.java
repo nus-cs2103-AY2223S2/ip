0d7812fe2b36ba4c3b9cf5465ac7670dccc13311
Yaladah <jeromeseet@hotmail.com>
2023-01-22 14:09:55 +0800

    feature: Added function for chatbot to echo the user inputs, and to exit the process when 'bye' is input.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,34 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static String projName = " ____        _        \n"
+                                    + "|  _ \\ _   _| | _____ \n"
+                                    + "| | | | | | | |/ / _ \\\n"
+                                    + "| |_| | |_| |   <  __/\n"
+                                    + "|____/ \\__,_|_|\\_\\___|\n";
+
+    public boolean readInput(String input) {
+        if (input.equals("bye")) {
+            System.out.println("It was a pleasure to help, goodbye!");
+            return false;
+        } else {
+            System.out.println(input);
+            return true;
+        }
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Yo! The name is\n" + projName);
+        System.out.println("How might I help you today?");
+        Scanner scanner = new Scanner(System.in);
+        Duke duke = new Duke();
+
+        boolean cont = true;
+
+        while (cont) {
+            System.out.println("\nQuery:");
+            String input = scanner.nextLine();
+            cont = duke.readInput(input);
+        }
     }
 }
