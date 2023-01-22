91f7e749a8ac387dde62ca90edd81b0f8b2e1b78
WillCWX <xuanwilliam@yahoo.com>
2023-01-22 23:13:11 +0800

    Adds gradle project manager

---
+++ src/test/java/duke/task/ToDoTest.java
@@ -0,0 +1,18 @@
+package duke.task;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class ToDoTest {
+
+    @Test
+    public void dummyTest() {
+        assertEquals(2, 2);
+    }
+
+    @Test
+    public void anotherDummyTest() {
+        assertEquals(4, 4);
+    }
+}
