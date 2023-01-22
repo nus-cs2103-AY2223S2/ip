46fa13026bc4a8f4b4e6ee4bbf2c5e63604582a4
noahxinjie <noahxinjie@gmail.com>
2023-01-22 17:41:42 +0800

    Level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,34 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Duke {
+    private static Task parse(String[] inputs) {
+        String taskType = inputs[0];
+        String rest = inputs[1];
+        Task task;
+
+        if (taskType.equals("todo")) {
+            task = new Todo(rest);
+        } else if (taskType.equals("deadline")) {
+            String[] addon = rest.split(" /", 2);
+            String description = addon[0];
+            String byString = addon[1];
+            String[] byPart = byString.split(" ", 2);
+            String by = byPart[1];
+            task = new Deadline(description, by);
+        } else {
+            String[] addon = rest.split(" /", 3);
+            String description = addon[0];
+            String fromString = addon[1];
+            String[] fromPart = fromString.split(" ", 2);
+            String from = fromPart[1];
+            String toString = addon[2];
+            String[] toPart = toString.split(" ", 2);
+            String to = toPart[1];
+            task = new Event(description, from, to);
+        }
+        return task;
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello! I'm Chiwa, your personal chatbot (◔◡◔✿)");
         System.out.println("What can I do for you today?");
@@ -51,9 +79,12 @@ public class Duke {
                     }
                     break;
                 default:  // adding new task
-                    Task task = new Task(input);
-                    tasks.add(task);
-                    System.out.println("task added: " + input + "\n");
+                    // Have 3 types of tasks: todo, deadline and event
+                    Task t = Duke.parse(inputs);
+                    tasks.add(t);
+                    System.out.println("Ok, I've added this task:");
+                    System.out.println(t);
+                    System.out.printf("You now have %d task(s) in your list!\n", tasks.size());
                     break;
             }
         }
