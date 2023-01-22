2d74717ffedc47ec2216b376646394cdc9ff9cf3
shenchenzi <shenchenzizoe@outlook.com>
2023-01-22 18:49:19 +0800

    finish level 4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -24,11 +24,34 @@ public class Duke {
                 System.out.println("Here are the tasks in your list:");
                 for(int i = 0; i < index; i++) {
                     if(!tasks[i].mark) {
-                        System.out.print(i + 1 + "." + "[ ] ");
+                        if(tasks[i].todo) {
+                            System.out.print(i + 1 + "." + "[T][ ] ");
+                        }
+                        if(tasks[i].deadline) {
+                            System.out.print(i + 1 + "." + "[D][ ] ");
+                        }
+                        if(tasks[i].event) {
+                            System.out.print(i + 1 + "." + "[E][ ] ");
+                        }
                     } else {
-                        System.out.print(i + 1 + "." + "[X] ");
+                        if(tasks[i].todo) {
+                            System.out.print(i + 1 + "." + "[T][X] ");
+                        }
+                        if(tasks[i].deadline) {
+                            System.out.print(i + 1 + "." + "[D][X] ");
+                        }
+                        if(tasks[i].event) {
+                            System.out.print(i + 1 + "." + "[E][X] ");
+                        }
                     }
-                    System.out.println(tasks[i].string);
+                    if(tasks[i].deadline) {
+                        System.out.println(tasks[i].string + " (by: " + tasks[i].time + ")");
+                    } else if(tasks[i].event) {
+                        System.out.println(tasks[i].string + " (from: "
+                            + tasks[i].startTime + " to: " + tasks[i].endTime + ")");
+                    } else {
+                        System.out.println(tasks[i].string);
+                    }
                 }
                 System.out.println("---------------------------");
             } else if(strArr[0].equals("mark")) {
@@ -37,7 +60,24 @@ public class Duke {
                 int curIndex = Integer.parseInt(strArr[1]) - 1;
                 tasks[curIndex].mark();
                 String curTask = tasks[curIndex].string;
-                System.out.println("[X] " + curTask);
+                if(tasks[curIndex].deadline) {
+                    System.out.print("[D]");
+                }
+                if(tasks[curIndex].todo) {
+                    System.out.print("[T]");
+                }
+                if(tasks[curIndex].event) {
+                    System.out.print("[E]");
+                }
+                System.out.print("[X] " + curTask);
+                if(tasks[curIndex].deadline) {
+                    System.out.println( " (by: " + tasks[curIndex].time + ")");
+                } else if (tasks[curIndex].event){
+                    System.out.println(" (from: " + tasks[curIndex].startTime
+                        + " to: " + tasks[curIndex].endTime + ")");
+                } else {
+                    System.out.println();
+                }
                 System.out.println("---------------------------");
             } else if(strArr[0].equals("unmark")) {
                 System.out.println("---------------------------");
@@ -45,15 +85,96 @@ public class Duke {
                 int curIndex = Integer.parseInt(strArr[1]) - 1;
                 tasks[curIndex].demark();
                 String curTask = tasks[curIndex].string;
-                System.out.println("[ ]" + curTask);
+                if(tasks[curIndex].deadline) {
+                    System.out.print("[D]");
+                }
+                if(tasks[curIndex].todo) {
+                    System.out.print("[T]");
+                }
+                if(tasks[curIndex].event) {
+                    System.out.print("[E]");
+                }
+                System.out.print("[ ]" + curTask);
+                if(tasks[curIndex].deadline) {
+                    System.out.println( " (by: " + tasks[curIndex].time + ")");
+                } else if (tasks[curIndex].event){
+                    System.out.println(" (from: " + tasks[curIndex].startTime
+                        + " to: " + tasks[curIndex].endTime + ")");
+                } else {
+                    System.out.println();
+                }
                 System.out.println("---------------------------");
             }
             else {
-                System.out.println("---------------------------");
-                System.out.println("add: " + str);
-                System.out.println("---------------------------");
-                tasks[index] =new Task(str);
-                index++;
+                if(strArr[0].equals("todo")) {
+                    System.out.println("---------------------------");
+                    System.out.println("Got it. I've added this task:");
+                    StringBuilder curStr = new StringBuilder();
+                    for(int i = 1; i < strArr.length; i++) {
+                        curStr.append(" ").append(strArr[i]);
+                    }
+                    System.out.println(" [T][ ] " + curStr);
+                    tasks[index] =new Task(curStr.toString());
+                    tasks[index].todo();
+                    index++;
+                    System.out.println("Now you have " + index + " tasks in the list.");
+                    System.out.println("---------------------------");
+                } else if(strArr[0].equals("deadline")) {
+                    System.out.println("---------------------------");
+                    System.out.println("Got it. I've added this task:");
+                    String[] strArr1 = str.split("/");
+                    String timeWithBy = strArr1[strArr1.length-1];
+                    String[] strArr2 = timeWithBy.split(" ");
+                    StringBuilder sbTime = new StringBuilder();
+                    for(int i = 1; i < strArr2.length; i++) {
+                        sbTime.append(" ").append(strArr2[i]);
+                    }
+                    String time = sbTime.toString();
+                    String[] strArr3 = strArr1[0].split(" ");
+                    StringBuilder task = new StringBuilder();
+                    for(int i = 1; i < strArr3.length; i++) {
+                        task.append(" ").append(strArr3[i]);
+                    }
+                    System.out.println(" [D][ ] " + task + " (by: " + time + ")");
+                    String curTask = task.toString();
+                    tasks[index] =new Task(curTask);
+                    tasks[index].deadline();
+                    tasks[index].setTime(time);
+                    index++;
+                    System.out.println("Now you have " + index + " tasks in the list.");
+                    System.out.println("---------------------------");
+                } else if (strArr[0].equals("event")) {
+                    System.out.println("---------------------------");
+                    System.out.println("Got it. I've added this task:");
+                    String[] strArr1 = str.split("/");
+                    String timeWithFrom = strArr1[strArr1.length-2];
+                    String timeWithTo = strArr1[strArr1.length-1];
+                    String[] strArr2 = timeWithFrom.split(" ");
+                    StringBuilder sbTime = new StringBuilder();
+                    for(int i = 1; i < strArr2.length; i++) {
+                        sbTime.append(" ").append(strArr2[i]);
+                    }
+                    String[] strArr3 = timeWithTo.split(" ");
+                    StringBuilder sbTime1 = new StringBuilder();
+                    for(int i = 1; i < strArr3.length; i++) {
+                        sbTime1.append(" ").append(strArr3[i]);
+                    }
+                    String startTime = sbTime.toString();
+                    String endTime = sbTime1.toString();
+                    String[] strArr4 = strArr1[0].split(" ");
+                    StringBuilder task = new StringBuilder();
+                    for(int i = 1; i < strArr4.length; i++) {
+                        task.append(" ").append(strArr4[i]);
+                    }
+                    System.out.println(" [E][ ] " + task + " (from: " + startTime + " to: " + endTime + ")");
+                    String curTask = task.toString();
+                    tasks[index] =new Task(curTask);
+                    tasks[index].event();
+                    tasks[index].setTime(startTime, endTime);
+                    index++;
+                    System.out.println("Now you have " + index + " tasks in the list.");
+                    System.out.println("---------------------------");
+                }
             }
             str = br.readLine();
             strArr = str.split(" ");
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,14 +1,43 @@
 public class Task {
   public String string;
   public Boolean mark;
+  public Boolean todo;
+  public Boolean deadline;
+  public Boolean event;
+  public String time;
+  public String startTime;
+  public String endTime;
+
   public Task(String string) {
     this.string = string;
     this.mark = false;
+    this.todo =false;
+    this.deadline = false;
+    this.event = false;
   }
   public void mark() {
     this.mark = true;
   }
-  public void demark(){
+  public void demark() {
     this.mark = false;
+  }
+  public void todo() {
+    this.todo = true;
+  }
+  public void deadline() {
+    this.deadline = true;
+  }
+
+  public void event() {
+    this.event = true;
+  }
+
+  public void setTime(String time) {
+    this.time = time;
+  }
+
+  public void setTime(String starTime, String endTime) {
+    this.startTime = starTime;
+    this.endTime = endTime;
   }
 }
