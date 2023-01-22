0f5cef15cf8ce2a2b55554c1d7b438a51ee6886d
Carmen <carmenang12@gmail.com>
2023-01-22 17:26:47 +0800

    added list function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +9,36 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        talk("Meow I'm Toto! What can I do for you?");
+        ArrayList<String> arrayList = new ArrayList<>(100);
+        echo(arrayList);
+
+    }
+
+    public static void talk(String s) {
+        System.out.println(s);
+    }
+
+    public static void echo(ArrayList<String> arrayList) {
+        Scanner scanner = new Scanner(System.in);
+        String answer = scanner.nextLine();
+        if (answer.equals("bye")) {
+            System.out.println("Byebye see you again!");
+            return;
+        }
+        if (answer.equals("list")) {
+            list(arrayList);
+            return;
+        }
+            System.out.println("added: " + answer);
+            arrayList.add(answer);
+            echo(arrayList);
+    }
+
+    public static void list(ArrayList<String> arrayList) {
+        for (int i = 1; i < arrayList.size() + 1; i++) {
+            System.out.println(i + ". " + arrayList.get(i - 1));
+        }
+        echo(arrayList);
     }
 }
--- src/main/java/List.java
+++ src/main/java/List.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class List {
+import java.util.ArrayList;
+public class List {
+
+    private ArrayList<String> list;
+
+    List() {
+        list = new ArrayList<>();
+    }
+    public String listOut() {
+        return "ogay";
+
+    }
+
+    public void add(String s) {
+        list.add(s);
+    }
 }
