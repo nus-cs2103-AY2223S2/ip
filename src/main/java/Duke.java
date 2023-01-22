6efd03c915da83d1c85b0fdb12be3ed850d939bd
antonlee59 <92142956+antonlee59@users.noreply.github.com>
2023-01-22 13:19:05 +0800

    Level 1. Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,21 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String EXIT_COMMAND = "bye";
+        Scanner user_input = new Scanner(System.in);
+
+        System.out.println("Hello! I'm Anton's Bot");
+        System.out.println("What can I do for you?");
+
+        while (true){
+            String current_input = user_input.next();
+            if (current_input.equals(EXIT_COMMAND)) {
+                System.out.println("        Bye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println("        " + current_input);
+            }
+        }
+        user_input.close();
     }
 }
