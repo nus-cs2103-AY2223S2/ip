7bb3b3d8f56abb21fc33c5d631ea36f061a1ef67
Carmen <carmenang12@gmail.com>
2023-01-22 18:46:18 +0800

    added Task class and can mark/unmark tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,7 +10,7 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         talk("Meow I'm Toto! What can I do for you?");
-        ArrayList<String> arrayList = new ArrayList<>(100);
+        ArrayList<Task> arrayList = new ArrayList<>(100);
         echo(arrayList);
 
     }
@@ -19,7 +19,7 @@ public class Duke {
         System.out.println(s);
     }
 
-    public static void echo(ArrayList<String> arrayList) {
+    public static void echo(ArrayList<Task> arrayList) {
         Scanner scanner = new Scanner(System.in);
         String answer = scanner.nextLine();
         if (answer.equals("bye")) {
@@ -30,12 +30,27 @@ public class Duke {
             list(arrayList);
             return;
         }
+        if (answer.startsWith("mark ")) {
+            int index = Integer.valueOf(answer.substring(5, answer.length()));
+            arrayList.get(index - 1).markAsDone();
+            System.out.println("I've marked this task as done: " + arrayList.get(index - 1));
+            echo(arrayList);
+            return;
+        }
+        if (answer.startsWith("unmark ")) {
+            int index = Integer.valueOf(answer.substring(7, answer.length()));
+            arrayList.get(index - 1).markAsUndone();
+            System.out.println("I've marked this task as not done yet: " + arrayList.get(index - 1 ));
+            echo(arrayList);
+            return;
+        }
             System.out.println("added: " + answer);
-            arrayList.add(answer);
+            Task t = new Task(answer);
+            arrayList.add(t);
             echo(arrayList);
     }
 
-    public static void list(ArrayList<String> arrayList) {
+    public static void list(ArrayList<Task> arrayList) {
         for (int i = 1; i < arrayList.size() + 1; i++) {
             System.out.println(i + ". " + arrayList.get(i - 1));
         }
