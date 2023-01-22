d6db482bafba939c3b884218c379a350dfb1a860
Vaidyanaath Suresh <svaidyanaath@gmail.com>
2023-01-23 01:15:54 +0800

    Add error handling for user input

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -27,135 +27,188 @@ public class Duke {
         System.out.println(exitMessage);
     }
 
-    private static void addToDo(String task) {
-        if (task.equals("")) {
-            System.out.println("Invalid command format!");
-            System.out.println("Use: `todo {description}`");
+    private static void addToDo(String userCommand) {
+        String[] userCommandParts = userCommand.split(" ", 2);
+        if (userCommandParts.length < 2) {
+            System.out.println("Please enter task description");
             return;
         }
 
-        ToDo toDo = new ToDo(task);
+        String taskDescription = userCommandParts[1].trim();
+        ToDo toDo = new ToDo(taskDescription);
         TASK_LIST.add(toDo);
         nTasks++;
-        System.out.println("Got it. I've added this task:\n" + toDo
+        System.out.println("Got it. I've added this task:\n\t" + toDo
                 + "\nNow you have " + nTasks + " task(s) in the list.");
     }
 
-    private static void addDeadline(String task) {
-        String[] taskParts = task.split("/by");
-        if (taskParts.length < 2 || taskParts[0].equals("")) {
-            System.out.println("Invalid command format!");
+    private static void addDeadline(String userCommand) {
+
+        String[] userCommandParts = userCommand.split(" /by", 2);
+        String description = userCommandParts[0].replace("deadline", "").trim();
+
+        if (description.isEmpty()) {
+            System.out.println("Please enter task description!");
+            return;
+        }
+
+        if (userCommandParts.length < 2) {
+            System.out.println("Invalid format!");
             System.out.println("Use `deadline {description} /by {due date}`");
             return;
         }
-        String description = taskParts[0].trim();
-        String dueDate = taskParts[1].trim();
 
-        if (dueDate.equals("")) {
-            System.out.println("Please enter a valid due date!");
+        if (userCommandParts[1].trim().isEmpty()) {
+            System.out.println("Please provide due date after `/by` parameter");
             return;
         }
 
+        String dueDate = userCommandParts[1].trim();
         Deadline deadline = new Deadline(description, dueDate);
         TASK_LIST.add(deadline);
         nTasks++;
 
-        System.out.println("Got it. I've added this task:\n" + deadline
+        System.out.println("Got it. I've added this task:\n\t" + deadline
                 + "\nNow you have " + nTasks + " task(s) in the list.");
     }
 
-    private static void addEvent(String task) {
-        String[] taskParts = task.split("/from");
-        if (taskParts.length < 2 || taskParts[1].equals("")) {
-            System.out.println("Invalid command format!");
-            System.out.println("Use `event {description} /from {start date/time} /to {end date/time}`");
+    private static void addEvent(String userCommand) {
+
+        String[] userCommandParts = userCommand.split(" /from", 2);
+        String description = userCommandParts[0].replace("event", "").trim();
+
+        if (description.isEmpty()) {
+            System.out.println("Please enter task description!");
             return;
         }
-        String[] taskTimeParts = taskParts[1].split("/to");
-        if (taskTimeParts.length < 2) {
-            System.out.println("Invalid command format!");
+
+        if (userCommandParts.length < 2) {
+            System.out.println("Invalid format!");
             System.out.println("Use `event {description} /from {start date/time} /to {end date/time}`");
             return;
         }
 
-        String description = taskParts[0].trim();
-        String startDateTime = taskTimeParts[0].trim();
-        String endDateTime = taskTimeParts[1].trim();
+        String[] timeParts = userCommandParts[1].split(" /to", 2);
 
-        if (description.equals("") || startDateTime.equals("") || endDateTime.equals("")) {
-            System.out.println("Please enter valid description/ start time/ end time!");
+        if (timeParts.length < 1 || timeParts[0].trim().isEmpty()) {
+            System.out.println("Please provide start date/time after `/from` parameter");
             return;
         }
+
+        if (timeParts.length < 2 || timeParts[1].trim().isEmpty()) {
+            System.out.println("Please provide end date/time after `/to` parameter");
+            return;
+        }
+
+        String startDateTime = timeParts[0].trim();
+        String endDateTime = timeParts[1].trim();
 
         Event event = new Event(description, startDateTime, endDateTime);
         TASK_LIST.add(event);
         nTasks++;
 
-        System.out.println("Got it. I've added this task:\n" + event
+        System.out.println("Got it. I've added this task:\n\t" + event
                 + "\nNow you have " + nTasks + " task(s) in the list.");
     }
 
-    private static void addTask(String userCommand) {
-        String[] userCommandParts = userCommand.split(" ", 2);
-        if (userCommandParts.length < 2) {
-            System.out.println("Invalid command format!");
-            System.out.println("Use: `{todo/deadline/event} {description} {...args}`");
-            return;
+    private static void printTaskList() {
+        if (nTasks == 0) {
+            System.out.println("There are no tasks in your Task List!");
+        } else {
+            System.out.println("Your Tasks:");
+            for (int i = 0; i < nTasks; i++) {
+                System.out.println((i + 1) + ". " + TASK_LIST.get(i));
+            }
         }
+        System.out.println();
+    }
 
-        String command = userCommandParts[0];
-        String description = userCommandParts[1];
+    private static void markTask(String userCommand) {
+        try {
+            String[] userCommandParts = userCommand.split(" ");
+            if (userCommandParts.length != 2) {
+                System.out.println("Use format: mark {task no.}");
+                return;
+            }
 
-        switch (command) {
-            case "todo":
-                addToDo(description);
-                break;
-            case "deadline":
-                addDeadline(description);
-                break;
-            case "event":
-                addEvent(description);
-                break;
-            default:
-                System.out.println("Please enter a valid command");
-                break;
+            String taskNumber = userCommandParts[1];
+            int taskIndex = Integer.parseInt(taskNumber) - 1;
+
+            if (nTasks == 0) {
+                System.out.println("There are no tasks in your Task List!");
+            } else if (0 <= taskIndex && taskIndex < nTasks) {
+                TASK_LIST.get(taskIndex).setDone();
+            } else {
+                System.out.println("Please provide a valid Task number");
+                System.out.println("You have " + nTasks + " task(s) in your Task List");
+            }
+
+        } catch (NumberFormatException exception) {
+            System.out.println("Please provide a valid Task number");
         }
     }
 
-    private static void printTaskList() {
-        System.out.println("Your Tasks:");
-        for (int i = 0; i < nTasks; i++) {
-            System.out.println((i + 1) + ". " + TASK_LIST.get(i));
+    private static void unmarkTask(String userCommand) {
+        try {
+            String[] userCommandParts = userCommand.split(" ");
+            if (userCommandParts.length != 2) {
+                System.out.println("Use format: unmark {task no.}");
+                return;
+            }
+
+            String taskNumber = userCommandParts[1];
+            int taskIndex = Integer.parseInt(taskNumber) - 1;
+
+            if (nTasks == 0) {
+                System.out.println("There are no tasks in your Task List!");
+            } else if (0 <= taskIndex && taskIndex < nTasks) {
+                TASK_LIST.get(taskIndex).setNotDone();
+            } else {
+                System.out.println("Please provide a valid Task number");
+                System.out.println("You have " + nTasks + " task(s) in your Task List");
+            }
+
+        } catch (NumberFormatException exception) {
+            System.out.println("Please provide a valid Task number");
         }
+
     }
 
     private static void processCommand(String userCommand) {
 
-        String[] userCommandParts = userCommand.split(" ");
-        String command = userCommandParts[0];
+        if (userCommand.isEmpty()) {
+            System.out.println("Please enter a command");
+            return;
+        }
 
-        if (userCommand.equals("")) {
-            System.out.println("Please enter a valid command");
+        String command = userCommand.split(" ", 2)[0];
 
-        } else if (userCommand.equals("list")) {
+        // Single word commands
+        if (userCommand.equals("list")) {
             printTaskList();
+            return;
+        }
 
-        } else if (command.equals("mark") && userCommandParts.length == 2) {
-            String taskNumber = userCommandParts[1];
-            int taskIndex = Integer.parseInt(taskNumber) - 1; // handle NumberFormatException
-            if (0 <= taskIndex && taskIndex < nTasks) {
-                TASK_LIST.get(taskIndex).setDone();
-            }
-
-        } else if (command.equals("unmark") && userCommandParts.length == 2) {
-            String taskNumber = userCommandParts[1];
-            int taskIndex = Integer.parseInt(taskNumber) - 1; // handle NumberFormatException
-            if (0 <= taskIndex && taskIndex < nTasks) {
-                TASK_LIST.get(taskIndex).setNotDone();
-            }
-
-        } else {
-            addTask(userCommand);
+        // Multi-word commands
+        switch (command) {
+            case "todo":
+                addToDo(userCommand);
+                break;
+            case "deadline":
+                addDeadline(userCommand);
+                break;
+            case "event":
+                addEvent(userCommand);
+                break;
+            case "mark":
+                markTask(userCommand);
+                break;
+            case "unmark":
+                unmarkTask(userCommand);
+                break;
+            default:
+                System.out.println("beep...boop... unrecognized command!");
+                break;
         }
 
         System.out.print("\n");
@@ -163,11 +216,10 @@ public class Duke {
 
     public static void start() {
         Duke.greet();
-
         Scanner scanner = new Scanner(System.in);
         while (true) {
             System.out.print("$ ");
-            String input = scanner.nextLine();
+            String input = scanner.nextLine().trim();
             if (input.equals("bye")) {
                 Duke.exit();
                 break;
