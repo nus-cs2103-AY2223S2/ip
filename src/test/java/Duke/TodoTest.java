0ed43d95665ea6f559f8980e830a4c50ade3b95c
yitong <ssunyitong@gmail.com>
2023-01-22 18:50:31 +0800

    add JUnit Test

---
+++ src/test/java/Duke/TodoTest.java
@@ -0,0 +1,20 @@
+package Duke;
+
+import Duke.Tasks.Todo;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+public class TodoTest {
+    @Test
+    public void markTest() {
+        Todo t = new Todo("Sleep", false);
+        assertEquals(t.mark().toString(), "[T][X] Sleep");
+    }
+
+    @Test
+    public void reformatTest() {
+        Todo t = new Todo("Revise CS2103", false);
+        assertEquals(t.reformat(), "T | 0 | Revise CS2103");
+    }
+
+}
