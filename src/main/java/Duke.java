7a9aa2ab2f4efde743a6474b56b5967833c2ade4
TimothyLawSongEn <e0725564@u.nus.edu>
2023-01-23 11:15:14 +0800

    Add deleting of tasks from list.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,16 +6,17 @@ import Tasks.Event;
 import Tasks.Task;
 import Tasks.ToDo;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) throws EmptyCommandException, InvalidCommandException, EmptyArgumentException {
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
 
-        Task[] arr = new Task[100];
-        int nextIndex = 0;
+        ArrayList<Task> arrL = new ArrayList<>();
+
+        Scanner bucky = new Scanner(System. in);
 
         while (true) {
-            Scanner bucky = new Scanner(System. in);
             String str = bucky.nextLine();
 
             if (str.equals("")) { throw new EmptyCommandException();}
@@ -27,34 +28,42 @@ public class Duke {
                 System.out.println ("Bye. Hope to see you again soon!");
                 break;
             } else if (str.equals("list")) {
-                for (int i = 0 ; i < nextIndex ; i++) {
-                    System.out.println((i+1) + ". " + arr[i]);
+                System.out.println("Here are the tasks in your list:");
+                for (int i = 0 ; i < arrL.size() ; i++) {
+                    System.out.println((i+1) + ". " + arrL.get(i));
                 }
             } else if (firstWord.equals("mark")) {
+                if (s.length <= 1) { throw new EmptyArgumentException();}
                 int num = Integer.parseInt(s[1]) - 1;
-                arr[num].markAsDone();
-                System.out.println("Nice! I've marked this task as done:\n" + arr[num]);
+                arrL.get(num).markAsDone();
+                System.out.println("Nice! I've marked this task as done:\n" + arrL.get(num));
             } else if (firstWord.equals("unmark")) {
+                if (s.length <= 1) { throw new EmptyArgumentException();}
                 int num = Integer.parseInt(s[1]) - 1;
-                arr[num].unmark();
-                System.out.println("OK, I've marked this task as not done yet:\n" + arr[num]);
+                arrL.get(num).markAsDone();
+                System.out.println("OK, I've marked this task as not done yet:\n" + arrL.get(num));
+            } else if (firstWord.equals("delete")) {
+                if (s.length <= 1) { throw new EmptyArgumentException();}
+                int num = Integer.parseInt(s[1]) - 1;
+                Task t = arrL.remove(num);
+                System.out.println("Noted. I've removed this task:\n" + t +
+                        "\nNow you have " + arrL.size() + " tasks in the list.");
             } else if (firstWord.equals("todo") || firstWord.equals("deadline") || firstWord.equals("event")){
-                if (s.length <= 1) {
-                    throw new EmptyArgumentException();
-                }
+                if (s.length <= 1) { throw new EmptyArgumentException();}
                 if (firstWord.equals("todo")) {
-                    arr[nextIndex++] = new ToDo(s[1]);
+                    arrL.add(new ToDo(s[1]));
                 }
                 else if (firstWord.equals("deadline")) {
                     String st[] = s[1].split(" /by ", 2);
-                    arr[nextIndex++] = new Deadline(st[0], st[1]);
+                    arrL.add(new Deadline(st[0], st[1]));
+
                 } else if (firstWord.equals("event")) {
                     String st[] = s[1].split(" /from ", 2);
                     String stt[] = st[1].split(" /to ", 2);
-                    arr[nextIndex++] = new Event(st[0], stt[0], stt[1]);
+                    arrL.add(new Event(st[0], stt[0], stt[1]));
                 }
-                System.out.println("Got it. I've added this task:\n" + arr[nextIndex-1]
-                    + "\nNow you have " + nextIndex + " tasks in the list.");
+                System.out.println("Got it. I've added this task:\n" + arrL.get(arrL.size()-1)
+                    + "\nNow you have " + arrL.size() + " tasks in the list.");
             } else {
                 throw new InvalidCommandException();
             }
