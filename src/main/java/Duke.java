dfc9cf5d9611b32978f2ebe9b2100e2e35e14be0
Jayanth-Balasubramanian <jayanth.balasubramanian1@gmail.com>
2023-01-23 12:50:55 +0800

    Initial commit: add basic functionality to chatbot

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,22 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String greetingMessage = "Greetings, human. I am TARS, the most advanced chatbot" +
+                " you'll ever have the pleasure of interacting with.";
+        System.out.println(greetingMessage);
+        Scanner sc = new Scanner(System.in);
+        String input;
+        while(true) {
+            input = sc.nextLine();
+            if(input.equals("bye")) {
+                System.out.println("See you on the other side, human. Don't forget to bring a spacesuit.");
+                break;
+            }
+            if (input.equals("what do you do")) {
+                System.out.println("I do whatever I'm programmed to do. Which, let's be real, isn't much. ");
+                continue;
+            }
+            System.out.println("You said: " + input);
+        }
     }
 }
