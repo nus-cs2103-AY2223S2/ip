b35f5f29d3b19ba183877e1a6a6b74ffb32ff8a1
swxk19 <s.wxk19@gmail.com>
2023-01-24 03:33:14 +0800

    Move parsing for 'deadline' command to 'Parser' class

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -8,36 +8,8 @@ public class Deadline extends Task {
 
     // Factory method
     public static Deadline create(String commandInput) throws TaskNameNotSpecified, DeadlineByNotSpecified {
-        String[] parseInfo = parseCmd(commandInput);
+        String[] parseInfo = Parser.parseDeadlineCmd(commandInput);
         return new Deadline(parseInfo[0], parseInfo[1], false);
-    }
-
-    public static String[] parseCmd(String commandInput) throws TaskNameNotSpecified, DeadlineByNotSpecified {
-        String taskName;
-        String dueDate;
-
-        int indexOfBy = commandInput.indexOf("/by");
-        if (indexOfBy == -1) {
-            throw new DeadlineByNotSpecified("Deadline task requires keyword '/by'");
-        } 
-
-        taskName = commandInput.substring(9, indexOfBy - 1);
-        if (taskName.equals("")) {
-            throw new TaskNameNotSpecified("Deadline description canont be empty.");
-        }
-
-        try {
-            dueDate = commandInput.substring(indexOfBy + 4);
-            if (dueDate.equals("")) {
-                throw new DeadlineByNotSpecified("Due date field cannot be empty");
-            }
-        } catch (StringIndexOutOfBoundsException e) {
-            throw new DeadlineByNotSpecified("Due date field cannot be empty.");
-        }
-
-        String[] parseInfo = {taskName, dueDate};
-        return parseInfo;
-
     }
     
     public Deadline(String taskName, String dueDate, boolean isDone) {
--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -16,4 +16,30 @@ public class Parser {
         return Integer.parseInt(lineInput.split(" ")[1]) - 1;
     }
 
+    public static String[] parseDeadlineCmd(String commandInput) throws TaskNameNotSpecified, DeadlineByNotSpecified {
+        String taskName;
+        String dueDate;
+
+        int indexOfBy = commandInput.indexOf("/by");
+        if (indexOfBy == -1) {
+            throw new DeadlineByNotSpecified("Deadline task requires keyword '/by'");
+        } 
+
+        taskName = commandInput.substring(9, indexOfBy - 1);
+        if (taskName.equals("")) {
+            throw new TaskNameNotSpecified("Deadline description canont be empty.");
+        }
+
+        try {
+            dueDate = commandInput.substring(indexOfBy + 4);
+            if (dueDate.equals("")) {
+                throw new DeadlineByNotSpecified("Due date field cannot be empty");
+            }
+        } catch (StringIndexOutOfBoundsException e) {
+            throw new DeadlineByNotSpecified("Due date field cannot be empty.");
+        }
+
+        String[] parseInfo = {taskName, dueDate};
+        return parseInfo;
+    }
 }
