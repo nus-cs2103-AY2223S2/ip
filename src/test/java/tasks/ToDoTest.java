cd7d4c81e8500b02bb8511a26380aa96760a911b
Tan Kai Min Russell <russelltanreg38@gmail.com>
2023-01-23 09:09:01 +0800

    Set up gradle with recommended testing enabled

---
+++ src/test/java/tasks/ToDoTest.java
@@ -0,0 +1,40 @@
+package tasks;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ToDoTest {
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void testCreate() {
+    }
+
+    @Test
+    void buildDescription() {
+    }
+
+    @Test
+    void mark() {
+    }
+
+    @Test
+    void unmark() {
+    }
+
+    @Test
+    void getTaskType() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void writeTask() {
+    }
+}
