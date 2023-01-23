99bcf83fc5d595e425a72ecc684b9954c1fb8ff5
tyx021 <yxting1109@gmail.com>
2023-01-23 19:41:34 +0800

    Adds save and load tasks

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -10,6 +10,20 @@ public class Event extends Task {
     }
 
     @Override
+    public String getData() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("E | ");
+        if (this.isDone) {
+            sb.append("1 | ");
+        } else {
+            sb.append("0 | ");
+        }
+        sb.append(this.description).append(" | ");
+        sb.append(this.start).append(" | ").append(this.end).append("\n");;
+        return sb.toString();
+    }
+
+    @Override
     public String toString() {
         return "[E]" + super.toString() + " (from: " + start + " to: " + end + ")";
     }
