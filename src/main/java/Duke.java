808b76ad9cd3617153d2b16b73fdcaa12fb9d719
Clyde Lhui <62978225+clydelhui@users.noreply.github.com>
2023-01-24 00:30:32 +0800

    Added Task.java and Modified Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import items.Task;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -11,22 +13,41 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         Scanner usrInput = new Scanner(System.in);
         String input = "";
-        List<String> lst = new ArrayList<>();
+        List<Task> lst = new ArrayList<>();
         while(true){
             input = usrInput.nextLine();
-            switch (input) {
+            String[] parsedInput = input.split(" ");
+            switch (parsedInput[0]) {
                 case "bye":
+                    if (parsedInput.length > 1){
+                        continue;
+                    }
                     System.out.println("Goodbye!");
                     usrInput.close();
                     System.exit(0);
                 case "list":
-                    for (int j = 0; j < lst.size(); j++) {
-                        System.out.println((j + 1) + ":" + lst.get(j));
+                    if (parsedInput.length > 1){
+                        continue;
                     }
+                    for (int j = 0; j < lst.size(); j++) {
+                        System.out.println((j + 1) + ":" + lst.get(j).toString());
+                    }
+                    break;
+                case "mark":
+                    Integer taskCode = Integer.parseInt(parsedInput[1]);
+                    Task chosenTask = lst.get(taskCode - 1);
+                    chosenTask.setDone();
+                    System.out.println("I have set the following task to done: \n" + chosenTask.toString());
+                    break;
+                case "unmark":
+                    Integer taskCodeUnmark = Integer.parseInt(parsedInput[1]);
+                    Task chosenTaskUnmark = lst.get(taskCodeUnmark - 1);
+                    chosenTaskUnmark.setNotDone();
+                    System.out.println("I have set the following task to not done: \n" + chosenTaskUnmark.toString());
                     break;
                 default:
                     System.out.println("added:" + input);
-                    lst.add(input);
+                    lst.add(new Task(input));
                     
             }
         }
