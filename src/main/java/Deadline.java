74bb18516436430981670d6bd91033f647749f83
junlee1991 <julio_harjo@hotmail.sg>
2023-01-23 21:40:10 +0800

    Level-8 done

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,16 +1,6 @@
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
-
 public class Deadline extends Task {
 
     protected String by;
-
-    /**
-     * Constructor that takes in description of deadline and
-     * @param description of deadline
-     * @param by in the format of a string in the format of day/month/year time eg. 02/05/2019 1800
-     */
 
     public Deadline(String description, String by) {
         super(description);
@@ -20,22 +10,9 @@ public class Deadline extends Task {
     public String getBy(){
         return by;
     }
-    /**
-     * Method to covert String into formatted string in date format.
-     * @param str with the exact format day/month/year time(24 hour format)
-     */
-
-    public String dateConvert(String str){
-        // String to LocalDate
-        DateTimeFormatter format1 = DateTimeFormatter.ofPattern("dd/MM/yyyy HHmm");
-        LocalDateTime date = LocalDateTime.parse(str, format1);
-        // DateTime to String
-        String formattedDate = date.format(DateTimeFormatter.ofPattern("dd MMM yyyy h:mm a"));
-        return formattedDate;
-    }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + dateConvert(by) + ")";
+        return "[D]" + super.toString() + " (by: " + by + ")";
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,18 +1,7 @@
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
 public class Event extends Task {
 
     protected String from;
     protected String to;
-
-    /**
-     * Constructor to instantiate an Event object
-     * @param description of Event
-     * @param from in the format of a string in the format of day/month/year time eg. 02/05/2019 1800
-     * @param to in the format of a string in the format of day/month/year time eg. 02/05/2019 1800
-     */
-
 
     public Event(String description, String from, String to) {
         super(description);
@@ -24,18 +13,8 @@ public class Event extends Task {
         return from + " - " + to;
     }
 
-
-    public String dateCovert(String str){
-        // String to LocalDate
-        DateTimeFormatter format1 = DateTimeFormatter.ofPattern("dd/MM/yyyy HHmm");
-        LocalDateTime date = LocalDateTime.parse(str, format1);
-        // DateTime to String
-        String formattedDate = date.format(DateTimeFormatter.ofPattern("dd MMM yyyy h:mm a"));
-        return formattedDate;
-    }
-
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + dateCovert(from) + " to: " + dateCovert(to) + ")";
+        return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
     }
 }
