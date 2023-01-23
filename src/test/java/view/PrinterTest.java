19ca45ace27cef5074f831bb33f0e0600b0af001
jiawei <b33b33o11@gmail.com>
2023-01-23 09:38:52 +0800

    test: add test for listPrint

--- src/test/java/view/PrinterTest.java
+++ src/test/java/view/PrinterTest.java
@@ -1,2 +1,34 @@
-package view;public class PrinterTest {
+package view;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PrinterTest {
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @Test
+    public void listPrintTest(){
+        String[] list1 = {"Task 1", "Task 2", "Task 3"};
+        Printer.listPrint(list1);
+
+        assertEquals("\t1. Task 1\n\t2. Task 2\n\t3. Task 3\n====================",
+                outputStreamCaptor.toString().stripTrailing());
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.setOut(standardOut);
+    }
 }
