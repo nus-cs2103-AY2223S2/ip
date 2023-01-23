52d87a9ff522a7a9b61ec5e2ac9ca1a297698232
securespider <e0725031@u.nus.edu>
2023-01-23 21:55:59 +0800

    Added Exceptions

---
+++ src/main/java/CommandException.java
@@ -0,0 +1,25 @@
+public class CommandException extends DukeException {
+    public CommandException() {
+    }
+
+    public CommandException(String message) {
+        super(message);
+    }
+
+    public CommandException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public CommandException(Throwable cause) {
+        super(cause);
+    }
+
+    public CommandException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+    @Override
+    public String toString() {
+        return "☹ OOPS!!! I'm sorry, but I don't know what that means :-(";
+    }
+}
