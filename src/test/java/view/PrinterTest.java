8785533c510a7d95fed7cc1f40f411dcb9918314
Jia Wei Lee <leejiawei.00@gmail.com>
2023-01-23 17:00:59 +0800

    test: add test cases for Printer

--- src/test/java/view/PrinterTest.java
+++ src/test/java/view/PrinterTest.java
@@ -19,12 +19,23 @@ public class PrinterTest {
     }
 
     @Test
-    public void listPrintTest(){
+    public void listPrintTest() {
         String[] list1 = {"Task 1", "Task 2", "Task 3"};
         Printer.listPrint(list1);
-
         assertEquals("\t1. Task 1\n\t2. Task 2\n\t3. Task 3\n====================",
                 outputStreamCaptor.toString().stripTrailing());
+        outputStreamCaptor.reset();
+
+        String[] list2 = {"This is a very long task"};
+        Printer.listPrint(list2);
+        assertEquals("\t1. This is a very long task\n====================",
+                outputStreamCaptor.toString().stripTrailing());
+        outputStreamCaptor.reset();
+
+        String[] list3 = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
+        Printer.listPrint(list3);
+        assertEquals("\t1. 1\n\t2. 2\n\t3. 3\n\t4. 4\n\t5. 5\n\t6. 6\n\t7. 7\n\t8. 8\n\t9. 9\n\t10. 10" +
+                        "\n====================", outputStreamCaptor.toString().stripTrailing());
     }
 
     @AfterEach
