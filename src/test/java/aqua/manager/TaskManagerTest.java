88ceef6f9944151e4c2070ac99d8aef1c08de944
daitenshionyan <snivy.oshowott@gmail.com>
2023-01-24 02:17:47 +0800

    TaskManager: add filter test

--- src/test/java/aqua/manager/TaskManagerTest.java
+++ src/test/java/aqua/manager/TaskManagerTest.java
@@ -1,6 +1,7 @@
 package aqua.manager;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import java.io.FileInputStream;
@@ -8,6 +9,9 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Path;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.junit.jupiter.api.Test;
 
@@ -78,6 +82,46 @@ public class TaskManagerTest {
             fail("Save file not created", fnfEx);
         } catch (IOException ioEx) {
             fail(ioEx);
+        }
+    }
+
+
+    @Test
+    public void filter_zeroMatch_emptyMap() {
+        TaskManager manager = new TaskManager();
+        manager.add(new MarkedTask());
+        manager.add(new MarkedTask());
+        manager.add(new UnMarkedTask());
+        manager.add(new UnMarkedTask());
+        manager.add(new UnMarkedTask());
+        manager.add(new MarkedTask());
+        manager.add(new MarkedTask());
+        manager.add(new UnMarkedTask());
+        LinkedHashMap<Integer, AquaTask> map = manager.filter("44.5");
+        assertTrue(map.isEmpty());
+    }
+
+
+    @Test
+    public void filter_someMatch_filteredMap() {
+        TaskManager manager = new TaskManager();
+        /* 1 */ manager.add(new MarkedTask());
+        /* 2 */ manager.add(new MarkedTask());
+        /* 3 */ manager.add(new UnMarkedTask());
+        /* 4 */ manager.add(new UnMarkedTask());
+        /* 5 */ manager.add(new UnMarkedTask());
+        /* 6 */ manager.add(new MarkedTask());
+        /* 7 */ manager.add(new MarkedTask());
+        /* 8 */ manager.add(new UnMarkedTask());
+        LinkedHashMap<Integer, AquaTask> map = manager.filter("+");
+        List<Integer> numList = List.of(1, 2, 6, 7);
+        assertEquals(4, map.size());
+        // check for order as well
+        int i = 0;
+        for (Map.Entry<Integer, AquaTask> entry : map.entrySet()) {
+            assertEquals(numList.get(i), entry.getKey());
+            assertEquals("+", entry.getValue().getName());
+            i++;
         }
     }
 
