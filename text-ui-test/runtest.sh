a60dbc91ff7d829366007c44049c72696ce753a0
swxk19 <s.wxk19@gmail.com>
2023-01-23 14:47:17 +0800

    Fix file paths for test scripts

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # create bin directory if it doesn't exist
-if [ ! -d "../bin" ]
+if [ ! -d "../bin/" ]
 then
-    mkdir ../bin
+    mkdir ../bin/
 fi
 
 # delete output from previous run
@@ -20,7 +20,7 @@ then
 fi
 
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ../bin Duke < input.txt > ACTUAL.TXT
+java -classpath ../bin/ Duke < input.txt > ACTUAL.TXT
 
 # convert to UNIX format
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
