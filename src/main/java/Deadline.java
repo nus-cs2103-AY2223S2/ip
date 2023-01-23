af06c3f7a3027eb53f2988a75a5bea1c5171f294
soowengkit <>
2023-01-23 21:26:06 +0800

    Process date as LocalDate Objects

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,8 +1,13 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Tasks{
     String icon = "[D]";
 
     String desc;
     String end;
+    LocalDate date;
+    String time;
     public Deadline(String str) throws Exception {
         super(str);
         if(!str.contains("deadline")) {
@@ -13,26 +18,37 @@ public class Deadline extends Tasks{
         }
 
         if ((super.getDesc().split("/", 2)).length == 1) {
-            throw new unspecTimeException("Please specify a deadline (by/ ... )");
+            throw new unspecTimeException("Please specify a deadline (/by ... )");
         }
-        String time = super.getDesc().split("/", 2)[1];
+        String dline = super.getDesc().split("/", 2)[1];
         this.desc = super.getDesc().split("/",2)[0];
-        this.end =  time.replaceFirst(" ", ": ");
+        this.end =  dline.replaceFirst(" ", ": ");
+        try {
+            String day = dline.substring(3).split(" ")[0];
+            String time = dline.substring(3).split(" ")[1];
+            this.date = LocalDate.parse(day);
+            this.time = time;
+        } catch(Exception ignored) {
+
+        }
     }
     public String icon() {
         return icon;
     }
 
     public String getDesc(){
-        return this.desc.split(" ", 2)[1] + "("+this.end+")";
+        if(this.date==null) {
+            return this.desc.split(" ", 2)[1] + "(" + this.end + ")";
+        } else {
+            return this.desc.split(" ", 2)[1] + "(by: " + date.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + " "
+            + this.time + ")";
+        }
     }
 
     public String added() {
-        return super.added() + " " + this.icon + super.symbol() + " " + this.desc.split(" ", 2)[1]
-                +"("+this.end+")";
+        return super.added() + " " + this.icon + super.symbol() + " " + this.getDesc();
     }
     public String deleted() {
-        return super.deleted() + " " + icon + super.symbol() + " " + this.desc.split(" ", 2)[1] +
-                "(" + this.end + ")";
+        return super.deleted() + " " + icon + super.symbol() + " " + this.getDesc();
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,7 +1,11 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 public class Event extends Tasks{
     String icon = "[E]";
     String desc;
     String timeframe;
+    String time1, time2;
+    LocalDate date1, date2;
     public Event(String str) throws Exception{
         super(str);
 
@@ -16,12 +20,27 @@ public class Event extends Tasks{
             throw new unspecTimeException("Please specify a timeframe (from/ ... to/ ...)");
         }
 
-        String time = super.getDesc().split("/", 2)[1];
-        String start = time.split("/", 2)[0];
-        String end = time.split("/", 2)[1];
+        String evnt = super.getDesc().split("/", 2)[1];
+        String start = evnt.split("/", 2)[0].replaceFirst(" ", ": ");
+        String end = evnt.split("/", 2)[1].replaceFirst(" ", ": ");
         this.desc = super.getDesc().split("/",2)[0];
-        this.timeframe = start.replaceFirst(" ", ": ") +
-                end.replaceFirst(" ", ": ");
+        this.timeframe = start + end;
+        try {
+            String d1 = start.substring(6).split(" ")[0];
+            this.time1 = start.substring(6).split(" ")[1];
+            String d2 = end.substring(4).split(" ")[0];
+            System.out.println(d1);
+            System.out.println(d2);
+            this.time2 = end.substring(4).split(" ")[1];
+            this.date1 = LocalDate.parse(d1);
+            this.date2 = LocalDate.parse(d2);
+
+            this.timeframe = "from: " + date1.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + " " + time1 +
+                    " to: " + date2.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + " "+ time2;
+
+        } catch(Exception ignored) {
+
+        }
     }
     public String icon() {
         return icon;
