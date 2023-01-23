7e996d6fc53789b62e2b7d3a154daf8a991ebd0c
TamKengHong <tamkenghong@gmail.com>
2023-01-23 22:14:13 +0800

    Level-8 Added DateTime support

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,16 +1,18 @@
+import java.time.LocalDate;
+
 public class Event extends Task {
-  protected String startTime;
-  protected String endTime;
+  protected LocalDate startTime;
+  protected LocalDate endTime;
 
   public Event(String description, String startTime, String endTime) {
     super(description);
-    this.startTime = startTime;
-    this.endTime = endTime;
+    this.startTime = LocalDate.parse(startTime);
+    this.endTime = LocalDate.parse(endTime);
   }
 
   @Override
   public String toString() {
-    return "[E]" + super.toString() + " (from: " +
-        this.startTime + " to: " + this.endTime + ")";
+    return String.format("[E]%s (from: %s to: %s)", super.toString(),
+        this.startTime, this.endTime);
   }
 }
