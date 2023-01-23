8708ffca6ca9a0090343c131a25c5badd4b34a54
filbert <fillberth@gmail.com>
2023-01-23 19:04:36 +0800

    Add javadoc for package duke.exception

--- src/main/java/duke/exception/DukeException.java
+++ src/main/java/duke/exception/DukeException.java
@@ -1,11 +1,22 @@
 package duke.exception;
 
+/** A base class representing exceptions that may be thrown by Duke. */
 public abstract class DukeException extends Exception {
 
+    /**
+     * Creates a new DukeException with a given message.
+     * 
+     * @param msg Additional information regarding the exception
+     */
     public DukeException(String msg) {
         super(msg);
     }
 
+    /**
+     * Gets the name of the current exception.
+     * 
+     * @return The name of the current exception
+     */
     public abstract String getExceptionName();
 
     @Override
--- src/main/java/duke/exception/InvalidArgumentException.java
+++ src/main/java/duke/exception/InvalidArgumentException.java
@@ -1,8 +1,15 @@
 package duke.exception;
 
+/** An exception thrown when a user issues a command with invalid arguments. */
 public class InvalidArgumentException extends DukeException {
+    /** The command causing the InvalidArgumentException */
     private String command;
 
+    /**
+     * Initializes an InvalidArgumentException that was caused by a command.
+     * 
+     * @param command The command causing the InvalidArgumentException
+     */
     public InvalidArgumentException(String command) {
         super("Arguments are of the wrong type or format");
         this.command = command;
