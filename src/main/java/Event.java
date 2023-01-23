5d3ce9359d52154a462c56f1858ee9ade3d970f1
swxk19 <s.wxk19@gmail.com>
2023-01-24 03:34:58 +0800

    Move parsing for 'event' command to 'Parser' class

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -9,52 +9,23 @@ public class Event extends Task {
     Optional<LocalDate> chronoToDate;
 
     public static Event create(String commandInput) throws TaskNameNotSpecified, EventFromToNotSpecified {
-        String[] parseInfo = parseCmd(commandInput);
+        String[] parseInfo = Parser.parseEventCmd(commandInput);
         return new Event(parseInfo[0], parseInfo[1], parseInfo[2], false);
     }
 
     public Event(String taskName, String fromDate, String toDate, boolean isDone) {
         super(taskName, "E");
+  
         this.fromDate = fromDate;
-        this.toDate = toDate;
-        this.completed = isDone;
         this.chronoFromDate = Parser.parseDate(fromDate);
+
+        this.toDate = toDate;
         this.chronoToDate = Parser.parseDate(toDate);
+
+        this.completed = isDone;
     }
 
-    public static String[] parseCmd(String commandInput) throws TaskNameNotSpecified, EventFromToNotSpecified {
-        String taskName;
-        String fromDate;
-        String toDate;
-
-        commandInput = commandInput.substring(6);
-        int indexOfFrom = commandInput.indexOf("/from");
-        int indexOfTo = commandInput.indexOf("/to");
-        if (indexOfFrom == -1 || indexOfTo == -1) {
-            throw new EventFromToNotSpecified("Event task requires keywords '/from' and '/to'");
-        }
-
-        taskName = commandInput.substring(0, indexOfFrom - 1);
-        if (taskName.equals("")) {
-            throw new TaskNameNotSpecified("The description of an event cannot be empty.");
-        }
-
-        fromDate = commandInput.substring(indexOfFrom + 6, indexOfTo - 1);
-        if (fromDate.equals("")) {
-            throw new EventFromToNotSpecified("from/to fields cannot be empty.");
-        }
-
-        try {
-            toDate = commandInput.substring(indexOfTo + 4, commandInput.length());
-           
-        } catch (StringIndexOutOfBoundsException e) {
-            throw new EventFromToNotSpecified("from/to fields cannot be empty.");
-        }
-
-        String[] parseInfo = {taskName, fromDate, toDate};
-        return parseInfo;
-    }
-
+    
     @Override
     public String stringFields() {
         String fromDateString = this.chronoFromDate.isEmpty() ? this.fromDate : this.chronoFromDate.get().format(DateTimeFormatter.ofPattern("MMM d yyyy"));
--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -42,4 +42,38 @@ public class Parser {
         String[] parseInfo = {taskName, dueDate};
         return parseInfo;
     }
+
+    public static String[] parseEventCmd(String commandInput) throws TaskNameNotSpecified, EventFromToNotSpecified {
+        String taskName;
+        String fromDate;
+        String toDate;
+
+        commandInput = commandInput.substring(6);
+        int indexOfFrom = commandInput.indexOf("/from");
+        int indexOfTo = commandInput.indexOf("/to");
+        if (indexOfFrom == -1 || indexOfTo == -1) {
+            throw new EventFromToNotSpecified("Event task requires keywords '/from' and '/to'");
+        }
+
+        taskName = commandInput.substring(0, indexOfFrom - 1);
+        if (taskName.equals("")) {
+            throw new TaskNameNotSpecified("The description of an event cannot be empty.");
+        }
+
+        fromDate = commandInput.substring(indexOfFrom + 6, indexOfTo - 1);
+        if (fromDate.equals("")) {
+            throw new EventFromToNotSpecified("from/to fields cannot be empty.");
+        }
+
+        try {
+            toDate = commandInput.substring(indexOfTo + 4, commandInput.length());
+           
+        } catch (StringIndexOutOfBoundsException e) {
+            throw new EventFromToNotSpecified("from/to fields cannot be empty.");
+        }
+
+        String[] parseInfo = {taskName, fromDate, toDate};
+        return parseInfo;
+    }
+
 }
