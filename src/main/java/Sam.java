f73129ea2fbaba6ed8cbeb646d74e7f099dcae8a
CloudHill <rh.cloudhill@gmail.com>
2023-01-23 17:21:10 +0800

    Add Ui class
    Assets class removed

--- src/main/java/Sam.java
+++ src/main/java/Sam.java
@@ -3,53 +3,45 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Scanner;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class Sam {
-  private Scanner scanner;
+  private Ui ui;
   private TaskList tasks;
   private HashMap<String, String> taskArgs;
   private boolean live;
   private Path savePath;
 
   public Sam() {
-    scanner = new Scanner(System.in);
+    ui = new Ui();
     tasks = new TaskList();
     taskArgs = new HashMap<>();
-    savePath = Path.of("data", "sam.txt");
     live = false;
+    savePath = Path.of("data", "sam.txt");
   }
 
   public void run() {
     live = true;
-    System.out.println(Assets.LOGO);
-    talk("Hello, I am Sam!");
+    ui.showLogo();
+    ui.talk("Hello, I am Sam!");
     try {
       load();
     } catch (SamLoadFailedException e) {
-      talk(e.getMessage());
+      ui.talk(e.getMessage());
     }
     while (live) {
-      System.out.println();
-      System.out.println(Assets.USER);
-      System.out.print("> ");
-      String[] input = scanner.nextLine().strip().split(" ", 2);
+      String input = ui.acceptInput();
+      String[] splitInput = input.split(" ", 2);
       try {
-        processInput(input);
+        processInput(splitInput);
       } catch (SamException e) {
-        talk(e.getMessage());
+        ui.talk(e.getMessage());
       } finally {
         taskArgs.clear();
       }
     }
-    close();
-  }
-
-  private void close() {
-    scanner.close();
   }
 
 	public static void main(String[] args) {
@@ -72,15 +64,15 @@ public class Sam {
     switch (command) {
       case BYE:
         live = false;
-        talk("Goodbye!");
+        ui.talk("Goodbye!");
         break;
       case LIST:
       if (tasks.count() == 0) {
-        talk("Your list is empty!");
+        ui.talk("Your list is empty!");
       } else {
           // "Here is your list:"
           String[] list = tasks.generateList();
-          talk(list);
+          ui.talk(list);
         }
         break;
       case MARK: {
@@ -92,7 +84,7 @@ public class Sam {
           throw new SamInvalidTaskException();
         }
         tasks.markTask(id, true);
-        talk("Great! I'll check the task:",
+        ui.talk("Great! I'll check the task:",
           tasks.printTask(id));
         save();
         break;
@@ -106,7 +98,7 @@ public class Sam {
           throw new SamInvalidTaskException();
         }
         tasks.markTask(id, false);
-        talk("Okay, I'll uncheck the task:",
+        ui.talk("Okay, I'll uncheck the task:",
           tasks.printTask(id));
           save();
         break;
@@ -169,7 +161,7 @@ public class Sam {
           throw new SamInvalidTaskException();
         }
         Task task = tasks.removeTask(id);
-        talk("Ok, I'll remove the task from your list:",
+        ui.talk("Ok, I'll remove the task from your list:",
           task.toString());
         save();
         break;
@@ -186,18 +178,9 @@ public class Sam {
   }
 
   private void newTask(Task task) {
-    talk("Gotcha, I'll add the task to your list:",
+    ui.talk("Gotcha, I'll add the task to your list:",
       task.toString(),
       String.format("Now you have %d tasks in the list", tasks.count()));
-  }
-
-  private void talk(String ...messages) {
-    System.out.println(Assets.SAM);
-    System.out.println("┌───────────────────────────────────────────┐");
-    for (String message : messages) {
-      System.out.println("  " + message);
-    }
-    System.out.println("└───────────────────────────────────────────┘");
   }
 
   private void save() throws SamSaveFailedException {
