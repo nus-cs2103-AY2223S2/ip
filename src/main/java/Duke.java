cdcba409336d51e9ad6ffd5951cdb4a798c4aff9
FireRadical22 <97332558+FireRadical22@users.noreply.github.com>
2023-01-24 19:44:16 +0800

    Level-8 Test
    - Mandate use of proper date format for deadline and event commands (in testing)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,7 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +10,31 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        //try{
+            //System.out.println(logo);
+            //"todo \D+"
+            //deadline .+/by \d{2}/\d{2}/\d{4}
+            //event .+/from \d{2}/\d{2}/\d{4} /to \d{2}/\d{2}/\d{4}
+            Scanner sc = new Scanner(System.in);
+            String command = sc.nextLine();
+            while (true){
+                Pattern todoPattern = Pattern.compile("todo \\D+");
+                Pattern deadlinePattern = Pattern.compile("deadline .+/by \\d{2}/\\d{2}/\\d{4}");
+                Pattern eventPattern = Pattern.compile("event .+/from \\d{2}/\\d{2}/\\d{4} /to \\d{2}/\\d{2}/\\d{4}");
+                if (todoPattern.matcher(command).find()){
+                    System.out.println("Todo Task added succesfully!");
+                } else if (deadlinePattern.matcher(command).find()){
+                    System.out.println("Deadline Task added succcesfully!");
+                } else if (eventPattern.matcher(command).find()){
+                    System.out.println("Event task added successfully!");
+                } else {
+                    System.out.println("Incorrect command given!");
+                }
+            }
+        //} catch(Exception e){
+
+        //}
     }
+
+
 }
