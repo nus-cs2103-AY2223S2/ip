cda40e052a584765e81f882d2e460729e795ddaa
Shane Duggan <>
2023-01-24 17:08:47 +0800

    no message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,25 +2,46 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-        String[] storedList = new String[100];
-        int index = 0;
+        Task[] taskList = new Task[100];
+        int index = 1;
         System.out.println("Hello I'm Duke\nWhat can I do for you?");
         Scanner scan = new Scanner(System.in);
         
         while (true) {
             String textInput = scan.nextLine();
+            Task t = new Task(textInput);
+
             if (textInput.equalsIgnoreCase("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 scan.close();
                 return;
             }
-            if (textInput.equals("list")) {
-                for (int i = 0; i < index; i++) {
-                    System.out.println(i + 1 + ". " + storedList[i]);
+
+            if (textInput.equalsIgnoreCase("list")) {
+                System.out.println("Here are the tasks in your list:");
+                for (int i = 1; i < index; i++) {
+                    System.out.println(i + ". " + taskList[i-1].toString());
                 }
                 continue;
             }
-            storedList[index] = textInput;
+
+            if (textInput.substring(0, 4).equalsIgnoreCase("mark")) {
+                int i = Integer.parseInt(textInput.substring(5));
+                Task currTask = taskList[i-1];
+                currTask.markDone();
+                System.out.println("Nice! I've marked this task as done\n" + currTask.toString());
+                continue;
+            }
+
+            if (textInput.substring(0, 6).equalsIgnoreCase("unmark")) {
+                int i = Integer.parseInt(textInput.substring(7));
+                Task currTask = taskList[i-1];
+                currTask.markUndone();
+                System.out.println("OK, I've marked this task as not done yet:\n" + currTask.toString());
+                continue;
+            }
+
+            taskList[index-1] = t;
             index++;
             System.out.println("added: " + textInput);
         }
---
+++ src/main/java/Task.java
@@ -0,0 +1,26 @@
+public class Task {
+  protected String description;
+  protected boolean isDone;
+
+  public Task(String description) {
+      this.description = description;
+      this.isDone = false;
+  }
+
+  public String getStatusIcon() {
+      return (isDone ? "X" : " "); // mark done task with X
+  }
+
+  public void markDone() {
+      this.isDone = true;
+  }
+
+  public void markUndone() {
+      this.isDone = false;
+  }
+
+  public String toString() {
+      String result = String.format("[%s] %s", this.getStatusIcon(), this.description);
+      return result;
+  }
+}
