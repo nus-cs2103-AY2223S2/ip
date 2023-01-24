73cc2af89578fd9f18ccd35785b3bf863e9864ad
swxk19 <s.wxk19@gmail.com>
2023-01-24 15:58:53 +0800

    Add JavaDoc for all /exception files

--- src/main/java/duke/exceptions/EventFromToNotSpecified.java
+++ src/main/java/duke/exceptions/EventFromToNotSpecified.java
@@ -1,6 +1,12 @@
 package duke.exceptions;
 
+// An exception for the case that a from/to date was not specified in an 'event' command.
 public class EventFromToNotSpecified extends TaskInitError {
+
+    /**
+     * Constructor method.
+     * @param errorMessage Error message to display to user
+     */
     public EventFromToNotSpecified(String errorMessage) {
         super(errorMessage);
     }
