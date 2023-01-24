a7a964a94cb575eb80f9bd0a0c46b028c04f3a8f
slackernoob <maxang12@gmail.com>
2023-01-24 15:11:43 +0800

    Add dates feature for tasks
    Dates and timings were treated as a string previously.
    
    This means that there is no formatting.
    
    To enable a format for deadlines and dates, they are stored as LocalDate objects
    within the respective classes Deadline and Event.

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,14 +1,19 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Deadline extends Task {
     protected String by;
+    protected LocalDate byDate;
 
     public Deadline(String description, String by) {
         super(description);
         this.by = by;
+        this.byDate = LocalDate.parse(by);
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: "
+                + byDate.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
     }
 }
