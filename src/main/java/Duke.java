0fd407c9340738c2c33df8af05ab320b5f39b01e
Teo Weide <teoweide1999@gmail.com>
2023-01-24 22:16:43 +0800

    Level 4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -5,7 +7,11 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        Task t = new Task();
+        ArrayList<Task> t = new ArrayList<>();
+        final String markAsDone = "Nice! I've marked this task as done: ";
+        final String unMarkTask = "OK, I've marked this task as not done yet: ";
+        final String addedTask = "Got it, I've added this task: ";
+
 
         System.out.println("Hello from Bench Monster");
         System.out.println("What can I do for you?");
@@ -13,58 +19,149 @@ public class Duke {
 
         while (true) {
             String type = s.nextLine();
-            String[] tokens = type.split(" ");
-            if (tokens[0].equals("bye")) {
+            String[] tokens = type.split("\\s+");
+            if (tokens[0].equalsIgnoreCase("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
-            } else if (tokens[0].equals("list")) {
-                t.showList();
-            } else if(tokens[0].equals("mark")) {
-                    int i = Integer.parseInt(tokens[1]);
-                    t.markTask(i);
-            } else if(tokens[0].equals("unmark")) {
-                    int i = Integer.parseInt(tokens[1]);
-                    t.unmarkedTask(i);
-            } else {
-                t.addTask(type);
-            }
+            } else if (tokens[0].equalsIgnoreCase("list")) {
+                    showList(t);
+            } else if(tokens[0].equalsIgnoreCase("mark")) {
+                 int i = Integer.parseInt(tokens[1]) - 1 ;
+                 t.get(i).mark();
+                 System.out.println(markAsDone);
+                 System.out.println(t.get(i).toString());
 
+            } else if(tokens[0].equalsIgnoreCase("unmark")) {
+                int i = Integer.parseInt(tokens[1]) - 1 ;
+                t.get(i).unmarked();
+                System.out.println(unMarkTask);
+                System.out.println(t.get(i).toString());
+
+            } else if(tokens[0].equalsIgnoreCase("todo")) {
+                String des = type.substring(type.indexOf(" ")).replace(" ", "");
+                Task newTask = new ToDo(des);
+                t.add(newTask);
+                System.out.println(addedTask);
+                System.out.println(newTask);
+                System.out.println("Now you have " + noOfTask(t) + " tasks in the list.");
+
+            } else if(tokens[0].equalsIgnoreCase("deadline")) {
+                String des = type.substring(type.indexOf(" ")).replace(" ", "");
+                String[] deadline = des.split("/");
+                Deadline dead = new Deadline(deadline[0] , deadline[1]);
+                t.add(dead);
+                System.out.println(addedTask);
+                System.out.println(dead);
+                System.out.println("Now you have " + noOfTask(t) + " tasks in the list.");
+
+            } else if(tokens[0].equalsIgnoreCase("event")) {
+                String des = type.substring(type.indexOf(" "));
+                String[] events = des.split("/");
+                Event e = new Event(events[0].trim(), events[1].trim(), events[2].trim());
+                t.add(e);
+                System.out.println(addedTask);
+                System.out.println(e);
+                System.out.println("Now you have " + noOfTask(t) + " tasks in the list.");
+
+            }else {
+                Task newTask = new Task(type);
+                t.add(newTask);
+                System.out.println("added: " + type);
             }
+            }
+        }
+
+        public static void showList(ArrayList<Task> tasks) {
+                for(int i=0; i < tasks.size(); i++) {
+                    int no = i + 1;
+                    System.out.println(no + "." + tasks.get(i));
+                }
+        }
+
+        public static int noOfTask(ArrayList<Task> tasks) {
+            return tasks.size();
         }
 }//class
 
 class Task {
-    String s;
-    List<String> task = new ArrayList<>();
-    List<String> marked = new ArrayList<>();
+    private String details;
+    private boolean status;
 
+    public Task(String details) {
+        this.details = details;
+        this.status = false;
+    }
 
-    public void showList() {
-        System.out.println("Here are the tasks in your list");
-        for (int i = 0; i < task.size(); i++) {
-            int index = i + 1;
-            System.out.println(index + "." + marked.get(i) + " " + task.get(i));
+    public String isDone() {
+        if(status) {
+            return "[X]";
+        } else {
+            return "[ ]";
         }
     }
 
-    public void markTask(int i) {
-        int index = i-1;
-        marked.set(index, "[X]");
-        System.out.println("Nice! I've marked this task as done:");
-        System.out.println(marked.get(index) + " " + task.get(index));
+    public void mark() {
+        status = true;
     }
 
-    public void unmarkedTask(int i) {
-        int index = i-1;
-        marked.set(index, "[ ]");
-        System.out.println("OK, I've marked this task as not done yet:");
-        System.out.println(marked.get(index) + " " + task.get(index));
+    public void unmarked() {
+        status = false;
     }
 
-    public void addTask(String s) {
-        task.add(s);
-        marked.add("[ ]");
-        System.out.println("added: " + s);
+    public String getDetails() {
+        return details;
+    }
+
+    @Override
+    public String toString() {
+        return isDone() + " " + this.details;
     }
 }
+
+class ToDo extends Task {
+    String icon = "[T]";
+    public ToDo(String details) {
+        super(details);
+    }
+
+    @Override
+    public String toString() {
+        return icon + super.toString();
+    }
+}
+
+class Deadline extends Task {
+    String icon = "[D]";
+    String due;
+    public Deadline(String details, String due) {
+        super(details);
+        this.due = due;
+    }
+
+    @Override
+    public String toString() {
+        return icon + super.toString() + "(by: " + due + ")";
+    }
+}
+
+class Event extends Task {
+    String icon = "[E]";
+    String from;
+    String to;
+
+    public Event(String details, String from, String to) {
+        super(details);
+        this.from = from.replace("from", "");
+        this.to = to.replace("to", "");
+    }
+
+    @Override
+    public String toString() {
+       return icon + super.toString() + " (from:" + from + " to:" + to + ")";
+    }
+}
+
+
+
+
 
