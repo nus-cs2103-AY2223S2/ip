4b2a60b550e13145e28d735f6cd85dff136690e9
evanpy <evanleewj@gmail.com>
2023-01-25 04:28:29 +0800

    Add data saving and loading feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,15 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.FileNotFoundException;
 
 public class Duke {
     private static ArrayList<Task> tasks = new ArrayList<Task>();
+    private static String PATH = "../data/taskData.txt";
+    private static boolean isInitializingData = true;
 
     /** 
      * Outputs given string with formatting.
@@ -42,6 +48,11 @@ public class Duke {
      */
     private static void addTask(Task task) {
         tasks.add(task);
+
+        if (Duke.isInitializingData) {
+            return;
+        }
+
         output("Got it. I've added this task:\n    " 
                 + task + "\n  "
                 + "Now you have " + tasks.size() + (tasks.size() == 1 ? " task " : " tasks ") + "in the list.\n");
@@ -62,10 +73,12 @@ public class Duke {
     /** Outputs all the tasks stored in task list. */
     private static void listTasks() {
         String listOfTasks = "Here are the tasks in your list:\n";
+
         for(int idx = 0; idx < tasks.size(); idx++) {
             Task task = tasks.get(idx);
             listOfTasks = listOfTasks + "  " + (idx + 1) + "." + task + "\n";
         }
+
         output(listOfTasks);
     }
 
@@ -98,6 +111,46 @@ public class Duke {
         }
     }
 
+    /** Opens file containing task data and loads them into Task arraylist. */
+    private static void loadData() {
+        try {
+            File file = new File(PATH);
+
+            if (!file.exists()) {
+                throw new DukeException("File does not exists!\n");
+            }
+            
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNext()) {
+                String input = scanner.nextLine();
+                String[] descriptions = input.split(" ", 2);
+                createTask(descriptions[0], descriptions);
+            }
+
+            Duke.isInitializingData = false;
+        } catch (DukeException error) {
+            output(error.getMessage());
+        } catch (FileNotFoundException error) {
+            output(error.getMessage());
+        }
+    }
+
+    /** Saves task description into file. */
+    private static void saveData() {
+        try {
+            FileWriter fileWriter = new FileWriter(PATH);
+
+            for (Task task : tasks) {
+                fileWriter.write(task.getDescription());
+                fileWriter.write(System.lineSeparator());
+            }
+
+            fileWriter.close();
+        } catch (IOException error) {
+            System.out.println("Something went wrong: " + error.getMessage());
+        }
+    }
+
     /** 
      * Creates task and stores it in Task arraylist.
      * 
@@ -120,6 +173,7 @@ public class Duke {
                 if (descriptions.length != 2) {
                     throw new DukeException("OOPS!!! The description of a todo cannot be empty.\n");
                 }
+
                 addTask(new Todo(descriptions[1]));
             } catch (DukeException error) {
                 output(error.getMessage());
@@ -128,9 +182,11 @@ public class Duke {
         case "deadline":
             try {
                 String[] deadlineDescription = descriptions[1].split("/by");
+
                 if (deadlineDescription.length != 2) {
                     throw new DukeException("OOPS!!! The description of a deadline cannot be empty.\n");
                 }
+
                 addTask(new Deadline(deadlineDescription[0], deadlineDescription[1]));
             } catch (DukeException error) {
                 output(error.getMessage());
@@ -139,9 +195,11 @@ public class Duke {
         case "event":
             try {
                 String[] eventDescription = descriptions[1].split("/from|/to");
+
                 if (eventDescription.length != 3) {
                     throw new DukeException("OOPS!!! The description of an event cannot be empty.\n");
                 }
+
                 addTask(new Event(eventDescription[0], eventDescription[1], eventDescription[2]));
             } catch (DukeException e) {
                 output(e.getMessage());
@@ -150,9 +208,6 @@ public class Duke {
         case "delete":
             removeTask(Integer.parseInt(descriptions[1]) - 1);
             break;
-        case "bye":
-            exitMsg();
-            return;
         default:
             handleUnknownInput();
             break;
@@ -161,12 +216,23 @@ public class Duke {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        loadData();
         welcomeMsg();
+
         while (true) {
             String input = sc.nextLine();
             String[] descriptions = input.split(" ", 2);
             String taskType = descriptions[0];
+
+            if (taskType.equals("bye")) {
+                saveData();
+                exitMsg();
+                return;
+            }
+            
             createTask(taskType, descriptions);
         }
+
+
     }
 }
