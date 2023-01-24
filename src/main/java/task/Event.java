23b677c97dcba464799bf237fac0111c7cb255a8
Ritika Joshi <ritikajoshi0114@gmail.com>
2023-01-24 14:29:10 +0800

    Add read date and time from user input feature

--- src/main/java/task/Event.java
+++ src/main/java/task/Event.java
@@ -1,17 +1,20 @@
 package task;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 public class Event extends Task {
-
-    String start;
-    String end;
+    private String startFormatted;
+    private String endFormatted;
     public Event(String description, String start, String end) {
         super(description);
-        this.start = start;
-        this.end = end;
+        this.startFormatted = super.dateFormatter(start);
+        this.endFormatted = super.dateFormatter(end);
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (Start: " + start + " End: " + end + ")";
+        return "[E]" + super.toString() + " (Start: " + startFormatted + " | End: " + endFormatted + ")";
     }
+
+    //event party /from 12/2/23 6:00PM /to 12/2/23 10:00PM
 }
