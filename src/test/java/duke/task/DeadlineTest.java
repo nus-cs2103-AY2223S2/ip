98194ffee44d2c318150ef99114ce8440b4f7543
chuakid <chuakid@gmail.com>
2023-01-24 22:12:48 +0800

    Create tests for Deadline

---
+++ src/test/java/duke/task/DeadlineTest.java
@@ -0,0 +1,26 @@
+package duke.task;
+
+import duke.TaskCreationException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+public class DeadlineTest {
+    @Test
+    public void testCreation() {
+        assertDoesNotThrow(() -> new Deadline("test", "2022-01-01"));
+    }
+
+    @Test
+    public void wrongDate_wrongDate_throwsException() {
+        TaskCreationException e = assertThrows(TaskCreationException.class, () -> new Deadline("test", "1"));
+        assertEquals(e.getMessage(), "Error parsing date");
+    }
+
+    @Test
+    public void testToString()   {
+        Deadline dl  = assertDoesNotThrow(() -> new Deadline("test", "2022-01-01"));
+        assertEquals(dl.toString(), "[D][] test (by: 2022-01-01)");
+    }
+}
