0777ef6ff28bdf69929bcb4387ba6750dd9d05c1
pzhengze <zhengze@u.nus.edu>
2023-01-25 02:48:58 +0800

    Added Ui class to handle interactions with the user

---
+++ src/main/java/Ui.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Ui {
+    private final Scanner scanner = new Scanner(System.in);
+
+    public Ui(){}
+
+    public void greet() {
+        printText("\t Hello! I'm Duke\n\t What can I do for you?");
+    }
+
+    public String getUserInput() {
+        return scanner.nextLine();
+    }
+
+    public void printResponse(String s) {
+        printText(s);
+    }
+
+    public void printException(DukeException e) {
+        printText(e.getMessage());
+    }
+
+    public void showLoadingError() {
+        printText("\t OOPS!!! An error occurred while loading save");
+    }
+
+    private void printText(String s) {
+        System.out.println("\t____________________________________________________________");
+        System.out.println(s);
+        System.out.println("\t____________________________________________________________\n");
+    }
+}
