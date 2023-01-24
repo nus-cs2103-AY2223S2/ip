c5bb9ca55861a2b801f63593de4d919f6968dc03
chuakid <chuakid@gmail.com>
2023-01-24 22:50:13 +0800

    Add javadoc

--- src/main/java/duke/Storage.java
+++ src/main/java/duke/Storage.java
@@ -6,23 +6,33 @@ public class Storage {
     static String saveFolder = "data";
     static String savePath = saveFolder + "/save.txt";
 
+    /**
+     * Stores the parameterized TaskList as a serialized file
+     *
+     * @param list TaskList to be stored
+     */
     static void store(TaskList list) {
         File dir = new File(saveFolder);
         if (!dir.exists()) {
             boolean ignored = dir.mkdir();
         }
-        try (FileOutputStream fw = new FileOutputStream(savePath);
-             ObjectOutputStream out = new ObjectOutputStream(fw)
-        ) {
+        try (FileOutputStream fw = new FileOutputStream(savePath); ObjectOutputStream out = new ObjectOutputStream(
+                fw)) {
             out.writeObject(list);
         } catch (IOException e) {
             System.out.println("Saving error");
         }
     }
 
+    /**
+     * Loads the stored TaskList if it exists, else creates a new one.
+     *
+     * @return TaskList
+     */
     static TaskList load() {
-        try (FileInputStream fileInputStream = new FileInputStream(savePath);
-             ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream)) {
+        try (FileInputStream fileInputStream = new FileInputStream(
+                savePath); ObjectInputStream objectInputStream = new ObjectInputStream(
+                fileInputStream)) {
             return (TaskList) objectInputStream.readObject();
         } catch (IOException | ClassNotFoundException | ClassCastException e) {
             System.out.println("No save file");
