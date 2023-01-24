051e25d31fff8231d772097140d5fab147c5a720
XylusChen <chenxinyang00@gmail.com>
2023-01-24 13:52:32 +0800

    Add Dates and Times support
    Duke can now parse date and time String inputs into LocalDateTime
    objects.

--- src/main/java/tasktypes/Deadline.java
+++ src/main/java/tasktypes/Deadline.java
@@ -1,11 +1,31 @@
 package tasktypes;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
 public class Deadline extends Task {
+    LocalDate byDate;
+    LocalTime byTime;
+    LocalDateTime byDateTime;
     String doneBy;
 
     public Deadline(String description, String by) {
         super(description);
-        this.doneBy = by;
+        String[] dateAndTime = by.split(" ");
+        String date = dateAndTime[0];
+        String time = dateAndTime[1];
+        date = date.replace('/','-');
+        time = time.substring(0, 2) + ':' + time.substring(2);
+
+        this.byDate = LocalDate.parse(date);
+        this.byTime = LocalTime.parse(time);
+        this.byDateTime = LocalDateTime.of(this.byDate, this.byTime);
+        this.doneBy = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM, FormatStyle.SHORT).
+                format(this.byDateTime);
+
         Task.numTask++;
     }
 
--- src/main/java/tasktypes/Event.java
+++ src/main/java/tasktypes/Event.java
@@ -1,18 +1,53 @@
 package tasktypes;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
 public class Event extends Task {
-    String start;
-    String end;
+    LocalDate endDate;
+    LocalTime endTime;
+    LocalDateTime endDateTime;
+    String endBy;
+
+    LocalDate startDate;
+    LocalTime startTime;
+    LocalDateTime startDateTime;
+    String startBy;
 
     public Event(String description, String start, String end) {
         super(description);
-        this.start = start;
-        this.end = end;
+        String[] startDateAndTime = start.split(" ");
+        String startDate = startDateAndTime[0];
+        String startTime = startDateAndTime[1];
+        startDate = startDate.replace('/','-');
+        startTime = startTime.substring(0, 2) + ':' + startTime.substring(2);
+
+        this.startDate = LocalDate.parse(startDate);
+        this.startTime = LocalTime.parse(startTime);
+        this.startDateTime = LocalDateTime.of(this.startDate, this.startTime);
+        this.startBy = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM, FormatStyle.SHORT).
+                format(this.startDateTime);
+
+        String[] endDateAndTime = end.split(" ");
+        String endDate = endDateAndTime[0];
+        String endTime = endDateAndTime[1];
+        endDate = endDate.replace('/','-');
+        endTime = endTime.substring(0, 2) + ':' + endTime.substring(2);
+
+        this.endDate = LocalDate.parse(endDate);
+        this.endTime = LocalTime.parse(endTime);
+        this.endDateTime = LocalDateTime.of(this.endDate, this.endTime);
+        this.endBy = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM, FormatStyle.SHORT).
+                format(this.endDateTime);
+
         Task.numTask++;
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + this.start + ", " + "to: " + this.end + ")";
+        return "[E]" + super.toString() + " (from: " + this.startBy + ", " + "to: " + this.endBy + ")";
     }
 }
