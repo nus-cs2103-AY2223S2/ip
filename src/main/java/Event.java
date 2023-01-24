c997522f75eebd8dfc7618cdff8e0336da429fa4
PeanutButters93 <lowjianfeng@hotmail.com>
2023-01-25 00:01:36 +0800

    Update Deadline and Event to use LocalDate

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,15 +1,20 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
-    protected String from;
-    protected String to;
+    protected LocalDate from;
+    protected LocalDate to;
 
     public Event(String description, String from, String to) {
         super(description);
-        this.from = from;
-        this.to = to;
+        this.from = LocalDate.parse(from);
+        this.to =  LocalDate.parse(to);
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
+        return "[E]" + super.toString() + " (from: "
+                + from.format(DateTimeFormatter.ofPattern("dd-MMM-YYYY (EEE)")) + " to: "
+                + to.format(DateTimeFormatter.ofPattern("dd-MMM-YYYY (EEE)")) + ")";
     }
 }
