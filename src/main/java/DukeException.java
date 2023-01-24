fcd0b7f20525416a137f9fbbd60b3eb26e2a7f73
wendyng0107 <79928462+wendy0107@users.noreply.github.com>
2023-01-25 00:26:26 +0800

    Modify DukeException to handle exceptions related to files

--- src/main/java/DukeException.java
+++ src/main/java/DukeException.java
@@ -3,9 +3,12 @@ import java.util.Arrays;
 
 public class DukeException extends Exception{
     protected ArrayList<String> commandError = new ArrayList<>(
-            Arrays.asList("todo", "deadline", "event"));
+            Arrays.asList("todo", "deadline", "event", "missing details"));
     protected ArrayList<String> indexError = new ArrayList<>(
             Arrays.asList("mark", "unmark", "delete"));
+    protected ArrayList<String> fileError = new ArrayList<>(
+            Arrays.asList("file already exists", "creating new file",
+                    "updating file", "saving changes to file", "file not found"));
     protected String errorMsg;
 
     public DukeException(String command) {
@@ -17,6 +20,10 @@ public class DukeException extends Exception{
             this.errorMsg = "This index is out of bounds!";
         } else if (command.equals("timing")) {
             this.errorMsg = "The task timings cannot be empty!";
+        } else if (command.equals("missing details")) {
+            this.errorMsg = "The task input is missing some details!";
+        } else if (fileError.contains(command)) {
+            this.errorMsg =  "An error occurred when/as " + command;
         } else {
             this.errorMsg = "I'm sorry but I don't know what that means :(";
         }
