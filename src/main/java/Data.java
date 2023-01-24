954b363c9df765543c08e0dfea31a498f7e547a0
Benny <benny.ong348@gmail.com>
2023-01-24 08:11:12 +0800

    Added delete function

---
+++ src/main/java/Data.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class Data {
+    ArrayList<Task> data = new ArrayList<>();
+
+    public void addEntry(Task task) {
+        this.data.add(task);
+        System.out.println("added: " + task.task);
+    }
+
+    public Task getEntry(int pos) {
+        return this.data.get(pos);
+    }
+    public void removeEntry(int pos) { this.data.remove(pos);}
+    public void mark(int pos) { this.data.get(pos).mark(); }
+    public void unmark(int pos) {
+        this.data.get(pos).unmark();
+    }
+    public int getNum() { return this.data.size();}
+
+    public void printData() {
+        for (int i = 0; i < this.data.size(); i++) {
+            System.out.println((i+1) + ". " + this.data.get(i).toString());
+        }
+    }
+}
--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Duke {
 
     public static void userInput() {
-        data hist = new data();
+        Data data = new Data();
         System.out.println("Greetings");
         Scanner user = new Scanner(System.in);
 
@@ -15,7 +15,7 @@ public class Duke {
             }
 
             if (input.equals("list")) {
-                hist.printHist();
+                data.printData();
                 continue;
             }
 
@@ -23,9 +23,9 @@ public class Duke {
                 char query = input.charAt(input.length() - 1);
                 int pos = Character.getNumericValue(query);
                 //error check for pos exceeding size
-                hist.unmark(pos-1);
+                data.unmark(pos-1);
                 System.out.println("Unmarked:");
-                System.out.println(hist.getHist(pos-1).toString());
+                System.out.println(data.getEntry(pos-1).toString());
                 continue;
             }
 
@@ -33,9 +33,20 @@ public class Duke {
                 char query = input.charAt(input.length() - 1);
                 int pos = Character.getNumericValue(query);
                 //error check for pos exceeding size
-                hist.mark(pos-1);
+                data.mark(pos-1);
                 System.out.println("Marked:");
-                System.out.println(hist.getHist(pos-1).toString());
+                System.out.println(data.getEntry(pos-1).toString());
+                continue;
+            }
+
+            if (input.contains("delete")) {
+                char query = input.charAt(input.length() - 1);
+                int pos = Character.getNumericValue(query);
+                //error check for pos exceeding size
+                Task del = data.getEntry(pos-1);
+                data.removeEntry(pos-1);
+                System.out.println("Deleted:");
+                System.out.println(del.toString());
                 continue;
             }
 
@@ -47,8 +58,8 @@ public class Duke {
                     System.out.println(e.getMessage());
                     continue;
                 }
-                hist.addHist(todo);
-                System.out.println(String.format("Now you have %d tasks in the list", hist.getNum()));
+                data.addEntry(todo);
+                System.out.println(String.format("Now you have %d tasks in the list", data.getNum()));
                 continue;
             }
 
@@ -60,8 +71,8 @@ public class Duke {
                     System.out.println(e.getMessage());
                     continue;
                 }
-                hist.addHist(event);
-                System.out.println(String.format("Now you have %d tasks in the list", hist.getNum()));
+                data.addEntry(event);
+                System.out.println(String.format("Now you have %d tasks in the list", data.getNum()));
                 continue;
             }
 
@@ -74,8 +85,8 @@ public class Duke {
                     continue;
                 }
 
-                hist.addHist(deadline);
-                System.out.println(String.format("Now you have %d tasks in the list", hist.getNum()));
+                data.addEntry(deadline);
+                System.out.println(String.format("Now you have %d tasks in the list", data.getNum()));
                 continue;
             }
             System.out.println("I do not understand your instructions...");
