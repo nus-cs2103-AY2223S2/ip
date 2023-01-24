8722a2108db4dd855f37caead24839261d1e1b44
Teo Weide <teoweide1999@gmail.com>
2023-01-24 23:58:22 +0800

    Automated Text UI Testing

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,3 @@
-
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -11,110 +9,123 @@ public class Duke {
         final String markAsDone = "Nice! I've marked this task as done: ";
         final String unMarkTask = "OK, I've marked this task as not done yet: ";
         final String addedTask = "Got it, I've added this task: ";
-
+        final String freeSpace = "----------------------------------------------";
 
         System.out.println("Hello from Bench Monster");
         System.out.println("What can I do for you?");
 
-            while (true) {
-                String type = s.nextLine();
-                String[] tokens = type.split("\\s+");
-                if (tokens[0].equalsIgnoreCase("bye")) {
-                    System.out.println("Bye. Hope to see you again soon!");
-                    break;
-                } else if (tokens[0].equalsIgnoreCase("list")) {
-                    showList(t);
-                } else if (tokens[0].equalsIgnoreCase("mark")) {
-                    try {
+        while (true) {
+            String type = s.nextLine();
+            String[] tokens = type.split("\\s+");
+            if (tokens[0].equalsIgnoreCase("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(freeSpace);
+                break;
+            } else if (tokens[0].equalsIgnoreCase("list")) {
+                showList(t);
+                System.out.println(freeSpace);
+            } else if (tokens[0].equalsIgnoreCase("mark")) {
+                try {
 
-                        int i = Integer.parseInt(tokens[1]) - 1;
-                        if(i >= t.size()) {
-                            throw new Exception();
-                        }
-                        t.get(i).mark();
-                        System.out.println(markAsDone);
-                        System.out.println(t.get(i).toString());
-                    } catch(Exception e) {
-                        System.out.println("Invalid value, there isn't this much tasks in the list :-( ");
-                    }
-
-                } else if (tokens[0].equalsIgnoreCase("unmark")) {
                     int i = Integer.parseInt(tokens[1]) - 1;
-                    try {
-                        if(i >= t.size()) {
-                            throw new Exception();
-                        }
-                        t.get(i).unmarked();
-                        System.out.println(unMarkTask);
-                        System.out.println(t.get(i).toString());
-                    } catch(Exception e) {
-                        System.out.println("Invalid value, there isn't this much tasks in the list :-( ");
+                    if(i >= t.size()) {
+                        throw new Exception();
                     }
-
-                } else if (tokens[0].equalsIgnoreCase("todo")) {
-                    try {
-                        if(!type.contains(" ")) {
-                            throw new Exception();
-                        }
-                        String des = type.substring(type.indexOf(" ")).replace(" ", "");
-                        Task newTask = new ToDo(des);
-                        t.add(newTask);
-                        System.out.println(addedTask);
-                        System.out.println(newTask);
-                        System.out.println("Now you have " + noOfTask(t) + " tasks in the list.");
-                    } catch(Exception e) {
-                        System.out.println("OOPS!!! The descripition of a todo cannot be empty");
-                    }
-
-                } else if (tokens[0].equalsIgnoreCase("deadline")) {
-                    try {
-                        if(!type.contains(" ")) {
-                            throw new Exception();
-                        }
-                        String des = type.substring(type.indexOf(" ")).replace(" ", "");
-                        String[] deadline = des.split("/");
-                        Deadline dead = new Deadline(deadline[0], deadline[1]);
-                        t.add(dead);
-                        System.out.println(addedTask);
-                        System.out.println(dead);
-                        System.out.println("Now you have " + noOfTask(t) + " tasks in the list.");
-                    } catch(Exception e) {
-                        System.out.println("OOPS!!! The description of a deadline cannot be empty");
-                    }
-
-                } else if (tokens[0].equalsIgnoreCase("event")) {
-                    try {
-                        if(!type.contains(" ")) {
-                            throw new Exception();
-                        }
-                        String des = type.substring(type.indexOf(" "));
-                        String[] events = des.split("/");
-                        Event e = new Event(events[0].trim(), events[1].trim(), events[2].trim());
-                        t.add(e);
-                        System.out.println(addedTask);
-                        System.out.println(e);
-                        System.out.println("Now you have " + noOfTask(t) + " tasks in the list.");
-                    } catch(Exception e) {
-                        System.out.println("OOPS!!! The description of an event cannot be empty");
-                    }
-
-                } else {
-                    System.out.println("OOPS!! I'm sorry, but I don't know what that means :-(");
+                    t.get(i).mark();
+                    System.out.println(markAsDone);
+                    System.out.println(t.get(i).toString());
+                    System.out.println(freeSpace);
+                } catch(Exception e) {
+                    System.out.println("Invalid value, there isn't this much tasks in the list :-( ");
+                    System.out.println(freeSpace);
                 }
+
+            } else if (tokens[0].equalsIgnoreCase("unmark")) {
+                int i = Integer.parseInt(tokens[1]) - 1;
+                try {
+                    if(i >= t.size()) {
+                        throw new Exception();
+                    }
+                    t.get(i).unmarked();
+                    System.out.println(unMarkTask);
+                    System.out.println(t.get(i).toString());
+                    System.out.println(freeSpace);
+                } catch(Exception e) {
+                    System.out.println("Invalid value, there isn't this much tasks in the list :-( ");
+                    System.out.println(freeSpace);
+                }
+
+            } else if (tokens[0].equalsIgnoreCase("todo")) {
+                try {
+                    if(!type.contains(" ")) {
+                        throw new Exception();
+                    }
+                    String des = type.substring(type.indexOf(" ")).trim();
+                    Task newTask = new ToDo(des);
+                    t.add(newTask);
+                    System.out.println(addedTask);
+                    System.out.println(newTask);
+                    System.out.println("Now you have " + noOfTask(t) + " tasks in the list.");
+                    System.out.println(freeSpace);
+                } catch(Exception e) {
+                    System.out.println("OOPS!!! The descripition of a todo cannot be empty");
+                    System.out.println(freeSpace);
+                }
+
+            } else if (tokens[0].equalsIgnoreCase("deadline")) {
+                try {
+                    if(!type.contains(" ")) {
+                        throw new Exception();
+                    }
+                    String des = type.substring(type.indexOf(" ")).replace(" ", "");
+                    String[] deadline = des.split("/");
+                    Deadline dead = new Deadline(deadline[0], deadline[1]);
+                    t.add(dead);
+                    System.out.println(addedTask);
+                    System.out.println(dead);
+                    System.out.println("Now you have " + noOfTask(t) + " tasks in the list.");
+                    System.out.println(freeSpace);
+                } catch(Exception e) {
+                    System.out.println("OOPS!!! The description of a deadline cannot be empty");
+                    System.out.println(freeSpace);
+                }
+
+            } else if (tokens[0].equalsIgnoreCase("event")) {
+                try {
+                    if(!type.contains(" ")) {
+                        throw new Exception();
+                    }
+                    String des = type.substring(type.indexOf(" "));
+                    String[] events = des.split("/");
+                    Event e = new Event(events[0].trim(), events[1].trim(), events[2].trim());
+                    t.add(e);
+                    System.out.println(addedTask);
+                    System.out.println(e);
+                    System.out.println("Now you have " + noOfTask(t) + " tasks in the list.");
+                    System.out.println(freeSpace);
+                } catch(Exception e) {
+                    System.out.println("OOPS!!! The description of an event cannot be empty");
+                    System.out.println(freeSpace);
+                }
+
+            } else {
+                System.out.println("OOPS!! I'm sorry, but I don't know what that means :-(");
+                System.out.println(freeSpace);
             }
         }
+    }
 
 
-        public static void showList(ArrayList<Task> tasks) {
-                for(int i=0; i < tasks.size(); i++) {
-                    int no = i + 1;
-                    System.out.println(no + "." + tasks.get(i));
-                }
+    public static void showList(ArrayList<Task> tasks) {
+        for(int i=0; i < tasks.size(); i++) {
+            int no = i + 1;
+            System.out.println(no + "." + tasks.get(i));
         }
+    }
 
-        public static int noOfTask(ArrayList<Task> tasks) {
-            return tasks.size();
-        }
+    public static int noOfTask(ArrayList<Task> tasks) {
+        return tasks.size();
+    }
 }
 
 class Task {
@@ -191,11 +202,6 @@ class Event extends Task {
 
     @Override
     public String toString() {
-       return icon + super.toString() + " (from:" + from + " to:" + to + ")";
+        return icon + super.toString() + " (from:" + from + " to:" + to + ")";
     }
 }
-
-
-
-
-
