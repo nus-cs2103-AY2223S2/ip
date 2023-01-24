a4cce1d252458bf828a10b0ea19f511c4d88f13d
LAPTOP-EG15LSJU\hungy <e0726431@u.nus.edu>
2023-01-25 01:04:08 +0800

    add level-8

--- src/main/java/Deadlines.java
+++ src/main/java/Deadlines.java
@@ -1,12 +1,17 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDate;
 public class Deadlines extends Task {
-
-    protected String deadline;
+    private static final DateTimeFormatter formatter =
+            DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+    protected LocalDateTime deadline;
     public Deadlines(String description, String deadline) {
         super(description);
-        this.deadline = deadline.substring(3);
+        this.deadline = LocalDateTime.parse(deadline.substring(3), formatter);
     }
     @Override
     public String toString() {
-        return "[D]" + super.toString() + String.format(" (by: %s)", this.deadline);
+        return "[D]" + super.toString() + String.format(" (by: %s)",
+                this.deadline.format(formatter));
     }
 }
--- src/main/java/Events.java
+++ src/main/java/Events.java
@@ -1,13 +1,20 @@
+import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 public class Events extends Task {
-    protected String start;
-    protected String end;
+    protected LocalDateTime start;
+    private static final DateTimeFormatter formatter =
+            DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+    protected LocalDateTime end;
     public Events(String description, String start, String end) {
         super(description);
-        this.start = start.substring(5);
-        this.end = end.substring(3);
+
+        this.start = LocalDateTime.parse(start.substring(5), formatter);
+        this.end = LocalDateTime.parse(end.substring(3), formatter);
     }
     @Override
     public String toString() {
-        return "[E]" + super.toString() + String.format(" (from: %s to: %s)", this.start, this.end);
+        return "[E]" + super.toString() + String.format(" (from: %s to: %s)", this.start.format(formatter),
+                this.end.format(formatter));
     }
 }
