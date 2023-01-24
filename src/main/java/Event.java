9144f96d3fc1017d2dbb77e8b7af487d9ff0998a
PawanPatil19 <pawankp1903@gmail.com>
2023-01-24 19:22:25 +0800

    Added feature to input date/time and print task on specific dates

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,7 +1,11 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
 public class Event extends Task{
-    private String to;
-    private String from;
-    public Event(String taskText, String from, String to) {
+    private LocalDateTime to;
+    private LocalDateTime from;
+    public Event(String taskText, LocalDateTime from, LocalDateTime to) {
         super(taskText);
         this.to = to;
         this.from = from;
@@ -9,10 +13,22 @@ public class Event extends Task{
 
     @Override
     public String toString() {
-        return String.format("[E]%s (from: %s to: %s)", super.toString(), this.from, this.to);
+        return String.format("[E]%s (from: %s to: %s)", super.toString(),
+                this.from.format(DateTimeFormatter.ofPattern("MMM dd yyyy hh:mm a")),
+                this.to.format(DateTimeFormatter.ofPattern("MMM dd yyyy hh:mm a")));
     }
 
     public String writeFile() {
-        return String.format("E|%s|%s|%s|%s", this.getCurrentStatus(), this.getTaskText(), this.from, this.to);
+        return String.format("E|%s|%s|%s|%s", this.getCurrentStatus(), this.getTaskText(),
+                this.from.format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm")),
+                this.to.format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm")));
+    }
+
+    public LocalDateTime getDate() {
+        return this.from;
+    }
+
+    public String getTaskType() {
+        return "E";
     }
 }
