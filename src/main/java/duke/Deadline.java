d984b1203bf509f23e8d146390725f559a4b3842
zhacatomn <zhacatomn@gmail.com>
2023-01-24 17:51:10 +0800

    Complete level 8

--- src/main/java/duke/Deadline.java
+++ src/main/java/duke/Deadline.java
@@ -1,7 +1,14 @@
 package duke;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Deadline extends Task {
     protected String by;
+    protected LocalDateTime byObj;
+    protected static DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("d/MM/yyyy HHmm");
+    protected static DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("MMM d yyyy, hh:mma");
     public Deadline(String description, String by) throws DukeException {
         super(description.trim());
         this.by = by.trim();
@@ -11,6 +18,11 @@ public class Deadline extends Task {
         if (this.by.equals("")) {
             throw new DukeException("The 'by' date of a deadline cannot be empty.");
         }
+        try {
+            byObj = LocalDateTime.parse(by, inputFormatter);
+        } catch (DateTimeParseException e) {
+            byObj = null;
+        }
     }
 
     /**
@@ -19,6 +31,10 @@ public class Deadline extends Task {
      */
     @Override
     public String toString() {
-        return String.format("[D]%s (by: %s)", super.toString(), by);
+        return String.format(
+                "[D]%s (by: %s)",
+                super.toString(),
+                byObj == null ? by : byObj.format(outputFormatter)
+        );
     }
 }
--- src/main/java/duke/Event.java
+++ src/main/java/duke/Event.java
@@ -1,8 +1,16 @@
 package duke;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Event extends Task {
     protected String startTime;
+    protected LocalDateTime startObj;
     protected String endTime;
+    protected LocalDateTime endObj;
+    protected static DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("d/MM/yyyy HHmm");
+    protected static DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("MMM d yyyy, hh:mma");
     public Event(String description, String startTime, String endTime) throws DukeException {
         super(description.trim());
         this.startTime = startTime.trim();
@@ -13,6 +21,16 @@ public class Event extends Task {
         if (this.startTime.equals("") || this.endTime.equals("")) {
             throw new DukeException("The start and/or end time of a deadline cannot be empty.");
         }
+        try {
+            startObj = LocalDateTime.parse(startTime, inputFormatter);
+        } catch (DateTimeParseException e) {
+            startObj = null;
+        }
+        try {
+            endObj = LocalDateTime.parse(endTime, inputFormatter);
+        } catch (DateTimeParseException e) {
+            endObj = null;
+        }
     }
     /**
      * Represent duke.Event as a string
@@ -20,6 +38,11 @@ public class Event extends Task {
      */
     @Override
     public String toString() {
-        return String.format("[E]%s (from: %s to: %s)", super.toString(), startTime, endTime);
+        return String.format(
+                "[E]%s (from: %s to: %s)",
+                super.toString(),
+                startObj == null ? startTime : startObj.format(outputFormatter),
+                endObj == null ? endTime : endObj.format(outputFormatter)
+        );
     }
 }
