919110e5ecabd7690510380a09e47b7e9857430f
cydtseng <tsengchenyu@gmail.com>
2023-01-24 23:29:39 +0800

    Add additional constructor for Deadline class

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,10 +1,18 @@
 public class Deadline extends Task{
-    protected String by;
+    private String by;
     public Deadline(String description, String by){
         super(description);
         this.by = by;
     }
 
+    public Deadline(String description, String by, boolean isDone){
+        super(description, isDone);
+        this.by = by;
+    }
+
+    public String getBy(){
+        return this.by;
+    }
     @Override
     public String toString() {
         return "[D]" + "[" + super.getStatusIcon() + "] " + super.toString() + " (by: " + by + ")";
