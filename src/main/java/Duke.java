fe961fed5e4ddbd30aeb3bff0cf794da224e77c3
pzaiming <zaiming.pan@gmail.com>
2023-01-24 17:55:16 +0800

    branch-Level-7

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,13 @@
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Duke {
+    private static ArrayList<Task> tasklst = new ArrayList<>();
 
     public enum Cmdtype {
         mark,
@@ -24,7 +30,6 @@ public class Duke {
         System.out.println(logo + "\nHello! I'm Duke\nWhat can I do for you?");
         System.out.println("--------------------------------------------------------------------");
 
-        ArrayList<Task> lst = new ArrayList<>();
         Scanner input = new Scanner(System.in);
         while (input.hasNextLine()) {
             String cmd = input.nextLine();
@@ -35,64 +40,70 @@ public class Duke {
                 String c = cmd.split(" ")[0];
                 Cmdtype cmdtype = Cmdtype.valueOf(c);
                 switch (cmdtype) {
-                    case list:
-                        int counter = 1;
-                        for (Task tmp : lst) {
-                            System.out.println(counter++ + ". " + tmp.toString());
-                        }
-                        break;
-                    case mark:
-                        int id1 = Integer.parseInt(cmd.split(" ")[1]);
-                        Task t1 = lst.get(id1 - 1);
-                        t1.mark();
-                        System.out.println("Nice! I've marked this task as done:\n" + t1);
-                        break;
-                    case unmark:
-                        int id2 = Integer.parseInt(cmd.split(" ")[1]);
-                        Task t2 = lst.get(id2 - 1);
-                        t2.unmark();
-                        System.out.println("OK, I've marked this task as not done yet:\n" + t2);
-                        break;
-                    case todo:
-                        String activity1 = cmd.substring(c.length() + 1);
-                        Task t3 = new Todo(activity1);
-                        lst.add(t3);
-                        System.out.println("Got it. I've added this task:\n" + t3
-                                + "\n Now you have " + lst.size() + " tasks in the list.");
-                        break;
-                    case deadline:
-                        int indexOfBy = cmd.indexOf("/by ");
-                        int indexOfDate = indexOfBy + 4; // "/by "
-                        String activity2 = cmd.substring(c.length() + 1, indexOfBy - 1);
-                        String date = cmd.substring(indexOfDate);
-                        Task t4 = new Deadline(activity2, date);
-                        lst.add(t4);
-                        System.out.println("Got it. I've added this task:\n" + t4
-                                + "\n Now you have " + lst.size() + " tasks in the list.");
-                        break;
-                    case event:
-                        int indexOfFrom = cmd.indexOf("/from ");
-                        int indexOfFromTime = indexOfFrom + 6;
-                        int indexOfTo = cmd.indexOf("/to ");
-                        int indexOfToTime = indexOfTo + 4;
-                        String activity = cmd.substring(c.length() + 1, indexOfFrom - 1);
-                        String from = cmd.substring(indexOfFromTime, indexOfTo - 1);
-                        String to = cmd.substring(indexOfToTime);
-                        Task t5 = new Event(activity, from, to);
-                        lst.add(t5);
-                        System.out.println("Got it. I've added this task:\n" + t5
-                                + "\n Now you have " + lst.size() + " tasks in the list.");
-                        break;
-                    case delete:
-                        int id3 = Integer.parseInt(cmd.split(" ")[1]);
-                        Task t6 = lst.remove(id3 - 1);
-                        System.out.println("Noted. I've removed this task:\n" + t6
-                                + "\n Now you have " + lst.size() + " tasks in the list.");
-                        break;
-                    case bye:
-                        System.out.println("Bye. Hope to see you again soon!");
-                        System.out.println("--------------------------------------------------------------------");
-                        return;
+                case list:
+                    int counter = 1;
+                    for (Task tmp : tasklst) {
+                        System.out.println(counter++ + ". " + tmp.toString());
+                    }
+                    break;
+                case mark:
+                    int id1 = Integer.parseInt(cmd.split(" ")[1]);
+                    Task t1 = tasklst.get(id1 - 1);
+                    t1.mark();
+                    System.out.println("Nice! I've marked this task as done:\n" + t1);
+                    updateTasklist();
+                    break;
+                case unmark:
+                    int id2 = Integer.parseInt(cmd.split(" ")[1]);
+                    Task t2 = tasklst.get(id2 - 1);
+                    t2.unmark();
+                    System.out.println("OK, I've marked this task as not done yet:\n" + t2);
+                    updateTasklist();
+                    break;
+                case todo:
+                    String activity1 = cmd.substring(c.length() + 1);
+                    Task t3 = new Todo(activity1);
+                    tasklst.add(t3);
+                    System.out.println("Got it. I've added this task:\n" + t3
+                            + "\n Now you have " + tasklst.size() + " tasks in the list.");
+                    updateTasklist();
+                    break;
+                case deadline:
+                    int indexOfBy = cmd.indexOf("/by ");
+                    int indexOfDate = indexOfBy + 4; // "/by "
+                    String activity2 = cmd.substring(c.length() + 1, indexOfBy - 1);
+                    String date = cmd.substring(indexOfDate);
+                    Task t4 = new Deadline(activity2, date);
+                    tasklst.add(t4);
+                    System.out.println("Got it. I've added this task:\n" + t4
+                            + "\n Now you have " + tasklst.size() + " tasks in the list.");
+                    updateTasklist();
+                    break;
+                case event:
+                    int indexOfFrom = cmd.indexOf("/from ");
+                    int indexOfFromTime = indexOfFrom + 6;
+                    int indexOfTo = cmd.indexOf("/to ");
+                    int indexOfToTime = indexOfTo + 4;
+                    String activity = cmd.substring(c.length() + 1, indexOfFrom - 1);
+                    String from = cmd.substring(indexOfFromTime, indexOfTo - 1);
+                    String to = cmd.substring(indexOfToTime);
+                    Task t5 = new Event(activity, from, to);
+                    tasklst.add(t5);
+                    System.out.println("Got it. I've added this task:\n" + t5
+                            + "\n Now you have " + tasklst.size() + " tasks in the list.");
+                    updateTasklist();
+                    break;
+                case delete:
+                    int id3 = Integer.parseInt(cmd.split(" ")[1]);
+                    Task t6 = tasklst.remove(id3 - 1);
+                    System.out.println("Noted. I've removed this task:\n" + t6
+                            + "\n Now you have " + tasklst.size() + " tasks in the list.");
+                    updateTasklist();
+                    break;
+                case bye:
+                    System.out.println("Bye. Hope to see you again soon!");
+                    System.out.println("--------------------------------------------------------------------");
+                    return;
                 }
             } catch (DukeException e) {
                 System.out.println(e);
@@ -103,8 +114,7 @@ public class Duke {
         }
     }
 
-    public static boolean checkCommand(String cmd) throws DukeException
-    {
+    public static boolean checkCommand(String cmd) throws DukeException {
         String cmdtype = cmd.split(" ")[0];
         // check that command list must not have any more description
         if (cmdtype.equals("list") && cmdtype.length() != cmd.length()) {
@@ -134,5 +144,27 @@ public class Duke {
             }
         }
         return true;
+    }
+
+    public static void updateTasklist() {
+        String DIRECTORY = "./data";
+        String FILENAME = "duke.txt";
+        try {
+            File directory = new File(DIRECTORY);
+            if (!directory.exists()){
+                directory.mkdir();
+            }
+            StringBuilder res = new StringBuilder();
+            int counter = 1;
+            for (Task tmp : tasklst) {
+                res.append(counter++ + ". " + tmp.toString() + "\n");
+            }
+            FileWriter myWriter = new FileWriter(DIRECTORY + "/" + FILENAME);
+            myWriter.write(res.toString());
+            myWriter.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     }
 }
