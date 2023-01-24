34d9b94e3b0249ab1ee7242ff9ae4eea308f988c
JavonTeo <javonteo@hotmail.com>
2023-01-24 11:26:54 +0800

    Level-8

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,6 +1,11 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Deadline extends Task {
-    protected String by;
-    public Deadline(String description, String by) {
+    protected LocalDateTime by;
+
+    public Deadline(String description, LocalDateTime by) {
         super(description);
         this.by = by;
     }
@@ -8,12 +13,23 @@ public class Deadline extends Task {
     @Override
     public String toString() {
         return "[D]" + super.getStatusIcon() + " "
-                + super.getDescription() + "(by: " + this.by + ")";
+                + super.getDescription() + "(by: " + this.by.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
     }
 
     public static Deadline addDeadline(String details) {
         String description = details.substring(0, details.indexOf("/by"));
-        String by = details.substring(details.indexOf("/by") + "/by".length() + 1);
+        String byString = details.substring(details.indexOf("/by") + "/by".length() + 1);
+
+        String[] possibleFormats = {"yyyy-MM-dd", "dd/MM/yyyy HHmm", "d/MM/yyyy HHmm", "MMM dd yyyy", "MMMM dd yyyy"};
+        LocalDateTime by = null;
+        for (int i = 0; i < possibleFormats.length; i++) {
+            try {
+                by = LocalDateTime.parse(byString, DateTimeFormatter.ofPattern(possibleFormats[i]));
+                break;
+            } catch (DateTimeParseException ignored) {
+            }
+        }
+
         Deadline t = new Deadline(description, by);
         System.out.println("Got it. I've added this task:");
         System.out.println(t);
