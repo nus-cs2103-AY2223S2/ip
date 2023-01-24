f975ec22edfc252f2587718952c76ec0c168d5c5
Sachin Kalinga <>
2023-01-24 23:26:45 +0800

    Level 1 added

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,48 @@
+import java.util.Scanner;
+import javafx.stage.Stage;
+import java.io.PrintWriter;
 public class Duke {
     public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+
+
+
+
+        horizontalLine();
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello there! I am: \n" + logo + "\nWhat can I help you with!");
+        System.out.println("You can let me know by typing it below, please!");
+        horizontalLine();
+
+        String userInput = scanner.nextLine();
+
+        while (!userInput.equals("bye")) {
+            horizontalLine();
+            System.out.println("    " + userInput);
+            horizontalLine();
+            userInput = scanner.nextLine();
+        }
+
+        System.out.println("Bye for now! Hope to see you again!");
+    }
+
+    public static void horizontalLine() {
+
+
+        for (int i = 0; i < 50; i++) {
+            char horizontalBar = '\u2015';
+            System.out.print(horizontalBar);
+
+        }
+
+
+        System.out.print("\n");
+
     }
 }
