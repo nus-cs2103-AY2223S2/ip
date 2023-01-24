38c6d7f71fdde740375a4af7a88dd3879026f896
Shane Duggan <>
2023-01-24 16:22:44 +0800

    no message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        String[] storedList = new String[100];
+        int index = 0;
         System.out.println("Hello I'm Duke\nWhat can I do for you?");
         Scanner scan = new Scanner(System.in);
         
@@ -12,7 +14,15 @@ public class Duke {
                 scan.close();
                 return;
             }
-            System.out.println(textInput);
+            if (textInput.equals("list")) {
+                for (int i = 0; i < index; i++) {
+                    System.out.println(i + 1 + ". " + storedList[i]);
+                }
+                continue;
+            }
+            storedList[index] = textInput;
+            index++;
+            System.out.println("added: " + textInput);
         }
     }
 }
