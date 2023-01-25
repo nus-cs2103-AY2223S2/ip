170389619fe8015ad8c2711ce38f063d0e4d0c6c
Isabel Chong <IsabelChong.CYJI@gmail.com>
2023-01-25 16:54:00 +0800

    Add interpreter methods for retrieving from file or saving to file.

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,4 +1,4 @@
-public class Task {
+public abstract class Task {
     protected boolean isDone;
     protected String description;
 
@@ -21,9 +21,22 @@ public class Task {
         return isDone ? "X" : " ";
     }
 
+    public abstract String interpretTaskToString();
+
+    public static Task interpretStringToTask(String s) {
+        String isDoneStatus = s.split(" \\| ")[1];
+        boolean isDone = isDoneStatus.equals("X") ? true : false;
+        if (s.split(" \\| ").length == 3) {
+            return new Todo(s.split(" \\| ")[2], isDone);
+        } else if (s.split(" \\| ").length == 4) {
+            return new Deadline(s.split(" \\| ")[2], isDone, s.split(" \\| ")[3]);
+        } else {
+            return new Event(s.split(" \\| ")[2], isDone, s.split(" \\| ")[3], s.split(" \\| ")[4]);
+        }
+    }
+
     @Override
     public String toString() {
-        String output = "[" + getStatusIcon() + "] " + this.description;
-        return output;
+        return "[" + getStatusIcon() + "] " + description;
     }
 }
