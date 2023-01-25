5d8b2ea5960d82558f72998139cfc56bd9e190f9
ScorpiusSigma <justinleng_@hotmail.com>
2023-01-25 23:04:48 +0800

    Database class: Remove prints

--- src/main/java/duke/database/Database.java
+++ src/main/java/duke/database/Database.java
@@ -9,7 +9,6 @@ public class Database<T> {
     public void listItems() {
         int size = this.size();
 
-        System.out.println("Here are the tasks in your list:");
         for (int i = 0; i < size; i++)
             System.out.println((i + 1) + ". " + this.getItem(i));
 
@@ -18,10 +17,6 @@ public class Database<T> {
 
     public void addItem(T item) {
         this.items.add(item);
-
-        System.out.println("Got it. I've added this task:");
-        System.out.println(item);
-        System.out.println("Now you have " + this.size() + " tasks in the list.\n");
     }
 
     public T getItem(int id) {
@@ -34,10 +29,7 @@ public class Database<T> {
         }
 
         T task = this.items.remove(id);
-        System.out.println("Noted. I've removed this task:");
-        System.out.println(task);
-        System.out.println("Now you have " + this.size() + " tasks in the list.\n");
-
+        
         return task;
     }
 
