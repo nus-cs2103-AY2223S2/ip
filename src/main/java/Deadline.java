a4c757841ff7c849b3cb71526a1fcd905ab37460
Jin <saofalling@gmail.com>
2023-01-25 10:24:18 +0800

    Level 8
    Used LocalDateTime for variables handling date and time.
    
    Date and time of the deadline and event can be used more meaningfully.

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,13 +1,16 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
-    private String deadline;
+    private LocalDateTime deadline;
 
     public Deadline(String input, String deadline) {
         super(input);
-        this.deadline = deadline;
+        this.deadline = LocalDateTime.parse(deadline, DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm"));
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + "(by: " + this.deadline + ")";
+        return "[D]" + super.toString() + "(by: " + this.deadline.format(DateTimeFormatter.ofPattern("dd MMM yyyy hh:mm a")) + ")";
     }
 }
