dc06d5f62f848aae1ab87b799113396cc177254c
daitenshionyan <snivy.oshowott@gmail.com>
2023-01-25 20:09:15 +0800

    UiManager: add documentation

--- src/main/java/aqua/manager/UiManager.java
+++ src/main/java/aqua/manager/UiManager.java
@@ -12,53 +12,81 @@ import aqua.exception.LoadException;
 import aqua.exception.ProcedureExecutionException;
 
 public class UiManager {
+    /** Line separator. */
     private static String SEPARATOR =
             "____________________________________________________________";
     
+    /** Greeting message. */
     private static final String MESSAGE_GREETING = 
             "Konaqua~~ Perfect idol gamer nekomimi super maid Minato Aqua desu!";
+    /** Load success message. */
     private static final String MESSAGE_LOAD_SUCCESS =
             "I remembered all your previous tasks! Praise me";
     
+    /** Syntax exception message format. */
     private static final String EXCEPTION_FORMAT_SYNTAX =
             "Hanya??\n" +
             "I do not understand because:\n" +
             "  %s\n" +
             "Gomennasai!!";
+    /** Execution exception message format. */
     private static final String EXCEPTION_FORMAT_EXECUTION = 
             "Hanya??\n" +
             "I was doing what you told me to do half way but messed up because:\n" +
             "  %s\n" +
             "Gomennasai!!";
+    /** Load exception message format. */
     private static final String EXCEPTION_FORMAT_LOAD = 
             "Hanya??\n" +
             "I was looking through my notes and could not remember your previous tasks because:\n" +
             "  %s\n" +
             "Gomennasai!! But you did not touch it right?";
+    /** All other exception message format. */
     private static final String EXCEPTION_FORMAT_DEATH =
             "UWAWAWA!!\n" +
             "I messed up big time...\n" +
             "  %s";
     
+    /** Reader to read input from. */
     private final BufferedReader reader;
 
 
+    /**
+     * Constructs the default UiManager that reads input from the
+     * {@link System#in} input stream.
+     */
     public UiManager() {
         this(System.in);
     }
 
 
+    /**
+     * Constructs a UiManager that reads inputs from the given input stream.
+     * 
+     * @param inStream - the input stream to read inputs from.
+     */
     public UiManager(InputStream inStream) {
         reader = new BufferedReader(new InputStreamReader(inStream));
     }
     
 
+    /**
+     * Reads a line from the set input stream.
+     * 
+     * @return a line in the set input stream.
+     * @throws IOException if an I/O error occurs.
+     */
     public String readLine() throws IOException {
         String msg = reader.readLine();
         return Optional.ofNullable(msg).orElse("");
     }
     
     
+    /**
+     * Formats and prints exception.
+     * 
+     * @param ex - the exception to print.
+     */
     public void replyException(Throwable ex) {
         reply(getExceptionReply(ex));
     }
@@ -80,6 +108,7 @@ public class UiManager {
     }
 
 
+    /** Prints the greeting message. */
     public void greet() {
         reply(getMessageGreeting());
     }
@@ -90,6 +119,9 @@ public class UiManager {
     }
 
 
+    /**
+     * Prints the message to signal that tasks have successfully been loaded.
+     */
     public void replyLoadSuccess() {
         reply(getMessageLoadSuccess());
     }
@@ -100,6 +132,11 @@ public class UiManager {
     }
 
 
+    /**
+     * Formats and prints the message.
+     * 
+     * @param msg - the message to print.
+     */
     public void reply(String msg) {
         System.out.println(formatMessage(msg));
     }
