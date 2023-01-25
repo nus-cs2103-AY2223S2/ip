f67291ed82f1db1fdb56a23ea845567002af6eb2
gyulong1 <48115998+gyulong1@users.noreply.github.com>
2023-01-26 00:32:12 +0800

    Add Level-8

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,16 +1,22 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
 
-    protected String from;
-    protected String to;
+    protected LocalDate from;
+    protected LocalDate to;
 
     public Event(String description, String from, String to) {
         super(description);
-        this.from = from;
-        this.to = to;
+        this.from = LocalDate.parse(from);
+        this.to = LocalDate.parse(to);
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + "(from: " + from + " to: " + to + ")";
+        String fromStr = from.format(DateTimeFormatter.ofPattern("MMM dd yyyy"));
+        String toStr = to.format(DateTimeFormatter.ofPattern("MMM dd yyyy"));
+
+        return "[E]" + super.toString() + "(from: " + fromStr + " to: " + toStr + ")";
     }
 }
