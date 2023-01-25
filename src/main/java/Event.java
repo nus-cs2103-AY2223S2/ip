cfde289abef8913f5f094ad42d3e6f913b38f5b5
cydtseng <tsengchenyu@gmail.com>
2023-01-25 10:34:16 +0800

    Change constructor and toString method for Event class

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,15 +1,31 @@
-public class Event extends Task{
-    protected String from;
-    protected String to;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
-    public Event(String description, String from, String to){
+public class Event extends Task{
+    private LocalDate eventStartDate;
+    private LocalDate eventEndDate;
+    private Date eventStartTime;
+    private Date eventEndTime;
+
+    public Event(String description, LocalDate eventStartDate, Date eventStartTime, LocalDate eventEndDate, Date eventEndTime){
         super(description);
-        this.from = from;
-        this.to = to;
+        this.eventStartDate = eventStartDate;
+        this.eventEndDate = eventEndDate;
+        this.eventStartTime = eventStartTime;
+        this.eventEndTime = eventEndTime;
     }
 
     @Override
     public String toString() {
-        return "[E]" + "[" + super.getStatusIcon() + "] " + super.toString() + " (from: " + this.from + " to: " + this.to +")";
+
+        String formattedStartDate = eventStartDate.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+        String formattedEndDate = eventEndDate.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+        String formattedStartTime = new SimpleDateFormat("h:mm a").format(eventStartTime);
+        String formattedEndTime = new SimpleDateFormat("h:mm a").format(eventEndTime);
+        String eventStartCombined = formattedStartDate + " " + formattedStartTime;
+        String eventEndCombined = formattedEndDate + " " + formattedEndTime;
+        return "[E]" + "[" + super.getStatusIcon() + "] " + super.toString() + " (from: " + eventStartCombined + " to: " + eventEndCombined +")";
     }
 }
