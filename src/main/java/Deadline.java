0fde8014694bcbf595ebc3ba92d56203413dcfbf
Oliver Loo <oliverloo09@gmail.com>
2023-01-26 01:55:52 +0800

    add minimal local date time feature to deadline

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,27 +1,36 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static java.time.format.DateTimeFormatter.ofPattern;
+
 public class Deadline extends Tasks {
 
-    private String deadline;
+    private LocalDateTime deadline;
 
     public Deadline(String content, String deadline) {
         super(content);
-        this.deadline = deadline;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        this.deadline = LocalDateTime.parse(deadline.trim(), formatter);
 
     }
 
     public Deadline(boolean isMarked, String content, String deadline) {
         super(isMarked, content);
-        this.deadline = deadline;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        this.deadline = LocalDateTime.parse(deadline.trim(), formatter);
     }
 
     @Override
     public String addDivider() {
         String d = " | ";
         int marked = this.isMarked() ? 1 : 0;
-        return "D" + d + marked + d + get_content() + d + deadline;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        return "D" + d + marked + d + get_content() + d + deadline.format(formatter);
     }
 
     @Override
     public String toString() {
-        return "[D] " + super.toString() + "(by: " + this.deadline + ")";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd yyyy HH a");
+        return "[D] " + super.toString() + "(by: " + this.deadline.format(formatter) + ")";
     }
 }
