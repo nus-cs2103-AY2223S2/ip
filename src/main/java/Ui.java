e1e1945163dd29a853421438ce00c73e0051b47a
Sean Wong <58366602+ExtraShotLatte@users.noreply.github.com>
2023-01-26 02:04:17 +0800

    Add UI class

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Ui {
-    public Ui() {
-        
+
+    private String name;
+    private Scanner sc;
+
+
+    public Ui(String name) {
+        this.name = name;
+        sc = new Scanner(System.in);
     }
+
+    public void printGreetings() {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Welcome to: \n" + logo);
+        System.out.println("Hello! I'm " + name + " !");
+    }
+
+
+
 }
