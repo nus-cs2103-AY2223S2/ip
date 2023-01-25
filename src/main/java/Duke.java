5c500317bd7c79e5b4c07d5a3b99d3184e120753
Shane Duggan <>
2023-01-25 20:18:47 +0800

    no message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,13 +3,12 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Task[] taskList = new Task[100];
-        int index = 1;
+        int index = 0;
         System.out.println("Hello I'm Duke\nWhat can I do for you?");
         Scanner scan = new Scanner(System.in);
         
         while (true) {
             String textInput = scan.nextLine();
-            Task t = new Task(textInput);
 
             if (textInput.equalsIgnoreCase("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
@@ -19,8 +18,8 @@ public class Duke {
 
             if (textInput.equalsIgnoreCase("list")) {
                 System.out.println("Here are the tasks in your list:");
-                for (int i = 1; i < index; i++) {
-                    System.out.println(i + ". " + taskList[i-1].toString());
+                for (int i = 0; i < index; i++) {
+                    System.out.println(i + 1 + ". " + taskList[i].toString());
                 }
                 continue;
             }
@@ -28,7 +27,7 @@ public class Duke {
             if (textInput.length() >= 6 && 
                 textInput.substring(0, 4).equalsIgnoreCase("mark")) {
                 int i = Integer.parseInt(textInput.substring(5));
-                Task currTask = taskList[i-1];
+                Task currTask = taskList[i - 1];
                 currTask.markDone();
                 System.out.println("Nice! I've marked this task as done\n" + currTask.toString());
                 continue;
@@ -37,15 +36,40 @@ public class Duke {
             if (textInput.length() >= 8 &&
                 textInput.substring(0, 6).equalsIgnoreCase("unmark")) {
                 int i = Integer.parseInt(textInput.substring(7));
-                Task currTask = taskList[i-1];
+                Task currTask = taskList[i - 1];
                 currTask.markUndone();
                 System.out.println("OK, I've marked this task as not done yet:\n" + currTask.toString());
                 continue;
             }
 
-            taskList[index-1] = t;
-            index++;
-            System.out.println("added: " + textInput);
+            if (textInput.length() >= 6 && 
+                textInput.substring(0, 4).equalsIgnoreCase("todo")) {
+                Task t = new Task.Todo(textInput.substring(5));
+                taskList[index] = t;
+                index++;
+                String output = String.format("Got it. I've added this task:\n%s\nNow you have %d tasks in the list", t.toString(), index);
+                System.out.println(output);
+            }
+
+            if (textInput.length() >= 10 && 
+                textInput.substring(0, 8).equalsIgnoreCase("deadline")) {
+                String[] parts = textInput.split("/");
+                Task t = new Task.Deadline(parts[0].substring(9), parts[1].substring(3));
+                taskList[index] = t;
+                index++;
+                String output = String.format("Got it. I've added this task:\n%s\nNow you have %d tasks in the list", t.toString(), index);
+                System.out.println(output);
+            }
+
+            if (textInput.length() >= 7 && 
+                textInput.substring(0, 5).equalsIgnoreCase("event")) {
+                String[] parts = textInput.split("/");
+                Task t = new Task.Event(parts[0].substring(6), parts[1].substring(5), parts[2].substring(3));
+                taskList[index] = t;
+                index++;
+                String output = String.format("Got it. I've added this task:\n%s\nNow you have %d tasks in the list", t.toString(), index);
+                System.out.println(output);
+            }
         }
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -7,6 +7,51 @@ public class Task {
       this.isDone = false;
   }
 
+  public static class Todo extends Task {
+
+    public Todo(String description) {
+        super(description);
+    }
+
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
+  }
+
+  public static class Deadline extends Task {
+
+    protected String by;
+
+    public Deadline(String description, String by) {
+        super(description);
+        this.by = by;
+    }
+
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + " (by: " + by + ")";
+    }
+  }
+
+  public static class Event extends Task {
+
+    protected String from;
+    protected String to;
+
+    public Event(String description, String from, String to) {
+        super(description);
+        this.from = from;
+        this.to = to;
+
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + " (from: " + from + "to: " + to + ")";
+    }
+  }
+
   public String getStatusIcon() {
       return (isDone ? "X" : " "); // mark done task with X
   }
@@ -19,6 +64,7 @@ public class Task {
       this.isDone = false;
   }
 
+  @Override
   public String toString() {
       String result = String.format("[%s] %s", this.getStatusIcon(), this.description);
       return result;
