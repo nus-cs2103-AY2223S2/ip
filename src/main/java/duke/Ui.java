786a7544c28f8e5980f96563e661a4d6d68421b5
Lanreath <tyfei00@gmail.com>
2023-01-25 21:41:33 +0800

    Added find command

--- src/main/java/duke/Ui.java
+++ src/main/java/duke/Ui.java
@@ -58,6 +58,13 @@ public class Ui{
         System.out.println("Now you have " + size + " tasks in the list.");
     }
 
+    public void showFind(TaskList list) {
+        System.out.println("Here are the matching tasks in your list:");
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println((i+1) + ". " + list.get(i));
+        }
+    }
+
     public void showLoadingError() {
         System.out.println("Error loading file!");
     }
