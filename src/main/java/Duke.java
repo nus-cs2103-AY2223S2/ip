708998d01d3aa7d6cbb9454e850222fb1b9d647f
gitsac <isaacchai99@gmail.com>
2023-01-25 12:02:57 +0800

    Add level-7 Increment

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,15 @@
 import java.io.InputStreamReader;
 import java.io.*;
 import java.util.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.nio.file.Files;
 
 public class Duke {
 
     static List<Task> storedText = new ArrayList<Task>();
+
     public static void main(String[] args) throws IOException, DukeExceptions, StringIndexOutOfBoundsException{
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -14,12 +19,114 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         greeting();
         BufferedReader bReader = new BufferedReader(new InputStreamReader(System.in));
+        Path fileData = getData();
+        storedText = loadTask(fileData);
         processUserInput(bReader);
+        storeTask(fileData);
         System.out.println("Bye! Hope to see you again soon!");
     }
 
     public static void greeting() {
         System.out.println("Hello, I am Duke!\nWhat can I do for you?");
+    }
+
+    public static Path getData() throws IOException{
+        //Check if directory AND file exists.
+        Path pathToCheck = Paths.get("..", "..", "..", "data");
+        if (!Files.exists(pathToCheck)) {
+            Files.createDirectory(pathToCheck);
+        }
+
+        Path fileToCheck = Paths.get("..", "..", "..", "data", "dukedata.txt");
+        if (!Files.exists(fileToCheck)) {
+            Files.createFile(fileToCheck);
+        }
+
+        return fileToCheck;
+    }
+
+    public static ArrayList<Task> loadTask(Path dataFile) throws IOException, DukeExceptions{
+        ArrayList<Task> useThis = new ArrayList<>();
+        Scanner scannerForFileData = new Scanner(dataFile);
+        if (!scannerForFileData.hasNext()) {
+            scannerForFileData.close();
+            return useThis;
+        }
+        while (scannerForFileData.hasNextLine()) {
+            String taskToLoad = scannerForFileData.nextLine();  
+            // String dataWithoutIndexes = taskToLoad.substring(taskToLoad.indexOf(".") + 1);
+            String dataWithoutIndexes = taskToLoad.substring(2);
+            Character isDone = dataWithoutIndexes.charAt(4);
+            Character taskType = dataWithoutIndexes.charAt(1);
+            Task toAdd = new Task("");
+
+            if (taskType.equals('T')) {
+                String nameOfTask = dataWithoutIndexes.substring(6);
+                toAdd = new ToDo(nameOfTask);
+                if (isDone.equals('X')) {
+                    toAdd.setDone();
+                }
+                useThis.add(toAdd);
+            }
+
+            if (taskType.equals('D')) {
+                String deadlineWithBy = dataWithoutIndexes.substring(dataWithoutIndexes.lastIndexOf("("));
+                String rawDate = deadlineWithBy.split("\\(by: ")[1].split("\\)")[0];
+                String[] nameSplitInArr = dataWithoutIndexes.substring(6).split(" ");
+                String nameOfTask = nameSplitInArr[0];
+                for (int i = 1; i < nameSplitInArr.length; i++) {
+                    if (nameSplitInArr[i].equals("(by:")) {
+                        break;
+                    }
+                    nameOfTask = nameOfTask + " " + nameSplitInArr[i];
+                }
+                String toInitialize = nameOfTask + " /by" + " " + rawDate;
+                toAdd = new Deadlines(toInitialize);
+                if (isDone.equals('X')) {
+                    toAdd.setDone();
+                }
+                useThis.add(toAdd);
+            }
+
+            if (taskType.equals('E')) {
+                String timeframe = dataWithoutIndexes.substring(dataWithoutIndexes.lastIndexOf("("));
+                String from = timeframe.substring(1).split(" to:")[0].substring(5);
+                String to = timeframe.substring(1).split("to: ")[1].split("\\)")[0];
+                String[] nameSplitInArr = dataWithoutIndexes.substring(6).split(" ");
+                String nameOfTask = nameSplitInArr[0];
+                for (int i = 1; i < nameSplitInArr.length; i++) {
+                    if (nameSplitInArr[i].equals("(from:")) {
+                        break;
+                    }
+                    nameOfTask = nameOfTask + " " + nameSplitInArr[i];
+                }
+                String toInitialize = nameOfTask + " /from" + from + " /to " + to;
+                toAdd = new Events(toInitialize);
+                if (isDone.equals('X')) {
+                    toAdd.setDone();
+                }
+                useThis.add(toAdd); 
+            }
+        }
+        scannerForFileData.close();
+        return useThis;
+    }
+
+    public static void storeTask(Path dataFile) throws IOException{
+        if (storedText.size() == 0) {
+            Files.write(dataFile, "".getBytes());
+            return;
+        }
+        Files.write(dataFile, "".getBytes());
+        Task firstTask = storedText.get(0);
+        String toSave = "1." + firstTask.toString() + " \n";
+        Files.write(dataFile, toSave.getBytes(), StandardOpenOption.APPEND);
+        for (int i = 1; i < storedText.size(); i++) {
+            Integer currIndex = i + 1;
+            Task currTask = storedText.get(i);
+            String toUse = currIndex.toString() + "." + currTask.toString() + " \n";
+            Files.write(dataFile, toUse.getBytes(), StandardOpenOption.APPEND);
+        }
     }
 
     public static void processUserInput(BufferedReader brToUse) throws IOException, DukeExceptions, StringIndexOutOfBoundsException{
