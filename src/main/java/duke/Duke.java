d56bc60426f06c5e87e38e11e67109ca3c3aab55
TamKengHong <tamkenghong@gmail.com>
2023-01-25 18:07:50 +0800

    Refactor

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -10,28 +10,29 @@ import java.io.IOException;
 
 public class Duke {
 
-  /**
-   * The main function for Duke, which runs the program.
-   * @param args commandline args.
-   * @throws IOException
-   */
-  public static void main(String[] args) throws IOException {
-    Ui ui = new Ui();
-    TaskList taskList = new TaskList();
-    Parser parser = new Parser();
-    Storage store = new Storage(taskList);
+    /**
+     * The main function for Duke, which runs the program.
+     *
+     * @param args commandline args.
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException {
+        Ui ui = new Ui();
+        TaskList taskList = new TaskList();
+        Parser parser = new Parser();
+        Storage store = new Storage(taskList);
 
-    ui.printGreeting();
-    String command = ui.readLine();
-    while (!command.equals("bye")) {
-      try {
-        parser.dispatch(command, ui, taskList);
-        store.saveTasks(taskList);
-      } catch (DukeException e) {
-        ui.printError(e);
-      }
-      command = ui.readLine();
+        ui.printGreeting();
+        String command = ui.readLine();
+        while (!command.equals("bye")) {
+            try {
+                parser.dispatch(command, ui, taskList);
+                store.saveTasks(taskList);
+            } catch (DukeException e) {
+                ui.printError(e);
+            }
+            command = ui.readLine();
+        }
+        ui.printBye();
     }
-    ui.printBye();
-  }
 }
--- src/main/java/duke/helper/Storage.java
+++ src/main/java/duke/helper/Storage.java
@@ -12,57 +12,59 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Storage {
-  private File f;
+    private File f;
 
-  public Storage(TaskList tl) throws IOException {
-    File dir = new File("data");
-    if (!dir.exists()) {
-      dir.mkdir();
+    public Storage(TaskList tl) throws IOException {
+        File dir = new File("data");
+        if (!dir.exists()) {
+            dir.mkdir();
+        }
+        File f = new File("data/taskData.txt");
+        if (!f.exists()) {
+            f.createNewFile();
+        }
+        this.f = f;
+        loadTasks(tl, f);
     }
-    File f = new File("data/taskData.txt");
-    if (!f.exists()) {
-      f.createNewFile();
-    }
-    this.f = f;
-    loadTasks(tl, f);
-  }
 
-  /**
-   * Loads the tasks from an existing file.
-   * @param tl, the Tasklist.
-   * @param f, the File.
-   * @throws FileNotFoundException
-   */
-  public void loadTasks(TaskList tl, File f) throws FileNotFoundException {
-    Scanner sc = new Scanner(f);
-    while (sc.hasNext()) {
-      tl.addTask(makeTask(sc.nextLine()));
+    /**
+     * Loads the tasks from an existing file.
+     *
+     * @param tl, the Tasklist.
+     * @param f,  the File.
+     * @throws FileNotFoundException
+     */
+    public void loadTasks(TaskList tl, File f) throws FileNotFoundException {
+        Scanner sc = new Scanner(f);
+        while (sc.hasNext()) {
+            tl.addTask(makeTask(sc.nextLine()));
+        }
+        sc.close();
     }
-    sc.close();
-  }
 
-  /**
-   * Helper method of loadTasks, to create tasks from file input.
-   * @param input, the string user input.
-   * @return the created task.
-   */
-  public Task makeTask(String input) {
-    String[] words = input.split("\\|");
-    switch (words[0]) {
-      case "T":
-        return new ToDo(words[2]);
-      case "D":
-        return new Deadline(words[2], words[3]);
-      default:
-        return new Event(words[2], words[3], words[4]);
+    /**
+     * Helper method of loadTasks, to create tasks from file input.
+     *
+     * @param input, the string user input.
+     * @return the created task.
+     */
+    public Task makeTask(String input) {
+        String[] words = input.split("\\|");
+        switch (words[0]) {
+        case "T":
+            return new ToDo(words[2]);
+        case "D":
+            return new Deadline(words[2], words[3]);
+        default:
+            return new Event(words[2], words[3], words[4]);
+        }
     }
-  }
 
-  public void saveTasks(TaskList tl) throws IOException {
-    FileWriter fw = new FileWriter(f);
-    for (Task t : tl.getTasks()) {
-      fw.write(t.fileOutput() + "\n");
+    public void saveTasks(TaskList tl) throws IOException {
+        FileWriter fw = new FileWriter(f);
+        for (Task t : tl.getTasks()) {
+            fw.write(t.fileOutput() + "\n");
+        }
+        fw.close();
     }
-    fw.close();
-  }
 }
