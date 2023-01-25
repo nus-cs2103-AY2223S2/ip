517080fc33e00cf87e4589a95937acbd489e0cdf
WindyCall <E0902032@U.NUS.EDU>
2023-01-25 22:25:40 +0800

    Complete Level-8 in branch-Level-8

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,10 +1,15 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.time.temporal.ChronoUnit;
 public class Deadline extends Task{
 
-    private String deadline;
+    private LocalDate deadline;
 
     public Deadline(String description, String deadline) {
         super(description);
-        this.deadline = deadline;
+        this.deadline = processDateTime(deadline);
     }
 
     @Override
@@ -12,8 +17,29 @@ public class Deadline extends Task{
         return "D";
     }
 
+    private LocalDate processDateTime(String deadline) {
+        // now assume date is in the form
+        // dd/mm/yy or yy-mm-dd
+        // in later versions, more form of date and time will be resolved
+        LocalDate dateTime;
+        try {
+            dateTime = LocalDate.parse(deadline);
+        } catch (DateTimeParseException e) {
+            String[] parts = deadline.split("/");
+            int day = Integer.parseInt(parts[0]);
+            int month = Integer.parseInt(parts[1]);
+            int year = Integer.parseInt(parts[2]);
+            dateTime = LocalDate.of(year, month, day);
+        }
+        return dateTime;
+    }
+
+    private String deadlineFormat() {
+        return deadline.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG));
+    }
+
     private String getDeadline() {
-        return " (by: " + this.deadline + ")";
+        return " (by: " + deadlineFormat() + ")";
     }
 
     @Override
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,12 +1,34 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.format.FormatStyle;
+
 public class Event extends Task {
 
-    private String from;
-    private String to;
+    private LocalDate from;
+    private LocalDate to;
 
     public Event(String description, String from, String to) {
         super(description);
-        this.from = from;
-        this.to = to;
+        this.from = processDate(from);
+        this.to = processDate(to);
+    }
+
+    private LocalDate processDate(String deadline) {
+        // now assume date is in the form
+        // dd/mm/yy or yy-mm-dd
+        // in later versions, more form of date and time will be resolved
+        LocalDate dateTime;
+        try {
+            dateTime = LocalDate.parse(deadline);
+        } catch (DateTimeParseException e) {
+            String[] parts = deadline.split("/");
+            int day = Integer.parseInt(parts[0]);
+            int month = Integer.parseInt(parts[1]);
+            int year = Integer.parseInt(parts[2]);
+            dateTime = LocalDate.of(year, month, day);
+        }
+        return dateTime;
     }
 
     @Override
@@ -14,8 +36,12 @@ public class Event extends Task {
         return "E";
     }
 
+    private String deadlineFormat(LocalDate date) {
+        return date.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG));
+    }
+
     private String getEventInterval() {
-        return " (from: " + from + " to: " + to + ")";
+        return " (from: " + deadlineFormat(from) + " to: " + deadlineFormat(to) + ")";
     }
 
     @Override
