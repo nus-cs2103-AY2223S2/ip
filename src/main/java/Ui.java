fc0d557e87c4d3f88da6a9ca9d6c7b7488e75e65
cydtseng <tsengchenyu@gmail.com>
2023-01-25 12:06:50 +0800

    Refactor interactions with user to Ui class

---
+++ src/main/java/Ui.java
@@ -0,0 +1,19 @@
+public class Ui {
+    public Ui(){
+        final String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
+        String greeting = "______________________________________\n"
+                + "Hey there buddy! I'm Duke. Your Personal Task Assistant!\n"
+                + "What can I do for you today?\n"
+                + "______________________________________\n";
+        System.out.print(greeting);
+    }
+
+    public void showResult(String textToDisplay) {
+        System.out.print(textToDisplay);
+    }
+}
