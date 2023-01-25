18886f119a7503c507aa1ba8dd922a6eb4cd2348
Sean Wong <58366602+ExtraShotLatte@users.noreply.github.com>
2023-01-25 17:53:44 +0800

    Add toCSV method for ToDo

--- src/main/java/ToDo.java
+++ src/main/java/ToDo.java
@@ -1,10 +1,18 @@
 public class ToDo extends Task{
+
+    protected String desc;
+
     public ToDo(String desc) {
         super(desc);
+        this.desc = desc;
     }
 
     @Override
     public String toString() {
         return "[T]" + super.toString();
+    }
+
+    public String asCSV() {
+        return "T," + desc;
     }
 }
