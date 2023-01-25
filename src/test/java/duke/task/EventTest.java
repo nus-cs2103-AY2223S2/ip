4f5c06ede10bfcadd6118b8fd617d1a750d0e65e
4ndrelim <andre_lin@u.nus.edu>
2023-01-26 03:34:27 +0800

    Add JUnit tests to test commands and tasks

---
+++ src/test/java/duke/task/EventTest.java
@@ -0,0 +1,54 @@
+package duke.task;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import duke.exceptions.InvalidDateException;
+import org.junit.jupiter.api.Test;
+
+public class EventTest {
+    @Test
+    public void addEvent() {
+        Event event = null;
+        try {
+            event = new Event("Climbing Camp", "2023-01-31", "2023-02-02");
+        } catch (InvalidDateException e) {
+            System.out.println(e);
+        }
+        assertEquals("[E][] Climbing Camp | FROM: Jan 31 2023 TO: Feb 2 2023", event.getStatusIcon());
+    }
+
+    @Test
+    public void mark() {
+        Event event = null;
+        try {
+            event = new Event("Climbing Camp", "2023-01-31", "2023-02-02");
+        } catch (InvalidDateException e) {
+            System.out.println(e);
+        }
+        event.markTask();
+        assertEquals("[E][X] Climbing Camp | FROM: Jan 31 2023 TO: Feb 2 2023", event.getStatusIcon());
+    }
+
+    @Test
+    public void unmark() {
+        Event event = null;
+        try {
+            event = new Event("Climbing Camp", "2023-01-31", "2023-02-02");
+        } catch (InvalidDateException e) {
+            System.out.println(e);
+        }
+        event.markTask();
+        event.unmarkTask();
+        assertEquals("[E][] Climbing Camp | FROM: Jan 31 2023 TO: Feb 2 2023", event.getStatusIcon());
+    }
+
+    @Test
+    public void encode() {
+        Event event = null;
+        try {
+            event = new Event("Climbing Camp", "2023-01-31", "2023-02-02");
+        } catch (InvalidDateException e) {
+            System.out.println(e);
+        }
+        event.markTask();
+        assertEquals("event ### true ### Climbing Camp ### 2023-01-31 ### 2023-02-02", event.encode());
+    }
+}
