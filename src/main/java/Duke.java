a2b356f087d636d608637971a125a4ee2543df9b
Jiexuan <81137599+botcon02@users.noreply.github.com>
2023-01-25 16:43:48 +0800

    Add TaskList class to abstract task manager

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,84 +10,17 @@ public class Duke {
     private TaskList taskList;
     private Ui ui;
 
-    private List<Task> tasks;
-
     public Duke(String filePath) throws DukeException {
         ui = new Ui();
         storage = new Storage(filePath);
         taskList = new TaskList(storage.load());
-        this.tasks = taskList.tasks;
     }
 
     public void run() {
 
     }
     // Methods
-    // List out all tasks and their rank.
-    private void list() {
-        if (tasks.size() == 0) {
-            System.out.println("No tasks left :)\n" + BORDER);
-            return;
-        }
-        int rank = 1;
-        for (Task t : tasks) {
-            System.out.println(rank + "." + t.fullMessage());
-            rank++;
-        }
-        System.out.println(BORDER);
-    }
 
-    // Mark task at index to be done
-    private void markDone(int index) throws DukeException {
-        if (index < 0 || index >= tasks.size()) {
-            throw new DukeException("OOPS!!! Invalid index");
-        }
-
-        Task curr = tasks.get(index);
-        curr.markAsDone();
-        System.out.println("Nice! I've marked this task as done:\n" +
-                curr.fullMessage() + "\n" + BORDER);
-    }
-
-    // Mark task at index to be undone
-    private void markUndone(int index) throws DukeException {
-        if (index < 0 || index >= tasks.size()) {
-            throw new DukeException("OOPS!!! Invalid index.");
-        }
-
-        Task curr = tasks.get(index);
-        curr.markAsUndone();
-        System.out.println("OK, I've marked this task as not done yet:\n" +
-                curr.fullMessage() + "\n" + BORDER);
-    }
-
-    // Delete task at index.
-    private void delete(int index) throws DukeException {
-        if (index < 0 || index >= tasks.size()) {
-            throw new DukeException("OOPS!!! Invalid index.");
-        }
-        String message = tasks.remove(index).fullMessage();
-        System.out.println("Noted. I've removed this task:\n" + message + "\n" +
-                "Now you have " + tasks.size() + " tasks in the list.\n" + BORDER);
-    }
-
-    // Create and add task given message and code. 0 - toDos, 1 - Deadlines, 2 - Event
-    private void addTask(String message, int code) throws DukeException {
-        Task t;
-        if (code == 0) {
-            t = new ToDos(false, new String[] {message});
-        } else if (code == 1) {
-            t = new Deadlines(false, message.split("/"));
-        } else if (code == 2) {
-            t = new Events(false, message.split("/"));
-        } else {
-            // Unreachable as of now.
-            return;
-        }
-        tasks.add(t);
-        System.out.println("Got it. I've added this task:\n" + t.fullMessage());
-        System.out.println("Now you have " + tasks.size() + " tasks in this list\n" + BORDER);
-    }
 
     // Read commands, possibly change to public. Return 0 at exit.
     protected int read(Scanner sc) {
@@ -101,7 +34,7 @@ public class Duke {
                     System.out.println("Bye. Hope to see you again soon!");
                     return 0;
                 case "list":
-                    list();
+                    taskList.list();
                     break;
                 case "mark":
                     try {
@@ -109,7 +42,7 @@ public class Duke {
                     } catch (NumberFormatException e) {
                         throw new DukeException("OOPS! mark must have an integer rank");
                     }
-                    markDone(rank - 1);
+                    taskList.markDone(rank - 1);
                     break;
                 case "unmark":
                     try {
@@ -117,19 +50,19 @@ public class Duke {
                     } catch (NumberFormatException e) {
                         throw new DukeException("OOPS! unmark must have an integer rank");
                     }
-                    markUndone(rank - 1);
+                    taskList.markUndone(rank - 1);
                     break;
                 case "todo":
                     message = sc.nextLine().trim();
-                    addTask(message, 0);
+                    taskList.addTask(message, 0);
                     break;
                 case "deadline":
                     message = sc.nextLine().trim();
-                    addTask(message, 1);
+                    taskList.addTask(message, 1);
                     break;
                 case "event":
                     message = sc.nextLine().trim();
-                    addTask(message, 2);
+                    taskList.addTask(message, 2);
                     break;
                 case "delete":
                     try {
@@ -137,7 +70,7 @@ public class Duke {
                     } catch (NumberFormatException e) {
                         throw new DukeException("OOPS! delete must have an integer rank");
                     }
-                    delete(rank - 1);
+                    taskList.delete(rank - 1);
                     break;
                 default:
                     System.out.println("OOPS!!! I'm sorry, but I don't know what that means :-(\n" + BORDER);
--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,8 +1,79 @@
 import java.util.List;
 
 public class TaskList {
-    protected List<Task> tasks;
+    static final String BORDER = "----------------------------------------";
+
+    private List<Task> tasks;
     protected TaskList(List<Task> tasks) {
         this.tasks = tasks;
     }
+
+    // List out all tasks and their rank.
+    protected void list() {
+        if (tasks.size() == 0) {
+            System.out.println("No tasks left :)\n" + BORDER);
+            return;
+        }
+        int rank = 1;
+        for (Task t : tasks) {
+            System.out.println(rank + "." + t.fullMessage());
+            rank++;
+        }
+        System.out.println(BORDER);
+    }
+
+    // Mark task at index to be done
+    protected void markDone(int index) throws DukeException {
+        if (index < 0 || index >= tasks.size()) {
+            throw new DukeException("OOPS!!! Invalid index");
+        }
+
+        Task curr = tasks.get(index);
+        curr.markAsDone();
+        System.out.println("Nice! I've marked this task as done:\n" +
+                curr.fullMessage() + "\n" + BORDER);
+    }
+
+    // Mark task at index to be undone
+    protected void markUndone(int index) throws DukeException {
+        if (index < 0 || index >= tasks.size()) {
+            throw new DukeException("OOPS!!! Invalid index.");
+        }
+
+        Task curr = tasks.get(index);
+        curr.markAsUndone();
+        System.out.println("OK, I've marked this task as not done yet:\n" +
+                curr.fullMessage() + "\n" + BORDER);
+    }
+
+    // Delete task at index.
+    protected void delete(int index) throws DukeException {
+        if (index < 0 || index >= tasks.size()) {
+            throw new DukeException("OOPS!!! Invalid index.");
+        }
+        String message = tasks.remove(index).fullMessage();
+        System.out.println("Noted. I've removed this task:\n" + message + "\n" +
+                "Now you have " + tasks.size() + " tasks in the list.\n" + BORDER);
+    }
+
+    // Create and add task given message and code. 0 - toDos, 1 - Deadlines, 2 - Event
+    protected void addTask(String message, int code) throws DukeException {
+        Task t;
+        if (code == 0) {
+            t = new ToDos(false, new String[] {message});
+        } else if (code == 1) {
+            t = new Deadlines(false, message.split("/"));
+        } else if (code == 2) {
+            t = new Events(false, message.split("/"));
+        } else {
+            // Unreachable as of now.
+            return;
+        }
+        tasks.add(t);
+        System.out.println("Got it. I've added this task:\n" + t.fullMessage());
+        System.out.println("Now you have " + tasks.size() + " tasks in this list\n" + BORDER);
+    }
+
+
+
 }
