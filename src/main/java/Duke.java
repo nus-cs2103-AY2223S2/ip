ea3f6f557e1e8da3e04fdc02b76fda0fe9b7da7a
owenyap <owen.y.yuhan@gmail.com>
2023-01-25 17:11:09 +0800

    level-2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
@@ -15,6 +17,14 @@ public class Duke {
         System.out.println("____________________________________________________________");
     }
 
+    private static void printList(String[] list) {
+        System.out.println("____________________________________________________________");
+        for (int i = 0; i < list.length; i++) {
+            System.out.println(String.format("%d. %s", i + 1, list[i]));
+        }
+        System.out.println("____________________________________________________________");
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -25,14 +35,22 @@ public class Duke {
         String[] welcomeMsg = {logo, "Hello I am Duke", "What can I do for you?"};
         printMsg(welcomeMsg);
 
+        List<String> data = new ArrayList<>();
+
         Scanner sc = new Scanner(System.in);
         while (true) {
-            String cmd = sc.next();
+            String cmd = sc.nextLine();
             if (cmd.equals("bye")) {
                 printMsg("Bye. Hope to see you again soon!");
                 break;
+            } else if (cmd.equals("list")) {
+                String[] list = new String[data.size()];
+                list = data.toArray(list);
+                printList(list);
+            } else {
+                data.add(cmd);
+                printMsg("added: " + cmd);
             }
-            printMsg(cmd);
         }
     }
 }
