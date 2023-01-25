f1267e332ea35b9377e684e11abd058993e10840
weilin1202 <e0775018@u.nus.edu>
2023-01-25 08:07:08 +0800

    add level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,17 +2,26 @@ import java.util.*;
 
 public class Duke {
     public static void main(String[] args) {
+
         System.out.println("Hello! I'm Happie \nWhat can I do for you?");
 
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
+        ArrayList taskList = new ArrayList<String>();
+
 
         while(!input.equals("bye")) {
-            System.out.println(input);
+
+            if (input.equals("list")) {
+                for (int i = 1; i < taskList.size() + 1; i++) {
+                    System.out.println(i + ". " + taskList.get(i - 1));
+                }
+            } else {
+                taskList.add(input);
+                System.out.println("added: " + input);
+            }
             input = sc.nextLine();
         }
-
         System.out.println("Bye. Hope to see you again soon!");
-
     }
 }
