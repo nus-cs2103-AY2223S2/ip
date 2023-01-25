f950afcc6a47f33c6a6bd1fe2dcddc46b217f1ce
heeeyi <e0772644@u.nus.edu>
2023-01-25 20:34:48 +0800

    Add Find feature, JavaDoc headers and coding standard
    Adjust the merge and makes the duke software combine the 3 features of find, javadoc and coding standard

--- src/main/java/dukes/command/FindCommand.java
+++ src/main/java/dukes/command/FindCommand.java
@@ -1,23 +1,41 @@
 package dukes.command;
 
-import dukes.util.*;
-import dukes.task.*;
+import dukes.util.DukeException;
+import dukes.util.TaskList;
+import dukes.util.UI;
+import dukes.util.Storage;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.*;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
+import dukes.task.Task;
 
+import java.util.List;
+
+/**
+ * Subclass of Command that handles the command to find task based on pattern.
+ */
 public class FindCommand extends Command {
 
+    /**
+     * Constructor of FindCommand class.
+     *
+     * @param isExit show if the command is an ExitCommand.
+     * @param isValid show if the command is valid.
+     * @param header the type the command belongs to, e.g. "add", "delete".
+     * @param body the search pattern to be matched.
+     */
     public FindCommand(boolean isExit, boolean isValid,
                       String header, String body) {
         super(isExit, isValid, header, body);
         // here body = search pattern
     }
 
+    /**
+     * Print the tasks in the list that matches the specific pattern.
+     *
+     * @param tasks contains the task list.
+     * @param ui the UI in charge of user interactions.
+     * @param storage handles the loading and saving of files.
+     * @throws DukeException if unexpected runtime issue occurs.
+     */
     public void execute(TaskList tasks, UI ui, Storage storage) throws DukeException {
         List<Task> findList = tasks.searchTaskList(this.body);
         StringBuilder sb = new StringBuilder();
