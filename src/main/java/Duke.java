bb33759c78ecdda4b1a325dc6b6dc1016eba1b06
owenyap <owen.y.yuhan@gmail.com>
2023-01-25 16:27:30 +0800

    level-1 greet, echo, exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,38 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static void printMsg(String[] msgs) {
+        System.out.println("____________________________________________________________");
+        for (String msg :  msgs) {
+            System.out.println(msg);
+        }
+        System.out.println("____________________________________________________________");
+    }
+
+    private static void printMsg(String msg) {
+        System.out.println("____________________________________________________________");
+        System.out.println(msg);
+        System.out.println("____________________________________________________________");
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        String[] welcomeMsg = {logo, "Hello I am Duke", "What can I do for you?"};
+        printMsg(welcomeMsg);
+
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String cmd = sc.next();
+            if (cmd.equals("bye")) {
+                printMsg("Bye. Hope to see you again soon!");
+                break;
+            }
+            printMsg(cmd);
+        }
     }
 }
