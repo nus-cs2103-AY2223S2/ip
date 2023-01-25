9e4f7510e61bdac0d616460be9712bf46d4c3d80
Sean Wong <58366602+ExtraShotLatte@users.noreply.github.com>
2023-01-25 20:27:55 +0800

    Update of ToDo to support initialising of boolean

--- src/main/java/ToDo.java
+++ src/main/java/ToDo.java
@@ -7,12 +7,21 @@ public class ToDo extends Task{
         this.desc = desc;
     }
 
+    public ToDo(String desc, boolean b) {
+        super(desc, b);
+        this.desc = desc;
+    }
+
     @Override
     public String toString() {
         return "[T]" + super.toString();
     }
 
     public String asCSV() {
-        return "T," + desc;
+        if (super.isDone) {
+            return "T,1," + desc;
+        } else {
+            return "T,0," + desc;
+        }
     }
 }
