2e6eb3430f10ff742bc4dfdd0d01eb625943cec3
Sean Wong <58366602+ExtraShotLatte@users.noreply.github.com>
2023-01-25 17:26:58 +0800

    Add printTask method to TaskList class

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -21,4 +21,12 @@ public class TaskList {
     public void addTask(String taskDesc, String taskFrom, String taskTo) {
         tasks.add(new Event(taskDesc, taskFrom, taskTo));
     }
+
+    public void printTask() {
+        System.out.println("    ____________________________________________________________");
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println("     " + (i + 1) + ". " + tasks.get(i).toString());
+        }
+        System.out.println("    ____________________________________________________________");
+    }
 }
