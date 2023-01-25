0cb5355a01cdb6c4519e04a45c9527d12761972f
Sean Wong <58366602+ExtraShotLatte@users.noreply.github.com>
2023-01-25 20:28:06 +0800

    Update of Event to support initialising of boolean

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -11,12 +11,23 @@ public class Event extends Task{
         this.to = to;
     }
 
+    public Event(String desc, String from, String to, boolean b) {
+        super(desc, b);
+        this.desc = desc;
+        this.from = from;
+        this.to = to;
+    }
+
     @Override
     public String toString() {
         return "[E]" + super.toString() + " (from: " + from +" to: " + to +")";
     }
 
     public String asCSV() {
-        return "E," + desc + "," + from + "," + to;
+        if (super.isDone) {
+            return "E,1," + desc + "," + from + "," + to;
+        } else {
+            return "E,0," + desc + "," + from + "," + to;
+        }
     }
 }
