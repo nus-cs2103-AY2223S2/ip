870f4a90b7df0a465cf4a06ba9825095acf08978
CedricPei <wenqi_pei@u.nus.edu>
2023-01-25 17:12:44 +0800

    feature: Level-7

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,17 @@
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.List;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Duke {
     static int exit = 0;
     static List<Task> task_list = new ArrayList<>();
 
     public static void main(String[] args) {
+        loadList();
         System.out.println("Hello! I'm Duke\nWhat can I do for you?" );
 
         while (exit != 1) {
@@ -24,21 +29,32 @@ public class Duke {
                     String first = segments[0];
                     switch (first) {
                     case "mark":
-                        int n = Integer.parseInt(segments[1]) - 1;
-                        System.out.println("Nice! I've marked this task as done:");
-                        System.out.println("[X] "+ task_list.get(n).msg);
-                        task_list.get(n).done = 1;
+                        try {
+                            int n = Integer.parseInt(segments[1]) - 1;
+                            System.out.println("Nice! I've marked this task as done:");
+                            System.out.println("[X] "+ task_list.get(n).msg);
+                            task_list.get(n).done = 1;
+                            saveList();
+                        } catch (IndexOutOfBoundsException e) {
+                            System.out.println("Index Out");
+                        }
                         break;
                     case "unmark":
-                        int num = Integer.parseInt(segments[1]) - 1;
-                        System.out.println("OK, I've marked this task as not done yet:");
-                        System.out.println("[ ] "+ task_list.get(num).msg);
-                        task_list.get(num).done = 0;
+                        try {
+                            int num = Integer.parseInt(segments[1]) - 1;
+                            System.out.println("OK, I've marked this task as not done yet:");
+                            System.out.println("[ ] "+ task_list.get(num).msg);
+                            task_list.get(num).done = 0;
+                            saveList();
+                        } catch (IndexOutOfBoundsException e) {
+                            System.out.println("Index Out");
+                        }
                         break;
                     case "todo":
                         String action = info.split(" ", 2)[1];
                         Task t = new Todo(action);
                         task_list.add(t);
+                        saveList();
                         System.out.println("Got it. I've added this task:");
                         System.out.println(t);
                         System.out.println("Now you have " + task_list.size() + " tasks in the list.");
@@ -48,6 +64,7 @@ public class Duke {
                         String by = segments[1].split("/by",2)[1];
                         Task d = new Deadline(msg, by);
                         task_list.add(d);
+                        saveList();
                         System.out.println("Got it. I've added this task:");
                         System.out.println(d);
                         System.out.println("Now you have " + task_list.size() + " tasks in the list.");
@@ -58,6 +75,7 @@ public class Duke {
                         String to = segments[1].split("/from",2)[1].split("/to")[1];
                         Task e = new Event(event, from, to);
                         task_list.add(e);
+                        saveList();
                         System.out.println("Got it. I've added this task:");
                         System.out.println(e);
                         System.out.println("Now you have " + task_list.size() + " tasks in the list.");
@@ -68,6 +86,7 @@ public class Duke {
                             System.out.println("Noted. I've removed this task:");
                             System.out.println(task_list.get(index));
                             task_list.remove(index);
+                            saveList();
                             System.out.println("Now you have " + task_list.size() + " tasks in the list.");
                         } catch (IndexOutOfBoundsException a) {
                             System.out.println("OOPS!!! You can not delete air~");
@@ -87,6 +106,70 @@ public class Duke {
         System.out.println("Here are the tasks in your list:");
         for (Task tk : task_list) {
             System.out.println( (task_list.indexOf(tk)+1) + "." + tk.toString());
+        }
+    }
+
+    public static void loadList() {
+        try {
+            File f = new File("src/main/data/task_list.txt");
+            System.out.println("Loading List...");
+            Scanner s = new Scanner(f);
+            while (s.hasNextLine()) {
+                String line = s.nextLine();
+                char type = line.charAt(0);
+                switch (type) {
+                case 'T':
+                    Task td = new Todo(line.substring(2));
+                    if (line.charAt(1) == '0') {
+                        td.done = 0;
+                    } else {
+                        td.done = 1;
+                    }
+                    task_list.add(td);
+                    break;
+                case 'D':
+                    String[] deadLine = line.substring(2).split(" /by ",2);
+                    Task dd = new Deadline(deadLine[0], deadLine[1]);
+                    if (line.charAt(1) == '0') {
+                        dd.done = 0;
+                    } else {
+                        dd.done = 1;
+                    }
+                    task_list.add(dd);
+                    break;
+                case 'E':
+                    String[] info = line.substring(2).split(" /from ",2);
+                    String[] time = info[1].split(" /to ",2);
+                    Task et = new Event(info[0], time[0], time[1]);
+                    if (line.charAt(1) == '0') {
+                        et.done = 0;
+                    } else {
+                        et.done = 1;
+                    }
+                    task_list.add(et);
+                    break;
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("task_list.txt Not Found");
+            try {
+                System.out.println("Creating the File...");
+                new File("src/main/data/task_list.txt").createNewFile();
+            } catch (IOException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+
+    public static void saveList() {
+        try {
+            FileWriter fw = new FileWriter("src/main/data/task_list.txt");
+            for (Task tk : task_list) {
+                fw.write(tk.getInfo().toString()+"\n");
+            }
+            fw.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
         }
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,9 +1,40 @@
 public class Task {
     int done;
     String msg;
+
     public Task(String msg) {
         this.msg = msg;
         this.done = 0;
+    }
+
+    public StringBuffer getInfo() {
+        StringBuffer sb = new StringBuffer("");
+        if (this instanceof Todo) {
+            sb.append('T');
+            if (this.done == 0) {
+                sb.append('0');
+            } else {
+                sb.append('1');
+            }
+            sb.append(this.msg);
+        } else if (this instanceof Deadline) {
+            sb.append('D');
+            if (this.done == 0) {
+                sb.append('0');
+            } else {
+                sb.append('1');
+            }
+            sb.append(this.msg + " /by " + ((Deadline) this).by);
+        } else if (this instanceof Event) {
+            sb.append('E');
+            if (this.done == 0) {
+                sb.append('0');
+            } else {
+                sb.append('1');
+            }
+            sb.append(this.msg + " /from " + ((Event) this).from + " /to " + ((Event) this).to);
+        }
+        return sb;
     }
 
     @Override
