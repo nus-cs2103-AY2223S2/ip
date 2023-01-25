3761dc3b65ef6597c5c62f1286479564c11a1301
Sean Wong <58366602+ExtraShotLatte@users.noreply.github.com>
2023-01-25 22:27:03 +0800

    Add datetime to Deadline

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,13 +1,19 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task{
 
     protected String by;
+    LocalDate byDate;
+
     public Deadline(String desc, String by) {
         super(desc);
         this.by = by;
+        byDate = LocalDate.parse(by);
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + byDate.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
     }
 }
