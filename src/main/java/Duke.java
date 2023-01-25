74129f045b76d3d81c7b7d238a9a364255dd0b5f
Wang <shaofeng1234567@gmail.com>
2023-01-25 10:29:18 +0800

    Testing branch 8

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -17,7 +17,7 @@ public class Duke {
         TaskList manager = new TaskList("/Users/s.f/ip/src/Data/duke.txt");
         //testing branches
         ///committing at new branch
-        //testing for branch 8
+        //testing for branch 8 merging
         loop:
         while (true) {
             String input = sc.nextLine();
