fd730175350dd9ae4c1c23431ffd148c0f5d5bfc
DESKTOP-401ETA8\USER <adrielkee@gmail.com>
2023-01-25 20:52:38 +0800

    Change processCommands to UI

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,21 +12,11 @@ public class Duke {
     public static void main(String[] args) throws EmptyDescException, InvalidInputException, DukeFileNotFoundException {
         TaskList list = new TaskList(100);
         Storage storage = new Storage("user.home", list);
-        greet();
+        UI ui = new UI();
+        ui.showWelcome();
         storage.connect();
         storage.load();
-        processInputs(list, storage);
-    }
-
-    private static void greet() {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
-        System.out.println("---------------------------------------");
+        processInputs(ui, list, storage);
     }
 
     //exits the application when "exit" is inputted
@@ -38,8 +28,7 @@ public class Duke {
     //adds items into the list and prints it when "list" is the input
     //our list takes in Tasks that are marked with a boolean.
     //processes the list with inputs from the user with list and Tasks operations.
-    private static void processInputs(TaskList list, Storage storage) throws InvalidInputException, EmptyDescException {
-        ProcessCommands processes = new ProcessCommands(list);
+    private static void processInputs(UI ui, TaskList list, Storage storage) throws InvalidInputException, EmptyDescException {
         Scanner sc = new Scanner(System.in).useDelimiter(" ");
         String input = sc.nextLine();
         while (input.equals("bye") == false) {
@@ -48,19 +37,19 @@ public class Duke {
                 Commands type = Commands.valueOf(split[0].toUpperCase().strip());
                 switch (type) {
                 case LIST:
-                    processes.processList();
+                    ui.processList(list);
                     break;
                 case MARK:
                     if (split.length < 2) {
                         throw new EmptyDescException("Sorry! you can't have empty descriptions!");
                     }
-                    processes.processMark(Integer.parseInt(split[1]) - 1);
+                    ui.processMark(list, Integer.parseInt(split[1]) - 1);
                     break;
                 case UNMARK:
                     if (split.length < 2) {
                         throw new EmptyDescException("Sorry! you can't have empty descriptions!");
                     }
-                    processes.processUnmark(Integer.parseInt(split[1]) - 1);
+                    ui.processUnmark(list, Integer.parseInt(split[1]) - 1);
                     break;
                 case TODO:
                     if (split.length < 2) {
@@ -70,14 +59,14 @@ public class Duke {
                     for (int i = 1; i < split.length; i++) {
                         task += split[i] + " ";
                     }
-                    processes.processTodo(task.trim());
+                    ui.processTodo(list, task.trim());
                     break;
                 case DEADLINE:
                     if (split.length < 2) {
                         throw new EmptyDescException("Sorry! you can't have empty descriptions!");
                     }
                     String[] parseInput = input.split("/by");
-                    processes.processDeadline(parseInput[0].replaceFirst("deadline ", ""), parseInput[1]);
+                    ui.processDeadline(list, parseInput[0].replaceFirst("deadline ", ""), parseInput[1]);
                     break;
                 case EVENT:
                     if (split.length < 2) {
@@ -85,19 +74,19 @@ public class Duke {
                     }
                     String[] parseFirst = input.split("/from");
                     String[] parseSecond = parseFirst[1].split("/to");
-                    processes.processEvent(parseFirst[0].replaceFirst("event ", ""), parseSecond[0], parseSecond[1]);
+                    ui.processEvent(list, parseFirst[0].replaceFirst("event ", ""), parseSecond[0], parseSecond[1]);
                     break;
                 case DELETE:
                     if (split.length < 2) {
                         throw new EmptyDescException("Sorry! you can't have empty descriptions!");
                     }
-                    processes.processDelete(Integer.parseInt(split[1]) - 1);
+                    ui.processDelete(list, Integer.parseInt(split[1]) - 1);
                     break;
                 case DATE:
                     if (split.length < 2) {
                         throw new EmptyDescException("Sorry! you can't have empty descriptions!");
                     }
-                    processes.processPrintDate(split[1].trim());
+                    ui.processPrintDate(list, split[1].trim());
                     break;
                 default:
                     throw new InvalidInputException("Sorry! I have no idea what that means ??? >:c");
--- src/main/java/UI.java
+++ src/main/java/UI.java
@@ -1,74 +1,78 @@
 import entities.*;
 
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 
-public class ProcessCommands {
-    protected TaskList list;
-
-    public ProcessCommands(TaskList list) {
-        this.list = list;
+public class UI {
+    public void showWelcome() {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+        printDivider();
     }
 
-    public void processAdd(Task t) {
-        this.list.addTask(t);
-    }
-
-    public void processDelete(int index) {
-        Task t = this.list.getTask(index);
-        this.list.deleteTask(index);
-        System.out.println(String.format("ok, this task has been removed:\n %s", t.toString()));
-        System.out.println(String.format("Now you have %d tasks in the list", list.getSize()));
+    public void printDivider() {
         System.out.println("---------------------------------------");
     }
 
-    public void processList() {
+    public void processDelete(TaskList list, int index) {
+        Task t = list.getTask(index);
+        list.deleteTask(index);
+        System.out.println(String.format("ok, this task has been removed:\n %s", t.toString()));
+        System.out.println(String.format("Now you have %d tasks in the list", list.getSize()));
+        printDivider();
+    }
+
+    public void processList(TaskList list) {
         for (int i = 0; i < list.getSize(); i++) {
             Task t = list.getTask(i);
             System.out.println(String.format("%d.%s", i + 1, t.toString()));
         }
-        System.out.println("---------------------------------------");
+        printDivider();
     }
 
-    public void processEvent(String desc, String from, String to) {
+    public void processEvent(TaskList list, String desc, String from, String to) {
         Event event = new Event(desc, from, to);
         list.addTask(event);
         System.out.println(String.format("Sure!, I've added the following events:\n %s", event.toString()));
         System.out.println(String.format("Now you have %d tasks in the list.", list.getSize()));
-        System.out.println("---------------------------------------");
+        printDivider();
     }
 
-    public void processDeadline(String desc, String by) {
+    public void processDeadline(TaskList list, String desc, String by) {
         Deadline deadline = new Deadline(desc, by);
         list.addTask(deadline);
         System.out.println(String.format("Received, I've added the following deadlines:\n %s", deadline.toString()));
         System.out.println(String.format("Now you have %d tasks in the list.", list.getSize()));
-        System.out.println("---------------------------------------");
+        printDivider();
     }
 
-    public void processTodo(String desc) {
+    public void processTodo(TaskList list, String desc) {
         Todo task = new Todo(desc);
         list.addTask(task);
         System.out.println(String.format("alright, I've added the following task:\n %s", task.toString()));
         System.out.println(String.format("Now you have %d tasks in the list.", list.getSize()));
-        System.out.println("---------------------------------------");
+        printDivider();
     }
 
-    public void processMark(int index) {
+    public void processMark(TaskList list, int index) {
         Task task = list.getTask(index);
         task.setDone();
         System.out.println(String.format("Nice, this task has been marked as done:\n %s", task.toString()));
-        System.out.println("---------------------------------------");
+        printDivider();
     }
 
-    public void processUnmark(int index) {
+    public void processUnmark(TaskList list, int index) {
         Task task = list.getTask(index);
         task.setUndone();
         System.out.println(String.format("Nice, this task has been marked as done:\n %s", task.toString()));
-        System.out.println("---------------------------------------");
+        printDivider();
     }
 
-    public void processPrintDate(String date) {
+    public void processPrintDate(TaskList list, String date) {
         LocalDate now = LocalDate.parse(date.trim());
         System.out.println("Tasks occurring on " + now.toString() + ":");
         for (int i = 0; i < list.getSize(); i++) {
@@ -83,7 +87,7 @@ public class ProcessCommands {
                 }
             }
         }
-        System.out.println("---------------------------------------");
+        printDivider();
     }
 
 }
