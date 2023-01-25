878bec7928208ab2c5de9078b918da7f66f01a26
Sean Wong <58366602+ExtraShotLatte@users.noreply.github.com>
2023-01-25 17:53:07 +0800

    Add asCSV method for Event

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -2,9 +2,11 @@ public class Event extends Task{
 
     protected String from;
     protected String to;
+    protected String desc;
 
     public Event(String desc, String from, String to) {
         super(desc);
+        this.desc = desc;
         this.from = from;
         this.to = to;
     }
@@ -12,5 +14,9 @@ public class Event extends Task{
     @Override
     public String toString() {
         return "[E]" + super.toString() + " (from: " + from +" to: " + to +")";
+    }
+
+    public String asCSV() {
+        return "E," + desc + "," + from + "," + to;
     }
 }
