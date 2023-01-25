5af085a6dc940bd21c6e1693488948bd9ada9e71
Elijah Ku <69575234+Zeli0@users.noreply.github.com>
2023-01-25 16:02:34 +0800

    Added error checking
    In order for the todo list bot to function as desired, it needs to check for any potential errors.
    
    Lets implement some of these errors to improve user experience.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -42,63 +42,25 @@ public class Duke {
         System.out.println(dashedLine());
         switch (inputAnalyzed[0].toLowerCase(Locale.ROOT)) {
             case "bye":
-                System.out.println("Pleasure doing business with you.");
-                end = true;
+                end = byeOperation(inputAnalyzed);
                 break;
-                //Check if there is anything other than bye
             case "list":
-                for (int i = 1; i <= list.size(); i++){
-                    System.out.println(i + ". " + list.get(i-1));
-                }
+                listOperation(inputAnalyzed,list);
                 break;
-                //Check if the list input contains anything else other than list
             case "mark":
-                int index = parseInt(inputAnalyzed[1]);
-                if (list.size() >= index) {
-                    list.get(index - 1).setChecked(true);
-                    System.out.println("It's all good man, just marked this task as done:\n"
-                            + list.get(index - 1).toString());
-                }
-                //array out of bounds, numberformatexception
+                markOperation(inputAnalyzed,list);
                 break;
             case "unmark":
-                int index1 = parseInt(inputAnalyzed[1]);
-                if (list.size() >= index1) {
-                    list.get(index1 - 1).setChecked(false);
-                    System.out.println("Alright, I marked this task as not done:\n"
-                            + list.get(index1 - 1).toString());
-                }
+                unmarkOperation(inputAnalyzed,list);
                 break;
             case "deadline":
-                String[] deadlineAnalyze = input.split("/by");
-                String deadline = deadlineAnalyze[1].trim();
-                String deets = deadlineAnalyze[0].split("deadline")[1].trim();
-                Deadline newDead = new Deadline(deets.toString(), deadline);
-                list.add(newDead);
-                System.out.println("Got it sir, just added this task to the list.\n"
-                                    + newDead + "\nYou now have " + list.size() + " tasks. Anything else?");
+                ddlOperation(inputAnalyzed,input,list);
                 break;
             case "todo":
-                String[] todoAnalyze = input.split("todo ");
-                try {
-                    Todo newTodo = new Todo(todoAnalyze[1].trim());
-                    list.add(newTodo);
-                    System.out.println("Got it sir, just added this task to the list.\n"
-                            + newTodo);
-                } catch (ArrayIndexOutOfBoundsException e) {
-                    System.out.println("Whoops, you need to put in what you're about to do!");
-                }
+                todoOperation(inputAnalyzed,input,list);
                 break;
             case "event":
-                String[] eventAnalyze = input.split("/from");
-                String[] timeAnalyze = eventAnalyze[1].split("/to");
-                String start = timeAnalyze[0].trim();
-                String over = timeAnalyze[1].trim();
-                String details = eventAnalyze[0].split("event")[1].trim();
-                Event newEvent = new Event(details, start, over);
-                list.add(newEvent);
-                System.out.println("Got it sir, just added this task to the list.\n"
-                        + newEvent + "\nYou now have " + list.size() + " tasks. Anything else?");
+                eventOperation(inputAnalyzed,input,list);
                 break;
             default:
                 System.out.println("Sorry sir, didn't quite get that." +
@@ -106,5 +68,131 @@ public class Duke {
         }
         System.out.println(dashedLine());
         return end;
+    }
+
+    private static boolean byeOperation(String[] inputAnalyzed){
+        //Check if there is anything other than bye
+        if (inputAnalyzed.length > 1) {
+            System.out.println("Did you want to close the program? A simple bye would be enough.");
+            return false;
+        } else {
+            System.out.println("Pleasure doing business with you.");
+            return true;
+        }
+    }
+
+    private static void listOperation(String[] inputAnalyzed,  ArrayList<Task> list){
+        //Check if there is anything other than list
+        if (inputAnalyzed.length > 1) {
+            System.out.println("Did you want to see your list? Just type \"list\". Nothing else.");
+        } else {
+            for (int i = 1; i <= list.size(); i++){
+                System.out.println(i + ". " + list.get(i-1));
+            }
+        }
+    }
+
+    private static void markOperation(String[] inputAnalyzed, ArrayList<Task> list){
+        try {
+            if (inputAnalyzed.length > 2) {
+                System.out.println("If you want to mark a task, just type \"mark\" and a number. Nothing else. Sheesh.");
+                return;
+            }
+            int index = parseInt(inputAnalyzed[1]);
+            if (list.size() >= index) {
+                list.get(index - 1).setChecked(true);
+                System.out.println("It's all good man, just marked this task as done:\n"
+                        + list.get(index - 1).toString());
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            //Inputted a number beyond the list's length
+            System.out.println("Woah there. That entry doesn't exist yo.");
+        } catch (NumberFormatException e) {
+            System.out.println("Label the place you want to mark with a NUMBER, not a word.");
+        }
+
+        //array out of bounds, numberformatexception
+    }
+
+    private static void unmarkOperation(String[] inputAnalyzed, ArrayList<Task> list){
+        try {
+            if (inputAnalyzed.length > 2) {
+                System.out.println("If you want to unmark a task, just type \"unmark\" and a number. Nothing else.");
+                return;
+            }
+            int index = parseInt(inputAnalyzed[1]);
+            if (list.size() >= index) {
+                list.get(index - 1).setChecked(false);
+                System.out.println("Alright, I marked this task as not done:\n"
+                        + list.get(index - 1).toString());
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            //Inputted a number beyond the list's length
+            System.out.println("Woah there. That entry doesn't exist yo.");
+        } catch (NumberFormatException e) {
+            System.out.println("Label the place you want to unmark with a NUMBER, not a word.");
+        }
+    }
+
+    private static void ddlOperation(String[] inputAnalyzed, String input, ArrayList<Task> list){
+        //Possible Errors:
+            //No descriptor
+            //No /by
+            //No deadline
+        try {
+            String[] deadlineAnalyze = input.split("/by");
+            String deadline = deadlineAnalyze[1].trim();
+            String deets = deadlineAnalyze[0].split("deadline")[1].trim();
+            if (deets.equals("")) {
+                System.out.println("Where's the description?");
+                return;
+            }
+            Deadline newDead = new Deadline(deets.toString(), deadline);
+            list.add(newDead);
+            System.out.println("Got it sir, just added this task to the list.\n"
+                    + newDead + "\nYou now have " + list.size() + " tasks. Anything else?");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("You forgot to add the deadline to your deadline. Reformat your deadline info and try again.");
+        }
+    }
+
+    private static void todoOperation(String[] inputAnalyzed, String input, ArrayList<Task> list){
+        //Possible Errors:
+        //No descriptor
+        String[] todoAnalyze = input.split("todo ");
+        try {
+            Todo newTodo = new Todo(todoAnalyze[1].trim());
+            list.add(newTodo);
+            System.out.println("Got it sir, just added this task to the list.\n"
+                    + newTodo);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Whoops, you need to put in what you're about to do!");
+        }
+    }
+
+    private static void eventOperation(String[] inputAnalyzed, String input, ArrayList<Task> list){
+        //Possible Errors:
+        //No descriptor
+        //No /from
+        //No /to
+        //No things in between or following these two labels
+        try {
+            String[] eventAnalyze = input.split("/from");
+            String[] timeAnalyze = eventAnalyze[1].split("/to");
+            String start = timeAnalyze[0].trim();
+            String over = timeAnalyze[1].trim();
+            String details = eventAnalyze[0].split("event")[1].trim();
+            if (start.equals("") || over.equals("") || details.equals("")) {
+                System.out.println("Where's the specifics?");
+                return;
+            }
+            Event newEvent = new Event(details, start, over);
+            list.add(newEvent);
+            System.out.println("Got it sir, just added this task to the list.\n"
+                    + newEvent + "\nYou now have " + list.size() + " tasks. Anything else?");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("You forgot to add the time frame. Reformat your event info and try again.");
+        }
+
     }
 }
