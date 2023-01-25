3c7abd3935c813c31ec8519bb48564bb970c3232
Sean Wong <58366602+ExtraShotLatte@users.noreply.github.com>
2023-01-25 17:57:34 +0800

    Add toCSV method for Deadline

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,13 +1,20 @@
 public class Deadline extends Task{
 
     protected String by;
+    protected String desc;
+
     public Deadline(String desc, String by) {
         super(desc);
+        this.desc = desc;
         this.by = by;
     }
 
     @Override
     public String toString() {
         return "[D]" + super.toString() + " (by: " + by + ")";
+    }
+
+    public String asCSV() {
+        return "D," + desc + "," + from + "," + to;
     }
 }
