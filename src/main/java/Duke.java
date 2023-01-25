433226920cfb618074e411f609b0b921ffd77b3c
tricixg <64681919+tricixg@users.noreply.github.com>
2023-01-26 02:32:33 +0800

    Level 4. ToDos, Events, Deadlines
    ToDos: tasks without any date/time attached to it e.g., visit new theme park
    Deadlines: tasks that need to be done before a specific date/time e.g., submit report by 11/10/2019 5pm
    Events: tasks that start at a specific date/time and ends at a specific date/time
    e.g., (a) team project meeting 2/10/2019 2-4pm (b) orientation week 4/10/2019 to 11/10/2019

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -19,7 +19,7 @@ public class Duke {
 
         //print welcome message, ask for user input
 
-        System.out.println("  ───── ･ ｡ﾟ☆: *.☽ .* :☆ﾟ. ───── \n splitInput! I'm Broccoli the dinosaur \n           <|°▿▿▿▿°|/ \n      What can I do for you? \n   ──── ･ ｡ﾟ☆: *.☽ .* :☆ﾟ. ────" ); 
+        System.out.println("  ───── ･ ｡ﾟ☆: *.☽ .* :☆ﾟ. ───── \n Hello! I'm Broccoli the dinosaur \n           <|°▿▿▿▿°|/ \n      What can I do for you? \n   ──── ･ ｡ﾟ☆: *.☽ .* :☆ﾟ. ────" ); 
         //getInput();
         ArrayList<Task> array=new ArrayList<Task>();
 
@@ -33,49 +33,26 @@ public class Duke {
                 System.out.println("\n   ‧˚₊꒷꒦︶︶︶︶︶꒷꒦︶︶︶︶︶꒦꒷‧₊˚⊹");
                 break;
 
-            } else if (combined.equals("list")){
-                System.out.println("    ────────────── ⋆⋅☆⋅⋆ ───────────────");
-                for(int j=0; j<array.size(); j++) {
-                    System.out.println("      "+(j+1) +"."+ array.get(j).toString());
-                    //System.out.println( "       "+ (j+1) + ". "+ array.get(j));
-                }
-                System.out.println("    ────────────── ⋆⋅☆⋅⋆ ───────────────");
-            } else if(splitInput[0].equals("mark")){
-                array.get((Integer.parseInt(splitInput[1])-1)).markAsDone();
-                System.out.println("    ────────────── ⋆⋅☆⋅⋆ ───────────────");
-                System.out.println("      Nice! I've marked this task as done:");
-                System.out.println("      "+ array.get((Integer.parseInt(splitInput[1])-1)).toString());
-                System.out.println("    ────────────── ⋆⋅☆⋅⋆ ───────────────");
+            }
+            //Displays the list of items 
+            else if (combined.equals("list")){
+                displayList(array);
+            } 
+            //Mark the task as done
+            else if(splitInput[0].equals("mark")){
+                markTask(array, splitInput);
             } 
             //Unmark the task as done
             else if(splitInput[0].equals("unmark")){
-                array.get((Integer.parseInt(splitInput[1])-1)).markAsUnDone();
-                System.out.println("    ────────────── ⋆⋅☆⋅⋆ ───────────────");
-                System.out.println("      OK, I've marked this task as not done yet:");
-                System.out.println("      "+ array.get((Integer.parseInt(splitInput[1])-1)).toString());
-                System.out.println("    ────────────── ⋆⋅☆⋅⋆ ───────────────");
-            } else if(splitInput[0].equals("deadline")){
-                for(int j=1; j< splitInput.length; j++){
-                    if(splitInput[j].equals("/by")){
-                        for (int k=1; k< j-1; k++){
-                            splitInput[1] = splitInput[1] + " " + splitInput[k+1];
-                        }
-                        for (int l=splitInput.length-1; l > j +1; l--){
-                            splitInput[splitInput.length-1] = splitInput[l-1]+" "+splitInput[splitInput.length-1];
-                        }
-                    } else {
-                        splitInput[j] = splitInput[j];
-                    }
-                }
-              
-                String date = splitInput[splitInput.length-1];
-                String desc = splitInput[1];
-                Deadline d = new Deadline(desc, date);
-                System.out.println("    ═══*.·:·.☽✧    ✦    ✧☾.·:·.*═══");
-                System.out.println("     Got it. I've added this task:");
-                System.out.println("     " + d.toString());
-                System.out.println("    ═══*.·:·.☽✧    ✦    ✧☾.·:·.*═══");
-                array.add(d);
+                unmarkTask(array, splitInput);
+            }
+            //Creates a Deadline type Task 
+            else if(splitInput[0].equals("deadline")){
+                createDeadlineTask(array, splitInput);
+            } else if(splitInput[0].equals("event")) {
+                createEventTask(array, splitInput);
+            } else if(splitInput[0].equals("todo")) {
+                createTodoTask(array, splitInput);
             } else {
                 System.out.println("    ═══*.·:·.☽✧    ✦    ✧☾.·:·.*═══");
                 System.out.println("     added:" + combined);
@@ -89,6 +66,100 @@ public class Duke {
 
     }
 
+    private static void createTodoTask(ArrayList<Duke.Task> array, String[] splitInput) {
+        for(int j=2; j< splitInput.length; j++){
+            splitInput[1] = splitInput[1] + " " + splitInput[j];
+        }
+           
+        String desc = splitInput[1];
+        Todo t = new Todo(desc);
+        array.add(t);
+        System.out.println("    ═══*.·:·.☽✧    ✦    ✧☾.·:·.*═══");
+        System.out.println("     Got it. I've added this task:");
+        System.out.println("     " + t.toString());
+        System.out.println("     Now you have " + array.size() + " tasks in the list.");
+        System.out.println("    ═══*.·:·.☽✧    ✦    ✧☾.·:·.*═══");
+        
+    }
+
+    private static void createEventTask(ArrayList<Duke.Task> array, String[] splitInput) {
+        for(int j=1; j< splitInput.length; j++){
+            if(splitInput[j].equals("/from")){
+                for (int k=1; k< j-1; k++){
+                    splitInput[1] = splitInput[1] + " " + splitInput[k+1];
+                }
+                for(int i = j + 1; i < splitInput.length; i++) {
+                    splitInput[i] = splitInput[i].replace("/to", "to:");
+                }
+                for (int l=splitInput.length-1; l > j +1; l--){
+                    splitInput[splitInput.length-1] = splitInput[l-1]+" "+splitInput[splitInput.length-1];
+                }
+            } else {
+                splitInput[j] = splitInput[j];
+            }
+        }
+           
+        String duration = splitInput[splitInput.length-1];
+        String desc = splitInput[1];
+        Event e = new Event(desc, duration);
+        array.add(e);
+        System.out.println("    ═══*.·:·.☽✧    ✦    ✧☾.·:·.*═══");
+        System.out.println("     Got it. I've added this task:");
+        System.out.println("     " + e.toString());
+        System.out.println("     Now you have " + array.size() + " tasks in the list.");
+        System.out.println("    ═══*.·:·.☽✧    ✦    ✧☾.·:·.*═══");
+    }
+
+    private static void createDeadlineTask(ArrayList<Task> array, String[] splitInput) {
+        for(int j=1; j< splitInput.length; j++){
+            if(splitInput[j].equals("/by")){
+                for (int k=1; k< j-1; k++){
+                    splitInput[1] = splitInput[1] + " " + splitInput[k+1];
+                }
+                for (int l=splitInput.length-1; l > j +1; l--){
+                    splitInput[splitInput.length-1] = splitInput[l-1]+" "+splitInput[splitInput.length-1];
+                }
+            } else {
+                splitInput[j] = splitInput[j];
+            }
+        }
+           
+        String date = splitInput[splitInput.length-1];
+        String desc = splitInput[1];
+        Deadline d = new Deadline(desc, date);
+        array.add(d);
+        System.out.println("    ═══*.·:·.☽✧    ✦    ✧☾.·:·.*═══");
+        System.out.println("     Got it. I've added this task:");
+        System.out.println("     " + d.toString());
+        System.out.println("     Now you have " + array.size() + " tasks in the list.");
+        System.out.println("    ═══*.·:·.☽✧    ✦    ✧☾.·:·.*═══");
+       
+    }
+
+    private static void displayList(ArrayList<Task> array) {
+        System.out.println("    ────────────── ⋆⋅☆⋅⋆ ───────────────");
+        for(int j=0; j<array.size(); j++) {
+            System.out.println("      "+(j+1) +"."+ array.get(j).toString());
+        }
+        System.out.println("    ────────────── ⋆⋅☆⋅⋆ ───────────────");
+    }
+
+    private static void unmarkTask(ArrayList<Task> array, String[] splitInput) {
+        array.get((Integer.parseInt(splitInput[1])-1)).markAsUnDone();
+        System.out.println("    ────────────── ⋆⋅☆⋅⋆ ───────────────");
+        System.out.println("      OK, I've marked this task as not done yet:");
+        System.out.println("      "+ array.get((Integer.parseInt(splitInput[1])-1)).toString());
+        System.out.println("    ────────────── ⋆⋅☆⋅⋆ ───────────────");
+    }
+
+    private static void markTask(ArrayList<Task> array, String[] splitInput) {
+        array.get((Integer.parseInt(splitInput[1])-1)).markAsDone();
+        System.out.println("    ────────────── ⋆⋅☆⋅⋆ ───────────────");
+        System.out.println("      Nice! I've marked this task as done:");
+        System.out.println("      "+ array.get((Integer.parseInt(splitInput[1])-1)).toString());
+        System.out.println("    ────────────── ⋆⋅☆⋅⋆ ───────────────");
+    }
+
     private static String[] getInput(){
         Scanner sc = new Scanner(System.in);
         String userinput = sc.nextLine();
@@ -96,6 +167,7 @@ public class Duke {
 
         return splitted;
     }
+
     static class Task {
         protected String description;
         protected boolean isDone;
@@ -118,7 +190,7 @@ public class Duke {
         }
 
         public String toString() {
-            return "[" + this.getStatusIcon() + "] " + this.description;
+            return  "[" + this.getStatusIcon() + "] " + this.description;
         }
     }
     
@@ -136,6 +208,33 @@ public class Duke {
             return "[D]" + super.toString() + " (by: " + by + ")";
         }
     }
-    
 
+    static class Event extends Task {
+
+        protected String duration;
+    
+        public Event(String description, String duration) {
+            super(description);
+            this.duration = duration;
+        }
+    
+        @Override
+        public String toString() {
+            return "[E]" + super.toString() + " (from: " + duration +")";
+        }
+    }
+
+    static class Todo extends Task {
+
+        protected String duration;
+    
+        public Todo(String description) {
+            super(description);
+        }
+    
+        @Override
+        public String toString() {
+            return "[T]" + super.toString();
+        }
+    }
 }
