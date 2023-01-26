11530e17da447bb45f504509b94a440ff88489bf
kohkaixun <e0726276@u.nus.edu>
2023-01-26 15:44:25 +0800

    Add date time OOP

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,9 +1,13 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Event extends Task {
     private String description;
-    private String start;
-    private String end;
+    private LocalDateTime start;
+    private LocalDateTime end;
 
-    private Event(String description, String start, String end) {
+    private Event(String description, LocalDateTime start, LocalDateTime end) {
         super(TaskType.EVENT, description);
         this.description = description;
         this.start = start;
@@ -14,14 +18,37 @@ public class Event extends Task {
         String target1 = " /from ";
         String target2 = " /to ";
         if (str.contains(target1) && str.contains(target2)) {
-            String description, start, end;
+            LocalDateTime startDateTime, endDateTime;
+            String description, start, end, startDay, startMonth, endDay, endMonth;
             int index1 = str.indexOf(target1);
             int index2 = str.indexOf(target2);
             if (index2 - index1 > 7) {
                 description = str.substring(0, index1);
                 start = str.substring(index1 + 7, index2);
                 end = str.substring(index2 + 5);
-                return new Event(description, start, end);
+                int startFirstSlash = start.indexOf("/");
+                int startSecondSlash = start.indexOf("/", startFirstSlash + 1);
+                int endFirstSlash = end.indexOf("/");
+                int endSecondSlash = end.indexOf("/", endFirstSlash + 1);
+                if (startFirstSlash == -1 || startSecondSlash == -1 || endFirstSlash == -1 || endSecondSlash == -1) {
+                    throw new RuntimeException("Unable to create Deadline! Check your format!\n");
+                }
+                startDay = startFirstSlash == 1 ? "d" : "dd";
+                startMonth = startSecondSlash - startFirstSlash == 2 ? "M" : "MM";
+                endDay = endFirstSlash == 1 ? "d" : "dd";
+                endMonth = endSecondSlash - endFirstSlash == 2 ? "M" : "MM";
+                DateTimeFormatter startInFormatter = DateTimeFormatter.ofPattern(startDay + "/" + startMonth + "/yyyy HHmm");
+                DateTimeFormatter endInFormatter = DateTimeFormatter.ofPattern(endDay + "/" + endMonth + "/yyyy HHmm");
+                try {
+                    startDateTime = LocalDateTime.parse(start, startInFormatter);
+                    endDateTime = LocalDateTime.parse(end, endInFormatter);
+                } catch (DateTimeParseException e) {
+                    throw new RuntimeException("Invalid date and time! Please try again!\n");
+                }
+                if (startDateTime.isAfter(endDateTime)) {
+                    throw new RuntimeException("Unable to create Event! Starting date and time cannot be after ending date and time!\n");
+                }
+                return new Event(description, startDateTime, endDateTime);
             }
             throw new RuntimeException("Unable to create Event! Check your /from and /to formatting!\n");
         }
@@ -30,14 +57,16 @@ public class Event extends Task {
 
     @Override
     public String taskToSavedForm() {
+        DateTimeFormatter outFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HHmm");
         String marked = super.getStatusIcon() == "X" ? "1" : "0";
-        return "event " + this.description + " /from " + this.start + " /to " + this.end + marked;
+        return "event " + this.description + " /from " + this.start.format(outFormatter) + " /to " + this.end.format(outFormatter) + marked;
     }
 
     @Override
     public String toString() {
+        DateTimeFormatter outFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HHmm");
         String str = super.toString();
-        str += " (from: " + this.start + " to: " + this.end + ")" + "\n";
+        str += " (from: " + this.start.format(outFormatter) + " to: " + this.end.format(outFormatter) + ")" + "\n";
         return str;
     }
 }
