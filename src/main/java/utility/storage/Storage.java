1cae68ec4fc3a6c68ba33eb8305091e87b105c8c
Brian Q <astraxquek@gmail.com>
2023-01-27 01:40:13 +0800

    fix storage to read object inputs and outputs

--- src/main/java/utility/storage/Storage.java
+++ src/main/java/utility/storage/Storage.java
@@ -1,91 +1,63 @@
 package utility.storage;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
 
-import tasklist.task_types.Deadline;
-import tasklist.task_types.Event;
-import tasklist.task_types.Task;
-import tasklist.task_types.ToDo;
+import tasklist.TaskList;
 
 public class Storage {
-    private static final String folderPath = "src/main/java/utility/storage";
-    private static final String filePath = "src/main/java/utility/storage/data.txt";
+    private static final String FOLDER_PATH = "src/main/java/utility/storage/data";
+    private static final String FILE_PATH = "src/main/java/utility/storage/data/data.txt";
 
-    public static ArrayList<Task> readData() {
-        ArrayList<Task> list = new ArrayList<>();
+    public static TaskList readData() {
+        TaskList list = new TaskList();
 
         try {
-            File f = validateFile();
-            Scanner s = new Scanner(f);
-
-            while (s.hasNext()) {
-                String unfilteredData = s.nextLine();
-                String[] data = unfilteredData.split("\\|");
-                Arrays.parallelSetAll(data, i -> data[i].trim());
-
-                if (data[0].equals("T")) {
-                    ToDo toDoTask = new ToDo(data[2]);
-                    if (data[1].equals("1")) {
-                        toDoTask.setStatus(true);
-                    }
-                    list.add(toDoTask);
-                } else if (data[0].equals("D")) {
-                    Deadline deadlineTask = new Deadline(data[2], data[3]);
-                    if (data[1].equals("1")) {
-                        deadlineTask.setStatus(true);
-                    }
-                    list.add(deadlineTask);
-                } else if (data[0].equals("E")) {
-                    Event eventTask = new Event(data[2], data[3], "");
-                    if (data[1].equals("1")) {
-                        eventTask.setStatus(true);
-                    }
-                    list.add(eventTask);
-                } else {
-                    System.out.println("Invalid input in data.txt");
-                }
-            }
-
-            s.close();
+            validateFile();
+            FileInputStream fileInput = new FileInputStream(FILE_PATH);
+            ObjectInputStream objInput = new ObjectInputStream(fileInput);
+            list = (TaskList) objInput.readObject();
+            objInput.close();
+            fileInput.close();
         } catch (FileNotFoundException e) {
             System.out.println("File not found.");
-        }
-
-        return list;
-    }
-
-    public static void writeData(ArrayList<Task> list) {
-        try {
-            FileWriter fw = new FileWriter(filePath);
-            for (int i = 0; i < list.size(); i++) {
-                fw.write(list.get(i).toTextString() + System.lineSeparator());
-            }
-            System.out.println(String.format("%d task(s) saved successfully.", list.size()));
-            fw.close();
         } catch (IOException e) {
-            System.out.println("Something went wrong: " + e.getMessage());
+            System.out.println(e + "IO EXCEPTION");
+        } catch (ClassNotFoundException e) {
+            System.out.println(e + "CLASS NOT FOUND");
+        }
+
+        return list == null ? new TaskList() : list;
+    }
+
+    public static void writeData(TaskList list) {
+        try {
+            FileOutputStream fileOutput = new FileOutputStream(FILE_PATH);
+            ObjectOutputStream objOutput = new ObjectOutputStream(fileOutput);
+            objOutput.writeObject(list);
+            objOutput.close();
+            fileOutput.close();
+        } catch (IOException e) {
+            System.out.println("Something went wrong: " + e);
         }
     }
 
-    private static File validateFile() {
-        File dataFolder = new File(folderPath);
-        File data = new File(filePath);
+    private static void validateFile() {
+        File data = new File(FILE_PATH);
 
-        if (!dataFolder.exists()) {
-            FileCreate.createFolder(Paths.get(folderPath));
+        if (data.exists()) {
+            return;
         }
 
-        if (!data.exists()) {
-            return FileCreate.createFile(Paths.get(filePath));
-        }
+        FileCreate.createFolder(Paths.get(FOLDER_PATH));
+        FileCreate.createFile(Paths.get(FILE_PATH));
+        Storage.writeData(new TaskList());
 
-        return data;
     }
 }
