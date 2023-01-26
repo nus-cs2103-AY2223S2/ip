db481732f8328b6d8172d549139e65e4ad524b1a
Oliver Loo <oliverloo09@gmail.com>
2023-01-26 11:09:17 +0800

    no message

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,28 +1,35 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 public class Event extends Tasks {
-    private String start_Date;
-    private String end_Date;
+    private LocalDateTime start_Date;
+    private LocalDateTime end_Date;
     public Event(String content, String start_Date, String end_Date) {
         super(content);
-        this.start_Date = start_Date;
-        this.end_Date = end_Date;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        this.start_Date = LocalDateTime.parse(start_Date.trim(), formatter);
+        this.end_Date = LocalDateTime.parse(end_Date.trim(), formatter);
     }
 
     public Event(boolean isMarked, String content, String start_Date, String end_Date) {
         super(isMarked, content);
-        this.start_Date = start_Date;
-        this.end_Date = end_Date;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        this.start_Date = LocalDateTime.parse(start_Date.trim(), formatter);
+        this.end_Date = LocalDateTime.parse(end_Date.trim(), formatter);
     }
 
     @Override
     public String addDivider() {
         String d = " | ";
         int marked = this.isMarked() ? 1 : 0;
-        String timePeriod = start_Date + d + end_Date;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        String timePeriod = start_Date.format(formatter) + d + end_Date.format(formatter);
         return "E" + d + marked + d + this.get_content() + d + timePeriod;
     }
 
     @Override
     public String toString() {
-        return "[E] " + super.toString() + "(from: " + start_Date + " to: " + end_Date + ")";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd yyyy HH:mm");
+
+        return "[E] " + super.toString() + " (from: " + start_Date.format(formatter) + " to: " + end_Date.format(formatter) + ")";
     }
 }
