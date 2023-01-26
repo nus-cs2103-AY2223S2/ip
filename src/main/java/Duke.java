e84f19c52f52397ad058d46a36fb0c6f49520aa3
GlennOngJunJie <glennongjj@gmail.com>
2023-01-26 14:33:00 +0800

    Add Level 8

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.io.File;
 import java.io.PrintWriter;
 
+
 public class Duke {
     public static void main(String[] args) throws DukeException, IOException {
 //        String logo = " ____        _        \n"
