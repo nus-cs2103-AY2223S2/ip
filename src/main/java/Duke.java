49879eb81559dfd381460e938afc7eb8d3d32649
lyndonlim27 <hnghng127@gmail.com>
2023-01-26 22:15:06 +0800

    Fix merge conflict residues

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -300,7 +300,6 @@ public class Duke {
             System.out.println("Error when loading file, a new file will be created");
         }
     }
-<<<<<<< HEAD
 
     /**
      * Return index of given word
@@ -345,7 +344,3 @@ public class Duke {
         }
     }
 }
-
-=======
-}
->>>>>>> branch-Level-7
