93200310b5d95ef652e85f9ffb72fd99af811a5b
Infrix <e0857426@u.nus.edu>
2023-01-26 22:03:30 +0800

    Add file read, file write, change logic of chatbot

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -5,35 +6,18 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         Lulu lulu = new Lulu();
         lulu.greet();
+        if (Save.isSavePresent()) {
+            Save.readSave(lulu);
+        }
+
         while (sc.hasNext()) {
-            try {
-                String s = sc.nextLine();
-                String[] command = s.split(" ");
-                if (command[0].compareTo("bye") == 0) {
-                    lulu.exit();
-                    break;
-                }
-                switch (command[0]) {
-                    case "list":
-                        lulu.list();
-                        break;
-                    case "mark":
-                        lulu.mark(Integer.valueOf(command[1]));
-                        break;
-                    case "unmark":
-                        lulu.unmark(Integer.valueOf(command[1]));
-                        break;
-                    case "delete":
-                        lulu.delete(Integer.valueOf(command[1]));
-                        break;
-                    default:
-                        lulu.add(s);
-                }
-            } catch (IndexOutOfBoundsException e) {
-                System.out.println(Lulu.LINE);
-                System.out.println("(=ಠᆽಠ=) That is out of bounds!");
-                System.out.println(Lulu.LINE);
+            String s = sc.nextLine();
+            String[] command = s.split(" ");
+            if (command[0].compareTo("bye") == 0) {
+                lulu.exit();
+                break;
             }
+            lulu.run(s);
         }
     }
 }
