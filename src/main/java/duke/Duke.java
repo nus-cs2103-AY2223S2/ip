f58de4b6ff630c0ac33c5264ad10d0bc945c75f8
evanpy <evanleewj@gmail.com>
2023-01-27 01:34:02 +0800

    organize file structure of classes and modify code accordingly

---
+++ src/main/java/duke/Duke.java
@@ -0,0 +1,39 @@
+package duke;
+
+import java.util.Scanner;
+
+public class Duke {
+    private Storage storage;
+    private TaskList tasks;
+    private String filePath;
+
+    public Duke(String filePath) {
+        this.storage = new Storage(filePath);
+        this.tasks = new TaskList();
+        this.filePath = filePath;
+    }
+
+    public void run() {
+        this.storage.loadData(this.tasks);
+        Ui.welcomeMsg();
+
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String input = sc.nextLine();
+            String[] descriptions = input.split(" ", 2);
+            String taskType = descriptions[0];
+
+            if (taskType.equals("bye")) {
+                this.storage.saveData(this.filePath, this.tasks);
+                Ui.exitMsg();
+                return;
+            }
+            
+            Parser.parseInput(this.tasks, taskType, descriptions);
+        }
+    }
+
+    public static void main(String[] args) {
+        new Duke("../data/taskData.txt").run();
+    }
+}
---
+++ src/main/java/duke/task/Todo.java
@@ -0,0 +1,33 @@
+package duke.task;
+
+public class Todo extends Task {
+
+    /** 
+     * A public constructor to initialize Todo instance.
+     * 
+     * @param task Task name.
+     */
+    public Todo(String task) {
+        super(task);
+    }
+
+    /** 
+     * Returns the description of Todo.
+     * 
+     * @return Todo description.
+     */
+    @Override
+    public String getDescription() {
+        return "todo " + super.getTaskName();
+    }
+
+    /** 
+     * Returns the string representation of the task.
+     *
+     * @return The string representation of the task.
+     */
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
+}
