5084d7522d824632f294c47860868c3bc0113157
Lee Jia Sheng <thegreyjs@gmail.com>
2023-01-27 00:28:32 +0800

    Add test cases for Test and Parser classes

---
+++ src/test/java/duke/ParserTest.java
@@ -0,0 +1,20 @@
+package duke;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class ParserTest {
+    @Test
+    public void parseInput_invalidInput_exceptionThrown() {
+        try {
+            new Parser().parseInput("hello", new TaskList(new ArrayList<>()), new Storage());
+            fail();
+        } catch (DukeException e) {
+            assertEquals("I'm sorry, but I don't know what that means :-(", e.getMessage());
+        }
+    }
+}
