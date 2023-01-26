05d045be3c11ae25a9665583d71db85d6f5317b4
gekhro <jethrosimsy+bitbucket@gmail.com>
2023-01-27 01:57:35 +0800

    level5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -17,89 +17,97 @@ public class Duke {
         int counter = 0;
 
         while (!input.equals("bye")){
-            
-            if (input.equals("todo")){
-                input = scan.nextLine();
-                Todo t = new Todo(input); 
-                list.add(t);
-                counter = counter + 1;
-                System.out.print("Got it. I've added this task:\n" + t.toString() + "\n" + "Now you have " + counter + " tasks in the list.\n");
-                input = scan.next();
-                continue;
-            }
-            
-            //deadline return book /by Sunday
-            if (input.equals("deadline")){
-                input = scan.nextLine();
-                String[] arrOfStr = input.split("/by");
-
-                // input1 is the task
-                String input1 = arrOfStr[0];
-                // input2 is the deadline
-                String input2 = arrOfStr[1];
-                Deadline d = new Deadline(input1, input2); 
-                list.add(d);
-                counter = counter + 1;
-                System.out.print("Got it. I've added this task:\n" + d.toString() + "\n" + "Now you have " + counter + " tasks in the list.\n");
-                input = scan.next();
-                continue;
-            }
-
-            //event project meeting /from Mon 2pm /to 4pm
-            if (input.equals("event")){
-                input = scan.nextLine();
-                String[] arrOfStr = input.split("/from|/to");
-
-                // input1 is the task
-                String input1 = arrOfStr[0];
-                // input2 is the start
-                String input2 = arrOfStr[1];
-                // input3 is the end
-                String input3 = arrOfStr[2];
-
-                Event e = new Event(input1, input2, input3); 
-                list.add(e);
-                counter = counter + 1;
-                System.out.print("Got it. I've added this task:\n" + e.toString() + "\n" + "Now you have " + counter + " tasks in the list.\n");
-                input = scan.next();
-                continue;
-            }
-
-            if (input.equals("mark")){
-                int index = scan.nextInt();
-                Task t = list.get(index - 1);
-                t.markAsDone();
-                System.out.println( "Nice! I've marked this task as done:\n" + t.toString());
-                input = scan.next();
-                continue;
-            }
-
-            if (input.equals("unmark")){
-                int index = scan.nextInt();
-                Task t = list.get(index - 1);
-                t.markAsNotDone();
-                System.out.println( "OK, I've marked this task as not done yet:\n" + t.toString());
-                input = scan.next();
-                continue;
-            }
-
-            if (input.equals("list")){
-                System.out.println("Here are the tasks in your list:");
-                for (int i = 0; i < counter; i++) {
-                    System.out.println((i + 1) + "." + list.get(i).toString());
+            try{
+                if (input.equals("todo")){
+                    input = scan.nextLine();
+                    if (input.isEmpty() || input.isBlank()) { 
+                        throw new DukeException("OOPS!!! The description of a todo cannot be empty");
+                    }
+                    
+                    Todo t = new Todo(input); 
+                    list.add(t);
+                    counter = counter + 1;
+                    System.out.print("Got it. I've added this task:\n" + t.toString() + "\n" + "Now you have " + counter + " tasks in the list.\n");
+                    input = scan.next();
+                    continue;
                 }
-                input = scan.next();
-                continue;
+                
+                //deadline return book /by Sunday
+                else if (input.equals("deadline")){
+                    input = scan.nextLine();
+                    String[] arrOfStr = input.split("/by");
+
+                    // input1 is the task
+                    String input1 = arrOfStr[0];
+                    // input2 is the deadline
+                    String input2 = arrOfStr[1];
+                    Deadline d = new Deadline(input1, input2); 
+                    list.add(d);
+                    counter = counter + 1;
+                    System.out.print("Got it. I've added this task:\n" + d.toString() + "\n" + "Now you have " + counter + " tasks in the list.\n");
+                    input = scan.next();
+                    continue;
+                }
+
+                //event project meeting /from Mon 2pm /to 4pm
+                else if (input.equals("event")){
+                    input = scan.nextLine();
+                    String[] arrOfStr = input.split("/from|/to");
+
+                    // input1 is the task
+                    String input1 = arrOfStr[0];
+                    // input2 is the start
+                    String input2 = arrOfStr[1];
+                    // input3 is the end
+                    String input3 = arrOfStr[2];
+
+                    Event e = new Event(input1, input2, input3); 
+                    list.add(e);
+                    counter = counter + 1;
+                    System.out.print("Got it. I've added this task:\n" + e.toString() + "\n" + "Now you have " + counter + " tasks in the list.\n");
+                    input = scan.next();
+                    continue;
+                }
+
+                else if (input.equals("mark")){
+                    int index = scan.nextInt();
+                    Task t = list.get(index - 1);
+                    t.markAsDone();
+                    System.out.println( "Nice! I've marked this task as done:\n" + t.toString());
+                    input = scan.next();
+                    continue;
+                }
+
+                else if (input.equals("unmark")){
+                    int index = scan.nextInt();
+                    Task t = list.get(index - 1);
+                    t.markAsNotDone();
+                    System.out.println( "OK, I've marked this task as not done yet:\n" + t.toString());
+                    input = scan.next();
+                    continue;
+                }
+
+                else if (input.equals("list")){
+                    System.out.println("Here are the tasks in your list:");
+                    for (int i = 0; i < counter; i++) {
+                        System.out.println((i + 1) + "." + list.get(i).toString());
+                    }
+                    input = scan.next();
+                    continue;
+                } 
+
+                else {
+                    throw new DukeException("OOPS!!! I'm sorry, but I don't know what that means :-(");
+                }
+                
+    
+            } catch (DukeException e) {
+                System.out.println(e.getMessage());
             }
 
-            
-            input = input + " " + scan.nextLine();
-            list.add(new Task(input));
-            counter = counter + 1;
-            System.out.println(" added: " + input);
             input = scan.next();
+          
         }
-
         System.out.println("Bye. Hope to see you again!");
     }
 }
