2bd53e8e18d5ed7c904556393dc6230e1109d9f1
Jiexuan <81137599+botcon02@users.noreply.github.com>
2023-01-26 20:41:21 +0800

    Adds help message

--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -53,8 +53,8 @@ public class Parser {
             message = s.split("/by ");
             message = Arrays.stream(message).map(String::trim).toArray(String[]::new);
             if (message.length != 2 || message[0].isEmpty() || message[1].isEmpty()) {
-                throw new DukeException("OOPS!!! Command should be in the format 'deadline [M] /by [M]'\n"
-                        + "The description, [M] cannot be empty.");
+                throw new DukeException("OOPS!!! Command should be in the format 'deadline [D] /by [D]'\n"
+                        + "The descriptions, [] cannot be empty.");
             }
             return new Command(5, message);
         case "event":
@@ -65,8 +65,8 @@ public class Parser {
             if (message.length != 3 || message[0].isEmpty()
                     || message[1].isEmpty() || message[2].isEmpty()
                     || indexFrom == -1 || indexTo == -1 || indexFrom >= indexTo) {
-                throw new DukeException("OOPS!!! Command should be in the format 'event [M] /from [M] /to [M]'\n"
-                        + "The description, [M] cannot be empty.");
+                throw new DukeException("OOPS!!! Command should be in the format 'event [M] /from [D] /to [D]'\n"
+                        + "The descriptions, [] cannot be empty.");
             }
             return new Command(6, message);
         case "delete":
