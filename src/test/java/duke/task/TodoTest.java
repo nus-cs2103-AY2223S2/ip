eacd46464655cad67df44d1733a3aa5971d622ea
jerome-neo <jeromeneo9@gmail.com>
2023-01-26 23:28:09 +0800

    Add additional test cases

--- src/test/java/duke/task/TodoTest.java
+++ src/test/java/duke/task/TodoTest.java
@@ -11,4 +11,17 @@ public class TodoTest {
         String correct = "[T]" + "[ ] " + "read a book";
         assertEquals(correct, generate.toString());
     }
+
+    @Test
+    public void test2() {
+        Todo generate = new Todo("read a book");
+        assertEquals(false, generate.getStatus());
+    }
+
+    @Test
+    public void test3() {
+        Todo generate = new Todo("read a book");
+        generate.taskDone();
+        assertEquals(true, generate.getStatus());
+    }
 }
