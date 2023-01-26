d99e39290a5bd5b48b182e57ee9597e59de28225
dsja612 <dsja612@gmail.com>
2023-01-26 11:07:40 +0800

    Add date support for events and deadlines

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,17 +1,25 @@
-public class Deadline extends Task{
-    private String deadline;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
-    public Deadline(String name, String deadline) {
+public class Deadline extends Task{
+    private LocalDate deadline;
+
+    public Deadline(String name, String deadline) throws DukeException {
         super(name);
-        this.deadline = deadline;
+        try {
+            this.deadline = LocalDate.parse(deadline, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        } catch (DateTimeParseException e) {
+            throw new DukeException("Please enter a valid date format in dd/mm/yyyy!");
+        }
     }
 
     public String getDeadline() {
-        return this.deadline;
+        return this.deadline.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + "(by " + this.deadline + ")";
+        return "[D]" + super.toString() + "(by " + this.getDeadline() + ")";
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,23 +1,34 @@
-public class Event extends Task{
-    private String startDate;
-    private String endDate;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
-    public Event(String name, String startDate, String endDate) {
+public class Event extends Task{
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    public Event(String name, String startDate, String endDate) throws DukeException {
         super(name);
-        this.startDate = startDate;
-        this.endDate = endDate;
+        try {
+            this.startDate = LocalDate.parse(startDate, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+            this.endDate = LocalDate.parse(endDate, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+            if (this.startDate.isAfter(this.endDate)) {
+                throw new DukeException("The starting date of this event is after its ending date!");
+            }
+        } catch (DateTimeParseException e) {
+            throw new DukeException("Please enter a valid date format in dd/mm/yyyy!");
+        }
     }
 
     public String getStartDate() {
-        return this.startDate;
+        return this.startDate.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
     }
 
     public String getEndDate() {
-        return this.endDate;
+        return this.endDate.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + "(Starts from " + this.startDate + "; ends at " + this.endDate + ")";
+        return "[E]" + super.toString() + "(Starts from " + this.getStartDate() + "; ends at " + this.getEndDate() + ")";
     }
 }
