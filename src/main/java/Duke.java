d16d577845e6210b08e357271f5ed09380f11459
Bryan Juniano <junianob@gmail.com>
2023-01-26 09:38:35 +0800

    Refactored Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,119 +1,43 @@
+import java.util.Locale;
 import java.util.Scanner;
 
-import static java.lang.Integer.parseInt;
-
 public class Duke {
+    private enum Commands {
+         MARK, UNMARK, TODO, DEADLINE, EVENT, LIST, DELETE, BYE
+    }
+    private static Enum getCommand(String input){
+        return Commands.valueOf(input.split(" ")[0].toUpperCase());
+    }
+    private static String[] getParameters(String input){
+        return input.split(" ", 2)[1].split(" ");
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("How can I help you?");
-        String list[] = new String[100];
-        Boolean done[] = new Boolean[100];
-        Boolean deleted[] = new Boolean[100];
-        String Type[] = new String[100];
-        int index = 0;
-        while(true) {
-            Scanner myObj = new Scanner(System.in);
-            String input[] = myObj.nextLine().split(" ",2);
-            String command = input[0];
-            String params = "";
-            if(input.length>1){
-                params = input[1];
-            }
-            if(command.equals("list")){
-                System.out.println("Here are your tasks:");
-                int num = 1;
-                for(int i = 0; i<index; i++){
-                    if(deleted[i]){
-                        continue;
-                    }
-                    if(done[i]){
-                        System.out.println(num + ".["+ Type[i]+ "]"+"[x] " + list[i]);
-                    }
-                    else {
-                        System.out.println(num + ".["+ Type[i]+ "]"+"[ ] " + list[i]);
-                    }
-                    num++;
-                }
-            }
-            else if(command.equals("delete")) {
-                if(params.equals("")){
-                    System.out.println("missing parameters.");
-                    continue;
-                }
-                deleted[parseInt(params)-1] = true;
-                System.out.println("Task deleted.");
+        Enum command;
+        String[] parameters;
+        Scanner myObj = new Scanner(System.in);
+        while (true) {
+            String input = myObj.nextLine();
+            command = getCommand(input);
 
-            }
-            else if(command.equals("mark")){
-                if(params.equals("")){
-                    System.out.println("missing parameters.");
-                    continue;
-                }
-                int temp = parseInt(params);
-                System.out.println("marking task " + temp);
-                done[temp-1] = true;
+            if (command.equals(Commands.BYE)) { // no parameters
 
-            }
-            else if(command.equals("unmark")){
-                if(params.equals("")){
-                    System.out.println("missing parameters.");
-                    continue;
-                }
-                int temp = parseInt(params);
-                System.out.println("unmarking task " + temp);
-                done[temp-1] = false;
+            } else if (command.equals(Commands.LIST)) {
 
-            }
-            else if(command.equals("bye")){
-                System.out.println("bai");
-                break;
-            }
-            else if(command.equals("todo")) {
-                if(params.equals("")){
-                    System.out.println("missing parameters.");
-                    continue;
+            } else { //has parameters
+                parameters = getParameters(input);
+                if (command.equals(Commands.TODO)) {
+
+                } else if (command.equals(Commands.DEADLINE)) {
+
+                } else if (command.equals(Commands.EVENT)) {
+
+                } else if (command.equals(Commands.DELETE)) {
+
+                } else if (command.equals(Commands.UNMARK)) {
+
+                } else if (command.equals(Commands.MARK)) {
+
                 }
-                System.out.println("added: " + params);
-                done[index] = false;
-                deleted[index] = false;
-                list[index] = params;
-                Type[index] = "T";
-                index++;
-                System.out.println("You have "+index+" tasks.");
-            }
-            else if(command.equals("deadline")) {
-                if(params.equals("")){
-                    System.out.println("missing parameters.");
-                    continue;
-                }
-                System.out.println("added: " + params);
-                done[index] = false;
-                deleted[index] = false;
-                list[index] = params;
-                Type[index] = "D";
-                index++;
-                System.out.println("You have "+index+" tasks.");
-            }
-            else if(command.equals("event")) {
-                if(params.equals("")){
-                    System.out.println("missing parameters.");
-                    continue;
-                }
-                System.out.println("added: " + params);
-                done[index] = false;
-                deleted[index] = false;
-                list[index] = params;
-                Type[index] = "E";
-                index++;
-                System.out.println("You have "+index+" tasks.");
-            }
-            else{
-                System.out.println(command + " command not recognized.");
             }
         }
     }
