4a54d96c7d655b8d7b2f9bea0d2946bb990cfe69
Bernice <bernicetoh2003@gmail.com>
2023-01-26 19:07:46 +0800

    Add JUnit tests for TaskList class

--- src/test/java/duke/TaskListTest.java
+++ src/test/java/duke/TaskListTest.java
@@ -1,8 +1,12 @@
 package duke;
 
+import duke.exception.DukeException;
+import duke.task.TaskList;
 import duke.task.Todo;
 import org.junit.jupiter.api.Test;
 
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -15,4 +19,25 @@ public class TaskListTest {
         assertEquals("[T][X] read newspaper", todo.toString());
     }
 
+    @Test
+    public void testDoesTaskExist() {
+        TaskList taskList = new TaskList(new ArrayList<>());
+        assertEquals(false, taskList.doesTaskExist(1));
+    }
+
+    @Test
+    public void testDeleteTask() throws DukeException {
+        TaskList taskList = new TaskList(new ArrayList<>());
+        taskList.addTodo("do homework");
+        taskList.deleteTask(1);
+        assertEquals("", taskList.toString());
+    }
+
+    @Test
+    public void testUnmarkTask() {
+        Todo todo = new Todo("read newspaper");
+        todo.mark();
+        todo.unMark();
+        assertEquals("[T][ ] read newspaper", todo.toString());
+    }
 }
--- src/test/java/duke/TodoTest.java
+++ src/test/java/duke/TodoTest.java
@@ -4,7 +4,6 @@ import duke.task.Todo;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
 
 public class TodoTest {
 
