a49b8b06597da52d5a51ab69c249537f0837518d
zhoyx <zhouyx2000@gmail.com>
2023-01-26 15:10:45 +0800

    Add method to load data from storage

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -1,54 +1,57 @@
-//import java.io.File;
-//import java.io.IOException;
-//import java.lang.reflect.Array;
-//import java.nio.file.Paths;
-//import java.util.ArrayList;
-//import java.util.Scanner;
-//
-//public class Storage {
-//    private File storageFile;
-//
-//    Storage(String filePath) {
-//        this.storageFile = new File(filePath);
-//    }
-//    // create an empty text file if there is no data or load the existing data to task list if data exist
-//    public ArrayList<Task> load() {
-//        try {
-//            ArrayList<Task> tasks = new ArrayList<Task>();
-//            if (storageFile.exists()) {
-//                // Parse the file and add the tasks
-//                Scanner s = new Scanner(storageFile);
-//                while (s.hasNext()) {
-//                    String line = s.nextLine();
-//                    switch (line.charAt(0)) {
-//                    case 'T':
-//
-//                    }
-//                }
-//
-//            } else {
-//                storageFile.createNewFile();
-//                return tasks;
-//            }
-//        } catch (IOException e) {
-//            System.out.println("Error creating Storage file");
-//            return new ArrayList<Task>();
-//        }
-//    }
-//
-//    public void add() {
-//
-//    }
-//
-//    public void delete(int index) {
-//
-//    }
-//
-//    public void mark(int index) {
-//
-//    }
-//
-//    public void saveState() {
-//
-//    }
-//}
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Storage {
+    private File storageFile;
+
+    Storage(String filePath) {
+        this.storageFile = new File(filePath);
+    }
+    // create an empty text file if there is no data or load the existing data to task list if data exist
+    public ArrayList<Task> load() {
+        try {
+            ArrayList<Task> tasks = new ArrayList<Task>();
+            if (storageFile.exists()) {
+                // Parse the file and add the tasks
+                Scanner s = new Scanner(storageFile);
+                while (s.hasNext()) {
+                    String line = s.nextLine();
+                    switch (line.charAt(0)) {
+                    case 'T':
+                        tasks.add(Todo.fromSaveFormat(line));
+                    case 'D':
+                        tasks.add(Deadline.fromSaveFormat(line));
+                    case 'E':
+                        tasks.add(Event.fromSaveFormat(line));
+                    }
+                }
+            } else {
+                storageFile.createNewFile();
+            }
+            return tasks;
+        } catch (IOException e) {
+            System.out.println("Error creating Storage file");
+            return new ArrayList<Task>();
+        }
+    }
+
+    public void add() {
+
+    }
+
+    public void delete(int index) {
+
+    }
+
+    public void mark(int index) {
+
+    }
+
+    public void saveState() {
+
+    }
+}
