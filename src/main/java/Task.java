04eeb050f9c8ec3f6f53b826be4a927a2bc41cdc
EvitanRelta <shauntanzongzhi@gmail.com>
2023-01-27 00:00:39 +0800

    Implement saving/loading tasklist feat.

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,3 +1,7 @@
+import java.util.function.UnaryOperator;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public abstract class Task {
     public final String description;
     protected boolean isDone;
@@ -14,6 +18,38 @@ public abstract class Task {
     public void markAsNotDone() {
         this.isDone = false;
     }
+
+    protected static String encodeValues(String[] values) {
+        UnaryOperator<String> escapeVerticalBar = str -> str.replace("|", "\\|");
+        return Stream.of(values)
+            .map(escapeVerticalBar)
+            .collect(Collectors.joining(" | "));
+    }
+
+    protected static String[] decodeValues(String encodedValues) {
+        UnaryOperator<String> unescapeVerticalBar = str -> str.replace("\\|", "|");
+        return Stream.of(encodedValues.split(" \\| "))
+            .map(unescapeVerticalBar)
+            .toArray(String[]::new);
+    }
+
+    public static Task loadFromString(String input) throws DukeLoadException {
+        String[] values = Task.decodeValues(input);
+        String taskType = values[0];
+        switch (taskType) {
+            case "T":
+                return TaskTodo.loadFromString(input);
+            case "D":
+                return TaskDeadline.loadFromString(input);
+            case "E":
+                return TaskEvent.loadFromString(input);
+            default:
+                String errorMessage = String.format("Unknown task in save-file: \"%s\"", input);
+                throw new DukeLoadException(errorMessage);
+        }
+    }
+
+    public abstract String encodeAsString();
 
     @Override
     public String toString() {
