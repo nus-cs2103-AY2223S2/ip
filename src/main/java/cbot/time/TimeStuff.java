92145fa86f1f0acfdd08d77ef86e8e05839a72a6
axmszr <e0727266@u.nus.edu>
2023-01-26 23:45:08 +0800

    Add TimeStuff

--- src/main/java/cbot/time/TimeStuff.java
+++ src/main/java/cbot/time/TimeStuff.java
@@ -6,8 +6,8 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class TimeStuff {
-    private static final String[] DFORMS = {"y-M-d", "d/M/y", "d MMM y", "MMM d y"};
-    private static final String[] TFORMS = {" Hmm", " H:m",
+    private static final String[] D_FORMS = {"y-M-d", "d/M/y", "d MMM y", "MMM d y"};
+    private static final String[] T_FORMS = {" Hmm", " H:m",
             " ha", " h a", " h.ma", " h.m a", " h:ma", " h:m a"};
     
     private static final DateTimeFormatter UI_FORM = DateTimeFormatter.ofPattern("dd/MM/yy, HHmm");
@@ -15,11 +15,11 @@ public class TimeStuff {
     public static LocalDateTime parseDT(String dtStr) throws DateTimeParseException {
         String str = dtStr.trim();
         
-        for (String df : DFORMS) {
+        for (String df : D_FORMS) {
             try {
                 return LocalDate.parse(str, DateTimeFormatter.ofPattern(df)).atStartOfDay();
             } catch (DateTimeParseException e) {
-                for (String tf : TFORMS) {
+                for (String tf : T_FORMS) {
                     try {
                         return LocalDateTime.parse(str, DateTimeFormatter.ofPattern(df + tf));
                     } catch (DateTimeParseException ignore) { }
