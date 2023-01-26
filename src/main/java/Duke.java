1986f7ca780aa0dee1dfeb0142fb3cc7808ca787
DerenC <chengderen953@gmail.com>
2023-01-26 15:48:25 +0800

    Finish Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 
 public class Duke {
+    static String[] taskList = new String[100];
+    static int firstUnused = 0;
+
     public static void main(String[] args) {
         String logo = "     ____        _        \n"
                 + "    |  _ \\ _   _| | _____ \n"
@@ -15,27 +18,43 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
         while (sc.hasNextLine()) {
             String userInput = sc.nextLine();
-            if (userInput.contains("bye.") || userInput.contains("bye ") ||
-                    userInput.contains("bye!") || userInput.contains("bye~") ||
-                    userInput.contains("bye,") || userInput.contains("Bye ") ||
-                    userInput.contains("Bye.") || userInput.contains("Bye!") ||
-                    userInput.contains("Bye~") || userInput.contains("Bye,")) {
-                print("Bye. Hope to see you again soon!");
+            if (userInput.equalsIgnoreCase("bye")) {
+                printNormal("Bye. Hope to see you again soon!");
                 break;
-            } else
-                print(userInput);
+            } else if (userInput.equalsIgnoreCase("list")) {
+                printList();
+            } else {
+                add(userInput);
+                printNormal("added: " + userInput);
+            }
         }
     }
 
-    static void print(String str) {
-        int length = str.length();
+    static void printNormal(String str) {
         System.out.println("    ------------------------------------------------------------ ");
 //        System.out.println("    ____________________________________________________________ ");
-        String message = "   |" + str;
-        for (int i = 0; i < 60 - length; i++)
+        println(str);
+        System.out.println("    ------------------------------------------------------------ ");
+    }
+
+    static void println(String str) {
+        int length = str.length();
+        String message = "   | " + str;
+        for (int i = 0; i < 60 - length - 1; i++)
             message += " ";
         message += "|";
         System.out.println(message);
+    }
+
+    static void add(String str) {
+        taskList[firstUnused++] = str;
+    }
+
+    static void printList() {
         System.out.println("    ------------------------------------------------------------ ");
+        for (int i = 0; i < firstUnused; i++)
+            println((i + 1) + ". " + taskList[i]);
+        System.out.println("    ------------------------------------------------------------ ");
+
     }
 }
