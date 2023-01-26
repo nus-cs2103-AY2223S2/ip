57228713b3565fd6faab0e3374f618827c83f717
Bryan Juniano <junianob@gmail.com>
2023-01-26 10:42:30 +0800

    Update Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,11 +5,25 @@ public class Duke {
     private enum Commands {
          BYE, LIST, MARK, UNMARK, DELETE, TODO, EVENT, DEADLINE
     }
-    private static Enum getCommand(String input){
+    private static Enum getCommand(String input){ //get the command
         return Commands.valueOf(input.split(" ")[0].toUpperCase());
     }
-    private static String[] getParameters(String input){
+    private static String[] getParameters(String input){ //get all tokens after the command
         return input.split(" ", 2)[1].split(" ");
+    }
+    private static String[] parseParameters(Enum command, String[] parameters){ //get only the useful tokens
+        if(command.equals(Commands.MARK) || command.equals(Commands.UNMARK) || command.equals(Commands.DELETE)){
+
+        }
+        else if(command.equals(Commands.TODO)){
+
+        }
+        else if(command.equals(Commands.EVENT)){
+
+        }
+        else if(command.equals(Commands.DEADLINE)){
+
+        }
     }
     public static void main(String[] args) {
         Enum command;
@@ -20,13 +34,12 @@ public class Duke {
         while (true) {
             String input = myObj.nextLine();
             command = getCommand(input);
-
             if (command.equals(Commands.BYE)) { // no parameters
                 System.out.println("bai");
             } else if (command.equals(Commands.LIST)) {
                 System.out.println(taskList.listTasks());
             } else { //has parameters
-                parameters = getParameters(input);
+                parameters = parseParameters(command,getParameters(input));
                 if (command.equals(Commands.MARK)) {
                     System.out.println(taskList.markTask(Integer.parseInt(parameters[0])));
                 } else if (command.equals(Commands.UNMARK)) {
@@ -36,9 +49,9 @@ public class Duke {
                 } else if (command.equals(Commands.DELETE)) {
                     System.out.println(taskList.deleteTask(Integer.parseInt(parameters[0])));
                 } else if (command.equals(Commands.EVENT)) {
-                    System.out.println(taskList.addTask(parameters[0], parameters[2]));
+                    System.out.println(taskList.addTask(parameters[0], parameters[1]));
                 } else if (command.equals(Commands.DEADLINE)) {
-                    System.out.println(taskList.addTask(parameters[0], parameters[2], parameters[4]));
+                    System.out.println(taskList.addTask(parameters[0], parameters[1], parameters[2]));
                 }
             }
         }
