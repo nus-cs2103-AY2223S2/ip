cb37c89824188363ff4585d83cf91bb68c848134
StanleyNeoh <95394480+StanleyNeoh@users.noreply.github.com>
2023-01-27 22:22:50 +0800

    Delete Deserializer.java

--- src/main/java/duke/storage/serializer/Deserializer.java
+++
@@ -1,9 +0,0 @@
-package duke.storage.serializer;
-
-import duke.exception.DukeException;
-import duke.task.Task;
-
-@FunctionalInterface
-public interface Deserializer {
-    public abstract Task deserialize(Serializer serializer) throws DukeException;
-}
