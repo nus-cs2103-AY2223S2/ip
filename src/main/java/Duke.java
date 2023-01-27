c4fee941cb230f2b1af179112d79fac077af71c8
DESKTOP-76Q8Q8L\Owner <e0772512@u.nus.edu>
2023-01-27 11:19:08 +0800

    Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+    public static void echo(){
+        Scanner myObj = new Scanner(System.in);
+        String inp = myObj.nextLine();
+        while (!inp.equals("bye")) {
+            System.out.println(inp);
+            inp = myObj.nextLine();
+        }
+        System.out.println("Bye. Hope to see you again soon!");
     }
+    public static void main(String[] args) {
+        String name = "chatty bot";
+        System.out.println("Hello from " + name);
+        System.out.println("talk to me :)");
+        echo();
+    }
+
+
+
 }
