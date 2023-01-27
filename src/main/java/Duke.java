9ff927d84f7a676eb910ce5d7578471c2faeafa9
DESKTOP-76Q8Q8L\Owner <e0772512@u.nus.edu>
2023-01-27 12:15:51 +0800

    Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,21 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
 
-    public static void echo(){
+    private static List<String> toDoList = new ArrayList<>();
+    private static void talk(){
         Scanner myObj = new Scanner(System.in);
         String inp = myObj.nextLine();
         while (!inp.equals("bye")) {
-            System.out.println(inp);
+            if (inp.equals("list")){
+                list();
+            }
+            else {
+                toDoList.add(inp);
+                System.out.println("added: " + inp);
+            }
             inp = myObj.nextLine();
         }
         System.out.println("Bye. Hope to see you again soon!");
     }
+
+    private static void list(){
+        for (int i = 0; i < toDoList.size(); i++){
+            System.out.println(Integer.toString(i+1) + ". " + toDoList.get(i));
+        }
+    }
+
     public static void main(String[] args) {
         String name = "chatty bot";
         System.out.println("Hello from " + name);
         System.out.println("talk to me :)");
-        echo();
+        talk();
     }
 
 
