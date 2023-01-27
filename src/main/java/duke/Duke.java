2657f824d579b6cbeb014462b3622c4698b4fadb
Li Bo Feng <libofeng37@gmail.com>
2023-01-27 15:45:24 +0800

    Fix file path

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -25,7 +25,7 @@ public class Duke {
     }
 
     public static void main(String[] args) {
-        new Duke("data", "../../../data/duke.txt").run();
+        new Duke("./data", "./data/duke.txt").run();
     }
 
     public void run() {
