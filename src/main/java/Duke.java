9a0c9c79339daedf97a99f46558820ab709aec1b
DESKTOP-GGI1K1E\kelvi <e0726844@u.nus.edu>
2023-01-27 20:06:34 +0800

    Add add and list
    Duke Class: add
    * add(): print user greeting
    * list(): print specified message
    * tasks: store input as task
    
    Duke Class: change
    * decision tree if statement to switch expression

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -10,15 +11,18 @@ public class Duke {
             + "|____/ \\__,_|_|\\_\\___|\n";
 
     /** Message to be printed before to greet the user */
-    static String MSG_GREET = "Hello! I'm Duke\n"
-            + "What can I do for you?";
+    static String MSG_GREET = " Hello! I'm Duke\n"
+            + " What can I do for you?";
 
     /** Message to be printed before exiting */
-    static String MSG_EXIT = "Bye. Hope to see you again soon!";
+    static String MSG_EXIT = " Bye. Hope to see you again soon!";
 
     public static void main(String[] args) {
         // Greets the user
         greet();
+
+        // Initialise variables
+        ArrayList<String> tasks = new ArrayList<>();
 
         // Decision loop
         boolean isContinue_decisionLoop = true;
@@ -28,10 +32,16 @@ public class Duke {
             String input = sc.nextLine();
 
             // Decision Tree
-            if (input.equals("bye")) {
+            switch (input) {
+            case "list":
+                list(tasks);
+                break;
+            case "bye":
                 isContinue_decisionLoop = false;
-            } else {
-                echo(input);
+                break;
+            default:
+                add(input, tasks);
+                break;
             }
         }
 
@@ -64,5 +74,33 @@ public class Duke {
      */
     static void exit() {
         echo(MSG_EXIT);
+    }
+
+    /**
+     * Adds specified task to specified list of tasks.
+     *
+     * @param task task to be added to tasks.
+     * @param tasks list of tasks to add task to.
+     */
+    static void add(String task, ArrayList<String> tasks) {
+        tasks.add(task);
+        echo(" added: " + task);
+    }
+
+    /**
+     * Prints specified list of tasks on console.
+     *
+     * @param tasks list of tasks to print on console.
+     */
+    static void list(ArrayList<String> tasks) {
+        StringBuilder output = new StringBuilder();
+        for (int i = 0; i < tasks.size(); i++) {
+            output.append(" ")
+                    .append(i + 1)
+                    .append(". ")
+                    .append(tasks.get(i))
+                    .append("\n");
+        }
+        echo(output.toString());
     }
 }
