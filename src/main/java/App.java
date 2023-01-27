0b70a75d9a70d67f3b0f40fb322e3e17899f7f7d
StanleyNeoh <95394480+StanleyNeoh@users.noreply.github.com>
2023-01-27 20:33:27 +0800

    Make main class into a package

--- src/main/java/App.java
+++
@@ -1,8 +0,0 @@
-import duke.Duke;
-
-public class App {
-    public static void main(String[] args) {
-        Duke duke = new Duke();
-        duke.start();
-    }
-}
--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -83,4 +83,9 @@ public class Duke {
             }
         }
     }
+
+    public static void main(String[] args) {
+        Duke duke = new Duke();
+        duke.start();
+    }
 }
