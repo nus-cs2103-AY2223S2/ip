fe4110b58f8f08168577a413cd830560a5035afe
DerenC <chengderen953@gmail.com>
2023-01-27 14:14:28 +0800

    Create 'Task' class

---
+++ src/main/java/Task.java
@@ -0,0 +1,22 @@
+public class Task {
+    protected final String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public void mark() {
+        this.isDone = true;
+    }
+
+    public void unmark() {
+        this.isDone = false;
+    }
+}
