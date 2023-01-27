be5a81e32d837187e161c5b0f9d3e87cddb284a0
Wang Jiefan <wng.jiefan@gmail.com>
2023-01-27 22:22:31 +0800

    basic javafx gui

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -11,6 +11,7 @@ package duke;
 import duke.command.ArgCommand;
 import duke.command.BasicCommand;
 import duke.command.Command;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -22,82 +23,63 @@ public class Duke {
   private final Ui ui;
   private final Parser parser;
 
-    private Duke(String filename) {
-        this.ui = new Ui();
-        TaskList taskList;
-        TaskList temp;
-        try {
-            temp = new TaskList(filename);
-        } catch (Exception e) {
-            ui.loadError(e);
-            temp = new TaskList(new ArrayList<>());
-        }
-        taskList = temp;
-        Command[] commands = new Command[]{
-                new BasicCommand("exit"
-                        , "exit the app"
-                        , hasQuit -> new String[]{"Goodbye."}),
-                new BasicCommand("help"
-                        , "show this help message"
-                        , hasQuit -> {
-                    ui.print();
-                    return new String[]{};
-                }),
-                new BasicCommand("list"
-                        , "list tasks"
-                        , hasQuit -> taskList.stringify()),
-                new ArgCommand("add"
-                        , "add task"
-                        , new String[]{"\\s"}
-                        , (args, hasQuit) -> taskList.add(args)),
-                new ArgCommand("mark"
-                        , "mark/unmark task as done"
-                        , new String[]{}
-                        , (args, hasQuit) -> taskList.mark(args)),
-                new ArgCommand("delete"
-                        , "delete task"
-                        , new String[]{}
-                        , (args, hasQuit) -> taskList.delete(args)),
-                new ArgCommand("find"
-                    , "find tasks containing text fragment"
-                    , new String[]{}
-                    , (args, hasQuit) -> taskList.find(args)),
-        };
-        ui.setCommands(commands);
-        this.parser = new Parser(commands);
-    }
+  public static final String DEFAULT_PATH = "data.txt";
+
+  private Boolean hasQuit = false; // in case there are more commands that affect
+  // program execution or state, might replace
+  // with a proper State class()
+
+  public Duke() {
+    this(DEFAULT_PATH);
+  }
+
+  public Duke(String filename) {
+      this.ui = new Ui();
+      TaskList taskList;
+      TaskList temp;
+      try {
+          temp = new TaskList(filename);
+      } catch (Exception e) {
+          ui.loadError(e);
+          temp = new TaskList(new ArrayList<>());
+      }
+      taskList = temp;
+      Command[] commands = new Command[]{
+              new BasicCommand("exit"
+                      , "exit the app"
+                      , hasQuit -> new String[]{"Goodbye."}),
+              new BasicCommand("help"
+                      , "show this help message"
+                      , hasQuit -> ui.getHelpMsg()),
+              new BasicCommand("list"
+                      , "list tasks"
+                      , hasQuit -> taskList.stringify()),
+              new ArgCommand("add"
+                      , "add task"
+                      , new String[]{"\\s"}
+                      , (args, hasQuit) -> taskList.add(args)),
+              new ArgCommand("mark"
+                      , "mark/unmark task as done"
+                      , new String[]{}
+                      , (args, hasQuit) -> taskList.mark(args)),
+              new ArgCommand("delete"
+                      , "delete task"
+                      , new String[]{}
+                      , (args, hasQuit) -> taskList.delete(args)),
+              new ArgCommand("find"
+                  , "find tasks containing text fragment"
+                  , new String[]{}
+                  , (args, hasQuit) -> taskList.find(args)),
+      };
+      ui.setCommands(commands);
+      this.parser = new Parser(commands);
+  }
 
   private void run() {
     this.ui.printIntro();
     Scanner scanner = new Scanner(System.in);
-    String[] outputs;
-    String[] arguments;
-    String[] lineParts;
-    Command cmd;
-    Boolean hasQuit = false; // in case there are more commands that affect
-                     // program execution or state, might replace
-                     // with a proper State class()
     while (scanner.hasNextLine() && !hasQuit) {
-      try {
-        lineParts = scanner.nextLine().split("\\s", 2);
-        cmd = this.parser.parseCommand(lineParts[0]);
-        if (cmd.hasParams()) {
-          if (lineParts.length < 2 || lineParts[1].isEmpty()) {
-            throw new IllegalArgumentException("Missing argument.");
-          }
-          arguments = Parser.parseArgs(lineParts[1], cmd);
-          outputs = cmd.execute(arguments, hasQuit);
-        } else {
-          outputs = cmd.execute(new String[]{}, hasQuit);
-        }
-        this.ui.print(outputs);
-        if (cmd.getName().equals("exit")) {
-          break;
-        }
-      } catch (Exception e) {
-        this.ui.error(e);
-        this.ui.print();
-      }
+      this.getResponse(scanner.nextLine());
     }
     scanner.close();
   }
@@ -108,6 +90,35 @@ public class Duke {
    * @param args The command line arguments.
    */
   public static void main(String[] args) {
-    new Duke("data.txt").run();
+    new Duke().run();
+  }
+
+  public String getResponse(String input) {
+    try {
+      String[] lineParts = input.split("\\s", 2);
+      Command cmd = this.parser.parseCommand(lineParts[0]);
+      if (cmd.getName().equals("exit")) {
+        this.hasQuit = false;
+      }
+      if (cmd.hasParams()) {
+        if (lineParts.length < 2 || lineParts[1].isEmpty()) {
+          throw new IllegalArgumentException("Missing argument.");
+        }
+        String[] arguments = Parser.parseArgs(lineParts[1], cmd);
+        return this.join(cmd.execute(arguments, hasQuit));
+      } else {
+        return this.join(cmd.execute(new String[]{}, hasQuit));
+      }
+    } catch (Exception e) {
+      return e.toString();
+    }
+  }
+
+  private String join(String[] lines) {
+    StringBuilder outputs = new StringBuilder();
+    for (String str : lines) {
+      outputs.append("\t").append(str).append("\n");
+    }
+    return outputs.toString();
   }
 }
