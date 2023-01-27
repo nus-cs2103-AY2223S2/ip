5a41a90ca76d7781eb5edeb4fddd8b9378b93101
kayyenl <yenyenlee9@gmail.com>
2023-01-27 17:17:29 +0800

    Add exclaimation mark to the greeting (actually testing out branching ability)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Duke {
     public static void main(String[] args) {
-        String greeting = formatStr("Hello! I'm Muse\n"
+        String greeting = formatStr("Hello! I'm Muse!\n"
                 + "What can I do for you?");
         System.out.println(greeting);
         Scanner sc = new Scanner(System.in);
