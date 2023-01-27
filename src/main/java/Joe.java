fe8238aba5ae395144ffba505db766c9a1392575
markusyeo <62845336+markusyeo@users.noreply.github.com>
2023-01-27 13:09:45 +0800

    Add Level-7 features

--- src/main/java/Joe.java
+++ src/main/java/Joe.java
@@ -1,16 +1,49 @@
-public class Joe {
-    static TaskList taskList = new TaskList();
-    static TaskParser parser = new TaskParser(taskList);
+import java.io.IOException;
+
+class Joe {
+    private Storage storage;
+    private TaskList taskList;
+    private TaskParser parser;
 
     public Joe() {
-        taskList = new TaskList();
+        try {
+            storage = new Storage("duke.txt");
+            taskList = storage.load();
+        } catch (IOException | DukeException e) {
+            System.out.println(e.getMessage());
+            taskList = new TaskList();
+        }
+        parser = new TaskParser(taskList);
     }
-
-
     String handleResponse(String input) {
-        return parser.parse(input);
+        String out = parser.parse(input);;
+        if (out.startsWith("added: ")) {
+            String t = out.split("added: ", 2)[1];
+            System.out.println(storage.matchesFormat(t));
+            if (storage.matchesFormat(t)) {
+                try {
+                    storage.write(t);
+                } catch (Exception e) {
+                    out = e.getMessage();
+                }
+            }
+        }
+        if (out.equals("")) {
+            out = sayBye();
+        }
+        return out;
     }
 
+    String sayBye() {
+        try {
+            storage.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println("\tBye. Hope to see you again soon!");
+        System.exit(0);
+        return "";
+    }
     public static void printNewLine() {
         String newline = "\t____________________________________________________________";
         System.out.println(newline);
