561625196513610b1bf2af6f4fcab31772b8018d
hansstanley <nusstuhss@gmail.com>
2023-01-27 18:30:40 +0800

    Change mainClassName for build

--- src/main/java/jarvis/ui/layout/MainWindow.java
+++ src/main/java/jarvis/ui/layout/MainWindow.java
@@ -6,11 +6,9 @@ import jarvis.Jarvis;
 import jarvis.ui.message.MessageBox;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 
