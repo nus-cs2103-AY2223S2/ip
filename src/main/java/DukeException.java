34267d0b5709e88a577538203a172c47cfb991f3
Lu Chenyu <adam07018@163.com>
2023-01-28 13:14:15 +0800

    Enum for exception

---
+++ src/main/java/DukeException.java
@@ -0,0 +1,20 @@
+public class DukeException extends Exception {
+    DukeException(ExceptionType e) {
+        switch (e) {
+            default:
+                System.out.println("Unknown error occured");
+                break;
+            case DESCRIPTION_EMPTY:
+                System.out.println("OOPS!!! The description of a task cannot be empty.");
+                break;
+            case TASK_FORMAT_ERROR:
+                System.out.println("Sorry, the format of task is incorrect");
+                break;
+            case TASK_NOT_EXIST:
+                System.out.println("Sorry, There is no such task for this command");
+            case NO_NUMBER:
+                System.out.println("Please key in number for this command");
+        }
+
+    }
+}
---
+++ src/main/java/ExceptionType.java
@@ -0,0 +1,3 @@
+public enum ExceptionType {
+    DESCRIPTION_EMPTY, TASK_FORMAT_ERROR, NO_NUMBER, TASK_NOT_EXIST
+}
