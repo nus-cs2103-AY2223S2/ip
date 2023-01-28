33d8fc84334f44b90f28f39819a841d88c10d86a
PeanutButters93 <lowjianfeng@hotmail.com>
2023-01-28 11:36:04 +0800

    Revert "Updated Deadline.java merging errors"
    This reverts commit 12f5da3d8903978a1db7e6909fc6d87022c45e3a.

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -2,7 +2,12 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class Deadline extends Task {
+<<<<<<< HEAD
+
+    protected String by;
+=======
     protected LocalDate by;
+>>>>>>> branch-Level-8
 
     public Deadline(String description, String by) {
         super(description);
