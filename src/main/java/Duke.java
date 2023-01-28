b2b42f487fe566a3c6a6af13edd78c9f3a6546ae
jengoc415 <jengoc415@gmail.com>
2023-01-28 10:56:44 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -15,14 +16,22 @@ public class Duke {
 
         Scanner sc = new Scanner(System.in);
         String command;
+        ArrayList<String> tasks = new ArrayList<>();
 
         while (true) {
             command = sc.nextLine().toLowerCase();
-            if (command.compareTo("bye") != 0) {
-                System.out.println(line + indent + command + "\n" + line);
-            } else {
+            if (command.compareTo("bye") == 0) {
                 System.out.println(line + indent + "Bye. Hope to see you again soon!\n" + line);
                 break;
+            } else if (command.compareTo("list") == 0) {
+                System.out.println(line);
+                for (int i = 1; i <= tasks.size(); i++) {
+                    System.out.println(indent + i + ". " + tasks.get(i-1));
+                }
+                System.out.println(line);
+            } else {
+                tasks.add(command);
+                System.out.println(line + indent + "added: " + command + "\n" + line);
             }
         }
     }
