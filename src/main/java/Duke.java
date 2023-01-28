dcd5bc72516c34d1add22913ec29de315dbed69f
DESKTOP-GGI1K1E\kelvi <e0726844@u.nus.edu>
2023-01-28 11:34:03 +0800

    Add mark, unmark, and Task
    Task Class: contain information on the task
    
    Duke Class: add
    * mark(): mark specified task as done
    * unmark(): change status of specified task back
                to not done
    * indent(): return indentation with spaces
    
    Duke Class: change
    * header comment: capitalise header comment
                      parameter starting word
    * add(): change order of parameters
    * echo(): overload echo to accept array of messages
              to be printed to console at the same time

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,26 +3,37 @@ import java.util.Scanner;
 
 public class Duke {
 
-    /** String of the Logo */
+    /** The Logo of Duke */
     static String MSG_LOGO = " ____        _        \n"
             + "|  _ \\ _   _| | _____ \n"
             + "| | | | | | | |/ / _ \\\n"
             + "| |_| | |_| |   <  __/\n"
             + "|____/ \\__,_|_|\\_\\___|\n";
 
+    /** Line to be appended before and after a message */
+    static String MSG_LINE = "____________________________________________________________\n";
+
     /** Message to be printed before to greet the user */
-    static String MSG_GREET = " Hello! I'm Duke\n"
-            + " What can I do for you?";
+    static String[] MSG_GREET = {"Hello! I'm Duke", " What can I do for you?"};
 
     /** Message to be printed before exiting */
-    static String MSG_EXIT = " Bye. Hope to see you again soon!";
+    static String MSG_EXIT = "Bye. Hope to see you again soon!";
+
+    /** Message to be printed after marking task as done */
+    static String MSG_MARK = "Nice! I've marked this task as done:";
+
+    /** Message to be printed after changing status of task to not done */
+    static String MSG_UNMARK = "OK, I've marked this task as not done yet:";
+
+    /** Indentation level of messages to be printed to console */
+    static int INDENTATION_LEVEL = 4;
 
     public static void main(String[] args) {
         // Greets the user
         greet();
 
         // Initialise variables
-        ArrayList<String> tasks = new ArrayList<>();
+        ArrayList<Task> tasks = new ArrayList<>();
 
         // Decision loop
         boolean isContinue_decisionLoop = true;
@@ -30,9 +41,16 @@ public class Duke {
             // Get user's input
             Scanner sc = new Scanner(System.in);
             String input = sc.nextLine();
+            String[] inputs = input.split(" ", 2);
 
             // Decision Tree
-            switch (input) {
+            switch (inputs[0]) {
+            case "mark":
+                mark(tasks, inputs[1]);
+                break;
+            case "unmark":
+                unmark(tasks, inputs[1]);
+                break;
             case "list":
                 list(tasks);
                 break;
@@ -40,7 +58,7 @@ public class Duke {
                 isContinue_decisionLoop = false;
                 break;
             default:
-                add(input, tasks);
+                add(tasks, input);
                 break;
             }
         }
@@ -50,41 +68,92 @@ public class Duke {
     }
 
     /**
-     * Prints greeting message on console.
+     * Prints specified message on console.
+     * Appends a line before and after message.
+     *
+     * @param msg Message to be printed on console.
      */
-    static void greet() {
-        System.out.println("Hello from\n" + MSG_LOGO);
-        echo(MSG_GREET);
+    static void echo(String msg) {
+        String indent = indent(INDENTATION_LEVEL);
+        System.out.println(indent + MSG_LINE
+                + indent + msg + "\n"
+                + indent + MSG_LINE);
     }
 
     /**
      * Prints specified message on console.
      * Appends a line before and after message.
      *
-     * @param msg message to be printed on console.
+     * @param msg Message to be printed on console.
+     * @param indentLevel Number of spaces to indent message.
      */
-    static void echo(String msg) {
-        String line = "____________________________________________________________\n";
-        System.out.println(line + msg + "\n"
-                + line);
+    static void echo(String msg, int indentLevel) {
+        String indent = indent(INDENTATION_LEVEL);
+        System.out.println(indent + MSG_LINE
+                + indent + indent(indentLevel)
+                + msg + "\n"
+                + indent + MSG_LINE);
+    }
+
+    /**
+     * Prints specified message on console.
+     * Appends a line before and after message.
+     *
+     * @param msg Array of message to be printed on console.
+     * @param indentLevel Array representing the indent level of each message.
+     */
+    static void echo(String[] msg, int[] indentLevel) {
+        String indent = indent(INDENTATION_LEVEL);
+        StringBuilder output = new StringBuilder(indent + MSG_LINE);
+
+        for (int i = 0; i < msg.length; i++) {
+            output.append(indent);
+            if (i < indentLevel.length) {
+                output.append(indent(indentLevel[i]));
+            }
+            output.append(msg[i])
+                    .append("\n");
+        }
+
+        output.append(indent).append(MSG_LINE);
+        System.out.println(output);
+    }
+
+    /**
+     * Returns indentation of the specified indentation level.
+     *
+     * @param count Level of indentation.
+     * @return Indentation
+     */
+    static String indent(int count) {
+        return " ".repeat(count);
+    }
+
+    /**
+     * Prints greeting message on console.
+     */
+    static void greet() {
+        System.out.println("Hello from\n" + MSG_LOGO);
+        int[] indentLevel = {1};
+        echo(MSG_GREET, indentLevel);
     }
 
     /**
      * Prints exiting message on console.
      */
     static void exit() {
-        echo(MSG_EXIT);
+        echo(MSG_EXIT, 1);
     }
 
     /**
      * Adds specified task to specified list of tasks.
      *
-     * @param task task to be added to tasks.
      * @param tasks list of tasks to add task to.
+     * @param task task to be added to tasks.
      */
-    static void add(String task, ArrayList<String> tasks) {
-        tasks.add(task);
-        echo(" added: " + task);
+    static void add(ArrayList<Task> tasks, String task) {
+        tasks.add(new Task(task));
+        echo("added: " + task, 1);
     }
 
     /**
@@ -92,15 +161,45 @@ public class Duke {
      *
      * @param tasks list of tasks to print on console.
      */
-    static void list(ArrayList<String> tasks) {
-        StringBuilder output = new StringBuilder();
-        for (int i = 0; i < tasks.size(); i++) {
-            output.append(" ")
-                    .append(i + 1)
-                    .append(". ")
-                    .append(tasks.get(i))
-                    .append("\n");
+    static void list(ArrayList<Task> tasks) {
+        String[] output = new String[tasks.size()];
+        int[] indentLevel = new int[tasks.size()];
+        for (int i = 0; i < output.length; i++) {
+            output[i] = i + 1
+                    + ". "
+                    + tasks.get(i);
+            indentLevel[i] = 1;
         }
-        echo(output.toString());
+        echo(output, indentLevel);
+    }
+
+    /**
+     * Marks specified task as done.
+     *
+     * @param tasks list of tasks to get specified task from.
+     * @param index index of task to be marked.
+     */
+    static void mark(ArrayList<Task> tasks, String index) {
+        int i = Integer.parseInt(index) - 1;
+        tasks.get(i).mark();
+        int[] indentLevel = {1, 3};
+        String[] message = {MSG_MARK,
+                tasks.get(i).toString()};
+        echo(message, indentLevel);
+    }
+
+    /**
+     * Changes status of specified task back to not done.
+     *
+     * @param tasks list of tasks to get specified task from.
+     * @param index index of task to be changed to not done.
+     */
+    static void unmark(ArrayList<Task> tasks, String index) {
+        int i = Integer.parseInt(index) - 1;
+        tasks.get(i).unmark();
+        int[] indentLevel = {1, 3};
+        String[] message = {MSG_UNMARK,
+                tasks.get(i).toString()};
+        echo(message, indentLevel);
     }
 }
