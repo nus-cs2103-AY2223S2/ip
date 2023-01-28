53418a93914f7232e93fa483129ab797aaf74bcb
UnwilledTangent <tinsean.work@gmail.com>
2023-01-28 21:18:32 +0800

    Do A-MoreOOP

---
+++ src/main/duke/src/Duke.java
@@ -0,0 +1,9 @@
+public class Duke {
+    private Storage storage;
+    private TaskList tasks;
+    private Ui ui;
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
