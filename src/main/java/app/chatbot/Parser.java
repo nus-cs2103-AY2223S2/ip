ce0a7784cf07753a72d8aa24dcd0866f69ed2c80
ajjajjajjajj <angjingjie0512@gmail.com>
2023-01-28 19:28:39 +0800

    Add support for Find function

--- src/main/java/app/chatbot/Parser.java
+++ src/main/java/app/chatbot/Parser.java
@@ -5,6 +5,7 @@ import app.command.Command;
 import app.command.CommandNotFoundException;
 import app.command.DeleteCommand;
 import app.command.ExitCommand;
+import app.command.FindCommand;
 import app.command.ListCommand;
 import app.command.MarkAsDoneCommand;
 import app.command.MarkAsUndoneCommand;
@@ -64,6 +65,8 @@ public class Parser {
                 return new MarkAsUndoneCommand(argValues.get(command)); // pass in index to command
             case "delete":
                 return new DeleteCommand(argValues.get(command)); // pass in index to command
+            case "find":
+                return new FindCommand(argValues.get(command));
             case "bye":
                 return new ExitCommand();
             default:
---
+++ src/main/java/app/command/FindCommand.java
@@ -0,0 +1,38 @@
+package app.command;
+
+import app.chatbot.Storage;
+import app.chatbot.Ui;
+import app.task.Task;
+import app.task.TaskList;
+
+public class FindCommand extends Command {
+    private final String searchContent;
+
+    public FindCommand(String searchContent) {
+        this.isExit = false;
+        this.isSave = false;
+        this.searchContent = searchContent.toLowerCase();
+    }
+
+    @Override
+    public void execute(TaskList tl, Ui ui, Storage storage) {
+        StringBuilder resultDisplay = new StringBuilder("");
+        boolean isSearchSuccessful = false;
+        for (int i = 0; i < tl.size(); i++) {
+            Task t = tl.getTask(i);
+            String desc = t.getDesc();
+            if (desc.toLowerCase().contains(this.searchContent)) {
+                resultDisplay.append(i+1);
+                resultDisplay.append(": " + t);
+                resultDisplay.append("\n");
+                isSearchSuccessful = true;
+            }
+        }
+        if (!isSearchSuccessful) {
+            ui.reply("Ah I didn't find any tasks matching '" + this.searchContent + "'." );
+        } else {
+            ui.reply("Here's the tasks matching '" + this.searchContent + "':\n" +
+                    resultDisplay);
+        }
+    }
+}
