255e1f84aedc344d0de53cb3a3dd4b8e24095fe7
“ShawnYip-Au” <shawnyipau@gmail.com>
2023-01-29 02:26:44 +0800

    Implement parse class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -41,7 +41,7 @@ public class Duke {
 
             // Check if valid
             try {
-                curEvent = decodeInput(userInput);
+                curEvent = Parser.parse(userInput);
             } catch (DukeException e) {
                 System.out.println(e.getMessage());
                 continue; // skip the rest of the code and go to next iteration
@@ -88,38 +88,7 @@ public class Duke {
         System.out.print("> ");
         return getInput.nextLine();
     }
-
-    // Throws exception if invalid keyword
-    private static EventType decodeInput(String input) throws DukeException {
-        String[] arr = input.split(" ");
-        
-        if (arr[0].equals("bye")) {
-            return EventType.BYE;
-        }
-        if (arr[0].equals("list")) {
-            return EventType.LIST;
-        }
-        if (arr[0].equals("mark")) {
-            return EventType.MARK;
-        }
-        if (arr[0].equals("unmark")) {
-            return EventType.UNMARK;
-        }
-        if (arr[0].equals("delete")) {
-            return EventType.DELETE;
-        }
-        if (arr[0].equals("todo")) {
-            return EventType.TODO;
-        }
-        if (arr[0].equals("deadline")) {
-            return EventType.DEADLINE;
-        }
-        if (arr[0].equals("event")) {
-            return EventType.EVENT;
-        }
-        throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
-    }
-
+    
     private static void markEvent(String userInput) {
         String[] arr = userInput.split(" ");
         int num = Integer.parseInt(arr[1]);
