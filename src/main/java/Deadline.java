7a88935822917d6c90289849ed4e06c09da6f5e4
jishnu28 <e0725046@u.nus.edu>
2023-01-28 13:54:43 +0800

    Added Level-8 features

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,18 +1,23 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
 
     // Attributes:
     protected String by_date;
+    protected LocalDate endDate;
 
     // Constructor:
     public Deadline(String user_input) {
         // use if-else and string.contains to check if this doesn't work
         super(user_input.substring(9, user_input.indexOf("/by ") - 1)); // title
         this.by_date = user_input.substring(user_input.indexOf("/by ") + 4); // due date
+        this.endDate = LocalDate.parse(by_date);
     }
 
     // Methods:
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by_date + ")";
+        return "[D]" + super.toString() + " (by: " + endDate.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,19 +1,26 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
 
     // Attributes:
     protected String from_date;
+    protected LocalDate startDate;
     protected String to_date;
+    protected LocalDate endDate;
 
     // Constructor:
     public Event(String user_input) {
         super(user_input.substring(6, user_input.indexOf("/from ") - 1));
         this.from_date = user_input.substring(user_input.indexOf("/from ") + 6, user_input.indexOf("/to ") - 1);
+        this.startDate = LocalDate.parse(from_date);
         this.to_date = user_input.substring(user_input.indexOf("/to ") + 4);
+        this.endDate = LocalDate.parse(to_date);
     }
 
     // Methods:
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + from_date + " to: " + to_date + ")";
+        return "[E]" + super.toString() + " (from: " + startDate.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + " to: " + endDate.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
     }
 }
