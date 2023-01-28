f80116081c4c67fc1283e49bfa9d7fe0a8c880d0
ChinJunAn <junan1599@gmail.com>
2023-01-28 17:35:29 +0800

    Peak version of GUI

---
+++ src/main/java/launcher/Launcher.java
@@ -0,0 +1,10 @@
+package launcher;
+
+import javafx.application.Application;
+import duke.Duke;
+import duke.Art;
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(Duke.class, args);
+    }
+}
