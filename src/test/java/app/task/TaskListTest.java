c588f624d2380cc837f8ccef596a88a039461eb2
ajjajjajjajj <angjingjie0512@gmail.com>
2023-01-28 18:21:38 +0800

    Edit test classes

--- src/test/java/app/task/TaskListTest.java
+++ src/test/java/app/task/TaskListTest.java
@@ -7,7 +7,6 @@ import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.fail;
 
 public class TaskListTest {
     @Test
