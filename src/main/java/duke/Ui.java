9ea10e1c2648437ffd0e0f9699c9bc78512dcecf
ina <>
2023-01-28 21:35:28 +0800

    Add a GUI to Duke for Level 10

--- src/main/java/duke/Ui.java
+++ src/main/java/duke/Ui.java
@@ -5,6 +5,34 @@ package duke;
  */
 public class Ui {
     /**
+     * Returns the welcome message in String format.
+     */
+    public String welcome() {
+        return "---\nhi i'm Duke! what's up?\n---";
+    }
+
+    /**
+     * Returns the top divider line ("---") in String format.
+     */
+    public String lineTop() {
+        return "---\n"; // show the divider line ("---")
+    }
+
+    /**
+     * Returns the bottom divider line ("---") in String format.
+     */
+    public String lineBottom() {
+        return "\n---"; // show the divider line ("---")
+    }
+
+    /**
+     * Returns the bye message in String format.
+     */
+    public String bye() {
+        return "---\nbye! see u soon! :-)\n---";
+    }
+
+    /**
      * Prints the welcome message.
      */
     public void showWelcome() {
