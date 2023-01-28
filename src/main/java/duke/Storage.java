2218b2330bedb15575a82ac4a209360c5aff01ec
chuakid <chuakid@gmail.com>
2023-01-28 23:35:44 +0800

    Rename variable

--- src/main/java/duke/Storage.java
+++ src/main/java/duke/Storage.java
@@ -8,11 +8,11 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
-public class Storage {
-    final String SAVE_PATH;
+class Storage {
+    private final String savePath;
 
-    Storage(String SAVE_PATH) {
-        this.SAVE_PATH = SAVE_PATH;
+    Storage(String savePath) {
+        this.savePath = savePath;
     }
 
     /**
@@ -21,8 +21,8 @@ public class Storage {
      * @param list TaskList to be stored
      */
     void store(TaskList list) {
-        boolean ignored = new File(SAVE_PATH).getParentFile().mkdirs();
-        try (FileOutputStream fw = new FileOutputStream(SAVE_PATH); ObjectOutputStream out = new ObjectOutputStream(
+        boolean ignored = new File(savePath).getParentFile().mkdirs();
+        try (FileOutputStream fw = new FileOutputStream(savePath); ObjectOutputStream out = new ObjectOutputStream(
                 fw)) {
             out.writeObject(list);
         } catch (IOException e) {
@@ -37,7 +37,7 @@ public class Storage {
      */
     TaskList load() {
         try (FileInputStream fileInputStream = new FileInputStream(
-                SAVE_PATH); ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream)) {
+                savePath); ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream)) {
             return (TaskList) objectInputStream.readObject();
         } catch (IOException | ClassNotFoundException | ClassCastException e) {
             System.out.println("No save file");
