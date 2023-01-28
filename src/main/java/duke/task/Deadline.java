72f23b1455b122a1c2815855dff48fb3a6fa144b
Guo-KeCheng <guo.kecheng99@gmail.com>
2023-01-28 11:58:37 +0800

    Add Java Doc for task

--- src/main/java/duke/task/Deadline.java
+++ src/main/java/duke/task/Deadline.java
@@ -3,34 +3,67 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
+/**
+ * Deadline class: Instantiate when user creates a deadline
+ */
 public class Deadline extends Task {
 
     private LocalDate endDate;
+
+    /**
+     * Deadline class constructor
+     * Default isCompleted to be false
+     *
+     * @param task Task description as String
+     * @param endDate End date as String
+     * @throws DateTimeParseException When endDate is in the wrong format
+     */
     public Deadline(String task, String endDate) throws DateTimeParseException {
         super(task, false);
         this.endDate = LocalDate.parse(endDate);
     }
 
-    public Deadline(String task, boolean isCompleted, String endDate) {
+
+    /**
+     * Deadline class constructor
+     *
+     * @param task Task description as String
+     * @param isCompleted Completion status. True -> Completed. False -> Not Completed
+     * @param endDate End date as String
+     * @throws DateTimeParseException When endDate is in the wrong format
+     */
+    public Deadline(String task, boolean isCompleted, String endDate) throws DateTimeParseException {
         super(task, isCompleted);
         this.endDate = LocalDate.parse(endDate);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String getTaskType() {
         return "Deadline";
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String getStatus() {
         return String.format("%s", isCompleted());
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String getDescription() {
         return getTask() + " | " + endDate;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String toString() {
         return "[D]" + super.toString()
--- src/main/java/duke/task/Task.java
+++ src/main/java/duke/task/Task.java
@@ -9,32 +9,73 @@ public abstract class Task {
         this.isCompleted = isCompleted;
     }
 
+    /**
+     * Get the String representation of the task type
+     *
+     * @return String representation of the task type
+     */
     public abstract String getTaskType();
 
+    /**
+     * Get the String representation of the completion status
+     *
+     * @return String representation of the completion status
+     */
     public abstract String getStatus();
 
+    /**
+     * Get the String representation of the task description
+     *
+     * @return String representation of the task description
+     */
     public abstract String getDescription();
-    
+
+    /**
+     * Get the completion status of task
+     *
+     * @return completion status of task
+     */
     public boolean isCompleted() {
         return isCompleted;
     }
 
+    /**
+     * Set isCompleted as false
+     */
     public void markUncompleted() {
         isCompleted = false;
     }
 
+    /**
+     * Set isCompleted as true
+     */
     public void markCompleted() {
         isCompleted = true;
     }
 
+    /**
+     * Get the String representation of the task description
+     *
+     * @return String representation of the task description
+     */
     public String getTask() {
         return this.task;
     }
 
+    /**
+     * Convert Task Object to String to be saved in data file
+     *
+     * @return String representation of Task Object
+     */
     public String encode() {
         return getTaskType() + " | " + getStatus() + " | " + getDescription();
     }
 
+    /**
+     * Override toString method
+     *
+     * @return String representation of task object including completion status
+     */
     @Override
     public String toString() {
         return (isCompleted ? "[X] " + task : "[ ] " + task);
