5bb9873329f946971dfa080d1222f2281e99291c
roultitude <51879825+roultitude@users.noreply.github.com>
2023-01-28 22:58:27 +0800

    Replace String with LocalDateTime parsing

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,14 +1,23 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Deadline extends Task {
 
-    protected String by;
+    protected LocalDateTime by;
 
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        try {
+            this.by = LocalDateTime.parse(by, DATE_TIME_FORMAT);
+        } catch (DateTimeParseException e) {
+            System.out.println("Please use the correct format for dates, i.e. '12-10-2023 16:00'");
+            System.out.println(e.getMessage());
+        }
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + by.format(PRINT_FORMAT) + ")";
     }
 }
