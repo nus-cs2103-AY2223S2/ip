533a1e7de3ab44cfb0f3747119b4dc9d578e250f
bokuanT <bokuan2000@gmail.com>
2023-01-28 20:13:16 +0800

    Finalize coding standard conventions
    Style conventions are not fully adhered to.
    
    It is good practice to follow the given coding standard.
    
    Let's,
    * Strictly follow the coding standard and pass gradle's checkstyles

--- src/test/java/duke/tasklist/TaskListTest.java
+++ src/test/java/duke/tasklist/TaskListTest.java
@@ -8,7 +8,17 @@ import org.junit.jupiter.api.Test;
 import duke.task.Task;
 import duke.task.TaskStub;
 
+/**
+ * Test class for the <code>TaskList</code> class.
+ *
+ * @author Bo Kuan (LG17)
+ * @version CS2103T AY22/23 Semester 2
+ */
 public class TaskListTest {
+    /**
+     * Append <code>Task</code> to empty <code>TaskList</code>.
+     * @result <code>TaskList</code> will have 1 item.
+     */
     @Test
     @DisplayName("Task list append test")
     public void appendTest() {
