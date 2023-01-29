a900b5d4122ad9d1e306a1615cc16e17a1bce3cb
ScorpiusSigma <justinleng_@hotmail.com>
2023-01-29 10:53:10 +0800

    Change Request to Command

---
+++ src/main/java/duke/command/Command.java
@@ -0,0 +1,82 @@
+package command;
+
+import java.util.Arrays;
+import dukeexception.CommandException;
+import storage.Storage;
+import taskList.TaskList;
+import ui.Ui;
+
+public abstract class Command {
+    public enum Commands {
+        LIST, UNMARK, MARK, TODO, DEADLINE, EVENT, DELETE, EXIT, DOES_NOT_EXIST
+    };
+
+    private String request;
+    private Commands command;
+    private boolean isExit = false;
+
+
+    public Command(Commands command) {
+        this.command = command;
+    }
+
+    public Command(String request) {
+        this.request = request;
+        this.command = getCommand(request);
+    }
+
+    public boolean isExit() {
+        return this.isExit;
+    }
+
+    public void toggleIsExit() {
+        this.isExit = !this.isExit;
+    }
+
+    public String getRequest() {
+        return this.request;
+    }
+
+    public Commands getCommand() {
+        return this.command;
+    }
+
+    public String[] unwrap() throws CommandException {
+        String[] values = this.request.split(" ");
+        String description = String.join(" ", Arrays.copyOfRange(values, 1, values.length));
+
+        if (this.command != Commands.LIST && description.isEmpty()) {
+            throw new CommandException("Command description cannot be empty!");
+        }
+
+        return new String[] { description };
+    }
+
+    public void checkRequestRequirement() throws CommandException {
+    }
+
+    public static Commands getCommand(String request) {
+        String cmd = request.split(" ")[0];
+
+        switch (cmd) {
+            case "list":
+                return Commands.LIST;
+            case "unmark":
+                return Commands.UNMARK;
+            case "mark":
+                return Commands.MARK;
+            case "todo":
+                return Commands.TODO;
+            case "deadline":
+                return Commands.DEADLINE;
+            case "event":
+                return Commands.EVENT;
+            case "delete":
+                return Commands.DELETE;
+            case "bye":
+                return Commands.EXIT;
+            default:
+                return Commands.DOES_NOT_EXIST;
+        }
+    }
+}
