e562f39e9478c08a2dab0186bd8c2a1e7841e7a1
ScorpiusSigma <justinleng_@hotmail.com>
2023-01-29 17:49:50 +0800

    Task class: documentation

--- src/main/java/duke/tasks/Task.java
+++ src/main/java/duke/tasks/Task.java
@@ -1,34 +1,74 @@
 package tasks;
 
+/**
+ * Task is the abstract base class for all tasks
+ * 
+ * @author Leng Wei Cong, Justin
+ */
 abstract public class Task {
     private String description;
     private boolean isDone;
 
+    /**
+     * Constructor.
+     * 
+     * @param description
+     */
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
 
+    /**
+     * Returns the status icon of the task based {@link #isDone}.
+     * 
+     * @return a string that repesents true as X and false as " "
+     */
     private String getStatusIcon() {
         return this.isDone ? "X" : " ";
     }
 
+    /**
+     * Marks the task as done.
+     * 
+     * @return nothing
+     */
     public void markAsDone() {
         this.isDone = true;
     }
 
+    /**
+     * Marks the task as not done.
+     * 
+     * @return nothing
+     */
     public void markAsNotDone() {
         this.isDone = false;
     }
 
+    /**
+     * Returns the description of the task.
+     * 
+     * @return the description of the task
+     */
     public String getDescription() {
         return this.description;
     }
 
+    /**
+     * Returns the boolean of the task done status.
+     * 
+     * @return the boolean of the task done status
+     */
     public boolean getIsDone() {
         return this.isDone;
     }
 
+    /**
+     * Returns a string representation of the task.
+     * 
+     * @return a string representation of the task
+     */
     @Override
     public String toString() {
         return String.format("[%s] %s", getStatusIcon(), this.description);
