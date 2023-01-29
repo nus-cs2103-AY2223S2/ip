bd93c3ca7acfc016b17c7ec5879c0af68073db26
ScorpiusSigma <justinleng_@hotmail.com>
2023-01-29 18:12:20 +0800

    Storage class: documentation

--- src/main/java/duke/storage/Storage.java
+++ src/main/java/duke/storage/Storage.java
@@ -5,18 +5,26 @@ import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
-
 import taskList.TaskList;
 import tasks.Deadline;
 import tasks.Event;
 import tasks.Task;
 import tasks.Todo;
 
+/**
+ * Storage class to interact with the text file store in ./data.
+ */
 public class Storage {
     private String cwd = System.getProperty("user.dir");
     private java.nio.file.Path dataPath;
     private String dataPathString;
 
+    /**
+     * Serialises task into a string that is storable in the text file.
+     * 
+     * @param task the task to be serialise
+     * @return the string of the serialised task
+     */
     private String serialise(Task task) {
         String data = "";
         String isDone = (task.getIsDone() ? "1" : "0");
@@ -42,6 +50,12 @@ public class Storage {
         return data;
     }
 
+    /**
+     * Deserialises the task string to a Task instance.
+     * 
+     * @param data the serialised task
+     * @return the task that has been deserialised from the input
+     */
     private Task deserialise(String data) {
         String[] params = data.split("\\|");
         String taskType = params[0].strip();
@@ -73,57 +87,82 @@ public class Storage {
         return task;
     }
 
+    /**
+     * Constructor.
+     * 
+     * @param filePath the path to the text file that stores the serialised tasks
+     */
     public Storage(String filePath) {
         this.dataPath = java.nio.file.Paths.get(cwd, filePath.split("/"));
         this.dataPathString = dataPath.toString();
     }
 
+    /**
+     * Returns the tasks from the text file.
+     * 
+     * @return the task list from the text file
+     */
     public TaskList load() {
         TaskList tasks = new TaskList();
 
         try {
             boolean directoryExists = java.nio.file.Files.exists(dataPath);
 
+            // 1. If the directory does not exist.
             if (!directoryExists) {
                 try {
+                    // 1a. creates a new file from the given data path.
                     new File(this.dataPathString).createNewFile();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
 
+            // 2. reads the file from the given data path.
             File dukeFile = new File(this.dataPathString);
             Scanner scanner = new Scanner(dukeFile);
 
+            // 3. Loops through the lines in the file.
             while (scanner.hasNextLine()) {
                 String data = scanner.nextLine();
+                // 3a. deserialises the line.
                 Task task = this.deserialise(data);
                 tasks.add(task);
             }
 
             scanner.close();
         } catch (FileNotFoundException error) {
-            // Try already handles the creation of new file when it doesn't exist
+            // Try already handles the creation of new file when it doesn't exist.
         }
 
         return tasks;
     }
 
+    /**
+     * Saves the tasks into the text file.
+     * 
+     * @param tasks the task list to be saved
+     */
     public void save(TaskList tasks) {
         boolean directoryExists = java.nio.file.Files.exists(dataPath);
 
+        // 1. If the directory does not exist.
         if (!directoryExists) {
             try {
+                // 1a. creates a new file from the given data path.
                 new File(this.dataPathString).createNewFile();
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
 
+        // 2. Writes task into file.
         try {
             FileWriter writerObj = new FileWriter(this.dataPathString, false);
-            for (int i = 0; i < tasks.size(); i++)
+            // 2a. serialise task into string to write into the file.
+            for (int i = 0; i < tasks.size(); i++) {
                 writerObj.write(this.serialise(tasks.get(i)) + "\n");
+            }
             writerObj.close();
         } catch (IOException e) {
             e.printStackTrace();
