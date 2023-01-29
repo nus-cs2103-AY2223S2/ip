96b1e0d9690217eb220b7389c2982a6727cc84c2
gitsac <isaacchai99@gmail.com>
2023-01-29 16:43:41 +0800

    Add A-JUnit Increment

---
+++ src/test/java/duke/EventsTest.java
@@ -0,0 +1,32 @@
+package duke;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import duke.tasktypes.Events;
+public class EventsTest {
+
+    @Test
+    public void testEmpty() {
+        String caughtIt = "";
+        try {
+            Events emptyOne = new Events("");
+        } catch  (DukeExceptions DE){
+            caughtIt = DE.toString();
+        }
+        assertEquals(":( Sorry, the description of an event cannot be empty!", caughtIt);
+    }
+
+    @Test
+    public void testUndoneValid() throws DukeExceptions {
+        assertEquals("[E][ ]test1 (from: Monday to: Thursday)", new Events("test1 /from Monday /to Thursday").toString());
+    }
+
+    @Test
+    public void testDoneValid() throws DukeExceptions {
+        Events toCheck = new Events("test2 /from now /to next month");
+        toCheck.setDone();
+        assertEquals("[E][X]test2 (from: now to: next month)", toCheck.toString());
+    }
+}
