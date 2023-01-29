e6b7f7bbefefdc65d9f69f6aca45858297e758a4
ScorpiusSigma <justinleng_@hotmail.com>
2023-01-29 17:37:07 +0800

    Duke class: Fix indentation

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -13,75 +13,75 @@ import ui.Ui;
  * @author Leng Wei Cong, Justin
  */
 public class Duke {
-  private Storage storage;
-  private TaskList tasks;
-  private Ui ui;
+	private Storage storage;
+	private TaskList tasks;
+	private Ui ui;
 
-  /**
-   * Constructor
-   * 
-   * @param filePath the path to the text file that stores the tasks
-   * @return nothing
-   */
-  public Duke(String filePath) {
-    this.ui = new Ui();
-    this.storage = new Storage(filePath);
-    this.tasks = new TaskList(this.storage.load());
-  }
+	/**
+	 * Constructor
+	 * 
+	 * @param filePath the path to the text file that stores the tasks
+	 * @return nothing
+	 */
+	public Duke(String filePath) {
+		this.ui = new Ui();
+		this.storage = new Storage(filePath);
+		this.tasks = new TaskList(this.storage.load());
+	}
 
-  /**
-   * Runs Duke.
-   * 
-   * @return nothing
-   */
-  public void run() {
-    this.ui.showWelcome();
-    this.handleRequest();
-    this.ui.showExit();
-  }
+	/**
+	 * Runs Duke.
+	 * 
+	 * @return nothing
+	 */
+	public void run() {
+		this.ui.showWelcome();
+		this.handleRequest();
+		this.ui.showExit();
+	}
 
-  /**
-   * Handles the request from user input.
-   * 
-   * @return nothing
-   */
-  public void handleRequest() {
-    while (true) {
-      try {
-        String fullcommand = ui.readCommand();
-        Command command = Parser.parse(fullcommand);
+	/**
+	 * Handles the request from user input.
+	 * 
+	 * @return nothing
+	 */
+	public void handleRequest() {
+		while (true) {
+			try {
+				String fullcommand = ui.readCommand();
+				Command command = Parser.parse(fullcommand);
 
-        // Breaks when is an exit command
-        if (command.isExit()) {
-          break;
-        }
+				// Breaks when is an exit command
+				if (command.isExit()) {
+					break;
+				}
 
-        // Executes command
-        command.execute(tasks, ui, storage);
+				// Executes command
+				command.execute(tasks, ui, storage);
 
-      } finally {
-        ui.showDivider();
-      }
-    }
-  }
+			} finally {
+				ui.showDivider();
+			}
+		}
+	}
 
-  /**
-   * Prints whatever the user inputs.
-   * 
-   * @param input the user input
-   * @return nothing
-   */
-  public void echo(String input) {
-    ui.showMessage(input);
-  }
+	/**
+	 * Prints whatever the user inputs.
+	 * 
+	 * @param input the user input
+	 * @return nothing
+	 */
+	public void echo(String input) {
+		ui.showMessage(input);
+	}
 
-  /**
-   * This is the main method which makes use of run method.
-   * 
-   * @param args Unused.
-   * @return Nothing.
-   */
-  public static void main(String[] args) {
-    new Duke("data/duke.txt").run();
-  }
+	/**
+	 * This is the main method which makes use of run method.
+	 * 
+	 * @param args Unused.
+	 * @return Nothing.
+	 */
+	public static void main(String[] args) {
+		new Duke("data/duke.txt").run();
+	}
 }
