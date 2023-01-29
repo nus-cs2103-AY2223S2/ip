8fbce28a8bcc4b95db80c9d3c73647b13df071d2
Elvern <elvern18@gmail.com>
2023-01-29 22:07:06 +0800

    merged level 8 branch w master

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -24,7 +24,7 @@ public class Duke {
 
     public static ArrayList<Task> list;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println(ENTRY);
         Scanner sc = new Scanner(System.in);
         list = new ArrayList<Task>();
