5db62e11b368e954cc9d6e5727f44f07fabbc368
UnwilledTangent <tinsean.work@gmail.com>
2023-01-29 18:03:40 +0800

    Do Level-9 Find

--- src/main/duke/src/Duke.java
+++ src/main/duke/src/Duke.java
@@ -2,8 +2,7 @@ public class Duke {
     private Storage storage;
     private TaskList tasks;
     private Ui ui;
-
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+//    public static void main(String[] args) {
+//        System.out.println("Hello world!");
+//    }
 }
