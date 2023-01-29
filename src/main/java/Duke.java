50591a7a080ee382cbba2634fe644d4c4482ecf0
Niu BoQian <e0774823@u.nus.edu>
2023-01-29 14:54:10 +0800

    main class: only use an ArrayList to store all tasks
    tasks input by the user can only be saved while Duke is running and will be gone once Duke stops running
    
    Create a data folder with a task.txt file in it to permanently store all tasks
    
    Additionally:
    1. changed line break style
    2. added different greeting messages depending on the existence of data folder and/or tasks.txt file

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,19 +1,13 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
 
 public class Duke {
-
-    /**
-     * method to print messages in desired format
-     * @param message a string describing the message
-     */
-    static void printToFormat(String message) {
-        String lineBreak1 = "(((***---***---***---***---***---***\n";
-        String lineBreak2 = "\n---***---***---***---***---***---***)))\n\n";
-        System.out.println(lineBreak1 + message + lineBreak2);
-    }
     public static void main(String[] args) {
-        ArrayList<Task> tasks = new ArrayList<>();
-
         Scanner sc = new Scanner(System.in);
 
         String logo = " ____        _        \n"
@@ -22,8 +16,52 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        
+        /**
+         * allTasks: the file that constains all the tasks
+         * taskFolder: the folder taht contains allTasks
+         */
+        File allTasks = new File("data/tasks.txt");
+        File taskFolder = new File("data");
+        ArrayList<Task> tasks = new ArrayList<Task>();
+
+        /**
+         * print different starting messages depending on whether allTasks and/or
+         * taskFolder exist
+         */
+        if (!taskFolder.exists()) {
+            printToFormat("The default Task Folder is not found, creating data folder with task file...");
+            taskFolder.mkdir();
+            System.out.println("---Task Folder created successfully");
+            File f = new File(taskFolder, "task.txt");
+            try {
+                f.createNewFile();
+                System.out.printf("---Task File created successfully\n---ready to create tasks\n");
+            } catch (IOException e) {
+                System.out.println("Error creating file: " + e.getMessage());
+            }
+        } else if (!allTasks.exists()) {
+            printToFormat("The default tasks do not exist, creating default task file...");
+            File f = new File(taskFolder, "task.txt");
+            try {
+                f.createNewFile();
+                System.out.printf("---Task File created successfully\n---ready to create tasks\n");
+            } catch (IOException e) {
+                System.out.println("Error creating file: " + e.getMessage());
+            }
+        } else {
+            try {
+                loadDefaultTasks(tasks, allTasks);
+            } catch (FileNotFoundException e) {
+                System.out.println("Could not load the default tasks: " + e.getMessage());
+            }
+            printToFormat("---Default Task List successfully loaded");
+        }
+
+
 
         while (sc.hasNext()) {
+            int byeIndicator = 0;
             try {
                 /**
                  * @param inputLine: a String that is the command entered by the user
@@ -37,6 +75,7 @@ public class Duke {
                 switch (command) {
                     case BYE: 
                         printToFormat("    GoodBye, have a nice day!");
+                        byeIndicator = 1;
                         break;
                     case LIST:
                     /**
@@ -82,7 +121,7 @@ public class Duke {
                      * creates and adds a deadline task to the arraylist of all tasks
                      */
                         String[] parts = inputLine.split("/");
-                        Deadline task = new Deadline(parts[0].split(" ")[1], false, parts[1]);
+                        Deadline task = new Deadline(parts[0].split(" ")[1], 0, parts[1]);
                         tasks.add(task);
                         printToFormat("    Successfully added the following task:\n    " + task);
                         break;
@@ -91,7 +130,7 @@ public class Duke {
                      * creates and adds an event task to the arraylist of all tasks
                      */
                         String[] parts1 = inputLine.split("/");
-                        Event event = new Event(parts1[0].split(" ")[1], false, parts1[1], parts1[2]);
+                        Event event = new Event(parts1[0].split(" ")[1], 0, parts1[1], parts1[2]);
                         tasks.add(event);
                         printToFormat("    Successfully added the following task:\n    " + event);
                         break;
@@ -99,7 +138,7 @@ public class Duke {
                     /**
                      * creates and adds a todo task to the arraylist of all tasks
                      */
-                        Todo todo = new Todo(inputLine.split(" ", 2)[1], false);
+                        Todo todo = new Todo(inputLine.split(" ", 2)[1], 0);
                         tasks.add(todo);
                         printToFormat("    Successfully added the following task:\n    " + todo);
                         break;
@@ -116,7 +155,39 @@ public class Duke {
                  */
                 printToFormat("    " + e.getMessage());
             }
+            if (byeIndicator == 1) {
+                break;
+            }
         }
+    }
+
+    /**
+     * method to print messages in desired format with a starting line and an ending line
+     * @param message a string describing the message
+     */
+    private static void printToFormat(String message) {
+        String lineBreak1 = "-->-->-->-->-->-->-->-->-->-->-->\n";
+        String lineBreak2 = "\n<--<--<--<--<--<--<--<--<--<--\n\n";
+        System.out.println(lineBreak1 + message + lineBreak2);
+    }
+
+    private static void loadDefaultTasks(ArrayList<Task> tasks, File file) throws FileNotFoundException {
+        Scanner s = new Scanner(file);
+        while(s.hasNext()) {
+            String[] lineArr = s.nextLine().split("/");
+            switch (lineArr[0]) {
+                case "D":
+                    tasks.add(new Deadline(lineArr[1], Integer.parseInt(lineArr[2]), lineArr[3]));
+                    break;
+                case "T":
+                    tasks.add(new Todo(lineArr[1], Integer.parseInt(lineArr[2])));
+                    break;
+                case "E":
+                    tasks.add(new Event(lineArr[1], Integer.parseInt(lineArr[2]), lineArr[3], lineArr[4]));
+                    break;
+            }
+        }
+        s.close();
     }
 }
 
@@ -132,9 +203,9 @@ class Task {
     protected String name;
     protected String status;
 
-    public Task(String name, boolean status) {
+    public Task(String name, int status) {
         this.name = name;
-        if (status) {
+        if (status == 1) {
             this.status = "[X]";
         } else {
             this.status = "[ ]";
@@ -172,7 +243,7 @@ class Deadline extends Task {
      * @param status: a String indicating whether the task is done or not
      * @param deadline: a string indicating the deadline of the task
      */
-    Deadline (String name, boolean status, String deadline) {
+    Deadline (String name, int status, String deadline) {
         super(name, status);
         this.deadline = deadline;
     }
@@ -194,7 +265,7 @@ class Event extends Task {
      * @param from: a string representing the starting time passed in by the user
      * @param to: a string representing the ending time passed in by the user
      */
-    Event(String name, boolean status, String from, String to) {
+    Event(String name, int status, String from, String to) {
         super(name, status);
         this.from = from;
         this.to = to;
@@ -213,7 +284,7 @@ class Todo extends Task {
      * @param name: a String indicating the name of the todo task
      * @param status: a String indicating whether the task is done
      */
-    Todo(String name, boolean status) {
+    Todo(String name, int status) {
         super(name, status);
     }
     /**
