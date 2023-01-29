a4f2bc0d220cfaa5bc9b681e75e5d0146ab2a425
bokung <e0774428@u.nus.edu>
2023-01-29 12:58:08 +0800

    Treat deadlines as dates

--- src/main/java/task/Deadline.java
+++ src/main/java/task/Deadline.java
@@ -1,16 +1,35 @@
 package task;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+import java.util.Optional;
+
+import output.Printer;
+
 public class Deadline extends Task {
 
-    private String deadline;
+    private String deadlineString;
+    private Optional<LocalDateTime> deadlineTime = Optional.empty();
 
     public Deadline(String task, String deadline) {
         super(task);
-        this.deadline = deadline;
+        this.deadlineString = deadline;
+        try {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/yyyy HHmm");
+            this.deadlineTime = Optional.of(LocalDateTime.parse(deadline, formatter));
+        } catch (DateTimeParseException e) {
+            Printer.printError("Could not parse date");
+        }
     }
+
 
     @Override
     public String toString() {
-        return String.format("%s%s (by: %s)", "[D]", super.toString(), deadline);
+        return String.format("%s%s (by: %s)", 
+                    "[D]", 
+                            super.toString(),
+                            deadlineTime.map(Object::toString).orElse(deadlineString));
     }
 }
