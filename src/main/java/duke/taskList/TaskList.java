10641040ac98cd1916a32df6e132fe82dce5d006
ScorpiusSigma <justinleng_@hotmail.com>
2023-01-29 10:46:13 +0800

    Add TaskList an inheritance of ArrayList<Task>

---
+++ src/main/java/duke/taskList/TaskList.java
@@ -0,0 +1,45 @@
+package taskList;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import dukeexception.StorageException;
+import task.Task;
+
+public class TaskList extends ArrayList<Task> {
+    public TaskList() {
+        super();
+    }
+
+    public TaskList(TaskList tasks) {
+        super.addAll(tasks);
+    }
+
+    public List<Task> getList() {
+        return Collections.unmodifiableList(this);
+    }
+
+    public boolean add(Task item) {
+        boolean added = super.add(item);
+
+        return added;
+    }
+
+    public Task get(int id) {
+        return super.get(id);
+    }
+
+    public Task delete(int id) throws StorageException {
+        if (id >= this.size()) {
+            throw new StorageException("☹ OOPS!!! delete index does not exist");
+        }
+
+        Task task = super.remove(id);
+
+        return task;
+    }
+
+    public int size() {
+        return super.size();
+    }
+}
