d5439a0e3693531445e1c89b07b2a0dc442bc591
ScorpiusSigma <justinleng_@hotmail.com>
2023-01-29 17:40:07 +0800

    Ui class: Documentation

--- src/main/java/duke/ui/Ui.java
+++ src/main/java/duke/ui/Ui.java
@@ -1,57 +1,122 @@
 package ui;
 
-
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.IntStream;
 import dukeexception.DukeException;
 import tasks.Task;
 
+/**
+ * The Ui class handles all the UI related operations, such as printing
+ * messages, displaying errors and dividing lines.
+ * It also reads commands from the user and prints out the list of tasks.
+ * 
+ * @author Leng Wei Cong, Justin
+ */
 public class Ui {
+    /**
+     * Scanner object for reading user commands.
+     */
     Scanner scanner = new Scanner(System.in);
 
+    /**
+     * Displays divider line on the console.
+     * 
+     * @return nothing
+     */
     public void showDivider() {
         System.out.println("\n_______________________________\n");
     }
 
+    /**
+     * Displays error message on the console.
+     * 
+     * @param error the error caught
+     * @return nothing
+     */
     public void showError(DukeException error) {
         System.out.println(error.getMessage());
     }
 
+    /**
+     * Displays loading error on the console.
+     * 
+     * @return nothing
+     */
     public void showLoadingError() {
 
     }
 
+    /**
+     * Displays welcome message on the console.
+     * 
+     * @return nothing
+     */
     public void showWelcome() {
         System.out.println("Hello! I'm Duke\nWhat can I do for you?\n");
 
     }
 
+    /**
+     * Displays exit message on the console.
+     * 
+     * @return nothing
+     */
     public void showExit() {
         this.close();
         System.out.println("toodeloo!\n");
     }
 
+    /**
+     * Reads user command.
+     * 
+     * @return user input
+     */
     public String readCommand() {
         return scanner.nextLine();
     }
 
+    /**
+     * Closes the scanner.
+     * 
+     * @return nothing
+     */
     public void close() {
         scanner.close();
     }
 
+    /**
+     * Displays message on the console when task is added.
+     * 
+     * @param task      the task that is being added
+     * @param tasksSize the size of the taskList
+     * @return nothing
+     */
     public void showTaskAdded(Task task, int tasksSize) {
         System.out.println("Got it. I've added this task:");
         System.out.println(task);
         System.out.println("Now you have " + tasksSize + " tasks in the list.");
     }
 
+    /**
+     * Displays message on the console when task is removed.
+     * 
+     * @param task      the task that is being added
+     * @param tasksSize the size of the taskList
+     * @return nothing
+     */
     public void showTaskRemoved(Task task, int tasksSize) {
         System.out.println("Noted. I've removed this task:");
         System.out.println(task);
         System.out.println("Now you have " + tasksSize + " tasks in the list.");
     }
 
+    /**
+     * Displays the a list of task from taskList on the console.
+     * 
+     * @param tasks the taskList
+     * @return nothing
+     */
     public void showTaskList(List<Task> tasks) {
         IntStream.range(0, tasks.size())
                 .forEach((index) -> {
@@ -59,8 +124,12 @@ public class Ui {
                 });
     }
 
+    /**
+     * Displays message on the console.
+     * 
+     * @param message
+     */
     public void showMessage(String message) {
         System.out.println(message);
     }
-
 }
