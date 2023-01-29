c97e4bac7b380c8f8c564d95b2857501f5ccea90
Kim Young Jin <kim@locofy.ai>
2023-01-29 16:14:22 +0800

    Do level 8

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,16 +1,28 @@
-public class Event extends Task {
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-    protected String from;
-    protected String to;
+public class Event extends Task {
+    protected LocalDateTime from;
+    protected LocalDateTime to;
+    private DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("d/M/yyyy HHmm");
+    private DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("MMM dd yyyy h:mma");
 
     public Event(String description, String from, String to) {
         super(description);
-        this.from = from;
-        this.to = to;
+        this.from = LocalDateTime.parse(from, dateTimeFormatter);
+        this.to = LocalDateTime.parse(to, dateTimeFormatter);
+    }
+
+    public String getFrom() {
+        return outputFormatter.format(from);
+    }
+
+    public String getTo() {
+        return outputFormatter.format(to);
     }
 
     @Override
     public String toString() {
-        return "[E]" + " " + super.getStatusIcon() + " " + super.toString() + " (from: " + from + " to: " + to + ")";
+        return "[E]" + " " + super.getStatusIcon() + " " + super.toString() + " (from: " + getFrom() + " to: " + getTo() + ")";
     }
 }
