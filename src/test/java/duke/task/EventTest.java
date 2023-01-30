779a9a4ba72b08f85054e9af137b476e60fe257f
Creationsv2 <jonlow1234@gmail.com>
2023-01-30 19:28:49 +0800

    Add increment A-CheckStyle

--- src/test/java/duke/task/EventTest.java
+++ src/test/java/duke/task/EventTest.java
@@ -1,9 +1,11 @@
 package duke.task;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+
 
 public class EventTest {
     @Test
