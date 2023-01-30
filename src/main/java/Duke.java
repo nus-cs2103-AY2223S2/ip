6b07de7f569f09afbbfd26634925f1a5176d158a
Niu BoQian <e0774823@u.nus.edu>
2023-01-30 23:37:14 +0800

    Completed A-JavaDoc

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,11 +5,17 @@ import duke.storage.Storage;
 import duke.tasklist.TaskList;
 import duke.ui.Ui;
 
+
 public class Duke {
     private Storage storage;
     private TaskList tasks;
     private Ui ui;
 
+    /**
+     * Initialises the Duke object
+     * @param filePath A string representing the path of the file containing all tasks
+     * @param folderPath A string representing the path of the folder containing the above file
+     */
     public Duke(String filePath, String folderPath) {
         ui = new Ui();
         storage = new Storage(filePath, folderPath);
@@ -21,6 +27,10 @@ public class Duke {
         }
     }
 
+    /**
+     * a function that runs the program by repeatedly accepting user input and respond to them
+     * until the user wants to exit the program.
+     */
     public void run() {
         ui.welcome();
         boolean isExit = false;
@@ -30,11 +40,15 @@ public class Duke {
                 Command c = Parser.parse(inputLine);
                 c.execute(tasks, ui, storage);
                 isExit = c.isExit();
-            } catch (Exception e) {
+            } catch (DukeException e) {
                 System.out.println(e.getMessage());
             }
         }
     }
+
+    /**
+     * main method that starts running the program
+     */
     public static void main(String[] args) {
         new Duke("data/tasks.txt", "data").run();
     }
