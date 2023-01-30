9863ad0fe70f5b3473d92b7ba7cb663bfac19fd6
niekis <funleon00@gmail.com>
2023-01-30 16:40:45 +0800

    Update deadline parser to recognize dates
    Deadline task's deadlines are stored exclusively as strings.
    
    Program is unable to make sense of the timing of deadlines.
    
    Let's update the parser to detect deadline tasks created with
    a deadline following the format "dd/mm/yyyy" and store deadlines
    as dates.
    
    Dates can be expressed more intuitively by the system. Features that
    compare the deadlines of tasks can be added in the future.

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,16 +1,27 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 // deadline is a type of task that takes in an additional string
 // as its deadline
 public class Deadline extends Task {
-    private String deadlineTiming;
+
+    private String timingString;
+    private LocalDate timingLocalDate;
 
     public Deadline(String description, String deadlineTiming) {
         super(description);
-        this.deadlineTiming = deadlineTiming;
+        try {
+            timingLocalDate = LocalDate.parse(deadlineTiming);
+            timingString = timingLocalDate.format(
+                    DateTimeFormatter.ofPattern("MMM dd yyyy"));
+        } catch (DateTimeParseException e) {
+            timingString = deadlineTiming;
+        }
     }
 
     @Override
     public String toString() {
-        return "[D]"
-                + super.toString() + "(by: " + deadlineTiming + ")";
+        return "[D]" + super.toString() + "(by: " + timingString + ")";
     }
 }
