5a78c6546c80503573b94f1803fa7d11961969a8
homuntan02 <homuntan02@gmail.com>
2023-01-31 01:14:56 +0800

    Level-8 completed

--- src/main/java/tasks/Deadline.java
+++ src/main/java/tasks/Deadline.java
@@ -1,23 +1,40 @@
 package tasks;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import dukeexceptions.DukeExceptions;
 
 public class Deadline extends Task {
 
-    protected String by;
+    protected LocalDateTime by;
     private static final String tag = "D";
 
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        try {
+            LocalDate byLocalDate = LocalDate.parse(by.split(" ")[0]);
+            LocalTime byLocalTime = LocalTime.parse(by.split(" ")[1]);
+
+            this.by = LocalDateTime.of(byLocalDate, byLocalTime);
+        } catch (DateTimeParseException e) {
+            throw new DukeExceptions("Format of date was not recognized, use YYYY-MM-DD and HH:MM");
+        }
     }
 
     public String saveTask() {
         String completed = this.isDone? "1":"0";
+        String formattedDate = by.format(DateTimeFormatter.ofPattern("MM/dd/yyyy 'at' hh:mm a"));
+
         return this.tag + " | " + completed + " | "
-                + this.description + " | " + this.by;
+                + this.description + " | " + formattedDate;
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by:" + by + ")" +"\n";
+        return "[D]" + super.toString()
+                + " (by:" + by.format(DateTimeFormatter.ofPattern("MM/dd/yyyy 'at' hh:mm a")) + ")"
+                +"\n";
     }
 }
--- src/main/java/tasks/Events.java
+++ src/main/java/tasks/Events.java
@@ -1,24 +1,49 @@
 package tasks;
+import dukeexceptions.DukeExceptions;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Events extends Task {
 
-    protected String from;
-    protected String to;
+    protected LocalDateTime from;
+    protected LocalDateTime to;
     private static final String tag = "E";
 
     public Events(String description, String from, String to) {
         super(description);
-        this.from = from;
-        this.to = to;
+        try {
+            String[] fromSplit = from.split(" ");
+            LocalDate fromLocalDate = LocalDate.parse(fromSplit[0]);
+            LocalTime fromLocalTime = LocalTime.parse(fromSplit[1]);
+            this.from = LocalDateTime.of(fromLocalDate, fromLocalTime);
+
+            String[] toSplit = to.split(" ");
+            LocalDate toLocalDate = LocalDate.parse(toSplit[0]);
+            LocalTime toLocalTime = LocalTime.parse(toSplit[1]);
+            this.to = LocalDateTime.of(toLocalDate, toLocalTime);
+
+        } catch (DateTimeParseException e) {
+            throw new DukeExceptions("Format of date was not recognized, use YYYY-MM-DD and HH:MM");
+        }
     }
 
     public String saveTask() {
         String completed = this.isDone? "1":"0";
         return this.tag + " | " + completed + " | "
-                + this.description + " | " + this.from + "-" + this.to;
+                + this.description
+                + " | " + this.from.format(DateTimeFormatter.ofPattern("MM/dd/yyyy 'at' hh:mm a"))
+                + " - " + this.to.format(DateTimeFormatter.ofPattern("MM/dd/yyyy 'at' hh:mm a"));
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + "(from:" + from + " to:" + to + ")" +"\n";
+        return "[E]" + super.toString()
+                + "(from:" + this.from.format(DateTimeFormatter.ofPattern("MM/dd/yyyy 'at' hh:mm a"))
+                + " to:" + this.to.format(DateTimeFormatter.ofPattern("MM/dd/yyyy 'at' hh:mm a"))
+                + ")" +"\n";
     }
 }
