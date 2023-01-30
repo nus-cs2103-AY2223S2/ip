ef6ae0b7df920f1fdfa2c5f6d0e6f685d4618e8d
dunliang0513 <dunliang014@gmail.com>
2023-01-30 23:13:59 +0800

    Teach Duke how to understand dates and time

--- src/main/java/Tasks/Deadline.java
+++ src/main/java/Tasks/Deadline.java
@@ -1,15 +1,40 @@
 package Tasks;
 
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeParseException;
+
 public class Deadline extends Task {
 
     protected String dueDate;
-    public Deadline(String description, String dueDate) {
+    protected LocalDate localDate;
+
+    public Deadline(String description, String dueDate) throws DateTimeParseException {
         super(description);
         this.dueDate = dueDate;
+        this.localDate = LocalDate.parse(dueDate);
+    }
+
+    public boolean isDueSoon() {
+        if (this.done) {
+            return false;
+        } else {
+            LocalDate currentTime = LocalDate.now();
+            Period interval = Period.between(currentTime, localDate);
+            int years = interval.getYears();
+            int months = interval.getMonths();
+            int days = interval.getDays();
+            if (years == 0 && months == 0 && days <= 3) {
+                return true;
+            }
+            return false;
+        }
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + this.dueDate + ")";
+        return "[D]" + super.toString() + " (" + localDate.getMonth() + " "
+                + localDate.getDayOfMonth() + " " + localDate.getYear() + ")";
     }
 }
--- src/main/java/Tasks/Event.java
+++ src/main/java/Tasks/Event.java
@@ -1,17 +1,41 @@
 package Tasks;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.Period;
+import java.time.format.DateTimeParseException;
+
 public class Event extends Task {
     protected String startingTime;
     protected String endingTime;
-    public Event(String description, String startingTime, String endingTime) {
+    protected LocalDate localStartingDate;
+
+    public Event(String description, String startingTime, String endingTime) throws DateTimeParseException {
         super(description);
         this.startingTime = startingTime;
         this.endingTime = endingTime;
+        this.localStartingDate = LocalDate.parse(startingTime);
+    }
+
+    public boolean isComingSoon() {
+        if (this.done) {
+            return false;
+        } else {
+            LocalDate currentTime = LocalDate.now();
+            Period interval = Period.between(currentTime, localStartingDate);
+            int years = interval.getYears();
+            int months = interval.getMonths();
+            int days = interval.getDays();
+            if (years == 0 && months == 0 && days <= 7) {
+                return true;
+            }
+            return false;
+        }
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " +
-                this.startingTime + " to: " + endingTime + ")";
+        return "[E]" + super.toString() + " (" + localStartingDate.getMonth() + " "
+                + localStartingDate.getDayOfMonth() + " " + localStartingDate.getYear() + ")";
     }
 }
