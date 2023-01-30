ca17bd508dd6af71a36e56da4fa821526a55cbf0
Carmen <carmenang12@gmail.com>
2023-01-30 11:47:52 +0800

    Add GUI

--- src/main/java/duke/Launcher.java
+++ src/main/java/duke/Launcher.java
@@ -1,2 +1,15 @@
-package duke;public class Launcher {
+package duke;
+
+import javafx.application.Application;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+
+/**
+ * A launcher class to workaround classpath issues.
+ */
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(Duke.class, args);
+    }
 }
