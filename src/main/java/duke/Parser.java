01560fa023bba3aa23a1932677b720ec2163db58
Sean Wong <58366602+ExtraShotLatte@users.noreply.github.com>
2023-01-30 20:46:48 +0800

    Refactor Parser to match naming convention

--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -4,7 +4,7 @@ public class Parser {
         
     }
 
-    public boolean checkForExit(String s) {
+    public boolean isExit(String s) {
         return s.equalsIgnoreCase("bye");
     }
 
@@ -13,79 +13,79 @@ public class Parser {
         int selectedNum;
         String[] inputCmdArr = inputCommand.split(" ");
         switch(inputCmdArr[0]) {
-            case "list":
-                tasks.printTask();
-                break;
-            case "todo":
-                String todoDesc;
-                try {
-                    todoDesc = inputCommand.split(" ", 2)[1];
-                } catch (Exception e) {
-                    throw new DukeException("Description of todo cannot be empty!!");
-                }
-                tasks.addTask(todoDesc, false);
-                tasks.printNewestTask();
-                storage.save(tasks);
-                break;
-            case "deadline":
-                String deadlineInput;
-                try {
-                    deadlineInput = inputCommand.split(" ", 2)[1];
-                } catch (Exception e) {
-                    throw new DukeException("Description of deadline cannot be empty!!");
-                }
-                String[] deadlineDesc = deadlineInput.split(" /by ");
-                tasks.addTask(deadlineDesc[0], deadlineDesc[1], false);
-                tasks.printNewestTask();
-                storage.save(tasks);
-                break;
-            case "event":
-                String eventInput;
-                try {
-                    eventInput = inputCommand.split(" ", 2)[1];
-                } catch (Exception e) {
-                    throw new DukeException("Description of event cannot be empty!!");
-                }
-                String[] eventDescArr = eventInput.split(" /from ");
-                String eventDesc = eventDescArr[0];
-                String[] eventTimeArr = eventDescArr[1].split(" /to ");
-                String eventFrom = eventTimeArr[0];
-                String eventTo = eventTimeArr[1];
-                tasks.addTask(eventDesc, eventFrom, eventTo, false);
-                tasks.printNewestTask();
-                storage.save(tasks);
-                break;
-            case "mark":
-                selectedNum = Integer.parseInt(inputCmdArr[1]);
-                tasks.markTask(selectedNum);
-                storage.save(tasks);
-                break;
-            case "unmark":
-                selectedNum = Integer.parseInt(inputCmdArr[1]);
-                tasks.unMarkTask(selectedNum);
-                storage.save(tasks);
-                break;
-            case "delete":
-                int numToDelete;
-                try {
-                    numToDelete = Integer.parseInt(inputCommand.split(" ", 2)[1]);
-                } catch (Exception e) {
-                    throw new DukeException("Please enter a valid number to delete!");
-                }
-                tasks.deleteTask(numToDelete);
-                storage.save(tasks);
-                break;
-            case "Storage":
-                //System.out.println("I RAN HERE!");
-                tasks = storage.load();
-                break;
-            case "Save":
-                storage.save(tasks);
-                break;
-            case "bye":
-                break;
-            default:
-                throw new DukeException("I don't get it!");
+        case "list":
+            tasks.printTask();
+            break;
+        case "todo":
+            String todoDesc;
+            try {
+                todoDesc = inputCommand.split(" ", 2)[1];
+            } catch (Exception e) {
+                throw new DukeException("Description of todo cannot be empty!!");
+            }
+            tasks.addTask(todoDesc, false);
+            tasks.printNewestTask();
+            storage.save(tasks);
+            break;
+        case "deadline":
+            String deadlineInput;
+            try {
+                deadlineInput = inputCommand.split(" ", 2)[1];
+            } catch (Exception e) {
+                throw new DukeException("Description of deadline cannot be empty!!");
+            }
+            String[] deadlineDesc = deadlineInput.split(" /by ");
+            tasks.addTask(deadlineDesc[0], deadlineDesc[1], false);
+            tasks.printNewestTask();
+            storage.save(tasks);
+            break;
+        case "event":
+            String eventInput;
+            try {
+                eventInput = inputCommand.split(" ", 2)[1];
+            } catch (Exception e) {
+                throw new DukeException("Description of event cannot be empty!!");
+            }
+            String[] eventDescArr = eventInput.split(" /from ");
+            String eventDesc = eventDescArr[0];
+            String[] eventTimeArr = eventDescArr[1].split(" /to ");
+            String eventFrom = eventTimeArr[0];
+            String eventTo = eventTimeArr[1];
+            tasks.addTask(eventDesc, eventFrom, eventTo, false);
+            tasks.printNewestTask();
+            storage.save(tasks);
+            break;
+        case "mark":
+            selectedNum = Integer.parseInt(inputCmdArr[1]);
+            tasks.markTask(selectedNum);
+            storage.save(tasks);
+            break;
+        case "unmark":
+            selectedNum = Integer.parseInt(inputCmdArr[1]);
+            tasks.unMarkTask(selectedNum);
+            storage.save(tasks);
+            break;
+        case "delete":
+            int numToDelete;
+            try {
+                numToDelete = Integer.parseInt(inputCommand.split(" ", 2)[1]);
+            } catch (Exception e) {
+                throw new DukeException("Please enter a valid number to delete!");
+            }
+            tasks.deleteTask(numToDelete);
+            storage.save(tasks);
+            break;
+        case "Storage":
+            //System.out.println("I RAN HERE!");
+            tasks = storage.load();
+            break;
+        case "Save":
+            storage.save(tasks);
+            break;
+        case "bye":
+            break;
+        default:
+            throw new DukeException("I don't get it!");
         }
     }
 }
