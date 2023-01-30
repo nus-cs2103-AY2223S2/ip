43fa407060b2b85ec517b4a3a132b5bca3083123
EksdeeeX <Bryantanzhongkang@gmail.com>
2023-01-31 01:36:41 +0800

    added date time recognition

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,13 +1,24 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Deadline extends Task{
-    private String deadline;
+    private LocalDateTime deadline;
+    private DateTimeFormatter acceptingFormat = DateTimeFormatter.ofPattern("yyyy/MM/dd HHmm");
+    private DateTimeFormatter displayFormat = DateTimeFormatter.ofPattern("dd/MMM/yyyy HHmm");
 
     public Deadline(String task, String deadline) {
         super(task);
-        this.deadline = deadline;
+        try {
+            LocalDateTime temp = LocalDateTime.parse(deadline, acceptingFormat);
+            this.deadline = LocalDateTime.parse(temp.format(displayFormat), displayFormat);
+        } catch (DateTimeParseException e) {
+            throw new DateTimeParseException(deadline, deadline, 0);
+        }
     }
 
     @Override 
     public String toString() {
-        return "[D] " + super.toString() + " (by: " + this.deadline + ")";
+        return "[D] " + super.toString() + " (by: " + this.deadline.format(displayFormat) + "Hrs" + ")";
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,15 +1,32 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Event extends Task {
-    private String start;
-    private String end;
+    private LocalDateTime start;
+    private LocalDateTime end;
+    private DateTimeFormatter acceptingFormat = DateTimeFormatter.ofPattern("yyyy/MM/dd HHmm");
+    private DateTimeFormatter displayFormat = DateTimeFormatter.ofPattern("dd/MMM/yyyy HHmm");
 
     public Event(String task, String start, String end) {
         super(task);
-        this.start = start;
-        this.end = end;
+        try {
+            LocalDateTime temp = LocalDateTime.parse(start, acceptingFormat);
+            this.start = LocalDateTime.parse(temp.format(displayFormat), displayFormat);
+        } catch (DateTimeParseException e) {
+            throw new DateTimeParseException(end, end, 0);
+            } try {
+                LocalDateTime temp = LocalDateTime.parse(end, acceptingFormat);
+                this.end = LocalDateTime.parse(temp.format(displayFormat), displayFormat);
+        } catch (DateTimeParseException e) {
+            throw new DateTimeParseException(end, end, 0);
+        }
+            
     }
 
     @Override
     public String toString() {
-        return "[E] " + super.toString() + " (from: " + this.start + " to: " + this.end + ")";
+        return "[E] " + super.toString() + " (from: " + this.start.format(displayFormat) + 
+            " to: " + this.end.format(displayFormat) + "Hrs" + ")";
     }
 }
