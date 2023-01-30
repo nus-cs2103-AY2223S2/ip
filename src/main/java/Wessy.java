f2b385fe9d75e6c234710d2cd1d7cdbd56d00a7e
DerenC <chengderen953@gmail.com>
2023-01-30 12:30:42 +0800

    Finish Level-4

--- src/main/java/Wessy.java
+++ src/main/java/Wessy.java
@@ -1,14 +1,16 @@
 import java.util.Scanner;
 
 public class Wessy {
+    static String OPENING_LINE = "    -Wessy---------------------------------------------------------------- ";
+    static String CLOSING_LINE = "    ---------------------------------------------------------------------- ";
     static Task[] tasks = new Task[100];
     static int firstUnusedIdx = 0;
 
     public static void main(String[] args) {
-        System.out.println("    -Wessy------------------------------------------------------ ");
+        System.out.println(OPENING_LINE);
         println("Hi, I am Wessy, your personal assistant chatbot.");
         println("Please type something to interact with me.");
-        System.out.println("    ------------------------------------------------------------ ");
+        System.out.println(CLOSING_LINE);
 
         Scanner sc = new Scanner(System.in);
         while (sc.hasNextLine()) {
@@ -19,41 +21,102 @@ public class Wessy {
                 break;
             } else if (userInput.equalsIgnoreCase("list")) {
                 printList();
-            } else if (len > 3 && userInput.substring(0,4).equalsIgnoreCase("mark")) {
+            } else if (checkCmd(userInput, "mark")) {
                 printMarkOrUnmark(userInput, true);
-            } else if (len > 5 && userInput.substring(0,6).equalsIgnoreCase("unmark")) {
+            } else if (checkCmd(userInput,"unmark")) {
                 printMarkOrUnmark(userInput, false);
             } else {
-                add(userInput);
-                printNormal("added: " + userInput);
+                String[] parsedTexts = new String[] {};
+                if (checkCmd(userInput, "todo")) {
+                    parsedTexts = parse(userInput, "todo ");
+                } else if (checkCmd(userInput, "deadline")) {
+                    parsedTexts = parse(userInput, "deadline ");
+                } else if (checkCmd(userInput, "event")) {
+                    parsedTexts = parse(userInput, "event ");
+                }
+                printAdded(add(parsedTexts));
             }
         }
     }
 
+    static String[] parse(String description, String type) {
+        String byStr = " /by ";
+        String fromStr = " /from ";
+        String toStr = " /to ";
+        int firstIdx;
+        int secondIdx;
+        description = description.substring(type.length());
+        if (type.charAt(0) == 'd') {
+            firstIdx = description.indexOf(byStr);
+            return new String[] {description.substring(0, firstIdx),
+                    description.substring(firstIdx + byStr.length())};
+        } else if (type.charAt(0) == 'e') {
+            firstIdx = description.indexOf(fromStr);
+            secondIdx = description.indexOf(toStr);
+            return new String[] {description.substring(0, firstIdx),
+                    description.substring(firstIdx + fromStr.length(), secondIdx),
+                    description.substring(secondIdx + toStr.length())};
+        } else if (type.charAt(0) == 't') {
+            return new String[] {description};
+        }
+        return new String[] {};
+    }
+
+    static boolean checkCmd(String userInput, String cmd) {
+        int threshold = cmd.length();
+        return userInput.length() >= threshold && userInput.substring(0,threshold).equalsIgnoreCase(cmd);
+    }
+
     static void printNormal(String... linesOfString) {
-        System.out.println("    -Wessy------------------------------------------------------ ");
+        System.out.println(OPENING_LINE);
         for (String line : linesOfString) {
             println(line);
         }
-        System.out.println("    ------------------------------------------------------------ ");
+        System.out.println(CLOSING_LINE);
+    }
+
+    static void printAdded(Task task) {
+        String numOfTasks = " " + firstUnusedIdx + " task";
+        if (firstUnusedIdx > 1) {
+            numOfTasks += "s";
+        }
+        printNormal("Got it. I've added this task:",
+                "  " + task, "Now you have" + numOfTasks + " in the list.");
     }
 
     static void println(String str) {
         int length = str.length();
         String message = "   |   " + str;
-        for (int i = 0; i < 60 - length - 3; i++)
+        for (int i = 0; i < 70 - length - 3; i++)
             message += " ";
         message += "|";
         System.out.println(message);
     }
 
-    static void add(String description) {
-        Task newTask = new Task(description);
-        tasks[firstUnusedIdx++] = newTask;
+    static Task add(String[] strings) {
+        int len = strings.length;
+        if (len == 1) {
+            tasks[firstUnusedIdx++] = new ToDo(strings[0]);
+        } else if (len == 2) {
+            tasks[firstUnusedIdx++] = new Deadline(strings[0], strings[1]);
+        } else if (len == 3) {
+            tasks[firstUnusedIdx++] = new Event(strings[0], strings[1], strings[2]);
+        }
+        return tasks[firstUnusedIdx - 1];
+    }
+
+    static Task addDeadline(String description, String by) {
+        tasks[firstUnusedIdx++] = new Deadline(description, by);
+        return tasks[firstUnusedIdx - 1];
+    }
+
+    static Task addEvent(String description, String from, String to) {
+        tasks[firstUnusedIdx++] = new Event(description, from, to);
+        return tasks[firstUnusedIdx - 1];
     }
 
     static void printList() {
-        System.out.println("    -Wessy------------------------------------------------------ ");
+        System.out.println(OPENING_LINE);
         if (firstUnusedIdx == 0) {
             println("WOOHOO! You do not have any task on the list.");
         } else {
@@ -62,7 +125,7 @@ public class Wessy {
                 println((i + 1) + "." + tasks[i]);
             }
         }
-        System.out.println("    ------------------------------------------------------------ ");
+        System.out.println(CLOSING_LINE);
     }
 
     static void printMarkOrUnmark(String userInput, boolean isMark) {
