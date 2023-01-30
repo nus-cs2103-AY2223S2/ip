0a3dc7823fed88ca4e30a703723377e7580d9b3e
Sean Wong <58366602+ExtraShotLatte@users.noreply.github.com>
2023-01-30 19:48:08 +0800

    Add JavaDocs to Deadline

--- src/main/java/duke/Deadline.java
+++ src/main/java/duke/Deadline.java
@@ -5,10 +5,21 @@ import java.time.format.DateTimeFormatter;
 
 public class Deadline extends Task{
 
+    /** String used to represent the deadline of task. */
     protected String by;
+
+    /** String used to assign the name of the task. */
     protected String desc;
+
+    /** Date object used to represent the deadline of task. */
     LocalDate byDate;
 
+    /**
+     * Constructor for the Deadline class.
+     *
+     * @param desc The name of the task.
+     * @param by The deadline of the task.
+     */
     public Deadline(String desc, String by) {
         super(desc);
         this.desc = desc;
@@ -16,6 +27,13 @@ public class Deadline extends Task{
         byDate = LocalDate.parse(by);
     }
 
+    /**
+     * Overloaded constructor for the Deadline class.
+     *
+     * @param desc The name of the task.
+     * @param by The deadline of the task.
+     * @param b The status of the task.
+     */
     public Deadline(String desc, String by, boolean b) {
         super(desc, b);
         this.desc = desc;
@@ -23,11 +41,17 @@ public class Deadline extends Task{
         byDate = LocalDate.parse(by);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String toString() {
         return "[D]" + super.toString() + " (by: " + byDate.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public String asCSV() {
         if (super.isDone) {
             return "D,1" + desc + "," + by;
