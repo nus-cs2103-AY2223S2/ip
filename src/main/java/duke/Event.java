8116451c00b0eb719493885ffeea17d2315297bb
Sean Wong <58366602+ExtraShotLatte@users.noreply.github.com>
2023-01-30 20:37:50 +0800

    Add JavaDocs for Event

--- src/main/java/duke/Event.java
+++ src/main/java/duke/Event.java
@@ -2,10 +2,20 @@ package duke;
 
 public class Event extends Task{
 
+    /** String used to represent the start time of event. */
     protected String from;
+    /** String used to represent the end time of event. */
     protected String to;
+    /** String used to assign the name of the event. */
     protected String desc;
 
+    /**
+     * Constructor for the Event class.
+     *
+     * @param desc The name of the task.
+     * @param from The starting time of event.
+     * @param to The ending time of event.
+     */
     public Event(String desc, String from, String to) {
         super(desc);
         this.desc = desc;
@@ -13,6 +23,14 @@ public class Event extends Task{
         this.to = to;
     }
 
+    /**
+     * Overloaded constructor for the Event class.
+     *
+     * @param desc The name of the task.
+     * @param from The starting time of event.
+     * @param to The ending time of event.
+     * @param b The status of the task.
+     */
     public Event(String desc, String from, String to, boolean b) {
         super(desc, b);
         this.desc = desc;
@@ -20,11 +38,17 @@ public class Event extends Task{
         this.to = to;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String toString() {
         return "[E]" + super.toString() + " (from: " + from +" to: " + to +")";
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public String asCSV() {
         if (super.isDone) {
             return "E,1," + desc + "," + from + "," + to;
