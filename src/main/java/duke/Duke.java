3492db4f53a373569a7f35abfe67b23c9af3c5a5
JrmCkh <Hhelloogkart@gmail.com>
2023-01-30 20:51:42 +0800

    Add a GUI to Duke using JavaFX

---
+++ src/main/java/duke/Duke.java
@@ -0,0 +1,58 @@
+package duke;
+
+import duke.commands.Command;
+import duke.parser.Parser;
+import duke.storage.Storage;
+import duke.task.TaskList;
+import duke.ui.Ui;
+
+/**
+ * The Duke class implements a personal assistant chatbot that helps the user to keep track of various tasks.
+ *
+ * @author Chia Jeremy
+ */
+public class Duke {
+
+    public static final String LOGO = " ____        _\n"
+            + "|  _ \\ _   _| | _____\n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n";
+
+    private final Storage storage;
+    private final TaskList tasks;
+    private final Ui ui;
+
+    /**
+     * Initializes a new Duke instance and populate it with values from the storage file if it exists.
+     * Otherwise, create a new empty Duke instance.
+     *
+     * @param filePath the path to the file
+     */
+    public Duke(String filePath) {
+        this.storage = new Storage(filePath);
+        this.tasks = new TaskList(storage.load());
+        this.ui = new Ui();
+    }
+
+    /**
+     * Returns the greeting message when Duke starts.
+     *
+     * @return the greeting message
+     */
+    public String getGreeting() {
+        return this.ui.greet();
+    }
+
+    /**
+     * Returns different Duke's responses depending on user input.
+     *
+     * @param input the user input
+     * @return the Duke's response
+     */
+    public String getResponse(String input) {
+        Command command = new Parser().parseCommand(input);
+        command.execute(this.storage, this.tasks, this.ui);
+        return ui.getResponse();
+    }
+}
---
+++ src/main/java/gui/Main.java
@@ -0,0 +1,55 @@
+package gui;
+
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import duke.Duke;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+/**
+ * A GUI for Duke using FXML.
+ */
+public class Main extends Application {
+
+    private final Duke duke = new Duke("data/tasks.txt");
+
+    /**
+     * Exits the application after three second.
+     */
+    public static void exit() {
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Platform.exit();
+                System.exit(0);
+            }
+        }, 3000);
+    }
+
+    /**
+     * Executes when the application starts.
+     *
+     * @param stage the primary stage for this application, onto which the application scene can be set.
+     *        Applications may create other stages, if needed, but they will not be primary stages.
+     */
+    @Override
+    public void start(Stage stage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
+            AnchorPane ap = fxmlLoader.load();
+            Scene scene = new Scene(ap);
+            stage.setScene(scene);
+            fxmlLoader.<MainWindow>getController().setDuke(this.duke);
+            fxmlLoader.<MainWindow>getController().run();
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
