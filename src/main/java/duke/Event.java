a9e4cf02445248374343ea198e163e6f1bac631f
tricixg <64681919+tricixg@users.noreply.github.com>
2023-01-31 00:08:41 +0800

    Level-7
    Implement save function
    able to load task from previous sessions

--- src/main/java/duke/Event.java
+++ src/main/java/duke/Event.java
@@ -1,25 +1,38 @@
 package duke;
 
+
 import java.util.ArrayList;
 
 import util.*;
 
 public class Event extends Task {
 
-    protected String duration;
+    protected String from;
+    protected String to;
     static String divider = "    ═══*.·:·.☽✧    ✦    ✧☾.·:·.*═══";
 
-    public Event(String description, String duration) {
+    public Event(String description, String from, String to) {
         super(description);
-        this.duration = duration;
+        this.from = from;
+        this.to = to;
+        this.taskType = "E";
+    }
+
+    public Event(Boolean isDone, String description, String from, String to) {
+        super(isDone, description);
+        this.from = from;
+        this.to = to;
+        this.taskType = "E";
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + duration +")";
+        return "[E]" + super.toString() + " (from:" + from + " to:" + to + ")";
     }
 
+
     public static void createEventTask(ArrayList<Task> array, String[] splitInput) {
+        String combinedString = String.join(" ", splitInput);
         if (splitInput.length == 1 || splitInput[1].equals("")){
             try {
                 throw new DukeException("event");
@@ -29,33 +42,85 @@ public class Event extends Task {
                 System.out.println(divider);
             }
         } else {
-            for(int j=1; j< splitInput.length; j++){
-                if(splitInput[j].equals("/from")){
-                    for (int k=1; k< j-1; k++){
-                        splitInput[1] = splitInput[1] + " " + splitInput[k+1];
-                    }
-                    for(int i = j + 1; i < splitInput.length; i++) {
-                        splitInput[i] = splitInput[i].replace("/to", "to:");
-                    }
-                    for (int l=splitInput.length-1; l > j +1; l--){
-                        splitInput[splitInput.length-1] = splitInput[l-1]+" "+splitInput[splitInput.length-1];
-                    }
-                } else {
-                    splitInput[j] = splitInput[j];
-                }
-            }
+            // for(int j=1; j< splitInput.length; j++){
+            //     if(splitInput[j].equals("/from")){
+            //         for (int k=1; k< j-1; k++){
+            //             splitInput[1] = splitInput[1] + " " + splitInput[k+1];
+            //         }
+            //         for(int i = j + 1; i < splitInput.length; i++) {
+            //             splitInput[i] = splitInput[i].replace("/to", "to:");
+            //         }
+            //         for (int l=splitInput.length-1; l > j +1; l--){
+            //             splitInput[splitInput.length-1] = splitInput[l-1]+" "+splitInput[splitInput.length-1];
+            //         }
+            //     } else {
+            //         splitInput[j] = splitInput[j];
+            //     }
+            // }
                
-            String duration = splitInput[splitInput.length-1];
-            String desc = splitInput[1];
-            Event e = new Event(desc, duration);
-            array.add(e);
-            System.out.println(divider);
-            System.out.println("     Got it. I've added this task:");
-            System.out.println("     " + e.toString());
-            System.out.println("     Now you have " + array.size() + " tasks in the list.");
-            System.out.println(divider);
+            // String duration = splitInput[splitInput.length-1];
+            // String desc = splitInput[1];
+           
+           
+            String event = combinedString.split(" ", 2)[1];
+            String[] event_Arr = event.split(" /from", 2);
+
+                if (event_Arr.length == 2) {
+                    String desc = event_Arr[0];
+                    String[] period_Arr = event_Arr[1].split(" /to");
+                    if (period_Arr.length == 2) {
+                        String from = period_Arr[0];
+                        String to = period_Arr[1];
+                        Event e = new Event(desc, from, to);
+                        array.add(e);
+                        System.out.println(divider);
+                        System.out.println("     Got it. I've added this task:");
+                        System.out.println("     " + e.toString());
+                        System.out.println("     Now you have " + array.size() + " tasks in the list.");
+                        System.out.println(divider);
+                    }
+                }
         }
         
     }
 
-}
+    public static String join(Object[] array, String separator, int startIndex, int endIndex) {
+        if (array == null) {
+            return null;
+        }
+        if (separator == null) {
+            separator = "";
+        }
+  
+        // endIndex - startIndex > 0:   Len = NofStrings *(len(firstString) + len(separator))
+        //           (Assuming that all Strings are roughly equally long)
+        int bufSize = (endIndex - startIndex);
+        if (bufSize <= 0) {
+            return "";
+        }
+  
+        bufSize *= ((array[startIndex] == null ? 16 : array[startIndex].toString().length())
+                        + separator.length());
+  
+        StringBuffer buf = new StringBuffer(bufSize);
+  
+        for (int i = startIndex; i < endIndex; i++) {
+            if (i > startIndex) {
+                buf.append(separator);
+            }
+            if (array[i] != null) {
+                buf.append(array[i]);
+            }
+        }
+        return buf.toString();
+    }
+
+    @Override
+    public String saveFormat() {
+        String d = " | ";
+        int marked = this.getIsDone() ? 1 : 0;
+        String timePeriod = from + d + to;
+        return "E" + d + marked + d + description + d + timePeriod;
+    }
+  
+  }
---
+++ src/main/java/duke/SaveData.java
@@ -0,0 +1,88 @@
+package duke;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import util.DukeException;
+
+public class SaveData {
+    // create a file object for the current location
+    public static void initSaveFile() {
+        File dir = new File("src/main/data/");
+        dir.mkdirs();
+        // create a file object for the current location
+        File file = new File(dir, "duke.txt");
+
+        try {
+            // create a new file with name specified
+            // by the file object
+            boolean value = file.createNewFile();
+            if (value) {
+                FileWriter output = new FileWriter("duke.txt");
+                output.write("---------------LIST OF TASKS--------------------");
+                output.append('\n');
+                System.out.println("New Save File is created.");
+            }
+            else {
+                System.out.println("Save file already exists.");
+            }
+        }
+        catch(Exception e) {
+            e.getStackTrace();
+        }
+    }
+
+    public static void loadFile(ArrayList<Task> list) throws IOException, DukeException {
+        File file = new File(System.getProperty("user.dir") + "/data/duke.txt");
+        File dir = new File(System.getProperty("user.dir") + "/data");
+
+        // if directory has not been created, make directory
+        if (! dir.exists() ) {
+            dir.mkdir();
+        }
+
+        file.createNewFile();
+
+        Scanner sc = new Scanner(file);  //takes file's content as input, scans through files and adds items to the list
+
+        while (sc.hasNext()) {
+            String[] currLine = sc.nextLine().split(" \\| ");
+            if (currLine[0].equalsIgnoreCase("T")) {
+                list.add(new Todo(strToBool(currLine[1]), currLine[2]));
+            } else if (currLine[0].equalsIgnoreCase("D")) {
+                list.add(new Deadline(strToBool(currLine[1]), currLine[2], currLine[3]));
+            } else if (currLine[0].equalsIgnoreCase("E")) {
+                list.add(new Event(strToBool(currLine[1]), currLine[2], currLine[3], currLine[4]));
+            } else {
+                throw new DukeException("Read Error");
+            }
+        }
+    }
+
+    public static boolean strToBool(String str) {
+        if (str.equalsIgnoreCase("1")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void saveToFile(ArrayList<Task> list) throws IOException {
+        FileWriter fw = new FileWriter(System.getProperty("user.dir") + "/data/duke.txt");
+        for (Task t: list) {
+            fw.write(t.saveFormat() +  "\n");
+        }
+        fw.close();
+    }
+
+
+    public static void clearFile(String filename) throws IOException {
+        FileOutputStream writer = new FileOutputStream(filename);
+        writer.write(("").getBytes());
+        writer.close();
+    }
+}
