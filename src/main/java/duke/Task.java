cb8a2814fc3bdf00fa93a22c1aab5071467b09b3
Sean Wong <58366602+ExtraShotLatte@users.noreply.github.com>
2023-01-30 19:38:46 +0800

    Add Javadocs for Task class

--- src/main/java/duke/Task.java
+++ src/main/java/duke/Task.java
@@ -1,26 +1,52 @@
 package duke;
 
 public abstract class Task {
+
+    /** Boolean used to determine if the task is completed or not. */
     protected boolean isDone = false;
+    /** String used to assign the name of the task. */
     private String name;
 
+    /**
+     * Constructor for the Task class.
+     *
+     * @param name The name of the Task.
+     */
     public Task(String name) {
         this.name = name;
     }
 
+    /**
+     * Constructor for the Task class. Overloaded to allow setting of
+     * the completion directly.
+     *
+     * @param name The name of the Task.
+     * @param b The boolean value to be assigned
+     */
     public Task(String name, boolean b) {
         this.name = name;
         this.isDone = b;
     }
 
+    /**
+     * Method to mark task as done.
+     */
     public void markDone() {
         isDone = true;
     }
 
+    /**
+     * Method to unmark task as done.
+     */
     public void markUndone() {
         isDone = false;
     }
 
+    /**
+     * To String method to return the String representation of the task.
+     *
+     * @return Task in String representation.
+     */
     public String toString() {
         if (isDone) {
             return "[X] " + name;
@@ -29,5 +55,10 @@ public abstract class Task {
         }
     }
 
+    /**
+     * Method to return the CSV representation of the task.
+     *
+     * @return Task in CSV representation.
+     */
     public abstract String asCSV();
 }
