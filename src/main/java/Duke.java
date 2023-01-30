2abf484d66868a310b8801e51252015e725ca9d8
QIU QIANHUI <672271700@qq.com>
2023-01-30 20:01:01 +0800

    Add Increment Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = "\t  ____        _        \n"
+                + "\t |  _ \\ _   _| | _____ \n"
+                + "\t | | | | | | | |/ / _ \\\n"
+                + "\t | |_| | |_| |   <  __/\n"
+                + "\t |____/ \\__,_|_|\\_\\___|\n";
+        System.out.println(logo);
+        System.out.println("\t Hello! I'm Duke");
+        System.out.println("\t What can I do for you?");
+
+        Scanner input = new Scanner(System.in);
+
+        while (true) {
+            String command = input.nextLine();
+
+            if (command.equals("bye")) {
+                System.out.println("\t Bye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println("\t " + command);
+            }
+        }
     }
 }
