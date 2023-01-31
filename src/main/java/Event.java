94dafc6248bf49598cb8d2b43e850bf18a836b9c
abenx162 <benxuan162@gmail.com>
2023-01-31 14:55:43 +0800

    31/01/23 branch-Level-8

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,15 +1,21 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Quest {
-    private String from;
-    private String to;
+    private LocalDateTime from;
+    private LocalDateTime to;
+    private static DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("HHmm dd/MM/yy");
+    private static DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("dd MMM yyyy hh:mma");
 
     public Event(String description, String from, String to) {
         super(description);
-        this.from = from;
-        this.to = to;
+        this.from = LocalDateTime.parse(from, inputFormatter);
+        this.to = LocalDateTime.parse(to, inputFormatter);
     }
 
     @Override
     public String toString() {
-        return "[E] " + super.toString() + " from: " + from + " to: " + to;
+        return "[E] " + super.toString() + " from: " +
+                from.format(outputFormatter) + " to: " + to.format(outputFormatter);
     }
 }
