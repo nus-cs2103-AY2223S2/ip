1016283aa559ea99ebc27739d731c81208454325
GlennOngJunJie <glennongjj@gmail.com>
2023-01-31 15:56:38 +0800

    A-JUnit Done

---
+++ src/test/java/TasklistTest.java
@@ -0,0 +1,26 @@
+import duke.*;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TasklistTest {
+    @Test
+    public void taskListTest1(){
+        Tasklist tasklist = new Tasklist();
+        tasklist.addingActivities("todo", "todo read");
+        ArrayList<Task> result = new ArrayList<Task>();
+        result.add(new Todo("read", "todo read"));
+        assertEquals(result.get(0).toString(), tasklist.getList().get(0).toString());
+    }
+
+    @Test
+    public void taskListTest2(){
+        Tasklist tasklist = new Tasklist();
+        tasklist.addingActivities("event", "event project meeting /from Mon 2pm /to 4pm");
+        ArrayList<Task> result = new ArrayList<Task>();
+        result.add(new Event("project meeting ", "event project meeting /from Mon 2pm /to 4pm", "Mon 2pm ", "4pm"));
+        assertEquals(result.get(0).toString(), tasklist.getList().get(0).toString());
+    }
+}
