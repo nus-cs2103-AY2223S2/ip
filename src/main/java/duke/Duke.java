e181f70add7f4aacfa558bd9e990dfd047754c1d
ruiyigan <85063215+ruiyigan@users.noreply.github.com>
2023-01-31 10:12:33 +0800

    Create Launcher class

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,4 +1,5 @@
 package duke;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -8,19 +9,25 @@ import duke.parser.Parser;
 import duke.storage.Storage;
 import duke.tasklist.TaskList;
 import duke.ui.Ui;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 /**
  * Duke is a class that reacts to user's input
  * It uses other classes like Command, Parser, Storage, and TaskList to perform
  * various operations depending on the user's input
  */
-public class Duke {
+public class Duke extends Application {
     private Storage storage;
     private TaskList tasks;
     private Ui ui;
     private Parser parser;
     private Command command;
 
+    public Duke() {
+    }
     /**
      * Constructor. Takes in a filePath to create Storage object
      * @param filePath
@@ -56,5 +63,14 @@ public class Duke {
 
     public static void main(String[] args) throws DukeException {
         new Duke("data/file.txt").run();
+    }
+
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
     }
 }
