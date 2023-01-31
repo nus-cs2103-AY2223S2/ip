08e92dde635bddee26abbca273d7cd19ba59247f
owenyap <owen.y.yuhan@gmail.com>
2023-02-01 01:38:50 +0800

    Add Javadoc comments for Ui class

--- src/main/java/duke/ui/Ui.java
+++ src/main/java/duke/ui/Ui.java
@@ -2,12 +2,25 @@ package duke.ui;
 
 import java.util.Scanner;
 
+/**
+ * The Ui class provides the user interface for the system.
+ * It implements methods for printing messages and lists, as well as reading user input.
+ *
+ * @author owen-yap
+ */
 public class Ui {
     private final Scanner sc;
-
+    /**
+     * Constructs a new UI by initializing a Scanner object for reading user input.
+     */
     public Ui() {
         sc = new Scanner(System.in);
     }
+    /**
+     * Prints an array of messages, surrounded by separators.
+     *
+     * @param msgs The message(s) to be printed
+     */
     public void printMsg(String[] msgs) {
         System.out.println("____________________________________________________________");
         for (String msg : msgs) {
@@ -15,13 +28,21 @@ public class Ui {
         }
         System.out.println("____________________________________________________________");
     }
-
+    /**
+     * Prints a message, surrounded by separators.
+     *
+     * @param msg The message to be printed
+     */
     public void printMsg(String msg) {
         System.out.println("____________________________________________________________");
         System.out.println(msg);
         System.out.println("____________________________________________________________");
     }
-
+    /**
+     * Prints an array of items as a numbered list, surrounded by separators.
+     *
+     * @param list The array of items to be printed
+     */
     public void printList(String[] list) {
         System.out.println("____________________________________________________________");
         for (int i = 0; i < list.length; i++) {
@@ -29,14 +50,19 @@ public class Ui {
         }
         System.out.println("____________________________________________________________");
     }
-
+    /**
+     * Prints a welcome message to the user.
+     */
     public void printWelcomeMsg() {
         String[] welcomeMsg = {"Hello I am Duke", "What can I do for you?"};
         printMsg(welcomeMsg);
     }
-
+    /**
+     * Reads a line of input from the user.
+     *
+     * @return The input entered by the user
+     */
     public String readInput() {
         return this.sc.nextLine();
     }
-
 }
