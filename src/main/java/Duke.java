1d8c8d5815e7f0671e7686ffc9303c4febaf3330
benjamin-wee <benjaminwee20@gmail.com>
2023-02-01 03:02:13 +0800

    no message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,7 +8,6 @@ public class Duke {
         System.out.println("Hello! I'm Somebody\n" + "What can I do for you?");
         Scanner scn = new Scanner(System.in);
         while (true) {
-
             String input = scn.nextLine();
 
             if (input.startsWith("mark")) {
