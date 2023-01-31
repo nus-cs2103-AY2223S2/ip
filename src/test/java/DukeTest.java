37e284662cca195a987b7defa0340a7a36e54103
cshuwen <chenshuwen0210@gmail.com>
2023-01-31 15:37:08 +0800

    JUnit Test implemented

---
+++ src/test/java/DukeTest.java
@@ -0,0 +1,13 @@
+//package seedu;
+
+import duke.ToDos;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DukeTest {
+    @Test
+    public void todoTest(){
+        ToDos todo = new ToDos("homework", 0);
+        assertEquals("[T][0]homework", todo.toString());
+    }
+}
