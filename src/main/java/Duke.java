23cde3625d8ed9aaba2c5bf1f7d688ef907f9b2a
Xiuxuan Wang <36021591+wxxedu@users.noreply.github.com>
2023-01-31 16:32:52 +0800

    tweak jfx so that its working now

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,6 +10,8 @@ import domain.usecases.ByeUsecase;
 import domain.usecases.EchoUsecase;
 import domain.usecases.TaskManagerUsecase;
 import domain.usecases.UnknownCommandUsecase;
+import javafx.application.Application;
+import javafx.stage.Stage;
 import presentation.controllers.DukeEventLoop;
 import presentation.ui.DummyWritable;
 import presentation.ui.SystemErr;
@@ -20,13 +22,27 @@ import java.time.format.DateTimeFormatter;
 /**
  * The main class for Duke.
  */
-public class Duke {
+public class Duke extends Application {
     /**
      * The main method for Duke.
      *
      * @param args the command line arguments, if any.
      */
     public static void main(String[] args) {
+        initOrCrash();
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
+        final DukeEventLoop initializingLoop = DukeEventLoop.createInitializingLoop();
+        initializingLoop.run();
+        final DukeEventLoop eventLoop = DukeEventLoop.createEventLoop();
+        eventLoop.run();
+    }
+
+    private static void initOrCrash() {
         try {
             registerSingletons();
         } catch (WriteException e) {
@@ -38,16 +54,6 @@ public class Duke {
             System.err.println(e.getMessage());
             System.exit(1);
         }
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        final DukeEventLoop initializingLoop = DukeEventLoop.createInitializingLoop();
-        initializingLoop.run();
-        final DukeEventLoop eventLoop = DukeEventLoop.createEventLoop();
-        eventLoop.run();
     }
 
     /**
@@ -92,5 +98,20 @@ public class Duke {
                 TokenUtilities.class,
                 TokenUtilities::new
         );
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        initOrCrash();
+//        try {
+//            FXMLLoader fxmlLoader = new FXMLLoader(Duke.class.getResource(
+//                    "/presentation/gui/MainWindow.fxml"));
+//            AnchorPane ap = fxmlLoader.load();
+//            Scene scene = new Scene(ap);
+//            primaryStage.setScene(scene);
+//            primaryStage.show();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
     }
 }
