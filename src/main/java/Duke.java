d2246c457ac4f4b78d330e1ddbdc086bd4eb673d
Gibson <gibsonyip@u.nus.edu>
2023-01-31 17:38:49 +0800

    Add Level 1. Greet, Echo, Exit feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,25 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            String userInput = sc.next();
+            if (userInput.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+            else {
+                echoInputs(userInput);
+            }
+        }
     }
+
+
+
+    public static void echoInputs(String userInput) {
+        System.out.println(userInput);
+    }
+
 }
