3081cd42c6732e3dd866f4e3d3603026e0d9d1dc
Nigel Teo <nigelteosw@gmail.com>
2023-01-31 14:52:51 +0800

    Update Storage.java

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -1,7 +1,5 @@
 import java.io.File;
 import java.io.FileWriter;
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
