28b99306a82a9b4227a68c96deac0de960b0df91
Gibson <gibsonyip@u.nus.edu>
2023-01-31 18:05:59 +0800

    Level 2 Completed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 
 public class Duke {
+    private static String[] txtList = new String[100];
+    private static int itemCount = 0;
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -10,23 +13,27 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
 
         Scanner sc = new Scanner(System.in);
-
         while (true) {
-            String userInput = sc.next();
+            String userInput = sc.nextLine();
             if (userInput.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (userInput.equals("list")) {
+                System.out.println("Duke's list of commands");
+                for (int i = 0; i < itemCount; i++) {
+                    System.out.printf("%d %s \n", i + 1, txtList[i]);
+                }
             }
             else {
-                echoInputs(userInput);
+                addInputToList(userInput);
             }
         }
     }
 
 
-
-    public static void echoInputs(String userInput) {
-        System.out.println(userInput);
+    public static void addInputToList(String userInput) {
+        System.out.printf("added: %s\n", userInput);
+        txtList[itemCount++] = userInput;
     }
 
 }
