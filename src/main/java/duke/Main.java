2d1a6f4b8c4dcd8bb4d4009ffc634374365dd0f4
dawg420 <88711624+dawg420@users.noreply.github.com>
2023-01-31 16:35:47 +0800

    A-CodingStandard
    Tweaked the code to comply with given coding standard.

--- src/main/java/duke/Main.java
+++ src/main/java/duke/Main.java
@@ -8,36 +8,35 @@ import static duke.Storage.SAVE_LOCATION;
 
 /**
  * The Duke program implements a 'to-do' list
- * where users can add, mark and delete tasks 
+ * where users can add, mark and delete tasks
  */
 
 public class Main {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        Ui.showWelcomeMessage();
+		Ui.showWelcomeMessage();
 
-        Tasklist tasklist = new Tasklist();
-        try {
-            File saveFile = new File(SAVE_LOCATION);
-            if (saveFile.createNewFile()) {
-                System.out.println("Save file created: " + saveFile.getName());
-            } else {
-                tasklist = Storage.load();
-            }
-        } catch (IOException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-        }
-        
+		Tasklist tasklist = new Tasklist();
+		try {
+			File saveFile = new File(SAVE_LOCATION);
+			if (saveFile.createNewFile()) {
+				System.out.println("Save file created: " + saveFile.getName());
+			} else {
+				tasklist = Storage.load();
+			}
+		} catch (IOException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}
 
 
-        boolean loop = true;
-        while (loop) {
-            Scanner echoScanner = new Scanner(System.in);
-            String msg = echoScanner.nextLine();
+		boolean loop = true;
+		while (loop) {
+			Scanner echoScanner = new Scanner(System.in);
+			String msg = echoScanner.nextLine();
 
-            loop = Parser.receiveCommand(tasklist, msg, echoScanner, loop);
-        }
-    }
+			loop = Parser.isReceivedCommand(tasklist, msg, echoScanner, loop);
+		}
+	}
 }
--- src/main/java/duke/Ui.java
+++ src/main/java/duke/Ui.java
@@ -2,24 +2,24 @@ package duke;
 
 
 public class Ui {
-    private static String logo = " ____        _        \n"
-    + "|  _ \\ _   _| | _____ \n"
-    + "| | | | | | | |/ / _ \\\n"
-    + "| |_| | |_| |   <  __/\n"
-    + "|____/ \\__,_|_|\\_\\___|\n";
-    
-    
-    public static void showWelcomeMessage() {
-        String welcomeMsg = "Hello from\n" + logo +
-                "\n" + "Hello! I'm Duke\nWhat can I do for you?";
-        System.out.println(welcomeMsg);
-    }
+	private static String logo = " ____        _        \n"
+			+ "|  _ \\ _   _| | _____ \n"
+			+ "| | | | | | | |/ / _ \\\n"
+			+ "| |_| | |_| |   <  __/\n"
+			+ "|____/ \\__,_|_|\\_\\___|\n";
 
-    public static void showGoodbyeMessage() {
-        System.out.println("Bye. Hope to see you again soon!");
-    }
 
-    public static void showToUser(String message) {
-        System.out.println(message);
-    }
+	public static void showWelcomeMessage() {
+		String welcomeMsg = "Hello from\n" + logo +
+				"\n" + "Hello! I'm Duke\nWhat can I do for you?";
+		System.out.println(welcomeMsg);
+	}
+
+	public static void showGoodbyeMessage() {
+		System.out.println("Bye. Hope to see you again soon!");
+	}
+
+	public static void showToUser(String message) {
+		System.out.println(message);
+	}
 }
