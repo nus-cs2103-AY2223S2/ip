5ff5edd004e60eaa05c8b0e971e8f3ccab6e2b1c
JavonTeo <javonteo@hotmail.com>
2023-02-01 23:00:35 +0800

    Add JUnit tests

--- src/main/java/duke/parser/Parser.java
+++ src/main/java/duke/parser/Parser.java
@@ -11,45 +11,45 @@ import duke.exception.DukeException;
 
 public class Parser {
     public static Command parse(String fullCommand) throws DukeException {
-        String command;
-        String restOfString = "";
+        String commandKeyword;
+        String restOfCommand = "";
         if (fullCommand.contains(" ")) {
-            command = fullCommand.substring(0, fullCommand.indexOf(" "));
-            restOfString = fullCommand.substring(fullCommand.indexOf(" ") + 1);
+            commandKeyword = fullCommand.substring(0, fullCommand.indexOf(" "));
+            restOfCommand = fullCommand.substring(fullCommand.indexOf(" ") + 1);
         } else {
-            command = fullCommand;
+            commandKeyword = fullCommand;
         }
-        Command c;
-        switch (command) {
+        Command command;
+        switch (commandKeyword) {
             case "bye": {
-                c = new ByeCommand();
+                command = new ByeCommand();
                 break;
             }
             case "list": {
-                c = new ListCommand();
+                command = new ListCommand();
                 break;
             }
             case "mark": {
-                c = new MarkCommand(restOfString);
+                command = new MarkCommand(restOfCommand);
                 break;
             }
             case "unmark": {
-                c = new UnmarkCommand(restOfString);
+                command = new UnmarkCommand(restOfCommand);
                 break;
             }
             case "todo":
             case "deadline":
             case "event": {
-                c = new AddCommand(command, restOfString);
+                command = new AddCommand(commandKeyword, restOfCommand);
                 break;
             }
             case "delete": {
-                c = new DeleteCommand(restOfString);
+                command = new DeleteCommand(restOfCommand);
                 break;
             }
             default:
                 throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
         }
-        return c;
+        return command;
     }
 }
