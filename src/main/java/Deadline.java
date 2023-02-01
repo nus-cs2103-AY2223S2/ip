20b0c348cb3c9c11b340245fcd09298df99ecd58
Cyrus-Krispin <cyruskrispin@gmail.com>
2023-02-02 05:47:26 +0800

    Add date and time

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,11 +1,40 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
 public class Deadline extends Task {
     protected String deadline;
+
+    protected LocalDate date;
+    protected LocalTime time = null;
 
     public Deadline(String description, String deadline) {
         super(description);
         this.deadline = deadline;
     }
 
+    public void handleDateAndTime() {
+        if (deadline.contains(" ")) {
+            LocalDate newDate = LocalDate.parse(deadline.split(" ")[0]);
+            LocalTime newTime = LocalTime.parse(deadline.split(" ")[1]);
+            this.date = newDate;
+            this.time = newTime;
+        }
+        else {
+            LocalDate newDate = LocalDate.parse(deadline);
+            this.date = newDate;
+        }
+    }
+
+    public String printDateAndTime() {
+        if (time.equals(null)) {
+            return date.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+        }
+        else {
+            return date.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + Integer.toString(time.getHour()) + Integer.toString(time.getMinute());
+        }
+    }
     @Override
     public String toString() {
         return "[D]" + super.toString() + " (by: " + deadline + ") ";
