4109538731fb084e5b25ee286df5c0040300d7f3
Ritika Joshi <ritikajoshi0114@gmail.com>
2023-02-02 01:03:18 +0800

    Add preliminary JavaFX

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,16 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 import ui.WelcomeUI;
 
-import java.util.Scanner;
-public class Duke {
+public class Duke extends Application {
+
     /**
      * Main driver method which initialises the chatbot with
      * a Scanner, Welcome UI and runs it.
@@ -15,5 +24,27 @@ public class Duke {
 
         chat.run();
         input.close();
+    }
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Duke.class.getResource("/view/MainWindow.fxml"));
+            AnchorPane ap = fxmlLoader.load();
+            Scene scene = new Scene(ap);
+            stage.setScene(scene);
+            fxmlLoader.<MainWindow>getController().setDuke(this);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * You should have your own function to generate a response to user input.
+     * Replace this stub with your completed method.
+     */
+    public String getResponse(String input) {
+        return "Duke heard: " + input;
     }
 }
