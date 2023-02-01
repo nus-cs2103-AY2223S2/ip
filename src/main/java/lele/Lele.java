1ede8922c8fdac9f1140f18feb57a4436477a50d
douglch <douglchwm@gmail.com>
2023-02-01 16:15:23 +0800

    Tutorial Level 1

--- src/main/java/lele/Lele.java
+++ src/main/java/lele/Lele.java
@@ -7,14 +7,17 @@ import lele.parser.Parser;
 import lele.storage.Storage;
 import lele.task.TaskList;
 import lele.ui.Ui;
-
 import java.io.IOException;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 /**
  * The main class that represents the chatbot, Lele, which helps
  * to build a checklist consisting of todos, deadlines, events.
  */
-public class Lele {
+public class Lele extends Application {
     private final Storage storage;
     private TaskList taskList;
     private final Ui ui;
@@ -68,5 +71,13 @@ public class Lele {
         new Lele("./data/lele.txt").run();
     }
 
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
+    }
 
 }
