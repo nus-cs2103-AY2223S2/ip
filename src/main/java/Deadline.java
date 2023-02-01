9bd7970a9002c6132229c952e8e845d6e0f09387
lxz333 <e0909010@u.nus.edu>
2023-02-01 18:25:52 +0800

    Complete level 8

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,17 +1,20 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
-    protected String ddl;
+    protected LocalDate ddl;
     protected String name;
 
     public Deadline(String name) {
         super(name);
         this.name = name.substring("deadline".length() + 1, name.indexOf("/by") - 1);
-        this.ddl = name.substring(name.indexOf("/by") + 4);
+        this.ddl = LocalDate.parse(name.substring(name.indexOf("/by") + 4));
     }
 
     @Override
     public String toString() {
         return "[D]" + super.toString().substring(0, "[ ] ".length()) +
-                this.name + " (by: " + this.ddl + ")";
+                this.name + " (by: " + ddl.format(DateTimeFormatter.ofPattern("MMM dd yyyy")) + ")";
     }
 
 
