35cdded74bb26ce2f5a4a0461837006b51de40c2
hingen <1627313-hingen@users.noreply.gitlab.com>
2023-02-01 22:01:38 +0800

    DialogBox class: remove unuse import

--- src/main/java/duke/ui/gui/DialogBox.java
+++ src/main/java/duke/ui/gui/DialogBox.java
@@ -11,7 +11,6 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.shape.Circle;
 import javafx.scene.text.Text;
-import javafx.scene.text.TextFlow;
 
 import java.io.IOException;
 import java.util.Collections;
