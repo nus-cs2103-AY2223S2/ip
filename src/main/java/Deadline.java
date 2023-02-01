73ac6b26b7e9d54dcc792b23bba0e9e7e7554f24
Saraan Kannusami <saraankannusami@gmail.com>
2023-02-01 21:06:12 +0800

    Modified event and deadline classes
    Change the data type of deadline and event attributes.
    
    DateTime datatype is more flexible and convenient in implementing date and time related functions in the future

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,14 +1,21 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
 
-    protected String by;
+    private final LocalDateTime by;
 
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        String[] dateAndTime = by.split(" ");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
+        if (dateAndTime.length == 1) by += " 2359";
+        this.by = LocalDateTime.parse(by, formatter);
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd yyyy HH:mm");
+        return "[D]" + super.toString() + " (by: " + formatter.format(this.by) + ")";
     }
 }
