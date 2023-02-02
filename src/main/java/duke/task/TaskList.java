8210e67d4969b004c7706566423025fcddfd4458
winsonheng <winsonh5147@gmail.com>
2023-02-02 23:22:13 +0800

    Completed A-CodingStandard

--- src/main/java/duke/task/TaskList.java
+++ src/main/java/duke/task/TaskList.java
@@ -21,27 +21,27 @@ public class TaskList extends ArrayList<Task> {
     }
 
     public Task deleteTask(int index) throws DukeException {
-        checkSize(index);
+        checkValidIndex(index);
         Task task = super.get(index - 1);
         super.remove(index - 1);
         return task;
     }
 
     public Task markTask(int index) throws DukeException {
-        checkSize(index);
+        checkValidIndex(index);
         Task task = super.get(index - 1);
         task.markAsDone();
         return task;
     }
 
     public Task unmarkTask(int index) throws DukeException {
-        checkSize(index);
+        checkValidIndex(index);
         Task task = super.get(index - 1);
         task.markAsUndone();
         return task;
     }
 
-    private boolean checkSize(int index) throws DukeException {
+    private boolean checkValidIndex(int index) throws DukeException {
         if (index == 0 || index > super.size()) {
             throw new DukeException(String.format(ERROR.INVALID_INDEX.getMessage(), super.size()));
         }
