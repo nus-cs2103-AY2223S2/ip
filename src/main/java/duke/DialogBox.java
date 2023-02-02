df643ffad81c2040e95a298b15f14214a3ab12c8
filbert <fillberth@gmail.com>
2023-02-03 01:56:47 +0800

    resize dialogbox labels

--- src/main/java/duke/DialogBox.java
+++ src/main/java/duke/DialogBox.java
@@ -5,18 +5,25 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.text.Font;
 
 public class DialogBox extends HBox {
 
-    private Label text;
-    private ImageView displayPicture;
-    //
     // user images
     private static final Image userImage = new Image(DialogBox.class.getResourceAsStream("/images/red.png"));
     private static final Image dukeImage = new Image(DialogBox.class.getResourceAsStream("/images/cyan.png"));
 
+    // javafx component default settings
+    private static final int FONT_SIZE = 16;
+    private static final Font DEFAULT_FONT = new Font(FONT_SIZE);
+
+    // javafx components
+    private Label text;
+    private ImageView displayPicture;
+
     private DialogBox(Label l, ImageView iv) {
         text = l;
+        text.setFont(DialogBox.DEFAULT_FONT);
         displayPicture = iv;
 
         text.setWrapText(true);
