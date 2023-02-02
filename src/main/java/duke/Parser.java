896ecdd40e1ed3e1364f078083d0c7f75a1a794a
chongweiguan <75872136+chongweiguan@users.noreply.github.com>
2023-02-03 01:36:59 +0800

    Complete A-JavaDoc

---
+++ src/main/java/duke/Parser.java
@@ -0,0 +1,104 @@
+package duke;
+
+import java.util.Arrays;
+
+/**
+ * Parser is a class that deals with making sense of the user command, this includes
+ * getting the description of a certain task, getting the index from the mark, unmark and delete
+ * commands, getting the deadline of a Deadline object and getting the start DateTime 
+ * and end DateTime of an Event Object
+ */
+public class Parser {
+
+    /**
+     * Returns an index from the commands mark, unmark or delete
+     * @param input the command from the user input
+     * @param isMark boolean that checks whether the command is 'mark'
+     * @return int index of the command
+     */
+    public int getMarkNum(String input, boolean isMark) {
+        if(isMark) {
+            return Integer.valueOf(input.substring(5));
+        } else {
+            return Integer.valueOf(input.substring(7));
+        }
+    }
+
+    /**
+     * Returns a description of a Todo object
+     * @param input todo command
+     * @return Description of the todo object 
+     */
+    public String getTodoDescription(String input) {
+        if (input.substring(4).equals("")) {
+            return "";
+        }
+        return input.substring(5);
+    }
+
+    /**
+     * Returns the deadline of a Deadline Object
+     * @param input deadline command
+     * @return deadline of the Deadline object
+     */
+    public String getDeadlineby(String input) {
+        if (input.indexOf("/by") == -1) {
+            return "";
+        }
+        return input.substring(input.indexOf("/by") + 4);
+    }
+
+    /**
+     * Returns the description of a deadline object
+     * @param input deadline command
+     * @return description of the deadline object
+     */
+    public String getDeadlineDescription(String input) {
+        if (input.indexOf("/by") == 9) {
+            return "";
+        }
+        return input.substring(9, input.indexOf("/by") - 1);
+    }
+
+    /**
+     * Returns the description of an event object
+     * @param input event command
+     * @return description of the event object
+     */
+    public String getEventDescription(String input) {
+        String[] arr = input.split(" ");
+        if (arr.length == 1) {
+            return "";
+        } else {
+            for(int i = 0; i < arr.length; i++) {
+                if(arr[i] == "/from") {
+                    return String.join(" ", Arrays.copyOfRange(arr, 1, i+1));
+                }
+            }
+            return "";
+        }
+    }
+
+    /**
+     * Returns the event start date and time
+     * @param input event command
+     * @return Start date and time of an event
+     */
+    public String getEventFrom(String input) {
+        int start = input.indexOf("/from");
+        int end = input.indexOf("/to");
+        if (start == -1 || end == -1) {
+            return "";
+        }
+        return input.substring(start + 6, end - 1);
+    }
+
+    /**
+     * Returns the event end date and time
+     * @param input event command
+     * @return End date and time of an event
+     */
+    public String getEventEnd(String input) {
+        return input.substring(input.indexOf("/to") + 4);
+    }
+}
---
+++ src/main/java/duke/UI.java
@@ -0,0 +1,112 @@
+package duke;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * UI deals with the interactions with the user, including starting the process,
+ * adding tasks into the task list, removing the task from the task list, 
+ * marking and unmarking tasks, and exiting the process
+ */
+public class UI {
+
+    /**
+     * Prints out the starting message when the Duke application begins
+     */
+    public void start() {
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+    }
+
+    /**
+     * Prints out the list of task from the TaskList
+     * @param list Task List
+     */
+    public void showList(TasksList list) {
+        System.out.println("Here are the task(s) in your list:");
+        for (int i = 1; i <= list.getSize(); i++) {
+            System.out.println(i + "." + list.getTask(i-1).toString());
+        }
+    }
+
+    /**
+     * Mark the task in the certain index of the Task List as done
+     * @param list Task List
+     * @param index index of the task
+     */
+    public void mark(TasksList list, int index) {
+        list.getTask(index - 1).markAsDone();
+        System.out.println("Nice! I've marked this task as done:\n  " + list.getTask(index - 1).toString());
+    }
+
+    /**
+     * Mark the task in the certain index of the Task List as undone
+     * @param list Task List
+     * @param index index of the task
+     */
+    public void unmark(TasksList list, int index) {
+        list.getTask(index - 1).markAsUndone();
+        System.out.println("OK, I've marked this task as not done yet:\n  " + list.getTask(index - 1).toString());
+    }
+
+    /**
+     * Adds a Todo Object into the Task List
+     * @param list Task List
+     * @param name Description of the todo task
+     */
+    public void addTodo(TasksList list, String name) {
+        list.addTask(new Todo(name));
+        System.out.println("Got it. I've added this task:");
+        System.out.println("  " + list.getTask(list.getSize() - 1).toString());
+        System.out.println("Now you have " + list.getSize() + " task(s) in the list.");
+    }
+
+    /**
+     * Adds a Deadline Object into the Task List
+     * @param list Task List
+     * @param name Description of the Deadline task
+     * @param deadline Date and Time of the deadline
+     */
+    public void addDeadline(TasksList list, String name, String deadline) {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HHmm");
+        LocalDateTime formattedDeadline = LocalDateTime.parse(deadline, dateTimeFormatter);
+        list.addTask(new Deadline(name, formattedDeadline));
+        System.out.println("Got it. I've added this task:");
+        System.out.println("  " + list.getTask(list.getSize() - 1).toString());
+        System.out.println("Now you have " + list.getSize() + " task(s) in the list.");
+    }
+
+    /**
+     * Adds an Event Object into the Task List
+     * @param list Task List
+     * @param name Description of the event task
+     * @param start Date and time of the start of the event
+     * @param end Date and time of the end of the event
+     */
+    public void addEvent(TasksList list, String name, String start, String end) {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HHmm");
+        LocalDateTime formattedstartTime = LocalDateTime.parse(start, dateTimeFormatter);
+        LocalDateTime formattedendTime= LocalDateTime.parse(end, dateTimeFormatter);
+        list.addTask(new Event(name, formattedstartTime, formattedendTime));
+        System.out.println("Got it. I've added this task:");
+        System.out.println("  " + list.getTask(list.getSize() - 1).toString());
+        System.out.println("Now you have " + list.getSize() + " task(s) in the list.");
+    }
+
+    /**
+     * Removes a task from the Task List
+     * @param list Task List
+     * @param index Index of the task
+     */
+    public void removeTask(TasksList list, int index) {
+        System.out.println("Noted. I've removed this task :");
+        System.out.println("  " + list.removeTask(index - 1).toString());
+        System.out.println("Now you have " + list.getSize() + " task(s) in the list.");
+    }
+
+    /**
+     * Prints the exit message when the Duke Application closes
+     */
+    public void showExit() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
