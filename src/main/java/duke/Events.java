01db36b7672308c03cd22c260cac1aaa15a0ad8d
toh-xinyi <tohxinyi10@gmail.com>
2023-02-02 18:40:12 +0800

    Add JavaDoc comments to the majority of methods.

--- src/main/java/duke/Events.java
+++ src/main/java/duke/Events.java
@@ -18,6 +18,11 @@ public class Events extends Task {
         this.endDate = LocalDate.parse(endDate);
     }
 
+    /**
+     * Adds on the start and end date to the <code>description()</code> method in
+     * <code>Task</code>.
+     * @return a <code>String</code> in the format to be output to the user
+     */
     @Override
     public String description() {
         return String.format("%s (from: %s to: %s)", super.description(),
@@ -25,6 +30,12 @@ public class Events extends Task {
                 this.endDate.format(DateTimeFormatter.ofPattern("MMM d yyyy")));
     }
 
+    /**
+     * Adds on the start and end date to the <code>formatDescription()</code>
+     * method in <code>Task</code> class.
+     * @return a <code>String</code> to be added in format of the task list
+     * representing this <code>Events</code> object.
+     */
     @Override
     public String formatDescription() {
         return super.formatDescription()
--- src/main/java/duke/TaskList.java
+++ src/main/java/duke/TaskList.java
@@ -13,33 +13,66 @@ public class TaskList {
         this.tasks = tasks;
     }
 
+    /**
+     * Returns the task at the given index from the task list.
+     * @param index index of task in the task list
+     * @return the task at the index given
+     */
     public Task getTaskByIndex(int index) {
         return this.tasks.get(index);
     }
+
+    /**
+     * Adds the given task into the task list.
+     * @param task task to be added into the task list
+     */
     public void addTask(Task task){
         this.tasks.add(task);
     }
 
+    /**
+     * Deletes and returns the task deleted from the task list.
+     * @param indexOfTask index of task to be deleted
+     * @return the task deleted
+     */
     public Task deleteTask(int indexOfTask) {
         return this.tasks.remove(indexOfTask - 1);
     }
 
+    /**
+     * Marks the task given by its index as done.
+     * @param indexOfTask index of the task to mark as done
+     * @return the task after marking it as done
+     */
     public Task markTaskAsDone(int indexOfTask) {
         Task toMarkDone = this.tasks.get(indexOfTask);
         toMarkDone.markDone();
         return toMarkDone;
     }
 
+    /**
+     * Marks the task given by its index as undone.
+     * @param indexOfTask index of the task to mark as undone
+     * @return the task after marking it as undone
+     */
     public Task markTaskAsUndone(int indexOfTask) {
         Task toMarkUndone = this.tasks.get(indexOfTask);
         toMarkUndone.markUndone();
         return toMarkUndone;
     }
 
+    /**
+     * Retrieves the size of the task list.
+     * @return size of task list
+     */
     public int getTaskListSize() {
         return tasks.size();
     }
 
+    /**
+     * Prints the list of tasks in user output format and informs
+     * user if no task is present in the task list.
+     */
     public void printTaskList() {
         for (int i = 0; i < tasks.size(); i++) {
             Task currTask = tasks.get(i);
