5fb58387b080f64aa2c100c7989371fe0372ab3f
Mmaimer33 <rchousalkar@gmail.com>
2023-02-02 23:58:27 +0800

    Setup javafx

---
+++ src/main/java/duke/gui/StageApp.java
@@ -0,0 +1,19 @@
+package duke.gui;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class StageApp extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        //todo
+    }
+
+}
