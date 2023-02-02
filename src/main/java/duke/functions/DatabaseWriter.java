4223d4ef15f0fbe3709d0a923446cb36a26a38f5
JamesLiuZX <liu.zixin@comp.nus.edu.sg>
2023-02-02 17:57:44 +0800

    A-JavaDoc

--- src/main/java/duke/functions/DatabaseWriter.java
+++ src/main/java/duke/functions/DatabaseWriter.java
@@ -1,23 +1,36 @@
 package duke.functions;
 
-import duke.tasks.Task;
-
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import duke.tasks.Task;
 import java.util.List;
 
-
+/**
+ * The main class for manipulating the database records in duke.txt.
+ * @author JamesLiuZX
+ * AY2223-S2 CS2103T
+ */
 public class DatabaseWriter {
     private static final String folderPath = "data";
     private static final String filePath = "data/duke.txt";
     private static Path path = Paths.get(filePath);
 
+    /**
+     * Default constructor for a DatabaseWriter object.
+     *
+     * @param path File path of database.
+     */
     public DatabaseWriter(Path path) {
         this.path = path;
     }
 
+    /**
+     * Inserts task to database.
+     *
+     * @param t Task object to be added.
+     */
     public void addToDb(Task t) {
         try {
             FileWriter fw = new FileWriter(filePath, true);
@@ -29,6 +42,11 @@ public class DatabaseWriter {
         }
     }
 
+    /**
+     * Removes task from database.
+     *
+     * @param index Index of Task object to be removed (1-indexed).
+     */
     public void removeFromDb(int index) {
         try {
             List<String> lines = Files.readAllLines(path);
@@ -38,6 +56,11 @@ public class DatabaseWriter {
         }
     }
 
+    /**
+     * Marks a task as done in the database.
+     *
+     * @param index Index of Task object to be marked done (1-indexed).
+     */
     public void setDone(int index) {
         try {
             List<String> lines = Files.readAllLines(path);
@@ -49,6 +72,11 @@ public class DatabaseWriter {
         }
     }
 
+    /**
+     * Marks a task as undone in the database.
+     *
+     * @param index Index of Task object to be marked undone (1-indexed).
+     */
     public void setUndone(int index) {
         try {
             List<String> lines = Files.readAllLines(path);
@@ -60,6 +88,14 @@ public class DatabaseWriter {
         }
     }
 
+    /**
+     * Helper method to edit the done status of task records in the database.
+     *
+     * @param s The record string in the database.
+     * @param index Index of character to be replaced.
+     * @param newValue The new char value.
+     * @return The new string with updated done status.
+     */
     public static String replaceChar(String s, int index, char newValue) {
         StringBuilder string = new StringBuilder(s);
         string.setCharAt(index, newValue);
