7e490d3acf11d52b1470a38e76d78704c2f354a1
DESKTOP-R4TTM5F\oPandaCAT <e0411191@u.nus.edu>
2023-02-03 01:29:29 +0800

    Pack all classes under duke package

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,11 +1,5 @@
-import java.io.FileWriter;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+package duke;
+
 import java.util.Scanner;
 
 public class Duke {
