03d6128ffcc9d493e646dcb3dfe946706135877d
zhoyx <zhouyx2000@gmail.com>
2023-02-02 22:29:51 +0800

    Add JavaDocs for Duke

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -2,17 +2,27 @@ package duke;
 
 import duke.command.Command;
 
+/**
+ * Instance of Duke class.
+ */
 public class Duke {
     private static TaskList tasks;
     private static Storage storage;
     private static Ui ui;
 
+    /**
+     * Creates an instance of the Duke chatbot.
+     * @param filePath File path to store task data for Tasks.
+     */
     public Duke(String filePath) {
         ui = new Ui();
         storage = new Storage(filePath);
         tasks = new TaskList(storage.load());
     }
 
+    /**
+     * Starts an instance of the Duke chatbot.
+     */
     public void run() {
         ui.greet();
         boolean isExit = false;
