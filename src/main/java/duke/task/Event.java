410a99b61f2b23a1764cb508b31519bf596f73be
zhoyx <zhouyx2000@gmail.com>
2023-02-02 22:12:39 +0800

    Add JavaDocs for Event

--- src/main/java/duke/task/Event.java
+++ src/main/java/duke/task/Event.java
@@ -5,27 +5,49 @@ import java.time.LocalDateTime;
 import static duke.utils.FormatHelper.PRINTFORMAT;
 import static duke.utils.FormatHelper.INPUTFORMAT;
 
+/**
+ * Task with to date and a from date.
+ */
 public class Event extends Task {
     protected LocalDateTime from;
     protected LocalDateTime to;
 
+    /**
+     * Constructor to create an Event
+     * @param description Description of the Event.
+     * @param from Start date and time of the Event.
+     * @param to End date and time of the Event.
+     */
     public Event (String description, LocalDateTime from, LocalDateTime to) {
         super(description);
         this.from = from;
         this.to = to;
     }
 
+    /**
+     * Converts Event data to Save string format.
+     * @return Save string format of Event.
+     */
     @Override
     public String toSaveFormat() {
         return "E||" + super.toSaveFormat() + "||" + this.from.format(INPUTFORMAT) + "||" + this.to.format(INPUTFORMAT);
     }
 
+    /**
+     * Converts Event data to print format.
+     * @return Print string format.
+     */
     @Override
     public String toString() {
         return "[E]" + super.toString()
                 + " (from: " + this.from.format(PRINTFORMAT) + ", to: " + this.to.format(PRINTFORMAT) + ")";
     }
 
+    /**
+     * Takes string savedData, parse the values and create an Event object.
+     * @param savedData String input to be decoded.
+     * @return Event object created from data in savedData.
+     */
     public static Event fromSaveFormat(String savedData) {
         String[] inputs = savedData.split("\\|\\|");
         LocalDateTime parseFrom = LocalDateTime.parse(inputs[3], INPUTFORMAT);
