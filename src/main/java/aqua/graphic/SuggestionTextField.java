815b98d9165c97ad79058e6ede8d1587a65a6257
daitenshionyan <snivy.oshowott@gmail.com>
2023-02-02 19:15:45 +0800

    Suggestions: fix flickering

--- src/main/java/aqua/graphic/SuggestionTextField.java
+++ src/main/java/aqua/graphic/SuggestionTextField.java
@@ -5,10 +5,15 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import aqua.logic.command.Command;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.value.ObservableValue;
 import javafx.geometry.Side;
 import javafx.scene.control.ContextMenu;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 
 
 /**
@@ -17,37 +22,62 @@ import javafx.scene.control.TextField;
  */
 public class SuggestionTextField extends TextField {
     private final ContextMenu suggestionMenu = new ContextMenu();
+    private final ObjectProperty<UserCommand> userCommand = new SimpleObjectProperty<>();
 
 
     public SuggestionTextField() {
+        initialiseSuggestionMenu();
         initialiseSuggestor();
     }
 
 
-    private void initialiseSuggestor() {
-        textProperty().addListener((ob, o, n) -> {
-            if (n == null || n.isBlank()) {
-                suggestionMenu.hide();
-                return;
+    private void initialiseSuggestionMenu() {
+        suggestionMenu.addEventFilter(KeyEvent.KEY_PRESSED, e -> {
+            if (e.getCode().equals(KeyCode.SPACE) || e.getCode().equals(KeyCode.ENTER)) {
+                e.consume();
             }
-
-            String inputCommand = n.split("\\s+")[0].toUpperCase();
-            boolean isCompleteCommand = n.matches("[^\\s]+\\s+.*");
-            List<MenuItem> menuSuggestions = getMenuSuggestions(inputCommand, isCompleteCommand);
-
-            if (menuSuggestions.isEmpty()) {
-                suggestionMenu.hide();
-                return;
-            }
-            suggestionMenu.getItems().setAll(menuSuggestions);
-            suggestionMenu.show(SuggestionTextField.this, Side.BOTTOM, 0, 0);
         });
     }
 
 
+    private void initialiseSuggestor() {
+        textProperty().addListener(this::handleTextChange);
+        userCommand.addListener(this::handleCommandChange);
+    }
+
+
+    private void handleTextChange(ObservableValue<? extends String> observable, String oldText, String newText) {
+        if (newText == null) {
+            suggestionMenu.hide();
+            return;
+        }
+        String commandString = newText.split("\\s+", 2)[0].toUpperCase();
+        boolean isComplete = newText.matches("[^\\s]+\\s+.*");
+        userCommand.set(new UserCommand(commandString, isComplete));
+    }
+
+
+    private void handleCommandChange(
+                ObservableValue<? extends UserCommand> observable, UserCommand oldCmd, UserCommand newCmd) {
+        if (newCmd == null) {
+            suggestionMenu.hide();
+            return;
+        }
+        List<MenuItem> menuItems = getMenuSuggestions(newCmd.commandString, newCmd.isComplete);
+        suggestionMenu.getItems().setAll(menuItems);
+        if (!suggestionMenu.isShowing()) {
+            suggestionMenu.show(SuggestionTextField.this, Side.BOTTOM, 0, 0);
+        }
+    }
+
+
     private List<MenuItem> getMenuSuggestions(String inputCmd, boolean isCompleteCommand) {
-        List<Command> cmdList = (isCompleteCommand) ?
-                forceCommand(inputCmd) : getCommandSuggestions(inputCmd);
+        if (inputCmd.isBlank()) {
+            return List.of();
+        }
+        List<Command> cmdList = (isCompleteCommand)
+                ? forceCommand(inputCmd)
+                : getCommandSuggestions(inputCmd);
         return cmdList.stream()
                 .map(command -> new MenuItem(command.name()))
                 .collect(Collectors.toList());
@@ -71,5 +101,20 @@ public class SuggestionTextField extends TextField {
             }
         }
         return commandList;
+    }
+
+
+
+
+
+    private static class UserCommand {
+        public final String commandString;
+        public final boolean isComplete;
+
+
+        UserCommand(String commandString, boolean isComplete) {
+            this.commandString = commandString;
+            this.isComplete = isComplete;
+        }
     }
 }
