e13780ca5c2ce0d36fb368ff7c2ce0a23b8093a1
“ShawnYip-Au” <shawnyipau@gmail.com>
2023-02-03 15:15:31 +0800

    Add JavaDOcs for DukeExcpetion and Duke

--- src/main/java/duke/DukeException.java
+++ src/main/java/duke/DukeException.java
@@ -4,7 +4,12 @@ package duke;
  * Consists of custom run-time exceptions for Duke.
  */
 public class DukeException extends Exception {
-    
+
+    /**
+     * Creates a duke exception.
+     *
+     * @param message Exception message.
+     */
     public DukeException(String message) {
         super(message);
     }
