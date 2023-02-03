8a1a005506241b585d0945887079e2dff27ac71f
Hafeez <izzhafeez@gmail.com>
2023-02-03 12:52:18 +0800

    Improve findCommand execution by using streams as the predicate

--- src/main/java/command/FindCommand.java
+++ src/main/java/command/FindCommand.java
@@ -1,5 +1,7 @@
 package command;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.function.Predicate;
 
 import gui.Gui;
@@ -11,26 +13,21 @@ import task.TaskList;
  * Command for finding matches to user inputs.
  */
 public class FindCommand extends Command {
-    private final String[] searchTerms;
+    private final ArrayList<String> searchTerms;
 
     /**
      * Constructor for FindCommand;
      * @param searchTerms The terms to search.
      */
     public FindCommand(String... searchTerms) {
-        this.searchTerms = searchTerms;
+        this.searchTerms = new ArrayList<>();
+        Collections.addAll(this.searchTerms, searchTerms);
     }
 
     @Override
     public void execute(TaskList taskList, Gui gui, Storage storage) {
-        Predicate<Task> taskPredicate = (task) -> {
-            for (String searchTerm : this.searchTerms) {
-                if (task.getContent().contains(searchTerm)) {
-                    return true;
-                }
-            }
-            return false;
-        };
+        Predicate<Task> taskPredicate = task -> this.searchTerms.stream()
+                .anyMatch(searchTerm -> task.getContent().contains(searchTerm));
         gui.say(taskList.listItems(taskPredicate));
     }
 }
