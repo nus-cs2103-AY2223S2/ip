584a3ebae697d87e5533e71fc67b8b6d5fa92a6c
ARPspoofing <mohdreyaaz99@gmail.com>
2023-02-03 10:08:16 +0800

    Add multipage navigation
    Single page application
    
    User will be unaware of what the application does
    
    Let's add a new scene that explains the app and a button to navigate to
    the chat application

--- src/main/java/duke/Main.java
+++ src/main/java/duke/Main.java
@@ -5,7 +5,10 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -22,7 +25,16 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             scene.getStylesheets().add(this.getClass().getResource("/style.css").toExternalForm());
-            stage.setScene(scene);
+
+            Label label1 = new Label("Welcome to Manchester United");
+            Button button = new Button();
+            button.setOnAction(e -> stage.setScene(scene));
+            VBox layout1 = new VBox(20);
+            layout1.getChildren().addAll(label1, button);
+            Scene scene2 = new Scene(layout1, 200, 200);
+
+            stage.setScene(scene2);
+            stage.setTitle("Duke");
             fxmlLoader.<MainWindow>getController().setDuke(duke);
             stage.show();
         } catch (IOException e) {
