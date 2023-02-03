6de796048241ba99af0214078b9553eef21ed85e
Jiatong Han <jiatong.han@u.nus.edu>
2023-02-03 11:08:30 +0800

    More OOP in-progress

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,86 +1,22 @@
+package duke;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Scanner;
-import java.io.File;
+
+import duke.UI;
+import duke.Parser;
+import duke.Storage;
+import duke.TaskList;
 
 
 public class Duke {
-    public static ArrayList<Task> todos = new ArrayList<>();
     public static File dir = new File("./data/");
     public static File file = new File("./data/todo_list.txt");
-
-    public static class Task {
-        protected String description;
-        protected boolean isDone;
-
-        public Task(String description) {
-            this.description = description;
-            this.isDone = false;
-        }
-
-        public String getStatusIcon() {
-            return (isDone ? "X" : " "); // mark done task with X
-        }
-
-        public void markAsDone() {
-            this.isDone = true;
-        }
-
-        public void markAsNotDone() {
-            this.isDone = false;
-        }
-
-        @Override
-        public String toString() {
-            return "[" + getStatusIcon() + "] " + this.description;
-        }
-    }
-
-    public static class Todo extends Task {
-        public Todo(String description) {
-            super(description);
-        }
-        @Override
-        public String toString() {
-            return "[T]" + super.toString();
-        }
-    }
-
-    public static class Deadline extends Task {
-
-        protected String by;
-
-        public Deadline(String description, String by) {
-            super(description);
-            this.by = by;
-        }
-
-        @Override
-        public String toString() {
-            return "[D]" + super.toString() + " (by: " + by + ")";
-        }
-    }
-
-    public static class Event extends Task {
-
-        protected String from;
-        protected String to;
-
-        public Event(String description, String from, String to) {
-            super(description);
-            this.from = from;
-            this.to = to;
-        }
-
-        @Override
-        public String toString() {
-            return "[E]" + super.toString() + " (from: " + from + " to " + to + ")";
-        }
-    }
 
     public static void print(String s) {
         System.out.println(s);
@@ -97,7 +33,6 @@ public class Duke {
             super(msg);
         }
     }
-
 
     public static String parse_date(String s) {
         DateTimeFormatter read_fmt = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
