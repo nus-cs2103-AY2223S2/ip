94c39832ec02bbcd4de9db478fef82cfa4fb2e54
homuntan02 <homuntan02@gmail.com>
2023-02-03 17:17:16 +0800

    Implemented Level-9 Find

--- src/main/java/duke/commands/FindCommand.java
+++ src/main/java/duke/commands/FindCommand.java
@@ -7,16 +7,16 @@ import duke.tasklist.TaskList;
 /**
  * Command to search for keywords in task of a task list.
  */
-public class FindCommand extends Command{
+public class FindCommand extends Command {
     private String keyword;
 
-    public FindCommand(String keyword){
+    public FindCommand(String keyword) {
         super("FIND");
         this.keyword = keyword;
     }
 
     @Override
-    public void execute(TaskList tasks, Ui ui, Storage storage){
+    public void execute(TaskList tasks, Ui ui, Storage storage) {
         tasks.find(this.keyword, ui);
     }
 }
