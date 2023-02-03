627df693445ef5a8917940736b585ca5c0372a6e
bokung <e0774428@u.nus.edu>
2023-02-03 18:48:58 +0800

    Add basic window

--- src/main/java/uwuke/UwUke.java
+++ src/main/java/uwuke/UwUke.java
@@ -2,6 +2,17 @@ package uwuke;
 
 import java.util.Scanner;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
 import uwuke.input.Advisor;
 import uwuke.input.Command;
 
@@ -11,13 +22,68 @@ import uwuke.output.Storage;
 
 import uwuke.task.TaskList;
 
-public class UwUke {
+public class UwUke extends Application {
 
     private final static int CAPACITY = 100;
     private static TaskList tasks;
     private static Scanner sc = new Scanner(System.in);
 
-    private static void init() {
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
+
+    @Override
+    public void start(Stage stage) {
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+        userInput = new TextField();
+        sendButton = new Button("Send");
+
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+        mainLayout.setPrefSize(400.0, 600.0);
+
+        scene = new Scene(mainLayout);
+        setStage(stage);
+        setScrollPane();
+        
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+        userInput.setPrefWidth(325.0);
+        sendButton.setPrefWidth(55.0);
+        setAnchorPane();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    private static void setStage(Stage stage) {
+        stage.setTitle("Duke");
+        stage.setResizable(false);
+        stage.setMinHeight(600.0);
+        stage.setMinWidth(400.0);
+    }
+
+    private void setScrollPane() {
+        scrollPane.setPrefSize(385, 535);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
+    }
+
+    private void setAnchorPane() {
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+        AnchorPane.setLeftAnchor(userInput , 1.0);
+        AnchorPane.setBottomAnchor(userInput, 1.0);
+    }
+
+
+
+    private static void inititialise() {
         Printer.uwu();
         tasks = new TaskList(CAPACITY);
         try {
@@ -85,9 +151,10 @@ public class UwUke {
     }
 
     public static void main(String[] args) {
-        init();
+        inititialise();
         run();
         sc.close();
         Printer.printBye();
     }
+
 }
