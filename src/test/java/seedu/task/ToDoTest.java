153e2fecb3b7aff2315dfd0eee8c7dbf7be07862
Elvern <elvern18@gmail.com>
2023-02-04 04:12:40 +0800

    completed A-Junit

--- src/test/java/seedu/task/ToDoTest.java
+++ src/test/java/seedu/task/ToDoTest.java
@@ -1,21 +1,24 @@
 package seedu.task;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
 import task.ToDo;
 public class ToDoTest {
-
+    @Test
     public void testToDoDescription(){
         ToDo toDo = new ToDo("return book");
         assertEquals("[T][ ] return book", toDo.toString());
     }
 
+    @Test
     public void testUnmark() {
         ToDo toDo = new ToDo("return book", true);
         toDo.unmark();
         assertEquals("[T][ ] return book", toDo.toString());
     }
 
+    @Test
     public void testMark(){
         ToDo toDo = new ToDo("return book");
         toDo.mark();
