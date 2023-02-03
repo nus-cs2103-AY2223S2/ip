8f62f37041af9b77db1a7a60f6c6a43dd2a11d48
zrei <88964793+zrei@users.noreply.github.com>
2023-02-04 00:20:42 +0800

    Add assertions
    Classes lack assertions.
    
    This causes a lack of documentation of important assumptions that
    should hold at various stages of runtime.
    
    Adding assertions allows runtime bugs to be identified more easily.
    
    Let's add assertions to multiple classes.

--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -133,6 +133,7 @@ public class Parser {
         } catch (DateTimeParseException e) {
             throw new DukeException(GuiText.generateDateErrorMessage());
         }
+        assert task != null : "Task should not be null";
         return new AddCommand(task);
     }
 
