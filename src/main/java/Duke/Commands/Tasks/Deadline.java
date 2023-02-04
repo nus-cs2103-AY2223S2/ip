4df123f7ac380e74c352211407390f6500bdbad6
Shi Jiaao <e0372500@u.nus.edu>
2023-02-04 17:32:55 +0800

    Add formatter for date of deadlines and events

--- src/main/java/Duke/Commands/Tasks/Deadline.java
+++ src/main/java/Duke/Commands/Tasks/Deadline.java
@@ -1,18 +1,28 @@
 package Duke.Commands.Tasks;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
-    protected String deadline;
+    protected LocalDate deadline;
+    protected static String DATE_FORMAT = "MMM dd yyyy";
+
     public Deadline(String description, String deadline) {
         super(description);
-        this.deadline = deadline;
+        this.deadline = LocalDate.parse(deadline);
     }
 
     private String getTaskClass() {
         return "D";
     }
 
+    private String getDeadline() {
+        return this.deadline.format(DateTimeFormatter.ofPattern("MMM dd yyyy"));
+    }
+
     public String toString() {
         return String.format("[%s][%s] %s (by: %s)",
-                this.getTaskClass(), this.getStatusIcon(), this.description, this.deadline);
+                this.getTaskClass(), this.getStatusIcon(), this.description, this.getDeadline());
     }
 }
--- src/main/java/Duke/Commands/Tasks/Event.java
+++ src/main/java/Duke/Commands/Tasks/Event.java
@@ -1,21 +1,32 @@
 package Duke.Commands.Tasks;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
-    protected String from;
-    protected String to;
+    protected LocalDate from;
+    protected LocalDate to;
     public Event(String description, String from, String to) {
         super(description);
-        this.from = from;
-        this.to = to;
+        this.from = LocalDate.parse(from);
+        this.to = LocalDate.parse(to);
     }
 
     private String getTaskClass() {
         return "T";
     }
 
+    private String getFrom() {
+        return this.from.format(DateTimeFormatter.ofPattern("MMM dd yyyy"));
+    }
+
+    private String getTo() {
+        return this.to.format(DateTimeFormatter.ofPattern("MMM dd yyyy"));
+    }
+
     public String toString() {
         return String.format("[%s][%s] %s (from: %s to: %s)",
                 this.getTaskClass(), this.getStatusIcon(),
-                this.description, this.from, this.to);
+                this.description, this.getFrom(), this.getTo());
     }
 }
