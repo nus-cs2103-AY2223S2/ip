a7ba8f57c16bf98b878301648cea89cbe3a14079
szejiancheng <sze2001jc@gmail.com>
2023-02-04 12:43:20 +0800

    Add javadocs for exceptions and tasks

--- src/main/java/duke/tasks/ToDo.java
+++ src/main/java/duke/tasks/ToDo.java
@@ -1,16 +1,31 @@
 package duke.tasks;
 
+/**
+ * This is the ToDo task class to represent events passed to Duke.
+ * Encapsulates all information related to the todo.
+ */
 public class ToDo extends Task {
+    /**
+     * Creates a new ToDo object.
+     * @param description String description of the ToDo represented.
+     */
     public ToDo(String description) {
         super(description);
-        //System.out.println("Todo constructor called");
     }
 
+    /**
+     * {@inheritDoc}
+     * @return {@inheritDoc}
+     */
     @Override
     String getTypeIcon() {
         return "[T]";
     }
 
+    /**
+     * {@inheritDoc}
+     * @return {@inheritDoc}
+     */
     @Override
     public String toString() {
         return "[T]" + super.toString();
