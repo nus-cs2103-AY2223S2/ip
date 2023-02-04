8af7771ecb943492c184c20c4b12939e13f3da0e
daitenshionyan <snivy.oshowott@gmail.com>
2023-02-04 11:52:43 +0800

    DaySchedule: add day label

--- src/main/java/aqua/graphic/DaySchedule.java
+++ src/main/java/aqua/graphic/DaySchedule.java
@@ -1,31 +1,54 @@
 package aqua.graphic;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 
 import aqua.util.DateUtils;
 import aqua.util.Timeable;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.shape.Rectangle;
 
 
-public class DaySchedule extends VBox {
-    private static final double CANVAS_WIDTH = 600;
-    private static final double CANVAS_HEIGHT = 15;
+public class DaySchedule extends HBox {
+    private static final double LABEL_WIDTH = 80;
+    private static final double ROW_WIDTH = 600;
+    private static final double ROWHEIGHT = 15;
     private static final double MINUTES_IN_A_DAY = 1440;
+
+    private final VBox rowDisplayArea = new VBox();
 
 
     public DaySchedule(LocalDateTime startTime, List<Timeable> timeables) {
-        System.out.println(timeables);
+        initialiseDay(startTime);
         List<? extends List<Timeable>> sepTimeable = separateConflicting(timeables);
-        System.out.println(sepTimeable);
         for (List<Timeable> row : sepTimeable) {
             System.out.println(startTime);
             addRow(startTime, row);
         }
+    }
+
+
+    private final void initialiseDay(LocalDateTime startTime) {
+        Label label = initialiseLabel(startTime);
+        getChildren().addAll(label, rowDisplayArea);
+    }
+
+
+    private Label initialiseLabel(LocalDateTime startTime) {
+        Label dateLabel = new Label();
+        dateLabel.setMinWidth(LABEL_WIDTH);
+        dateLabel.setPrefWidth(LABEL_WIDTH);
+        dateLabel.setMaxWidth(LABEL_WIDTH);
+        dateLabel.setAlignment(Pos.CENTER);
+        dateLabel.setText(startTime.format(DateTimeFormatter.ofPattern("LLL d (EEE)")));
+        return dateLabel;
     }
 
 
@@ -63,19 +86,17 @@ public class DaySchedule extends VBox {
 
     private void addRow(LocalDateTime startTime, List<Timeable> timeables) {
         Pane pane = new Pane();
-        pane.setPrefWidth(CANVAS_WIDTH);
-        pane.setPrefHeight(CANVAS_HEIGHT);
+        pane.setMinWidth(ROW_WIDTH);
+        pane.setMaxHeight(ROWHEIGHT);
         for (Timeable timeable : timeables) {
             double startX = (startTime.until(timeable.getStart(), ChronoUnit.MINUTES) / MINUTES_IN_A_DAY)
-                    * CANVAS_WIDTH;
+                    * ROW_WIDTH;
             double endX = (startTime.until(timeable.getEnd(), ChronoUnit.MINUTES) / MINUTES_IN_A_DAY)
-                    * CANVAS_WIDTH;
-            System.out.println(startX);
-            System.out.println(endX);
-            Rectangle rectangle = new Rectangle(endX - startX, CANVAS_HEIGHT);
+                    * ROW_WIDTH;
+            Rectangle rectangle = new Rectangle(endX - startX, ROWHEIGHT);
             pane.getChildren().add(rectangle);
             rectangle.setLayoutX(startX);
         }
-        getChildren().add(pane);
+        rowDisplayArea.getChildren().add(pane);
     }
 }
