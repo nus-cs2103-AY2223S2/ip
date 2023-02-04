7a0eb0b675bda5b2c22026d329c3518dc6fb75d4
Shi Jiaao <e0372500@u.nus.edu>
2023-02-04 19:17:35 +0800

    Add JUnit tests for Parser and Storage classes

---
+++ src/test/java/duke/TemplateTest.java
@@ -0,0 +1,27 @@
+package duke;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Template for testing from cs2103 website
+ */
+public class DukeTest {
+    /*
+    @Test
+    public void dummyTest(){
+        assertEquals(2, 2);
+    }
+
+    @Test
+    public void anotherDummyTest(){
+        assertEquals(4, 4);
+    }
+    */
+
+    @Test
+    void main() {
+        assertEquals(4, 4);
+    }
+}
