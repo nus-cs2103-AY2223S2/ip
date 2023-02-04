c135b9d08b293b6abbc295a490d0d19cdecf3d83
Warren Yang <e0544083@u.nus.edu>
2023-02-05 00:06:53 +0800

    Level 10

---
+++ src/main/java/skittles/Launcher.java
@@ -0,0 +1,8 @@
+package skittles;
+import skittles.Duke;
+import javafx.application.Application;
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(Main.class, args);
+    }
+}
