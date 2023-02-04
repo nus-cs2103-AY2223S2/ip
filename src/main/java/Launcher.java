507a5b13417f83463cbaaf1b6d5ba06d953735b7
gitsac <isaacchai99@gmail.com>
2023-02-04 16:25:41 +0800

    Add JavaFX tutorial parts 1-3

---
+++ src/main/java/Launcher.java
@@ -0,0 +1,13 @@
+import javafx.application.Application;
+
+/**
+ * xxx
+ */
+public class Launcher {
+    /**
+     * @param args xxxx
+     */
+    public static void main(String[] args) {
+        Application.launch(Duke.class, args);
+    }
+}
