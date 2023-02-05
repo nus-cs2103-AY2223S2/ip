57dacc4dce8e559aeaf6a8c09d2ac4ba27aabc39
Tan Jun Da <e0726893@u.nus.edu>
2023-02-05 22:51:01 +0800

    Update GUI
    The current GUI has no application header and icon.
    
    The GUI is currently ugly with the default Java icon and header.
    
    Let's add the GUI header and icon by using scene Image and some String
    name called Duke in the Main class start method.
    
    Best way to change the Icon and header of the application to look better
    .

--- src/main/java/seedu/duke/Main.java
+++ src/main/java/seedu/duke/Main.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -26,7 +27,12 @@ public class Main extends Application {
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
+            Image image = new Image("/images/DaDuke.png");
+
+            stage.setTitle("Duke");
+            stage.getIcons().add(image);
             stage.setScene(scene);
+
             fxmlLoader.<MainWindow>getController().setDuke(duke);
             stage.show();
         } catch (IOException e) {
