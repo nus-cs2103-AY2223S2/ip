8cfafa42985d42f2f3b3ad1ba16daed2fedb37db
Sean Wong <58366602+ExtraShotLatte@users.noreply.github.com>
2023-02-05 17:43:05 +0800

    Remove dummy variable in mainwindow to enable it to use Duke

--- src/main/java/MainWindow.java
+++ src/main/java/MainWindow.java
@@ -43,11 +43,12 @@ public class MainWindow extends AnchorPane {
     @FXML
     private void handleUserInput() {
         String input = userInput.getText();
-        //String response = duke.getResponse(input);
-//        dialogContainer.getChildren().addAll(
-//                DialogBox.getUserDialog(input, userImage),
-//                DialogBox.getDukeDialog(response, dukeImage)
-//        );
+        //String response = input;
+        String response = duke.getDukeResponse(input);
+        dialogContainer.getChildren().addAll(
+                DialogBox.getUserDialog(input, userImage),
+                DialogBox.getDukeDialog(response, dukeImage)
+        );
         userInput.clear();
     }
 }
