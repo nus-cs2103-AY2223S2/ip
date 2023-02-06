affc3e842a61b88e5697156227751dc29d089970
Douglch <douglchwm@gmail.com>
2023-02-06 12:02:41 +0800

    Added jar files

--- src/main/java/lele/Main.java
+++ src/main/java/lele/Main.java
@@ -4,15 +4,12 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
-import javafx.scene.layout.Region;
 
 import java.io.IOException;
 
