aa04344ee3ace0acf3a7a46aac7321686a77f499
securespider <e0725031@u.nus.edu>
2023-02-06 14:07:48 +0800

    Added javadocs for some methods and classes

--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -10,6 +10,12 @@ import duke.Exceptions.TaskIndexException;
 import duke.Tasks.Task;
 
 public class Parser {
+    /**
+     * Reads the command string and converts it into a Command object based on the string
+     * @param input string that the user has input
+     * @return Command object representing what the command that user input
+     * @throws DukeException If the command is invalid
+     */
     public static Command parse(String input) throws DukeException {
         String[] inputArray = input.split(" ", 2);
         String inputCommand = inputArray[0];
--- src/main/java/duke/Storage.java
+++ src/main/java/duke/Storage.java
@@ -5,6 +5,9 @@ import duke.Tasks.TaskList;
 
 import java.io.*;
 
+/**
+ * Represents an abstraction that helps in dealing with files
+ */
 public class Storage {
 
     BufferedReader content;
@@ -12,6 +15,11 @@ public class Storage {
     File file;
     String filepath;
 
+    /**
+     * A constructor to specify the path
+     * @param filepath the path to save the file and load the file from
+     * @throws IOException If the path input is invalid
+     */
     public Storage(String filepath) throws IOException{
         this.filepath = filepath;
         this.file = new File(filepath);
@@ -21,6 +29,11 @@ public class Storage {
         this.fileReader = new FileReader(this.file);
     }
 
+    /**
+     * Loads tasks from the file.
+     *
+     * @return reader object for the file
+     */
     public BufferedReader load() {
         if (this.content != null) {
             return content;
@@ -29,6 +42,11 @@ public class Storage {
         return this.content;
     }
 
+    /**
+     * Updates the file and stores the tasklist into the file
+     * @param tasks The tasks to be written in the file
+     * @throws IOException If the file cannot be created or written in
+     */
     public void store(TaskList tasks) throws IOException {
         //noinspection ResultOfMethodCallIgnored
         this.file.createNewFile();
