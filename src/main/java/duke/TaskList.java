cd8afb761ffb8604ed442e4419b6d59ed7a2b360
chuakid <chuakid@gmail.com>
2023-02-06 18:15:32 +0800

    Sort by priority when listing tasks

--- src/main/java/duke/TaskList.java
+++ src/main/java/duke/TaskList.java
@@ -120,6 +120,7 @@ public class TaskList implements Serializable {
 
     @Override
     public String toString() {
+        list.sort((a, b) -> b.priority.compareTo(a.priority));
         StringBuilder str = new StringBuilder();
         for (int i = 0; i < this.list.size(); i++) {
             str.append(String.format("%d: %s\n", i + 1, this.list.get(i)));
