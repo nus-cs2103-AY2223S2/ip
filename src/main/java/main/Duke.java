cd7159354f5d409ee42aaba0b9a5393ab367ea3d
liujiayue314 <e0773130@u.nus.edu>
2023-02-07 02:57:37 +0800

    Add GUI

--- src/main/java/main/Duke.java
+++ src/main/java/main/Duke.java
@@ -10,43 +10,88 @@ public class Duke {
     private Storage storage;
     private TaskList tasks;
     private Ui ui;
-
+    private static final String filePath = "tasks.txt";
 
     /**
      * Constructs Duke task manager.
      */
-    private Duke(String filePath) {
+    public Duke() {
         try {
             ui = new Ui();
             storage = new Storage(filePath);
             tasks = new TaskList(storage.load());
         } catch (DukeException e) {
-            ui.outputError(e.getMessage());
+            System.out.println(e.getMessage());
         }
     }
+
 
     /**
      * Takes in user input and runs task manager until exit is called.
      */
-    private void run() {
-        Ui.greet();
-        boolean isExit = false;
-        while (!isExit) {
-            try {
-                String fullCommand = ui.readCommand();
-                ui.showLine();
-                Command c = Parser.parse(fullCommand);
-                c.execute(tasks, ui, storage);
-                isExit = c.isExit();
-            } catch (DukeException e) {
-                ui.outputError(e.getMessage());
-            } finally {
-                ui.showLine();
-            }
+    public String getResponse(String input) {
+        String str;
+        try {
+            ui.startOfInput();
+            Command c = Parser.parse(input);
+            c.execute(tasks, ui, storage);
+        } catch (DukeException e) {
+            ui.outputError(e.getMessage());
+        } finally {
+            str = ui.endOfInput();
         }
-
-    }
-    public static void main(String[] args) {
-        new Duke("tasks.txt").run();
+        return str;
     }
 }
+
+/**
+ * @Override
+ *     public void start(Stage stage) {
+ *
+ *         scrollPane = new ScrollPane();
+ *         dialogContainer = new VBox();
+ *         scrollPane.setContent(dialogContainer);
+ *         userInput = new TextField();
+ *         sendButton = new Button("test");
+ *         AnchorPane mainLayout = new AnchorPane();
+ *         mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+ *         scene = new Scene(mainLayout);
+ *         stage.setScene(scene);
+ *         stage.show();
+ *         stage.setTitle("Duke");
+ *         stage.setResizable(false);
+ *         stage.setMinHeight(600.0);
+ *         stage.setMinWidth(400.0);
+ *         mainLayout.setPrefSize(400.0, 600.0); //？
+ *         scrollPane.setPrefSize(385, 535);
+ *         scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+ *         scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+ *         scrollPane.setVvalue(1.0);
+ *         scrollPane.setFitToWidth(true); //？
+ *         dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE); //？
+ *         userInput.setPrefWidth(325.0);
+ *         sendButton.setPrefWidth(55.0);
+ *         AnchorPane.setTopAnchor(scrollPane, 1.0);
+ *         AnchorPane.setBottomAnchor(sendButton, 1.0);
+ *         AnchorPane.setRightAnchor(sendButton, 1.0);
+ *         AnchorPane.setLeftAnchor(userInput , 1.0);
+ *         AnchorPane.setBottomAnchor(userInput, 1.0);
+ *         sendButton.setOnMouseClicked((event) -> {
+ *             handleUserInput();
+ *         });
+ *
+ *         userInput.setOnAction((event) -> {
+ *             handleUserInput();
+ *         });
+ *         dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
+ *     }
+ *
+ *
+ *     private void handleUserInput() {
+ *         dialogContainer.getChildren().addAll(
+ *                 DialogBox.getUserDialog(userInput.getText(), user),
+ *                 DialogBox.getDukeDialog(getResponse(userInput.getText()), duke)
+ *         );
+ *         userInput.clear();
+ *     }
+ */
--- src/main/java/main/Ui.java
+++ src/main/java/main/Ui.java
@@ -1,7 +1,6 @@
 package main;
 
 import java.util.ArrayList;
-import java.util.Scanner;
 
 import task.Task;
 
@@ -9,49 +8,35 @@ import task.Task;
  * Class the takes input from user and output messages to user.
  */
 public class Ui {
-
-    private static final String indentation = "     ";
-    private static final String newLine = "    ____________________________________________________________";
-    private final Scanner sc;
-
+    private static StringBuilder str;
     /**
      * Constructs Ui.
      */
     public Ui() {
-        sc = new Scanner(System.in);
-    }
-
-    /**
-     * Greats user when programme starts.
-     */
-    public static void greet() {
-        System.out.println(newLine);
-        System.out.println(indentation + "Hello! I'm Duke, your task planner");
-        System.out.println(indentation + "What can I do for you?");
-        System.out.println(newLine);
+        str = new StringBuilder();
     }
 
     /**
      * Output goodbye messages to users when programme ends.
      */
     public static void outputExit() {
-        System.out.println(indentation + "Bye. Hope to see you again soon!");
+        str.append("Bye. Hope to see you again soon!\n");
+    }
+
+
+    /**
+     * Output a first blank line.
+     */
+    public void startOfInput() {
+        str.setLength(0);
     }
 
     /**
-     * Reads commands.
-     *
-     * @return Message that user input.
+     * Output a last blank line.
+     * @return A stringBuilder that contains all output to be printed.
      */
-    public String readCommand() {
-        return sc.nextLine();
-    }
-
-    /**
-     * Output a black line.
-     */
-    public void showLine() {
-        System.out.println(newLine);
+    public String endOfInput() {
+        return str.toString();
     }
 
     /**
@@ -59,8 +44,8 @@ public class Ui {
      * @param t Task that is added.
      */
     public void outputAddTask(Task t) {
-        System.out.println(indentation + "Got it. I've added this task:");
-        System.out.println(indentation + t);
+        str.append("Got it. I've added this task:\n");
+        str.append(t + "\n");
     }
 
     /**
@@ -68,8 +53,8 @@ public class Ui {
      * @param t Task that is marked as done.
      */
     public void outputTaskDone(Task t) {
-        System.out.println(indentation + "Nice! I've marked this task as done:");
-        System.out.println(indentation + t);
+        str.append("Nice! I've marked this task as done:\n");
+        str.append(t + "\n");
     }
 
     /**
@@ -77,8 +62,8 @@ public class Ui {
      * @param t Task that is marked as not done.
      */
     public void outputTaskNotDone(Task t) {
-        System.out.println(indentation + "OK, I've marked this task as not done yet:");
-        System.out.println(indentation + t);
+        str.append("OK, I've marked this task as not done yet:\n");
+        str.append(t + "\n");
     }
 
     /**
@@ -86,8 +71,8 @@ public class Ui {
      * @param t Task that is deleted.
      */
     public void outputDeleteTask(Task t) {
-        System.out.println(indentation + "Noted. I've removed this task:");
-        System.out.println(indentation + t);
+        str.append("Noted. I've removed this task:\n");
+        str.append(t + "\n");;
     }
 
     /**
@@ -95,20 +80,21 @@ public class Ui {
      * @param taskList List of tasks.
      */
     public void outputListTask(TaskList taskList) {
-        System.out.println(indentation + "You have " + taskList.getTotalNumberOfTask() + " tasks in the list.");
+        str.append("You have ").append(taskList.getTotalNumberOfTask()).append(" tasks in the list.\n");
         for (int i = 0; i < taskList.getTotalNumberOfTask(); i++) {
-            System.out.println(indentation + (i + 1) + "." + taskList.getTaskAtIndex(i));
+            str.append(i + 1 + "." + taskList.getTaskAtIndex(i) + "\n");
         }
     }
 
     /**
      * Outputs details of tasks that is searched.
+     *
      * @param taskList List of tasks searched.
      */
     public void outputFindTask(ArrayList<Task> taskList) {
-        System.out.println(indentation + "Here are the matching tasks in your list:");
+        str.append("Here are the matching tasks in your list:\n");
         for (int i = 0; i < taskList.size(); i++) {
-            System.out.println(indentation + (i + 1) + "." + taskList.get(i));
+            str.append(i + 1 + "." + taskList.get(i) + "\n");
         }
     }
 
@@ -117,6 +103,6 @@ public class Ui {
      * @param message Details of why an exception is raised.
      */
     public void outputError(String message) {
-        System.out.println(indentation + message);
+        str.append(message);
     }
 }
