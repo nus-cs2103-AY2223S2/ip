441931eac1753f28d1d9fd76aa926ffbd93a5c2a
daitenshionyan <snivy.oshowott@gmail.com>
2023-02-06 14:16:49 +0800

    UserDeadline: use better naming

--- src/main/java/aqua/usertask/UserDeadline.java
+++ src/main/java/aqua/usertask/UserDeadline.java
@@ -6,45 +6,43 @@ import java.util.Optional;
 import aqua.util.DateUtils;
 
 
-/** An {@code AquaTask} to represent a deadline. */
+/** An {@code UserTask} to represent a deadline. */
 public class UserDeadline extends UserTask {
-    /** Tag of {@code by} parameter when parsing. */
-    public static final String TAG_BY = "by";
+    /** Tag of {@code dueTime} parameter when parsing. */
+    public static final String TAG_DUE_TIME = "by";
 
-    /** If the task is completed. */
     private final boolean isComplete;
-    /** Time the task is due. */
-    private final LocalDateTime by;
+    private final LocalDateTime dueTime;
 
 
     /**
-     * Constructs an {@code AquaDeadline} from the given parameters while
+     * Constructs an {@code UserDeadline} from the given parameters while
      * setting the task completion to {@code false}.
      *
      * @param name - the name of the task.
-     * @param by - the time the task is due.
+     * @param dueTime - the time the task is due.
      */
-    public UserDeadline(String name, LocalDateTime by) {
-        this(name, false, by);
+    public UserDeadline(String name, LocalDateTime dueTime) {
+        this(name, false, dueTime);
     }
 
     /**
-     * Constructs an {@code AquaDeadline} from the given parameters.
+     * Constructs an {@code UserDeadline} from the given parameters.
      *
      * @param name - the name of the task.
      * @param isComplete - if the task is completed.
-     * @param by - the time the task is due.
+     * @param dueTime - the time the task is due.
      */
-    public UserDeadline(String name, boolean isComplete, LocalDateTime by) {
+    public UserDeadline(String name, boolean isComplete, LocalDateTime dueTime) {
         super(name);
         this.isComplete = isComplete;
-        this.by = by;
+        this.dueTime = dueTime;
     }
 
 
     @Override
     public UserDeadline mark(boolean isComplete) {
-        return new UserDeadline(getName(), isComplete, by);
+        return new UserDeadline(getName(), isComplete, dueTime);
     }
 
 
@@ -56,7 +54,7 @@ public class UserDeadline extends UserTask {
 
     @Override
     public Optional<LocalDateTime> getEnd() {
-        return Optional.ofNullable(by);
+        return Optional.ofNullable(dueTime);
     }
 
 
@@ -64,7 +62,7 @@ public class UserDeadline extends UserTask {
     public String getReloadString() {
         return String.format("deadline %s /%s %s /%s %s",
                 getName(),
-                TAG_BY, by,
+                TAG_DUE_TIME, dueTime,
                 TAG_IS_COMPLETE, isComplete);
     }
 
@@ -73,6 +71,6 @@ public class UserDeadline extends UserTask {
     public String toString() {
         return String.format("[D]%s (by: %s)",
                 super.toString(),
-                DateUtils.formatNice(by));
+                DateUtils.formatNice(dueTime));
     }
 }
