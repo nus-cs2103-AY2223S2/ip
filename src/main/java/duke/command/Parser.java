7f12cbc048414d3d51460857a9e12595703d757e
linustws <linustws00@gmail.com>
2023-02-06 14:41:16 +0800

    Use Assertions
    No assertions used.
    
    Assertions can be used to enforce assumptions that are critical to the
    correctness of the code.
    
    Let's,
    * add assertions to check that the index the user entered for mark,
      unmark and delete commands is 1 or greater.
    * insert the assertion before tasks.get(index) to show the assertion
      error before the DukeException is thrown.
    * must enable assertions in the run configuration to display assertion
      errors, but this will prevent displaying the DukeException error
      message.

--- src/main/java/duke/command/Parser.java
+++ src/main/java/duke/command/Parser.java
@@ -3,6 +3,7 @@ package duke.command;
 import duke.exception.DukeException;
 import duke.task.Deadline;
 import duke.task.Event;
+import duke.task.Task;
 import duke.task.Todo;
 
 import java.time.LocalDate;
