b16ac239fbe523cf7dcc1793b1d01b62fcc61323
duong truong minh <gaoranger2002@gmail.com>
2023-02-06 13:00:27 +0800

    Complete A-JUnit

--- src/test/java/duke/TaskTest.java
+++ src/test/java/duke/TaskTest.java
@@ -1,5 +1,29 @@
 package duke;
 
-public class TaskTest {
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TaskTest {
+    @Test
+    public void createNewToDoTask(){
+        try {
+            String newTodoTask = new Task().createNewTask("todo",
+                    "read book").toString();
+            assertEquals(newTodoTask, "[T][ ] read book");
+        } catch (DukeExceptions ex) {
+            System.out.println(ex.getErrorMessage());
+        }
+    }
+
+    @Test
+    public void createNewDeadlineTask(){
+        try {
+            String newDeadlineTask = new Task().createNewTask("deadline",
+                    "return book /by 2023-02-10 18:00").toString();
+            assertEquals(newDeadlineTask, "[D][ ] return book (by: Feb 10 2023 06:00 PM)");
+        } catch (DukeExceptions ex) {
+            System.out.println(ex.getErrorMessage());
+        }
+    }
 }
---
+++ src/test/java/duke/TodoListTest.java
@@ -0,0 +1,18 @@
+package duke;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TodoListTest {
+    @Test
+    public void addToDoTask(){
+        try {
+            TodoList newTodoList = new TodoList();
+            newTodoList.add("todo", "read book");
+            assertEquals(newTodoList.number_of_tasks(), 1);
+        } catch (DukeExceptions ex) {
+            System.out.println(ex.getErrorMessage());
+        }
+    }
+}
