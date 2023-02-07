cbd37e814e730d69569543442ac36bfbc666b0f6
kim <kimberlybpp@gmail.com>
2023-02-07 17:40:24 +0800

    Add JUnit tests

---
+++ src/test/java/ParserTest.java
@@ -0,0 +1,27 @@
+import commands.Command;
+import commands.CommandType;
+import duke.Parser;
+import exceptions.DukeException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ParserTest {
+    @Test
+    public void addTodoCommand() throws DukeException {
+        Command command = Parser.parse("todo read book 1");
+        assertEquals(command.getType(), CommandType.TODO);
+    }
+
+    @Test
+    public void addDeadlineCommand() throws DukeException {
+        Command command = Parser.parse("deadline read book 2 /by 2023-02-02");
+        assertEquals(command.getType(), CommandType.DEADLINE);
+    }
+
+    @Test
+    public void addEventCommand() throws DukeException {
+        Command command = Parser.parse("event read book 3 /from 10 Feb 2pm /to 3pm");
+        assertEquals(command.getType(), CommandType.EVENT);
+    }
+}
---
+++ src/test/java/TaskTest.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.Test;
+import tasks.Deadline;
+import tasks.Event;
+import tasks.Todo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TaskTest {
+    @Test
+    public void initTodoTask() {
+        Todo todo = new Todo("write essay");
+        String expectedOutput = "[T][ ] write essay";
+        assertEquals(expectedOutput, todo.toString());
+    }
+
+    @Test
+    public void initDeadlineTask() {
+        Deadline deadline = new Deadline("do that", "2023-03-03");
+        String expectedOutput = "[D][ ] do that (by: Mar 3 2023)";
+        assertEquals(expectedOutput, deadline.toString());
+    }
+
+    @Test
+    public void initEventTask() {
+        Event event = new Event("event A", "23 March 4pm", "5pm");
+        String expectedOutput = "[E][ ] event A (from: 23 March 4pm to: 5pm)";
+        assertEquals(expectedOutput, event.toString());
+    }
+}
