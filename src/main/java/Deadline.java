f308ef42fb1e2a4e966d84a9bae179ef93c1dffe
QIU QIANHUI <672271700@qq.com>
2023-02-07 13:16:50 +0800

    Add Increments as parallel branche-Level-8 with Dates and Times

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,13 +1,29 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
-    protected String by;
+    protected LocalDateTime by;
 
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        this.by = transferTOLocalDateTime(by);
+    }
+
+    public LocalDateTime transferTOLocalDateTime(String by) {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
+        LocalDateTime dateTime = LocalDateTime.parse(by, dateTimeFormatter);
+        return dateTime;
+    }
+
+    public String deadlineDateTime() {
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("MMM dd yyyy HH:mm");
+        String deadline = this.by.format(format);
+        String dayOfWeek = this.by.getDayOfWeek().toString();
+        return dayOfWeek + ", " + deadline;
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + deadlineDateTime() + ")";
     }
 }
