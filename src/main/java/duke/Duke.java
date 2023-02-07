5d76f49d5954688ebbb4a07f3a3137d0d5364881
DESKTOP-4FAGDBQ\Lee Jia Xin <jxleejiaxin@gmail.com>
2023-02-07 11:38:19 +0800

    Add javaFx part 2

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,5 +1,15 @@
 package duke;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
 import duke.commands.Command;
 import duke.exception.DukeException;
 import duke.parser.Parser;
@@ -10,20 +20,28 @@ import duke.ui.Ui;
 /**
  * Represents Duke
  */
-public class Duke {
+public class Duke extends Application {
 
-    /** Default file path which Duke will attempt to access to load duke.task.Task List */
+    /** Default file path which Duke will attempt to access to load Task List */
     private static final String PATH_TO_FILE = "data/duke.txt";
 
     private final Storage storage;
     private final Ui ui;
     private TaskList tasks;
 
+    //Application
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
+
+
     /**
-     * Constructs Duke using specified filePath
-     * @param filePath path to file where Duke will read from and save to
+     * Constructs Duke using PATH_TO_FILE
      */
-    public Duke(String filePath) {
+    public Duke() {
+        String filePath = PATH_TO_FILE;
         ui = new Ui();
         storage = new Storage(filePath);
         try {
@@ -57,8 +75,58 @@ public class Duke {
         ui.showGoodBye();
     }
 
+    @Override
+    public void start(Stage stage) {
+        //Step 1. Setting up required components
+
+        //The container for the content of the chat to scroll.
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+
+        userInput = new TextField();
+        sendButton = new Button("Send");
+
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+        scene = new Scene(mainLayout);
+
+        stage.setScene(scene);
+        stage.show();
+
+        //Step 2. Formatting the window to look as expected
+        stage.setTitle("Duke");
+        stage.setResizable(false);
+        stage.setMinHeight(600.0);
+        stage.setMinWidth(400.0);
+
+        mainLayout.setPrefSize(400.0, 600.0);
+
+        scrollPane.setPrefSize(385, 535);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
+
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+
+        userInput.setPrefWidth(325.0);
+
+        sendButton.setPrefWidth(55.0);
+
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+
+        AnchorPane.setLeftAnchor(userInput , 1.0);
+        AnchorPane.setBottomAnchor(userInput, 1.0);
+    }
+
     public static void main(String[] args) {
-        new Duke(PATH_TO_FILE).run();
+        new Duke().run();
     }
 
 
