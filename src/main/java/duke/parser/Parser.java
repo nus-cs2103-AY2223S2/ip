79a41f3e7122836aff84d1f4cd386a987532e21c
DESKTOP-4FAGDBQ\Lee Jia Xin <jxleejiaxin@gmail.com>
2023-02-07 14:53:11 +0800

    Add assertions

--- src/main/java/duke/parser/Parser.java
+++ src/main/java/duke/parser/Parser.java
@@ -67,6 +67,7 @@ public class Parser {
      * @throws DukeException if user input is invalid.
      */
     public static Command parseFromUser(String fullCommand) throws DukeException {
+        assert !fullCommand.isBlank() ;
         String[] splitCommand = fullCommand.split(" ", 2);
         String keyword = splitCommand[0];
         switch (keyword) {
