3dfcd21dfc006fb5438035bd2662307db952ad55
Gloria Zhang Xinhui <glozxi@gmail.com>
2023-02-07 19:49:51 +0800

    Improve code quality

--- src/main/java/duke/uitext/UiText.java
+++ src/main/java/duke/uitext/UiText.java
@@ -4,19 +4,6 @@ package duke.uitext;
  * Class that deals with interactions with the user.
  */
 public class UiText {
-
-    /**
-     * Prints a greeting.
-     */
-    public static String getGreeting() {
-        return "Hello, I am Duke.\n"
-                + "What can I do for you?";
-    }
-
-    /**
-     * Prints a goodbye.
-     */
-    public static String sayGoodbye() {
-        return "Goodbye. I hope to see you again.";
-    }
+    public static final String GREETING = "Hello, I am Duke.\n"
+            + "What can I do for you?";
 }
