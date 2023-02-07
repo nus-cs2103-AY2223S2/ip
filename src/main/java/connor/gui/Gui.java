e1f902226b130767df6560abe60ff0d29421ca93
lywich <lywichcy@gmail.com>
2023-02-07 21:07:58 +0800

    Abstracted logic from main method.
    Separated logic from main method to improve readability.

--- src/main/java/connor/gui/Gui.java
+++ src/main/java/connor/gui/Gui.java
@@ -22,6 +22,7 @@ public class Gui extends Application  {
     private ScrollPane scrollPane;
     private VBox dialogContainer;
     private TextField userInput;
+    private Button sendButton;
     private final Image user = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
     private final Image duke = new Image(this.getClass().getResourceAsStream("/images/Connor.png"));
     private Connor connor;
@@ -49,54 +50,48 @@ public class Gui extends Application  {
         );
     }
 
-    @Override
-    public void start(Stage stage) {
+    private void initializeAnchorPlane() {
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+        AnchorPane.setLeftAnchor(userInput , 1.0);
+        AnchorPane.setBottomAnchor(userInput, 1.0);
+    }
 
-        //The container for the content of the chat to scroll.
+    private Scene initializeScene() {
         scrollPane = new ScrollPane();
         dialogContainer = new VBox();
         scrollPane.setContent(dialogContainer);
-
         userInput = new TextField();
-        Button sendButton = new Button("Send");
-
+        sendButton = new Button("Send");
         AnchorPane mainLayout = new AnchorPane();
         mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+        mainLayout.setPrefSize(400.0, 600.0);
+        userInput.setPrefWidth(325.0);
+        sendButton.setPrefWidth(55.0);
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+        return new Scene(mainLayout);
+    }
 
-        Scene scene = new Scene(mainLayout);
-
+    private void initializeStage(Stage stage) {
+        Scene scene = initializeScene();
         stage.setScene(scene);
         stage.show();
-
-        connor = new Connor();
         stage.setTitle("Connor");
         stage.setResizable(false);
         stage.setMinHeight(600.0);
         stage.setMinWidth(400.0);
+    }
 
-        mainLayout.setPrefSize(400.0, 600.0);
-
+    private void initializeScrollPane() {
         scrollPane.setPrefSize(385, 535);
         scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
         scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-
         scrollPane.setVvalue(1.0);
         scrollPane.setFitToWidth(true);
+    }
 
-        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
-
-        userInput.setPrefWidth(325.0);
-
-        sendButton.setPrefWidth(55.0);
-
-        AnchorPane.setTopAnchor(scrollPane, 1.0);
-
-        AnchorPane.setBottomAnchor(sendButton, 1.0);
-        AnchorPane.setRightAnchor(sendButton, 1.0);
-
-        AnchorPane.setLeftAnchor(userInput , 1.0);
-        AnchorPane.setBottomAnchor(userInput, 1.0);
-
+    private void initiateEventLoop() {
         sendButton.setOnMouseClicked((event) -> {
             dialogContainer.getChildren().add(getDialogLabel(userInput.getText()));
             userInput.clear();
@@ -107,11 +102,8 @@ public class Gui extends Application  {
             userInput.clear();
         });
 
-        //Scroll down to the end every time dialogContainer's height changes.
         dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
 
-        initialGreet();
-
         sendButton.setOnMouseClicked((event) -> {
             handleUserInput();
         });
@@ -119,6 +111,15 @@ public class Gui extends Application  {
         userInput.setOnAction((event) -> {
             handleUserInput();
         });
+    }
 
+    @Override
+    public void start(Stage stage) {
+        connor = new Connor();
+        initializeStage(stage);
+        initializeScrollPane();
+        initializeAnchorPlane();
+        initialGreet();
+        initiateEventLoop();
     }
 }
