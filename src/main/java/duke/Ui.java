7209ba90dd097870074d1b247dceaf5b527378a5
Filbert Phang <fillberth@gmail.com>
2023-02-08 16:14:37 +0800

    remove redundant imports in Ui

--- src/main/java/duke/Ui.java
+++ src/main/java/duke/Ui.java
@@ -1,10 +1,7 @@
 package duke;
 
-import java.util.Scanner;
-
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
