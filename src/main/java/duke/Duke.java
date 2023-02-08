53e72c6cab5877f3f9ce1b96a6b44db8152b18db
kynhan <kynhan123@gmail.com>
2023-02-08 17:16:03 +0800

    Started GUI

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -2,10 +2,23 @@ package duke;
 
 import java.util.Scanner;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.scene.layout.Region;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 /**
  * Main runner class for the Duke program.
  */
-public class Duke {
+public class Duke extends Application {
     private Scanner scanner;
     private String horizontalLine = "************************";
     private TaskList taskList;
@@ -13,6 +26,7 @@ public class Duke {
     private Storage storage;
     private Ui ui;
     private Parser parser;
+    private DukeGui dukeGui;
 
     public Duke() {
         scanner = new Scanner(System.in);
@@ -21,12 +35,21 @@ public class Duke {
         parser = new Parser();
         taskList = new TaskList(storage);
         ui = new Ui(taskList);
+        dukeGui = new DukeGui();
     }
 
     public static void main(String[] args) {
         Duke duke = new Duke();
         duke.run();
     }
+
+    @Override
+    public void start(Stage stage) {
+        Duke duke = new Duke();
+
+        dukeGui.start(stage);
+    }
+
 
     /**
      * Runs the Duke program
---
+++ src/main/java/duke/DukeGui.java
@@ -0,0 +1,110 @@
+package duke;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.scene.layout.Region;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class DukeGui {
+
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
+
+    private Image user = new Image(this.getClass().getResourceAsStream("/images/wall_e.png"));
+    private Image duke = new Image(this.getClass().getResourceAsStream("/images/eve.png"));
+
+    public void start(Stage stage) {
+        // Creating window
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+
+        userInput = new TextField();
+        sendButton = new Button("Send");
+
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+        scene = new Scene(mainLayout);
+
+        // Formatting window
+        stage.setTitle("Duke");
+        stage.setResizable(false);
+        stage.setMinWidth(450.0);
+        stage.setMinHeight(500.0);
+
+        mainLayout.setPrefSize(450.0, 500.0);
+
+        scrollPane.setPrefSize(450, 435);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
+
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+        userInput.setPrefWidth(385.0);
+        sendButton.setPrefWidth(55.0);
+
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+
+        AnchorPane.setLeftAnchor(userInput , 1.0);
+        AnchorPane.setBottomAnchor(userInput, 1.0);
+
+        sendButton.setOnMouseClicked((event) -> {
+            handleUserInput();
+        });
+
+        userInput.setOnAction((event) -> {
+            handleUserInput();
+        });
+
+        //Scroll down to the end every time dialogContainer's height changes.
+        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
+
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
+    }
+
+    /**
+     * Creates a label with the specified text and adds it to the dialog container
+     *
+     * @param text String containing text to add
+     * @return a label with the specified text that has word wrap enabled.
+     */
+    private Label getDialogLabel(String text) {
+        Label textToAdd = new Label(text);
+        textToAdd.setWrapText(true);
+
+        return textToAdd;
+    }
+
+    private void handleUserInput() {
+        Label userText = new Label(userInput.getText());
+        Label dukeText = new Label(getResponse(userInput.getText()));
+        dialogContainer.getChildren().addAll(
+                DialogBox.getUserDialog(userText, new ImageView(user)),
+                DialogBox.getDukeDialog(dukeText, new ImageView(duke))
+        );
+        userInput.clear();
+    }
+
+    private String getResponse(String input) {
+        return "Duke heard: " + input;
+    }
+
+}
