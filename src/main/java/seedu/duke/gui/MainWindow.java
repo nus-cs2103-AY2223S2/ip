12d3d9293b2683393befde594f863cbda877fdea
FredericChow00 <ruihui00.chow@gmail.com>
2023-02-08 10:30:47 +0800

    Modify build.gradle for creation of new JAR file
    Previous JAR file was outdated
    
    Changed the mainClassName so that the newly incorporated GUI using JavaFX can be included in the JAR file.
    
    Allows users to:
    * Refer to GUI for easier usage of Duke.
    * Type on text box in GUI instead of CLI commands.

--- src/main/java/seedu/duke/gui/MainWindow.java
+++ src/main/java/seedu/duke/gui/MainWindow.java
@@ -1,7 +1,6 @@
 package seedu.duke.gui;
 
-import javafx.scene.paint.ImagePattern;
-import javafx.scene.shape.Circle;
+
 import seedu.duke.Duke;
 import seedu.duke.Ui;
 
@@ -12,7 +11,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.util.Duration;
