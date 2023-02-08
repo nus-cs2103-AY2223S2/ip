0418e3212d0ad6d4f4947c9feaa4b5390b22a084
kayyenl <yenyenlee9@gmail.com>
2023-02-08 21:29:05 +0800

    Implement assert check to make sure commands are properly processed @ parser.java, using gradle to run

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -61,8 +61,7 @@ public class Duke extends Application {
             scrollPane.setContent(dialogContainer);
             mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
 
-            int tester = 1;
-            assert false;
+
             //initializing of scene
             scene = new Scene(mainLayout);
             stage.setScene(scene);
