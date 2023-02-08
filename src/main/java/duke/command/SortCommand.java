54d51474fa8f8d7283ce6c8d97127d11141c7943
bojie3 <xubojie2013@gmail.com>
2023-02-08 16:05:41 +0800

    Add javadocs for sortCommand class

--- src/main/java/duke/command/SortCommand.java
+++ src/main/java/duke/command/SortCommand.java
@@ -5,11 +5,20 @@ import duke.storage.Storage;
 import duke.tasklist.TaskList;
 import duke.ui.Ui;
 
-import java.util.Collections;
-
-
+/**
+ * Class to exectue the command to sort
+ */
 public class SortCommand extends Command {
 
+    /**
+     * Sorts the task in tasklist, and then update the task in data.txt
+     *
+     * @param tl Tasklist to sort
+     * @param ui Ui to give user output after sorting
+     * @param storage Storage which will be updated during sorting
+     * @return Tasks in sorted order
+     * @throws DukeException If there is error in sorting
+     */
     @Override
     public String execute(TaskList tl, Ui ui, Storage storage) throws DukeException {
         tl.sortTaskList(storage);
