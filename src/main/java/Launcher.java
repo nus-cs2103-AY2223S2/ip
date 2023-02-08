55a759f06b4c5302a3212ff03b6170765c2c2e7d
lxz333 <e0909010@u.nus.edu>
2023-02-08 19:07:19 +0800

    Add fxml

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -2,6 +2,6 @@ import javafx.application.Application;
 
 public class Launcher {
     public static void main(String[] args) {
-        Application.launch(Execute.class, args);
+        Application.launch(Main.class, args);
     }
 }
