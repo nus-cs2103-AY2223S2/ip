9e1c43d8f10337a5e4be835f10e1b78aa80ab811
DESKTOP-JD6N7BR\Tiger <oliverloo09@gmail.com>
2023-02-09 16:51:05 +0800

    UI implemented

---
+++ src/main/java/duke/TaskList.java
@@ -0,0 +1,34 @@
+package com.example.demo1;
+
+import java.util.ArrayList;
+
+// class TaskList - handles lists of Task objects using an ArrayList
+public class TaskList {
+    private ArrayList<Task> tasks;
+
+    public TaskList(ArrayList<Task> t) {
+        this.tasks = t;
+    }
+
+    public TaskList() {
+        this.tasks = new ArrayList<Task>();
+    }
+
+    public String displayTasks() {
+        String s = "";
+        if (tasks.isEmpty()) {
+            s += "    No tasks";
+        } else {
+            for (int i = 0; i < tasks.size(); i++) {
+                Task task = tasks.get(i);
+                s += "    " + (i + 1) + ". " + task;
+                s += i != tasks.size() ? "\n" : "";
+            }
+        }
+        return s;
+    }
+
+    public ArrayList<Task> getTasks() {
+        return this.tasks;
+    }
+}
---
+++ src/main/java/duke/Todo.java
@@ -0,0 +1,19 @@
+package com.example.demo1;
+
+// class To do - Type of task with just a description
+public class Todo extends Task {
+
+    public Todo(String description) {
+        super(description);
+    }
+
+    @Override
+    public String toSavedString() {
+        return "T | " + super.toSavedString();
+    }
+
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
+}
