9b4f6d8afc727c40b2c3f196f4bb75e66b0d3946
Gibson <gibsonyip@u.nus.edu>
2023-02-09 22:40:06 +0800

    Added more Java Docs

--- src/main/java/duke/Todo.java
+++ src/main/java/duke/Todo.java
@@ -1,15 +1,27 @@
 package duke;
 
 public class Todo extends Task {
+    /**
+     * Constructor method for Todo
+     * @param description task's description
+     */
     public Todo(String description) {
         super(description);
     }
 
+    /**
+     * Overridden method for print Todo task
+     * @return String output of the task
+     */
     @Override
     public String toString() {
         return "[T]" + super.toString();
     }
 
+    /**
+     * Formats the task's details for saving to file
+     * @return formatted task's details
+     */
     @Override
     public String formatForFile() {
         return "T | " + (this.isDone ? 1 : 0) + " | " + this.description + "\n";
