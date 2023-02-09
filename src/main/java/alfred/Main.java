f1a8e9812818b88e25a139bf1535e8762d8f0309
Pang Rui Wei <>
2023-02-09 17:51:54 +0800

    Fix GUI bug: Syntax error in MainWindow

--- src/main/java/alfred/Main.java
+++ src/main/java/alfred/Main.java
@@ -21,6 +21,7 @@ public class Main extends Application {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
             AnchorPane ap = fxmlLoader.load();
+            System.out.println("tet");
             Scene scene = new Scene(ap);
             stage.setTitle("Alfred");
             stage.setResizable(false);
