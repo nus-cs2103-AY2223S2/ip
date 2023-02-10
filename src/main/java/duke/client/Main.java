2f962c254a65dca52c773af7c2b84ed3296d1730
Jiahua <edures@protonmail.com>
2023-02-10 17:36:45 +0800

    Refactor to improve quality
    Some variables have short abbreviated names without much information.
    Some magic strings are present as well.
    
    Let's change the variable names to be fully spelt out and be more
    descriptive. Assign magic strings to constants represented by
    class-level members.
    
    Readers can get a clearer idea of what each of the variables mean.
    Readers can further understand the meaning behind the string constants
    used. Future changes may be more convenient as one would only have to
    alter the constants, instead of having to change them at all of the
    locations they are used at.

--- src/main/java/duke/client/Main.java
+++ src/main/java/duke/client/Main.java
@@ -1,6 +1,7 @@
 package duke.client;
 
 import java.io.IOException;
+import java.net.URL;
 
 import duke.DukeException;
 import duke.client.components.MainWindow;
@@ -16,21 +17,21 @@ import duke.Duke;
  * A GUI for Duke using FXML.
  */
 public class Main extends Application {
-
+    private static final String RESOURCE_PATH = "/view/MainWindow.fxml";
     @Override
     public void start(Stage stage) {
         try {
             Duke duke = new Duke();
-
-            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
-            AnchorPane ap = fxmlLoader.load();
-            Scene scene = new Scene(ap);
+            URL mainWindowResource = Main.class.getResource(RESOURCE_PATH);
+            FXMLLoader fxmlLoader = new FXMLLoader(mainWindowResource);
+            AnchorPane anchorPane = fxmlLoader.load();
+            Scene scene = new Scene(anchorPane);
             stage.setScene(scene);
             fxmlLoader.<MainWindow>getController().setDuke(duke);
             fxmlLoader.<MainWindow>getController().initialize();
             stage.show();
-        } catch (IOException | DukeException e) {
-            e.printStackTrace();
+        } catch (IOException | DukeException exception) {
+            exception.printStackTrace();
         }
     }
 }
