a5198beae419ee5d6dde6280cd5a56e816a37597
Shi Jiaao <e0372500@u.nus.edu>
2023-02-10 19:00:23 +0800

    Add basic GUI

---
+++ src/main/java/duke/Launcher.java
@@ -0,0 +1,12 @@
+package duke;
+
+import javafx.application.Application;
+
+/**
+ * This class launches Duke using JavaFX. Class template taken from CS2103.
+ */
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(Main.class, args);
+    }
+}
