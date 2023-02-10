1084cfb25185b15c0181f2f212ae42e2ccc43986
benjamin-wee <benjaminwee20@gmail.com>
2023-02-10 14:12:22 +0800

    add stylesheet

--- src/main/java/duke/MainWindow.java
+++ src/main/java/duke/MainWindow.java
@@ -24,7 +24,7 @@ public class MainWindow extends AnchorPane {
     private Duke duke;
     private Ui ui;
 
-    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/Sasuke.png"));
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/sukuna.png"));
     private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/gojo2.png"));
 
     @FXML
