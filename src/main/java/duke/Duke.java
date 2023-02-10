3fe71624a770c1808168e94ff7ed2e8b918b83d5
runoutofit <runoutofit@gmail.com>
2023-02-10 16:19:32 +0800

    add coding standards

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -2,17 +2,17 @@ package duke;
 import java.io.FileNotFoundException;
 
 public class Duke {
-    private final Storage storage;
-    private final Ui ui;
+    private final Storage STORAGE;
+    private final Ui UI;
     private TaskList tasks;
 
     public Duke(String filePath) throws DukeException {
-        ui = new Ui();
-        storage = new Storage(filePath);
+        UI = new Ui();
+        STORAGE = new Storage(filePath);
         try {
-            tasks = new TaskList(storage.load());
+            tasks = new TaskList(STORAGE.load());
         } catch (DukeException e) {
-            ui.showError();
+            UI.showError();
             tasks = new TaskList();
         } catch (FileNotFoundException e) {
             throw new DukeException(e.toString());
@@ -20,20 +20,20 @@ public class Duke {
     }
 
     public void run(){
-        ui.showWelcome();
-        String cmd = ui.readCommand();
+        UI.showWelcome();
+        String cmd = UI.readCommand();
         boolean isTerminated = false;
         while (!isTerminated) {
             if (!cmd.equals("bye")) {
                 try {
                     Parser.parse(cmd, tasks);
-                    cmd = ui.readCommand();
+                    cmd = UI.readCommand();
                 } catch (DukeException e) {
                     System.out.println("something went wrong.");
                 }
             } else {
                 isTerminated = true;
-                ui.showBye();
+                UI.showBye();
             }
         }
     }
