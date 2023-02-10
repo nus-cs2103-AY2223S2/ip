d7fc3a5b590cc6d1c3708633247509852ac18b3c
chia-yh <0.chiayuhong.0@gmail.com>
2023-02-11 00:17:51 +0800

    Fix some checkstyle issues

--- src/main/java/book/gui/MainWindow.java
+++ src/main/java/book/gui/MainWindow.java
@@ -2,7 +2,6 @@ package book.gui;
 
 import book.Book;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
