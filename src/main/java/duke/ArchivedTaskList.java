392b0a60479651c528e5905ff0c255e71750cb10
Carmen <carmenang12@gmail.com>
2023-02-12 00:06:06 +0800

    Add Archive Function
    User can now archive all tasks and deleted tasks will go to the archive list. This is so that the user can obtain have a clean slate in their main task list.

---
+++ src/main/java/duke/ArchivedTaskList.java
@@ -0,0 +1,15 @@
+package duke;
+import duke.task.Task;
+
+import java.util.ArrayList;
+
+public class ArchivedTaskList extends TaskList{
+
+    public ArchivedTaskList(ArrayList<Task> arrayList) {
+        super(arrayList);
+    }
+
+    public void addDeletedTask(Task task) {
+        arrayList.add(task);
+    }
+}
