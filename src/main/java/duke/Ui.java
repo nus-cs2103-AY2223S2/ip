5a4e3cc52594d41ae29c85e22fb9af9c159027d7
jweeyh <jweeyh@gmail.com>
2023-02-11 22:44:03 +0800

    Change Ui to fit the coding standards
    There was text present that contained the default String drawing of Duke, which was relevant only in CLI, but has since become redundant with the new GUI.
    
    This currently unused text is thus deleted in accordance with the coding standard to remove unused and outdated code.
    
    Let's try to find other instances in the code which could be simplified and refactored.

--- src/main/java/duke/Ui.java
+++ src/main/java/duke/Ui.java
@@ -6,11 +6,6 @@ import java.util.ArrayList;
 public class Ui implements Serializable{
 
     private ArrayList<String> statementList = new ArrayList<>();
-    private String logo = " ____        _        \n"
-            + "|  _ \\ _   _| | _____ \n"
-            + "| | | | | | | |/ / _ \\\n"
-            + "| |_| | |_| |   <  __/\n"
-            + "|____/ \\__,_|_|\\_\\___|\n";
 
     public Ui() {
     }
