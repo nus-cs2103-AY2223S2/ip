dd0e99f89df3e2575af3b611cf027544360dc1a4
weekiat-douze <lumweekiat98@gmail.com>
2023-02-11 15:20:26 +0800

    Complete Level-10

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -17,7 +17,7 @@ public class Duke {
 
     private Storage storage;
     private TaskList tasks;
-    private Ui ui;
+
 
     /**
      * Constructor of Duke class.
@@ -26,76 +26,60 @@ public class Duke {
      */
     public Duke(String filePath) {
         storage = new Storage(filePath);
-        ui = new Ui();
         try {
             tasks = new TaskList(storage.load());
         } catch (DukeException dukeException) {
-            ui.showLoadingError();
             tasks = new TaskList();
         }
     }
 
-    /**
-     *  Duke Application's entry point.
-     *  Creates a Duke object and calls <code>run()</code>.
-     *
-     * @param args command line arguments.
-     */
-    public static void main(String[] args) {
-        new Duke("data/duke.txt").run();
-    }
 
     /**
      * Starts Duke application.
      * Sets up persistent storage and user interface. Begins to receive and process input form user.
      * Terminates based on user input.
      */
-    public void run() {
+    public String getResponse(String userInput) {
+        if (userInput.equals("bye")) {
+            return "Bye. Hope to see you again soon!";
+        }
 
-        ui.showWelcome();
-
-        String userInput = null;
-        while (!(userInput = ui.readCommand()).equals("bye")) {
-            String[] splitInput = userInput.split(" ", 2);
-            try {
-                Command inputCommand = Parser.validateCommand(splitInput[0]);
-                switch (inputCommand) {
-                case LIST:
-                    listTask();
-                    break;
-                case MARK:
-                    markTask(splitInput);
-                    break;
-                case UNMARK:
-                    unmarkTask(splitInput);
-                    break;
-                case DELETE:
-                    deleteTask(splitInput);
-                    break;
-                case TODO:
-                    addTodo(splitInput);
-                    break;
-                case DEADLINE:
-                    addDeadline(splitInput);
-                    break;
-                case EVENT:
-                    addEvent(splitInput);
-                    break;
-                case FIND:
-                    findTask(splitInput);
-                    break;
-                default:
-                    throw new DukeException(" ☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
-                } //end of switch-case
-
-            } catch (DukeException dukeException) {
-                ui.dukeSpeak(dukeException.getMessage());
-            }
-        } // end of while-loop
-
-
-        ui.dukeSpeak("Bye. Hope to see you again soon!");
-        ui.close();
+        String[] splitInput = userInput.split(" ", 2);
+        try {
+            Command inputCommand = Parser.validateCommand(splitInput[0]);
+            String outputMsg = "";
+            switch (inputCommand) {
+            case LIST:
+                outputMsg = listTask();
+                break;
+            case MARK:
+                outputMsg = markTask(splitInput);
+                break;
+            case UNMARK:
+                outputMsg = unmarkTask(splitInput);
+                break;
+            case DELETE:
+                outputMsg = deleteTask(splitInput);
+                break;
+            case TODO:
+                outputMsg = addTodo(splitInput);
+                break;
+            case DEADLINE:
+                outputMsg = addDeadline(splitInput);
+                break;
+            case EVENT:
+                outputMsg = addEvent(splitInput);
+                break;
+            case FIND:
+                outputMsg = findTask(splitInput);
+                break;
+            default:
+                throw new DukeException("...Hmm... I'm not sure what that means...");
+            } //end of switch-case
+            return outputMsg;
+        } catch (DukeException dukeException) {
+            return dukeException.getMessage();
+        }
     }
 
 
@@ -106,7 +90,7 @@ public class Duke {
      * @param input User input separated into {command, description}.
      * @throws DukeException If user input is invalid, e.g. empty description.
      */
-    private void addTodo(String[] input) throws DukeException {
+    private String addTodo(String[] input) throws DukeException {
         Todo tempTodo = Parser.parseTodo(input);
         String saveString = "T | 0 | " + tempTodo.getDescription();
 
@@ -115,22 +99,22 @@ public class Duke {
 
         String message = "Got it. I've added this task:\n " + tempTodo.toString();
         message += "\nNow you have " + tasks.numTasks() + " tasks in the list.";
-        ui.dukeSpeak(message);
+        return message;
     }
 
 
-    private void addDeadline(String[] input) throws DukeException {
+    private String addDeadline(String[] input) throws DukeException {
         Deadline tempDeadline = Parser.parseDeadline(input);
         String saveString = "D | 0 | " + tempDeadline.getDescription() + " | " + tempDeadline.getByDate();
         storage.saveEntry(saveString);
         tasks.addTask(tempDeadline);
         String message = "Got it. I've added this task:\n " + tempDeadline.toString();
         message += "\nNow you have " + tasks.numTasks() + " tasks in the list.";
-        ui.dukeSpeak(message);
+        return message;
 
     }
 
-    private void addEvent(String[] input) throws DukeException {
+    private String addEvent(String[] input) throws DukeException {
         Event tempEvent = Parser.parseEvent(input);
         String saveString = "E | 0 | " + tempEvent.getDescription() + " | "
                 + tempEvent.getStartDate() + ">" + tempEvent.getEndDate();
@@ -139,24 +123,24 @@ public class Duke {
         tasks.addTask(tempEvent);
         String message = "Got it. I've added this task:\n " + tempEvent.toString();
         message += "\nNow you have " + tasks.numTasks() + " tasks in the list.";
-        ui.dukeSpeak(message);
+        return message;
 
     }
 
-    private void listTask() {
+    private String listTask() {
         String message = "Here are the tasks in your list:";
         message += tasks.getTaskList();
-        ui.dukeSpeak(message);
+        return message;
     }
 
-    private void findTask(String[] input) throws DukeException {
+    private String findTask(String[] input) throws DukeException {
         String searchString = Parser.parseSearch(input);
         String message = "Here are the matching tasks in your list:";
         message += tasks.getMatchingTasks(searchString);
-        ui.dukeSpeak(message);
+        return message;
     }
 
-    private void markTask(String[] input) throws DukeException {
+    private String markTask(String[] input) throws DukeException {
 
         int taskNum = Parser.parseMarkTask(input);
         if (taskNum > tasks.numTasks() || taskNum <= 0) {
@@ -169,12 +153,12 @@ public class Duke {
         tasks.markTask(taskNum - 1);
 
         String message = "Nice! I've marked this task as done:\n " + oneTask.toString();
-        ui.dukeSpeak(message);
+        return message;
     }
 
-    private void unmarkTask(String[] input) throws DukeException {
-
+    private String unmarkTask(String[] input) throws DukeException {
         int taskNum = Parser.parseUnmarkTask(input);
+
         if (taskNum > tasks.numTasks() || taskNum <= 0) {
             throw new DukeException(" ☹ OOPS!!! The item number is out of range.");
         }
@@ -184,10 +168,10 @@ public class Duke {
         Task oneTask = tasks.getTask(taskNum - 1);
         tasks.unmarkTask(taskNum - 1);
         String message = "OK! I've marked this task as not done yet:\n " + oneTask.toString();
-        ui.dukeSpeak(message);
+        return message;
     }
 
-    private void deleteTask(String[] input) throws DukeException {
+    private String deleteTask(String[] input) throws DukeException {
         int taskNum = Parser.parseDeleteTask(input);
         if (taskNum > tasks.numTasks() || taskNum <= 0) {
             throw new DukeException(" ☹ OOPS!!! The item number is out of range.");
@@ -199,7 +183,7 @@ public class Duke {
         tasks.deleteTask(taskNum - 1);
         String message = "Noted. I've removed this task:\n " + delTask.toString();
         message += "\nNow you have " + tasks.numTasks() + " tasks in the list.";
-        ui.dukeSpeak(message);
+        return message;
     }
 
 
