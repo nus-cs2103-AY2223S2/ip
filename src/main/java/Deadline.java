80f20aa8dc000630022e6d516ffbbf244484fb49
jeraldkiew <jeraldkiew007@gmail.com>
2023-02-11 20:28:33 +0800

    add Level-8 dates and Times to Deadline and Event

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,13 +1,24 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
-    private final String by;
+
+    private static final DateTimeFormatter INPUT_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+    private static final DateTimeFormatter OUTPUT_FORMAT = DateTimeFormatter.ofPattern("MMM dd yyyy HH:mm");
+    protected LocalDateTime by;
 
     public Deadline(String str, String by) {
         super(str);
-        this.by = by;
+        this.by = LocalDateTime.parse(by, INPUT_FORMAT);
+    }
+
+    public LocalDateTime getBy() {
+        return by;
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + "(by: " + this.by + ")";
+        return "[D]" + super.toString() + "(by: " + this.by.format(OUTPUT_FORMAT)
+                + ")";
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,14 +1,23 @@
-public class Event extends Task{
-    private final String from, to;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Event extends Task {
+
+    private static final DateTimeFormatter INPUT_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+    private static final DateTimeFormatter OUTPUT_FORMAT = DateTimeFormatter.ofPattern("MMM dd yyyy HH:mm");
+    protected LocalDateTime from;
+    protected LocalDateTime to;
 
     public Event(String str, String from, String to) {
         super(str);
-        this.from = from;
-        this.to = to;
+        this.from = LocalDateTime.parse(from, INPUT_FORMAT);
+        this.to = LocalDateTime.parse(to, INPUT_FORMAT);
+        ;
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + "(from: " + this.from + " to: " + this.to + ")";
+        return "[E]" + super.toString() + "(from: " + this.from.format(OUTPUT_FORMAT) + " to: "
+                + this.to.format(OUTPUT_FORMAT) + ")";
     }
 }
