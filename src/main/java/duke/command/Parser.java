1d1d7aaef594d7914ecf99ad8a2e637ccfea7fd7
Valerie Tan <valerietanhx@gmail.com>
2023-02-11 13:48:46 +0800

    Improve code quality

--- src/main/java/duke/command/Parser.java
+++ src/main/java/duke/command/Parser.java
@@ -9,79 +9,120 @@ import duke.exception.DukeInvalidCommandException;
  */
 public class Parser {
 
+    /**
+     * Parses the command.
+     * @param line User input.
+     * @return An appropriate command.
+     * @throws DukeException If user input is invalid.
+     */
     public static Command parse(String line) throws DukeException {
         String[] chunked = line.split(" ");
         String command = chunked[0];
         if (command.equals("bye")) {
-            return new ByeCommand();
+            return handleBye();
         } else if (command.equals("mark")) {
-            if (chunked.length == 1) {
-                throw new DukeInvalidCommandException();
-            } else {
-                return new MarkCommand(Integer.parseInt(chunked[1]));
-            }
+            return handleMark(chunked);
         } else if (command.equals("unmark")) {
-            if (chunked.length == 1) {
-                throw new DukeInvalidCommandException();
-            } else {
-                return new UnmarkCommand(Integer.parseInt(chunked[1]));
-            }
+            return handleUnmark(chunked);
         } else if (command.equals("delete")) {
-            if (chunked.length == 1) {
-                throw new DukeInvalidCommandException();
-            } else {
-                return new DeleteCommand(Integer.parseInt(chunked[1]));
-            }
+            return handleDelete(chunked);
         } else if (command.equals("list")) {
-            return new ListCommand();
+            return handleList();
         } else if (command.equals("find")) {
-            if (chunked.length != 2) {
-                throw new DukeInvalidCommandException();
-            } else {
-                return new FindCommand(chunked[1]);
-            }
+            return handleFind(chunked);
         } else if (command.equals("todo")) {
-            String rest = line.substring(4).trim();
-            if (rest.isBlank()) {
-                throw new DukeInvalidCommandException();
-            } else {
-                return new AddToDoCommand(rest);
-            }
+            return handleToDo(line);
         } else if (command.equals("deadline")) {
-            String rest = line.substring(8).trim();
-            String pattern = ".+ /by [0-9][0-9]/[0-9][0-9]/[0-9][0-9][0-9][0-9] [0-9][0-9][0-9][0-9]";
-
-            if (!rest.matches(pattern)) {
-                throw new DukeInvalidCommandException();
-            }
-
-            String desc = rest.substring(0, rest.indexOf(" /by"));
-            String[] by = rest.substring(rest.indexOf("/by")).split(" ");
-            String date = by[1];
-            String time = by[2];
-
-            return new AddDeadlineCommand(desc, date, time);
+            return handleDeadline(line);
 
         } else if (command.equals("event")) {
-            String rest = line.substring(5).trim();
-            String pattern = ".+ /from [0-9][0-9]/[0-9][0-9]/[0-9][0-9][0-9][0-9] [0-9][0-9][0-9][0-9]"
-                    + " /to [0-9][0-9]/[0-9][0-9]/[0-9][0-9][0-9][0-9] [0-9][0-9][0-9][0-9]";
-
-            if (!rest.matches(pattern)) {
-                throw new DukeInvalidCommandException();
-            }
-
-            String desc = rest.substring(0, rest.indexOf(" /from"));
-            String[] fromTo = rest.substring(rest.indexOf("/from")).split(" ");
-            String dateFrom = fromTo[1];
-            String timeFrom = fromTo[2];
-            String dateTo = fromTo[4];
-            String timeTo = fromTo[5];
-
-            return new AddEventCommand(desc, dateFrom, timeFrom, dateTo, timeTo);
+            return handleEvent(line);
 
         } else {
             throw new DukeInvalidCommandException("Huh? Sorry I don't know what this means :(");
         }
+    }
+
+    static Command handleBye() {
+        return new ByeCommand();
+    }
+    static Command handleMark(String[] chunked) throws DukeException {
+        if (chunked.length == 1) {
+            throw new DukeInvalidCommandException();
+        } else {
+            return new MarkCommand(Integer.parseInt(chunked[1]));
+        }
+    }
+
+    static Command handleUnmark(String[] chunked) throws DukeException {
+        if (chunked.length == 1) {
+            throw new DukeInvalidCommandException();
+        } else {
+            return new UnmarkCommand(Integer.parseInt(chunked[1]));
+        }
+    }
+
+    static Command handleDelete(String[] chunked) throws DukeException {
+        if (chunked.length == 1) {
+            throw new DukeInvalidCommandException();
+        } else {
+            return new DeleteCommand(Integer.parseInt(chunked[1]));
+        }
+    }
+
+    static Command handleList() {
+        return new ListCommand();
+    }
+
+    static Command handleFind(String[] chunked) throws DukeException {
+        if (chunked.length != 2) {
+            throw new DukeInvalidCommandException();
+        } else {
+            return new FindCommand(chunked[1]);
+        }
+    }
+
+    static Command handleToDo(String line) throws DukeException {
+        String rest = line.substring(4).trim();
+        if (rest.isBlank()) {
+            throw new DukeInvalidCommandException();
+        } else {
+            return new AddToDoCommand(rest);
+        }
+    }
+
+    static Command handleDeadline(String line) throws DukeException {
+        String rest = line.substring(8).trim();
+        String pattern = ".+ /by [0-9][0-9]/[0-9][0-9]/[0-9][0-9][0-9][0-9] [0-9][0-9][0-9][0-9]";
+
+        if (!rest.matches(pattern)) {
+            throw new DukeInvalidCommandException();
+        }
+
+        String desc = rest.substring(0, rest.indexOf(" /by"));
+        String[] by = rest.substring(rest.indexOf("/by")).split(" ");
+        String date = by[1];
+        String time = by[2];
+
+        return new AddDeadlineCommand(desc, date, time);
+    }
+
+    static Command handleEvent(String line) throws DukeException {
+        String rest = line.substring(5).trim();
+        String pattern = ".+ /from [0-9][0-9]/[0-9][0-9]/[0-9][0-9][0-9][0-9] [0-9][0-9][0-9][0-9]"
+                + " /to [0-9][0-9]/[0-9][0-9]/[0-9][0-9][0-9][0-9] [0-9][0-9][0-9][0-9]";
+
+        if (!rest.matches(pattern)) {
+            throw new DukeInvalidCommandException();
+        }
+
+        String desc = rest.substring(0, rest.indexOf(" /from"));
+        String[] fromTo = rest.substring(rest.indexOf("/from")).split(" ");
+        String dateFrom = fromTo[1];
+        String timeFrom = fromTo[2];
+        String dateTo = fromTo[4];
+        String timeTo = fromTo[5];
+
+        return new AddEventCommand(desc, dateFrom, timeFrom, dateTo, timeTo);
     }
 }
