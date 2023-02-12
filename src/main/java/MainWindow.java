e8411bafe951a51be0ddf6df3bc07e5554d26a87
kennethk-1201 <kiangjiaming@gmail.com>
2023-02-13 01:39:08 +0800

    Add user guide

--- src/main/java/MainWindow.java
+++ src/main/java/MainWindow.java
@@ -1,6 +1,5 @@
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
