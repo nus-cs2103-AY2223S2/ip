0fecaa2b4609abf9d1c74e6b3650727e14a8a551
yanlings <e0774088@u.nus.edu>
2023-02-12 16:45:56 +0800

    GUI does not return anything when user does mark, unmark or delete.
    It needs to be change because the GUI would seem unresponsive if it did not return anything.
    
    3 new methods have been added into UI.java
    Add stringMark()
    Add stringUnmark()
    Add stringDelete()
    
    By separating the methods, it improves readability of the code, and allows the GUI to respond accordingly.

--- src/main/java/duke/Launcher.java
+++ src/main/java/duke/Launcher.java
@@ -1,3 +1,5 @@
+package duke;
+
 import duke.Duke;
 import duke.DukeException;
 import duke.Main;
@@ -9,7 +11,7 @@ import java.io.IOException;
  * A constructor to run the GUI
  */
 public class Launcher{
-    public static void main(String[] args) throws DukeException, IOException {
+    public static void main(String[] args) throws DukeException {
         Application.launch(Main.class,args);
 
     }
