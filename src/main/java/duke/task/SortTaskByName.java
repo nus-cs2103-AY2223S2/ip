5290b775be4a46250b9425220928d3761d8e6288
John Chia Heng Kit <programmingwithjohn@gmail.com>
2023-02-12 17:34:47 +0800

    Delete A class, add javadocs to SortTaskByName

--- src/main/java/duke/task/SortTaskByName.java
+++ src/main/java/duke/task/SortTaskByName.java
@@ -2,6 +2,10 @@ package duke.task;
 
 import java.util.Comparator;
 
+
+/**
+ * Sorts task in alphabetical order.
+ */
 public class SortTaskByName implements Comparator<Task> {
 
     public int compare(Task a, Task b)
