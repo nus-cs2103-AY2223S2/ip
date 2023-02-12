58cdf16639b1b386b6f0c89808e1f58da4ddc16c
ARPspoofing <mohdreyaaz99@gmail.com>
2023-02-12 17:14:33 +0800

    Remove unnecessary code from Main

--- src/main/java/duke/Main.java
+++ src/main/java/duke/Main.java
@@ -5,11 +5,6 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -23,29 +18,11 @@ public class Main extends Application {
     public void start(Stage stage) {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/InitialWindow.fxml"));
-            //AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(fxmlLoader.load());
-            //scene.getStylesheets().add(this.getClass().getResource("/style.css").toExternalForm());
-
-            //FXMLLoader fxmlLoaderInitial = new FXMLLoader(Main.class.getResource("/view/InitialWindow.fxml"));
-            //BorderPane bp = fxmlLoaderInitial.load();
-            //Scene scene2 = new Scene(fxmlLoaderInitial.load());
-            //bp.getChildren().
-            //Button button = scene2.lookup("#goButton");
-
-            /*
-            Label label1 = new Label("Welcome to Manchester United");
-            Button button = new Button();
-            button.setOnAction(e -> stage.setScene(scene));
-            VBox layout1 = new VBox(20);
-            layout1.getChildren().addAll(label1, button);
-            Scene scene2 = new Scene(layout1, 200, 200);
-             */
 
             stage.setScene(scene);
             stage.setTitle("Duke");
-            //FXMLLoader fxmlLoader2 = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
-            //fxmlLoader2.<MainWindow>getController().setDuke(duke);
+            
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
