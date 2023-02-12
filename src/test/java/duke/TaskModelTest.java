6de19d8df3874818533ce520d732f423b8c052b6
Jayanth-Balasubramanian <jayanth.balasubramanian1@gmail.com>
2023-02-12 19:18:00 +0800

    Write tests to test public methods of TaskModel and TaskView

--- src/test/java/duke/TaskModelTest.java
+++ src/test/java/duke/TaskModelTest.java
@@ -1,2 +1,31 @@
-package duke;public class TaskModelTest {
+package duke;
+import duke.model.Task;
+import duke.model.TaskModel;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+public class TaskModelTest {
+    TaskModel model;
+    @Test
+    public void testAddTask() {
+        model = new TaskModel();
+        // delete any previously existing tasks
+        List<Task> currentTaskList = model.getTasks();
+        for (Task task: currentTaskList) {
+            model.deleteTask(task);
+        }
+        model.createTask("task 1");
+        model.createTask("task 2");
+        model.createTask("task 3");
+        for (Task task: model.getTasks()) {
+            System.out.println(task);
+        }
+        assertEquals(3, model.getTasks().size());
+        assertTrue(model.getTask(0).toString().contains("task 1"));
+        assertTrue(model.getTask(1).toString().contains("task 2"));
+        assertTrue(model.getTask(2).toString().contains("task 3"));
+    }
 }
--- src/test/java/duke/TaskViewTest.java
+++ src/test/java/duke/TaskViewTest.java
@@ -1,10 +1,61 @@
 package duke;
 
-import duke.model.TaskModel;
+import duke.model.Task;
+import duke.model.ToDo;
+import duke.view.TaskView;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class PresenterTest {
-    TaskModel taskModel = new TaskModel();
-    View
+public class TaskViewTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
+
+    @BeforeEach
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+    }
+
+    @Test
+    public void testShowMessage() {
+        TaskView view = new TaskView();
+        view.showMessage("Hello World!");
+        assertEquals("____________________________________________________________\n" +
+                "Hello World!\n" +
+                "____________________________________________________________\n", outContent.toString());
+    }
+
+    @Test
+    public void testShowError() {
+        TaskView view = new TaskView();
+        view.showError("An error has occurred");
+        assertEquals("An error has occurred\n", errContent.toString());
+    }
+
+    @Test
+    public void testRenderTasks() {
+        TaskView view = new TaskView();
+        List<Task> tasks = Arrays.asList(new ToDo("Task 1"), new ToDo("Task 2"));
+        view.renderTasks(tasks);
+        assertEquals("____________________________________________________________\n" +
+                "Here are the tasks in your list:\n" +
+                "1. [T][ ] Task 1\n" +
+                "2. [T][ ] Task 2\n" +
+                "____________________________________________________________\n", outContent.toString());
+    }
 }
