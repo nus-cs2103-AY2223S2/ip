53d908ec96bca8b9417bd6c560d38aab5feac17d
UnwilledTangent <tinsean.work@gmail.com>
2023-02-12 18:15:41 +0800

    Do JavaFX Tutorial Part 1

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -10,14 +10,26 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
-public class Duke {
+public class Duke extends Application {
     private static Storage storage = new Storage();
     private static TaskList tasks = new TaskList();
     private static Ui ui = new Ui();
     private static Parser parser = new Parser();
 
-    public static void main(String[] args) {
-        ui.welcomeMessage();
-        parser.getInput();
+//    public static void main(String[] args) {
+//        ui.welcomeMessage();
+//        parser.getInput();
+//    }
+
+    @Override
+    public void start(Stage stage) {
+        // Creating a new Label control
+        Label helloWorld = new Label("Hello World!");
+
+        // Setting the scene to be our Label
+        Scene scene = new Scene(helloWorld);
+
+        stage.setScene(scene);
+        stage.show();
     }
 }
