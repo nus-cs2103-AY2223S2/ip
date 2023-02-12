de1611f286810b9057de46d1efce07904563ea56
WesleyBLDC <wezzdc@gmail.com>
2023-02-12 23:53:00 +0800

    implemented GUI and commands for GUI

---
+++ src/main/java/willy/Launcher.java
@@ -0,0 +1,12 @@
+package willy;
+
+import javafx.application.Application;
+
+/**
+ * Represents the launcher class
+ */
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(DukeApp.class, args);
+    } 
+}
