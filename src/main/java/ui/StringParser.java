d632d7a9f7a31e88c6043bfea53bca248ba10ca0
GlenLim <glenlimyf@gmail.com>
2023-02-12 19:42:54 +0800

    Remove unnecessary comment

--- src/main/java/ui/StringParser.java
+++ src/main/java/ui/StringParser.java
@@ -43,40 +43,7 @@ public class StringParser {
                 throw new DukeException("Sorry, I don't know that command");
         }
     }
-    //    if (wordArr[0].equals("mark")) {
-    //        int index = Integer.parseInt(wordArr[1]);
-    //        return new MarkCommand(index - 1);
-    //    } else if (wordArr[0].equals("unmark")) {
-    //        int index = Integer.parseInt(wordArr[1]);
-    //        return new UnmarkCommand(index - 1);
-    //    } else if (answer.equals("bye")) {
-    //        return new ByeCommand();
-    //    } else if (wordArr[0].equals("delete")) {
-    //        int index = Integer.parseInt(wordArr[1]);
-    //        return new DeleteCommand(index - 1);
-    //    } else if (answer.equals("list")) {
-    //        return new ListCommand();
-    //    } else if (wordArr[0].equals("todo")) {
-    //        this.parseTodo(answer);
-    //        String desc = wordArr[1];
-    //        return new TodoCommand(desc);
-    //    } else if (wordArr[0].equals("deadline")) {
-    //        String desc = wordArr[1].split(" /by ")[0];
-    //        String by = wordArr[1].split(" /by ")[1];
-    //        return new DeadlineCommand(desc, by);
-    //    } else if (wordArr[0].equals("event")) {
-    //        String desc = wordArr[1].split(" ", 2)[0];
-    //        String from = wordArr[1].split(" /from ")[1].split(" /to ")[0];
-    //        String to = wordArr[1].split(" /to ", 2)[1];
-    //        return new EventCommand(desc, from, to);
-    //    } else if (wordArr[0].equals("find")) {
-    //        String keyWord = wordArr[1];
-    //        return new FindCommand(keyWord);
-    //    } else{
-    //        throw new DukeException("Sorry, I don't know that command");
-    //    }
-    //}
-
+    
     private Commands getCommand(String input) throws DukeException{
         try {
             Commands command = Commands.valueOf(input.toUpperCase());
