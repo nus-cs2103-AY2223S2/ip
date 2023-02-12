bfaf1e2b949dd50585db25dee0d1c337f9a0fd83
GlenLim <glenlimyf@gmail.com>
2023-02-12 14:08:57 +0800

    Add assertions

--- src/main/java/command/DeleteCommand.java
+++ src/main/java/command/DeleteCommand.java
@@ -8,6 +8,7 @@ import ui.Ui;
 public class DeleteCommand extends Command{
     private int index;
     public DeleteCommand(int index){
+        assert index > -1: "Index should not be negative";
         this.index = index;
     }
     public String execute(TaskList taskList, Storage storage, Ui ui){
