265a58297ae04449ec3782b3ae74b316d7e4dbb3
sembcorpp <e0772507@u.nus.edu>
2023-02-13 01:04:45 +0800

    Making the GUI for Jane Level-10

--- src/main/java/Jane/TaskList.java
+++ src/main/java/Jane/TaskList.java
@@ -1,7 +1,5 @@
 package jane;
 
-import jane.task.Task;
-
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,7 +7,7 @@ import java.util.List;
 public class TaskList {
         protected static ArrayList<jane.task.Task> tasks;
 
-        TaskList(ArrayList<jane.task.Task> tasks) {
+        public TaskList(ArrayList<jane.task.Task> tasks) {
             this.tasks = tasks;
         }
 
@@ -19,7 +17,8 @@ public class TaskList {
      * @throws JaneException if input format is wrong
      * @throws JaneException if input is invalid
      */
-        public static void useCommand(String output) {
+        public String useCommand(String output) {
+            StringBuilder finalOutput = new StringBuilder();
             if (output.equals("bye")) {
                 List<String> currentList = new ArrayList<>();
                 for (jane.task.Task t : tasks) {
@@ -27,9 +26,11 @@ public class TaskList {
                 }
                 try {
                     jane.Storage.updateList(tasks);
+                    return "BYE! see you soon";
+
                 } catch (Exception err) {
-                    System.out.println("cannot save list");
                     err.printStackTrace();
+                    return ("cannot save list");
                 }
             } else if (output.startsWith("find")) {
                 String[] s = output.split(" ");
@@ -37,87 +38,97 @@ public class TaskList {
                 String action = s[1];
                 for (jane.task.Task i : tasks) {
                     if (i.description.contains(action)) {
-                        System.out.println(i.toString());
+                        finalOutput.append(i.toString());
 
                     }
                 }
-
-
+                return finalOutput.toString();
             } else if (output.startsWith("mark")) {
                 String[] s = output.split(" ");
                 int num = Integer.parseInt(s[1]);
                 try {
-                    System.out.println("Nice! I've marked this task as done");
+                    finalOutput.append("Nice! I've marked this task as done");
                     jane.task.Task n = tasks.get(num - 1);
                     n.changeState(true);
-                    System.out.println(n.toString());
+                    finalOutput.append(n.toString());
+                    return finalOutput.toString();
                 } catch (Exception err) {
                     err.printStackTrace();
-                    System.out.println("Number out of index");
+                    return ("Number out of index");
                 }
 
             } else if (output.equals("todo") || output.equals("deadline") || output.equals("event")) {
-                System.out.println("Please specify the task to be done :(((");
+                finalOutput.append("Please specify the task to be done :(((");
+                return finalOutput.toString();
             } else if (output.startsWith("todo")) {
                 jane.task.Todo todo = jane.Parser.parserT(output, tasks.size());
                 tasks.add(todo);
-                System.out.println(todo.toString());
+                finalOutput.append(todo.toString());
+                return finalOutput.toString();
             } else if (output.startsWith("deadline")) {
                 jane.task.Deadline d = jane.Parser.parserD(output, tasks.size() + 1);
                 tasks.add(d);
-                System.out.println(d.toString());
+                finalOutput.append(d.toString());
             } else if (output.startsWith("event")) {
                 String des = output.substring(6);
                 String[] s = des.split("/");
                 if (s.length == 1) {
-                    System.out.println("Please specify when the event is :(((");
+                    finalOutput.append("Please specify when the event is :(((");
+                    return finalOutput.toString();
 
                 }
                 String[] start = s[1].substring(5).split(" ");
                 LocalDateTime startE = LocalDateTime.parse(String.format("%sT%s", start[0], start[1]));
                 //here i am assuming an event only lasts 1 day since the day it starts is the day it ends
                 LocalDateTime end = LocalDateTime.parse(String.format("%sT%s", start[0], s[2]));
-                jane.task.Event e = jane.Parser.parserE(output, tasks.size()+1);
+                jane.task.Event e = jane.Parser.parserE(output, tasks.size() + 1);
                 tasks.add(e);
-                System.out.println(e.toString());
+                finalOutput.append(e.toString());
+                return finalOutput.toString();
             } else if (output.startsWith("unmark")) {
                 String[] s = output.split(" ");
                 int num = Integer.parseInt(s[1]);
                 try {
-                    System.out.println("OK, I've marked this task as not done yet");
+                    finalOutput.append("OK, I've marked this task as not done yet" + "\n");
                     jane.task.Task n = tasks.get(num - 1);
                     n.changeState(false);
-                    System.out.println(n.toString());
+                    finalOutput.append(n.toString());
+                    return finalOutput.toString();
                 } catch (Exception err) {
-                    System.out.println("Number out of index");
+                    finalOutput.append("Number out of index");
                     err.printStackTrace();
+                    return finalOutput.toString();
                 }
+
             } else if (output.startsWith("delete")) {
                 String[] s = output.split(" ");
                 int num = Integer.parseInt(s[1]);
                 try {
-                    System.out.println("Noted. I've removed this task:");
+                    finalOutput.append("Noted. I've removed this task:" + "\n");
                     jane.task.Task n = tasks.get(num - 1);
-                    System.out.println(n.toString());
+                    finalOutput.append(n.toString());
                     for (int j = num; j < tasks.size(); j++) {
                         jane.task.Task t = tasks.get(j);
                         t.changeNum();
                     }
 
                     tasks.remove(n);
-                    System.out.println("You now have " + tasks.size() + " tasks");
+                    finalOutput.append("You now have " + tasks.size() + " tasks");
+                    return finalOutput.toString();
                 } catch (Exception err) {
-                    System.out.println("Number out of index");
+                    finalOutput.append("Number out of index");
                     err.printStackTrace();
                 }
+                return finalOutput.toString();
             } else if (output.equals("list")) {
                 for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println(tasks.get(i).toString());
-                }
+                    String s1 = tasks.get(i).toString() + "\n";
+                    finalOutput.append(s1);
 
-            } else {
-                System.out.println("Im sorry I don't understand what you mean :((");
+                }
+                return finalOutput.toString();
             }
+        return "Sorry I don't understand what do you mean :((";
         }
     }
 
---
+++ src/main/java/Jane/javafx/MainWindow.java
@@ -0,0 +1,73 @@
+package jane.javafx;
+
+import jane.Jane;
+import jane.Storage;
+import jane.TaskList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+
+
+/**
+ * Controller for MainWindow. Provides the layout for the other controls.
+ */
+public class MainWindow extends AnchorPane {
+    @FXML
+    private ScrollPane scrollPane;
+    @FXML
+    private VBox dialogContainer;
+    @FXML
+    private TextField userInput;
+    @FXML
+    private Button sendButton;
+
+    private Jane jane;
+
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/UserImg.png"));
+    private Image janeImage = new Image(this.getClass().getResourceAsStream("/images/Jane.png"));
+
+    private TaskList tasks;
+    @FXML
+    public void initialize() {
+        scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
+        this.tasks = this.load();
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        dialogContainer.getChildren().addAll(
+                DialogBox.getDukeDialog(logo, janeImage)
+        );
+    }
+
+    public void setJane(Jane j) {
+        jane = j;
+    }
+
+    /**
+     * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
+     * the dialog container. Clears the user input after processing.
+     */
+
+    private TaskList load() {
+        Storage.createDir();
+        TaskList tasks = new TaskList(Storage.loadList());
+        return tasks;
+    }
+    @FXML
+    private void handleUserInput() {
+        String input = userInput.getText();
+        String response = tasks.useCommand(input);
+        dialogContainer.getChildren().addAll(
+                DialogBox.getUserDialog(input, userImage),
+                DialogBox.getDukeDialog(response, janeImage)
+        );
+        userInput.clear();
+    }
+}
