ec5fc91b9c18c8bd32fec2bccbc6cda3cb8f40b0
Valerie Tan <valerietanhx@gmail.com>
2023-02-12 12:11:00 +0800

    Make display picture a circle

--- src/main/java/DialogBox.java
+++ src/main/java/DialogBox.java
@@ -11,6 +11,7 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.shape.Circle;
 
 /**
  * An example of a custom control using FXML.
@@ -36,6 +37,8 @@ public class DialogBox extends HBox {
 
         dialog.setText(text);
         displayPicture.setImage(img);
+        Circle clip = new Circle(40, 40, 40);
+        this.displayPicture.setClip(clip);
     }
 
     /**
