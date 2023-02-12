fa3d9139d21be744704828c76d19b2551099202a
ginloy <liyongleow@gmail.com>
2023-02-12 22:35:48 +0800

    Remove unused imports

--- src/main/java/io/gui/DialogueBox.java
+++ src/main/java/io/gui/DialogueBox.java
@@ -14,7 +14,6 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
-import javafx.scene.layout.VBox;
 import javafx.scene.shape.Circle;
 
 /**
