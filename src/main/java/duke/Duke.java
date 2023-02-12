70d7987984630f8e5902a03b90f1d6b0d4e2a39f
UnwilledTangent <tinsean.work@gmail.com>
2023-02-12 18:06:51 +0800

    Delete production files

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -5,12 +5,17 @@ import duke.parser.Parser;
 import duke.storage.Storage;
 import duke.task.TaskList;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
 public class Duke {
     private static Storage storage = new Storage();
     private static TaskList tasks = new TaskList();
     private static Ui ui = new Ui();
     private static Parser parser = new Parser();
-//    Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         ui.welcomeMessage();
         parser.getInput();
