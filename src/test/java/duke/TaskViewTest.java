2751b38d3ee10de497ac520c24455ffac6a7bada
Jayanth-Balasubramanian <jayanth.balasubramanian1@gmail.com>
2023-02-12 19:13:49 +0800

    Add JUnit tests for TaskModel and TaskView

---
+++ src/test/java/duke/TaskViewTest.java
@@ -0,0 +1,10 @@
+package duke;
+
+import duke.model.TaskModel;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PresenterTest {
+    TaskModel taskModel = new TaskModel();
+    View
+}
