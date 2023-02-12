4437c3726a28cea8855d99e198f9afd3b706d87f
Wei Jie <ongweijie7@gmail.com>
2023-02-13 01:53:00 +0800

    Add a few catch statements in Parser and edited the error message thrown.
    Added catch statements to catch exceptions because they could be thrown when user gave missing commands.
    
    Exceptions that occur when there is a missing time/date or wrong format given will be caught and I will throw a Missing DukeException.
    
    It is done this way since this error is specific to a missing command.
    
    The error message for Missing exception is also improved as I realised the missing command is also used when there is no information on the task name given

--- src/main/java/duke/functions/Parser.java
+++ src/main/java/duke/functions/Parser.java
@@ -1,4 +1,5 @@
 package duke.functions;
+import duke.dukeexceptions.DukeException;
 import duke.dukeexceptions.Missing;
 import duke.dukeexceptions.WrongKeyWord;
 import duke.task.Deadline;
@@ -9,6 +10,7 @@ import duke.task.ToDo;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 /**
  * Class that understands and handles commands.
@@ -30,82 +32,87 @@ public class Parser {
     public static String understandInput(String input, String[] inputArr, TaskList tasks, Storage storage)
             throws Missing, WrongKeyWord {
         String reply;
+        Task newTask;
+        String type;
 
-        Task newTask = null;
-        String type = inputArr[0];
+        try {
+            if (inputArr[0].equals("bye")) {
+                reply = "bye";
+            } else if (inputArr[0].equals("find")) {
+                reply = tasks.findTask(inputArr[1]);
+            } else if (inputArr[0].equals("list")) {
+                reply = tasks.printTasks();
+            } else if (inputArr[0].equals("mark")) {
+                int index = Integer.valueOf(inputArr[1]) - 1;
+                Task task = tasks.getTask(index);
+                reply =  task.setDone();
+            } else if (inputArr[0].equals("unmark")) {
+                int index = Integer.valueOf(inputArr[1]) - 1;
+                Task task = tasks.getTask(index);
+                reply = task.setNotDone();
+            } else if (inputArr[0].equals("delete")) {
+                int index = Integer.valueOf(inputArr[1]) - 1;
+                Task task = tasks.deleteTask(index);
+                reply = task.removeTask();
 
-        if (inputArr[0].equals("bye")) {
-            reply = "bye";
-        } else if (inputArr[0].equals("find")) {
-            reply = tasks.findTask(inputArr[1]);
-        } else if (inputArr[0].equals("list")) {
-            reply = tasks.printTasks();
-        } else if (inputArr[0].equals("mark")) {
-            int index = Integer.valueOf(inputArr[1]) - 1;
-            Task task = tasks.getTask(index);
-            reply =  task.setDone();
-        } else if (inputArr[0].equals("unmark")) {
-            int index = Integer.valueOf(inputArr[1]) - 1;
-            Task task = tasks.getTask(index);
-            reply = task.setNotDone();
-        } else if (inputArr[0].equals("delete")) {
-            int index = Integer.valueOf(inputArr[1]) - 1;
-            Task task = tasks.deleteTask(index);
-            reply = task.removeTask();
+            } else if (inputArr[0].equals("todo")) {
+                input = input.replaceFirst("todo", "");
+                if (input.equals("")) {
+                    throw new Missing("");
+                }
+                newTask = new ToDo(input);
+                type = "T";
 
-        } else if (inputArr[0].equals("todo")) {
-            input = input.replaceFirst("todo", "");
-            if (input.equals("")) {
-                throw new Missing("");
+                tasks.addTask(newTask);
+                reply = newTask.toString();
+                storage.writeToFile(input, type);
+
+            } else if (inputArr[0].equals("deadline")) {
+
+                input = input.replaceFirst("deadline", "");
+                if (input.equals("")) {
+                    throw new Missing("");
+                }
+                String[] taskDate = input.split("/by ");
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/MM/yyyy HHmm");
+                LocalDateTime dateTime = LocalDateTime.parse(taskDate[1], formatter);
+                String dateTimeString = dateTime.format(DateTimeFormatter.ofPattern("MMM d yyyy hhmma"));
+                input = taskDate[0] + "(by:" + dateTimeString + ")";
+                newTask = new Deadline(input);
+                type = "D";
+
+                tasks.addTask(newTask);
+                reply = newTask.toString();
+                storage.writeToFile(input, type);
+            } else if (inputArr[0].equals("event")) {
+                input = input.replaceFirst("event", "");
+                if (input.equals("")) {
+                    throw new Missing("");
+                }
+                String[] taskDate2 = input.split("/from ");
+                String[] fromTo = taskDate2[1].split(" /to ");
+                DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("d/MM/yyyy HHmm");
+                LocalDateTime fromDateTime = LocalDateTime.parse(fromTo[0], formatter2);
+                LocalDateTime toDateTime = LocalDateTime.parse(fromTo[1], formatter2);
+
+                String fromString = fromDateTime.format(DateTimeFormatter.ofPattern("MMM d yyyy hhmma"));
+                String toString = toDateTime.format(DateTimeFormatter.ofPattern("MMM d yyyy hhmma"));
+                input = taskDate2[0] + "(from: " + fromString + " ";
+                input = input + "to: " + toString + ")";
+
+                newTask = new Event(input);
+                type = "E";
+
+                tasks.addTask(newTask);
+                reply = newTask.toString();
+                storage.writeToFile(input, type);
+            } else {
+                throw new WrongKeyWord("");
             }
-            newTask = new ToDo(input);
-            type = "T";
-
-            tasks.addTask(newTask);
-            reply = newTask.toString();
-            storage.writeToFile(input, type);
-
-        } else if (inputArr[0].equals("deadline")) {
-
-            input = input.replaceFirst("deadline", "");
-            if (input.equals("")) {
-                throw new Missing("");
-            }
-            String[] taskDate = input.split("/by ");
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/MM/yyyy HHmm");
-            LocalDateTime dateTime = LocalDateTime.parse(taskDate[1], formatter);
-            String dateTimeString = dateTime.format(DateTimeFormatter.ofPattern("MMM d yyyy hhmma"));
-            input = taskDate[0] + "(by:" + dateTimeString + ")";
-            newTask = new Deadline(input);
-            type = "D";
-
-            tasks.addTask(newTask);
-            reply = newTask.toString();
-            storage.writeToFile(input, type);
-        } else if (inputArr[0].equals("event")) {
-            input = input.replaceFirst("event", "");
-            if (input.equals("")) {
-                throw new Missing("");
-            }
-            String[] taskDate2 = input.split("/from ");
-            String[] fromTo = taskDate2[1].split(" /to ");
-            DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("d/MM/yyyy HHmm");
-            LocalDateTime fromDateTime = LocalDateTime.parse(fromTo[0], formatter2);
-            LocalDateTime toDateTime = LocalDateTime.parse(fromTo[1], formatter2);
-
-            String fromString = fromDateTime.format(DateTimeFormatter.ofPattern("MMM d yyyy hhmma"));
-            String toString = toDateTime.format(DateTimeFormatter.ofPattern("MMM d yyyy hhmma"));
-            input = taskDate2[0] + "(from: " + fromString + " ";
-            input = input + "to: " + toString + ")";
-
-            newTask = new Event(input);
-            type = "E";
-
-            tasks.addTask(newTask);
-            reply = newTask.toString();
-            storage.writeToFile(input, type);
-        } else {
-            throw new WrongKeyWord("");
+        } catch (DateTimeParseException e) {
+            throw new Missing("");
+        } catch(ArrayIndexOutOfBoundsException e) {
+            throw new Missing("");
         }
         return reply;
     }
