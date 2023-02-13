a28339fefe964848b02a0005c9e043a026438d24
anchengyang <anchengyang72@gmail.com>
2023-02-13 22:24:30 +0800

    Add find command unit test

---
+++ src/test/java/duke/commands/FindCommandTest.java
@@ -0,0 +1,35 @@
+package duke.commands;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import duke.DukeException;
+import duke.Storage;
+import duke.TaskList;
+import duke.tasks.Deadline;
+
+
+public class FindCommandTest {
+    @Test
+    public void executeTest() {
+        TaskList tl = new TaskList();
+        Storage storage = new Storage("taskListTest.txt");
+        try {
+            Deadline d = new Deadline("homework",
+                    "2024-10-20 2359");
+            tl.add(d);
+        } catch (DukeException e) {
+            e.getMessage();
+        }
+        FindCommand findCommand1 = new FindCommand("find home");
+        FindCommand findCommand2 = new FindCommand("find a");
+
+        assertEquals(findCommand1.execute(tl, storage),
+                "Here are the tasks in your list:\n"
+                        + "    1. [D] [   ]  homework\n"
+                        + " (by: Oct 20 2024 11:59 PM)\n\n");
+        assertEquals(findCommand2.execute(tl, storage),
+                "There are no tasks.");
+    }
+}
