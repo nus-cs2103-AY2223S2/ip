914347b1eb416f98b3a3c36e8e7fa1f544808118
Muhammad Fahim Tajwar <fahim.tajwar@u.nus.edu>
2023-02-13 22:12:12 +0800

    Add Javadoc comments to duke.exceptions and duke.tasks package classes.

--- src/main/java/duke/exceptions/DukeException.java
+++ src/main/java/duke/exceptions/DukeException.java
@@ -1,6 +1,13 @@
 package duke.exceptions;
 
+/**
+ * Parent class for all Duke-internal exceptions.
+ */
 public class DukeException extends Exception {
+  /**
+   * Constructor for a Duke exception.
+   * @param msg The exception message.
+   */
   public DukeException(String msg) {
     super(msg);
   }
--- src/main/java/duke/tasks/Task.java
+++ src/main/java/duke/tasks/Task.java
@@ -1,28 +1,61 @@
 package duke.tasks;
 
-public class Task {
-    public final String description;
-    public boolean isDone;
+/**
+ * An abstract task class that encapsulates the description of any type of Task.
+ */
+abstract public class Task {
+    private final String description;
+    private boolean isDone;
 
-    public Task(String description) {
+    /**
+     * Constructor for a Task object. Only to be used by subclasses.
+     * @param description The name of the Task.
+     */
+    protected Task(String description) {
         this.description = description;
         this.isDone = false;
     }
 
-    public String getStatusIcon() {
-        return (isDone ? "X" : " "); // mark done task with X
+    private String getStatusIcon() {
+        return (isDone ? "X" : " ");
     }
 
+    /**
+     * Getter for description of a Task.
+     * @return Description as a String.
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * Getter for the done status of a Task.
+     * @return True for done tasks, false for undone tasks.
+     */
+    public boolean isDone() {
+        return isDone;
+    }
+
+    /**
+     * Marks this Task as done.
+     */
     public void mark() {
       this.isDone = true;
     }
-    
+
+    /**
+     * (Un)Marks this task as done.
+     */
     public void unmark() {
       this.isDone = false;
     }
 
+    /**
+     * String representation of any Task, in the format <code>[doneIcon] description</code>.
+     * @return String representation of this Task.
+     */
     @Override
-    public String toString() {    
+    public String toString() {
       return "[" + this.getStatusIcon() + "] " + this.description;
     }
 }
