5a83b9180977480605400f2ae7dec4ecd505d7bf
CloudHill <rh.cloudhill@gmail.com>
2023-02-14 00:05:10 +0800

    Fix find command count

--- src/main/java/sam/command/FindCommand.java
+++ src/main/java/sam/command/FindCommand.java
@@ -18,10 +18,11 @@ public class FindCommand extends Command {
     @Override
     public Result execute(TaskList tasks, Storage storage) {
         List<Task> list = tasks.findTasks(args);
-        if (list.isEmpty()) {
+        int count = (int) list.stream().filter(t -> t != null).count();
+        if (count == 0) {
             result.addMessage(Dialog.FIND_EMPTY.getDialog());
         } else {
-            result.addMessage(String.format(Dialog.FIND.getDialog(), list.size()));
+            result.addMessage(String.format(Dialog.FIND.getDialog(), count));
             result.addTasks(list);
         }
         return result;
