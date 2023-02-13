5f3b19524a5b106ff86f550a157b13be7c3bdcbf
Jia Wei Lee <b33b33o11@gmail.com>
2023-02-13 16:11:45 +0800

    test: add tests for `UiPrinter`

---
+++ src/test/java/view/UiPrinterTest.java
@@ -0,0 +1,79 @@
+package view;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import membot.view.UiPrinter;
+
+public class UiPrinterTest {
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @Test
+    public void printlnEmptyTest() {
+        UiPrinter p = new UiPrinter(System.out::println);
+
+        String[] s1 = {};
+
+        p.println(s1);
+        assertEquals(
+                "",
+                this.outputStreamCaptor.toString().trim()
+        );
+    }
+
+    @Test
+    public void printlnTest() {
+        UiPrinter p = new UiPrinter(System.out::println);
+
+        String[] s1 = {"test", "1", "2", "3"};
+
+        p.println(s1);
+        assertEquals(
+                String.join("\n", s1),
+                this.outputStreamCaptor.toString().trim()
+        );
+    }
+
+    @Test
+    public void printlnErrorEmptyTest() {
+        UiPrinter p = new UiPrinter(System.out::println);
+
+        String[] s1 = {};
+
+        p.printlnError(s1);
+        assertEquals(
+                "",
+                this.outputStreamCaptor.toString().trim()
+        );
+    }
+
+    @Test
+    public void printlnErrorTest() {
+        UiPrinter p = new UiPrinter(System.out::println);
+
+        String[] s1 = {"testerror", "1", "2", "3"};
+
+        p.printlnError(s1);
+        assertEquals(
+                "*Error*\n" + String.join("\n", s1),
+                this.outputStreamCaptor.toString().trim()
+        );
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.setOut(standardOut);
+    }
+}
