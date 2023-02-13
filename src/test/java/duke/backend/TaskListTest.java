41b42eff5fc475456e8f629a8563d828534d857f
Muhammad Fahim Tajwar <fahim.tajwar@u.nus.edu>
2023-02-13 22:22:36 +0800

    Modify TaskListTest to clear tasks.txt before testing.

--- src/test/java/duke/backend/TaskListTest.java
+++ src/test/java/duke/backend/TaskListTest.java
@@ -4,33 +4,48 @@ import duke.tasks.Task;
 import duke.tasks.Todo;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class TaskListTest {
+class TaskListTest {
+    /**
+     * Empties the tasks.txt before testing.
+     * @throws IOException
+     */
+    private void clearTasks() throws IOException {
+        FileWriter fw = new FileWriter(new File("./tasks.txt"));
+        fw.write("");
+        fw.flush();
+        fw.close();
+    }
     @Test
     public void markTask_UnmarkedTodoIndex_MarkedTodo() throws IOException {
+        clearTasks();
         Todo t = new Todo("Hello");
         TaskList list = new TaskList();
         list.add(t);
-        assertTrue(list.mark(0).isDone);
+        assertTrue(list.mark(0).isDone());
     }
 
     @Test
     public void unmarkTask_markedTodoIndex_unmarkedTodo() throws IOException {
+        clearTasks();
         Todo t = new Todo("Hello");
         t.mark();
         TaskList list = new TaskList();
         list.add(t);
-        assertFalse(list.unmark(0).isDone);
+        assertFalse(list.unmark(0).isDone());
     }
 
     @Test
     public void deleteTask_deleteAnIndex_TaskAtIndexDeleted() throws IOException {
-        Task t1 = new Task("Task 1");
-        Task t2 = new Task("Task 2");
-        Task t3 = new Task("Task 3");
+        clearTasks();
+        Task t1 = new Todo("Task 1");
+        Task t2 = new Todo("Task 2");
+        Task t3 = new Todo("Task 3");
         TaskList list = new TaskList();
         list.add(t1);
         list.add(t2);
