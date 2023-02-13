9cad7bade1c3a7edd641abe664e3b425d8c60bd9
eugenetangkj <dragoneugene10@gmail.com>
2023-02-13 17:23:08 +0800

    Refactor validateEvent of Parser

--- src/main/java/boo/parser/Parser.java
+++ src/main/java/boo/parser/Parser.java
@@ -241,38 +241,6 @@ public class Parser {
     }
 
     /**
-     * Validates the 'deadline' input of the user.
-     *
-     * @param rawCommand The user's raw command.
-     * @return a deadline command type if 'deadline' input is valid, else return an Exception command type.
-     */
-    private CommandType validateDeadline(String rawCommand) {
-        try {
-            //Check if task name and deadline are given in the command
-            String[] output = checkDeadlineInformation(rawCommand);
-            String taskName = output[0];
-            String deadlineOfTask = output[1];
-            DateTime.getDateTimeObject(deadlineOfTask);
-
-            //Create the command type object from the given input
-            CommandType ctDeadline = CommandType.DEADLINE;
-            ctDeadline.setTaskName(taskName);
-            ctDeadline.setDeadline(deadlineOfTask);
-            return ctDeadline;
-        } catch (BooException booException) {
-            CommandType ctException = CommandType.EXCEPTION;
-            ctException.setExceptionMessage(booException.getMessage());
-            return ctException;
-        } catch (DateTimeParseException dateTimeException) {
-            CommandType ctException = CommandType.EXCEPTION;
-            ctException.setExceptionMessage(
-                    "Please check that you entered a valid date, and that the date should be in "
-                            + "the format of\nyyyy-MM-dd hh:mm or yyyy-MM-dd.");
-            return ctException;
-        }
-    }
-
-    /**
      * Checks if the raw deadline input has the necessary fields, which include
      * task name and deadline date.
      *
@@ -318,30 +286,101 @@ public class Parser {
         return output;
     }
 
+    /**
+     * Validates the 'deadline' input of the user.
+     *
+     * @param rawCommand The user's raw command.
+     * @return a deadline command type if 'deadline' input is valid, else return an Exception command type.
+     */
+    private CommandType validateDeadline(String rawCommand) {
+        try {
+            //Check if task name and deadline are given in the command
+            String[] output = checkDeadlineInformation(rawCommand);
+            String taskName = output[0];
+            String deadlineOfTask = output[1];
+            DateTime.getDateTimeObject(deadlineOfTask);
+
+            //Create the command type object from the given input
+            CommandType ctDeadline = CommandType.DEADLINE;
+            ctDeadline.setTaskName(taskName);
+            ctDeadline.setDeadline(deadlineOfTask);
+            return ctDeadline;
+        } catch (BooException booException) {
+            CommandType ctException = CommandType.EXCEPTION;
+            ctException.setExceptionMessage(booException.getMessage());
+            return ctException;
+        } catch (DateTimeParseException dateTimeException) {
+            CommandType ctException = CommandType.EXCEPTION;
+            ctException.setExceptionMessage(
+                    "Please check that you entered a valid date, and that the date should be in "
+                            + "the format of\nyyyy-MM-dd hh:mm or yyyy-MM-dd.");
+            return ctException;
+        }
+    }
 
 
 
+    /**
+     * Checks if the raw event input has the necessary fields, which include
+     * task name, start and end dates.
+     *
+     * @param rawCommand The raw event input entered by the user.
+     * @return a string array of size 3, containing the task name, start date and end date respectively
+     * @throws BooException if the task name, the start date or the end date is empty.
+     */
+    private String[] checkEventInformation(String rawCommand) throws BooException {
+        int indexOfType = rawCommand.indexOf("event");
+        if (indexOfType + 5 > rawCommand.length() - 1) {
+            throw new BooException("The event command cannot be left blank.");
+        }
 
+        int indexOfFrom = rawCommand.indexOf("/from");
+        if (indexOfFrom == -1) {
+            throw new BooException("There seems to be a missing from date.");
+        }
 
+        int indexOfTo = rawCommand.indexOf("/to");
+        if (indexOfTo == -1) {
+            throw new BooException("There seems to be a missing to date.");
+        }
 
+        //Check taskName
+        if ((indexOfType + 6 > indexOfFrom - 1)) {
+            throw new BooException("There seems to be a missing task name.");
+        }
 
+        String taskName = rawCommand.substring(indexOfType + 6, indexOfFrom - 1);
+        if (taskName.isBlank()) {
+            throw new BooException("The task name cannot be left blank.");
+        }
 
+        //Check startDate
+        if (indexOfFrom + 6 > indexOfTo - 1) {
+            throw new BooException("There seems to be a missing start date.");
+        }
 
+        String startDate = rawCommand.substring(indexOfFrom + 6, indexOfTo - 1);
+        if (startDate.isBlank()) {
+            throw new BooException("The start date cannot be left blank.");
+        }
 
+        //Check endDate
+        if (indexOfTo + 4 > rawCommand.length() - 1) {
+            throw new BooException("There seems to be a missing end date.");
+        }
 
+        String endDate = rawCommand.substring(indexOfTo + 4);
+        if (endDate.isBlank()) {
+            throw new BooException("The end date cannot be left blank.");
+        }
 
-
-
-
-
-
-
-
-
-
-
-
-
+        //Prepare output
+        String[] output = new String[3];
+        output[0] = taskName;
+        output[1] = startDate;
+        output[2] = endDate;
+        return output;
+    }
 
 
     /**
@@ -352,59 +391,20 @@ public class Parser {
      */
     private CommandType validateEvent(String rawCommand) {
         try {
-            int indexOfType = rawCommand.indexOf("event");
-            if (indexOfType + 5 > rawCommand.length() - 1) {
-                throw new BooException("The event command cannot be left blank.");
-            }
+            //Get the task name, start and end date
+            String[] output = checkEventInformation(rawCommand);
+            String taskName = output[0];
+            String startDate = output[1];
+            String endDate = output[2];
 
-            int indexOfFrom = rawCommand.indexOf("/from");
-            if (indexOfFrom == -1) {
-                throw new BooException("There seems to be a missing from date.");
-            }
-
-            int indexOfTo = rawCommand.indexOf("/to");
-            if (indexOfTo == -1) {
-                throw new BooException("There seems to be a missing to date.");
-            }
-
-            //Check taskName
-            if ((indexOfType + 6 > indexOfFrom - 1)) {
-                throw new BooException("There seems to be a missing task name.");
-            }
-
-            String taskName = rawCommand.substring(indexOfType + 6, indexOfFrom - 1);
-            if (taskName.isBlank()) {
-                throw new BooException("The task name cannot be left blank.");
-            }
-
-            //Check startDate
-            if (indexOfFrom + 6 > indexOfTo - 1) {
-                throw new BooException("There seems to be a missing start date.");
-            }
-
-            String startDate = rawCommand.substring(indexOfFrom + 6, indexOfTo - 1);
-            if (startDate.isBlank()) {
-                throw new BooException("The start date cannot be left blank.");
-            }
-
-            //Check endDate
-            if (indexOfTo + 4 > rawCommand.length() - 1) {
-                throw new BooException("There seems to be a missing end date.");
-            }
-
-            String endDate = rawCommand.substring(indexOfTo + 4);
-            if (endDate.isBlank()) {
-                throw new BooException("The end date cannot be left blank.");
-            }
-
-            //Create new event task
+            //Check validity of duration
             Temporal start = DateTime.getDateTimeObject(startDate);
             Temporal end = DateTime.getDateTimeObject(endDate);
-
             if (!DateTime.isValidDuration(start, end)) {
                 throw new BooException("Start date must be before end date.");
             }
 
+            //Create event command
             CommandType ctEvent = CommandType.EVENT;
             ctEvent.setTaskName(taskName);
             ctEvent.setStartDate(startDate);
