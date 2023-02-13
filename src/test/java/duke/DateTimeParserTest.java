00ee98a6872fd7953ebc95893128e3f16239670b
NoyaReoT <rayonteo@gmail.com>
2023-02-13 16:23:45 +0800

    Add more test cases for DateTimeParser

--- src/test/java/duke/DateTimeParserTest.java
+++ src/test/java/duke/DateTimeParserTest.java
@@ -2,6 +2,7 @@ package duke;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeParseException;
 
@@ -10,10 +11,18 @@ import static org.junit.jupiter.api.Assertions.fail;
 
 public class DateTimeParserTest {
     @Test
+    public void parse_hyphenDate_success() {
+        try {
+            assertEquals(DateTimeParser.parse("13-08-2000"), LocalDateTime.parse("2000-08-13T00:00"));
+        } catch (DateTimeParseException e) {
+            fail();
+        }
+    }
+
+    @Test
     public void parse_forwardSlashDate_success() {
         try {
-            DateTimeParser parser = new DateTimeParser();
-            assertEquals(parser.parse("13/08/2000"), LocalDateTime.parse("2000-08-13T00:00"));
+            assertEquals(DateTimeParser.parse("13/08/2000"), LocalDateTime.parse("2000-08-13T00:00"));
         } catch (DateTimeParseException e) {
             fail();
         }
@@ -22,10 +31,39 @@ public class DateTimeParserTest {
     @Test
     public void parse_shortFormMonth_success() {
         try {
-            DateTimeParser parser = new DateTimeParser();
-            assertEquals(parser.parse("3 Dec 2000"), LocalDateTime.parse("2000-12-03T00:00"));
+            assertEquals(DateTimeParser.parse("3 Dec 2000"), LocalDateTime.parse("2000-12-03T00:00"));
         } catch (DateTimeParseException e) {
             fail();
         }
     }
+
+    @Test
+    public void parse_withTime_success() {
+        try {
+            assertEquals(DateTimeParser.parse("03-08-2000 16:00"), LocalDateTime.parse("2000-08-03T16:00"));
+        } catch (DateTimeParseException e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void dateToStorageString_testDate_success() {
+        try {
+            LocalDate testDate = DateTimeParser.parse("13-01-2012").toLocalDate();
+            assertEquals(DateTimeParser.dateToStorageString(testDate), "13-01-2012");
+        } catch (DateTimeParseException e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void dateTimeToStorageString_testDateTime_success() {
+        try {
+            LocalDateTime testDateTime = DateTimeParser.parse("13-01-2012 : 0230")
+            assertEquals(DateTimeParser.dateTimeToStorageString(testDateTime), "13-01-2012 02:30");
+        } catch (DateTimeParseException e) {
+            fail();
+        }
+    }
+
 }
