59ff456627643779effaded61b99dcc8ef5d3f4d
jnjy <jefferyng2000@gmail.com>
2023-02-14 00:20:58 +0800

    Add: UI png - chadbot screenshot

--- src/main/java/chad/Main.java
+++ src/main/java/chad/Main.java
@@ -6,6 +6,7 @@ import chad.ui.MainWindow;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -23,6 +24,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.getIcons().add(new Image(this.getClass().getResourceAsStream("/images/simpson.jpg")));
             stage.setTitle("Chad");
             stage.setResizable(false);
             fxmlLoader.<MainWindow>getController().setChad(chad);
