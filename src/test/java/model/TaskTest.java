5ffd7ac27e5d21c589398f3b3a9c5e75b283c7cb
Jia Wei Lee <b33b33o11@gmail.com>
2023-02-13 16:22:53 +0800

    style: removed unused import

--- src/test/java/model/TaskTest.java
+++ src/test/java/model/TaskTest.java
@@ -9,7 +9,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.NoSuchElementException;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
