f4a14d0e87bc85306296f7fc650876457f5dec82
Restyled.io <commits@restyled.io>
2023-02-14 13:31:44 +0000

    Restyled by clang-format

--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -12,73 +12,72 @@ import duke.command.MarkCommand;
 import duke.command.UnmarkCommand;
 
 /**
- * A parser that parses all the user text based commands into the respective commands to be executed.
+ * A parser that parses all the user text based commands into the respective
+ * commands to be executed.
  */
 public class Parser {
-    /**
-     * Checks on whether the command is recognised. If it is, a corresponding command is returned. Otherwise, an
-     * exception is throw to inform them that the command does not exit.
-     *
-     * @param fullCommand The full string test command that the user input
-     * @return The respective command that the user called for
-     */
-    public static Command parse(String fullCommand) throws DukeException {
-        String[] inputSplit = fullCommand.split(" ", 2);
-        switch (inputSplit[0]) {
-        case "bye":
-            return new ExitCommand();
-        case "list":
-            return new ListCommand();
-        case "mark":
-            checkInput(inputSplit, ErrorMsg.MARK);
-            return new MarkCommand(inputSplit[1]);
-        case "unmark":
-            checkInput(inputSplit, ErrorMsg.UNMARK);
-            return new UnmarkCommand(inputSplit[1]);
-        case "todo":
-            checkInput(inputSplit, ErrorMsg.TODO);
-            return new AddTodoCommand(inputSplit[1]);
-        case "deadline":
-            checkInput(inputSplit, ErrorMsg.DEADLINE);
-            return new AddDeadlineCommand(inputSplit[1]);
-        case "event":
-            checkInput(inputSplit, ErrorMsg.EVENT);
-            return new AddEventCommand(inputSplit[1]);
-        case "delete":
-            checkInput(inputSplit, ErrorMsg.DELETE);
-            return new DeleteCommand(inputSplit[1]);
-        case "find":
-            checkInput(inputSplit, ErrorMsg.FIND);
-            return new FindCommand(inputSplit[1]);
-        default:
-            throw new DukeException(ErrorMsg.DEFAULT.getText());
-        }
+  /**
+   * Checks on whether the command is recognised. If it is, a corresponding
+   * command is returned. Otherwise, an exception is throw to inform them that
+   * the command does not exit.
+   *
+   * @param fullCommand The full string test command that the user input
+   * @return The respective command that the user called for
+   */
+  public static Command parse(String fullCommand) throws DukeException {
+    String[] inputSplit = fullCommand.split(" ", 2);
+    switch (inputSplit[0]) {
+    case "bye":
+      return new ExitCommand();
+    case "list":
+      return new ListCommand();
+    case "mark":
+      checkInput(inputSplit, ErrorMsg.MARK);
+      return new MarkCommand(inputSplit[1]);
+    case "unmark":
+      checkInput(inputSplit, ErrorMsg.UNMARK);
+      return new UnmarkCommand(inputSplit[1]);
+    case "todo":
+      checkInput(inputSplit, ErrorMsg.TODO);
+      return new AddTodoCommand(inputSplit[1]);
+    case "deadline":
+      checkInput(inputSplit, ErrorMsg.DEADLINE);
+      return new AddDeadlineCommand(inputSplit[1]);
+    case "event":
+      checkInput(inputSplit, ErrorMsg.EVENT);
+      return new AddEventCommand(inputSplit[1]);
+    case "delete":
+      checkInput(inputSplit, ErrorMsg.DELETE);
+      return new DeleteCommand(inputSplit[1]);
+    case "find":
+      checkInput(inputSplit, ErrorMsg.FIND);
+      return new FindCommand(inputSplit[1]);
+    default:
+      throw new DukeException(ErrorMsg.DEFAULT.getText());
     }
+  }
 
-    private static void checkInput(String[] inputSplit, ErrorMsg errorMsg) throws DukeException {
-        if (inputSplit.length < 2) {
-            throw new DukeException(errorMsg.getText());
-        }
+  private static void checkInput(String[] inputSplit, ErrorMsg errorMsg)
+      throws DukeException {
+    if (inputSplit.length < 2) {
+      throw new DukeException(errorMsg.getText());
     }
+  }
 
-    private enum ErrorMsg {
-        MARK("Mark command missing list numbering."),
-        UNMARK("Unmark command missing list numbering."),
-        TODO("Todo command missing DESCRIPTION."),
-        DEADLINE("Deadline command missing DESCRIPTION."),
-        EVENT("Event command missing DESCRIPTION."),
-        DELETE("Delete command missing list numbering."),
-        FIND("Find command missing terms."),
-        DEFAULT("Sorry but I don't understand what this means.");
+  private enum ErrorMsg {
+    MARK("Mark command missing list numbering."),
+    UNMARK("Unmark command missing list numbering."),
+    TODO("Todo command missing DESCRIPTION."),
+    DEADLINE("Deadline command missing DESCRIPTION."),
+    EVENT("Event command missing DESCRIPTION."),
+    DELETE("Delete command missing list numbering."),
+    FIND("Find command missing terms."),
+    DEFAULT("Sorry but I don't understand what this means.");
 
-        private final String text;
+    private final String text;
 
-        ErrorMsg(String text) {
-            this.text = text;
-        }
+    ErrorMsg(String text) { this.text = text; }
 
-        public String getText() {
-            return text;
-        }
-    }
+    public String getText() { return text; }
+  }
 }
--- src/main/java/duke/command/AddEventCommand.java
+++ src/main/java/duke/command/AddEventCommand.java
@@ -8,49 +8,51 @@ import duke.task.Task;
 import duke.textui.TextUi;
 
 /**
- * A command that stores the command to add a new event task. The action of adding the task can be carried out when
- * called.
+ * A command that stores the command to add a new event task. The action of
+ * adding the task can be carried out when called.
  */
 public class AddEventCommand extends Command {
-    /**
-     * The DESCRIPTION, start and end date of the event task.
-     */
-    private final String data;
+  /**
+   * The DESCRIPTION, start and end date of the event task.
+   */
+  private final String data;
 
-    /**
-     * Constructor for a command to add new event task.
-     *
-     * @param data The DESCRIPTION, start and end date of the event task
-     */
-    public AddEventCommand(String data) {
-        super(AvailableCommands.ADD_EVENT);
-        this.data = data;
+  /**
+   * Constructor for a command to add new event task.
+   *
+   * @param data The DESCRIPTION, start and end date of the event task
+   */
+  public AddEventCommand(String data) {
+    super(AvailableCommands.ADD_EVENT);
+    this.data = data;
+  }
+
+  /**
+   * Adds a new event task into the task list.
+   * The start and end dates are filtered out. If they exist, then an event task
+   * will be created. Otherwise, an exception would be thrown stating that
+   * either the start or end dates were not specified.
+   *
+   * @param taskList List of tasks that are stored
+   * @param ui       UI to deal with the visual output
+   * @param storage  Storage to deal with input and output of data
+   */
+  @Override
+  public String execute(TaskList taskList, TextUi ui, Storage storage)
+      throws DukeException {
+    String[] splitData1 = data.split(" /from ", 2);
+    if (splitData1.length < 2) {
+      throw new DukeException("Event command format error. Missing /from");
     }
 
-    /**
-     * Adds a new event task into the task list.
-     * The start and end dates are filtered out. If they exist, then an event task will be created. Otherwise, an
-     * exception would be thrown stating that either the start or end dates were not specified.
-     *
-     * @param taskList List of tasks that are stored
-     * @param ui       UI to deal with the visual output
-     * @param storage  Storage to deal with input and output of data
-     */
-    @Override
-    public String execute(TaskList taskList, TextUi ui, Storage storage) throws DukeException {
-        String[] splitData1 = data.split(" /from ", 2);
-        if (splitData1.length < 2) {
-            throw new DukeException("Event command format error. Missing /from");
-        }
-
-        String[] splitData2 = splitData1[1].split(" /to ", 2);
-        if (splitData2.length < 2) {
-            throw new DukeException("Event command format error. Missing /to");
-        }
-
-        Task event = new Event(splitData1[0], splitData2[0], splitData2[1]);
-        taskList.addTask(event);
-
-        return ui.showAddTask(event.toString(), taskList.size());
+    String[] splitData2 = splitData1[1].split(" /to ", 2);
+    if (splitData2.length < 2) {
+      throw new DukeException("Event command format error. Missing /to");
     }
+
+    Task event = new Event(splitData1[0], splitData2[0], splitData2[1]);
+    taskList.addTask(event);
+
+    return ui.showAddTask(event.toString(), taskList.size());
+  }
 }
