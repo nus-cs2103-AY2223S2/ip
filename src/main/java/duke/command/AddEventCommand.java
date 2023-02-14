d5624188948342f4de3203fb7a2b38b07de274cc
Liu Muchen <liumuchen1@gmail.com>
2023-02-15 00:39:30 +0800

    Add assert statements
    Adding assertions allows us to be sure that the correct flow of
    code is happening during runtime.
    
    Let's add assertions to code statements that check whether the split
    operation really resulted in split of string into 2. Moreover,
    checking if resources are null for images.
    
    All of these prevents exceptions and errors due to programmer error
    to occur during runtime.

--- src/main/java/duke/command/AddEventCommand.java
+++ src/main/java/duke/command/AddEventCommand.java
@@ -12,47 +12,51 @@ import duke.textui.TextUi;
  * adding the task can be carried out when called.
  */
 public class AddEventCommand extends Command {
-  /**
-   * The DESCRIPTION, start and end date of the event task.
-   */
-  private final String data;
+    /**
+     * The DESCRIPTION, start and end date of the event task.
+     */
+    private final String data;
 
-  /**
-   * Constructor for a command to add new event task.
-   *
-   * @param data The DESCRIPTION, start and end date of the event task
-   */
-  public AddEventCommand(String data) {
-    super(AvailableCommands.ADD_EVENT);
-    this.data = data;
-  }
-
-  /**
-   * Adds a new event task into the task list.
-   * The start and end dates are filtered out. If they exist, then an event task
-   * will be created. Otherwise, an exception would be thrown stating that
-   * either the start or end dates were not specified.
-   *
-   * @param taskList List of tasks that are stored
-   * @param ui       UI to deal with the visual output
-   * @param storage  Storage to deal with input and output of data
-   */
-  @Override
-  public String execute(TaskList taskList, TextUi ui, Storage storage)
-      throws DukeException {
-    String[] splitData1 = data.split(" /from ", 2);
-    if (splitData1.length < 2) {
-      throw new DukeException("Event command format error. Missing /from");
+    /**
+     * Constructor for a command to add new event task.
+     *
+     * @param data The DESCRIPTION, start and end date of the event task
+     */
+    public AddEventCommand(String data) {
+        super(AvailableCommands.ADD_EVENT);
+        this.data = data;
     }
 
-    String[] splitData2 = splitData1[1].split(" /to ", 2);
-    if (splitData2.length < 2) {
-      throw new DukeException("Event command format error. Missing /to");
+    /**
+     * Adds a new event task into the task list.
+     * The start and end dates are filtered out. If they exist, then an event task
+     * will be created. Otherwise, an exception would be thrown stating that
+     * either the start or end dates were not specified.
+     *
+     * @param taskList List of tasks that are stored
+     * @param ui       UI to deal with the visual output
+     * @param storage  Storage to deal with input and output of data
+     */
+    @Override
+    public String execute(TaskList taskList, TextUi ui, Storage storage)
+            throws DukeException {
+        String[] splitData1 = data.split(" /from ", 2);
+        if (splitData1.length < 2) {
+            throw new DukeException("Event command format error. Missing /from");
+        }
+
+        assert splitData1.length == 2;
+
+        String[] splitData2 = splitData1[1].split(" /to ", 2);
+        if (splitData2.length < 2) {
+            throw new DukeException("Event command format error. Missing /to");
+        }
+
+        assert splitData2.length == 2;
+
+        Task event = new Event(splitData1[0], splitData2[0], splitData2[1]);
+        taskList.addTask(event);
+
+        return ui.showAddTask(event.toString(), taskList.size());
     }
-
-    Task event = new Event(splitData1[0], splitData2[0], splitData2[1]);
-    taskList.addTask(event);
-
-    return ui.showAddTask(event.toString(), taskList.size());
-  }
 }
