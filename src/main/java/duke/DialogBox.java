cd68d6cffb87017193fd6aafe06e49bf73761bed
ARPspoofing <mohdreyaaz99@gmail.com>
2023-02-14 08:28:09 +0800

    Add javadoc comments for dialogbox class

--- src/main/java/duke/DialogBox.java
+++ src/main/java/duke/DialogBox.java
@@ -16,9 +16,11 @@ import javafx.scene.layout.HBox;
 import javafx.scene.shape.Circle;
 
 /**
- * An example of a custom control using FXML.
- * This control represents a dialog box consisting of an ImageView to represent the speaker's face and a label
- * containing text from the speaker.
+ * Creates a custom user and bot dialog chat in GUI.
+ * It combines an Image and a text into a HBox.
+ *  * @author Muhammad Reyaaz
+ *  * @version %I% %G%
+ *  * @since 11
  */
 public class DialogBox extends HBox {
     @FXML
@@ -26,6 +28,12 @@ public class DialogBox extends HBox {
     @FXML
     private ImageView displayPicture;
 
+    /**
+     * Creates a new instance of the DialogBox class with the specified text and image.
+     *
+     * @param text The text to be displayed in the label
+     * @param img The image to be displayed in the ImageView
+     */
     private DialogBox(String text, Image img) {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(MainWindow.class.getResource("/view/DialogBox.fxml"));
@@ -38,14 +46,30 @@ public class DialogBox extends HBox {
         initStyle(text, img);
     }
 
+    /**
+     * Gets the label that displays the text.
+     *
+     * @return The label that displays the text
+     */
     Label getDialog() {
         return dialog;
     }
 
+    /**
+     * Gets the ImageView that displays the speaker's picture.
+     *
+     * @return The ImageView that displays the speaker's picture
+     */
     ImageView getDisplayPicture() {
         return displayPicture;
     }
 
+    /**
+     * Initializes the style of the dialog box.
+     *
+     * @param text The text to be displayed in the label
+     * @param img The image to be displayed in the ImageView
+     */
     void initStyle(String text, Image img) {
         dialog.setText(text);
         dialog.getStyleClass().add("round-lbl");
@@ -65,10 +89,22 @@ public class DialogBox extends HBox {
         setAlignment(Pos.TOP_LEFT);
     }
 
+    /**
+     * Returns a DialogBox object representing a user's dialog.
+     * @param text the text of the dialog
+     * @param img the image of the user's face
+     * @return a DialogBox object representing a user's dialog
+     */
     public static DialogBox getUserDialog(String text, Image img) {
         return new DialogBox(text, img);
     }
 
+    /**
+     * Returns a DialogBox object representing Duke's dialog.
+     * @param text the text of the dialog
+     * @param img the image of Duke's face
+     * @return a DialogBox object representing Duke's dialog
+     */
     public static DialogBox getDukeDialog(String text, Image img) {
         var db = new DialogBox(text, img);
         db.flip();
