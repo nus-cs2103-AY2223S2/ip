453f6be00e27ff36d74e52dcd6d8f9149936bfc6
zhoyx <zhouyx2000@gmail.com>
2023-02-15 02:07:17 +0800

    Add exception for absent Description in Deadline input

--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -45,43 +45,9 @@ public class Parser {
             String description = userInput.substring(5);
             return new AddTodoCommand(description);
         } else if (userInput.matches("^deadline .*")) {
-            int byPos = userInput.indexOf(" /by ");
-            if (byPos == -1) {
-                throw new DukeException("Deadline not specified with /by");
-            }
-            String description = userInput.substring(9, byPos);
-            String by = userInput.substring(byPos + 5);
-            LocalDateTime convertedBy;
-            try {
-                convertedBy = LocalDateTime.parse(by, INPUTFORMAT);
-            } catch (DateTimeParseException e) {
-                throw new DukeException("Invalid Date and Time provided, use the format: dd/MM/yyyy HH:mm");
-            }
-            return new AddDeadlineCommand(description, convertedBy);
+            return parseDeadline(userInput);
         } else if (userInput.matches("^event .*")) {
-            int fromPos = userInput.indexOf(" /from ");
-            int toPos = userInput.indexOf(" /to ");
-            if (fromPos == -1 || toPos == -1 || toPos > userInput.length() + 4) {
-                throw new DukeException("Please include both /from and /to");
-            }
-            if (fromPos > toPos) {
-                throw new DukeException("Please add the from date first followed by to date");
-            }
-            if (fromPos == 5) {
-                throw new DukeException("Please include a description of the task");
-            }
-            String description = userInput.substring(6, fromPos);
-            String from = userInput.substring(fromPos + 7, toPos);
-            String to = userInput.substring(toPos + 5);
-            LocalDateTime convertedFrom;
-            LocalDateTime convertedTo;
-            try {
-                convertedFrom = LocalDateTime.parse(from, INPUTFORMAT);
-                convertedTo = LocalDateTime.parse(to, INPUTFORMAT);
-            } catch (DateTimeParseException e) {
-                throw new DukeException("Invalid Date and Time provided, use the format: dd/MM/yyyy HH:mm");
-            }
-            return new AddEventCommand(description, convertedFrom, convertedTo);
+            return parseEvent(userInput);
         } else if (userInput.matches("^delete \\d+")) {
             int index = Integer.parseInt(userInput.substring(7)) - 1;
             return new DeleteCommand(index);
@@ -91,5 +57,50 @@ public class Parser {
         } else {
             return new UnknownCommand();
         }
+    }
+
+    public static Command parseDeadline(String userInput) throws DukeException {
+        int byPos = userInput.indexOf(" /by ");
+        if (byPos == -1) {
+            throw new DukeException("Deadline not specified with /by");
+        }
+        if (byPos == 8) {
+            throw new DukeException("Please include a description of the task");
+        }
+        String description = userInput.substring(9, byPos);
+        String by = userInput.substring(byPos + 5);
+        LocalDateTime convertedBy;
+        try {
+            convertedBy = LocalDateTime.parse(by, INPUTFORMAT);
+        } catch (DateTimeParseException e) {
+            throw new DukeException("Invalid Date and Time provided, use the format: dd/MM/yyyy HH:mm");
+        }
+        return new AddDeadlineCommand(description, convertedBy);
+    }
+
+    public static Command parseEvent(String userInput) throws DukeException {
+        int fromPos = userInput.indexOf(" /from ");
+        int toPos = userInput.indexOf(" /to ");
+        if (fromPos == -1 || toPos == -1 || toPos > userInput.length() + 4) {
+            throw new DukeException("Please include both /from and /to");
+        }
+        if (fromPos > toPos) {
+            throw new DukeException("Please add the from date first followed by to date");
+        }
+        if (fromPos == 5) {
+            throw new DukeException("Please include a description of the task");
+        }
+        String description = userInput.substring(6, fromPos);
+        String from = userInput.substring(fromPos + 7, toPos);
+        String to = userInput.substring(toPos + 5);
+        LocalDateTime convertedFrom;
+        LocalDateTime convertedTo;
+        try {
+            convertedFrom = LocalDateTime.parse(from, INPUTFORMAT);
+            convertedTo = LocalDateTime.parse(to, INPUTFORMAT);
+        } catch (DateTimeParseException e) {
+            throw new DukeException("Invalid Date and Time provided, use the format: dd/MM/yyyy HH:mm");
+        }
+        return new AddEventCommand(description, convertedFrom, convertedTo);
     }
 }
