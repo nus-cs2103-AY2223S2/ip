e0ecd2879b8b663cf80d2cfd69fce903c611479f
Cyrus-Krispin <cyruskrispin@gmail.com>
2023-02-14 20:40:54 +0800

    Implement GUI

--- src/main/java/baymax/Baymax.java
+++ src/main/java/baymax/Baymax.java
@@ -9,47 +9,179 @@ import java.util.Scanner;
 
 import commands.Command;
 
-public class Baymax {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.scene.layout.Region;
+import javafx.scene.control.Label;
 
-    private final Storage storage;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+
+public class Baymax extends Application {
+
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
+
+    private Storage storage;
     private TaskList tasks;
     private Scanner input = new Scanner(System.in);
     private Parser parser = new Parser();
     private static Ui ui = new Ui();
 
+    private Image user = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
+    private Image duke = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+
     /**
      * Constructor for Baymax.
      * @param filepath The filepath of the data file.
      */
-    public Baymax(String filepath) {
-        Ui ui = new Ui();
-        storage = new Storage(filepath);
-        try {
-            tasks= new TaskList(storage.load());
-        } catch (BaymaxException e) {
-            ui.showLoadingErrorMessage();
-            tasks = new TaskList();
-        }
-    }
+//    public Baymax(String filepath) {
+//        Ui ui = new Ui();
+//        storage = new Storage(filepath);
+//        try {
+//            tasks= new TaskList(storage.load());
+//        } catch (BaymaxException e) {
+//            ui.showLoadingErrorMessage();
+//            tasks = new TaskList();
+//        }
+//    }
 
 
-    public static void main(String[] args) {
-        new Baymax("./data/Baymax.txt").run();
-    }
+//    //public static void main(String[] args) {
+//        new Baymax("./data/Baymax.txt").run();
+//    }
 
     /**
      * Runs the program until termination.
      */
-    public void run() {
-        Command welcomeCommand = new WelcomeCommand();
-        welcomeCommand.execute(tasks, ui, storage);
-        while (input.hasNextLine()) {
-            Command command = parser.parse(input.nextLine());
-            command.execute(tasks, ui, storage);
-            if (command instanceof ByeCommand) {
-                break;
-            }
-        }
+//    public void run() {
+//        Command welcomeCommand = new WelcomeCommand();
+//        welcomeCommand.execute(tasks, ui, storage);
+//        while (input.hasNextLine()) {
+//            Command command = parser.parse(input.nextLine());
+//            command.execute(tasks, ui, storage);
+//            if (command instanceof ByeCommand) {
+//                break;
+//            }
+//        }
+//    }
+
+    @Override
+    public void start(Stage stage) {
+        //Step 1. Setting up required components
+
+        //The container for the content of the chat to scroll.
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+
+        userInput = new TextField();
+        sendButton = new Button("Send");
+
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+        scene = new Scene(mainLayout);
+
+        stage.setScene(scene);
+        stage.show();
+
+        stage.setTitle("Duke");
+        stage.setResizable(false);
+        stage.setMinHeight(600.0);
+        stage.setMinWidth(400.0);
+
+        mainLayout.setPrefSize(400.0, 600.0);
+
+        scrollPane.setPrefSize(385, 535);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
+
+        // You will need to import `javafx.scene.layout.Region` for this.
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+
+        userInput.setPrefWidth(325.0);
+
+        sendButton.setPrefWidth(55.0);
+
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+
+        AnchorPane.setLeftAnchor(userInput , 1.0);
+        AnchorPane.setBottomAnchor(userInput, 1.0);
+
+        sendButton.setOnMouseClicked((event) -> {
+            dialogContainer.getChildren().add(getDialogLabel(userInput.getText()));
+            userInput.clear();
+        });
+
+        userInput.setOnAction((event) -> {
+            dialogContainer.getChildren().add(getDialogLabel(userInput.getText()));
+            userInput.clear();
+        });
+
+        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
+
+        sendButton.setOnMouseClicked((event) -> {
+            handleUserInput();
+        });
+
+        userInput.setOnAction((event) -> {
+            handleUserInput();
+        });
+    }
+
+    /**
+     * Iteration 1:
+     * Creates a label with the specified text and adds it to the dialog container.
+     * @param text String containing text to add
+     * @return a label with the specified text that has word wrap enabled.
+     */
+    private Label getDialogLabel(String text) {
+        // You will need to import `javafx.scene.control.Label`.
+        Label textToAdd = new Label(text);
+        textToAdd.setWrapText(true);
+
+        return textToAdd;
+    }
+
+    /**
+     * Iteration 2:
+     * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
+     * the dialog container. Clears the user input after processing.
+     */
+    private void handleUserInput() {
+        String userText = String.valueOf(new Label(userInput.getText()));
+        String dukeText = String.valueOf(new Label(getResponse(userInput.getText())));
+        dialogContainer.getChildren().addAll(
+                DialogBox.getUserDialog(userText, new ImageView(user).getImage()),
+                DialogBox.getDukeDialog(dukeText, new ImageView(duke).getImage())
+        );
+        userInput.clear();
+    }
+
+    /**
+     * You should have your own function to generate a response to user input.
+     * Replace this stub with your completed method.
+     */
+    String getResponse(String input) {
+        return "Duke heard: " + input;
     }
 }
+
 
