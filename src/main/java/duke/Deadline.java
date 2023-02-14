c684289ca20f75816e84119030cc6ef22dc58ae4
ARPspoofing <mohdreyaaz99@gmail.com>
2023-02-14 08:18:08 +0800

    Add javadoc for deadline class

--- src/main/java/duke/Deadline.java
+++ src/main/java/duke/Deadline.java
@@ -5,13 +5,8 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 /**
  * <h1>Deadline tasks with date</h1>
- * The Deadline class is a specific subclass of the Task that allows user to
- * add a stipulated end date for their keyed input. By default, it assumes
- * the Deadline task is undone, because it does not make sense for a user to
- * enter a new Deadline task if it is done. However, should the user want to
- * input a past done Deadline task, he is allowed to do so. The user can
- * also toggle the status of the Deadline task as done or undone. Lastly,
- * the Deadline date can be both a day, and a local date.
+ * Assumes the deadline class is not done unless specified.
+ * Parses a date in localdatetime and stores it as a string.
  * @author Muhammad Reyaaz
  * @version %I% %G%
  * @since 11
@@ -20,15 +15,41 @@ import java.time.format.DateTimeParseException;
 
 class Deadline extends Task {
     protected String by;
+
+    /**
+     * Creates an instance of Deadline with an empty description and end date.
+     */
     Deadline() {}
+
+    /**
+     * Creates an instance of Deadline with a description and an end date.
+     *
+     * @param description The description of the task
+     * @param by          The end date of the task
+     */
     Deadline(String description, String by) {
         super(description);
         this.by = by;
     }
+
+    /**
+     * Creates an instance of Deadline with a description, an end date, and a boolean indicating if the task is done.
+     *
+     * @param description The description of the task
+     * @param by          The end date of the task
+     * @param isDone      A boolean indicating if the task is done
+     */
     Deadline(String description, String by, boolean isDone) {
         super(description, isDone);
         this.by = by;
     }
+
+    /**
+     * Parses the end date into a `LocalDate` object, if possible, and formats it into the format "MMM dd yyyy". If
+     * the end date cannot be parsed into a `LocalDate` object, returns the end date as is.
+     *
+     * @return The formatted end date as a string
+     */
     String localDateParser() {
         try {
             LocalDate date = LocalDate.parse(by);
@@ -37,10 +58,22 @@ class Deadline extends Task {
             return by;
         }
     }
+
+    /**
+     * Marks the Deadline task as done.
+     *
+     * @return A new instance of Deadline with the same description, end date, and isDone set to `true`
+     */
     @Override
     Deadline markAsDone() {
         return new Deadline(getDescription(), by, true);
     }
+
+    /**
+     * Marks the Deadline task as undone.
+     *
+     * @return A new instance of Deadline with the same description, end date, and isDone set to `false`
+     */
     @Override
     Deadline markAsUndone() {
         return new Deadline(getDescription(), by, false);
