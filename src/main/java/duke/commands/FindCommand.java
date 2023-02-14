ed680fe5e3c11ed004fba5a6a4d034d5b58160eb
Gloria Zhang Xinhui <glozxi@gmail.com>
2023-02-14 18:40:32 +0800

    Add response when no tasks are found for find command

--- src/main/java/duke/commands/FindCommand.java
+++ src/main/java/duke/commands/FindCommand.java
@@ -17,6 +17,9 @@ public class FindCommand extends Command {
     @Override
     public String execute(TaskList taskList, UiText uiText, Storage storage) {
         TaskList matchingList = taskList.getMatchingTasks(keyword);
+        if (matchingList.getSize() == 0) {
+            return "There are no tasks that match '" + keyword + "'.";
+        }
         return "These are the matching tasks in your list:\n"
                 + matchingList;
     }
