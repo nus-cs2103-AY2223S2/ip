fd119046e630eeffcee394a83c9107b5a8ac9061
lxz333 <e0909010@u.nus.edu>
2023-02-14 13:39:03 +0800

    Delete run() method

--- src/main/java/task/Parser.java
+++ src/main/java/task/Parser.java
@@ -21,14 +21,14 @@ public class Parser {
     public void checkEmpty(String input, String command) throws DukeException {
         if (input.length() < command.length() + 1) {
             switch (command) {
-                case "todo":
-                    throw new EmptyTodoException();
-                case "deadline":
-                    throw new EmptyDeadlineException();
-                case "event":
-                    throw new EmptyEventException();
-                case "mark": case "unmark": case "delete": case "find":
-                    throw new EmptyListException();
+            case "todo":
+                throw new EmptyTodoException();
+            case "deadline":
+                throw new EmptyDeadlineException();
+            case "event":
+                throw new EmptyEventException();
+            case "mark": case "unmark": case "delete": case "find":
+                throw new EmptyListException();
             }
         }
     }
@@ -42,9 +42,9 @@ public class Parser {
     public String convertEnum(Command c) {
         String res = "";
         switch (c) {
-            case LIST: case MARK: case TODO: case EVENT: case FIND:
-            case DELETE: case UNMARK: case DEADLINE: case BYE:
-                res = c.name().toLowerCase();
+        case LIST: case MARK: case TODO: case EVENT: case FIND:
+        case DELETE: case UNMARK: case DEADLINE: case BYE:
+            res = c.name().toLowerCase();
         }
         return res;
     }
