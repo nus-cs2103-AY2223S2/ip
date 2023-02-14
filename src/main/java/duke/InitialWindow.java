83ef708fbb27a862a79af61710beff453f5171dc
ARPspoofing <mohdreyaaz99@gmail.com>
2023-02-14 08:49:49 +0800

    Provide javadoc comments for initial window class

--- src/main/java/duke/InitialWindow.java
+++ src/main/java/duke/InitialWindow.java
@@ -11,6 +11,14 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * Controls the class for the InitialWindow.fxml file.
+ * Provides the functionality for switching to MainWindow.
+ * @author Muhammad Reyaaz
+ * @version %I% %G%
+ * @see Task
+ * @since 11
+ */
 public class InitialWindow {
 
     private Duke duke = new Duke();
@@ -19,6 +27,11 @@ public class InitialWindow {
     private Scene scene;
     private Parent root;
 
+    /**
+     * Switches the scene to MainWindow when button is pressed.
+     * @param event The ActionEvent that triggered this method.
+     * @throws IOException If the FXMLLoader encounters an error while loading the MainWindow.fxml file.
+     */
     @FXML
     public void switchToScene1(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(Objects.requireNonNull(Main.class.getResource("/view/MainWindow.fxml")));
@@ -26,8 +39,6 @@ public class InitialWindow {
         scene = new Scene(root);
         scene.getStylesheets().add(this.getClass().getResource("/style.css").toExternalForm());
         stage.setScene(scene);
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
-        //System.out.println("fxmlLoader " + fxmlLoader);
         stage.show();
     }
 
