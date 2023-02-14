c4d7a30d128e670c323a6d8965483b4e462243dc
Gloria Zhang Xinhui <glozxi@gmail.com>
2023-02-14 18:21:15 +0800

    Add tests

--- src/test/java/duke/task/ToDoTaskTest.java
+++ src/test/java/duke/task/ToDoTaskTest.java
@@ -9,4 +9,16 @@ public class ToDoTaskTest {
     public void testToString() {
         assertEquals("[T][ ] bla bla bla", new ToDoTask("bla bla bla").toString());
     }
+
+    @Test
+    public void isUpcoming_daysZero() {
+        assertEquals(false, new ToDoTask("bla bla bla").isUpcoming(0));
+    }
+
+    @Test
+    public void isUpcoming_daysPositive() {
+        assertEquals(false, new ToDoTask("bla bla bla").isUpcoming(1));
+        assertEquals(false, new ToDoTask("bla bla bla").isUpcoming(2));
+        assertEquals(false, new ToDoTask("bla bla bla").isUpcoming(3));
+    }
 }
