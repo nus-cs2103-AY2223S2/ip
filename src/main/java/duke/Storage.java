718c08f7b36fdf5442e57bac277e07a794c9ce1b
dawg420 <88711624+dawg420@users.noreply.github.com>
2023-02-15 06:24:50 +0800

    CodeQuality
    Storage class now has private functions loadTodo, loadDeadline and loadEvent
    
    Extracting these functions allows the code to follow the SLAP principle more closely.
    
    This is done to ensure that different levels of abstraction are contained in different blocks of code, keeping the code clean.

--- src/main/java/duke/Storage.java
+++ src/main/java/duke/Storage.java
@@ -15,6 +15,8 @@ import java.time.format.DateTimeFormatter;
  */
 public class Storage {
     public static final String SAVE_LOCATION = "duke.txt";
+    private static final int INDEX_OF_TYPE = 4;
+    private static final int INDEX_OF_DONE = 7;
 
     /**
      * Loads the save file from a fixed location
@@ -27,48 +29,60 @@ public class Storage {
         Scanner fileReader = new Scanner(saveFile);
         while (fileReader.hasNextLine()) {
             String data = fileReader.nextLine();
-            char taskType = data.charAt(4);
-            boolean taskDone = (data.charAt(7) == 'X');
-            String taskName = "";
-
-
+            char taskType = data.charAt(INDEX_OF_TYPE);
             switch (taskType) {
             case 'T':
-                taskName = data.substring(10, data.length());
-                Task.Todo newTodo = new Task.Todo(taskName);
-                if (taskDone) {
-                    newTodo.setDone();
-                }
-                listOfThings.add(newTodo);
+                listOfThings.add(loadTodo(data));
                 break;
             case 'D':
-                taskName = data.substring(10, data.indexOf("(by:") - 1);
-                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd, yyyy");
-                String byWhen = data.substring(data.indexOf("by:") + 4, data.length() - 7);
-                LocalDate date = LocalDate.parse(byWhen, formatter);
-                String byTime = data.substring(data.length() - 6, data.length() - 1);
-                LocalTime time = LocalTime.parse(byTime);
-                Task.Deadline newDeadline = new Task.Deadline(taskName, date, time);
-                if (taskDone) {
-                    newDeadline.setDone();
-                }
-                listOfThings.add(newDeadline);
+                listOfThings.add(loadDeadline(data));
                 break;
             case 'E':
-                taskName = data.substring(10, data.indexOf("(from:") - 1);
-                String fromWhen = data.substring(data.indexOf("from:") + 6, data.indexOf("to:") - 1);
-                String toWhen = data.substring(data.indexOf("to:") + 4, data.length() - 1);
-                Task.Event newEvent = new Task.Event(taskName, fromWhen, toWhen);
-                if (taskDone) {
-                    newEvent.setDone();
-                }
-                listOfThings.add(newEvent);
+                listOfThings.add(loadEvent(data));
                 break;
             default:
                 break;
             }
         }
         return listOfThings;
+    }
+
+
+    private static Task loadTodo(String saveString) {
+        String taskName = saveString.substring(10, saveString.length());
+        boolean taskDone = (saveString.charAt(INDEX_OF_DONE) == 'X');
+        Task.Todo newTodo = new Task.Todo(taskName);
+        if (taskDone) {
+            newTodo.setDone();
+        }
+        return newTodo;
+    }
+
+    private static Task loadDeadline(String saveString) {
+        boolean taskDone = saveString.charAt(INDEX_OF_DONE) == 'X';
+        String taskName = saveString.substring(10, saveString.indexOf("(by:") - 1);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd, yyyy");
+        String byWhen = saveString.substring(saveString.indexOf("by:") + 4, saveString.length() - 7);
+        LocalDate date = LocalDate.parse(byWhen, formatter);
+        String byTime = saveString.substring(saveString.length() - 6, saveString.length() - 1);
+        LocalTime time = LocalTime.parse(byTime);
+        Task.Deadline newDeadline = new Task.Deadline(taskName, date, time);
+        if (taskDone) {
+            newDeadline.setDone();
+        }
+        return newDeadline;
+    }
+
+    private static Task loadEvent(String saveString) {
+        boolean taskDone = saveString.charAt(INDEX_OF_DONE) == 'X';
+        String taskName = saveString.substring(10, saveString.indexOf("(from:") - 1);
+        String fromWhen = saveString.substring(saveString.indexOf("from:") + 6, saveString.indexOf("to:") - 1);
+        String toWhen = saveString.substring(saveString.indexOf("to:") + 4, saveString.length() - 1);
+        Task.Event newEvent = new Task.Event(taskName, fromWhen, toWhen);
+        if (taskDone) {
+            newEvent.setDone();
+        }
+        return newEvent;
     }
 
 
