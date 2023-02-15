a6fc5b810a700f0bddf1d3fb408782ea665aeeae
Valerie Tan <valerietanhx@gmail.com>
2023-02-16 01:30:26 +0800

    Add product name to GUI

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -22,6 +22,7 @@ public class Main extends Application {
             stage.setScene(scene);
             fxmlLoader.<MainWindow>getController().setDuke(duke);
             stage.show();
+            stage.setTitle("Duke!!!");
         } catch (IOException e) {
             e.printStackTrace();
         }
