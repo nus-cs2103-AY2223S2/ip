04758aa02042e7f605515654e849fbbbca5f332b
Ritika Joshi <ritikajoshi0114@gmail.com>
2023-02-15 22:16:08 +0800

    Improve GUI and update images

--- src/main/java/MainWindow.java
+++ src/main/java/MainWindow.java
@@ -20,8 +20,8 @@ public class MainWindow extends AnchorPane {
 
     private Duke duke;
 
-    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/user.png"));
-    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/ryo.png"));
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/star.png"));
+    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/ryo-trans.png"));
 
     @FXML
     public void initialize() {
