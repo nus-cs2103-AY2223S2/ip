46619d5a3d47252945b2da7761fefd3da5832b07
Sachin Kalinga <>
2023-02-15 23:32:38 +0800

    UI class was slightly edited

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -16,7 +16,7 @@ public class Duke {
         Parser parser = new Parser(storage);
         String home = System.getProperty("user.home");
         UI ui = new UI();
-        ui.printLogo();
+        ui.printWelcomeMessage();
         String userInput = scanner.nextLine();
 
 
