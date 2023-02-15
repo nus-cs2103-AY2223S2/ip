e05e1c13d9bffa2575959dcc430237b8b9ac15e2
Joy Ng Jing Ru <e0943052@u.nus.edu>
2023-02-16 07:14:45 +0800

    Update Junit tests

--- src/test/java/task/TodoTest.java
+++ src/test/java/task/TodoTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TodoTest {
     @Test
     public void toString_task_string() {
-        assertEquals("[T][ ] get book from library", new Todo("get book from library").toString());
+        assertEquals("[T][ ] get book from library", new Todo("todo get book from library").toString());
     }
 
 }
