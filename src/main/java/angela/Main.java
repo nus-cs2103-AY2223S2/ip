ba24be5663bbbf97ef41c33996a0cfabeb8e65db
Gloria Zhang Xinhui <glozxi@gmail.com>
2023-02-15 14:55:51 +0800

    Add icon

--- src/main/java/angela/Main.java
+++ src/main/java/angela/Main.java
@@ -6,6 +6,7 @@ import angela.exception.AngelaException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -24,6 +25,7 @@ public class Main extends Application {
             Scene scene = new Scene(ap);
             stage.setScene(scene);
             stage.setTitle("Angela");
+            stage.getIcons().add(new Image("/images/Angela.png"));
 
             angela = new Angela("data/angela.txt");
             fxmlLoader.<MainWindow>getController().setAngela(angela);
