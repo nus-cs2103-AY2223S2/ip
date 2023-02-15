4797e82a5d7f3b49fb5d2b052d13fa4e7bc94ac4
Gloria Zhang Xinhui <glozxi@gmail.com>
2023-02-15 12:06:34 +0800

    Add title to window

--- src/main/java/duke/Main.java
+++ src/main/java/duke/Main.java
@@ -23,6 +23,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Angela");
 
             duke = new Duke("data/duke.txt");
             fxmlLoader.<MainWindow>getController().setDuke(duke);
