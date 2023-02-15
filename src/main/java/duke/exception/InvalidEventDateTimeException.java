7d447d4d1e1e12d4e9f73ead81510ef297fc39a1
dohaduong <duonghado03@gmail.com>
2023-02-15 16:44:16 +0800

    Add Personality

--- src/main/java/duke/exception/InvalidEventDateTimeException.java
+++ src/main/java/duke/exception/InvalidEventDateTimeException.java
@@ -1,10 +1,10 @@
 package duke.exception;
 
 /**
- * Represents wrong event date-time format
+ * Represents invalid event date-time format error
  */
 public class InvalidEventDateTimeException extends DukeException {
     public InvalidEventDateTimeException() {
-        super("WOOF! What were you thinking boss?? The event date time format should be YYYY-MM-DD HHmm! Change it NOW!");
+        super("WOOF WHAT?! The event date time format should be YYYY-MM-DD HHmm! Change it NOW, Boss!");
     }
 }
