86e1c0bb996e35b52b51338a28411099a5150927
weilin1202 <e0775018@u.nus.edu>
2023-02-16 07:14:58 +0800

    Add A-Personality to Duke

--- src/main/java/duke/ui/Ui.java
+++ src/main/java/duke/ui/Ui.java
@@ -15,11 +15,11 @@ public class Ui {
      * @return String to greet user
      */
     public String greet() {
-        return "Hello! I'm Bear Bear!! \nWhat can I do for you?";
+        return "Hello! I'm Bear Bear!! \nNice to meet you! \nWhat can I do for you?";
     }
 
     public String sayBye() {
-        return "Bye. Hope to see you again soon!";
+        return "Bye!! Do visit me again! I'll always be free for you :)";
     }
 
     public String showError(String errorMessage) {
@@ -27,28 +27,28 @@ public class Ui {
     }
 
     public String showAddTask(String task, int totalTaskNumber) {
-        return "Got it. I've added this task: \n  " + task
+        return "Wow! I've added this task: \n  " + task
                 + "\nNow you have " + totalTaskNumber + " tasks in the list.";
     }
 
     public String showDeleteTask(String task, int totalTaskNumber) {
-        return "Noted. I've removed this task: \n  " + task
+        return "Okay!! I've removed this task: \n  " + task
                 + "\nNow you have " + totalTaskNumber + " tasks in the list.";
     }
 
     public String showFindResult(Boolean isFound, String result) {
         if (!isFound) {
-            return "Oops! :( There are no matching tasks found.";
+            return "Oops! :( There are no matching tasks found. Try again?";
         }
         return result;
     }
 
     public String showMarkTask(String task) {
-        return "Nice! I've marked this task as done: \n  " + task;
+        return "WOWWWWWW Good Job!!! \n I've marked this task as done: \n  " + task;
     }
 
     public String showUnmarkTask(String task) {
-        return "Ok, I've marked this task as not done yet: \n  " + task;
+        return "Ok... I've marked this task as not done yet: \n  " + task;
     }
 
     public String showList(String list) {
