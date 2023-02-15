4f4978b0992c326df509002742b9d3c684229075
cshuwen <chenshuwen0210@gmail.com>
2023-02-15 15:10:37 +0800

    duke package: separate the package into two packages
    Previous duke package contains too many classes that serve different purposes
    
    Add an additional package called task and place Task class and its child classes into the package.
    
    Improve clarity in purpose of each package.

---
+++ src/main/java/task/Event.java
@@ -0,0 +1,54 @@
+package task;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Event is a variation of Tasks that carries a task description and its start and end time
+ * @author CShuwen
+ * @version 1.0
+ * @since 0.0
+ */
+public class Event extends Task {
+    private int isDone;
+    private final LocalDate from;
+    private final LocalDate to;
+
+    public Event(String description, LocalDate from, LocalDate to, Integer isDone) {
+        super(description, isDone);
+        this.isDone = isDone;
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    public void unMark() {
+        isDone = 0;
+    }
+
+    @Override
+    public void markAsDone() {
+        isDone = 1;
+    }
+
+    @Override
+    public String dataFormat() {
+        if (isDone == 1) {
+            return "E/1/" + description + "/" + this.from + "/" + this.to;
+        } else {
+            return "E/0/" + description + "/" + this.from + "/" + this.to;
+        }
+    }
+
+    public String getStatusIcon() {
+        return (isDone == 1 ? "[X]" : "[ ]");
+    }
+
+
+    @Override
+    public String toString() {
+        return "[E][" + isDone + "]" + description + " (from "
+                + this.from.format(DateTimeFormatter.ofPattern("MMM d yyyy"))
+                + " to " + this.to.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
+    }
+}
