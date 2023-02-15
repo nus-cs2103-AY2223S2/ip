651234d425f96226546d557b789574f5e824c45d
Jedidiah Cheng <chengjedidiah@gmail.com>
2023-02-16 01:36:44 +0800

    Add min width and height of window

--- src/main/java/duke/DukeGui.java
+++ src/main/java/duke/DukeGui.java
@@ -12,6 +12,8 @@ import javafx.stage.Stage;
 public class DukeGui extends Application {
     private static final String MAIN_WINDOW_RESOURCE_PATH = "/view/MainWindow.fxml";
     private static final String STYLESHEET_RESOURCE_PATH = "/view/style.css";
+    private static final double MIN_WIDTH = 400;
+    private static final double MIN_HEIGHT = 700;
 
     private final Bot bot = new Bot();
 
@@ -27,6 +29,8 @@ public class DukeGui extends Application {
             FXMLLoader fxmlLoader = new FXMLLoader(DukeGui.class.getResource(MAIN_WINDOW_RESOURCE_PATH));
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
+            stage.setMinHeight(MIN_HEIGHT);
+            stage.setMinWidth(MIN_WIDTH);
             stage.setScene(scene);
             String styleSheet = this.getClass().getResource(STYLESHEET_RESOURCE_PATH).toExternalForm();
             scene.getStylesheets().addAll(styleSheet);
