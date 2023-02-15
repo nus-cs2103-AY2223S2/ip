241d0485a170ef65473d102bce513195b9a5d51f
gitsac <isaacchai99@gmail.com>
2023-02-15 17:32:41 +0800

    Center profile picture of user and chatbot

--- src/main/java/duke/gui/DialogBox.java
+++ src/main/java/duke/gui/DialogBox.java
@@ -54,7 +54,7 @@ public class DialogBox extends HBox {
         ObservableList<Node> tmp = FXCollections.observableArrayList(this.getChildren());
         Collections.reverse(tmp);
         getChildren().setAll(tmp);
-        setAlignment(Pos.TOP_LEFT);
+        setAlignment(Pos.CENTER_LEFT);
     }
 
     public static DialogBox getUserDialog(String text, Image img, String type) {
