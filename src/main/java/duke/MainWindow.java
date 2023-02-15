47d8a3a1ca10aa0119c440232620b7ebbe521748
noahxinjie <noahxinjie@gmail.com>
2023-02-15 23:22:45 +0800

    Improve GUI

--- src/main/java/duke/MainWindow.java
+++ src/main/java/duke/MainWindow.java
@@ -22,8 +22,8 @@ public class MainWindow extends AnchorPane {
 
     private Duke duke;
 
-    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.jpg"));
-    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.jpg"));
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/eggy.jpg"));
+    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/jizo.png"));
 
     /**
      * Initialises the container for dialogue between user and Duke.
