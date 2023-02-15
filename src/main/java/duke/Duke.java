8d2a2231ac26ac671edbd7906a6139541edb21a3
WillCWX <xuanwilliam@yahoo.com>
2023-02-15 17:08:34 +0800

    Removes deprecated Ui class from Duke
    Duke currently retains the Ui class from its previous functions as a
    CLI chat bot.
    
    However, as new updates are given to the GUI form of Duke, the CLI form
    is no longer maintained as well. The CLI form now lacks the Undo function
    that is already implemented for the GUI.
    
    Let's delete deprecated functions that still use Ui.
    
    This will reduce the errors and is part of the deprecation process of Ui.

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -10,16 +10,16 @@ import duke.storage.Storage;
 import duke.task.History;
 import duke.task.TaskList;
 import duke.ui.Gui;
-import duke.ui.Ui;
 
 /**
- * The Duke class represents a CLI chatbot that performs operations based on CLI user input.
+ * The Duke class represents a CLI chatbot that performs operations based on CLI
+ * user input.
  * <p>
- * Currently, Duke accepts the commands: {@code echo, list, mark, unmark, todo, deadline, event, bye}
+ * Currently, Duke accepts the commands:
+ * {@code echo, list, mark, unmark, todo, deadline, event, bye}
  */
 public class Duke {
     private TaskList tasks;
-    private Ui ui;
     private Storage store;
     private Gui gui;
     private History history;
@@ -28,45 +28,13 @@ public class Duke {
      * Constructs Duke.
      */
     public Duke() {
-        ui = Ui.getInstance();
         history = History.getInstance();
         try {
             store = new Storage("src/main/resources/duke.txt");
             tasks = store.loadFromFile();
             tasks = tasks == null ? new TaskList() : tasks;
         } catch (Exception e) {
-            ui.produceExceptionOutput(e.getMessage());
             tasks = new TaskList();
-        }
-    }
-
-    /**
-     * Main function of Duke.
-     *
-     * @param args There are no options available for now.
-     */
-    @Deprecated
-    public static void main(String[] args) {
-        new Duke().run();
-    }
-
-    /**
-     * Runs Duke as a CLI chatbot.
-     */
-    @Deprecated
-    public void run() {
-        ui.greet();
-        boolean isExit = false;
-        while (!isExit) {
-            try {
-                String command = ui.getCommand();
-                Command c = Parser.parseCommand(command);
-                c.execute(tasks, ui, store);
-                history.addState(tasks);
-                isExit = c.canExit();
-            } catch (DukeException e) {
-                ui.produceExceptionOutput(e.getMessage());
-            }
         }
     }
 
