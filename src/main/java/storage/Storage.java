c577c7ad54e97fd5c1a92bf4e7157e03c00415b0
GlenLim <glenlimyf@gmail.com>
2023-02-16 16:32:05 +0800

    remove unnecessary imports

--- src/main/java/storage/Storage.java
+++ src/main/java/storage/Storage.java
@@ -2,13 +2,9 @@ package storage;
 
 import tasks.Task;
 import tasks.TaskList;
-import java.io.FileWriter;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
 import java.util.ArrayList;
 import java.nio.charset.StandardCharsets;
 
