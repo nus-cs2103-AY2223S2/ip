3373b57b654ce750c9f2dd1b512820988bbd96cb
Sean Wong <58366602+ExtraShotLatte@users.noreply.github.com>
2023-02-16 19:35:37 +0800

    Organize Parser and remove unused methods

--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -1,6 +1,15 @@
 package duke;
 
-import duke.Command.*;
+import duke.Command.Command;
+import duke.Command.DeadlineCommand;
+import duke.Command.DeleteCommand;
+import duke.Command.EventCommand;
+import duke.Command.ListCommand;
+import duke.Command.MarkCommand;
+import duke.Command.SearchCommand;
+import duke.Command.ToDoCommand;
+import duke.Command.UnknownCommand;
+import duke.Command.UnmarkCommand;
 import duke.ui.Ui;
 
 /**
@@ -22,73 +31,6 @@ public class Parser {
      */
     public boolean isExit(String s) {
         return s.equalsIgnoreCase("bye");
-    }
-
-    public String runCommand(String inputCommand, TaskList tasks, Storage storage, Ui ui) throws DukeException{
-        int selectedNum;
-        String commandResponse;
-        commandResponse = "";
-
-        String[] inputCmdArr = inputCommand.split(" ");
-        switch(inputCmdArr[0]) {
-        case "list":
-            commandResponse+=tasks.returnTaskAsString();
-            break;
-        case "todo":
-            String todoDesc;
-            todoDesc = inputCommand.split(" ", 2)[1];
-            tasks.addTask(todoDesc, false);
-            commandResponse+=tasks.returnNewestTaskAsString();
-            storage.save(tasks);
-            break;
-        case "deadline":
-            String deadlineInput;
-            deadlineInput = inputCommand.split(" ", 2)[1];
-            String[] deadlineDesc = deadlineInput.split(" /by ");
-            tasks.addTask(deadlineDesc[0], deadlineDesc[1], false);
-            commandResponse+=tasks.returnNewestTaskAsString();
-            storage.save(tasks);
-            break;
-        case "event":
-            String eventInput;
-            eventInput = inputCommand.split(" ", 2)[1];
-            String[] eventDescArr = eventInput.split(" /from ");
-            String eventDesc = eventDescArr[0];
-            String[] eventTimeArr = eventDescArr[1].split(" /to ");
-            String eventFrom = eventTimeArr[0];
-            String eventTo = eventTimeArr[1];
-            tasks.addTask(eventDesc, eventFrom, eventTo, false);
-            commandResponse+=tasks.returnNewestTaskAsString();
-            storage.save(tasks);
-            break;
-        case "mark":
-            selectedNum = Integer.parseInt(inputCmdArr[1]);
-            commandResponse+=tasks.markTaskWithResult(selectedNum);
-            storage.save(tasks);
-            break;
-        case "unmark":
-            selectedNum = Integer.parseInt(inputCmdArr[1]);
-            commandResponse+=tasks.unMarkTaskWithResult(selectedNum);
-            storage.save(tasks);
-            break;
-        case "delete":
-            int numToDelete;
-            numToDelete = Integer.parseInt(inputCommand.split(" ", 2)[1]);
-            commandResponse+=tasks.deleteTaskWithResult(numToDelete);
-            storage.save(tasks);
-            break;
-        case "search":
-            String searchStr;
-            searchStr = inputCommand.split(" ", 2)[1];
-            commandResponse+=tasks.searchTaskWithResult(searchStr);
-            break;
-        default:
-            commandResponse+="This command is not supported!";
-        }
-
-        assert !commandResponse.equals("");
-        
-        return commandResponse;
     }
 
     public String runCommand2(String inputCommand, TaskList tasks, Storage storage, Ui ui) throws DukeException {
