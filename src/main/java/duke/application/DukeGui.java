3fc0c7583daf0dfa3ddd54f0a0f8c94eb8ccf376
rexcyrio <stefancyrio@gmail.com>
2023-02-16 19:18:26 +0800

    A-UserGuide: table of contents & screenshots

--- src/main/java/duke/application/DukeGui.java
+++ src/main/java/duke/application/DukeGui.java
@@ -19,6 +19,7 @@ public class DukeGui extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Duchess");
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
