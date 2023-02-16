b116d27cf5790fda8b3dd0b39a4445e4e7b3d01b
zm-l <e0735481@u.nus.edu>
2023-02-17 00:52:17 +0800

    checkstyle fix

--- src/test/java/duke/commands/ExitCommandTest.java
+++ src/test/java/duke/commands/ExitCommandTest.java
@@ -1,13 +1,14 @@
 package duke.commands;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import org.junit.jupiter.api.Test;
 
 public class ExitCommandTest {
     @Test
     public void test1() {
         Command c = new ExitCommand();
-        assertEquals(false, c.isExit());
+        assertFalse(c.isExit());
     }
 }
