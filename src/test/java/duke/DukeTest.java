fa19ba4e15832aeeef3553bf6ebcf0589fe3df89
rockman007372 <rockman007372@gmail.com>
2023-02-17 03:25:16 +0800

    A bit more testing

--- src/test/java/duke/DukeTest.java
+++ src/test/java/duke/DukeTest.java
@@ -1,13 +1,30 @@
 package duke;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
 
 public class DukeTest {
     @Test
-    public void dummyTest() {
-        assertEquals(2, 2);
+    public void getResponse_invalidCommand_exceptionMessageGiven() {
+        Duke duke = new Duke();
+        String exceptionString = duke.getResponse("abcd");
+        assertEquals("Error: Invalid command.", exceptionString);
+    }
+
+    @Test
+    public void getResponse_missingDescription_exceptionMessageGiven() {
+        Duke duke = new Duke();
+        String exceptionString = duke.getResponse("update");
+        assertEquals("Error: No command description given.", exceptionString);
+    }
+
+    @Test
+    public void getResponse_wrongIndex_exceptionMessageGiven() {
+        Duke duke = new Duke();
+        String exceptionString = duke.getResponse("delete 1");
+        assertEquals("Error: Task index out of bounds.", exceptionString);
     }
 }
