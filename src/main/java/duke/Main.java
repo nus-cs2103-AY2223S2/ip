a771e1865e5bcf9dc19bb4702dfd920b22df8683
zm-l <e0735481@u.nus.edu>
2023-02-17 05:45:29 +0800

    d

--- src/main/java/duke/Main.java
+++ src/main/java/duke/Main.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -19,6 +20,8 @@ public class Main extends Application {
     public void start(Stage stage) {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
+            stage.setTitle("Shinsoo");
+            stage.getIcons().add(new Image(getClass().getResourceAsStream("../images/icon.jpeg")));
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
