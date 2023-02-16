e1c745549ba233ae3baa89ca6e96d33a487a62d0
zm-l <e0735481@u.nus.edu>
2023-02-17 01:31:33 +0800

    Update ExitCommandTest.java

--- src/test/java/duke/commands/ExitCommandTest.java
+++ src/test/java/duke/commands/ExitCommandTest.java
@@ -1,6 +1,5 @@
 package duke.commands;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import org.junit.jupiter.api.Test;
