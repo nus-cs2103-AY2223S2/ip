852d646e2fc04d040e6b8504dda916451142fdf7
zm-l <e0735481@u.nus.edu>
2023-02-17 01:52:16 +0800

    h

--- src/main/java/duke/MainWindow.java
+++ src/main/java/duke/MainWindow.java
@@ -22,8 +22,8 @@ public class MainWindow extends AnchorPane {
 
     private Duke duke;
 
-    private final Image userImage = new Image(this.getClass().getResourceAsStream("/images/user.jpeg"));
-    private final Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/bot.jpeg"));
+    private final Image userImage = new Image(this.getClass().getResourceAsStream("/images/user.png"));
+    private final Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/bot.png"));
 
     @FXML
     public void initialize() {
