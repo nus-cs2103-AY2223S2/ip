dc1c8a64acebc9bdb098ec4b7981c7e1076ac6b9
winsonheng <winsonh5147@gmail.com>
2023-02-17 23:22:27 +0800

    Final Changes to GUI and DateTime formatting

--- src/main/java/duke/task/Event.java
+++ src/main/java/duke/task/Event.java
@@ -1,23 +1,76 @@
 package duke.task;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 /**
  * Represents an event which has to from and to period.
  */
 public class Event extends Task {
-    private String startDate;
-    private String endDate;
+    private String start;
+    private String end;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private boolean isStartDate;
+    private boolean isEndDate;
+
 
     /**
      * Creates an instance of Event with the start and end dates.
      *
      * @param description Describes the event.
-     * @param startDate Event starts at this time.
-     * @param endDate Event ends at this time.
+     * @param start Event starts at this time.
+     * @param end Event ends at this time.
      */
-    public Event(String description, String startDate, String endDate) {
+    public Event(String description, String start, String end) {
         super(description);
-        this.startDate = startDate;
-        this.endDate = endDate;
+        // Checks if the deadline provided is a valid date
+        this.isStartDate = false;
+        for (DateTimeFormatter format: Task.DATETIME_FORMATS) {
+            try {
+                this.startDate = LocalDateTime.parse(start, format);
+                this.isStartDate = true;
+                break;
+            } catch (DateTimeParseException ex) {
+
+            }
+        }
+        if (!isStartDate) {
+            for (DateTimeFormatter format: Task.DATE_FORMATS) {
+                try {
+                    this.startDate = LocalDate.parse(start, format).atStartOfDay();
+                    this.isStartDate = true;
+                    break;
+                } catch (DateTimeParseException ex) {
+
+                }
+            }
+        }
+        this.start = start;
+        this.isEndDate = false;
+        for (DateTimeFormatter format: Task.DATETIME_FORMATS) {
+            try {
+                this.endDate = LocalDateTime.parse(end, format);
+                this.isEndDate = true;
+                break;
+            } catch (DateTimeParseException ex) {
+
+            }
+        }
+        if (!isEndDate) {
+            for (DateTimeFormatter format: Task.DATE_FORMATS) {
+                try {
+                    this.endDate = LocalDate.parse(end, format).atStartOfDay();
+                    this.isEndDate = true;
+                    break;
+                } catch (DateTimeParseException ex) {
+
+                }
+            }
+        }
+        this.end = end;
     }
 
     /**
@@ -27,11 +80,15 @@ public class Event extends Task {
      */
     @Override
     public String toString() {
-        return String.format("[E]%s (from: %s to: %s)", super.toString(), this.startDate, this.endDate);
+        String startDateToPrint = this.isStartDate ? this.startDate.format(DEFAULT_DATETIME_FORMAT) : this.start;
+        String endDateToPrint = this.isEndDate ? this.endDate.format(DEFAULT_DATETIME_FORMAT) : this.end;
+        return String.format("[E]%s (from: %s to: %s)", super.toString(), startDateToPrint, endDateToPrint);
     }
 
     @Override
     public String toCsvString() {
-        return String.format("E,%s,%s,%s", super.toCsvString(), this.startDate, this.endDate);
+        String startDateToPrint = this.isStartDate ? this.startDate.format(DEFAULT_DATETIME_FORMAT) : this.start;
+        String endDateToPrint = this.isEndDate ? this.endDate.format(DEFAULT_DATETIME_FORMAT) : this.end;
+        return String.format("E,%s,%s,%s", super.toCsvString(), startDateToPrint, endDateToPrint);
     }
 }
