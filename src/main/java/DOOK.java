dd7e69222203569a7346aa3c7314dd9367f7ef18
iapetusbob <haoyuansayhi@gmail.com>
2023-02-17 22:25:54 +0800

    branch-Level-10

--- src/main/java/DOOK.java
+++ src/main/java/DOOK.java
@@ -1,3 +1,23 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.scene.layout.Region;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+
 
 import java.io.IOException;
 
@@ -15,7 +35,7 @@ import DukeHelpfulCode.Commands.*;
  * @since   11 Feb 2023
  */
 
-public class DOOK {
+public class DOOK {//extends Application{
 
     private static String LINEBREAK = "_________________________________________________________________\n";
     private static TaskList USERLIST = new TaskList();
@@ -23,6 +43,10 @@ public class DOOK {
     UI ui;
     Storage storage;
     TaskList tasks;
+
+    public DOOK(){
+        new DOOK("./src/main/resources/data/tasks.txt");
+    }
 
     public DOOK(String filePath) {
         this.ui = new UI();
@@ -63,8 +87,18 @@ public class DOOK {
         }
     }
 
+
+
+    /**
+     * You should have your own function to generate a response to user input.
+     * Replace this stub with your completed method.
+     */
+    protected String getResponse(String input) {
+        return "Duke heard: " + input;
+    }
+
     public static void main(String[] args) {
-        new DOOK("./src/main/resources/data/tasks.txt").run();
+        new DOOK().run();
     }
 
 
