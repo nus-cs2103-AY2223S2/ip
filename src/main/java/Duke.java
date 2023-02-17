a999eb82c00c1778bd3f0166f5217ec2d5ada347
owen-yap <owen.y.yuhan@gmail.com>
2023-02-17 20:45:52 +0800

    Add GUI using JavaFX and FXML
    Our current text-based UI is plain and has no personality.
    
    Adding a simple GUI using JavaFX can greatly increase the user
    experience and add some personality to the text-bot.
    
    JavaFX is used because of its cross-platform capabilities and ease of
    implementing.
    
    Refer to this tutorial to show how JavaFX was implemented:
    https://se-education.org/guides/tutorials/javaFxPart1.html

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,33 +1,33 @@
-package duke;
-
 import duke.parser.Parser;
 import duke.storage.Storage;
 import duke.ui.Ui;
-import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.VBox;
 /**
  * The main class of the software engineering project Duke.
  * This class is responsible for coordinating the work of the user interface, storage and parser classes.
  *
  * @author owen-yap
  */
-public class Duke extends Application {
+public class Duke {
     private final Parser parser;
     private final Storage storage;
     private final Ui ui;
 
-    @Override
-    public void start(Stage stage) {
-        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
-        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
-        System.out.println("here");
-        stage.setScene(scene); // Setting the stage to show our screen
-        System.out.println("here");
-        stage.show(); // Render the stage.
-        System.out.println("here");
-    }
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
+
+
+    private Image user = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
+    private Image duke = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+
     /**
      * Constructs a Duke object which takes a file path and initializes the storage, user interface and parser.
      *
@@ -38,6 +38,8 @@ public class Duke extends Application {
         this.ui = new Ui();
         this.parser = new Parser(storage, ui);
     }
+
+
     /**
      * The main method of the Duke class which initializes the program and runs it.
      */
@@ -56,5 +58,13 @@ public class Duke extends Application {
      */
     public static void main(String[] args) {
         new Duke("data.txt").run();
+    }
+
+    /**
+     * You should have your own function to generate a response to user input.
+     * Replace this stub with your completed method.
+     */
+    public String getResponse(String input) {
+        return "Duke heard: " + input;
     }
 }
--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -1,5 +1,3 @@
-package duke;
-
 import javafx.application.Application;
 
 /**
@@ -7,6 +5,6 @@ import javafx.application.Application;
  */
 public class Launcher {
     public static void main(String[] args) {
-        Application.launch(Duke.class, args);
+        Application.launch(Main.class, args);
     }
 }
