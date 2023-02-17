6e5476e24ca63a0a609c6ff168678b80e05cdaf5
Chandrasekaran Akash <enigmatrix2000@gmail.com>
2023-02-17 16:05:14 +0800

    fix: checkstyle lints

--- src/main/java/kude/gui/DukeApp.java
+++ src/main/java/kude/gui/DukeApp.java
@@ -6,7 +6,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import kude.Storage;
 import kude.processor.Processor;
 
