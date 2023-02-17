d39e51b233d430080d208d4ecab01f19f826e2dc
Elijah Ku <elijahku716@gmail.com>
2023-02-17 22:45:19 +0800

    Give main window title

--- src/main/java/center/Main.java
+++ src/main/java/center/Main.java
@@ -19,6 +19,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Skylar");
             fxmlLoader.<MainWindow>getController().setSkylar(skylar);
             stage.show();
         } catch (IOException e) {
