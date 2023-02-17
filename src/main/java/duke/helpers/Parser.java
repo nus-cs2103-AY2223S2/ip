9c2f51d2802501edbcacb1bb7faaf6b796ace213
jengoc415 <jengoc415@gmail.com>
2023-02-17 17:31:00 +0800

    A-Assertions

--- src/main/java/duke/helpers/Parser.java
+++ src/main/java/duke/helpers/Parser.java
@@ -45,62 +45,71 @@ public class Parser {
         this.command = instrSplit[0];
 
         switch (command) {
-        case "bye":
-            message = "Bye. Hope to see you again soon!\n";
-            break;
-        case "list":
-            message = "Here are the tasks in your list:\n" + taskList.list();
-            break;
-        case "mark":
-            index = Integer.parseInt(instrSplit[1]);
-            message = "Nice! I've marked this task as done:\n";
-            message += "    " + taskList.mark(index) + "\n";
-            this.taskList.save();
-            break;
-        case "unmark":
-            index = Integer.parseInt(instrSplit[1]);
-            message = "OK, I've marked this task as not done yet:\n";
-            message += "    " + taskList.unmark(index) + "\n";
-            this.taskList.save();
-            break;
-        case "delete":
-            index = Integer.parseInt(instrSplit[1]);
-            message = "Noted. I've removed this task:\n";
-            message += "    " + taskList.delete(index) + "\n";
-            this.taskList.save();
-            break;
-        case "todo":
-            task = new Todo(instr);
-            message = "Got it. I've added this task:\n";
-            message += taskList.add(task);
-            this.taskList.save();
-            break;
-        case "deadline":
-            try {
-                task = new Deadline(instruction);
+            case "bye":
+                assert instrSplit.length == 1 : "Invalid no. of arguments";
+                message = "Bye. Hope to see you again soon!\n";
+                break;
+            case "list":
+                assert instrSplit.length == 1 : "Invalid no. of arguments";
+                message = "Here are the tasks in your list:\n" + taskList.list();
+                break;
+            case "mark":
+                assert instrSplit.length == 2 : "Invalid no. of arguments";
+                index = Integer.parseInt(instrSplit[1]);
+                message = "Nice! I've marked this task as done:\n";
+                message += "    " + taskList.mark(index) + "\n";
+                this.taskList.save();
+                break;
+            case "unmark":
+                assert instrSplit.length == 2 : "Invalid no. of arguments";
+                index = Integer.parseInt(instrSplit[1]);
+                message = "OK, I've marked this task as not done yet:\n";
+                message += "    " + taskList.unmark(index) + "\n";
+                this.taskList.save();
+                break;
+            case "delete":
+                assert instrSplit.length == 2 : "Invalid no. of arguments";
+                index = Integer.parseInt(instrSplit[1]);
+                message = "Noted. I've removed this task:\n";
+                message += "    " + taskList.delete(index) + "\n";
+                this.taskList.save();
+                break;
+            case "todo":
+                assert instrSplit.length > 1 : "Invalid no. of arguments";
+                task = new Todo(instr);
                 message = "Got it. I've added this task:\n";
                 message += taskList.add(task);
-            } catch (DateTimeParseException e) {
-                message = "Date format incorrect!!\n";
-                message += "Usage: 'deadline task /by YYYY-MM-DD'\n";
-            } catch (DukeException e) {
-                message = e.getMessage();
-            }
-            this.taskList.save();
-            break;
-        case "event":
-            task = new Event(instruction);
-            message = "Got it. I've added this task:\n";
-            message += taskList.add(task);
-            this.taskList.save();
-            break;
-        case "find":
-            String keyword = instrSplit[1];
-            message = taskList.find(keyword);
-            break;
-        default:
-            message = "☹ OOPS!!! I'm sorry, but I don't know what that means :-(\n";
-            break;
+                this.taskList.save();
+                break;
+            case "deadline":
+                assert instrSplit.length > 2 : "Invalid no. of arguments";
+                try {
+                    task = new Deadline(instruction);
+                    message = "Got it. I've added this task:\n";
+                    message += taskList.add(task);
+                } catch (DateTimeParseException e) {
+                    message = "Date format incorrect!!\n";
+                    message += "Usage: 'deadline task /by YYYY-MM-DD'\n";
+                } catch (DukeException e) {
+                    message = e.getMessage();
+                }
+                this.taskList.save();
+                break;
+            case "event":
+                assert instrSplit.length > 3 : "Invalid no. of arguments";
+                task = new Event(instruction);
+                message = "Got it. I've added this task:\n";
+                message += taskList.add(task);
+                this.taskList.save();
+                break;
+            case "find":
+                assert instrSplit.length > 1 : "Invalid no. of arguments";
+                String keyword = instrSplit[1];
+                message = taskList.find(keyword);
+                break;
+            default:
+                message = "☹ OOPS!!! I'm sorry, but I don't know what that means :-(\n";
+                break;
         }
         return message;
     }
