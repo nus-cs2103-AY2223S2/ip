f553a71ec4072f95ac7e9a5072e0ceb6da67b4af
Daniel-Kur <daniel.classroom2003@gmail.com>
2023-02-17 09:24:13 +0800

    Fix typo in javadoc

--- src/main/java/commands/ListCommand.java
+++ src/main/java/commands/ListCommand.java
@@ -5,7 +5,7 @@ import tasks.TaskList;
 import ui.Ui;
 
 /**
- * Represents command for printing the current list of tasks
+ * Represents command for printing the current list of tasks in the TaskList
  */
 public class ListCommand extends Command{
     public ListCommand(String command) {
