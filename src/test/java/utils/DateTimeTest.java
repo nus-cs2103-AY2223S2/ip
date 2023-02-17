f7536e58cb4dcd3753a09a0d46c3dcdd99b6c883
QIU QIANHUI <672271700@qq.com>
2023-02-17 20:09:38 +0800

    Fix the checkstyle error in the DateTimeTest class

--- src/test/java/utils/DateTimeTest.java
+++ src/test/java/utils/DateTimeTest.java
@@ -12,7 +12,7 @@ public class DateTimeTest {
     public void getDateTimeTest() {
         try {
             assertEquals(DateTime.dateFormatter("2018-08-05 1800"), "Aug 05 2018 18:00");
-            assertEquals(DateTime.dateFormatter("2022-02-02 0937"),"Feb 02 2022 09:37");
+            assertEquals(DateTime.dateFormatter("2022-02-02 0937"), "Feb 02 2022 09:37");
         } catch (DukeException e) {
             System.out.println(e.getMessage());
         }
