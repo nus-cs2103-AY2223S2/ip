d36165bfdbcd06fb57b947eec5417b176d6bc83f
joytqt-1202 <joytanqiaotong@gmail.com>
2023-02-18 21:32:42 +0800

    Add javadocs for storage package files

--- src/main/java/storage/Storage.java
+++ src/main/java/storage/Storage.java
@@ -10,12 +10,19 @@ import java.util.Scanner;
 
 import exceptions.FileLoadException;
 
+/**
+ * This class represents the storage for Duke to load and store previous task list
+ * from database of local file
+ */
 public class Storage {
 
     private String home;
     private Path path;
     private File file;
 
+    /**
+     * Creates storage for Duke
+     */
     public Storage() {
         this.home = System.getProperty("user.dir");
         this.path = Paths.get(this.home, "../../../DataDuke");
@@ -24,15 +31,17 @@ public class Storage {
         if (!Files.exists(this.path)) {
             try {
                 Files.createDirectories(this.path);
-            } catch (IOException ignore) { }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-        
+
     }
 
     /**
-     * to load previous task list from local storage into the application
+     * Load previous task list from local storage into the application
      * 
-     * @return previous task list in string
+     * @return previous task list data as a string
      * @throws FileLoadException
      */
     public String load() throws FileLoadException {
@@ -50,8 +59,9 @@ public class Storage {
     }
 
     /**
-     * saves the updated listed into a txt file
+     * Saves the updated listed into a local file
      * 
+     * @param taskList current task list to be saved for next use
      * @throws IOException
      */
     public void store(TaskList taskList) throws IOException {
@@ -62,7 +72,7 @@ public class Storage {
         } catch (FileNotFoundException e) {
                 Files.createDirectories(path);
                 stream = new PrintStream(file.toString());
-        } 
+        }
         for (int i = 0; i < taskList.size(); i++) {
             stream.print(taskList.get(i).stringifyTaskToSave() + "\n");
         }
