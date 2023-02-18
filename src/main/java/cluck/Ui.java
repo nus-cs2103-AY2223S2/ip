1fca158e3f2785435adaf51296b3d7e852d58760
ChickenChiang <96032660+ChickenChiang@users.noreply.github.com>
2023-02-18 22:44:48 +0800

    Added functions for UI
    Added greetUser and farewellUser functions for Ui to display start and end messages for Cluck, using the messages package

--- src/main/java/cluck/Ui.java
+++ src/main/java/cluck/Ui.java
@@ -1,8 +1,30 @@
 package cluck;
 
+import java.io.File;
+import java.util.Scanner;
+
+import cluck.messages.Messages;
+
+
+
 /**
  * Cluck.Ui handles user commands and outputs messages from Cluck.Duke
  **/
 public class Ui {
-    private
+    private final Scanner userInput = new Scanner(System.in);
+
+    public Ui() {}
+
+    public void greetUser() {
+        System.out.println(Messages.MESSAGE_WELCOME);
+    }
+
+    public void farewellUser() {
+        System.out.println(Messages.MESSAGE_GOODBYE);
+    }
+
+    public void readCommand() {
+        userInput.hasNextLine();
+    }
+
 }
