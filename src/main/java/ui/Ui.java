0017e16f8957841f32bd7b3da3e3001c563d56fb
joytqt-1202 <e0775479@u.nus.edu>
2023-02-19 01:46:03 +0800

    Add Javadocs for ui package

--- src/main/java/ui/Ui.java
+++ src/main/java/ui/Ui.java
@@ -3,15 +3,22 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+ * This class is the user interface of Duke.
+ * Consists of the main bulk of processes for interacting with the user
+ */
 public class Ui {
 
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    /** Reader for faster scanning of input*/
+    private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+    /** Logo of Duke */
     final String logo = " ____        _        \n"
                         + "|  _ \\ _   _| | _____ \n"
                         + "| | | | | | | |/ / _ \\\n"
                         + "| |_| | |_| |   <  __/\n"
                         + "|____/ \\__,_|_|\\_\\___|\n";
+
     /**
      * Prints Duke's greeting to the user
      */
@@ -19,18 +26,20 @@ public class Ui {
         printResponse("Hello from\n" + logo + "\n Please wait while Duke loads your previous list...");
         try {
             Thread.sleep(500);
-        } catch (InterruptedException ignore) { }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
-     * Notify user of successful loading of previous task list
+     * Notifies user of successful loading of previous task list
      */
     public void notifySuccessfulLoad() {
         printResponse("List successfully loaded! Please carry on with your Duke-y activities!");
     }
 
-    /** 
-     * Gather input commands from user
+    /**
+     * Gathers input commands from user
      * 
      * @return String of user input
      */
@@ -42,9 +51,9 @@ public class Ui {
             return getUserInput();
         }
     }
-    
+
     /**
-     * Standardises Duke's response printing format by enclosing the response within 2 lines
+     * Standardises Duke's response printing format by enclosing the response within 2 horizontal lines
      * 
      * @param response Duke's response to User
      */
@@ -54,6 +63,11 @@ public class Ui {
         System.out.println("\n----------------------------------\n");
     }
 
+    /**
+     * Prints exception message
+     * 
+     * @param e exception thrown
+     */
     public void printException(Exception e) {
         printResponse(e.getMessage());
     }
@@ -62,6 +76,8 @@ public class Ui {
         printResponse("Bye! Hope you enjoyed using Duke! \n Your list awaits your return!");
         try {
             br.close();
-        } catch (IOException ignore) { }
+        } catch (IOException e) {
+            printResponse("NO! U ALREADY SAID BYE!\nLaunch Duke again if you want to talk to Duke again! BYE!");
+        }
     }
 }
