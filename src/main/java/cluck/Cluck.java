a653274eea9cbaaf92a8d92f6af4cf5983e53bcc
ChickenChiang <96032660+ChickenChiang@users.noreply.github.com>
2023-02-19 14:14:53 +0800

    Parser class now builds commands instead of Cluck

--- src/main/java/cluck/Cluck.java
+++ src/main/java/cluck/Cluck.java
@@ -1,30 +1,22 @@
 package cluck;
 
 import java.io.File;
-import java.util.Scanner;
 
-import cluck.messages.Messages;
-import cluck.tasks.Deadline;
-import cluck.tasks.Event;
-import cluck.tasks.Task;
-import cluck.tasks.ToDo;
+import cluck.commands.Command;
+import cluck.exceptions.CluckException;
+import cluck.parser.Parser;
 import cluck.taskList.TaskList;
 import cluck.ui.Ui;
 
 
-public class Cluck {
-    private static final String MAKE_DEADLINE = "deadline";
-    private static final String MAKE_TODO = "todo";
-    private static final String MAKE_EVENT = "event";
-    private static final String DUE_DATE_FLAG = "/by ";
-    private static final String EVENT_START_FLAG = "/from ";
-    private static final String EVENT_END_FLAG = "/to ";
 
+public class Cluck {
     private static final String SAVE_DIR_STRING = "SavedData";
     private static final String SAVE_FILE_STRING = "CluckSave.txt";
 
     private TaskList taskList;
-    private Ui userInterface;
+    private Ui ui;
+    private boolean isRunning = true;
 
     /**
      * @param strNum String of interest.
@@ -47,17 +39,30 @@ public class Cluck {
      */
     public Cluck() {
         File savedFile = new File(SAVE_FILE_STRING);
-        this.userInterface = new Ui();
-        this.taskList = new TaskList().readSave(savedFile);
+        this.ui = new Ui();
+        this.taskList = new TaskList();
     }
 
     /**
      * Starts cluck instance such that it loads saved data and
      * begins taking in user commands.
      */
-    public void run() {
-        this.userInterface.greetUser();
+    public void start() {
+        ui.greetUser();
+        String userInput;
+        String response;
+        Command currCommand;
+        while (this.isRunning) {
+            userInput = ui.readInput();
+            try {
+                currCommand = Parser.commandFactory(userInput);
+                response = currCommand.execute(taskList);
+            } catch (CluckException e) {
+                response = e.getMessage();
+            }
 
+            ui.printResponse(response);
+        }
     }
 
     /**
@@ -71,120 +76,7 @@ public class Cluck {
      * @param args
      */
     public static void main(String[] args) {
-        System.out.println(Messages.MESSAGE_LOGO);
-        System.out.println(Messages.MESSAGE_WELCOME);
 
-        boolean loop = true;
-        TaskList toDoList = TaskList.readSave();
-        Scanner sc = new Scanner(System.in);
 
-        while (loop) {
-            String input = sc.nextLine();
-            System.out.println(input);
-            String[] words = input.split(" ");
-
-            switch (words[0]) {
-            case "bye":
-                writeSave(toDoList);
-                System.out.println(Messages.MESSAGE_GOODBYE);
-                loop = false;
-                break;
-
-            case "list":
-                System.out.println(Messages.MESSAGE_LIST_DISPLAY);
-                for (int i = 0; i < toDoList.size(); i++) {
-                    System.out.println("    " + (i + 1) + ": " + toDoList.get(i).toString());
-                }
-                break;
-
-            case "mark":
-                if (words.length == 1) {
-                    System.out.println(Messages.MESSAGE_INDEX_MISSING);
-                } else if (isNumeric(words[1])) {
-                    Integer itemNumber = Integer.parseInt(words[1]);
-                    if (itemNumber > toDoList.size() || itemNumber <= 0) {
-                        System.out.println(Messages.MESSAGE_INDEX_OUT_OF_BOUNDS);
-                    } else {
-                        toDoList.get(itemNumber - 1).mark();
-                        System.out.println(Messages.MESSAGE_MARK_SUCCESSFUL
-                                + toDoList.get(itemNumber - 1).toString());
-                    }
-                } else {
-                    System.out.println(Messages.MESSAGE_INDEX_INVALID);
-                }
-                break;
-
-            case "unmark":
-                if (words.length == 1) {
-                    System.out.println(Messages.MESSAGE_INDEX_MISSING);
-                } else if (isNumeric(words[1])) {
-                    Integer itemNumber = Integer.parseInt(words[1]);
-                    if (itemNumber > toDoList.size() || itemNumber <= 0) {
-                        System.out.println(Messages.MESSAGE_INDEX_OUT_OF_BOUNDS);
-                    } else {
-                        toDoList.get(itemNumber - 1).unmark();
-                        System.out.println(Messages.MESSAGE_UNMARK_SUCCESSFUL
-                                + toDoList.get(itemNumber - 1).toString());
-                    }
-                } else {
-                    System.out.println(Messages.MESSAGE_INDEX_INVALID);
-                }
-                break;
-
-            case MAKE_TODO:
-                Task newTodo = new ToDo(input.substring(5));
-                toDoList.add(newTodo);
-                System.out.println(Messages.MESSAGE_TODO_ADDED + "\n" + newTodo.toString());
-                System.out.println(String.format(Messages.MESSAGE_LIST_COUNT, toDoList.size()));
-                break;
-
-            case MAKE_DEADLINE:
-                String body = input.substring(9);
-                if (body.contains(DUE_DATE_FLAG)) {
-                    String[] fields = body.split(" " + DUE_DATE_FLAG);
-                    String description = fields[0];
-                    String dueDate = fields[1];
-                    Task currDeadline = new Deadline(description, dueDate);
-                    toDoList.add(currDeadline);
-                    System.out.println(Messages.MESSAGE_DEADLINE_ADDED + currDeadline.toString());
-                    System.out.println(String.format(Messages.MESSAGE_LIST_COUNT, toDoList.size()));
-                    break;
-                }
-                System.out.println("    You're missing the '/by' flag, bucko!");
-                break;
-
-            case MAKE_EVENT:
-                String substring = input.substring(6);
-                if (substring.contains(EVENT_START_FLAG) && substring.contains(EVENT_END_FLAG)) {
-                    String[] fields = substring.split("\\s/\\w{2,4}\\s");
-                    Task currEvent = new Event(fields[0], fields[1], fields[2]);
-                    toDoList.add(currEvent);
-                    System.out.println(Messages.MESSAGE_EVENT_ADDED + currEvent.toString());
-                    System.out.println(String.format(Messages.MESSAGE_LIST_COUNT, toDoList.size()));
-                    break;
-                }
-                System.out.println("    You're missing the either the '/from' or '/to' flag, or both! Buhcock!");
-                break;
-
-            case "delete":
-                if (words.length == 1) {
-                    System.out.println(Messages.MESSAGE_INDEX_MISSING);
-                } else if (isNumeric(words[1])) {
-                    Integer itemNumber = Integer.parseInt(words[1]);
-                    if (itemNumber > toDoList.size() || itemNumber <= 0) {
-                        System.out.println(Messages.MESSAGE_INDEX_OUT_OF_BOUNDS);
-                    } else {
-                        System.out.println(Messages.MESSAGE_DELETE_SUCCESSFUL + "\n"
-                                + toDoList.get(itemNumber - 1).toString());
-                    }
-                } else {
-                    System.out.println(Messages.MESSAGE_INDEX_INVALID);
-                }
-                break;
-
-            default:
-                System.out.println(Messages.MESSAGE_INVALID_COMMAND);
-            }
-        }
     }
 }
--- src/main/java/cluck/parser/Parser.java
+++ src/main/java/cluck/parser/Parser.java
@@ -1,6 +1,12 @@
 package cluck.parser;
 
 import cluck.commands.Command;
+import cluck.messages.Messages;
+import cluck.tasks.Deadline;
+import cluck.tasks.Event;
+import cluck.tasks.Task;
+import cluck.tasks.ToDo;
+
 /**
  * Parser class takes user input and parses it into commands
  * that can be executed by Cluck in other packages.
@@ -17,8 +23,10 @@ public class Parser {
     private static final String SAVE_FILE_STRING = "CluckSave.txt";
 
     /**
+     * Returns true if input is a number in string format, false otherwise.
+     *
      * @param strNum String of interest.
-     * @return returns true if strNum is a number in string format, false otherwise.
+     * @return boolean value
      */
     public static boolean isNumeric(String strNum) {
         if (strNum == null) {
@@ -37,7 +45,109 @@ public class Parser {
      *
      * @return sublcass of Command
      */
-    public static Command commandFactory() {
+    public static Command commandFactory(String userInput) {
+        String[] words = userInput.split(" ");
 
+        switch (words[0]) {
+            case "bye":
+                writeSave(toDoList);
+                System.out.println(Messages.MESSAGE_GOODBYE);
+                loop = false;
+                break;
+
+            case "list":
+                System.out.println(Messages.MESSAGE_LIST_DISPLAY);
+                for (int i = 0; i < toDoList.size(); i++) {
+                    System.out.println("    " + (i + 1) + ": " + toDoList.get(i).toString());
+                }
+                break;
+
+            case "mark":
+                if (words.length == 1) {
+                    System.out.println(Messages.MESSAGE_INDEX_MISSING);
+                } else if (isNumeric(words[1])) {
+                    Integer itemNumber = Integer.parseInt(words[1]);
+                    if (itemNumber > toDoList.size() || itemNumber <= 0) {
+                        System.out.println(Messages.MESSAGE_INDEX_OUT_OF_BOUNDS);
+                    } else {
+                        toDoList.get(itemNumber - 1).mark();
+                        System.out.println(Messages.MESSAGE_MARK_SUCCESSFUL
+                                + toDoList.get(itemNumber - 1).toString());
+                    }
+                } else {
+                    System.out.println(Messages.MESSAGE_INDEX_INVALID);
+                }
+                break;
+
+            case "unmark":
+                if (words.length == 1) {
+                    System.out.println(Messages.MESSAGE_INDEX_MISSING);
+                } else if (isNumeric(words[1])) {
+                    Integer itemNumber = Integer.parseInt(words[1]);
+                    if (itemNumber > toDoList.size() || itemNumber <= 0) {
+                        System.out.println(Messages.MESSAGE_INDEX_OUT_OF_BOUNDS);
+                    } else {
+                        toDoList.get(itemNumber - 1).unmark();
+                        System.out.println(Messages.MESSAGE_UNMARK_SUCCESSFUL
+                                + toDoList.get(itemNumber - 1).toString());
+                    }
+                } else {
+                    System.out.println(Messages.MESSAGE_INDEX_INVALID);
+                }
+                break;
+
+            case MAKE_TODO:
+                Task newTodo = new ToDo(input.substring(5));
+                toDoList.add(newTodo);
+                System.out.println(Messages.MESSAGE_TODO_ADDED + "\n" + newTodo.toString());
+                System.out.println(String.format(Messages.MESSAGE_LIST_COUNT, toDoList.size()));
+                break;
+
+            case MAKE_DEADLINE:
+                String body = input.substring(9);
+                if (body.contains(DUE_DATE_FLAG)) {
+                    String[] fields = body.split(" " + DUE_DATE_FLAG);
+                    String description = fields[0];
+                    String dueDate = fields[1];
+                    Task currDeadline = new Deadline(description, dueDate);
+                    toDoList.add(currDeadline);
+                    System.out.println(Messages.MESSAGE_DEADLINE_ADDED + currDeadline.toString());
+                    System.out.println(String.format(Messages.MESSAGE_LIST_COUNT, toDoList.size()));
+                    break;
+                }
+                System.out.println("    You're missing the '/by' flag, bucko!");
+                break;
+
+            case MAKE_EVENT:
+                String substring = input.substring(6);
+                if (substring.contains(EVENT_START_FLAG) && substring.contains(EVENT_END_FLAG)) {
+                    String[] fields = substring.split("\\s/\\w{2,4}\\s");
+                    Task currEvent = new Event(fields[0], fields[1], fields[2]);
+                    toDoList.add(currEvent);
+                    System.out.println(Messages.MESSAGE_EVENT_ADDED + currEvent.toString());
+                    System.out.println(String.format(Messages.MESSAGE_LIST_COUNT, toDoList.size()));
+                    break;
+                }
+                System.out.println("    You're missing the either the '/from' or '/to' flag, or both! Buhcock!");
+                break;
+
+            case "delete":
+                if (words.length == 1) {
+                    System.out.println(Messages.MESSAGE_INDEX_MISSING);
+                } else if (isNumeric(words[1])) {
+                    Integer itemNumber = Integer.parseInt(words[1]);
+                    if (itemNumber > toDoList.size() || itemNumber <= 0) {
+                        System.out.println(Messages.MESSAGE_INDEX_OUT_OF_BOUNDS);
+                    } else {
+                        System.out.println(Messages.MESSAGE_DELETE_SUCCESSFUL + "\n"
+                                + toDoList.get(itemNumber - 1).toString());
+                    }
+                } else {
+                    System.out.println(Messages.MESSAGE_INDEX_INVALID);
+                }
+                break;
+
+            default:
+                System.out.println(Messages.MESSAGE_INVALID_COMMAND);
     }
 }
