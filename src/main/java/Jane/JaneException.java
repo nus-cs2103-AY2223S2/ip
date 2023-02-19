6e4d68df9018c49edbdffc42d00ad162d9bc38c2
sembcorpp <e0772507@u.nus.edu>
2023-02-20 01:01:26 +0800

    Making a release

--- src/main/java/Jane/JaneException.java
+++ src/main/java/Jane/JaneException.java
@@ -1,6 +1,8 @@
 package jane;
 
-
+/**
+ * Exception for methods to throw if there are errors
+ */
 public class JaneException extends Exception {
     public JaneException(String err) {
         super(err);
