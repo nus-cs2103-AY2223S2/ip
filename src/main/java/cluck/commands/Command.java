3e877f7fc580f97e717c022674a1128dc061e51f
ChickenChiang <96032660+ChickenChiang@users.noreply.github.com>
2023-02-19 12:58:59 +0800

    Changed Command class into and interface instead
    It seems to make more sense for Command to be an interface that commands implement

--- src/main/java/cluck/commands/Command.java
+++ src/main/java/cluck/commands/Command.java
@@ -3,8 +3,6 @@ package cluck.commands;
 /**
  * Abstract class for Commands, defines the default of commands.
  */
-public abstract class  Command {
-
-    public abstract void execute();
-    
+public interface Command {
+    void execute();
 }
