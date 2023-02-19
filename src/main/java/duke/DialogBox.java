abf60b47f78fe9e0732e2df41285ff5264318907
zhacatomn <zhacatomn@gmail.com>
2023-02-19 17:23:30 +0800

    Refactor to use fxml

--- src/main/java/duke/DialogBox.java
+++ src/main/java/duke/DialogBox.java
@@ -1,11 +1,17 @@
 package duke;
 
+import java.io.IOException;
+
+import duke.ui.MainWindow;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
@@ -21,46 +27,40 @@ public class DialogBox extends HBox {
     private static final Color DUKE_BG_COLOR = Color.rgb(54, 54, 54);
     private static final Color USER_BG_COLOR = Color.rgb(2, 119, 189);
     private static final CornerRadii CORNER_RADIUS = new CornerRadii(5);
-    private static final double IMAGE_WIDTH = 40.0;
-    private static final double IMAGE_HEIGHT = 40.0;
+    @FXML
+    private Label dialog;
+    @FXML
     private ImageView displayPicture;
-    private Label text;
+
 
     /**
      * Creates a new DialogBox.
      * @param l Label object to be shown
      * @param iv ImageView object to be shown
      */
-    private DialogBox(Label l, ImageView iv, Color labelColor) {
-        text = l;
-        displayPicture = iv;
-
-        text.setWrapText(true);
-        displayPicture.setFitWidth(IMAGE_WIDTH);
-        displayPicture.setFitHeight(IMAGE_HEIGHT);
-
-        this.setAlignment(Pos.TOP_RIGHT);
-        this.getChildren().addAll(text, displayPicture);
-
-        // Adding padding to the dialog box
-        this.setPadding(new Insets(10, 10, 0, 10));
-        this.setSpacing(5);
-
-        // Styling the Label
-        text.setPadding(new Insets(5, 10, 5, 10));
-        text.setBackground(new Background(
-                new BackgroundFill(
-                        labelColor,
-                        CORNER_RADIUS,
-                        Insets.EMPTY
-                )
+    private DialogBox(String l, Image iv, Color labelColor) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(MainWindow.class.getResource("/view/DialogBox.fxml"));
+            fxmlLoader.setController(this);
+            fxmlLoader.setRoot(this);
+            fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // Styling dialog
+        dialog.setText(l);
+        dialog.setBackground(new Background(
+               new BackgroundFill(
+                       labelColor,
+                       CORNER_RADIUS,
+                       Insets.EMPTY
+               )
         ));
-        text.setTextFill(Color.WHITE);
-
-        // Clipping the image to be a circle
+        // Styling image
         double radius = displayPicture.getFitWidth() / 2;
         Circle circleClip = new Circle(radius, radius, radius);
         displayPicture.setClip(circleClip);
+        displayPicture.setImage(iv);
     }
 
     /**
@@ -79,7 +79,7 @@ public class DialogBox extends HBox {
      * @param iv ImageView of the user.
      * @return DialogBox for the user to be served in the GUI.
      */
-    public static DialogBox getUserDialog(Label l, ImageView iv) {
+    public static DialogBox getUserDialog(String l, Image iv) {
         return new DialogBox(l, iv, USER_BG_COLOR);
     }
 
@@ -89,7 +89,7 @@ public class DialogBox extends HBox {
      * @param iv ImageView of duke.
      * @return DialogBox for duke to be served in the GUI.
      */
-    public static DialogBox getDukeDialog(Label l, ImageView iv) {
+    public static DialogBox getDukeDialog(String l, Image iv) {
         var db = new DialogBox(l, iv, DUKE_BG_COLOR);
         db.flip();
         return db;
--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,47 +1,15 @@
 package duke;
 
-import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.stage.Stage;
-
 /**
  * Class encapsulating the entire Duke application.
  */
-public class Duke extends Application {
+public class Duke {
     protected static final String DATA_DIR = "data/";
     protected static final String DATA_FILENAME = "duke.txt";
-    protected static final String WELCOME_MESSAGE = "Hello! I'm Duke!\nWhat can I do for you?";
-    protected static final String EXIT_MESSAGE = "Bye. Hope to see you again soon!";
-    protected static final String EXIT_COMMAND = "bye";
-    private static final double STAGE_WIDTH = 500;
-    private static final double STAGE_HEIGHT = 750;
-    private static final double SEND_BUTTON_WIDTH = 55;
     private final Storage storage;
     private TaskList tasks;
     private final Parser parser;
     private final Ui ui;
-
-    // For javaFX UI
-    private ScrollPane scrollPane;
-    private VBox dialogContainer;
-    private TextField userInput;
-    private Button sendButton;
-    private Scene scene;
-    private final Image user = new Image(this.getClass().getResourceAsStream("/images/cartoon_user.png"));
-    private final Image duke = new Image(this.getClass().getResourceAsStream(("/images/cartoon_robot.png")));
 
     public Duke() {
         this(DATA_DIR + DATA_FILENAME);
@@ -65,103 +33,21 @@ public class Duke extends Application {
         assert tasks != null : "tasks should not be null";
     }
 
-    @Override
-    public void start(Stage stage) {
-        // Setting up components
-        scrollPane = new ScrollPane();
-        dialogContainer = new VBox();
-        scrollPane.setContent(dialogContainer);
-
-        userInput = new TextField();
-        sendButton = new Button("Send");
-
-        AnchorPane mainLayout = new AnchorPane();
-        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
-
-        scene = new Scene(mainLayout);
-
-        stage.setScene(scene);
-        stage.show();
-
-        // Formatting window
-        stage.setTitle("Duke");
-        stage.setResizable(false);
-        stage.setHeight(STAGE_HEIGHT);
-        stage.setWidth(STAGE_WIDTH);
-
-        mainLayout.setPrefSize(STAGE_WIDTH, STAGE_HEIGHT);
-
-        scrollPane.setPrefSize(STAGE_WIDTH, STAGE_HEIGHT - 75);
-        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-        scrollPane.setVvalue(1.0);
-        scrollPane.setFitToWidth(true);
-
-        dialogContainer.setMinHeight(STAGE_HEIGHT - 75); // Fit to scrollPane
-
-        userInput.setPrefWidth(STAGE_WIDTH - SEND_BUTTON_WIDTH);
-
-        sendButton.setPrefWidth(SEND_BUTTON_WIDTH);
-
-        AnchorPane.setTopAnchor(scrollPane, 1.0);
-        AnchorPane.setBottomAnchor(sendButton, 1.0);
-        AnchorPane.setRightAnchor(sendButton, 1.0);
-        AnchorPane.setLeftAnchor(userInput, 1.0);
-        AnchorPane.setBottomAnchor(userInput, 1.0);
-
-        // Styling ScrollPane
-        dialogContainer.setBackground(new Background(
-                        new BackgroundFill(
-                                Color.rgb(40, 40, 40),
-                                CornerRadii.EMPTY,
-                                Insets.EMPTY)
-                )
-        );
-        dialogContainer.setPadding(new Insets(0, 0, 10, 0));
-
-        // Add functionality
-        sendButton.setOnMouseClicked((event) -> {
-            handleUserInput(stage);
-        });
-
-        userInput.setOnAction((event) -> {
-            handleUserInput(stage);
-        });
-        // Scroll down to the end every time dialogContainer's height changes
-        dialogContainer.heightProperty().addListener((observable -> scrollPane.setVvalue(1.0)));
-
-        // Send an initial welcome message
-        dialogContainer.getChildren().add(DialogBox.getDukeDialog(
-                new Label(WELCOME_MESSAGE),
-                new ImageView(duke))
-        );
-    }
-
     /**
-     * Handles the text input given by the user through the GUI.
+     * Returns a response, given a command.
+     *
+     * @param command Command given by the user.
+     * @return Response from the bot.
      */
-    private void handleUserInput(Stage stage) {
-        String userInputStr = userInput.getText().trim();
-        if (userInputStr.equalsIgnoreCase(EXIT_COMMAND)) {
-            dialogContainer.getChildren().addAll(
-                    DialogBox.getDukeDialog(new Label(EXIT_MESSAGE), new ImageView(duke))
-            );
-            stage.close();
-            return;
-        }
-        Label userText = new Label(userInputStr);
-        String dukeResponseStr;
+    public String getResponse(String command) {
+        String trimmedCommand = command.trim();
+        String response;
         try {
-            dukeResponseStr = parser.parseString(userInputStr, tasks, ui);
+            response = parser.parseString(trimmedCommand, tasks, ui);
             storage.save(tasks);
         } catch (BadCommandException | DukeException e) {
-            dukeResponseStr = ui.showErrorMessage(e.getMessage());
+            response = ui.showErrorMessage(e.getMessage());
         }
-        Label dukeText = new Label(dukeResponseStr);
-        dialogContainer.getChildren().addAll(
-                DialogBox.getUserDialog(userText, new ImageView(user)),
-                DialogBox.getDukeDialog(dukeText, new ImageView(duke))
-        );
-        userInput.clear();
+        return response;
     }
 }
