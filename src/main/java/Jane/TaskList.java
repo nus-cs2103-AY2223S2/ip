f2b5748f8a05011c7cb8165db8425657717fe225
sembcorpp <e0772507@u.nus.edu>
2023-02-20 01:27:13 +0800

    Updating handle methods in TaskList

--- src/main/java/Jane/TaskList.java
+++ src/main/java/Jane/TaskList.java
@@ -11,50 +11,124 @@ public class TaskList {
     public TaskList(ArrayList<jane.task.Task> tasks) {
         this.tasks = tasks;
     }
-
+    /**
+     * Processes input command "bye
+     */
+    public String handleBye() {
+        List<String> currentList = new ArrayList<>();
+        for (jane.task.Task t : tasks) {
+            currentList.add(t.save());
+        }
+        try {
+            jane.Storage.updateList(tasks);
+            return "BYE! see you soon";
+        } catch (Exception err) {
+            err.printStackTrace();
+            return ("cannot save list");
+        }
+    }
+    /**
+     * Processes input commands "find"
+     * @param output is user's input
+     */
+    public String handleFind(String output) {
+        String[] s = output.split(" ");
+        StringBuilder finalOutput = new StringBuilder();
+        String action = s[1];
+        for (jane.task.Task i : tasks) {
+            if (i.getDesc().contains(action)) {
+                finalOutput.append(i);
+            }
+        }
+        return finalOutput.toString();
+    }
+    /**
+     * Processes input command "mark"
+     * @param output is user's input
+     */
+    public String handleMark(String output) {
+        String[] s = output.split(" ");
+        StringBuilder finalOutput = new StringBuilder();
+        int num = Integer.parseInt(s[1]);
+        try {
+            finalOutput.append("Nice! I've marked this task as done");
+            jane.task.Task n = tasks.get(num - 1);
+            n.changeState(true);
+            finalOutput.append(n);
+            return finalOutput.toString();
+        } catch (Exception err) {
+            err.printStackTrace();
+            return ("Number out of index");
+        }
+    }
+    /**
+     * Processes input command "unmark"
+     * @param output is user's input
+     */
+    public String handleUnmark(String output) {
+        String[] s = output.split(" ");
+        StringBuilder finalOutput = new StringBuilder();
+        int num = Integer.parseInt(s[1]);
+        try {
+            finalOutput.append("OK, I've marked this task as not done yet" + "\n");
+            jane.task.Task n = tasks.get(num - 1);
+            n.changeState(false);
+            finalOutput.append(n);
+            return finalOutput.toString();
+        } catch (Exception err) {
+            finalOutput.append("Number out of index");
+            err.printStackTrace();
+            return finalOutput.toString();
+        }
+    }
+    /**
+     * Processes input command "delete"
+     * @param output is user's input
+     */
+    public String handleDelete(String output) {
+        String[] s = output.split(" ");
+        StringBuilder finalOutput = new StringBuilder();
+        int num = Integer.parseInt(s[1]);
+        try {
+            finalOutput.append("Noted. I've removed this task:" + "\n");
+            jane.task.Task n = tasks.get(num - 1);
+            finalOutput.append(n.toString());
+            for (int j = num; j < tasks.size(); j++) {
+                jane.task.Task t = tasks.get(j);
+                t.changeNum();
+            }
+            tasks.remove(n);
+            finalOutput.append("You now have " + tasks.size() + " tasks");
+            return finalOutput.toString();
+        } catch (Exception err) {
+            finalOutput.append("Number out of index");
+            err.printStackTrace();
+        }
+        return finalOutput.toString();
+    }
+    /**
+     * Processes input command "list"
+     */
+    public String handleList() {
+        StringBuilder finalOutput = new StringBuilder();
+        for (jane.task.Task task : tasks) {
+            String s1 = task.toString() + "\n";
+            finalOutput.append(s1);
+        }
+        return finalOutput.toString();
+    }
     /**
      * Processes input commands
      * @param output is user's input
-     * @throws JaneException if input format is wrong
-     * @throws JaneException if input is invalid
      */
     public String useCommand(String output) {
         StringBuilder finalOutput = new StringBuilder();
         if (output.equals("bye")) {
-            List<String> currentList = new ArrayList<>();
-            for (jane.task.Task t : tasks) {
-                currentList.add(t.save());
-            }
-            try {
-                jane.Storage.updateList(tasks);
-                return "BYE! see you soon";
-            } catch (Exception err) {
-                err.printStackTrace();
-                return ("cannot save list");
-            }
+            return handleBye();
         } else if (output.startsWith("find")) {
-            String[] s = output.split(" ");
-            String action = s[1];
-            for (jane.task.Task i : tasks) {
-                if (i.getDesc().contains(action)) {
-                    finalOutput.append(i);
-
-                }
-            }
-            return finalOutput.toString();
+            return handleFind(output);
         } else if (output.startsWith("mark")) {
-            String[] s = output.split(" ");
-            int num = Integer.parseInt(s[1]);
-            try {
-                finalOutput.append("Nice! I've marked this task as done");
-                jane.task.Task n = tasks.get(num - 1);
-                n.changeState(true);
-                finalOutput.append(n);
-                return finalOutput.toString();
-            } catch (Exception err) {
-                err.printStackTrace();
-                return ("Number out of index");
-            }
+            return handleMark(output);
         } else if (output.equals("todo") || output.equals("deadline") || output.equals("event")) {
             finalOutput.append("Please specify the task to be done :(((");
             return finalOutput.toString();
@@ -75,44 +149,11 @@ public class TaskList {
             finalOutput.append(e);
             return finalOutput.toString();
         } else if (output.startsWith("unmark")) {
-            String[] s = output.split(" ");
-            int num = Integer.parseInt(s[1]);
-            try {
-                finalOutput.append("OK, I've marked this task as not done yet" + "\n");
-                jane.task.Task n = tasks.get(num - 1);
-                n.changeState(false);
-                finalOutput.append(n);
-                return finalOutput.toString();
-            } catch (Exception err) {
-                finalOutput.append("Number out of index");
-                err.printStackTrace();
-                return finalOutput.toString();
-            }
+            return handleUnmark(output);
         } else if (output.startsWith("delete")) {
-            String[] s = output.split(" ");
-            int num = Integer.parseInt(s[1]);
-            try {
-                finalOutput.append("Noted. I've removed this task:" + "\n");
-                jane.task.Task n = tasks.get(num - 1);
-                finalOutput.append(n.toString());
-                for (int j = num; j < tasks.size(); j++) {
-                    jane.task.Task t = tasks.get(j);
-                    t.changeNum();
-                }
-                tasks.remove(n);
-                finalOutput.append("You now have " + tasks.size() + " tasks");
-                return finalOutput.toString();
-            } catch (Exception err) {
-                finalOutput.append("Number out of index");
-                err.printStackTrace();
-            }
-            return finalOutput.toString();
+            return handleDelete(output);
         } else if (output.equals("list")) {
-            for (jane.task.Task task : tasks) {
-                String s1 = task.toString() + "\n";
-                finalOutput.append(s1);
-            }
-            return finalOutput.toString();
+            return handleList();
         }
         return "Sorry I don't understand what do you mean :((";
     }
