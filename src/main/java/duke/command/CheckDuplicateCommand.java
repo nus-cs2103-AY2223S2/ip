646d6a15c01628ffe02292401fe066d76d3be766
tingyic <e0735450@u.nus.edu>
2023-02-20 00:12:30 +0800

    Add CheckDuplicateCommand.java

--- src/main/java/duke/command/CheckDuplicateCommand.java
+++ src/main/java/duke/command/CheckDuplicateCommand.java
@@ -1,2 +1,33 @@
-package duke.command;public class CheckDuplicateCommand {
+package duke.command;
+
+import java.util.HashMap;
+
+import duke.task.Task;
+import duke.task.TaskList;
+import duke.storage.Storage;
+import duke.UI.UI;
+
+public class CheckDuplicateCommand extends Command {
+    public static final String COMMAND = "check_duplicate";
+
+    @Override
+    public void runCommand(TaskList taskList, UI ui, Storage storage) {
+        HashMap<String, Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < taskList.numberOfTasks(); i++) {
+            Task task = taskList.getTask(i);
+
+            if (hashMap.get(task.toString()) != null) {
+                ui.printResponse("Goodness me. I have detected repeated tasks. \n" +
+                        "Tasks " + (hashMap.get(task.toString()) + 1) + " and " + (i + 1) +
+                        " are duplicates. \nPlease make amendments.");
+            }
+            hashMap.put(task.toString(), i);
+        }
+        ui.printResponse("Good work! I did not find any duplicate tasks.");
+    }
+
+    @Override
+    public boolean isExit() {
+        return false;
+    }
 }
