d21231b53b6bfcd7f48ccd56e7c88c39dc61894b
“ShawnYip-Au” <shawnyipau@gmail.com>
2023-02-19 10:08:34 +0800

    Make profile picture round

--- src/main/java/gui/DialogBox.java
+++ src/main/java/gui/DialogBox.java
@@ -13,6 +13,8 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
 
 /**
  * An example of a custom control using FXML.
@@ -25,6 +27,9 @@ public class DialogBox extends HBox {
     @FXML
     private ImageView displayPicture;
 
+    @FXML
+    private Circle circleImage;
+
     private DialogBox(String text, Image img) {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(MainWindow.class.getResource("/view/DialogBox.fxml"));
@@ -36,7 +41,8 @@ public class DialogBox extends HBox {
         }
 
         dialog.setText(text);
-        displayPicture.setImage(img);
+        //displayPicture.setImage(img);
+        circleImage.setFill(new ImagePattern(img));
     }
 
     /**
