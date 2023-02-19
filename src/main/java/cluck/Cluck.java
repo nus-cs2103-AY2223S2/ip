e51bf46e79cdb38f4976ea41f6817e990a67c855
ChickenChiang <96032660+ChickenChiang@users.noreply.github.com>
2023-02-19 15:42:08 +0800

    Reformatted cluck to fit OOP principles
    Minor fixes to cluck and reformatted to improve readability and abstraction.

--- src/main/java/cluck/Cluck.java
+++ src/main/java/cluck/Cluck.java
@@ -1,58 +1,39 @@
 package cluck;
 
-import java.io.File;
-
 import cluck.commands.Command;
 import cluck.exceptions.CluckException;
 import cluck.parser.Parser;
+import cluck.storage.Storage;
 import cluck.taskList.TaskList;
 import cluck.ui.Ui;
 
 
 
 public class Cluck {
-    private static final String SAVE_DIR_STRING = "SavedData";
-    private static final String SAVE_FILE_STRING = "CluckSave.txt";
-
     private TaskList taskList;
     private Ui ui;
     private boolean isRunning = true;
-
-    /**
-     * @param strNum String of interest.
-     * @return returns true if strNum is a number in string format, false otherwise.
-     */
-    public static boolean isNumeric(String strNum) {
-        if (strNum == null) {
-            return false;
-        }
-        try {
-            double d = Double.parseDouble(strNum);
-        } catch (NumberFormatException nfe) {
-            return false;
-        }
-        return true;
-    }
+    private Storage storage;
 
     /**
      * Cluck class contains tasklist, User interface and parser needed.
      */
-    public Cluck() {
-        File savedFile = new File(SAVE_FILE_STRING);
+    public Cluck(String filePath) {
         this.ui = new Ui();
-        this.taskList = new TaskList();
+        this.storage = new Storage(filePath);
+        this.taskList = storage.readSave();
     }
 
     /**
      * Starts cluck instance such that it loads saved data and
      * begins taking in user commands.
      */
-    public void start() {
+    public void run() {
         ui.greetUser();
         String userInput;
         String response;
         Command currCommand;
-        while (this.isRunning) {
+        while (isRunning) {
             userInput = ui.readInput();
             try {
                 currCommand = Parser.commandFactory(userInput);
@@ -60,23 +41,12 @@ public class Cluck {
             } catch (CluckException e) {
                 response = e.getMessage();
             }
-
             ui.printResponse(response);
         }
     }
 
-    /**
-     * Saves taskList and stop cluck instance
-     */
-    public void stop() {
-
-    }
-
-    /**
-     * @param args
-     */
     public static void main(String[] args) {
-
+        new Cluck("data/CluckSave.txt").run();
 
     }
 }
