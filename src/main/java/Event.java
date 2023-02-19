136f8240721920db1ebb071dcb723b7af7ab0426
Benny <benny.ong348@gmail.com>
2023-02-19 15:38:28 +0800

    branch level-7

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,72 +1,28 @@
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
-import java.time.temporal.ChronoUnit;
-
-
 public class Event extends Task {
-<<<<<<< .merge_file_a10728
-<<<<<<< Updated upstream
     String tag = "[E]";
-=======
-    String tag = "E";
->>>>>>> .merge_file_a17964
     protected String from;
     protected String to;
-=======
-    String tag = "E";
-    protected LocalDateTime from;
-    protected LocalDateTime to;
->>>>>>> Stashed changes
 
     public Event() {
         super.tag = tag;
     }
 
-    public Event(String description) {
-        super.tag = tag;
-        super.description = description;
-    }
-
     //KEEP ALL WORDS SEPARATED BY SPACES
     @Override
-    public void genDscp(String input) throws InvalidEvent {
-        if (input.isBlank()) {
+    public void genDscp(String input) throws InvalidEvent{
+        String dscp = input.replace("event ", "");
+        if (dscp.isBlank()) {
             throw new InvalidEvent();
         }
-        int fromId = input.indexOf("/from");
-        int toId = input.indexOf(("/to"));
+        int fromId = dscp.indexOf("/from");
+        int toId = dscp.indexOf(("/to"));
         if (fromId == -1 || toId == -1) {
             throw new InvalidEvent();
         }
-<<<<<<< .merge_file_a10728
-<<<<<<< Updated upstream
         this.from = dscp.substring(fromId + 6, toId - 1);
         this.to = dscp.substring(toId + 4);
         String task = String.format("%s (from: %s to: %s)", dscp.substring(0, fromId - 1), this.from, this.to);
         super.task = task;
-=======
-        try {
-            this.from = LocalDateTime.parse(input.substring(fromId + 6, toId - 1),
-                    DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm"));
-            this.to = LocalDateTime.parse(input.substring(toId + 4),
-                    DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm"));;
-        } catch (DateTimeParseException e) {
-            throw new InvalidEvent("Please enter date correctly");
-        }
-        String formattedFrom = this.from.format(DateTimeFormatter.ofPattern("MMM d yyyy HHmm"));
-        String formattedTo = this.to.format(DateTimeFormatter.ofPattern("MMM d yyyy HHmm"));
-
-        String description = String.format("%s (from: %s to: %s)", input.substring(0, fromId - 1),
-                formattedFrom, formattedTo);
-        super.description = description;
->>>>>>> Stashed changes
-=======
-        this.from = input.substring(fromId + 6, toId - 1);
-        this.to = input.substring(toId + 4);
-        String description = String.format("%s (from: %s to: %s)", input.substring(0, fromId - 1), this.from, this.to);
-        super.description = description;
->>>>>>> .merge_file_a17964
     }
 
     //Override toString
--- src/main/java/ToDo.java
+++ src/main/java/ToDo.java
@@ -1,12 +1,7 @@
 public class ToDo extends Task{
-<<<<<<< .merge_file_a17176
     String tag = "[T]";
     public ToDo() {
         super.tag = tag;
-    }
-    public ToDo(String description) {
-        super.tag = tag;
-        super.description = description;
     }
 
     @Override
@@ -16,19 +11,6 @@ public class ToDo extends Task{
             throw new InvalidTodo();
         }
         super.task = dscp;
-=======
-    String tag = "T";
-    public ToDo() {
-        super.tag = tag;
-    }
-
-    @Override
-    public void genDscp(String input) throws InvalidTodo {
-        if (input.isBlank()) {
-            throw new InvalidTodo();
-        }
-        super.description = input;
->>>>>>> .merge_file_a15092
     }
 
     //Override toString
