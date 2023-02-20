e100e4ff907abb622fdd328c783b2654c643904d
Benny <benny.ong348@gmail.com>
2023-02-20 18:48:23 +0800

    Commit message for Level-10
    Add GUI and JavaFX support
    
    As the program grows, text based UI is unable to fully and easily allow
    the user to view and edit information.
    
    Therefore a GUI is important as it allows users to view their information,
    as well as input data much easily.
    
    It also allows easier debugging as there is now visual indication for any bugs.

--- src/main/java/duke/duke/Ui.java
+++ src/main/java/duke/duke/Ui.java
@@ -6,16 +6,6 @@ import java.util.Scanner;
  * Deals with the reading user inputs and responding to their inputs.
  */
 public class Ui {
-
-    public void start() {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("Greetings");
-    }
     /**
      * Returns user inputs
      */
