dfc7d8e8b0fa49fddadf5c63138f451fc1a8b6ca
daniellingzy <e0773883@u.nus.edu>
2023-02-20 11:47:02 +0800

    Implemented Todo, Deadline, Event classes.

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,9 @@
+public class Deadline extends Task {
+
+    protected String deadline;
+
+    public Deadline(String description, String deadline) {
+        super(description);
+        this.deadline = deadline;
+    }
+}
