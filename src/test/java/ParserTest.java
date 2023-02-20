35bfc3587696ee60f1afcdf7c3c7b0e35ede6eea
Benny <benny.ong348@gmail.com>
2023-02-20 23:27:07 +0800

    no message

--- src/test/java/ParserTest.java
+++ src/test/java/ParserTest.java
@@ -12,7 +12,7 @@ public class ParserTest {
         Parser parser = new Parser(data);
         String msg = parser.parse("todo abc");
         assertEquals(data.getSize(), 1);
-        assertEquals(msg, "Now you have 1 tasks in the list");
+        assertEquals(msg, "added: abc\nNow you have 1 tasks in the list");
     }
 
     @Test
@@ -21,6 +21,6 @@ public class ParserTest {
         Parser parser = new Parser(data);
         String msg = parser.parse("event abc /from 2023-02-15 1800 /to 2023-02-26 1800");
         assertEquals(data.getSize(), 1);
-        assertEquals(msg, "Now you have 1 tasks in the list");
+        assertEquals(msg, "added: abc (from: Feb 15 2023 1800 to: Feb 26 2023 1800)\nNow you have 1 tasks in the list");
     }
 }
