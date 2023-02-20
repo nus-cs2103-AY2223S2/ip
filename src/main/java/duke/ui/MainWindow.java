0848ca2c4f3fa5790c0601d72b48e44009881761
homuntan02 <homuntan02@gmail.com>
2023-02-20 13:22:43 +0800

    GUI images used were basic
    User might find it boring
    
    Added new images for the GUI
    
    User can find it more fun to use the application

--- src/main/java/duke/ui/MainWindow.java
+++ src/main/java/duke/ui/MainWindow.java
@@ -26,8 +26,8 @@ public class MainWindow extends AnchorPane {
 
     private Duke duke;
 
-    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
-    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/EVA.png"));
+    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/wall-e.png"));
 
     @FXML
     public void initialize() {
