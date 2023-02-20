40c9f2c94b94f9287e771356d3ba277459b39502
szejiancheng <sze2001jc@gmail.com>
2023-02-20 12:35:02 +0800

    Tweak window formatting

--- src/main/java/duke/gui/DukeGui.java
+++ src/main/java/duke/gui/DukeGui.java
@@ -6,6 +6,7 @@ import duke.components.Duke;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -31,6 +32,8 @@ public class DukeGui extends Application {
         Scene scene = new Scene(ap);
         stage.setScene(scene);
         fxmlLoader.<MainWindow>getController().setDuke(duke);
+        stage.setTitle("Duke");
+        stage.getIcons().add(new Image(this.getClass().getResourceAsStream("/images/DaDuke.png")));
         stage.show();
     }
 }
