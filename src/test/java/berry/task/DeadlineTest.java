854c2be21f8ad93e19e8aaa8743a55319061c76f
Isabel <IsabelChong.CYJI@gmail.com>
2023-02-21 03:25:19 +0800

    Update DeadlineTest.java

--- src/test/java/berry/task/DeadlineTest.java
+++ src/test/java/berry/task/DeadlineTest.java
@@ -2,9 +2,9 @@ package berry.task;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import berry.exception.IncorrectDateException;
-
 import org.junit.jupiter.api.Test;
+
+import berry.exception.IncorrectDateException;
 
 public class DeadlineTest {
     @Test
