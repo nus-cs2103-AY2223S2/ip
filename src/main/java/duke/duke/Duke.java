601c137d0412be1e4634b8f49d0959949f4d606b
Benny <benny.ong348@gmail.com>
2023-02-20 18:44:29 +0800

    Commit message for Level-9
    Add Find command
    
    As the program expands, the user is now able to add more tasks that they want.
    
    In the case where the list of task gets too long, it might be difficult for
    the user to find a specific task that they want to view or mark.
    
    Therefore a find command is important as it allows the user
    to filter the task list using keywords, allowing them to find their
    tasks much faster.

--- src/main/java/duke/duke/Duke.java
+++ src/main/java/duke/duke/Duke.java
@@ -14,25 +14,6 @@ public class Duke {
         this.ui = new Ui();
         this.parser = new Parser(data);
     }
-    /**
-     * Loads preexisting data into the program, then reads user inputs
-     * for commands.
-     */
-//    public void run() {
-//        this.data = Storage.populateList();
-//        this.ui = new Ui();
-//        this.parser = new Parser(data);
-//        this.ui.start();
-//        String responseMsg = "";
-//        while (true) {
-//            String input = this.ui.readInput();
-//            responseMsg = this.parser.parse(input);
-//            System.out.println(responseMsg);
-//            if (responseMsg.equals("Bye!")) {
-//                break;
-//            }
-//        }
-//    }
 
     /**
      * You should have your own function to generate a response to user input.
