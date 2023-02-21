6f3f60d7bda589adfa2755903fa546a3fd039ef1
Cyrus-Krispin <cyruskrispin@gmail.com>
2023-02-22 03:11:40 +0800

    Fix coding style issues

--- src/main/java/baymax/MainWindow.java
+++ src/main/java/baymax/MainWindow.java
@@ -2,7 +2,6 @@ package baymax;
 
 import javafx.application.Platform;
 import javafx.fxml.FXML;
-
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
