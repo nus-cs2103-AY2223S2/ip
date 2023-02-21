0b382ab9fa712800edba56365d7c53998146a175
daniellingzy <e0773883@u.nus.edu>
2023-02-21 17:56:35 +0800

    updated TestUiTesting.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,7 +11,7 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
          **/
 
-        System.out.println("Hello Brother\nWelcome to Brother Bot\n" + "Whats up what can I do for you mi amigo");
+        System.out.println("Hello Brother\nWelcome to Brother Bot\nWhats up what can I do for you mi amigo");
 
         Scanner inputScanner = new Scanner(System.in);
         String input;
@@ -21,49 +21,55 @@ public class Duke {
 
 
         while(true) {
-            input = inputScanner.nextLine();
+            if (inputScanner.hasNextLine()) {
+                input = inputScanner.nextLine();
 
-            // level-1 feature: exit when user types "bye"
-            if (input.equalsIgnoreCase("bye")) {
-                System.out.println("ok see you brother all love no cringe!");
-                break;
-            }
+                // level-1 feature: exit when user types "bye"
+                if (input.equalsIgnoreCase("bye")) {
+                    System.out.println("ok see you brother all love no cringe!");
+                    break;
+                }
 
-            // level-3 feature: use input to construct Task object and add to array + display array when required + mark Task as done
-            if (input.equalsIgnoreCase("display")) {
-                System.out.println("Here you go my brother!" );
-                for(int i = 0; i < storeIndex; i++) {
+                // level-3 feature: use input to construct Task object and add to array + display array when required + mark Task as done
+                if (input.equalsIgnoreCase("display")) {
+                    System.out.println("Here you go my brother!" );
+                    for(int i = 0; i < storeIndex; i++) {
+                        System.out.println((i + 1) + ". " + storage[i].toString());
+                    }
+                    System.out.println("Anything else I can do for you top G" );
+                } else if (input.length() >= 6 && input.substring(0,4).equalsIgnoreCase("mark")) {
+                    int i = Integer.parseInt(input.substring(5)) - 1;
+                    storage[i].markAsDone();
+                    System.out.println("Marked as you wish my brother:");
                     System.out.println((i + 1) + ". " + storage[i].toString());
-                }
-                System.out.println("Anything else I can do for you top G" );
-            } else if (input.length() >= 6 && input.substring(0,4).equalsIgnoreCase("mark")) {
-                int i = Integer.parseInt(input.substring(5)) - 1;
-                storage[i].markAsDone();
-                System.out.println("Marked as you wish my brother:");
-                System.out.println((i + 1) + ". " + storage[i].toString());
-                System.out.println(" Whats next?");
-            } else {
-                // Level-4 feature: Todo, Deadline, Event
-                // note existing exception: indexOf() is case sensitive so /by etc must be in right caps
-                if (input.substring(0, 4).equalsIgnoreCase("todo")) {
-                    storage[storeIndex] = new Todo(input);
-                } else if (input.substring(0, 5).equalsIgnoreCase("event")) {
-                    int startIndex = input.indexOf("/from ") + 6;
-                    int x = input.indexOf("/to ");
-                    int endIndex = x + 4;
-                    String start = input.substring(startIndex, x);
-                    String end = input.substring(endIndex);
-                    storage[storeIndex] = new Event(input, start, end);
-                } else if (input.substring(0, 8).equalsIgnoreCase("deadline")) {
-                    int startIndex = input.indexOf("/by ") + 4; // exception
-                    String deadline = input.substring(startIndex);
-                    storage[storeIndex] = new Deadline(input, deadline);
+                    System.out.println(" Whats next?");
                 } else {
-                    //storage[storeIndex] = new Task(input);
+                    // Level-4 feature: Todo, Deadline, Event
+                    // note existing exception: indexOf() is case sensitive so /by etc must be in right caps
+                    if (input.substring(0, 4).equalsIgnoreCase("todo")) {
+                        storage[storeIndex] = new Todo(input);
+                    } else if (input.substring(0, 5).equalsIgnoreCase("event")) {
+                        int startIndex = input.indexOf("/from ");
+                        int x = input.indexOf("/to ");
+                        int endIndex = x + 4;
+                        String description = input.substring(6, startIndex);
+                        String start = input.substring(startIndex + 6, x);
+                        String end = input.substring(endIndex);
+                        storage[storeIndex] = new Event(description, start, end);
+                    } else if (input.substring(0, 8).equalsIgnoreCase("deadline")) {
+                        int startIndex = input.indexOf("/by "); // exception
+                        String description = input.substring(9, startIndex);
+                        String deadline = input.substring(startIndex + 4);
+                        storage[storeIndex] = new Deadline(description, deadline);
+                    } else {
+                        storage[storeIndex] = new Task(input);
+                    }
+                    System.out.println("added to list my brother: \n" + storage[storeIndex].toString() + "\nNow you have " + (storeIndex + 1) + " tasks!");
+                    storeIndex++;
                 }
-                System.out.println("added to list my brother: \n " + storage[storeIndex].toString() + "\n Now you have " + (storeIndex + 1) + " tasks!");
-                storeIndex++;
             }
+
+
         }
 
     }
