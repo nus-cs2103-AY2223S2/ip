46a843876934680915339507107928e33e6a41a9
daniellingzy <e0773883@u.nus.edu>
2023-02-23 19:05:28 +0800

    Added Level-6 features.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Duke {
@@ -15,16 +16,13 @@ public class Duke {
 
         Scanner inputScanner = new Scanner(System.in);
         String input;
-        Task[] storage = new Task[100];
-        int storeIndex = 0;
-
-
+        ArrayList<Task> storage = new ArrayList<>();
 
         while(true) {
             if (inputScanner.hasNextLine()) {
                 input = inputScanner.nextLine();
                 try {
-                    validateInput(input);
+                    validateInput(input, storage);
                 } catch(DukeException x) {
                     System.out.println(x.getMessage());
                     continue;
@@ -39,21 +37,29 @@ public class Duke {
                 // level-3 feature: use input to construct Task object and add to array + display array when required + mark Task as done
                 if (input.equalsIgnoreCase("display")) {
                     System.out.println("Here you go my brother!" );
-                    for(int i = 0; i < storeIndex; i++) {
-                        System.out.println((i + 1) + ". " + storage[i].toString());
+                    int i = 0;
+                    for(Task task: storage) {
+                        System.out.println((i + 1) + ". " + task.toString());
+                        i++;
                     }
                     System.out.println("Anything else I can do for you top G" );
                 } else if (input.length() >= 6 && input.substring(0,4).equalsIgnoreCase("mark")) {
                     int i = Integer.parseInt(input.substring(5)) - 1;
-                    storage[i].markAsDone();
+                    storage.get(i).markAsDone();
                     System.out.println("Marked as you wish my brother:");
-                    System.out.println((i + 1) + ". " + storage[i].toString());
+                    System.out.println((i + 1) + ". " + storage.get(i).toString());
                     System.out.println(" Whats next?");
+                } else if (input.length() > 7 && input.substring(0,6).equalsIgnoreCase("delete")) {
+                    int i = Integer.parseInt(input.substring(7)) - 1;
+                    Task removed = storage.get(i);
+                    storage.remove(i);
+                    System.out.println("Deleted this task for you my brother:\n" + removed.toString());
+                    System.out.println("Now you have " + storage.size() + " tasks left");
                 } else {
                     // Level-4 feature: Todo, Deadline, Event
                     // note existing exception: indexOf() is case sensitive so /by etc must be in right caps
                     if (input.substring(0, 4).equalsIgnoreCase("todo")) {
-                        storage[storeIndex] = new Todo(input);
+                        storage.add(new Todo(input));
                     } else if (input.substring(0, 5).equalsIgnoreCase("event")) {
                         int startIndex = input.indexOf("/from ");
                         int x = input.indexOf("/to ");
@@ -61,17 +67,17 @@ public class Duke {
                         String description = input.substring(6, startIndex);
                         String start = input.substring(startIndex + 6, x);
                         String end = input.substring(endIndex);
-                        storage[storeIndex] = new Event(description, start, end);
+                        storage.add(new Event(description, start, end));
                     } else if (input.substring(0, 8).equalsIgnoreCase("deadline")) {
                         int startIndex = input.indexOf("/by "); // exception
                         String description = input.substring(9, startIndex);
                         String deadline = input.substring(startIndex + 4);
-                        storage[storeIndex] = new Deadline(description, deadline);
+                        storage.add(new Deadline(description, deadline));
                     } else {
-                        storage[storeIndex] = new Task(input);
+                        storage.add(new Task(input));
                     }
-                    System.out.println("added to list my brother: \n" + storage[storeIndex].toString() + "\nNow you have " + (storeIndex + 1) + " tasks!");
-                    storeIndex++;
+                    int x = storage.size();
+                    System.out.println("added to list my brother: \n" + x + "." + storage.get(x - 1).toString() + "\nNow you have " + x + " tasks!");
                 }
             }
 
@@ -82,19 +88,30 @@ public class Duke {
 
     }
 
-    public static void validateInput(String input) throws DukeException {
-        if (input.indexOf("todo") < 0 && input.indexOf("event") < 0 && input.indexOf("display") < 0 && input.indexOf("deadline") < 0 && input.indexOf("mark") < 0) {
+    public static void validateInput(String input, ArrayList<Task> storage) throws DukeException {
+        if (input.indexOf("todo") < 0 && input.indexOf("event") < 0 && input.indexOf("display") < 0 && input.indexOf("deadline") < 0 && input.indexOf("mark") < 0 && input.indexOf("bye") < 0 && input.indexOf("delete") < 0) {
             throw new DukeException("OOPS! invalid command la bro");
         }
-        if (input.substring(0, 4).equalsIgnoreCase("todo") && input.length() <= 5) {
+        if (input.length() > 4 && input.substring(0, 4).equalsIgnoreCase("todo") && input.length() <= 5) {
             throw new DukeException("OOPS wrong format my brother! consider this format: \ntodo xxx");
         }
 
-        if (input.substring(0, 5).equalsIgnoreCase("event") && (input.indexOf("/from") < 0 || input.indexOf("/to") < 0 || input.indexOf("/from") > input.indexOf("/to"))) {
+        if (input.length() > 5 && input.substring(0, 5).equalsIgnoreCase("event") && (input.indexOf("/from") < 0 || input.indexOf("/to") < 0 || input.indexOf("/from") > input.indexOf("/to"))) {
             throw new DukeException("OOPS wrong format my brother! consider this format: \nevent xxxx /from xxx /to xxx");
         }
 
-        if (input.substring(0, 8).equalsIgnoreCase("deadline") && input.indexOf("/by") < 0) {
+        if (input.length() > 6 && input.substring(0, 6).equalsIgnoreCase("delete")) {
+            try {
+                Integer.parseInt(input.substring(7));
+            } catch(NumberFormatException e) {
+                throw new DukeException("OOPS wrong format my brother! consider this format: \ndelete INSERT_NUMBER");
+            }
+            if (Integer.parseInt(input.substring(7)) > storage.size()) {
+                throw new DukeException("OOPS inserted number is invalid");
+            }
+        }
+
+        if (input.length() > 8 && input.substring(0, 8).equalsIgnoreCase("deadline") && input.indexOf("/by") < 0) {
             throw new DukeException("OOPS wrong format my brother! consider this format: \nevent xxxx /from xxx /to xxx");
         }
 
