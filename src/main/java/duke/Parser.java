b9b9c899b2ad042977769bafdac9e2fa6c2347bc
Sachin Kalinga <>
2023-02-23 12:57:50 +0800

    Added JavaFX dependencies to the gradle file

--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -13,6 +13,7 @@ public class Parser {
     /**
      * Constructor for the Parser, which takes in a Storage object to facilitate the updating of the storage
      * file each time there is a task executed
+     *
      * @param storage The storage object the handles storing of the tasks to a file so that it could be retrieved
      *                for subsequent uses of the bot
      */
@@ -22,123 +23,124 @@ public class Parser {
 
     /**
      * This method interprets the user input and calls the relevant methods to facilitate the request of the user
+     *
      * @param userInput the String entered by the user specifying their request
-     * @param list the TaskList object that stores all the user's tasks
+     * @param list      the TaskList object that stores all the user's tasks
      * @throws IOException
      */
     public void parseAndExecute(String userInput, TaskList list) throws IOException {
 
-            String[] userInputComponents = userInput.split(" ");
-            if (userInputComponents.length == 0) {
-                System.out.println("Your request cannot be empty! Please re-enter your request");
-                return;
+        String[] userInputComponents = userInput.split(" ");
+        if (userInputComponents.length == 0) {
+            System.out.println("Your request cannot be empty! Please re-enter your request");
+            return;
+        }
+
+        String requestType = userInputComponents[0];
+        try {
+            Request request = Request.getRequest(requestType);
+
+            switch (request) {
+            case LIST: {
+                UI.printHorizontalLine();
+                list.printItems();
+                list.getTaskDetails();
+                UI.printHorizontalLine();
+                break;
             }
 
-            String requestType = userInputComponents[0];
-            try {
-                Request request = Request.getRequest(requestType);
+            case MARK: {
+                if (userInputComponents.length != 2) {
+                    System.out.println("This request requires exactly one task number as the second argument!");
+                    return;
+                }
+                int taskNumber = Integer.parseInt(userInputComponents[1]);
+                UI.printHorizontalLine();
+                list.markDone(taskNumber);
+                list.getTaskDetails();
+                UI.printHorizontalLine();
+                ;
+                break;
+            }
 
-                switch (request) {
-                case LIST: {
+            case UNMARK: {
+                if (userInputComponents.length != 2) {
+                    System.out.println("This request requires exactly one task number as the second argument!");
+                    return;
+                }
+                int taskNumber = Integer.parseInt(userInputComponents[1]);
+                UI.printHorizontalLine();
+                list.markUndone(taskNumber);
+                list.getTaskDetails();
+                UI.printHorizontalLine();
+                break;
+            }
+
+            case TODO: {
+                try {
                     UI.printHorizontalLine();
-                    list.printItems();
+                    list.addTask(new ToDo(userInput.substring(5).strip()));
                     list.getTaskDetails();
                     UI.printHorizontalLine();
-                    break;
+                } catch (StringIndexOutOfBoundsException e) {
+                    System.out.println("The description of todo cannot be empty!");
                 }
+                break;
+            }
+            case DEADLINE: {
+                String[] splitDeadline = userInput.split("/");
+                String description = splitDeadline[0].substring(9).strip();
+                String deadline = splitDeadline[1];
+                UI.printHorizontalLine();
+                list.addTask(new Deadline(description, deadline));
+                list.getTaskDetails();
+                UI.printHorizontalLine();
+                break;
+            }
+            case EVENT: {
+                String[] splitTimes = userInput.split("/");
+                String description = splitTimes[0].substring(6);
+                String startDayTime = splitTimes[1];
+                String endDayTime = splitTimes[2];
+                UI.printHorizontalLine();
+                list.addTask(new Event(startDayTime, endDayTime, description));
+                list.getTaskDetails();
+                UI.printHorizontalLine();
+                break;
+            }
 
-                case MARK: {
-                    if (userInputComponents.length != 2) {
-                        System.out.println("This request requires exactly one task number as the second argument!");
-                        return;
-                    }
-                    int taskNumber = Integer.parseInt(userInputComponents[1]);
-                    UI.printHorizontalLine();
-                    list.markDone(taskNumber);
-                    list.getTaskDetails();
-                    UI.printHorizontalLine();
-                    ;
-                    break;
+            case DELETE: {
+                if (userInputComponents.length != 2) {
+                    System.out.println("You have to specify a task number to be deleted!");
+                    return;
                 }
+                UI.printHorizontalLine();
+                list.deleteTask(Integer.parseInt(userInputComponents[1]));
+                UI.printHorizontalLine();
+                break;
+            }
 
-                case UNMARK: {
-                    if (userInputComponents.length != 2) {
-                        System.out.println("This request requires exactly one task number as the second argument!");
-                        return;
-                    }
-                    int taskNumber = Integer.parseInt(userInputComponents[1]);
-                    UI.printHorizontalLine();
-                    list.markUndone(taskNumber);
-                    list.getTaskDetails();
-                    UI.printHorizontalLine();
-                    break;
+            case FIND: {
+                if (userInputComponents.length != 2) {
+                    System.out.println("You have to enter a keyword to find a task with a match!");
+                    return;
                 }
-
-                case TODO: {
-                    try {
-                        UI.printHorizontalLine();
-                        list.addTask(new ToDo(userInput.substring(5).strip()));
-                        list.getTaskDetails();
-                        UI.printHorizontalLine();
-                    } catch (StringIndexOutOfBoundsException e) {
-                        System.out.println("The description of todo cannot be empty!");
-                    }
-                    break;
-                }
-                case DEADLINE: {
-                    String[] splitDeadline = userInput.split("/");
-                    String description = splitDeadline[0].substring(9).strip();
-                    String deadline = splitDeadline[1];
-                    UI.printHorizontalLine();
-                    list.addTask(new Deadline(description, deadline));
-                    list.getTaskDetails();
-                    UI.printHorizontalLine();
-                    break;
-                }
-                case EVENT: {
-                    String[] splitTimes = userInput.split("/");
-                    String description = splitTimes[0].substring(6);
-                    String startDayTime = splitTimes[1];
-                    String endDayTime = splitTimes[2];
-                    UI.printHorizontalLine();
-                    list.addTask(new Event(startDayTime, endDayTime, description));
-                    list.getTaskDetails();
-                    UI.printHorizontalLine();
-                    break;
-                }
-
-                case DELETE: {
-                    if (userInputComponents.length != 2) {
-                        System.out.println("You have to specify a task number to be deleted!");
-                        return;
-                    }
-                    UI.printHorizontalLine();
-                    list.deleteTask(Integer.parseInt(userInputComponents[1]));
-                    UI.printHorizontalLine();
-                    break;
-                }
-
-                case FIND: {
-                    if (userInputComponents.length != 2) {
-                        System.out.println("You have to enter a keyword to find a task with a match!");
-                        return;
-                    }
-                    String keyword = userInputComponents[1].strip();
-                    UI.printHorizontalLine();
-                    list.findTask(keyword);
-                    UI.printHorizontalLine();
-                    break;
-
-                }
-                default: {
-                    System.out.println("You may have accidentally entered in an invalid command. Please re-enter!");
-                }
-
-                }
-            } catch (DukeException e) {
-                System.out.println("Invalid Duke Request; please re-enter your request!");
+                String keyword = userInputComponents[1].strip();
+                UI.printHorizontalLine();
+                list.findTask(keyword);
+                UI.printHorizontalLine();
+                break;
 
             }
+            default: {
+                System.out.println("You may have accidentally entered in an invalid command. Please re-enter!");
+            }
+
+            }
+        } catch (DukeException e) {
+            System.out.println("Invalid Duke Request; please re-enter your request!");
+
+        }
 
         try {
             storage.updateTasksInFile(list);
