072cd381dd8db6a50e1d77ae4cda787999ad9591
jnjy <jefferyng2000@gmail.com>
2023-02-23 23:14:57 +0800

    Fix: remove bugged emoji

--- src/main/java/chad/exception/UnknownCommandException.java
+++ src/main/java/chad/exception/UnknownCommandException.java
@@ -10,6 +10,6 @@ public class UnknownCommandException extends DukeException {
      * Constructor to create an exception for unknown command
      */
     public UnknownCommandException() {
-        super("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+        super("OOPS!!! I'm sorry, but I don't know what that means :-(");
     }
 }
