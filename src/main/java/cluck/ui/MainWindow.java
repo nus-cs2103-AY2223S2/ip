d6dd838e56e67fe0e4b6e2031c8f23a5e62806ab
ChickenChiang <96032660+ChickenChiang@users.noreply.github.com>
2023-03-02 14:56:15 +0800

    Added XML files and restructured the code for better  OOP
    Classes that make up the Ui are now packages in the Ui package, namely DialogBox and MainWindow
    
    XML files have also been added such that scenebuilder can be used to format JavaFx objects

---
+++ src/main/java/cluck/ui/MainWindow.java
@@ -0,0 +1,53 @@
+package cluck.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+import cluck.Cluck;
+/**
+ * Controller for MainWindow. Provides the layout for the other controls.
+ */
+public class MainWindow extends AnchorPane {
+    @FXML
+    private ScrollPane scrollPane;
+    @FXML
+    private VBox dialogContainer;
+    @FXML
+    private TextField userInput;
+    @FXML
+    private Button sendButton;
+
+    private Cluck cluck;
+
+    private final Image userImage = new Image(this.getClass().getResourceAsStream("/images/Cluck.png"));
+    private final Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/Monke.jpg"));
+
+    @FXML
+    public void initialize() {
+        scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
+    }
+
+    public void setCluck(Cluck c) {
+        cluck = c;
+    }
+
+    /**
+     * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
+     * the dialog container. Clears the user input after processing.
+     */
+    @FXML
+    private void handleUserInput() {
+        String input = userInput.getText();
+        String response = cluck.getResponse(input);
+        dialogContainer.getChildren().addAll(
+                DialogBox.getUserDialog(input, userImage),
+                DialogBox.getDukeDialog(response, dukeImage)
+        );
+        userInput.clear();
+    }
+}
