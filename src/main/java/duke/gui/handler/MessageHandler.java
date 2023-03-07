ccbc9a9d1ecf26d0278c75e7b95252394367ad2c
StanleyNeoh <95394480+StanleyNeoh@users.noreply.github.com>
2023-03-07 13:49:30 +0800

    Use FXML

--- src/main/java/duke/gui/handler/MessageHandler.java
+++
@@ -1,9 +0,0 @@
-package duke.gui.handler;
-
-/**
- * MessageHandler is a functional interface that is used to handle an incoming message from the GUI.
- */
-@FunctionalInterface
-public interface MessageHandler {
-    String handle(String command);
-}
