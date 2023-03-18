a1bf68e3fc2614c2777cad98293dccb0c8257c16
jayhee3 <jiayuanhee@gmail.com>
2023-03-19 01:47:39 +0800

    Fix GUI bugs

--- src/main/java/duke/Main.java
+++ src/main/java/duke/Main.java
@@ -8,7 +8,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  * A GUI for Duke using FXML.
