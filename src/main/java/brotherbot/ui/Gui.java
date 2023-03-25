06f1d190cee57cd0e35d95ed2f7cb29139d5041c
daniellingzy <e0773883@u.nus.edu>
2023-03-25 16:40:15 +0800

    enhance code quality

--- src/main/java/brotherbot/ui/Gui.java
+++ src/main/java/brotherbot/ui/Gui.java
@@ -45,47 +45,21 @@ public class Gui {
         dialogContainer = new VBox();
         scrollPane.setContent(dialogContainer);
 
-        // Stage Setup
-        scrollPane.setStyle("-fx-background: #121c2b");
-        BackgroundImage backgroundImage = new BackgroundImage(background,
-                BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT,
-                BackgroundPosition.CENTER, BackgroundSize.DEFAULT);
-        Background background = new Background(backgroundImage);
-        dialogContainer.setBackground(background);
-
         userInput = new TextField();
         Button sendButton = new Button("SEND");
         AnchorPane mainLayout = new AnchorPane();
         mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
-        Scene scene = new Scene(mainLayout);
 
-        stage.setTitle("BrotherBot");
-        stage.setResizable(false);
-        stage.setMinHeight(600.0);
-        stage.setMinWidth(700.0);
-
-        mainLayout.setPrefSize(700.0, 600.0);
-
-        scrollPane.setPrefSize(700, 565);
-        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-        scrollPane.setVvalue(1.0);
-        scrollPane.setFitToWidth(true);
-
-        dialogContainer.setPrefHeight(565);
-        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
-
-        userInput.setPrefWidth(640.0);
-
-        sendButton.setPrefWidth(55.0);
-
-        AnchorPane.setTopAnchor(scrollPane, 1.0);
-        AnchorPane.setBottomAnchor(sendButton, 1.0);
-        AnchorPane.setRightAnchor(sendButton, 1.0);
-        AnchorPane.setLeftAnchor(userInput , 1.0);
-        AnchorPane.setBottomAnchor(userInput, 1.0);
+        formatStage(stage);
+        formatScrollPane(scrollPane);
+        formatAnchorPane(mainLayout);
+        formatDialogContainer(dialogContainer);
+        formatSendButton(sendButton);
+        formatUserInput(userInput);
+        anchor(scrollPane, sendButton);
 
         // User Interaction Setup
+        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
         sendButton.setOnMouseClicked((event) -> {
             handleUserInput();
         });
@@ -94,6 +68,7 @@ public class Gui {
             handleUserInput();
         });
 
+        Scene scene = new Scene(mainLayout);
         stage.setScene(scene);
     }
 
@@ -121,6 +96,52 @@ public class Gui {
             delay.setOnFinished(event -> stage.close());
             delay.play();
         }
+    }
+
+    private void  formatStage(Stage stage) {
+        stage.setTitle("BrotherBot");
+        stage.setResizable(false);
+        stage.setMinHeight(600.0);
+        stage.setMinWidth(700.0);
+    }
+
+    private void formatScrollPane(ScrollPane scrollPane) {
+        scrollPane.setPrefSize(700, 565);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
+        scrollPane.setStyle("-fx-background: #121c2b");
+    }
+
+    private void formatAnchorPane(AnchorPane mainLayout) {
+        mainLayout.setPrefSize(700.0, 600.0);
+    }
+
+    private void formatDialogContainer(VBox dialogContainer) {
+        BackgroundImage backgroundImage = new BackgroundImage(background,
+                BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT,
+                BackgroundPosition.CENTER, BackgroundSize.DEFAULT);
+        Background background = new Background(backgroundImage);
+        dialogContainer.setBackground(background);
+        dialogContainer.setPrefHeight(565);
+    }
+
+
+    private void anchor(ScrollPane scrollPane, Button sendButton) {
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+        AnchorPane.setLeftAnchor(userInput , 1.0);
+        AnchorPane.setBottomAnchor(userInput, 1.0);
+    }
+
+    private void formatSendButton(Button sendButton) {
+        sendButton.setPrefWidth(55.0);
+    }
+
+    private void formatUserInput(TextField userInput) {
+        userInput.setPrefWidth(640.0);
     }
 
 }
