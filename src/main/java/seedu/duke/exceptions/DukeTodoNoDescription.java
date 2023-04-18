9d8879919daaebc628e986520b8e9fb90bab58bc
thedesalizes <77891790+thedesalizes@users.noreply.github.com>
2023-04-18 21:42:13 +0800

    Fix some styling issues

--- src/main/java/seedu/duke/exceptions/DukeTodoNoDescription.java
+++ src/main/java/seedu/duke/exceptions/DukeTodoNoDescription.java
@@ -1,6 +1,6 @@
 package seedu.duke.exceptions;
 
-public class DukeTodoNoDescription extends DukeException{
+public class DukeTodoNoDescription extends DukeException {
 
     public DukeTodoNoDescription() {
         super("☹ OOPS!!! The description of a todo cannot be empty.");
